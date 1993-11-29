c67d983262f967a07a243e87fa26be6dc09e3c19
condor <condor>
1993-11-29 23:18:19 +0000

    Original CVS version of Condor - imported on Nov 29, 1993.

---
+++ src/condor_includes/_condor_fix_resource.h
@@ -0,0 +1,47 @@
+#ifndef _RESOURCE_H
+#define _RESOURCE_H
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#if !defined(HPUX9)
+#include <sys/resource.h>
+#else
+
+/*
+ * Resource utilization information.
+ */
+
+#define	RUSAGE_SELF	0
+#define	RUSAGE_CHILDREN	-1
+
+struct	rusage {
+	struct timeval ru_utime;	/* user time used */
+	struct timeval ru_stime;	/* system time used */
+					/* actual values kept in proc struct */
+	long	ru_maxrss;
+#define	ru_first	ru_ixrss
+	long	ru_ixrss;		/* integral shared memory size */
+	long	ru_idrss;		/* integral unshared data " */
+	long	ru_isrss;		/* integral unshared stack " */
+	long	ru_minflt;		/* page reclaims */
+	long	ru_majflt;		/* page faults */
+	long	ru_nswap;		/* swaps */
+	long	ru_inblock;		/* block input operations */
+	long	ru_oublock;		/* block output operations */
+	long	ru_ioch;		/* # of characters read/written */
+	long	ru_msgsnd;		/* messages sent */
+	long	ru_msgrcv;		/* messages received */
+	long	ru_nsignals;		/* signals received */
+	long	ru_nvcsw;		/* voluntary context switches */
+	long	ru_nivcsw;		/* involuntary " */
+#define	ru_last		ru_nivcsw
+};
+#endif /* HPUX9 */
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* _RESOURCE_h */
