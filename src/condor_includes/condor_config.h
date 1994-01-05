9dddb115cbee3b34f71300148cb00d4e5a9b008a
condor <condor>
1994-01-05 20:54:05 +0000

    Added prototype for macro_expand().

--- src/condor_includes/condor_config.h
+++ src/condor_includes/condor_config.h
@@ -15,17 +15,19 @@ extern "C" {
 #endif
 
 #if defined(__STDC__) || defined(__cplusplus)
-void config ( char *a_out_name, CONTEXT *context );
-char * param ( char *name );
-void insert ( char *name, char *value, BUCKET *table[], int table_size );
-char * expand_macro ( char *value, BUCKET *table[], int table_size );
-char * lookup_macro ( char *name, BUCKET *table[], int table_size );
+	void config ( char *a_out_name, CONTEXT *context );
+	char * param ( char *name );
+	void insert ( char *name, char *value, BUCKET *table[], int table_size );
+	char * expand_macro ( char *value, BUCKET *table[], int table_size );
+	char * lookup_macro ( char *name, BUCKET *table[], int table_size );
+	char * macro_expand ( char *name );
 #else
-void config ();
-char * param ();
-insert();
-char * expand_macro();
-char * lookup_macro();
+	void config ();
+	char * param ();
+	insert();
+	char * expand_macro();
+	char * lookup_macro();
+	char * macro_expand();
 #endif
 
 #if defined(__cplusplus)
