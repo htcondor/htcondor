0ed7607809473a75c0c952298386329055c48eee
condor <condor>
1994-01-17 16:29:48 +0000

    In the POSIX environment Ultrix and HP-UX machines don't define struct timeval in <sys/time.h>, but outside the POSIX environment, they do.  Made the local definition for struct timeval conditional on being outside the POSIX environment to keep if from getting re-defined in this situation.

--- src/condor_includes/condor_fix_timeval.h
+++ src/condor_includes/condor_fix_timeval.h
@@ -2,10 +2,12 @@
 #define TIMEVAL_H
 
 #if defined(ULTRIX42) || defined(ULTRIX43) || defined(HPUX9)
+#if !defined(_ALL_SOURCE)
 struct timeval {
 	long    tv_sec;     /* seconds */
 	long    tv_usec;    /* and microseconds */
 };
+#endif	/* _ALL_SOURCE */
 #else
 #include <sys/time.h>
 #endif
