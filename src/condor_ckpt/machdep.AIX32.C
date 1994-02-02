5b3fef0acf17ef613461c1d972bbb38c1f255cde
condor <condor>
1994-02-02 02:37:15 +0000

    Checkpointing primitives for AIX 3.2

---
+++ src/condor_ckpt/machdep.AIX32.C
@@ -0,0 +1,72 @@
+#include "image.h"
+
+/*
+  Return starting address of the data segment
+*/
+extern int _data;
+long
+data_start_addr()
+{
+	return (long)&_data;
+}
+
+/*
+  Return ending address of the data segment
+*/
+long
+data_end_addr()
+{
+	return (long)sbrk( 0 );
+}
+
+/*
+  Return TRUE if the stack grows toward lower addresses, and FALSE
+  otherwise.
+*/
+BOOL StackGrowsDown()
+{
+	return TRUE;
+}
+
+/*
+  Return the index into the jmp_buf where the stack pointer is stored.
+  Expect that the jmp_buf will be viewed as an array of integers for
+  this.
+*/
+int JmpBufSP_Index()
+{
+	return 3;
+}
+
+/*
+  Return starting address of stack segment.
+*/
+long
+stack_start_addr()
+{
+	long	answer;
+
+	jmp_buf env;
+	(void)SETJMP( env );
+	return JMP_BUF_SP(env) & ~1023; // Curr sp, rounded down
+}
+
+/*
+  Return ending address of stack segment.
+*/
+#include <sys/pseg.h>
+long
+stack_end_addr()
+{
+	return USRSTACK;
+}
+
+/*
+  Patch any registers whose vaules should be different at restart
+  time than they were at checkpoint time.
+*/
+void
+patch_registers( void *generic_ptr )
+{
+	// Nothing Needed
+}
