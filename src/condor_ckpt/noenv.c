5e0c09731618cadb90e0e4d520b5059c69585448
condor <condor>
1994-02-03 22:18:09 +0000

    Just execute a command with a null environment.  This is so that we can execute commands with a consistent environment on different machines for process migration testing.

---
+++ src/condor_ckpt/noenv.c
@@ -0,0 +1,13 @@
+#define _POSIX_SOURCE
+#include <stdio.h>
+
+int
+main( int argc, char *argv[] )
+{
+	char	*path = argv[1];
+	char 	**args = &argv[1];
+	char	*foo = NULL;
+	char	**envp = &foo;
+
+	execve( path, args, envp );
+}
