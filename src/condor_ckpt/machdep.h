e5b6d19e564ad38e611205226b1b1be11b0ac29e
condor <condor>
1994-02-15 22:33:09 +0000

    Add sections for IRIX4 and IRIX5.  Also bring in definitions for SETJMP/LONGJMP which were previously in "image.h".

--- src/condor_ckpt/machdep.h
+++ src/condor_ckpt/machdep.h
@@ -3,18 +3,24 @@
 	extern "C" int brk( void * );
 	extern "C" void *sbrk( int );
 	typedef void (*SIG_HANDLER)();
+#	define SETJMP _setjmp
+#	define LONGJMP _longjmp
 
 #elif defined(ULTRIX43)
 
 	extern "C" char *brk( char * );
 	extern "C" char *sbrk( int );
 	typedef void (*SIG_HANDLER)();
+#	define SETJMP _setjmp
+#	define LONGJMP _longjmp
 
 #elif defined(SUNOS41)
 
 	extern "C" int brk( void * );
 	extern "C" void *sbrk( int );
 	typedef void (*SIG_HANDLER)();
+#	define SETJMP _setjmp
+#	define LONGJMP _longjmp
 
 #elif defined(OSF1)
 
@@ -22,6 +28,8 @@
 #	include <sys/types.h>
 	extern "C" void *sbrk( ssize_t );
 	typedef void (*SIG_HANDLER)( int );
+#	define SETJMP _setjmp
+#	define LONGJMP _longjmp
 
 #elif defined(HPUX9)
 
@@ -29,12 +37,34 @@
 	extern "C" void *sbrk( int );
 #	include <signal.h>
 	typedef void (*SIG_HANDLER)( __harg );
+#	define SETJMP _setjmp
+#	define LONGJMP _longjmp
 
 #elif defined(AIX32)
 
 	extern "C" int brk( void * );
 	extern "C" void *sbrk( int );
 	typedef void (*SIG_HANDLER)( int );
+#	define SETJMP _setjmp
+#	define LONGJMP _longjmp
+
+#elif defined(IRIX4)
+
+	extern "C" int brk( void * );
+	extern "C" void *sbrk( int );
+	typedef void (*SIG_HANDLER)( int );
+#	define SETJMP setjmp
+#	define LONGJMP longjmp
+	extern "C" int kill( pid_t, int );
+
+#elif defined(IRIX5)
+
+	extern "C" int brk( void * );
+	extern "C" void *sbrk( int );
+	typedef void (*SIG_HANDLER)();
+#	define SETJMP setjmp
+#	define LONGJMP longjmp
+	extern "C" int kill( pid_t, int );
 
 #else
 
