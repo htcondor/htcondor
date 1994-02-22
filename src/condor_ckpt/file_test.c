d76b4431e1a8f8120b338033652f2578657c8acf
condor <condor>
1994-02-22 22:28:20 +0000

    A very simple test program which accesses a file across several checkpoint/restarts.

--- src/condor_ckpt/file_test.c
+++ src/condor_ckpt/file_test.c
@@ -2,7 +2,14 @@
 
 #include <fcntl.h>
 #include "stdlib.h"
-#include "c_proto.h"
+
+#if defined(OSF1)
+const char	*DICTIONARY = "/usr/lbin/spell/american";
+#else
+const char	*DICTIONARY = "/usr/dict/words";
+#endif
+
+void cat_one_line( int fd );
 
 
 int
@@ -11,56 +18,86 @@ main( int argc, char *argv[] )
 	int		scm;
 	int		i;
 	int		fd1, fd2, fd3, fd4, fd5, fd6;
+	char	buf[20];
 
-	for( i=0; i<3; i++ ) {
-		if( PreOpen(i) < 0 ) {
-			perror( "PreOpen" );
-			exit( 1 );
-		}
+	if( argc > 1 ) {
+		restart();
 	}
 
-	scm = SetSyscalls( SYS_MAPPED );
-
-	fd1 = MarkFileOpen( 3, "foo", O_RDONLY, FI_RSC );
-	fd2 = MarkFileOpen( 4, "bar", O_WRONLY, FI_RSC );
-	fd3 = MarkFileOpen( 5, "glarch", O_RDWR, FI_RSC );
-
-	DoDup2( fd2, 12 );
-	DoDup2( fd2, 13 );
-	DumpOpenFds();
-	MarkFileClosed( fd2 );
-	DumpOpenFds();
-	MarkFileClosed( 12 );
+	printf( "At Startup:\n" );
 	DumpOpenFds();
 
-	MarkFileClosed( 0 );
-	MarkFileClosed( 1 );
-	MarkFileClosed( fd1 );
-	MarkFileClosed( fd2 );
-	MarkFileClosed( fd3 );
-	MarkFileClosed( 13 );
+	fd1 = open( DICTIONARY, O_RDONLY, 0 );
+	if( fd1 < 0 ) {
+		perror( "open" );
+		exit( 1 );
+	}
+	printf( "Opened Dictionary as %d\n", fd1 );
 	DumpOpenFds();
 
+	if( (fd2 = dup2(fd1,4)) < 0 ) {
+		perror( "dup" );
+		exit( 1 );
+	}
+	printf( "Duped %d onto %d\n", fd1, fd2 );
+	DumpOpenFds();
+
+	if( close(fd1) < 0 ) {
+		perror( "close" );
+		exit( 1 );
+	}
+	printf( "Closed %d\n", fd1 );
+	DumpOpenFds();
+
+	printf( "Duping %d onto %d\n", fd2, 12 );
+	fd3 = dup2( fd2, 12 );
+	if( fd3 < 0 ) {
+		perror( "dup" );
+		exit( 1 );
+	}
+	DumpOpenFds();
+
+	printf( "Reading from %d\n", fd3 );
+	for( i=0; i<10; i++ ) {
+		cat_one_line( fd3 );
+		ckpt();
+		DumpOpenFds();
+	}
+	write( 1, "\n", 1 );
 
 	/*
-	fd4 = MarkFileOpen( 3, "alpha", O_RDONLY, FI_DIRECT );
-	fd5 = MarkFileOpen( 4, "bravo", O_WRONLY, FI_DIRECT );
-	fd6 = MarkFileOpen( 5, "charlie", O_RDWR, FI_DIRECT );
-
-
-	MarkFileClosed( fd1 );
-	MarkFileClosed( fd2 );
-	MarkFileClosed( fd3);
-
-	DumpOpenFds();
-
-	MarkFileClosed( fd4 );
-	MarkFileClosed( fd5 );
-	MarkFileClosed( fd6);
-
+	printf( "Closing %d\n", fd2 );
+	if( close(fd2) < 0 ) {
+		perror( "close" );
+		exit( 1 );
+	}
 	DumpOpenFds();
 	*/
 
+	SaveFileState();
+	printf( "Saved File State:\n" );
+	DumpOpenFds();
+
+	RestoreFileState();
+	DumpOpenFds();
+
 	exit( 0 );
 	return 0;
+}
+
+void
+cat_one_line( int fd )
+{
+	char	buf[1];
+
+	for(;;) {
+		if( read(fd,buf,sizeof(buf)) != sizeof(buf) ) {
+			perror( "read" );
+			exit( 1 );
+		}
+		write( 1, buf, sizeof(buf) );
+		if( buf[0] == '\n' ) {
+			return;
+		}
+	}
 }
