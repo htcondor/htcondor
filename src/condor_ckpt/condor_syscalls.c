5dc8e11f721248052571d297c59b6109c3f771fe
condor <condor>
1994-02-22 22:25:29 +0000

    Stuff to manipulate the system call mode - local or remote and mapped or unmapped.

---
+++ src/condor_ckpt/condor_syscalls.c
@@ -0,0 +1,50 @@
+/*******************************************************************
+**
+** Manage system call mode and do remote system calls.
+**
+*******************************************************************/
+
+#define _POSIX_SOURCE
+#include <stdio.h>
+#include "condor_syscalls.h"
+
+static int		SyscallMode;
+
+int
+SetSyscalls( int mode )
+{
+	int	answer;
+	answer = SyscallMode;
+	SyscallMode = mode;
+	return answer;
+}
+
+BOOL
+LocalSysCalls()
+{
+	return SyscallMode & SYS_LOCAL;
+}
+
+BOOL
+RemoteSysCalls()
+{
+	return (SyscallMode & SYS_LOCAL) == 0;
+}
+
+BOOL
+MappingFileDescriptors()
+{
+	return (SyscallMode & SYS_MAPPED);
+}
+
+int
+REMOTE_syscall( int syscall_num, ... )
+{
+	fprintf(
+		stderr,
+		"Don't know how to do system call %d remotely - yet\n",
+		syscall_num
+	);
+	abort();
+	return -1;
+}
