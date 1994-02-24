b12adb74862376fe9f26d7c000e2ea65c13b951e
condor <condor>
1994-02-24 21:44:26 +0000

    Fix up syntax error which only affected AIX machines.

--- src/condor_ckpt/fcntl.h
+++ src/condor_ckpt/fcntl.h
@@ -1,7 +1,7 @@
 #if defined(__cplusplus) && !defined(OSF1)	/* GNU G++ */
 #	include "fix_gnu_fcntl.h"
 #elif defined(AIX32)						/* AIX bsdcc */
-#	typedef unsigned short ushort;
+	typedef unsigned short ushort;
 #	include <fcntl.h>
 #else										/* Everybody else */
 #	include <fcntl.h>
