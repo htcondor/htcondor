6b948dba99426ee216e62ff541486f9abf4ea956
condor <condor>
1994-02-24 23:09:47 +0000

    Fix fcntl() prototype for HP-UX 9 machines.

--- src/condor_ckpt/fcntl.h
+++ src/condor_ckpt/fcntl.h
@@ -3,6 +3,11 @@
 #elif defined(AIX32)						/* AIX bsdcc */
 	typedef unsigned short ushort;
 #	include <fcntl.h>
+#elif defined(HPUX9)						/* HPUX 9 */
+#	define fcntl __condor_hide_fcntl
+#	include <fcntl.h>
+#	undef fcntl
+	int fcntl( int fd, int req, int arg );
 #else										/* Everybody else */
 #	include <fcntl.h>
 #endif
