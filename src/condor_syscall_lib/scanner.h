b1e1910a974865a362546d7d4bee35becb69b5e2
condor <condor>
1994-03-21 22:33:22 +0000

    Add boolean type so yacc productions can return a boolean value. Add parameter mode type so we can tag parameters as IN, OUT, or BOTH.

--- src/condor_syscall_lib/scanner.h
+++ src/condor_syscall_lib/scanner.h
@@ -13,15 +13,26 @@ struct node {
 	char	*id;
 	int		is_ptr;
 	int		is_const;
+	int		is_const_ptr;
 	int		is_mapped;
 	int		is_array;
+	int		in_param;
+	int		out_param;
 	int		extract;
 	struct node	*next;
 	struct node *prev;
 };
-	
+
+struct p_mode {
+	int		in;
+	int		out;
+};
+
+
 union yystype {
-	struct token tok;
-	struct node *node;
+	struct	token tok;
+	struct	node *node;
+	struct	p_mode param_mode;
+	int		bool;
 };
 #define YYSTYPE union yystype
