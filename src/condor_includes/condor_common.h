9b44ec9c94d0f0665dc97d3fdfb8856f018aaccb
condor <condor>
1994-07-06 18:48:20 +0000

    Changed to "fixed" version of <limits.h>.

--- src/condor_includes/condor_common.h
+++ src/condor_includes/condor_common.h
@@ -1,10 +1,9 @@
 #include "condor_fix_stdio.h"
 #include <stdlib.h>
 #include "condor_fix_unistd.h"
-#include <limits.h>
+#include "condor_fix_limits.h"
 #include <string.h>
 #include <ctype.h>
 #include "_condor_fix_types.h"
 #include <fcntl.h>
 #include <errno.h>
-
