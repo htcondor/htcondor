e9148261b2105762119d5fecae3ef88d653d519f
condor <condor>
1994-07-06 18:53:02 +0000

    Simplify a bit as special treatment is no longer needed for HPUX.

--- src/condor_includes/condor_fix_timeval.h
+++ src/condor_includes/condor_fix_timeval.h
@@ -1,20 +1,17 @@
 #ifndef TIMEVAL_H
 #define TIMEVAL_H
 
-#if defined(ULTRIX42) || defined(ULTRIX43) || defined(HPUX9)
-#if !defined(_ALL_SOURCE)
-struct timeval {
-	long    tv_sec;     /* seconds */
-	long    tv_usec;    /* and microseconds */
-};
-struct  itimerval {
-    struct  timeval it_interval;    /* timer interval */
-    struct  timeval it_value;   /* current value */
-};
-
-#endif	/* _ALL_SOURCE */
+#if defined(ULTRIX43) && !defined(_ALL_SOURCE)
+	struct timeval {
+		long    tv_sec;     /* seconds */
+		long    tv_usec;    /* and microseconds */
+	};
+	struct  itimerval {
+		struct  timeval it_interval;    /* timer interval */
+		struct  timeval it_value;   /* current value */
+	};
 #else
-#include <sys/time.h>
+#	include <sys/time.h>
 #endif
 
 #endif
