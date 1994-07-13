7d2c8d0aed20b6d2b02c7bd79aed173664742a6e
condor <condor>
1994-07-13 19:35:42 +0000

    Add SUNOS41 to an existing conditional compilation directive.

--- src/condor_ckpt/condor_syscalls.h
+++ src/condor_ckpt/condor_syscalls.h
@@ -29,7 +29,7 @@ BOOL RemoteSysCalls();
 BOOL MappingFileDescriptors();
 int REMOTE_syscall( int syscall_num, ... );
 
-#if defined(OSF1) || defined(HPUX9)
+#if defined(OSF1) || defined(HPUX9) || defined(SUNOS41)
 	int syscall( int, ... );
 #endif
 
