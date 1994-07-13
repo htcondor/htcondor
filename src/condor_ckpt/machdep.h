7aa7be91d00413ba5341544b9554063984046886
condor <condor>
1994-07-13 19:39:43 +0000

    Use ansi c++ definition for typedef SIG_HANDLER.

--- src/condor_ckpt/machdep.h
+++ src/condor_ckpt/machdep.h
@@ -21,7 +21,7 @@
 
 	extern "C" int brk( void * );
 	extern "C" void *sbrk( int );
-	typedef void (*SIG_HANDLER)();
+	typedef void (*SIG_HANDLER)( ... );
 #	define SETJMP _setjmp
 #	define LONGJMP _longjmp
 
