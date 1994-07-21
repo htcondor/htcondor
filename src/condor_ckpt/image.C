d9aff259856aa418b3e64c386c9014f87488188b
condor <condor>
1994-07-21 15:44:21 +0000

    1. Add copyright notice. 2. When we want to terminate ourself with a signal, we set the handler to be SIG_DFL.  Don't try to do this for SIGKILL and SIGSTOP, as they cannot be handled, and the resulting sigaction() call results in an EINVAL.

--- src/condor_ckpt/image.C
+++ src/condor_ckpt/image.C
@@ -1,3 +1,31 @@
+/* 
+** Copyright 1994 by Miron Livny, and Mike Litzkow
+** 
+** Permission to use, copy, modify, and distribute this software and its
+** documentation for any purpose and without fee is hereby granted,
+** provided that the above copyright notice appear in all copies and that
+** both that copyright notice and this permission notice appear in
+** supporting documentation, and that the names of the University of
+** Wisconsin and the copyright holders not be used in advertising or
+** publicity pertaining to distribution of the software without specific,
+** written prior permission.  The University of Wisconsin and the 
+** copyright holders make no representations about the suitability of this
+** software for any purpose.  It is provided "as is" without express
+** or implied warranty.
+** 
+** THE UNIVERSITY OF WISCONSIN AND THE COPYRIGHT HOLDERS DISCLAIM ALL
+** WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES
+** OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE UNIVERSITY OF
+** WISCONSIN OR THE COPYRIGHT HOLDERS BE LIABLE FOR ANY SPECIAL, INDIRECT
+** OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+** OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+** OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE
+** OR PERFORMANCE OF THIS SOFTWARE.
+** 
+** Author:  Mike Litzkow
+**
+*/ 
+
 #define _POSIX_SOURCE
 
 #include <stdio.h>
@@ -660,12 +688,14 @@ terminate_with_sig( int sig )
 	SetSyscalls( SYS_LOCAL | SYS_UNMAPPED );
 
 		// Make sure we have the default action in place for the sig
-	act.sa_handler = (SIG_HANDLER)SIG_DFL;
-	sigemptyset( &act.sa_mask );
-	act.sa_flags = 0;
-	errno = 0;
-	if( sigaction(sig,&act,0) < 0 ) {
-		EXCEPT( "sigaction" );
+	if( sig != SIGKILL && sig != SIGSTOP ) {
+		act.sa_handler = (SIG_HANDLER)SIG_DFL;
+		sigemptyset( &act.sa_mask );
+		act.sa_flags = 0;
+		errno = 0;
+		if( sigaction(sig,&act,0) < 0 ) {
+			EXCEPT( "sigaction" );
+		}
 	}
 
 		// Send ourself the signal
