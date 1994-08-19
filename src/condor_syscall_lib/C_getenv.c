64f1a6180e4db17a9f350ada7eedc7c49cec712a
condor <condor>
1994-08-19 16:33:23 +0000

    This file is specifically needed for linking AIX fortran programs with the xlf compiler and libraries.

---
+++ src/condor_syscall_lib/C_getenv.c
@@ -0,0 +1,27 @@
+extern char	**environ;
+
+char *
+_getenv( name )
+char	*name;
+{
+	char	**envp;
+	char	*p1, *p2;
+
+	for( envp = environ; *envp; envp++ ) {
+		for( p1 = name, p2 = *envp; *p1 && *p2 && *p1 == *p2; p1++, p2++ )
+			;
+		if( *p1 == '\0' ) {
+			return p2;
+		}
+	}
+	return (char *)0;
+}
+
+char *
+getenv( name )
+char	*name;
+{
+	return _getenv(name);
+}
+
+sigset(){}
