672a4052a17ab24ca1d8a42c8434741e7af70d86
thakur <thakur>
1994-09-06 17:37:51 +0000

    C++ class for OS dependent parameters.

---
+++ src/condor_includes/condor_sysparam.h
@@ -0,0 +1,88 @@
+//
+// 	These are the condor system parameters. Their values are dependent on 
+//  the platform on which condor is running
+//
+enum SysParamName {
+			Arch            = 0,		// system architecture
+			OpSys              ,        // operating system
+			LoadAvg			   ,        // load average
+			SwapSpace          ,		// swap space
+			PhyMem             ,		// physical memory
+			CpuType            ,		// cpu type
+				};
+enum SysType
+{
+			Integer				,       // Integer
+			Float				,       // float , real
+			Char				,       // character
+			String				,       // strings
+			Long				,       // long
+			Other						// any other type
+};
+
+// the base virtual platform
+class BaseOs
+{
+public:
+	virtual	int	readSysParam(const SysParamName,char*& buffer,int& size,
+														SysType& type){};
+	virtual void	disSysParam(const SysParamName sys, const char*buffer, 
+									const int size, const SysType type){};
+};
+
+class SunOs		: public BaseOs
+{
+public:
+	SunOs();                      // do required initialization
+private:
+	int  	readSysParam(const SysParamName,char*& buffer,int& size,
+													SysType& type) ;
+	void	disSysParam(const SysParamName sys, const char*buffer, 
+								const int size, const SysType type);
+} ;
+class Osf1	: public BaseOs 
+{
+public:
+	Osf1();                      // do required initialisation
+private:
+	int  	readSysParam(const SysParamName, char*& buffer, int& size, 
+														SysType& type);
+	void	disSysParam(const SysParamName sys, const char*buffer, 
+									const int size, const SysType type);
+};
+
+
+class Ultrix	: public BaseOs 
+{
+public:
+	Ultrix();                      // do required initialisation
+private:
+	int  	readSysParam(const SysParamName, char*& buffer, int& size, 
+														SysType& type);
+	void	disSysParam(const SysParamName sys, const char*buffer, 
+									const int size, const SysType type);
+};
+
+class Aix	: public BaseOs 
+{
+public:
+	Aix();                      // do required initialisation
+private:
+	int  	readSysParam(const SysParamName, char*& buffer, int& size, 
+														SysType& type);
+	void	disSysParam(const SysParamName sys, const char*buffer, 
+									const int size, const SysType type);
+};
+
+class Hpux	: public BaseOs 
+{
+public:
+	Hpux();                      // do required initialisation
+private:
+	int  	readSysParam(const SysParamName, char*& buffer, int& size, 
+														SysType& type);
+	void	disSysParam(const SysParamName sys, const char*buffer, 
+									const int size, const SysType type);
+};
+
+extern  BaseOs*     platform ;
