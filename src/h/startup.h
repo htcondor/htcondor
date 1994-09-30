ede9100cc73dfc9a0ed831c1801a3d100cf5b9a9
condor <condor>
1994-09-30 18:51:29 +0000

    Structure defining information starter needs from the shadow to start up a user job.

---
+++ src/h/startup.h
@@ -0,0 +1,18 @@
+typedef struct {
+	int		cluster;				/* Condor Cluster # */
+	int		proc;					/* Condor Proc # */
+	int		job_class;				/* STANDARD, VANILLA, PVM, PIPE, ... */
+	uid_t	uid;					/* Execute job under this UID */
+	gid_t	gid;					/* Execute job under this pid */
+	pid_t	virt_pid;				/* PVM virt pid of this process */
+	int		soft_kill_sig;			/* Use this signal for a soft kill */
+	char	*a_out_name;			/* Where to get executable file */
+	char	*cmd;					/* Command name given by the user */
+	char	*args;					/* Command arguments given by the user */
+	char	*env;					/* Environment variables */
+	char	*iwd;					/* Initial working directory */
+	BOOLEAN	ckpt_wanted;			/* Whether user wants checkpointing */
+	BOOLEAN	is_restart;				/* Whether this run is a restart */
+	BOOLEAN	coredump_limit_exists;	/* Whether user wants to limit core size */
+	int		coredump_limit;			/* Limit in bytes */
+} STARTUP_INFO;
