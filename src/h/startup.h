b5220189bc6a5ec22efa7c6b8ae39bc166303c62
condor <condor>
1994-12-01 17:35:13 +0000

    Add version number and get rid if a.out name.

--- src/h/startup.h
+++ src/h/startup.h
@@ -1,12 +1,12 @@
 typedef struct {
+	int		version_num;			/* version of this structure */
 	int		cluster;				/* Condor Cluster # */
 	int		proc;					/* Condor Proc # */
 	int		job_class;				/* STANDARD, VANILLA, PVM, PIPE, ... */
 	uid_t	uid;					/* Execute job under this UID */
-	gid_t	gid;					/* Execute job under this pid */
+	gid_t	gid;					/* Execute job under this gid */
 	pid_t	virt_pid;				/* PVM virt pid of this process */
 	int		soft_kill_sig;			/* Use this signal for a soft kill */
-	char	*a_out_name;			/* Where to get executable file */
 	char	*cmd;					/* Command name given by the user */
 	char	*args;					/* Command arguments given by the user */
 	char	*env;					/* Environment variables */
@@ -16,6 +16,8 @@ typedef struct {
 	BOOLEAN	coredump_limit_exists;	/* Whether user wants to limit core size */
 	int		coredump_limit;			/* Limit in bytes */
 } STARTUP_INFO;
+
+#define STARTUP_VERSION 1
 
 /*
 Should eliminate starter knowing the a.out name.  Should go to
