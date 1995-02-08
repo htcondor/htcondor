7e5cacc6031cc2c7a125281ffb84942d12bc2f9a
pruyne <pruyne>
1995-02-08 19:54:27 +0000

    CARMI definition files: carmi.h - included by all CARMI programs. carmipro.h - definitions for CARMI protocols.  Included by CARMI library and the shadow.

---
+++ src/condor_includes/carmipro.h
@@ -0,0 +1,10 @@
+#if !defined(_CARMIPRO_H)
+#define _CARMIPRO_H
+#include "pvmsdpro.h"
+
+#define CARMI_FIRST	(SM_LAST+1)
+
+#define CARMI_RESPAWN (CARMI_FIRST+1)
+#define CARMI_CHKPT   (CARMI_FIRST+2)
+#define CARMI_ADDHOST (CARMI_FIRST+3)
+#endif
