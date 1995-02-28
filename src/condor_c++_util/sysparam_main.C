00a3bcc24d698cae4c29a5d2a6db9ec0e08d7a97
condor <condor>
1995-02-28 17:43:58 +0000

    Add "ptype" object for Linux.

--- src/condor_c++_util/sysparam_main.C
+++ src/condor_c++_util/sysparam_main.C
@@ -1,15 +1,17 @@
 #include "condor_sysparam.h"
 
 #if			defined(SUNOS41)
-SunOs       ptype;
+SunOs      	ptype;
 #elif  		defined(ULTRIX43) || defined(ULTRIX42)
-Ultrix      ptype;
+Ultrix     	ptype;
 #elif		defined(OSF1)
 Osf1		ptype;
 #elif		defined(HPUX9)
 Hpux		ptype;
 #elif 		defined(AIX32)
 Aix			ptype;
+#elif		defined(LINUX)
+Linux		ptype;
 #endif
 
 BaseOs*     platform =  &ptype;
