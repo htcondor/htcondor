cb69d5a6db9e5839e76304d4e53dc03471e7be31
rcmallen <rcmallen>
1995-03-10 23:22:11 +0000

    This has the DGRAM_IO_HANDLE datastructure which is used while changing all the 'send' calls to 'sendto' calls. Essentially, all the information required by the 'sendto' call (like the receiver's sockaddr_in, portnum etc) is packaged into this structure and passed around in the xdr code.

---
+++ src/h/dgram_io_handle.h
@@ -0,0 +1,14 @@
+#ifndef _DGRAM_IO_HANDLE_H
+#define _DGRAM_IO_HANDLE_H 
+
+/* this structure is used to compact together all the information
+   needed to 'send' datagram sockets from schedd and startd to the
+   collector.  -- Raghu 2/28/95
+*/
+
+typedef struct {
+        struct  sockaddr_in addr;
+        int     sock;
+} DGRAM_IO_HANDLE;
+
+#endif
