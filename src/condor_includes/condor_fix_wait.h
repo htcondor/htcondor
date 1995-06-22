0abb07f2b2d6e5c6d70d8886fd7af373c2452beb
pruyne <pruyne>
1995-06-22 22:00:06 +0000

    Borrow a whole bunch of defines and undefines from NON_POSIX.C of the starter which seems to know how to do this better than it was done here.

--- src/condor_includes/condor_fix_wait.h
+++ src/condor_includes/condor_fix_wait.h
@@ -1,6 +1,10 @@
 #ifndef FIX_WAIT_H
 #define FIX_WAIT_H
 
+#if defined(_POSIX_SOURCE)
+#define HOLD_POSIX_SOURCE
+#undef _POSIX_SOURCE
+#endif
 /* To get union wait on OSF1, _OSF_SOURCE & _BSD must be defined */
 
 #if defined(OSF1)
@@ -18,11 +22,38 @@
 #endif
 
 #if defined(AIX32)
+#       undef _POSIX_SOURCE
+#       define _BSD
+#       include <sys/m_wait.h>
+#elif defined(OSF1)
+#       undef _POSIX_SOURCE
+#       define _BSD
+#       define _OSF_SOURCE
+#       include <sys/wait.h>
+#elif defined(SUNOS41)
+#       undef _POSIX_SOURCE
+#       include <sys/wait.h>
+#elif defined(ULTRIX43)
+#       undef _POSIX_SOURCE
+#       include <sys/wait.h>
+#elif defined(HPUX9)
+#       undef _POSIX_SOURCE
+#       define _BSD
+#       include <sys/wait.h>
+#elif defined(LINUX)
+#       undef _POSIX_SOURCE
+#       define __USE_BSD
+#       include <sys/wait.h>
+#endif
+
+/*
+#if defined(AIX32)
 #	include <sys/m_wait.h>
 #	define WNOHANG 1
 #else
 #	include <sys/wait.h>
 #endif
+*/
 
 #if defined(OSF1)
 
@@ -37,5 +68,10 @@
 #	undef _TMP_BSD
 
 #endif /* OSF1 */
+
+#if defined(HOLD_POSIX_SOURCE)
+#define _POSIX_SOURCE
+#undef HOLD_POSIX_SOURCE
+#endif
 
 #endif
