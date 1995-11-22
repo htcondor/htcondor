ab5765b523a41c17ec91b24529d2524618117550
pruyne <pruyne>
1995-11-22 21:43:05 +0000

    This implements the mailto: URL which is used for sending mail to a given user.  This is implemented using the filter: URL which makes it very easy.

---
+++ src/condor_c++_util/mailto_url.C
@@ -0,0 +1,56 @@
+/* 
+** Copyright 1995 by Miron Livny and Jim Pruyne
+** 
+** Permission to use, copy, modify, and distribute this software and its
+** documentation for any purpose and without fee is hereby granted,
+** provided that the above copyright notice appear in all copies and that
+** both that copyright notice and this permission notice appear in
+** supporting documentation, and that the names of the University of
+** Wisconsin and the copyright holders not be used in advertising or
+** publicity pertaining to distribution of the software without specific,
+** written prior permission.  The University of Wisconsin and the 
+** copyright holders make no representations about the suitability of this
+** software for any purpose.  It is provided "as is" without express
+** or implied warranty.
+** 
+** THE UNIVERSITY OF WISCONSIN AND THE COPYRIGHT HOLDERS DISCLAIM ALL
+** WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES
+** OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE UNIVERSITY OF
+** WISCONSIN OR THE COPYRIGHT HOLDERS BE LIABLE FOR ANY SPECIAL, INDIRECT
+** OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+** OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+** OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE
+** OR PERFORMANCE OF THIS SOFTWARE.
+** 
+** Author:  Jim Pruyne
+**
+*/ 
+
+#include <string.h>
+#include "condor_common.h"
+#include "url_condor.h"
+
+#define MAIL_PROG "/bin/mail"
+
+static
+int condor_open_mailto_url( const char *name, int flags, 
+									   size_t n_bytes )
+{
+	char	*mail_prog;
+	char	filter_url[1000];
+
+	if( flags != O_WRONLY ) {
+		return -1;
+	} 
+
+#if !defined(MAIL_PROG)
+	mail_prog = param("MAIL");
+#else
+	mail_prog = MAIL_PROG;
+#endif
+	sprintf(filter_url, "filter:%s %s", mail_prog, name);
+	return open_url(filter_url, flags, n_bytes);
+}
+
+
+URLProtocol MAILTO_URL("mailto", "MailToUrl", condor_open_mailto_url);
