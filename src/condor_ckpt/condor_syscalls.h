49f0c31007a27a17d07b815278ba529ce0786bb5
dhaval <dhaval>
1995-12-06 07:36:23 +0000

    For Solaris syscall.h is in the /usr/include/sys directory

--- src/condor_ckpt/condor_syscalls.h
+++ src/condor_ckpt/condor_syscalls.h
@@ -31,8 +31,10 @@
 
 #if defined( AIX32)
 #	include "syscall.aix32.h"
+#elif defined(Solaris)
+#	include <sys/syscall.h> /* Solaris specific change ..dhaval 6/30 */
 #else
-#	include <syscall.h>
+#	include <syscall.h> 
 #endif
 
 
