d07a3dae5eb4e2ed0c0617bcf3860c069d76714b
weiruc <weiruc>
1996-03-24 18:53:43 +0000

    ast stuff + attrlist stuff

---
+++ src/condor_classad/attrlist.h
@@ -0,0 +1,165 @@
+//******************************************************************************
+// attrlist.h
+//
+// Definition of AttrList classes and AttrListList class.
+//
+//******************************************************************************
+
+#ifndef _ATTRLIST_H
+#define _ATTRLIST_H
+
+#include <fstream.h>
+
+#include "proc_obj.h"
+#include "condor_expressions.h"
+#include "exprtype.h"
+#include "astbase.h"
+
+enum {RANGE, FIXED};			// used by VarTypeTable and Sel_best_mach()
+enum							// various AttrLists
+{
+    ATTRLISTENTITY,
+    ATTRLISTREP
+};
+enum {AGG_INSERT, AGG_REMOVE};	// operations on aggregate classes
+
+class AttrListElem
+{
+    public :
+
+        AttrListElem(ExprTree*);			// constructor
+        AttrListElem(AttrListElem&);		// copy constructor
+        ~AttrListElem() { delete tree; }	// destructor
+
+        friend class AttrList;
+        friend class ClassAd;
+        friend class AttrListList;
+  
+    private :
+
+        ExprTree*		tree;	// the tree pointed to by this element
+		int				dirty;	// has this tree been changed?
+		char*			name;	// the name of the tree
+        AttrListElem*	next;	// next element in the list
+};
+
+class AttrListAbstract
+{
+    public :
+
+		int		Type() { return type; }		// type of the AttrList
+
+		friend	class		AttrList;
+		friend	class		AttrListList;
+
+    protected :
+
+		AttrListAbstract(int);
+		virtual ~AttrListAbstract() {}
+
+		int					type;		// type of this AttrList
+		class AttrListList*	inList;		// I'm in this AttrList list
+		AttrListAbstract*	next;		// next AttrList in the list
+		AttrListAbstract*	prev;		// previous AttrList in the list
+};
+
+class AttrListRep: public AttrListAbstract
+{
+    public:
+
+        AttrListRep(AttrList*, AttrListList*);	// constructor
+
+		friend			AttrList;
+		friend			AttrListList;
+
+    private:
+
+        AttrList*		attrList;		// the original AttrList 
+        AttrListRep*	nextRep;		// next copy of original AttrList 
+};
+
+class AttrList : public AttrListAbstract
+{
+    public :
+
+		AttrList();							// No associated AttrList list
+        AttrList(AttrListList*);			// Associated with AttrList list
+        AttrList(FILE *, char *, int &);	// Constructor, read from file.
+		AttrList(ProcObj*);					// create from a proc object
+		AttrList(CONTEXT*);					// create from a CONTEXT
+        AttrList(char *, char);				// Constructor, from string.
+        AttrList(AttrList&);				// copy constructor
+        virtual ~AttrList();				// destructor
+
+        int        	Insert(ExprTree*);		// insert at the tail
+        int			Delete(char*);			// delete the expr with the name
+
+		void		ResetFlags();			// reset the all the dirty flags
+		int			UpdateExpr(char*, ExprTree*);	// update an expression
+
+		void		ResetExpr() { this->ptrExpr = exprList; }
+		void		ResetName() { this->ptrName = exprList; }
+		ExprTree*	NextExpr();					// next unvisited expression
+		char*		NextName();					// next unvisited name
+        ExprTree*	Lookup(char*);				// look up an expression
+		int			IsInList(AttrListList*);	// am I in this AttrList list?
+
+		int			fPrintExpr(FILE*, char*);	// print an expression
+        virtual int	fPrint(FILE*);				// print the AttrList to a file
+
+		friend	class	AttrListRep;			// access "next" 
+		friend	class	AttrListList;			// access "UpdateAgg()"
+		friend	class	ClassAd;
+
+    private :
+
+		// update an aggregate expression if the AttrList list associated with
+		// this AttrList is changed
+      	int				UpdateAgg(ExprTree*, int);
+		// convert a (key, value) pair to an assignment tree. used by the
+		// constructor that builds an AttrList from a proc structure.
+		ExprTree*		ProcToTree(char*, LexemeType, int, float, char*);
+        AttrListElem*	exprList;		// my collection of expressions
+		AttrListList*	associatedList;	// the AttrList list I'm associated with
+		AttrListElem*	tail;			// used by Insert
+		AttrListElem*	ptrExpr;		// used by NextExpr 
+		AttrListElem*	ptrName;		// used by NextName
+};
+
+class AttrListList
+{
+    public:
+    
+        AttrListList();					// constructor
+        AttrListList(AttrListList&);	// copy constructor
+        ~AttrListList();				// destructor
+
+        void 	  	OpenList();			// set pointer to the head of the queue
+        void 	  	CloseList();		// set pointer to NULL
+        AttrList* 	NextAttrList();		// return AttrList pointed to by "ptr"
+        ExprTree* 	Lookup(char*, AttrList*&);	// look up an expression
+      	ExprTree* 	Lookup(char*);
+
+      	void 	  	Insert(AttrList*); 		// insert at the tail of the list
+      	int			Delete(AttrList*); 		// delete a AttrList
+
+      	void  	  	fPrintAttrListList(FILE *); 	// print out the list
+      	int 	  	MyLength() { return length; } 	// length of this list
+      	ExprTree* 	BuildAgg(char*, LexemeType);	// build aggregate expr
+
+      	friend	  	class		AttrList;
+      	friend	  	class		ClassAd;
+  
+    protected:
+
+        // update aggregate expressions in associated AttrLists
+		void				UpdateAgg(ExprTree*, int);
+
+        AttrListAbstract*	head;			// head of the list
+        AttrListAbstract*	tail;			// tail of the list
+        AttrListAbstract*	ptr;			// used by NextAttrList
+        AttrListList*		associatedAttrLists;	// associated AttrLists
+        int					length;			// length of the list
+};
+
+#endif
---
+++ src/condor_classad/classad.h
@@ -0,0 +1,59 @@
+//******************************************************************************
+// classad.h
+//
+// Definition of ClassAd classes and ClassAdList class. They are derived from
+// AttrList class and AttrListList class respectively.
+//
+//******************************************************************************
+
+#ifndef _CLASSAD_H
+#define _CLASSAD_H
+
+#include <fstream.h>
+
+#include "proc_obj.h"
+#include "condor_expressions.h"
+#include "exprtype.h"
+#include "ast.h"
+#include "attrlist.h"
+
+struct AdType                   // type of a ClassAd.
+{
+    int		number;             // type number, internal thing.
+    char*	name;               // type name.
+    
+    AdType(char * = NULL);      // constructor.
+    ~AdType();                  // destructor.
+};
+
+
+class ClassAd : public AttrList
+{
+    public :
+
+		ClassAd();								// No associated AttrList list
+		ClassAd(ProcObj*);						// create from a proc object
+		ClassAd(CONTEXT*);						// create from a CONTEXT
+        ClassAd(FILE *, char *, int &);			// Constructor, read from file.
+        ClassAd(char *, char);					// Constructor, from string.
+		ClassAd(const ClassAd&);				// copy constructor
+        virtual ~ClassAd();						// destructor
+
+        virtual int	fPrint(FILE*);				// print the AttrList to a file
+        void		SetMyTypeName(char *);		// my type name set.
+        char*		GetMyTypeName();			// my type name returned.
+        void 		SetTargetTypeName(char *);	// target type name set.
+        char*		GetTargetTypeName();		// target type name returned.
+        int			GetMyTypeNumber();			// my type number returned.
+        int			GetTargetTypeNumber();		// target type number returned.
+        int			IsAMatch(ClassAd*);			// tests whether it's a match
+												// mutually.
+    private :
+
+		AdType*		myType;						// my type field.
+        AdType*		targetType;					// target type field.
+};
+
+typedef	AttrList ClassAdList;
+
+#endif
