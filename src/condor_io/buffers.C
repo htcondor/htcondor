2101ee6d79b1a44ab024813bfbff6cc3b574af58
raman <raman>
1996-06-19 20:10:48 +0000

    New version of condor_io; now includes buffers.C and stream.C

---
+++ src/condor_io/buffers.C
@@ -0,0 +1,328 @@
+#define _POSIX_SOURCE
+
+#include "condor_common.h"
+#include "condor_constants.h"
+#include "condor_io.h"
+
+
+unsigned long num_created = 0;
+unsigned long num_deleted = 0;
+
+
+
+void
+sanity_check()
+{
+	fprintf(stderr, "Buffer management:\n");
+	fprintf(stderr, "    created: %d\n", num_created);
+	fprintf(stderr, "    deleted: %d\n\n", num_deleted);
+}
+
+
+Buf::Buf(
+	int	sz
+	)
+{
+	_dta = new char[sz];
+	_dta_maxsz = sz;
+	_dta_sz = 0;
+	_dta_pt = 0;
+
+	num_created++;
+}
+
+
+Buf::~Buf()
+{
+	if (_dta) delete [] _dta;
+	num_deleted++;
+}
+
+
+
+int Buf::write_to_fd(
+	int		fd,
+	int		sz
+	)
+{
+	int	nw;
+
+	if (sz < 0 || sz > num_untouched()) sz = num_untouched();
+
+	nw = write(fd, &_dta[num_touched()], sz);
+	if (nw <= 0) return -1;
+
+	_dta_pt += nw;
+	return nw;
+}
+
+
+int Buf::flush_to_fd(
+	int		fd,
+	void	*hdr,
+	int		sz
+	)
+{
+	int		dbg_fd;
+
+	if (sz > max_size()) return -1;
+	if (hdr && sz > 0){
+		memcpy(_dta, hdr, sz);
+	}
+
+
+	rewind();
+
+/* DEBUG SESSION
+	if ((dbg_fd = open("trace.snd", O_WRONLY|O_APPEND|O_CREAT, 0700)) < 0){
+		fprintf(stderr, "Error opening trace file\n");
+		exit(1);
+	}
+	if (write(dbg_fd, _dta, _dta_maxsz) != _dta_maxsz){
+		fprintf(stderr, "ERROR LOGGING\n");
+		return FALSE;
+	}
+	::close(dbg_fd);
+*/
+
+
+	sz = write_to_fd(fd);
+	reset();
+
+	return sz;
+}
+
+
+int Buf::read_frm_fd(
+	int		fd,
+	int		sz
+	)
+{
+	int	nr;
+	int nro;
+
+	if (sz < 0 || sz > num_free()){
+		fprintf(stderr, "Buffer too small\n");
+		return -1;
+		/* sz = num_free(); */
+	}
+
+	for(nr=0;nr <sz;){
+
+		nro = read(fd, &_dta[num_used()+nr], sz-nr);
+
+		if (nro <= 0) return -1;
+
+		nr += nro;
+	}
+
+	_dta_sz += nr;
+
+
+/* DEBUG SESSION
+	if ((dbg_fd = open("trace.rcv", O_WRONLY|O_APPEND|O_CREAT, 0700)) < 0){
+		fprintf(stderr, "Error opening trace file\n");
+		exit(1);
+	}
+	if (write(dbg_fd, _dta, _dta_maxsz) != _dta_maxsz){
+		fprintf(stderr, "ERROR LOGGING\n");
+		return FALSE;
+	}
+	::close(dbg_fd);
+*/
+
+	return nr;
+}
+
+
+
+int Buf::put_max(
+	const void	*dta,
+	int			sz
+	)
+{
+	if (sz > num_free()) sz = num_free();
+
+	memcpy(&_dta[num_used()], dta, sz);
+
+	_dta_sz += sz;
+	return sz;
+}
+
+
+int Buf::get_max(
+	void		*dta,
+	int			sz
+	)
+{
+	if (sz > num_untouched()) sz = num_untouched();
+
+	memcpy(dta, &_dta[num_touched()], sz);
+
+	_dta_pt += sz;
+	return sz;
+}
+
+
+int Buf::find(
+	char	delim
+	)
+{
+	char	*tmp;
+
+	if (!(tmp = (char *)memchr(&_dta[num_touched()], delim, num_untouched()))){
+		return -1;
+	}
+
+	return (tmp - &_dta[num_touched()]);
+}
+
+
+int Buf::peek(
+	char		&c
+	)
+{
+	if (empty() || consumed()) return FALSE;
+
+	c = _dta[num_touched()];
+	return TRUE;
+}
+
+
+
+int Buf::seek(
+	int		pos
+	)
+{
+	int	tmp;
+
+	tmp = _dta_pt;
+	_dta_pt = (pos < 0) ? 0 : ((pos < _dta_maxsz) ? pos : _dta_maxsz-1);
+	if (_dta_pt > _dta_sz) _dta_sz = _dta_pt;
+	return tmp;
+}
+
+
+
+void ChainBuf::reset()
+{
+	Buf	*trav;
+	Buf	*trav_n;
+
+	if (_tmp) { delete [] _tmp; _tmp = (char *)0; }
+
+	for(trav=_head; trav; trav=trav_n){
+		trav_n = trav->next();
+		delete trav;
+	}
+
+	_head = _tail = _curr = (Buf *)0;
+}
+
+
+int dbg_count = 0;
+
+int ChainBuf::get(
+	void	*dta,
+	int		sz
+	)
+{
+	int		last_incr;
+	int		nr;
+	int		i;
+
+	if (dbg_count++ > 307){
+		dbg_count--;
+	}
+
+
+	for(nr=0; _curr; _curr=_curr->next()){
+		last_incr = _curr->get_max(&((char *)dta)[nr], sz-nr);
+		nr += last_incr;
+		if (nr == sz) break;
+	}
+
+	return nr;
+}
+
+
+
+int ChainBuf::put(
+	Buf		*dta
+	)
+{
+	if (_tmp) { delete [] _tmp; _tmp = (char *)0; }
+
+	if (!_tail){
+		_head = _tail = _curr = dta;
+		dta->set_next((Buf *)0);
+	}
+	else{
+		_tail->set_next(dta);
+		_tail = dta;
+		_tail->set_next((Buf *)0);
+	}
+
+	return TRUE;
+}
+
+
+int ChainBuf::get_tmp(
+	void	*&ptr,
+	char	delim
+	)
+{
+	int	nr;
+	int	tr;
+	Buf	*trav;
+
+	if (_tmp) { delete [] _tmp; _tmp = (char *)0; }
+
+	if (!_curr) return -1;
+
+	/* case 1: in one buffer */
+	if ((tr = _curr->find(delim)) >= 0){
+		ptr = _curr->get_ptr();
+		nr = _curr->seek(0);
+		_curr->seek(nr+tr+1);
+		return tr+1;
+	}
+
+	/* case 2: string is in >1 buffers. */
+
+	nr = _curr->num_untouched();
+	if (!_curr->next()) return -1;
+
+	for(trav = _curr->next(); trav; trav = trav->next()){
+		if ((tr = trav->find(delim)) < 0){
+			nr += trav->num_untouched();
+		}
+		else{
+			nr += tr;
+			if (!(_tmp = new char[nr+1])) return -1;
+			get(_tmp, nr+1);
+			ptr = _tmp;
+			return nr+1;
+		}
+	}
+
+	return -1;
+}
+
+
+int ChainBuf::peek(
+	char	&c
+	)
+{
+	if (_tmp) { delete [] _tmp; _tmp = (char *)0; }
+	if (!_curr) return FALSE;
+
+
+	if (!_curr->peek(c)){
+		if (!(_curr = _curr->next())) return FALSE;
+		return _curr->peek(c);
+	}
+
+	return TRUE;
+}
+
---
+++ src/condor_io/stream.C
@@ -0,0 +1,646 @@
+/* 
+** Copyright 1993 by Miron Livny, Mike Litzkow, and Emmanuel Ackaouy.
+** 
+** Permission to use, copy, modify, and distribute this software and its
+** documentation for any purpose and without fee is hereby granted,
+** provided that the above copyright notice appear in all copies and that
+** both that copyright notice and this permission notice appear in
+** supporting documentation, and that the names of the University of
+** Wisconsin and the copyright holders not be used in advertising or
+** publicity pertaining to distribution of the software without specific,
+** written prior permission.  The University of Wisconsin and the 
+** copyright holders make no representations about the suitability of this
+** software for any purpose.  It is provided "as is" without express
+** or implied warranty.
+** 
+** THE UNIVERSITY OF WISCONSIN AND THE COPYRIGHT HOLDERS DISCLAIM ALL
+** WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES
+** OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE UNIVERSITY OF
+** WISCONSIN OR THE COPYRIGHT HOLDERS BE LIABLE FOR ANY SPECIAL, INDIRECT
+** OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+** OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+** OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE
+** OR PERFORMANCE OF THIS SOFTWARE.
+** 
+** Author:  Emmanuel Ackaouy
+**
+*/ 
+
+/* The macro definition and file was added for debugging purposes */
+
+# include <iostream.h>
+# include <fstream.h>
+
+int putcount =0;
+int getcount = 0;
+
+static int shipcount =0;
+#ifdef CLIENT
+ofstream nwdump("CLIENTDUMP",ios::out);
+#endif
+
+#ifndef CLIENT
+ofstream nwdump("SERVERDUMP",ios::out);
+#endif
+
+#define NETWORK_TRACE(s) { shipcount++; nwdump << s << "|"; \
+              if(shipcount % 4 == 0) nwdump  << endl; } 
+
+#define _POSIX_SOURCE
+
+#include "condor_common.h"
+#include "condor_constants.h"
+#include "condor_io.h"
+
+#include <math.h>
+
+
+
+#define FRAC_CONST		2147483647
+#define BIN_NULL_CHAR	"\255"
+
+
+
+
+/*
+**	CODE ROUTINES
+*/
+
+
+
+int Stream::code(
+	char	&c
+	)
+{
+	switch(_coding){
+		case stream_encode:
+			return put(c);
+		case stream_decode:
+			return get(c);
+	}
+
+	return FALSE;	/* will never get here	*/
+}
+
+
+
+int Stream::code(
+	int		&i
+	)
+{
+	switch(_coding){
+		case stream_encode:
+			return put(i);
+		case stream_decode:
+			return get(i);
+	}
+
+	return FALSE;	/* will never get here	*/
+}
+
+
+
+int Stream::code(
+	long	&l
+	)
+{
+	switch(_coding){
+		case stream_encode:
+			return put(l);
+		case stream_decode:
+			return get(l);
+	}
+
+	return FALSE;	/* will never get here	*/
+}
+
+
+
+int Stream::code(
+	short	&s
+	)
+{
+	switch(_coding){
+		case stream_encode:
+			return put(s);
+		case stream_decode:
+			return get(s);
+	}
+
+	return FALSE;	/* will never get here	*/
+}
+
+
+
+int Stream::code(
+	float	&f
+	)
+{
+	switch(_coding){
+		case stream_encode:
+			return put(f);
+		case stream_decode:
+			return get(f);
+	}
+
+	return FALSE;	/* will never get here	*/
+}
+
+
+
+int Stream::code(
+	double	&d
+	)
+{
+	switch(_coding){
+		case stream_encode:
+			return put(d);
+		case stream_decode:
+			return get(d);
+	}
+
+	return FALSE;	/* will never get here	*/
+}
+
+
+
+int Stream::code(
+	char	*&s
+	)
+{
+	switch(_coding){
+		case stream_encode:
+			return put(s);
+		case stream_decode:
+			return get(s);
+	}
+
+	return FALSE;	/* will never get here	*/
+}
+
+
+
+int Stream::code(
+	char	*&s,
+	int		&len
+	)
+{
+	switch(_coding){
+		case stream_encode:
+			return put(s, len);
+		case stream_decode:
+			return get(s, len);
+	}
+
+	return FALSE;	/* will never get here	*/
+}
+
+
+
+
+
+
+/*
+**	PUT ROUTINES
+*/
+
+
+
+int Stream::put(
+	char	c
+	)
+{
+  getcount =0;
+  NETWORK_TRACE("put char " << c << " c(" << ++putcount << ") ");
+	if (!valid()) return FALSE;
+
+	switch(_code){
+		case internal:
+		case external:
+		case ascii:
+			if (put_bytes(&c, 1) != 1) return FALSE;
+			break;
+	}
+
+	return TRUE;
+}
+
+
+
+int Stream::put(
+	int		i
+	)
+{
+	int		tmp;
+  getcount =0;
+  putcount +=4;
+  NETWORK_TRACE("put int " << i << " c(" << putcount << ") ");
+	if (!valid()) return FALSE;
+
+	switch(_code){
+		case internal:
+			if (put_bytes(&i, sizeof(int)) != sizeof(int)) return FALSE;
+			break;
+
+		case external:
+			tmp = htonl(i);
+			if (put_bytes(&tmp, sizeof(int)) != sizeof(int)) return FALSE;
+			break;
+
+		case ascii:
+			return FALSE;
+	}
+
+	return TRUE;
+}
+
+
+
+int Stream::put(
+	long	l
+	)
+{
+  NETWORK_TRACE("put long " << l);
+	if (!valid()) return FALSE;
+
+	switch(_code){
+		case internal:
+			if (put_bytes(&l, sizeof(long)) != sizeof(long)) return FALSE;
+			break;
+
+		case external:
+			return put((int)l);
+
+		case ascii:
+			return FALSE;
+	}
+
+	return TRUE;
+}
+
+
+
+int Stream::put(
+	short	s
+	)
+{
+  NETWORK_TRACE("put short " << s);
+	if (!valid()) return FALSE;
+
+	switch(_code){
+		case internal:
+			if (put_bytes(&s, sizeof(short)) != sizeof(short)) return FALSE;
+			break;
+
+		case external:
+			return put((int)s);
+
+		case ascii:
+			return FALSE;
+	}
+
+	return TRUE;
+}
+
+
+
+int Stream::put(
+	float	f
+	)
+{
+  NETWORK_TRACE("put float " << f);
+	if (!valid()) return FALSE;
+
+	switch(_code){
+		case internal:
+			if (put_bytes(&f, sizeof(float)) != sizeof(float)) return FALSE;
+			break;
+
+		case external:
+			return put((double)f);
+
+		case ascii:
+			return FALSE;
+	}
+
+	return TRUE;
+}
+
+
+
+int Stream::put(
+	double	d
+	)
+{
+  NETWORK_TRACE("put double " << d);
+	int		frac, exp;
+
+	if (!valid()) return FALSE;
+
+	switch(_code){
+		case internal:
+			if (put_bytes(&d, sizeof(double)) != sizeof(double)) return FALSE;
+			break;
+
+		case external:
+			frac = (int) (frexp(d, &exp) * (double)FRAC_CONST);
+			if (!put(frac)) return FALSE;
+			return put(exp);
+
+		case ascii:
+			return FALSE;
+	}
+
+	return TRUE;
+}
+
+
+
+int Stream::put(
+	char	*s
+	)
+{
+	int		len;
+
+  NETWORK_TRACE("put string " << s);
+	if (!valid()) return FALSE;
+
+	switch(_code){
+		case internal:
+		case external:
+			if (!s){
+				if (put_bytes(BIN_NULL_CHAR, 1) != 1) return FALSE;
+			}
+			else{
+				len = strlen(s)+1;
+				if (put_bytes(s, len) != len) return FALSE;
+			}
+			break;
+
+		case ascii:
+			return FALSE;
+	}
+
+	return TRUE;
+}
+
+
+
+int Stream::put(
+	char	*s,
+	int		l
+	)
+{
+    NETWORK_TRACE("put  string and int " <<   l);
+	if (!valid()) return FALSE;
+
+	switch(_code){
+		case internal:
+		case external:
+			if (!s){
+				if (put_bytes(BIN_NULL_CHAR, 1) != 1) return FALSE;
+			}
+			else{
+				if (put_bytes(s, l) != l) return FALSE;
+			}
+			break;
+
+		case ascii:
+			return FALSE;
+	}
+
+	return TRUE;
+}
+
+
+
+
+
+/*
+**	GET ROUTINES
+*/
+
+
+
+int Stream::get(
+	char	&c
+	)
+{
+   putcount =0;
+	if (!valid()) return FALSE;
+
+	switch(_code){
+		case internal:
+		case external:
+		case ascii:
+			if (get_bytes(&c, 1) != 1) return FALSE;
+			break;
+	}
+   NETWORK_TRACE("get char " << c << " c(" << ++getcount << ") ");    
+	return TRUE;
+}
+
+
+
+int Stream::get(
+	int		&i
+	)
+{
+	int		tmp;
+
+	if (!valid()) return FALSE;
+
+	switch(_code){
+		case internal:
+			if (get_bytes(&i, sizeof(int)) != sizeof(int)) return FALSE;
+			break;
+
+		case external:
+			if (get_bytes(&tmp, sizeof(int)) != sizeof(int)) return FALSE;
+			i = ntohl(tmp);
+			break;
+
+		case ascii:
+			return FALSE;
+	}
+   putcount =0;
+   getcount += 4;
+   NETWORK_TRACE("get int " << i<< " c(" << getcount << ") ");
+	return TRUE;
+}
+
+
+
+int Stream::get(
+	long	&l
+	)
+{
+	int		i;
+
+	if (!valid()) return FALSE;
+
+	switch(_code){
+		case internal:
+			if (get_bytes(&l, sizeof(long)) != sizeof(long)) return FALSE;
+			break;
+
+		case external:
+			if (!get(i)) return FALSE;
+			l = (long) i;
+			break;
+
+		case ascii:
+			return FALSE;
+	}
+    NETWORK_TRACE("get long " << l);
+	return TRUE;
+}
+
+
+
+int Stream::get(
+	short	&s
+	)
+{
+	int		i;
+
+	if (!valid()) return FALSE;
+
+	switch(_code){
+		case internal:
+			if (get_bytes(&s, sizeof(short)) != sizeof(short)) return FALSE;
+			break;
+
+		case external:
+			if (!get(i)) return FALSE;
+			s = (short) i;
+			break;
+
+		case ascii:
+			return FALSE;
+	}
+        NETWORK_TRACE("get short " << s);
+	return TRUE;
+}
+
+
+
+int Stream::get(
+	float	&f
+	)
+{
+	double	d;
+
+	if (!valid()) return FALSE;
+
+	switch(_code){
+		case internal:
+			if (get_bytes(&f, sizeof(float)) != sizeof(float)) return FALSE;
+			break;
+
+		case external:
+			if (!get(d)) return FALSE;
+			f = (float) d;
+			break;
+
+		case ascii:
+			return FALSE;
+	}
+        NETWORK_TRACE("get float " << f);
+	return TRUE;
+}
+
+
+
+int Stream::get(
+	double	&d
+	)
+{
+	int		frac, exp;
+
+	if (!valid()) return FALSE;
+
+	switch(_code){
+		case internal:
+			if (get_bytes(&d, sizeof(double)) != sizeof(double)) return FALSE;
+			break;
+
+		case external:
+			if (!get(frac)) return FALSE;
+			if (!get(exp)) return FALSE;
+			d = ldexp((((double)frac)/((double)FRAC_CONST)), exp);
+			break;
+
+		case ascii:
+			return FALSE;
+	}
+        NETWORK_TRACE("get double " << d);
+	return TRUE;
+}
+
+
+
+int Stream::get(
+	char	*&s
+	)
+{
+	char	c;
+	void	*tmp_ptr;
+
+	if (!valid()) return FALSE;
+
+	switch(_code){
+		case internal:
+		case external:
+			if (!peek(c)) return FALSE;
+			if (c == '\255'){
+				s = (char *)0;
+				if (get_bytes(&c, 1) != 1) return FALSE;
+			}
+			else{
+				tmp_ptr = s;
+				if (get_ptr(tmp_ptr, '\0') <= 0) return FALSE;
+				s = (char *)tmp_ptr;
+			}
+			break;
+
+		case ascii:
+			return FALSE;
+	}
+        NETWORK_TRACE("get string " << s);
+	return TRUE;
+}
+
+
+
+int Stream::get(
+	char	*&s,
+	int		&l
+	)
+{
+	char	c;
+	void	*tmp_ptr;
+
+	if (!valid()) return FALSE;
+
+	switch(_code){
+		case internal:
+		case external:
+			if (!peek(c)) return FALSE;
+			if (c == '\255'){
+				s = (char *)0;
+				if (get_bytes(&c, 1) != 1) return FALSE;
+			}
+			else{
+				tmp_ptr = s;
+				if ((l = get_ptr(tmp_ptr, '\0')) <= 0) return FALSE;
+				s = (char *)tmp_ptr;
+			}
+			break;
+
+		case ascii:
+			return FALSE;
+	}
+        NETWORK_TRACE("get string and int " <<   l);
+	return TRUE;
+}
