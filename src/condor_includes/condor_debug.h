73b0c950887e8fe251763d742657fd64a3a5a3e6
jbasney <jbasney>
1996-07-04 21:44:31 +0000

    #define BIN_MAIL "/usr/ucb/mail" /* HPUX9 -- /usr/bin/mailx is gone */

--- src/condor_includes/condor_debug.h
+++ src/condor_includes/condor_debug.h
@@ -107,8 +107,10 @@ char    *mymalloc(), *myrealloc(), *mycalloc();
         (ptr)->ru_stime.tv_usec ); \
 }
 
-#if defined(AIX31) || defined(AIX32) || defined(HPUX9)
+#if defined(AIX31) || defined(AIX32)
 #define BIN_MAIL "/usr/bin/mail"
+#elif defined(HPUX9)
+#define BIN_MAIL "/usr/ucb/mail"
 #else
 #define BIN_MAIL "/bin/mail"
 #endif
