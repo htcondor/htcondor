35ccfd2194744910b18846a8ab07d6b01e0067ff
raman <raman>
1996-08-27 17:08:56 +0000

    File of canonical attributes found in class ads

---
+++ src/condor_includes/condor_attributes.h
@@ -0,0 +1,53 @@
+#ifndef __CONDOR_ATTRIBUTES_H__
+#define __CONDOR_ATTRIBUTES_H__
+
+const char ATTR_ARCH                     [] = "Arch";
+const char ATTR_CLIENT_MACHINE           [] = "ClientMachine"; 
+const char ATTR_CLOCK_DAY                [] = "ClockDay";
+const char ATTR_CLOCK_MIN                [] = "ClockMin";
+const char ATTR_CONTINUE                 [] = "Continue";
+const char ATTR_CPUS                     [] = "Cpus";
+const char ATTR_DISK                     [] = "Disk";
+const char ATTR_ENTERED_CURRENT_STATE    [] = "EnteredCurrentState";
+const char ATTR_FILE_SYSTEM_DOMAIN       [] = "FileSystemDomain";
+const char ATTR_FLAVOR                   [] = "Flavor";
+const char ATTR_IDLE_JOBS                [] = "IdleJobs";
+const char ATTR_INACTIVE                 [] = "Inactive";
+const char ATTR_JOB_PRIO                 [] = "JobPrio";
+const char ATTR_JOB_START                [] = "JobStart";
+const char ATTR_JOB_STATUS               [] = "JobStatus";
+const char ATTR_JOB_UNIVERSE             [] = "JobUniverse";
+const char ATTR_KEYBOARD_IDLE            [] = "KeyboardIdle";
+const char ATTR_KFLOPS                   [] = "KFlops";
+const char ATTR_KILL                     [] = "Kill";
+const char ATTR_LAST_HEARD_FROM          [] = "LastHeardFrom";
+const char ATTR_LAST_PERIODIC_CHECKPOINT [] = "LastPeriodicCheckpoint";
+const char ATTR_LOAD_AVG                 [] = "LoadAvg";
+const char ATTR_MACHINE                  [] = "Machine";
+const char ATTR_MAX_JOBS_RUNNING         [] = "MaxJobsRunning";
+const char ATTR_MEMORY                   [] = "Memory";
+const char ATTR_MIPS                     [] = "Mips";
+const char ATTR_NAME                     [] = "Name";
+const char ATTR_NEST                     [] = "Nest";
+const char ATTR_OPSYS                    [] = "OpSys";
+const char ATTR_OWNER                    [] = "Owner"; 
+const char ATTR_PERIODIC_CHECKPOINT      [] = "PeriodicCheckpoint";
+const char ATTR_PRIO                     [] = "Prio";
+const char ATTR_Q_DATE                   [] = "QDate";
+const char ATTR_REMOTE_USER              [] = "RemoteUser";
+const char ATTR_REQUIREMENT              [] = "Requirement";
+const char ATTR_RUNNING_JOBS             [] = "RunningJobs";
+const char ATTR_SCHEDD_IP_ADDR           [] = "ScheddIpAddr";
+const char ATTR_SCHEDD_PRIO              [] = "ScheddPrio";
+const char ATTR_START                    [] = "Start";
+const char ATTR_STARTD_IP_ADDR           [] = "StartdIpAddr";
+const char ATTR_STATE                    [] = "State";
+const char ATTR_SUBNET                   [] = "Subnet";
+const char ATTR_SUSPEND                  [] = "Suspend";
+const char ATTR_UID_DOMAIN               [] = "UidDomain";
+const char ATTR_UPDATE_PRIO              [] = "UpdatePrio";
+const char ATTR_USERS                    [] = "Users";
+const char ATTR_VACATE                   [] = "Vacate";
+const char ATTR_VIRTUAL_MEMORY           [] = "VirtualMemory";
+
+#endif
