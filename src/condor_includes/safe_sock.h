c69ca449bb9d3b8f01738da1868e848fd8e6e671
jbasney <jbasney>
1996-10-23 04:06:27 +0000

    Initial checkin for "safe socket" class (UDP).

---
+++ src/condor_includes/safe_sock.h
@@ -0,0 +1,137 @@
+/* 
+** Copyright 1993 by Miron Livny, Mike Litzkow, and Emmanuel Ackaouy.
+** 
+** Permission to use, copy, modify, and distribute this software and its
+** documentation for any purpose and without fee is hereby granted,
+** provided that the above copyright notice appear in all copies and that
+** both that copyright notice and this permission notice appear in
+** supporting documentation, and that the names of the University of
+** Wisconsin and the copyright holders not be used in advertising or
+** publicity pertaining to distribution of the software without specific,
+** written prior permission.  The University of Wisconsin and the 
+** copyright holders make no representations about the suitability of this
+** software for any purpose.  It is provided "as is" without express
+** or implied warranty.
+** 
+** THE UNIVERSITY OF WISCONSIN AND THE COPYRIGHT HOLDERS DISCLAIM ALL
+** WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES
+** OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE UNIVERSITY OF
+** WISCONSIN OR THE COPYRIGHT HOLDERS BE LIABLE FOR ANY SPECIAL, INDIRECT
+** OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+** OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+** OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE
+** OR PERFORMANCE OF THIS SOFTWARE.
+** 
+** Author:  Jim Basney
+**
+*/ 
+
+
+#ifndef SAFE_SOCK_H
+#define SAFE_SOCK_H
+
+#include <netinet/in.h>
+#include "buffers.h"
+#include "sock.h"
+
+/*
+**	S A F E    S O C K
+*/
+
+class SafeSock : public Sock {
+
+//	PUBLIC INTERFACE TO SAFE SOCKS
+//
+public:
+
+	/*
+	**	Methods
+	*/
+
+	// Virtual socket services
+	//
+	virtual int handle_incoming_packet();
+	virtual int end_of_message();
+	virtual int close();
+	virtual int connect(char *, int);
+	inline int connect(char *h, char *s) { return connect(h,getportbyserv(s));}
+
+
+	// Safe socket services
+	//
+
+	SafeSock() : Sock() {}		/* virgin safe_sock		*/
+	SafeSock(int);				/* listen on port		*/
+	SafeSock(char *);			/* listen on serv 		*/
+	SafeSock(char *, int);		/* connect to host/port	*/
+	SafeSock(char *, char *);	/* connect to host/serv	*/
+	~SafeSock();
+
+	int listen();
+	inline int listen(int p) { if (!bind(p)) return FALSE; return listen(); }
+	inline int listen(char *s) { if (!bind(s)) return FALSE; return listen(); }
+
+	int get_port();
+	int get_file_desc();
+	int attach_to_file_desc(int);
+	struct sockaddr_in *endpoint();
+	char *endpoint_IP();
+	int endpoint_port();
+
+	/*
+	**	Stream protocol
+	*/
+
+	virtual stream_type type() { return Stream::safe_sock; }
+
+	//	byte operations
+	//
+	virtual int put_bytes(const void *, int);
+	virtual int get_bytes(void *, int);
+	virtual int get_ptr(void *&, char);
+	virtual int peek(char &);
+
+	
+//	PRIVATE INTERFACE TO SAFE SOCKS
+//
+protected:
+
+	/*
+	**	Types
+	*/
+
+	enum safesock_state { safesock_listen };
+
+	/*
+	**	Methods
+	*/
+
+
+	/*
+	**	Data structures
+	*/
+
+	int rcv_packet(int);
+	class RcvMsg {
+	public:
+		RcvMsg() : buf(65536), ready(0) {}
+
+		Buf			buf;
+		int			ready;
+	} rcv_msg;
+
+	int snd_packet(int);
+	class SndMsg {
+	public:
+		SndMsg() : buf(65536) {}
+
+		Buf			buf;
+	} snd_msg;
+
+	safesock_state	_special_state;
+	struct sockaddr_in _who;	// endpoint of "connection"
+};
+
+
+
+#endif
---
+++ src/condor_io/safe_sock.C
@@ -0,0 +1,378 @@
+/* 
+** Copyright 1993 by Miron Livny, Mike Litzkow, and Emmanuel Ackaouy.
+** 
+** Permission to use, copy, modify, and distribute this software and its
+** documentation for any purpose and without fee is hereby granted,
+** provided that the above copyright notice appear in all copies and that
+** both that copyright notice and this permission notice appear in
+** supporting documentation, and that the names of the University of
+** Wisconsin and the copyright holders not be used in advertising or
+** publicity pertaining to distribution of the software without specific,
+** written prior permission.  The University of Wisconsin and the 
+** copyright holders make no representations about the suitability of this
+** software for any purpose.  It is provided "as is" without express
+** or implied warranty.
+** 
+** THE UNIVERSITY OF WISCONSIN AND THE COPYRIGHT HOLDERS DISCLAIM ALL
+** WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES
+** OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE UNIVERSITY OF
+** WISCONSIN OR THE COPYRIGHT HOLDERS BE LIABLE FOR ANY SPECIAL, INDIRECT
+** OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+** OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+** OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE
+** OR PERFORMANCE OF THIS SOFTWARE.
+** 
+** Author:  Jim Basney
+**
+*/ 
+
+/* Note: this code needs to have error handling overhauled */
+
+#define _POSIX_SOURCE
+
+#include "condor_common.h"
+#include "condor_constants.h"
+#include "condor_io.h"
+
+
+
+SafeSock::SafeSock(					/* listen on port		*/
+	int		port
+	)
+	: Sock()
+{
+	listen(port);
+}
+
+
+SafeSock::SafeSock(					/* listen on serv		*/
+	char	*serv
+	)
+	: Sock()
+{
+	listen(serv);
+}
+
+
+SafeSock::SafeSock(
+	char	*host,
+	int		port
+	)
+	: Sock()
+{
+	connect(host, port);
+}
+
+
+
+SafeSock::SafeSock(
+	char	*host,
+	char	*serv
+	)
+	: Sock()
+{
+	connect(host, serv);
+}
+
+
+
+SafeSock::~SafeSock()
+{
+	close();
+}
+
+
+
+int SafeSock::listen()
+{
+	if (!valid()) return FALSE;
+
+	if (_state != sock_bound) return FALSE;
+	if (::listen(_sock, 5) < 0) return FALSE;
+
+	_state = sock_special;
+	_special_state = safesock_listen;
+
+	return TRUE;
+}
+
+
+
+int SafeSock::close()
+{
+	if (_state == sock_virgin) return FALSE;
+
+	if (::close(_sock) < 0) return FALSE;
+
+	_state = sock_virgin;
+	return TRUE;
+}
+
+
+
+int SafeSock::handle_incoming_packet()
+{
+	/* do not queue up more than one message at a time on safe sockets */
+	/* but return 1, because old message can still be read.	           */
+	if (rcv_msg.ready) return TRUE;
+
+	if (!rcv_packet(_sock)) return FALSE;
+
+	return TRUE;
+}
+
+
+
+int SafeSock::end_of_message()
+{
+	switch(_coding){
+		case stream_encode:
+			if (!snd_msg.buf.empty()){
+				return snd_packet(_sock);
+			}
+			break;
+
+		case stream_decode:
+			if (rcv_msg.ready && rcv_msg.buf.consumed()){
+				rcv_msg.ready = FALSE;
+				rcv_msg.buf.reset();
+				return TRUE;
+			}
+			break;
+
+		default:
+			assert(0);
+	}
+
+	return FALSE;
+}
+
+
+
+int SafeSock::connect(
+	char	*host,
+	int		port
+	)
+{
+	hostent			*hostp;
+	unsigned long	inaddr;
+
+	if (!valid()) return FALSE;
+	if (!host || port < 0) return FALSE;
+
+		/* we bind here so that a sock may be	*/
+		/* assigned to the stream if needed		*/
+	if (_state == sock_virgin || _state == sock_assigned) bind();
+
+	if (_state != sock_bound) return FALSE;
+	
+	memset(&_who, 0, sizeof(sockaddr_in));
+	_who.sin_family = AF_INET;
+	_who.sin_port = htons((u_short)port);
+
+	/* try to get a decimal notation first 			*/
+	if ((inaddr = inet_addr(host)) != 0){
+		memcpy((char *)&_who.sin_addr, &inaddr, sizeof(inaddr));
+	}
+	/* if dotted notation fails, try host database	*/
+	else{
+		if ((hostp = gethostbyname(host)) == (hostent *)0) return FALSE;
+		memcpy(&_who.sin_addr, hostp->h_addr, hostp->h_length);
+	}
+
+	_state = sock_connect;
+	return TRUE;
+}
+
+int SafeSock::put_bytes(
+	const void	*dta,
+	int			sz
+	)
+{
+	int		tw;
+	int		nw;
+
+	if (!valid()) return -1;
+
+	for(nw=0;;){
+
+		if (snd_msg.buf.full()){
+			return FALSE;
+		}
+
+/*
+		if (snd_msg.buf.empty()){
+			snd_msg.buf.seek(5);
+		}
+*/
+
+		if ((tw = snd_msg.buf.put_max(&((char *)dta)[nw], sz-nw)) < 0)
+			return -1;
+		nw += tw;
+		if (nw == sz) break;
+	}
+
+	return nw;
+}
+
+
+int SafeSock::get_bytes(
+	void		*dta,
+	int			max_sz
+	)
+{
+	if (!valid()) return -1;
+
+	while (!rcv_msg.ready) {
+		if (!handle_incoming_packet()){
+			fprintf(stderr, "HANDLE_INCOMING FAILED\n");
+			return FALSE;
+		}
+	}
+
+	return rcv_msg.buf.get_max(dta, max_sz);
+}
+
+
+int SafeSock::get_ptr(
+	void		*&ptr,
+	char		delim
+	)
+{
+	int nr;
+	int tr;
+
+	if (!valid()) return -1;
+
+	while (!rcv_msg.ready){
+		if (!handle_incoming_packet()) return FALSE;
+	}
+
+	if ((tr = rcv_msg.buf.find(delim)) < 0) {
+		return -1;
+	}
+	ptr = rcv_msg.buf.get_ptr();
+	nr = rcv_msg.buf.seek(0);
+	rcv_msg.buf.seek(nr+tr+1);
+	return tr+1;
+}
+
+
+int SafeSock::peek(
+	char		&c
+	)
+{
+	if (!valid()) return -1;
+
+	while (!rcv_msg.ready) {
+		if (!handle_incoming_packet()) return FALSE;
+	}
+
+	return rcv_msg.buf.peek(c);
+}
+
+
+
+int SafeSock::rcv_packet(
+	int	_sock
+	)
+{
+	char	tmp[65536];
+	int		len, fromlen;
+
+	fromlen = sizeof(struct sockaddr_in);
+	if ((len = recvfrom(_sock, tmp, 65536, 0,
+						(struct sockaddr *)&_who,&fromlen)) < 0) {
+		return FALSE;
+	}
+	if (!rcv_msg.buf.put_max(tmp, len)) {
+		fprintf(stderr, "Packet storing failed\n");
+		return FALSE;
+	}
+
+	rcv_msg.ready = TRUE;
+
+	return TRUE;
+}
+
+
+int SafeSock::snd_packet(
+	int		_sock
+	)
+{
+	int		ns;
+
+	ns = snd_msg.buf.num_used();
+
+	if (sendto(_sock, (const char *)snd_msg.buf.get_ptr(),
+			   snd_msg.buf.num_used(), 0,
+			   (const struct sockaddr *)&_who,
+			   sizeof(struct sockaddr_in)) < 0) {
+		return FALSE;
+	}
+	snd_msg.buf.reset();
+
+	return TRUE;
+}
+
+
+
+int SafeSock::get_port()
+{
+	sockaddr_in	addr;
+	int			addr_len;
+
+	addr_len = sizeof(sockaddr_in);
+
+	if (getsockname(_sock, (sockaddr *)&addr, &addr_len) < 0) return -1;
+
+	return (int) ntohs(addr.sin_port);
+}
+
+
+int SafeSock::get_file_desc()
+{
+	return _sock;
+}
+
+
+int SafeSock::attach_to_file_desc(
+	int		fd
+	)
+{
+	if (_state != sock_virgin) return FALSE;
+
+	_sock = fd;
+	_state = sock_connect;
+	return TRUE;
+}
+
+struct sockaddr_in *SafeSock::endpoint()
+{
+	return &_who;
+}
+
+char *SafeSock::endpoint_IP()
+{
+	int             i;
+	static  char    buf[24];
+	char    tmp_buf[10];
+	char    *cur_byte;
+	unsigned char   this_byte;
+
+	buf[0] = '\0';
+	cur_byte = (char *) &(_who.sin_addr);
+	for (i = 0; i < sizeof(_who.sin_addr); i++) {
+		this_byte = (unsigned char) *cur_byte;
+		sprintf(tmp_buf, "%u.", this_byte);
+		cur_byte++;
+		strcat(buf, tmp_buf);
+	}
+	buf[strlen(buf) - 1] = '\0';
+	return buf;
+}
+
+int SafeSock::endpoint_port()
+{
+	return (int) ntohs(_who.sin_port);
+}
