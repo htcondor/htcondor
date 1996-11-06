0bf2f6eebb4bc848645984237daa15311337f739
raman <raman>
1996-11-06 17:24:09 +0000

    Added collector commands to handle CKPT_SRVR_AD's as well as MASTER, STARTD and SCHEDD

--- src/condor_includes/condor_collector.h
+++ src/condor_includes/condor_collector.h
@@ -9,7 +9,7 @@ enum AdTypes
     SCHEDD_AD,
     MASTER_AD,
     GATEWAY_AD,
-    CONFIG_SRV_AD,
+    CKPT_SRVR_AD,
 
     NUM_AD_TYPES
 };
@@ -19,13 +19,13 @@ const int UPDATE_STARTD_AD       = 0;
 const int UPDATE_SCHEDD_AD       = 1;
 const int UPDATE_MASTER_AD       = 2;
 const int UPDATE_GATEWAY_AD      = 3;
-const int UPDATE_CONFIG_SRVR_AD  = 4;
+const int UPDATE_CKPT_SRVR_AD  = 4;
 
 const int QUERY_STARTD_ADS       = 5;
 const int QUERY_SCHEDD_ADS       = 6;
 const int QUERY_MASTER_ADS       = 7;
 const int QUERY_GATEWAY_ADS      = 8;
-const int QUERY_CONFIG_SRVR_ADS  = 9;
+const int QUERY_CKPT_SRVR_ADS  = 9;
 
 #endif // __COLLECTOR_H__
 
