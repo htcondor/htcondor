8cfc48983cef78cfa82e5c402ec2313f45bf67ef
jbasney <jbasney>
1997-06-13 17:12:11 +0000

    don't use proc_obj in V6

--- src/condor_c++_util/instantiate.C
+++ src/condor_c++_util/instantiate.C
@@ -2,14 +2,12 @@
 
 #pragma implementation "list.h"
 #include "list.h"
-#include "proc_obj.h"
 #include "filter.h"
 
 #pragma implementation "simplelist.h"
 #include "simplelist.h"
 
 typedef List<FilterObj> listfilterobj;
-typedef List<ProcObj> listprocobj;
 typedef List<char> listchar;
 typedef List<int> listint;
 
