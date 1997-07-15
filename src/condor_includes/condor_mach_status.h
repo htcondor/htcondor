b2f71febef20462d3715fea03266257850867385
tannenba <tannenba>
1997-07-15 17:46:22 +0000

    had to switch the states from being defined with "const ints" to "#defines". why? because SUSPENDED and CHECKPOINTING are also used as an enumerated type elsewhere in the code (like the starter), and g++ still implements enums as #defines behind the scenes, so we get error recompiling the starter. Note that some .h files #undef SUSPENDED and CHECKPOINTING after including sched.h in order to get around this problem (gross!)

--- src/condor_includes/condor_mach_status.h
+++ src/condor_includes/condor_mach_status.h
@@ -2,25 +2,16 @@
 #define _MACH_STATUS
 
 
-#if !defined(__STDC__) && !defined(__cplusplus)
-#define const
-#endif
-
-static const int	NO_JOB			= 0;
-static const int	JOB_RUNNING		= 1;
-static const int	KILLED			= 2;
-static const int	CHECKPOINTING	= 3;
-static const int	SUSPENDED		= 4;
-static const int	USER_BUSY		= 5;
-static const int	M_IDLE			= 6;
-static const int	BLOCKED			= 7;
-static const int	SYSTEM			= 8;
-static const int	CONDOR_DOWN		= - 1;
-
-#if !defined(__STDC__) && !defined(__cplusplus)
-#undef const
-#endif
-
+#define NO_JOB			0
+#define JOB_RUNNING		1
+#define KILLED			2
+#define CHECKPOINTING	3
+#define SUSPENDED		4
+#define USER_BUSY		5
+#define M_IDLE			6
+#define BLOCKED			7
+#define SYSTEM			8
+#define CONDOR_DOWN		-1
 
 
 #if defined( __STDC__) || defined(__cplusplus)	/* ANSI Prototypes */
