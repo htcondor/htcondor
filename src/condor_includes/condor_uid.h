e900232bb4244fd98eec484988e6aa205e108252
jbasney <jbasney>
1997-07-24 18:05:40 +0000

    set_priv and friends are now macros that call _set_priv for debugging purposes; added a description of this module

--- src/condor_includes/condor_uid.h
+++ src/condor_includes/condor_uid.h
@@ -1,3 +1,47 @@
+/* 
+** Copyright 1986, 1987, 1988, 1989, 1990, 1991, 1997 by the Condor Design Team
+** 
+** Permission to use, copy, modify, and distribute this software and its
+** documentation for any purpose and without fee is hereby granted,
+** provided that the above copyright notice appear in all copies and that
+** both that copyright notice and this permission notice appear in
+** supporting documentation, and that the names of the University of
+** Wisconsin and the Condor Design Team not be used in advertising or
+** publicity pertaining to distribution of the software without specific,
+** written prior permission.  The University of Wisconsin and the Condor
+** Design Team make no representations about the suitability of this
+** software for any purpose.  It is provided "as is" without express
+** or implied warranty.
+** 
+** THE UNIVERSITY OF WISCONSIN AND THE CONDOR DESIGN TEAM DISCLAIM ALL
+** WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES
+** OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE UNIVERSITY OF
+** WISCONSIN OR THE CONDOR DESIGN TEAM BE LIABLE FOR ANY SPECIAL, INDIRECT
+** OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+** OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+** OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE
+** OR PERFORMANCE OF THIS SOFTWARE.
+** 
+**/
+
+/*
+** This module implements all process privilege switching in Condor.  No
+** other code in the system should call seteuid and friends directly.
+** Privilege classes are defined in the priv_state enumeration.  To
+** switch to a given privilege class, call set_priv(new_priv_class).
+** This call will return the privilege class in place at the time of
+** the call.  The set_condor_priv(), set_user_priv(), set_user_priv_final(),
+** and set_root_priv() macros are provided for convenience, which in
+** turn call set_priv().  Either init_user_ids() or set_user_ids() should be
+** called before set_user_priv() or set_user_priv_final().
+**
+** Privilege classes designated as "final" set real uid/gid so that
+** the process can not later switch to another privilege class.
+**
+** The display_priv_log() function writes a log of the recent privilege
+** class switches using dprintf().
+*/
+
 #ifndef _UID_H
 #define _UID_H
 
@@ -7,26 +51,29 @@
 extern "C" {
 #endif
 
-typedef enum { PRIV_UNKNOWN,
-			   PRIV_ROOT,
-			   PRIV_CONDOR,
-			   PRIV_USER,
-			   PRIV_USER_FINAL
+typedef enum {
+	PRIV_UNKNOWN,
+	PRIV_ROOT,
+	PRIV_CONDOR,
+	PRIV_USER,
+	PRIV_USER_FINAL
 } priv_state;
 
+#define set_priv(s)	_set_priv(s, __FILE__, __LINE__, 1)
+#define set_condor_priv() _set_priv(PRIV_CONDOR, __FILE__, __LINE__, 1)
+#define set_user_priv()	_set_priv(PRIV_USER, __FILE__, __LINE__, 1)
+#define set_user_priv_final() _set_priv(PRIV_USER_FINAL, __FILE__, __LINE__, 1)
+#define set_root_priv()	_set_priv(PRIV_ROOT, __FILE__, __LINE__, 1)
+
 void init_condor_ids();
-void init_user_ids(char username[]);
+void init_user_ids(const char username[]);
 void set_user_ids(uid_t uid, gid_t gid);
-priv_state set_priv(priv_state s);
-priv_state set_condor_priv();
-priv_state set_user_priv();
-void set_user_priv_final();
-priv_state set_root_priv();
+priv_state _set_priv(priv_state s, char file[], int line, int dologging);
 uid_t get_condor_uid();
 gid_t get_condor_gid();
 uid_t get_user_uid();
 gid_t get_user_gid();
-
+void display_priv_log();
 
 #if defined(__cplusplus)
 }
