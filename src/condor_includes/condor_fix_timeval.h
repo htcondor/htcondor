007d5f2dbd1696d464a58874fd99f3dfb664661c
wright <wright>
1997-10-17 19:32:54 +0000

    Indented to make more legible, and fixed up some Solaris stuff.

--- src/condor_includes/condor_fix_timeval.h
+++ src/condor_includes/condor_fix_timeval.h
@@ -14,20 +14,32 @@
     	int tz_minuteswest; /* minutes west of Greenwich */
     	int tz_dsttime; /* type of dst correction */
 	};
+
 #elif defined(Solaris251)
-#define __EXTENSIONS__
-#	include <sys/time.h>
+#  define __EXTENSIONS__
+#	 include <sys/time.h>
+#  undef __EXTENSIONS__
+
+#if 0
+
 #elif defined(Solaris)
 /* Solaris specific change ..dhaval 6/26 */
-#if defined(_POSIX_SOURCE)
-#define HOLD_POSIX_SOURCE
-#undef _POSIX_SOURCE
-#endif /* _POSIX_SOURCE */
-#	include <sys/types.h>
-#if defined(HOLD_POSIX_SOURCE)
-#define _POSIX_SOURCE
-#endif
+#  if defined(_POSIX_SOURCE)
+#    define HOLD_POSIX_SOURCE
+#    undef _POSIX_SOURCE
+#  endif /* _POSIX_SOURCE */
+#  include <sys/types.h>
+#  if defined(HOLD_POSIX_SOURCE)
+#    define _POSIX_SOURCE
+#  endif
+
+#endif 0
+
+
 #else  
-#	include <sys/time.h> 
+#  include <sys/time.h> 
+
 #endif 
+
+
 #endif
