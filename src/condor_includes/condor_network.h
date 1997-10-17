d51cfc87c7c0b7b553d5b041b1af4befb7bc165b
wright <wright>
1997-10-17 19:44:30 +0000

    Everything should use one port for UDP/TCP, whatever.

--- src/condor_includes/condor_network.h
+++ src/condor_includes/condor_network.h
@@ -3,16 +3,21 @@
 
 #include "_condor_fix_types.h"
 
-#define SCHED_PORT						9605
-#define START_PORT						9611
-#define COLLECTOR_PORT					9612
-#define COLLECTOR_UDP_PORT				9613
+#define SCHED_PORT					9605
+
 #define NEGOTIATOR_PORT					9614
-#define START_UDP_PORT					9615
+
 #define ACCOUNTANT_PORT					9616
-#define COLLECTOR_COMM_PORT				9617
-#define COLLECTOR_UDP_COMM_PORT			9618
-#define	DEFAULT_CONFIG_SERVER_PORT	    9600
+
+#define START_PORT					9611
+#define START_UDP_PORT					9611
+
+#define COLLECTOR_PORT					9618
+#define COLLECTOR_UDP_PORT				9618
+#define COLLECTOR_COMM_PORT				9618
+#define COLLECTOR_UDP_COMM_PORT				9618
+
+#define	DEFAULT_CONFIG_SERVER_PORT		    9600
 
 #if defined(__cplusplus)
 extern "C" {
