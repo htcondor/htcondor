fe8b37bf3b859434de2bead4cd4fb00bda48f5c9
wright <wright>
1997-10-17 19:30:00 +0000

    Use condor_fix_signal.h, not <signal.h>, and include <sys/ioctl.h> properly.  Also, NT specific changes committed to main trunk.

--- src/condor_includes/condor_common.h
+++ src/condor_includes/condor_common.h
@@ -1,3 +1,15 @@
+#if defined(WIN32)
+
+#define NOGDI
+#define NOUSER
+#define NOSOUND
+#include <winsock2.h>
+#include <windows.h>
+#include "_condor_fix_nt.h"
+#include <stdlib.h>
+
+#else
+
 #include "_condor_fix_types.h"
 #include "condor_fix_stdio.h"
 #include <stdlib.h>
@@ -7,6 +19,14 @@
 #include <ctype.h>
 #include <fcntl.h>
 #include <errno.h>
-#if !defined(SUNOS41)
-#include <signal.h>
+#include "condor_fix_signal.h"
+
+#if defined(Solaris)
+#	define BSD_COMP
 #endif
+#include <sys/ioctl.h>
+#if defined(Solaris)
+#	undef BSD_COMP
+#endif
+
+#endif // defined(WIN32)
