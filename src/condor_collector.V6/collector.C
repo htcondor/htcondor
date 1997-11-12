2379fe94379be129a02cdce71c5a1590adc2b0fe
wright <wright>
1997-11-12 19:51:19 +0000

    Made the handler for QUERY_STARTD_PVT_ADS registered with NEGOTIATOR permission.

--- src/condor_collector.V6/collector.C
+++ src/condor_collector.V6/collector.C
@@ -119,8 +119,8 @@ main_init(int argc, char *argv[])
 	// install command handlers for queries
 	daemonCore->Register_Command(QUERY_STARTD_ADS,"QUERY_STARTD_ADS",
 		(CommandHandler)receive_query,"receive_query",NULL,READ);
-	daemonCore->Register_Command(QUERY_STARTD_ADS,"QUERY_STARTD_PVT__ADS",
-		(CommandHandler)receive_query,"receive_query",NULL,READ);
+	daemonCore->Register_Command(QUERY_STARTD_PVT_ADS,"QUERY_STARTD_PVT_ADS",
+		(CommandHandler)receive_query,"receive_query",NULL,NEGOTIATOR);
 	daemonCore->Register_Command(QUERY_SCHEDD_ADS,"QUERY_SCHEDD_ADS",
 		(CommandHandler)receive_query,"receive_query",NULL,READ);
 	daemonCore->Register_Command(QUERY_MASTER_ADS,"QUERY_MASTER_ADS",
