0d461be9a543769558c923e79566276c05f1030b
wright <wright>
1997-12-16 23:17:42 +0000

    + On all platforms, if WCOREFLAG is defined and WCOREFLG isn't, we   define WCOREFLG in terms of WCOREFLAG + Cleaned up the indentation to be more readable.

--- src/condor_includes/condor_fix_sys_wait.h
+++ src/condor_includes/condor_fix_sys_wait.h
@@ -1,42 +1,53 @@
 #ifndef CONDOR_FIX_SYS_WAIT_H
 #define CONDOR_FIX_SYS_WAIT_H
 
-
 #if defined(Solaris) && defined(_POSIX_C_SOURCE)
-#define HOLD_POSIX_C_SOURCE _POSIX_C_SOURCE
-#undef _POSIX_C_SOURCE
+#	define HOLD_POSIX_C_SOURCE _POSIX_C_SOURCE
+#	undef _POSIX_C_SOURCE
+#endif
+
+#if defined(OSF1) && !defined(_XOPEN_SOURCE)
+#	define CONDOR_XOPEN_DEFINED
+#	define _XOPEN_SOURCE
 #endif
 
 #if defined(IRIX53)
-#define _save_ansimode _NO_ANSIMODE
-#undef _NO_ANSIMODE
-#define _NO_ANSIMODE 0
-#define _save_xopen _XOPEN4UX
-#undef _XOPEN4UX
-#define _XOPEN4UX 1
+#	define _save_ansimode _NO_ANSIMODE
+#	undef _NO_ANSIMODE
+#	define _NO_ANSIMODE 0
+#	define _save_xopen _XOPEN4UX
+#	undef _XOPEN4UX
+#	define _XOPEN4UX 1
 #endif
 
 #include <sys/wait.h>
 
-#if defined(IRIX53)
-#undef _NO_ANSIMODE
-#define _NO_ANSIMODE _save_ansimode
-#undef _save_ansimode
-#undef _XOPEN4UX
-#define _XOPEN4UX _save_xopen
-#undef _save_xopen
+#if defined(OSF1) && !defined(_XOPEN_SOURCE)
+#	define CONDOR_XOPEN_DEFINED
+#	define _XOPEN_SOURCE
 #endif
 
+#if defined(IRIX53)
+#	undef _NO_ANSIMODE
+#	define _NO_ANSIMODE _save_ansimode
+#	undef _save_ansimode
+#	undef _XOPEN4UX
+#	define _XOPEN4UX _save_xopen
+#	undef _save_xopen
+#endif
+
+
 #if defined(LINUX) && !defined( WCOREFLG )
-#define WCOREFLG 0200
+#	define WCOREFLG 0200
 #endif 
 
-#if defined(IRIX53)
-#define WCOREFLG WCOREFLAG
+#if defined(WCOREFLAG) && !defined(WCOREFLG)
+#	define WCOREFLG WCOREFLAG
 #endif
 
 #if defined(HOLD_POSIX_C_SOURCE)
-#define _POSIX_C_SOURCE HOLD_POSIX_C_SOURCE 
+#	define _POSIX_C_SOURCE HOLD_POSIX_C_SOURCE 
+#	undef HOLD_POSIX_C_SOURCE 
 #endif
 
 #endif CONDOR_FIX_SYS_WAIT_H
