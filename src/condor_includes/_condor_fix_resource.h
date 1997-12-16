dfa71888cb5abf9d4c1b8a4e3cfcb31366dbe25b
wright <wright>
1997-12-16 23:09:46 +0000

    + Ripped out crap for ancient platforms + Got rid of condor_fix_timeval.h which is no longer needed.

--- src/condor_includes/_condor_fix_resource.h
+++ src/condor_includes/_condor_fix_resource.h
@@ -1,27 +1,11 @@
-#ifndef _RESOURCE_H
-#define _RESOURCE_H
+#ifndef CONDOR_RESOURCE_H
+#define CONDOR_RESOURCE_H
 
 #if defined(__cplusplus)
 extern "C" {
 #endif
 
-#if defined(ULTRIX43) 
-#	include <time.h>
-#endif
-
-#if (defined(Solaris) && !defined(Solaris251)) || defined(SUNOS41)
-#	include <sys/time.h>
-#	include "condor_fix_timeval.h"
-#	if !defined(SUNOS41)
-#	include </usr/ucbinclude/sys/rusage.h>
-#	endif
-#endif
-
-#if defined(Solaris251)
-#	include "condor_fix_timeval.h"
-#endif
-
-#if defined(SUNOS41) || ( defined(HPUX9) && !defined(HPUX10) ) || defined(ULTRIX43)
+#if ( defined(HPUX9) && !defined(HPUX10) )
 	typedef int rlim_t;
 #endif
 
@@ -29,18 +13,8 @@ extern "C" {
 #include <sys/resource.h>
 #endif
 
-#if defined(ULTRIX43)
-#if defined(__STDC__) || defined(__cplusplus)
-	int getrlimit( int resource, struct rlimit *rlp );
-	int setrlimit( int resource, const struct rlimit *rlp );
-#else
-	int getrlimit();
-	int setrlimit();
-#endif
-#endif /* ULTRIX43 */
-
 #if defined(__cplusplus)
 }
 #endif
 
-#endif /* _RESOURCE_h */
+#endif /* CONDOR_RESOURCE_h */
