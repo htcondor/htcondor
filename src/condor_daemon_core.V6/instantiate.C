94fc300ff45ec4e810b07b50a126f81885b10191
jbasney <jbasney>
1997-12-18 21:56:31 +0000

    template instantiations

---
+++ src/condor_daemon_core.V6/instantiate.C
@@ -0,0 +1,9 @@
+#ifdef __GNUG__
+#pragma implementation "HashTable.h"
+#pragma implementation "list.h"
+#endif
+
+#include "condor_common.h"
+
+#include "condor_timer_manager.h"
+#include "condor_daemon_core.h"
