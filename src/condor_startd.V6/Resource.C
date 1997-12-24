f1d4e7c73cd58f1e71e059669264b9501ea74bd5
wright <wright>
1997-12-24 21:40:44 +0000

    + Added new public methods (from action): release_claim, kill_claim,   got_alive, periodic_checkpoint, request_new_proc, deactivate_claim,   and deactivate_claim_forcibly + Added new public methods (from util): wants_vacate, wants_suspend,   eval_kill, eval_suspend, eval_continue, and eval_vacate + We now store our single classad per resource, r_classad, that   contains all expressions in the config file and all other startd   info.  When we want to update the CM, we call make_public_ad and   make_private_ad which copy out the relevent fields from r_classad. + init_classad() now rebuilds the r_classad from scratch which makes   reconfig really work how you want it to. + send_classad_to_sock() now takes pointers to the classads you want   sent.  If either pointer is NULL, that ad isn't sent. + Moved phys_mem and AFS cell into ResAttributes. + Modified to use the new ResAttributes interface (compute and refresh   instead of timeout and update). + Made Resource derived from Service so that poll_resource   (eval_state) and eval_and_update handlers can be registered with C++   interface to DaemonCore.

--- src/condor_startd.V6/Resource.C
+++ src/condor_startd.V6/Resource.C
@@ -1,16 +1,14 @@
 #include "startd.h"
 static char *_FileName_ = __FILE__;
 
-
 Resource::Resource( Sock* coll_sock, Sock* alt_sock )
 {
-	r_classad = new ClassAd;
-	r_private_classad = new ClassAd;
+	r_classad = NULL;
 	r_state = new ResState( this );
 	r_starter = new Starter;
 	r_cur = new Match;
 	r_pre = NULL;
-	r_reqexp = new Reqexp( r_classad );
+	r_reqexp = new Reqexp( &r_classad );
 	r_attr = new ResAttributes( this );
 	r_name = strdup( my_hostname() );
 
@@ -28,7 +26,6 @@ Resource::~Resource()
 
 	delete r_state;
 	delete r_classad;
-	delete r_private_classad;
 	delete r_starter;
 	delete r_cur;		
 	delete r_pre;		
@@ -38,31 +35,187 @@ Resource::~Resource()
 }
 
 
+int
+Resource::release_claim()
+{
+	switch( state() ) {
+	case claimed_state:
+		return change_state( preempting_state, vacating_act );
+		break;
+	case matched_state:
+		return change_state( owner_state );
+		break;
+	default:
+		return FALSE;
+	}
+}
+
+
+int
+Resource::kill_claim()
+{
+	switch( state() ) {
+	case claimed_state:
+		return change_state( preempting_state, killing_act );
+		break;
+	case matched_state:
+		return change_state( owner_state );
+		break;
+	default:
+		return FALSE;
+	}
+}
+
+
+int
+Resource::got_alive()
+{
+	assert( state() == claimed_state );
+	if( !r_cur ) {
+		dprintf( D_ALWAYS, "Got keep alive with no current match object.\n" );
+		return FALSE;
+	}
+	if( !r_cur->client() ) {
+		dprintf( D_ALWAYS, "Got keep alive with no current client object.\n" );
+		return FALSE;
+	}
+	r_cur->alive();
+	return TRUE;
+}
+
+
+int
+Resource::periodic_checkpoint()
+{
+	char tmp[80];
+	if( state() != claimed_state ) {
+		return FALSE;
+	}
+	if( r_starter->active() ) {
+		if( r_starter->kill( DC_SIGPCKPT ) < 0 ) {
+			return FALSE;
+		}
+	}
+	sprintf( tmp, "%s=%d", ATTR_LAST_PERIODIC_CHECKPOINT, (int)time(NULL) );
+	r_classad->Insert(tmp);
+	return TRUE;
+}
+
+
+int
+Resource::request_new_proc()
+{
+	if( state() != claimed_state ) {
+		return FALSE;
+	}
+	if( r_starter->active() ) {
+		if( r_starter->kill( DC_SIGHUP ) < 0 ) {
+			return FALSE;
+		} else {
+			return TRUE;
+		}
+	}
+	return FALSE;
+}	
+
+
+int
+Resource::deactivate_claim()
+{
+	dprintf(D_ALWAYS, "Called deactivate_claim()\n");
+	if( (state() == claimed_state) &&
+		(r_starter->active()) ) {
+		return r_starter->kill( DC_SIGSOFTKILL );
+	} else {
+		return FALSE;
+	}
+}
+
+
+int
+Resource::deactivate_claim_forcibly()
+{
+	dprintf(D_ALWAYS, "Called deactivate_claim_forcibly()\n");
+	if( (state() == claimed_state) &&
+		(r_starter->active()) ) {
+		return r_starter->kill( DC_SIGHARDKILL );
+	} else {
+		return FALSE;
+	}
+}
+
+
+int
+Resource::change_state( State newstate )
+{
+	return r_state->change( newstate );
+}
+
+
+int
+Resource::change_state( Activity newact )
+{
+	return r_state->change( newact );
+}
+
+
+int
+Resource::change_state( State newstate, Activity newact )
+{
+	return r_state->change( newstate, newact );
+}
+
+
+bool
+Resource::in_use()
+{
+	State s = state();
+	if( s == owner_state || s == unclaimed_state ) {
+		return false;
+	} 
+	return true;
+}
+
+
 void
+Resource::starter_exited()
+{
+	dprintf( D_ALWAYS, "Starter pid %d has exited.\n",
+			 r_starter->pid() );
+	r_starter->exited();
+	change_state( idle_act );
+}
+
+
+int
 Resource::init_classad()
 {
-	char 	tmp[80];
-	int 	phys_memory = -1;
+	char 	tmp[1024];
 	char*	ptr;
 	int		needs_free = 0;
 
-		// Insert state and activity attributes into public ad
-	r_state->update( r_classad );
+	if( r_classad )	delete(r_classad);
+	r_classad 		= new ClassAd();
 
-		// Name of this resource (needs to be in public and private ads)
+		// Initialize classad types.
+	r_classad->SetMyTypeName( STARTD_ADTYPE );
+	r_classad->SetTargetTypeName( JOB_ADTYPE );
+
+		// Read in config files and fill up local ad with all attributes 
+	config( r_classad, 0 );
+
+		// Name of this resource
 	sprintf( tmp, "%s = \"%s\"", ATTR_NAME, r_name );
 	r_classad->Insert( tmp );
-	r_private_classad->Insert( tmp );
 
-		// Stick the hostname of this machine into classad.
+		// Grab the hostname of this machine
 	sprintf( tmp, "%s = \"%s\"", ATTR_MACHINE, my_hostname() );
 	r_classad->Insert( tmp );
 
-		// STARTD_IP_ADDR (needs to be in public and private ads)
+		// STARTD_IP_ADDR (needs to be in all ads)
 	sprintf( tmp, "%s = \"%s\"", ATTR_STARTD_IP_ADDR, 
 			 daemonCore->InfoCommandSinfulString() );
 	r_classad->Insert( tmp );
-	r_private_classad->Insert( tmp );
 
 		// Arch and OpSys.  Note: these will always return something,
 		// since config() will insert values for these from uname() if
@@ -77,16 +230,11 @@ Resource::init_classad()
 	r_classad->Insert( tmp );
 	free( ptr );
 
-#if !defined(WIN32) /* NEED TO PORT TO WIN32 */
-		// AFS cell
-	if( r_attr->afs_cell() ) {
-		sprintf( tmp, "%s = \"%s\"", ATTR_AFS_CELL, r_attr->afs_cell() );
-		r_classad->Insert( tmp );
-		dprintf( D_ALWAYS, "%s\n", tmp );
-	} else {
-		dprintf( D_ALWAYS, "AFS_Cell not set\n" );
-	}
-#endif
+		// Insert state and activity attributes.
+	r_state->update( r_classad );
+
+		// Insert all resource attribute info.
+	r_attr->init( r_classad );
 
 		// If the UID domain is not set, use our hostname as the
 		// default.   
@@ -136,31 +284,7 @@ Resource::init_classad()
 	sprintf( tmp, "%s = %d", ATTR_CPUS, calc_ncpus() );
 	r_classad->Insert( tmp );
 
-		// Physical memory
-	phys_memory = calc_phys_memory();
-	if( phys_memory <= 0 ) {
-			// calc_phys_memory() failed to give us something useful,
-			// try paraming. 
-		if( (ptr = param("MEMORY")) != NULL ) {
-			phys_memory = atoi(ptr);
-			free(ptr);
-		}
-	}
-	if( phys_memory > 0 ) {
-		sprintf( tmp, "%s = %d", ATTR_MEMORY, phys_memory );
-		r_classad->Insert( tmp );
-	}
-
-		// Nest
-	if( (ptr = param("NEST")) != NULL ) {
-		sprintf( tmp, "%s = \"%s\"", ATTR_NEST, ptr );
-		r_classad->Insert( tmp );
-		free( ptr );
-	}
-
-		// Current rank
-	sprintf( tmp, "%s = 0", ATTR_CURRENT_RANK );
-	r_classad->Insert( tmp );
+	return TRUE;
 }
 
 
@@ -169,10 +293,17 @@ Resource::update_classad()
 {
 	char line[100];
 
-		// Recompute update-only statistics and fill in classad
-	r_attr->update( r_classad );
+		// Recompute update only stats and fill in classad.
+	r_attr->compute( UPDATE );
+	r_attr->refresh( r_classad, UPDATE );
 	
-		// Add capability to private classad.  If r_pre exists, we
+		// Put in state info
+	r_state->update( r_classad );
+
+		// Put in requirement expression info
+	r_reqexp->update( r_classad );
+
+		// Add currently useful capability.  If r_pre exists, we
 		// need to advertise it's capability.  Otherwise, we should
 		// get the capability from r_cur.
 	if( r_pre ) {
@@ -180,9 +311,9 @@ Resource::update_classad()
 	} else {
 		sprintf( line, "%s = \"%s\"", ATTR_CAPABILITY, r_cur->capab() );
 	}		
-	r_private_classad->Insert( line );
+	r_classad->Insert( line );
 
-		// Update current rank expression in classad
+		// Update current rank expression in local and public ads.
 	sprintf( line, "%s = %f", ATTR_CURRENT_RANK, r_cur->rank() );
 	r_classad->Insert( line );
 }
@@ -192,7 +323,8 @@ void
 Resource::timeout_classad()
 {
 		// Recompute statistics needed at every timeout and fill in classad
-	r_attr->timeout( r_classad );
+	r_attr->compute( TIMEOUT );
+	r_attr->refresh( r_classad, TIMEOUT );
 }
 
 
@@ -200,19 +332,26 @@ int
 Resource::update()
 {
 	int rval1 = TRUE, rval2 = TRUE;
+	ClassAd private_ad;
+	ClassAd public_ad;
 
-		// Recompute stats needed only for updates and refresh classad. 
+		// Recompute stats needed for updates and refresh classad. 
 	this->update_classad();
 
-		// Send class ad to collector
-	if( (rval1 = send_classad_to_sock( coll_sock, TRUE )) ) {
+	this->make_public_ad( &public_ad );
+	this->make_private_ad( &private_ad );
+
+
+		// Send class ads to collector
+	if( (rval1 = 
+		 send_classad_to_sock( coll_sock, &public_ad, &private_ad )) ) {
 		dprintf( D_ALWAYS, "Sent update to the collector (%s)\n", 
 				 collector_host );
 	}  
 
-		// If we have an alternate collector, send CA there.
+		// If we have an alternate collector, send public CA there.
 	if( alt_sock ) {
-		if( (rval2 = send_classad_to_sock( alt_sock, FALSE )) ) {
+		if( (rval2 = send_classad_to_sock( alt_sock, &public_ad, NULL )) ) {
 			dprintf( D_FULLDEBUG, 
 					 "Sent update to the condor_view host (%s)\n",
 					 condor_view_host );
@@ -251,14 +390,10 @@ Resource::start_update_timer()
 {
 	up_tid = 
 		daemonCore->Register_Timer( update_interval, 0,
-									(TimerHandler)timeout_and_update,
-									"timeout_and_update" );
+									(TimerHandlercpp)eval_and_update,
+									"eval_and_update", this );
 	if( up_tid < 0 ) {
 		EXCEPT( "Can't register DaemonCore timer" );
-	}
-
-	if( ! daemonCore->Register_DataPtr( (void*) this ) ) {
-		EXCEPT( "Can't register update timer data pointer" );
 	}
 	return TRUE;
 }
@@ -271,18 +406,13 @@ Resource::start_poll_timer()
 			// Timer already started.
 		return TRUE;
 	}
-
 	poll_tid = 
 		daemonCore->Register_Timer( polling_interval,
 									polling_interval, 
-									(TimerHandler)poll_resource,
-									"poll_resource" );
+									(TimerHandlercpp)eval_state,
+									"poll_resource", this );
 	if( poll_tid < 0 ) {
 		EXCEPT( "Can't register DaemonCore timer" );
-	}
-
-	if( ! daemonCore->Register_DataPtr( (void*) this ) ) {
-		EXCEPT( "Can't register update timer data pointer" );
 	}
 	return TRUE;
 }
@@ -300,80 +430,252 @@ Resource::cancel_poll_timer()
 
 
 int
-Resource::change_state( State newstate )
+Resource::wants_vacate()
 {
-	return r_state->change( newstate );
+	int want_vacate = 0;
+	if( r_cur->universe() == VANILLA ) {
+		if( r_classad->EvalBool( "WANT_VACATE_VANILLA",
+								   r_cur->ad(),
+								   want_vacate ) == 0) { 
+			want_vacate = 1;
+		}
+	} else {
+		if( r_classad->EvalBool( "WANT_VACATE",
+								   r_cur->ad(),
+								   want_vacate ) == 0) { 
+			want_vacate = 1;
+		}
+	}
+	return want_vacate;
+}
+
+
+int 
+Resource::wants_suspend()
+{
+	int want_suspend;
+	if( r_cur->universe() == VANILLA ) {
+		if( r_classad->EvalBool( "WANT_SUSPEND_VANILLA",
+								   r_cur->ad(),
+								   want_suspend ) == 0) {  
+			want_suspend = 1;
+		}
+	} else {
+		if( r_classad->EvalBool( "WANT_SUSPEND",
+								   r_cur->ad(),
+								   want_suspend ) == 0) { 
+			want_suspend = 1;
+		}
+	}
+	return want_suspend;
 }
 
 
 int
-Resource::change_state( Activity newact )
+Resource::eval_kill()
 {
-	return r_state->change( newact );
+	int tmp;
+	if( r_cur->universe() == VANILLA ) {
+		if( (r_classad->EvalBool( "KILL_VANILLA",
+									r_cur->ad(), tmp) ) == 0 ) {  
+			if( (r_classad->EvalBool( "KILL",
+										r_classad,
+										tmp) ) == 0 ) { 
+				EXCEPT("Can't evaluate KILL");
+			}
+		}
+	} else {
+		if( (r_classad->EvalBool( "KILL",
+									r_cur->ad(), 
+									tmp)) == 0 ) { 
+			EXCEPT("Can't evaluate KILL");
+		}	
+	}
+	return tmp;
 }
 
 
-bool
-Resource::in_use()
+int
+Resource::eval_vacate()
 {
-	State s = state();
-	if( s == owner_state || s == unclaimed_state ) {
-		return false;
+	int tmp;
+	if( r_cur->universe() == VANILLA ) {
+		if( (r_classad->EvalBool( "VACATE_VANILLA",
+								   r_cur->ad(), 
+								   tmp)) == 0 ) {
+			if( (r_classad->EvalBool( "VACATE",
+									   r_cur->ad(), 
+									   tmp)) == 0 ) {
+				EXCEPT("Can't evaluate VACATE");
+			}
+		}
 	} else {
-		return true;
+		if( (r_classad->EvalBool( "VACATE",
+								   r_cur->ad(), 
+								   tmp)) == 0 ) {
+			EXCEPT("Can't evaluate VACATE");
+		}
 	}
+	return tmp;
+}
+
+
+int
+Resource::eval_suspend()
+{
+	int tmp;
+	if( r_cur->universe() == VANILLA ) {
+		if( (r_classad->EvalBool( "SUSPEND_VANILLA",
+								   r_cur->ad(),
+								   tmp)) == 0 ) {
+			if( (r_classad->EvalBool( "SUSPEND",
+									   r_cur->ad(),
+									   tmp)) == 0 ) {
+				EXCEPT("Can't evaluate SUSPEND");
+			}
+		}
+	} else {
+		if( (r_classad->EvalBool( "SUSPEND",
+								   r_cur->ad(),
+								   tmp)) == 0 ) {
+			EXCEPT("Can't evaluate SUSPEND");
+		}
+	}
+	return tmp;
+}
+
+
+int
+Resource::eval_continue()
+{
+	int tmp;
+	if( r_cur->universe() == VANILLA ) {
+		if( (r_classad->EvalBool( "CONTINUE_VANILLA",
+								   r_cur->ad(),
+								   tmp)) == 0 ) {
+			if( (r_classad->EvalBool( "CONTINUE",
+									   r_cur->ad(),
+									   tmp)) == 0 ) {
+				EXCEPT("Can't evaluate CONTINUE");
+			}
+		}
+	} else {	
+		if( (r_classad->EvalBool( "CONTINUE",
+								   r_cur->ad(),
+								   tmp)) == 0 ) {
+			EXCEPT("Can't evaluate CONTINUE");
+		}
+	}
+	return tmp;
 }
 
 
 void
-Resource::starter_exited()
+Resource::make_public_ad(ClassAd* pubCA)
 {
-	dprintf( D_ALWAYS, "Starter pid %d has exited.\n",
-			 r_starter->pid() );
-	r_starter->exited();
-	change_state( idle_act );
+	char*	expr;
+	char*	ptr;
+	char	tmp[1024];
+
+	StringList	config_file_exprs;
+
+	pubCA->SetMyTypeName( STARTD_ADTYPE );
+	pubCA->SetTargetTypeName( JOB_ADTYPE );
+
+	caInsert( pubCA, r_classad, ATTR_NAME );
+	caInsert( pubCA, r_classad, ATTR_MACHINE );
+	caInsert( pubCA, r_classad, ATTR_STARTD_IP_ADDR );
+	caInsert( pubCA, r_classad, ATTR_ARCH );
+	caInsert( pubCA, r_classad, ATTR_OPSYS );
+	caInsert( pubCA, r_classad, ATTR_UID_DOMAIN );
+	caInsert( pubCA, r_classad, ATTR_FILE_SYSTEM_DOMAIN );
+
+	r_state->update( pubCA );
+	r_attr->refresh( pubCA, PUBLIC );
+
+	caInsert( pubCA, r_classad, ATTR_CPUS );
+	caInsert( pubCA, r_classad, ATTR_MEMORY );
+	caInsert( pubCA, r_classad, ATTR_AFS_CELL );
+
+		// Put everything in the public classad from STARTD_EXPRS. 
+	ptr = param("STARTD_EXPRS");
+	if( ptr ) {
+		config_file_exprs.initializeFromString( ptr );   
+		free( ptr );
+		config_file_exprs.rewind();   
+		while( (ptr = config_file_exprs.next()) ) {
+			expr = param( ptr );
+			if( expr == NULL ) continue;
+			sprintf( tmp, "%s = %s", ptr, expr );
+			pubCA->Insert( tmp );
+			free( expr );
+		}
+	}
+		// Insert the currently active requirements expression.  If
+		// it's just START, we need to insert that too.
+	if( (r_reqexp->update( pubCA ) == ORIG) ) {
+		caInsert( pubCA, r_classad, ATTR_START );
+	}
+
+	caInsert( pubCA, r_classad, ATTR_RANK );
+	caInsert( pubCA, r_classad, ATTR_CURRENT_RANK );
+
+	if( this->state() == claimed_state ) {
+		caInsert( pubCA, r_classad, ATTR_CLIENT_MACHINE );
+		caInsert( pubCA, r_classad, ATTR_REMOTE_USER );
+		caInsert( pubCA, r_classad, ATTR_JOB_START );
+		caInsert( pubCA, r_classad, ATTR_JOB_ID );
+		caInsert( pubCA, r_classad, ATTR_JOB_UNIVERSE );
+		caInsert( pubCA, r_classad, ATTR_LAST_PERIODIC_CHECKPOINT );
+	}		
+
+}
+
+
+void
+Resource::make_private_ad(ClassAd* privCA)
+{
+	privCA->SetMyTypeName( STARTD_ADTYPE );
+	privCA->SetTargetTypeName( JOB_ADTYPE );
+
+	caInsert( privCA, r_classad, ATTR_NAME );
+	caInsert( privCA, r_classad, ATTR_STARTD_IP_ADDR );
+	caInsert( privCA, r_classad, ATTR_CAPABILITY );
 }
 
 
 int
-Resource::send_classad_to_sock( Sock* sock, int send_private ) 
+Resource::send_classad_to_sock( Sock* sock, ClassAd* pubCA, ClassAd* privCA ) 
 {
 	sock->encode();
-
-	r_classad->SetMyTypeName( STARTD_ADTYPE );
-	r_classad->SetTargetTypeName( JOB_ADTYPE );
-
-	if( send_private ) {
-		r_private_classad->SetMyTypeName( STARTD_ADTYPE );
-		r_private_classad->SetTargetTypeName( JOB_ADTYPE );
-	}
-
 	if( ! sock->put( UPDATE_STARTD_AD ) ) {
 		dprintf( D_ALWAYS, "Can't send command\n");
 		sock->end_of_message();
 		return FALSE;
 	}
-
-	if( ! r_classad->put( *sock ) ) {
-		dprintf( D_ALWAYS, "Can't send public classad\n");
-		sock->end_of_message();
-		return FALSE;
+	if( pubCA ) {
+		if( ! pubCA->put( *sock ) ) {
+			dprintf( D_ALWAYS, "Can't send public classad\n");
+			sock->end_of_message();
+			return FALSE;
+		}
 	}
-
-	if( send_private ) {
-		if( ! r_private_classad->put( *sock ) ) {
+	if( privCA ) {
+		if( ! privCA->put( *sock ) ) {
 			dprintf( D_ALWAYS, "Can't send private classad\n");
 			sock->end_of_message();
 			return FALSE;
 		}
 	}
-
 	if( ! sock->end_of_message() ) {
 		dprintf( D_ALWAYS, "Can't send end_of_message\n");
 		return FALSE;
 	}
-
 	return TRUE;
 }
 
 
+/*
+int
+Resource::
+*/
--- src/condor_startd.V6/Resource.h
+++ src/condor_startd.V6/Resource.h
@@ -1,40 +1,62 @@
 #ifndef _STARTD_RESOURCE_H
 #define _STARTD_RESOURCE_H
 
-class Resource
+class Resource : public Service
 {
 public:
 	Resource( Sock*, Sock* );
 	~Resource();
 
-		// Members to initialize and refresh the resource classad.
-	void	init_classad();		
+		// Public methods that can be called from command handlers
+	int		release_claim();	// Gracefully kill starter and release claim
+	int		kill_claim();		// Quickly kill starter and release claim
+	int		got_alive();			// You got a keep alive command
+	int 	periodic_checkpoint();	// Do a periodic checkpoint
+		// Multi-shadow wants to run more processes.  Send a SIGHUP to
+		// the starter
+	int		request_new_proc();
+		// Gracefully kill starter but keep claim	
+	int		deactivate_claim();	
+		// Quickly kill starter but keep claim
+	int		deactivate_claim_forcibly();
+	
+		// Resource state methods
+	int		change_state( State );
+	int		change_state( Activity );
+	int		change_state( State, Activity );
+	State		state()		{return r_state->state();};
+	Activity	activity()	{return r_state->activity();};
+	int		eval_state()	{return r_state->eval();};
+	bool	in_use();
+	float	condor_load() {return r_state->condor_load();};
+
+		// Called from the reaper to handle things for this rip
+	void	starter_exited();	
+
+		// Methods to initialize and refresh the resource classads.
+	int		init_classad();		
 	void	update_classad();	
 	void	timeout_classad();	
 
 	int		update();				// Update the central manager.
 	int		eval_and_update();		// Evaluate state and update CM. 
 
-		// Members to control various timers 
+		// Methods to control various timers 
 	int		start_update_timer();	// Timer for updating CM. 
 	int		start_poll_timer();		// Timer for polling the resource
 	void	cancel_poll_timer();	//    when it's in use by Condor. 
 
-		// Resource state members
-	int		change_state( State );
-	int		change_state( Activity );
-	State	state() {return r_state->state();};
-	Activity	activity() {return r_state->activity();};
-	int		eval_state() {return r_state->eval();};
-	bool	in_use();
-	float	condor_load() {return r_state->condor_load();};
-
-	void	starter_exited();
+ 		// Helper functions to evaluate resource expressions
+	int		wants_vacate();
+	int		wants_suspend();
+	int		eval_kill();
+	int		eval_vacate();
+	int		eval_suspend();
+	int		eval_continue();
 
 		// Data members
 	ResState*		r_state;	// Startd state object, contains state and activity
-	ClassAd*		r_classad;	// Resource classad
-	ClassAd*		r_private_classad;	// Private classad w/ capability, etc.
+	ClassAd*		r_classad;	// Resource classad (contains everything in config file)
 	Starter*		r_starter;	// Starter object
 	Match*			r_cur;		// Info about the current match
 	Match*			r_pre;		// Info about the possibly preempting match
@@ -43,9 +65,12 @@ public:
 	char*			r_name;		// Name of this resource
 	
 private:
-		// Send resource classad to the given sock.  The int indicates
-		// if we should send the private classad or not.
-	send_classad_to_sock( Sock* sock, int send_private = FALSE );
+		// Make public and private ads out of our classad
+	void make_public_ad( ClassAd* );
+	void make_private_ad( ClassAd* );
+		// Send given classads to the given sock.  If either pointer
+		// is NULL, the class ad is not sent.  
+	send_classad_to_sock( Sock* sock, ClassAd* pubCA, ClassAd* privCA );
 
 	int			did_update;		// Flag set when we do an update.
 
