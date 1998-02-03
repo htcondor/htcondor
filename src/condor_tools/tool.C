fe9d5bebfd58a6e5184f5a5f66bc0dad16e5b603
wright <wright>
1998-02-03 01:46:31 +0000

    Made "the tool" even smarter:  Now it understands vacate, checkpoint, reconfig_schedd, and reschedule.  It also decides which tool it is based on argv[0], not a command line option, which means it can seamlessly replace the above mentioned tools.

--- src/condor_tools/tool.C
+++ src/condor_tools/tool.C
@@ -28,8 +28,7 @@
 */ 
 
 /*********************************************************************
-* Tell the condor_master to re-read it's config files and tell all the
-* daemons under it to do the same.
+  Generic condor tool.
 *********************************************************************/
 
 #include "condor_common.h"
@@ -39,16 +38,79 @@
 #include "get_full_hostname.h"
 
 extern "C" char *get_master_addr(const char *);
+extern "C" char *get_schedd_addr(const char *);
+extern "C" char *get_startd_addr(const char *);
 extern "C" int strincmp( char*, char*, int );
 
 void do_command( char *host );
 
-void usage( char *str )
-{
-	fprintf( stderr, "Usage: %s command [hostnames]\n", str );
-	fprintf( stderr, "  where command is one of: reconfig, off, on, restart\n" );
-	fprintf( stderr, "  (if no hostname is specified, the local host is used).\n" );
+enum daemonType {MASTER, SCHEDD, STARTD};
 
+// Global variables
+int cmd = 0;
+daemonType dt;
+char* daemonName;
+char* daemonNames[] = {
+	"master",
+	"schedd",
+	"startd"
+};
+
+void
+usage( char *str )
+{
+	fprintf( stderr, "Usage: %s [-help] [hostnames]\n", str );
+	fprintf( stderr, "  -help gives this usage information.\n" );
+	fprintf( stderr, "  The given command is sent to all hosts specified.\n" ); 
+	fprintf( stderr, 
+			 "  (if no hostname is specified, the local host is used).\n\n" );
+
+	switch( cmd ) {
+	case DAEMONS_ON:
+		fprintf( stderr, 
+				 "  %s turns on the condor daemons specified in the config file.\n", 
+				 str);
+		break;
+	case DAEMONS_OFF:
+		fprintf( stderr, "  %s turns off any currently running condor daemons.\n", 
+				 str );
+		break;
+	case RESTART:
+		fprintf( stderr, "  %s causes the condor_master to restart itself.\n", str );
+		break;
+	case RECONFIG:
+		if( dt == MASTER ) {
+			fprintf( stderr, 
+					 "  %s causes all condor daemons to reconfigure themselves.\n", 
+					 str );
+		} else {
+				// This is reconfig_schedd
+			fprintf( stderr, 
+					 "  %s causes the condor_schedd to reconfigure itself.\n", 
+					 str );
+		}
+		break;
+	case RESCHEDULE:
+		fprintf( stderr, "  %s %s\n  %s\n", str, 
+				 "causes the condor_schedd to update the central manager",
+				 "and initiate a new negotiation cycle." );
+		break;
+	case VACATE_ALL_CLAIMS:
+		fprintf( stderr, "  %s %s\n  %s\n", str, 
+				 "causes the condor_startd to checkpoint any running jobs",
+				 "and make them vacate the machine." );
+		break;
+	case PCKPT_ALL_JOBS:
+		fprintf( stderr, "  %s %s\n  %s%s\n  %s\n", str, 
+				 "causes the condor_startd to perform a periodic", 
+				 "checkpoint on any running jobs.  The jobs continue to run ",
+				 "once", 
+				 "they are done checkpointing." );
+		break;
+	default:
+		fprintf( stderr, "  Error: Unknown command %s\n", str);
+	}
+	fprintf(stderr, "\n" );
 	exit( 1 );
 }
 
@@ -64,46 +126,81 @@ cmd_to_str( int c )
 		return "RECONFIG";
 	case RESTART:
 		return "RESTART";
-	default:
-		return "Unknown";
+	case VACATE_ALL_CLAIMS:
+		return "VACATE_ALL_CLAIMS";
+	case PCKPT_ALL_JOBS:
+		return "PCKPT_ALL_JOBS";
+	case RESCHEDULE:
+		return "RESCHEDULE";
 	}
 }
-
-
-int cmd;
-
-int main( int argc, char *argv[] )
+	
+int
+main( int argc, char *argv[] )
 {
-	char* fullname, *MyName = argv[0];
-	char* cmd_str;
+	char *fullname, *MyName = argv[0];
+	char *cmd_str;
 
 	config( 0 );
 
-	if( argc < 2 ) {
-		usage( MyName );
-	}
-	
-	cmd_str = argv[1];
-	if( !strincmp( cmd_str, "rec", 3 ) ) {
-		cmd = RECONFIG;
-	} else if( !strincmp( cmd_str, "res", 3 ) ) {
-		cmd = RESTART;
-	} else if( !strincmp( cmd_str, "on", 2 ) ) {
-		cmd = DAEMONS_ON;
-	} else if( !strincmp( cmd_str, "of", 2 ) ) {
-		cmd = DAEMONS_OFF;
+	MyName = strrchr( argv[0], DIR_DELIM_CHAR );
+	if( !MyName ) {
+		MyName = argv[0];
 	} else {
-		fprintf( stderr, "Error: unknown command %s\n", cmd_str );
+		MyName++;
+	}
+	cmd_str = strchr( MyName, '_');
+	if( !cmd_str ) {
+		fprintf( stderr, "Error: unknown command %s\n", MyName );
 		exit(1);
 	}
-	argv++;
+		// Figure out what kind of tool we are.
+	if( !strcmp( cmd_str, "_reconfig" ) ) {
+		cmd = RECONFIG;
+		dt = MASTER;
+		daemonName = daemonNames[0];
+	} else if( !strcmp( cmd_str, "_restart" ) ) {
+		cmd = RESTART;
+		dt = MASTER;
+		daemonName = daemonNames[0];
+	} else if( !strcmp( cmd_str, "_on" ) ) {
+		cmd = DAEMONS_ON;
+		dt = MASTER;
+		daemonName = daemonNames[0];
+	} else if( !strcmp( cmd_str, "_off" ) ) {
+		cmd = DAEMONS_OFF;
+		dt = MASTER;
+		daemonName = daemonNames[0];
+	} else if( !strcmp( cmd_str, "_reschedule" ) ) {
+		cmd = RESCHEDULE;
+		dt = SCHEDD;
+		daemonName = daemonNames[1];
+	} else if( !strcmp( cmd_str, "_reconfig_schedd" ) ) {
+		cmd = RECONFIG;
+		dt = SCHEDD;
+		daemonName = daemonNames[1];
+	} else if( !strcmp( cmd_str, "_vacate" ) ) {
+		cmd = VACATE_ALL_CLAIMS;
+		dt = STARTD;
+		daemonName = daemonNames[2];
+	} else if( !strcmp( cmd_str, "_checkpoint" ) ) {
+		cmd = PCKPT_ALL_JOBS;
+		dt = STARTD;
+		daemonName = daemonNames[2];
+	} else {
+		fprintf( stderr, "Error: unknown command %s\n", MyName );
+		exit(1);
+	}
 
-	if( argc == 2 ) {
+	if( argc == 1 ) {
 		do_command( my_full_hostname() );
 		exit( 0 );
 	}
 
 	for( argv++; *argv; argv++ ) {
+		if( **argv == '-' ) {
+			usage( MyName );
+		}
 		if( (fullname = get_full_hostname(*argv)) == NULL ) {
 			fprintf( stderr, "%s: unknown host %s\n", MyName, *argv );
 			continue;
@@ -117,27 +214,45 @@ int main( int argc, char *argv[] )
 void
 do_command( char *host )
 {
-	char		*masterAddr;
-
-	if ((masterAddr = get_master_addr(host)) == NULL) {
-		fprintf( stderr, "Can't find master address on %s\n", host );
-		return;
+	char		*daemonAddr;
+	switch( dt ) {
+	case MASTER:
+		if ((daemonAddr = get_master_addr(host)) == NULL) {
+			fprintf( stderr, "Can't find master address on %s\n", host );
+			return;
+		}
+		break;
+	case SCHEDD:
+		if ((daemonAddr = get_schedd_addr(host)) == NULL) {
+			fprintf( stderr, "Can't find master address on %s\n", host );
+			return;
+		}
+		break;
+	case STARTD:
+		if ((daemonAddr = get_startd_addr(host)) == NULL) {
+			fprintf( stderr, "Can't find master address on %s\n", host );
+			return;
+		}
+		break;
 	}
 
-		/* Connect to the master */
-	ReliSock sock(masterAddr, SCHED_PORT);
+		/* Connect to the daemon */
+	ReliSock sock(daemonAddr, 0);
 	if(sock.get_file_desc() < 0) {
-		fprintf( stderr, "Can't connect to master (%s)\n", masterAddr );
+		fprintf( stderr, "Can't connect to %s (%s)\n", daemonName, 
+				 daemonAddr );
 		return;
 	}
 
 	sock.encode();
 	if( !sock.code(cmd) || !sock.eom() ) {
-		fprintf( stderr, "Can't send %s command to master\n", cmd_to_str(cmd) );
+		fprintf( stderr, "Can't send %s command to %s\n", cmd_to_str(cmd), 
+				 daemonName );
 		return;
 	}
 
-	printf( "Sent %s command to master on %s\n", cmd_to_str(cmd), host );
+	printf( "Sent %s command to %s on %s\n", cmd_to_str(cmd), 
+			daemonName, host );
 }
 
 extern "C" int SetSyscalls(){}
