5a4b13d4c10cedf7522aa396a87c4781694dea47
tannenba <tannenba>
1998-02-25 23:19:27 +0000

    patches to port to IRIX 6.3

--- src/condor_includes/condor_fix_signal.h
+++ src/condor_includes/condor_fix_signal.h
@@ -6,23 +6,29 @@
 #endif
 
 #if defined(IRIX62)
-#	define SIGTRAP 5
-#	define SIGBUS 10
-#	define _save_XOPEN4UX _XOPEN4UX
-#	undef _XOPEN4UX
-#	define _XOPEN4UX 1
+#define SIGTRAP 5
+#define SIGBUS 10
+#define _save_NO_ANSIMODE _NO_ANSIMODE
+#undef _NO_ANSIMODE
+#define _NO_ANSIMODE 1
+#define _save_XOPEN4UX _XOPEN4UX
+#undef _XOPEN4UX
+#define _XOPEN4UX 1
 #endif
 
 #include <signal.h>
 
 #if defined(LINUX)
-#	undef SignalHandler
+#undef SignalHandler
 #endif
 
 #if defined(IRIX62)
-#   undef _XOPEN4UX
-#   define _XOPEN4UX _save_XOPEN4UX
-#   undef _save_XOPEN4UX
+#undef _NO_ANSIMODE
+#define _NO_ANSIMODE _save_NO_ANSIMODE
+#undef _XOPEN4UX
+#define _XOPEN4UX _save_XOPEN4UX
+#undef _save_NO_ANSIMODE
+#undef _save_XOPEN4UX
 #endif
 
 #endif CONDOR_FIX_SIGNAL_H
