204676361749e6b46ae73aee1481a24c625b7a08
wright <wright>
1998-02-25 22:15:41 +0000

    On IRIX62, define _XOPEN4UX 1, include signal.h, then restore the old value of _XOPEN4UX.

--- src/condor_includes/condor_fix_signal.h
+++ src/condor_includes/condor_fix_signal.h
@@ -2,18 +2,27 @@
 #define CONDOR_FIX_SIGNAL_H
 
 #if defined(LINUX)
-#define SignalHandler _hide_SignalHandler
+#	define SignalHandler _hide_SignalHandler
 #endif
 
 #if defined(IRIX62)
-#define SIGTRAP 5
-#define SIGBUS 10
+#	define SIGTRAP 5
+#	define SIGBUS 10
+#	define _save_XOPEN4UX _XOPEN4UX
+#	undef _XOPEN4UX
+#	define _XOPEN4UX 1
 #endif
 
 #include <signal.h>
 
 #if defined(LINUX)
-#undef SignalHandler
+#	undef SignalHandler
+#endif
+
+#if defined(IRIX62)
+#   undef _XOPEN4UX
+#   define _XOPEN4UX _save_XOPEN4UX
+#   undef _save_XOPEN4UX
 #endif
 
 #endif CONDOR_FIX_SIGNAL_H
