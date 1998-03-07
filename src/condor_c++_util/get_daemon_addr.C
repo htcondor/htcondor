52b8cbc38c9b5332de0e65e0bffa0568f442a896
wright <wright>
1998-03-07 21:52:27 +0000

    + Added build_valid_daemon_name(), which constructs a valid daemon   name for the local host, given the string a user has put in the   config file or used on the command line. + Added my_daemon_name(), which returns the name of the given daemon   as configured on the local host (params for subsys_NAME, and if   that's not defined, uses my_full_hostname()) + In get_daemon_addr(), if the name we're looking for matches the   local daemon name, do the local case. + In get_daemon_addr(), the local case now params for   subsys_ADDRESS_FILE, and if that exists, and the file can be read,   and it contains a reasonable address string, we use that instead of   querying the collector.

--- src/condor_c++_util/get_daemon_addr.C
+++ src/condor_c++_util/get_daemon_addr.C
@@ -34,67 +34,11 @@
 
 #include "condor_common.h"
 #include "condor_query.h"
+#include "condor_config.h"
 #include "my_hostname.h"
 #include "get_full_hostname.h"
 
 extern "C" {
-
-char* get_daemon_addr( const char* constraint_attr, 
-					   const char* name,
-					   AdTypes adtype, const char* attribute )
-{
-	CondorQuery			query(adtype);
-	ClassAd*			scan;
-	char*				daemonAddr = (char*)malloc(sizeof(char)*100);
-	char				constraint[500];
-	ClassAdList			ads;
-	char*				fullname;
-	
-	if( name && *name ) {
-		fullname = (char*)name;
-	} else {
-		fullname = my_full_hostname();
-	}
-
-	sprintf(constraint, "%s == \"%s\"", constraint_attr, fullname ); 
-	query.addConstraint(constraint);
-	query.fetchAds(ads);
-	ads.Open();
-	scan = ads.Next();
-	if(!scan)
-	{
-		delete daemonAddr;
-		return NULL; 
-	}
-	if(scan->EvalString(attribute, NULL, daemonAddr) == FALSE)
-	{
-		delete daemonAddr;
-		return NULL; 
-	}
-	return daemonAddr;
-}
-
-
-char*
-get_schedd_addr(const char* name)
-{
-	return get_daemon_addr(ATTR_NAME, name, SCHEDD_AD, ATTR_SCHEDD_IP_ADDR);
-} 
-
-
-char*
-get_startd_addr(const char* name)
-{
-	return get_daemon_addr(ATTR_MACHINE, name, STARTD_AD, ATTR_STARTD_IP_ADDR);
-} 
-
-
-char*
-get_master_addr(const char* name)
-{
-	return get_daemon_addr(ATTR_NAME, name, MASTER_AD, ATTR_MASTER_IP_ADDR);
-} 
-
 
 // Return the host portion of a daemon name string.  Either the name
 // includes an "@" sign, in which case we return whatever is after it,
@@ -112,6 +56,10 @@ get_host_part( const char* name )
 }
 
 
+// Return a pointer to a static buffer that contains the valid daemon
+// name that corresponds with the given name.  Basically, see if
+// there's an '@'.  If so, resolve everything after it as a hostname.
+// If not, resolve what we were passed as a hostname.
 char*
 get_daemon_name( const char* name )
 {
@@ -155,5 +103,149 @@ get_daemon_name( const char* name )
 		return daemon_name;
 	}
 }
+
+
+// Given some name, create a valid name for ourself with our full
+// hostname.  If the name contains an '@', strip off everything after
+// it and append my_full_hostname().  If there's no '@', try to
+// resolve what we have and see if it's my_full_hostname.  If so, use
+// it, otherwise, use name@my_full_hostname().
+char*
+build_valid_daemon_name( char* name ) 
+{
+	char *tmp;
+	static char daemonName[100];
+	int size;
+	if( name && *name ) {
+		tmp = strchr( name, '@' );
+		if( tmp ) {
+				// name we were passed has an '@'
+			*tmp = '\0';
+			sprintf( daemonName, "%s@%s", name, my_full_hostname() );
+		} else {
+				// no '@', see if what we have is our hostname
+			if( !strcmp(get_full_hostname(name), my_full_hostname()) ) {
+				sprintf( daemonName, "%s", my_full_hostname() );
+			} else {
+				sprintf( daemonName, "%s@%s", name, my_full_hostname() );
+			}
+		}
+	} else {
+		sprintf( daemonName, "%s", my_full_hostname() );
+	}
+	return daemonName;
+}
+
+
+// Return a pointer to a static buffer which contains the name of the
+// given daemon (specified by subsys string) as configured on the
+// local host.
+char*
+my_daemon_name( const char* subsys )
+{
+	static char my_name[100];
+	char line [100], *tmp;
+	sprintf( line, "%s_NAME", subsys );
+	tmp = param( line );
+	if( tmp ) {
+		sprintf( my_name, "%s", build_valid_daemon_name(tmp) );
+		free( tmp );
+	} else {
+		sprintf( my_name, "%s", my_full_hostname() );
+	}
+	return my_name;
+}
+
+
+char*
+get_daemon_addr( const char* constraint_attr, 
+				 const char* name, AdTypes adtype,
+				 const char* attribute, const char* subsys )
+{
+	CondorQuery			query(adtype);
+	ClassAd*			scan;
+	static char			daemonAddr[100];
+	char				constraint[500];
+	ClassAdList			ads;
+	char				*fullname = NULL, *addr_file, *tmp, *my_name;
+	FILE				*addr_fp;
+	int					is_local = 0;
+
+	daemonAddr[0] = '\0';
+	my_name = my_daemon_name( subsys );
+
+		// Figure out if we want to find a local daemon or not.
+	if( name && *name ) {
+		fullname = (char*)name;
+		if( !strcmp( fullname, my_name ) ) {
+			is_local = 1;
+		}
+	} else {
+		fullname = my_name;
+		is_local = 1;
+	}
+
+	if( is_local ) {
+		sprintf( constraint, "%s_ADDRESS_FILE", subsys );
+		addr_file = param( constraint );
+		if( addr_file ) {
+			if( (addr_fp = fopen(addr_file, "r")) ) {
+					// Read out the sinful string.
+				fgets( daemonAddr, 100, addr_fp );
+					// chop off the newline
+				tmp = strchr( daemonAddr, '\n' );
+				if( tmp ) {
+					*tmp = '\0';
+				}
+				fclose( addr_fp );
+			}
+			free( addr_file );
+		} 
+		if( daemonAddr[0] == '<' ) {
+				// We found something reasonable.
+			return daemonAddr;
+		}
+	}
+
+	sprintf(constraint, "%s == \"%s\"", constraint_attr, fullname ); 
+	query.addConstraint(constraint);
+	query.fetchAds(ads);
+	ads.Open();
+	scan = ads.Next();
+	if(!scan)
+	{
+		return NULL; 
+	}
+	if(scan->EvalString(attribute, NULL, daemonAddr) == FALSE)
+	{
+		return NULL; 
+	}
+	return daemonAddr;
+}
+
+
+char*
+get_schedd_addr(const char* name)
+{
+	return get_daemon_addr( ATTR_NAME, name, SCHEDD_AD, 
+							ATTR_SCHEDD_IP_ADDR, "SCHEDD" );
+} 
+
+
+char*
+get_startd_addr(const char* name)
+{
+	return get_daemon_addr( ATTR_MACHINE, name, STARTD_AD, 
+							ATTR_STARTD_IP_ADDR, "STARTD" );
+} 
+
+
+char*
+get_master_addr(const char* name)
+{
+	return get_daemon_addr( ATTR_NAME, name, MASTER_AD, 
+							ATTR_MASTER_IP_ADDR, "MASTER" );
+} 
+
 
 } /* extern "C" */
