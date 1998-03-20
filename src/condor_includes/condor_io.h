47dd3dfdb706d48fcbc6e8ccfa3f11325517c6c8
wright <wright>
1998-03-20 00:57:27 +0000

    Removed a ton of stuff that is dealt with in condor_common.h.

--- src/condor_includes/condor_io.h
+++ src/condor_includes/condor_io.h
@@ -1,46 +1,7 @@
 #ifndef CONDOR_IO_H
 #define CONDOR_IO_H
 
-
 #include "condor_common.h"
-#include "condor_constants.h"
-
-#if !defined(WIN32)
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#endif
-
-#if defined(OSF1) && defined(__cplusplus)
-
-extern "C" {
-void bzero(char *, int);
-int gethostname(char *, int);
-
-unsigned int htonl(unsigned int);
-unsigned int ntohl(unsigned int);
-unsigned short htons(unsigned short);
-unsigned short ntohs(unsigned short);
-}
-
-#endif
-
-/*
-#if defined(__cplusplus)
-
-extern "C" {
-int accept(int, void *, int *);
-int bind(int, void *, int);
-int connect(int, void *, int);
-int listen(int, int);
-int socket(int, int, int);
-int getsockname(int, struct sockaddr *,int *);
-}
-
-#endif
-*/
 
 #include "stream.h"
 #include "buffers.h"
@@ -49,5 +10,4 @@ int getsockname(int, struct sockaddr *,int *);
 #include "safe_sock.h"
 #include "sockCache.h"
 
-
-#endif
+#endif /* CONDOR_IO_H */
