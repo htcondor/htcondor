8fdb862c9b86bf5fb4a00b52f9510e2177de75c0
wright <wright>
1998-04-06 16:25:59 +0000

    Removed dead code from IRIX3.1, AIX, SunOS4, Ultrix, Dynix, etc.

--- src/condor_util_lib/virt_mem.c
+++ src/condor_util_lib/virt_mem.c
@@ -23,11 +23,9 @@
 /* Do not remove the following line! Required for SCCS. */
 static char sccsid[] = "@(#)virt_mem.c	4.3 9/18/91"; 
 
- 
+#include "condor_common.h"
 
 #if defined(WIN32)
-
-#include "condor_common.h"
 
 /*
 ** Try to determine the swap space available on our own machine.  The answer
@@ -43,18 +41,10 @@ calc_virt_memory()
 
 #else
 
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/time.h>
-#include <sys/resource.h>
-#include <ctype.h>
 #include <nlist.h>
-#include <fcntl.h>
 #include "debug.h"
 #include "except.h"
 #include "condor_uid.h"
-
-#define MATCH 0
 
 /* Fix #3. work around problems with pclose if caller handle SIGCHLD. */
 int HasSigchldHandler = 0;
@@ -142,155 +132,6 @@ calc_virt_memory()
 	return free_swap / 1024;
 }
 #endif /*LINUX*/
-
-#ifdef IRIX331
-/*
-** Try to determine the swap space available on our own machine.  The answer
-** is in kilobytes.
-*/
-close_kmem() {}
-calc_virt_memory()
-{
-	return free_fs_blocks( "/debug" );
-}
-#endif /* Irix 3.1.1 code */
-
-#if defined(AIX31) || defined(AIX32)
-/*
-** Try to determine the swap space available on our own machine.  The answer
-** is in kilobytes.
-*/
-
-static int memfd;
-
-close_kmem()
-{
-	close( memfd );
-}
-
-calc_virt_memory()
-{
-/*
- * We can look in /dev/kmem and avoid ugliness with pstat for AIX 3. jrhc
- *
- */
-	struct {
-		int pad0, pad1, pad2, pad3, pad4, pad5, pad6, pad7, pad8;
-		int total_real_mem;		/* Real memory ( hardware memory ) */
-		int pad10;
-		int free_real_mem;      /* Real memory not currently allocated */
-		int pad12, pad13;
-		int total_virt_mem;		/* Defined swap space on disk. Seems not to 
-											include any real memory. */
-		int free_virt_mem;		/* Defined swap space which is not allocated */
-	} vmker;
-	struct nlist knames[1] = {"vmker", 0, 0, 0, 0, 0 };
-	static int initialized = 0;
-
-	/* get address of vmker structure */
-	if (knlist(knames, 1, sizeof(struct nlist)) == -1) {
-		dprintf(D_ALWAYS, "Cannot read kernel structure for swap information.\n");
-		return(-1);
-	}
-
-	/* open /dev/kmem. Since we do this often, leave it open for next time. */
-	if ( !initialized ) {
-		memfd = open("/dev/kmem", O_RDONLY);
-		if ( memfd < 0 ) {
-			dprintf(D_ALWAYS, " Cannot open /dev/kmem.\n");
-			return(-1);
-		}
-		initialized = 1;
-	} 
-	
-	/* Read in vmker structure from /dev/kmem */
-	lseek(memfd, knames[0].n_value, SEEK_SET);
-	if ( read(memfd, &vmker, sizeof(vmker)) < 0 ) {
-		dprintf(D_ALWAYS, "Cannot read /dev/kmem.\n");
-		return(-1);
-	}
-
-	/* Results are in 4k blocks. Convert to kbytes and return. */
-	return( (vmker.free_virt_mem) * 4 );
-}
-#endif /* AIX 3 code */
-
-#if defined(SUNOS40) || defined(SUNOS41) || defined(CMOS)
-#include <kvm.h>
-#include <vm/anon.h>
-static kvm_t *kd = (kvm_t *)NULL;
-
-close_kmem()
-{
-	kvm_close( kd );
-}
-
-calc_virt_memory()
-{
-	static int initialized = 0;
-	static int page_to_k;              /* for page to Kbyte conversion */
-	static unsigned long offset;   /* Store offset to symbol in kernel */
-	static char s_anon[] = "_anoninfo";       /* symbol name in kernel */
-	static char *namelist, *corefile, *swapfile;
-	static char errstr [] = "virt_mem.c";
-	struct nlist nl[2];
-	struct anoninfo a_info;
-	int result, vm_free;
-	priv_state	priv;
-	
-	
-	
-	/* Operate as root to read /dev/kmem */
-	priv = set_root_priv();
-	
-	/*
-	 * First time in this call, get the offset to the anon structure.
-	 * and page to kbyte conversion.
-	 */
-	if ( !initialized ) {
-		namelist = corefile = swapfile = (char *) NULL;
-		nl [0].n_name = s_anon;
-		nl [1].n_name = (char *) NULL;
-		kd = kvm_open (namelist, corefile, swapfile, O_RDONLY, errstr);
-		if ( kd == (kvm_t *) NULL) {
-			set_priv(priv);
-			dprintf (D_ALWAYS, "Open failure on kernel. First call\n");
-			return (-1);
-		}
-		if (kvm_nlist (kd, nl) != 0) {
-			(void)kvm_close( kd );
-			kd = (kvm_t *)NULL;
-			set_priv(priv);
-			dprintf (D_ALWAYS, "kvm_nlist failed. First call.\n");
-			return (-1);
-		}
-		offset = nl[0].n_value;
-		page_to_k = getpagesize () / 1024;
-		initialized = 1;
-	}
-	if( !kd ) {
-		kd = kvm_open (namelist, corefile, swapfile, O_RDONLY, errstr);
-		if ( kd == (kvm_t *) NULL) {
-			set_priv(priv);
-			dprintf (D_ALWAYS, "Open failure on kernel.\n");
-			return (-1);
-		}
-	}
-	result = kvm_read (kd, offset, &a_info, sizeof (a_info));
-	if (result != sizeof (a_info)) {
-		kvm_close( kd );
-		kd = (kvm_t *)NULL;
-		set_priv(priv);
-		dprintf (D_ALWAYS, "kvm_read error.\n");
-		return (-1);
-	}
-	vm_free = (int) (a_info.ani_max - a_info.ani_resv);
-	vm_free *= page_to_k;
-	
-	set_priv(priv);
-	return (vm_free);
-}
-#endif /* SunOS4.0 and SunOS4.1 code */
 
 #ifdef HPUX9
 /* The HPUX code here grabs the amount of free swap in Kbyes.
@@ -475,42 +316,6 @@ calc_virt_memory()
 	} else {
 		return size;
 	}
-}
-#endif
-
-
-#if defined(ULTRIX43) || defined(ULTRIX42) || defined(BSD43) || defined(DYNIX) || defined(SUNOS32) 
-int parse_pstat_line( str )
-char	*str;
-{
-	char	*ptr,*temp;
-	int		n_blocks, block_size, answer, count=0;
-
-        if( strncmp("avail",str,5) != MATCH ) {
-		return -1;
-	}
-
-	for(ptr=str; *ptr && !isdigit(*ptr); ptr++ )
-		;
-	if( !ptr ) {
-		return -1;
-	}
-	n_blocks = atoi( ptr );
-
-	while( *ptr && *ptr != '*' ) {
-		ptr += 1;
-	}
-	if( *ptr != '*' ) {
-		return -1;
-	}
-	ptr += 1;
-	block_size = atoi( ptr );
-#if defined(DYNIX)
-	answer =  (n_blocks/2 - 1) * block_size;
-#else defined(DYNIX)
-	answer =  (n_blocks - 1) * block_size;
-#endif defined(DYNIX)
-	return answer;
 }
 #endif
 
