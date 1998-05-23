ffe8d3522a0654ff6eb6b5b768d217ac75dec99a
wright <wright>
1998-05-23 00:07:23 +0000

    patch level 4 version string.

--- src/condor_c++_util/condor_version.C
+++ src/condor_c++_util/condor_version.C
@@ -22,7 +22,7 @@
 ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
 
 /* Here is the version string - update before a public release */
-static char* CondorVersionString = "$Version: 6.0 patch level 3 1998/05/15 $";
+static char* CondorVersionString = "$Version: 6.0 patch level 4 1998/05/22 $";
 
 
 extern "C" {
