1bf5aa20e58d7b5ef79b06d302fc484611622560
wright <wright>
1998-06-02 21:57:04 +0000

    With glibc, there are a bunch of changes in prototypes... things that the man pages say are ints that the headers say are unsigned, things that are supposed to be const are not, etc, etc.  So, we hide the broken prototypes and define our own.

--- src/condor_includes/condor_fix_socket.h
+++ src/condor_includes/condor_fix_socket.h
@@ -24,7 +24,7 @@
 #define FIX_SOCKET_H
 
 
-#if defined(OSF1) && defined( _XOPEN_SOURCE_EXTENDED )
+#if defined(OSF1) && defined(_XOPEN_SOURCE_EXTENDED)
 /* If _XOPEN_SOURCE_EXTENDED is defined on dux4.0 when you include
    sys/socket.h "accept" gets #define'd, which screws up condor_io,
    since we have methods called "accept" in there.  -Derek 3/26/98 */
@@ -32,11 +32,44 @@
 #	undef _XOPEN_SOURCE_EXTENDED
 #endif
 
+#if defined(LINUX) && defined(GLIBC) 
+/* With the new glibc, there are a bunch of changes in
+   prototypes... things that the man pages say are ints that the
+   headers say are unsigned, things that are supposed to be const are
+   not, etc, etc.  So, we hide the broken prototypes and define our
+   own. -Derek 4/17/98 */
+#define accept hide_accept
+#define getpeername hide_getpeername
+#define getsockname hide_getsockname
+#define setsockopt hide_setsockopt
+#define recvfrom hide_recvfrom
+#endif /* LINUX && GLIBC */
+
 #if !defined(WIN32)
 #	include <sys/socket.h>
 #endif
 
-#if defined(OSF1) && defined( CONDOR_XOPEN_SOURCE_EXTENDED )
+#if defined(LINUX) && defined(GLIBC) 
+#undef accept
+#undef getpeername
+#undef getsockname
+#undef setsockopt
+#undef recvfrom
+#ifdef __cplusplus
+extern "C" {
+#endif
+int accept(int, struct sockaddr *, int *);
+int getpeername(int, struct sockaddr *, int *);
+int getsockname(int, struct sockaddr *, int *);
+int setsockopt(int, int, int, const void *, int);
+int recvfrom(int, void *, int, unsigned int, struct sockaddr *, int *);
+#ifdef __cplusplus
+}
+#endif
+#endif /* LINUX && GLIBC */
+
+
+#if defined(OSF1) && defined(CONDOR_XOPEN_SOURCE_EXTENDED)
 #	undef CONDOR_XOPEN_SOURCE_EXTENDED
 #	define _XOPEN_SOURCE_EXTENDED
 #endif
