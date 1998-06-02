b2e62b1af8ef9fcd2e8e3d82857e080be74edb2a
wright <wright>
1998-06-02 23:48:44 +0000

    Removed stuff that's in condor_common.h

--- src/condor_c++_util/format_time.C
+++ src/condor_c++_util/format_time.C
@@ -21,13 +21,8 @@
  * WI 53706-1685, (608) 262-0856 or miron@cs.wisc.edu.
 ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
 
-#ifdef WIN32
 #include "condor_common.h"
-#else
-#include <stdio.h>
-#include <time.h>
 #include "types.h"
-#endif
 
 /*
   Format a date expressed in "UNIX time" into "month/day hour:minute".
