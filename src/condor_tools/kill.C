c61b7220ce62e11d8be66a94176a62d90c682d5a
wright <wright>
1998-06-02 18:19:07 +0000

    + When we build the list of Condor processes, we now have a list of   CondorPid objects, which hold not only the pid, but the full output   line from ps for use in syslog messages. + Added syslog messages whenever we kill a process, including the pid   we killed and the full output from ps for that pid. + Reorganized the code a little and gave it more comments.

--- src/condor_tools/kill.C
+++ src/condor_tools/kill.C
@@ -37,26 +37,34 @@
 #include <string.h>
 #include <sys/types.h>
 #include <signal.h>
+#include <syslog.h>
+#include <assert.h>
 
-#include "simplelist.h"
-template class SimpleList<pid_t>;
+#include "list.h"
+
+class CondorPid {
+public:
+	CondorPid( pid_t, char* );
+	~CondorPid();
+	bool Kill( int );	// Kill this pid with the given signal, and log it 
+	pid_t Pid() {return m_pid;};
+private:
+	pid_t m_pid;
+	char* m_line;
+};
+
 
 // Global variables
 extern int errno;
 static const char* PS_CMD = "/bin/ps -ef";
-SimpleList<pid_t>* condor_pids = NULL;
+List<CondorPid>* condor_pids = NULL;
+
 
 // Prototypes
+void usage(char*);			// print usage info and exit
+void my_exit(int);			// close the syslog and exit
 void find_condor_pids();	// find all pids from ps that begin with "condor_"
-bool is_condor(pid_t pid);	// returns if the given pid is a Condor pid
-void do_kill(pid_t pid, int sig);
-
-
-void
-usage( char* name ) {
-	fprintf( stderr, "Usage: %s -<signal number> <pid> ...\n", name );
-	exit( 1 );
-}
+CondorPid* find_cpid(pid_t);	// find the CondorPid object with the given pid
 
 
 int
@@ -65,11 +73,13 @@ main( int argc, char* argv[] )
 	char** tmp;
 	int sig = 0;
 	pid_t pid;
+	CondorPid *cpid;
+	int had_err = 0;
 
+		// Make sure we've got reasonable input
 	if( argc < 3 ) {
 		usage( argv[0] );
 	}
-
 	if( argv[1][0] != '-' || !(argv[1][1]) ) {
 		usage( argv[0] );
 	}		
@@ -80,83 +90,144 @@ main( int argc, char* argv[] )
 		usage( argv[0] );
 	}
 
-	find_condor_pids();	
+		// Initialize system logging
+	openlog( "condor_kill", LOG_PID, LOG_AUTH );
+
+		// Find all Condor processes and store them in a linked list
+	find_condor_pids();			
 	
+		// Process the rest of our args
 	for( tmp = &argv[2]; *tmp; tmp++ ) {
+
+			// Make sure our arg is a reasonably valid pid
 		pid = (pid_t)atoi(*tmp);
 		if( !pid ) {
 			fprintf( stderr, "Error: \"%s\" is not a valid pid\n", *tmp );
+			had_err = 1;
 			continue;
 		}
-		if( is_condor(pid) ) {
-			do_kill( pid, sig );
+
+			// See if that pid is a Condor process 
+		if( (cpid = find_cpid(pid)) ) {
+				// If so, try to kill it.
+			if( !cpid->Kill(sig) ) {
+				had_err = 1;
+			}
 		} else {
-			fprintf( stderr, "Error: %d is not a Condor pid\n", pid );
+			fprintf( stderr, "Error: %ld is not a Condor pid\n", (long)pid );
+			had_err = 1;
 			continue;
 		}
 	}
+	my_exit( had_err );
 }
 
 
+//////////////////////////////////////////////////////////////////////
+// Utility functions
+//////////////////////////////////////////////////////////////////////
+
+void
+usage( char* name ) {
+	fprintf( stderr, "Usage: %s -<signal number> <pid> ...\n", name );
+	my_exit( 1 );
+}
+
+
+// Find all Condor processes in the system.  We open /bin/ps, and find
+// all entries that have "condor_" in argv[0].  For each pid, we save
+// the whole line from ps to print out in the syslog.
 void
 find_condor_pids() {
 
 	FILE *ps;
 	char line[250], *tmp;
 	pid_t pid;
+	CondorPid *cpid;
 
-	condor_pids = new SimpleList<pid_t>;
+	condor_pids = new List<CondorPid>;
 	if( !condor_pids ) {
 		fprintf( stderr, "ERROR: out of memory!\n" );
-		exit( 1 );
+		my_exit( 1 );
 	}
 	
 	ps = popen( PS_CMD, "r" );
 	if( !ps ) {
 		fprintf( stderr, "Error: can't open %s for reading.\n", 
 				 PS_CMD );
-		exit( 1 );
+		my_exit( 1 );
 	}
 
-   fgets( line, 249, ps );  /* skip the column header line */
-   while( fgets(line,249,ps) != NULL ) {
-	   tmp = strstr( line, "condor_" );
-	   if( tmp ) {
-		   if ( sizeof(pid_t) == sizeof(long) ) {
-			   sscanf( line, "%*s %ld", &pid );
-		   } else { 
-			   sscanf( line, "%*s %d", &pid );
-		   }
-		   condor_pids->Append( pid );
-	   }    
-   }
+	fgets( line, 249, ps );  /* skip the column header line */
+	while( fgets(line,249,ps) != NULL ) {
+		tmp = strstr( line, "condor_" );
+		if( tmp ) {
+				// Found a line that contains "condor_", grab the pid
+			sscanf( line, "%*s %ld", &pid );
+			cpid = new CondorPid( pid, line );
+			condor_pids->Append( cpid );
+		}
+	}    
 }
 
 
-bool
-is_condor( pid_t pid )
+// Return a pointer to the CondorPid object with the given pid
+CondorPid*
+find_cpid( pid_t pid ) 
 {
-	
-	pid_t tmp;
+	CondorPid *cpid; 
 	if( !condor_pids ) {
-		return false;
+		return NULL;
 	}
 	condor_pids->Rewind();
-	while( condor_pids->Next(tmp) ) {
-		if( pid == tmp ) return true;
+	while( (cpid = condor_pids->Next()) ) {
+		if( cpid->Pid() == pid ) return cpid;
 	}
-	return false;
+	return NULL;
 }
 
 
 void
-do_kill( pid_t pid, int sig )
+my_exit( int status ) 
 {
-	if( kill(pid, sig) < 0 ) {
+	closelog();
+	exit( status );
+}
+
+
+//////////////////////////////////////////////////////////////////////
+// class CondorPid member functions
+//////////////////////////////////////////////////////////////////////
+
+bool
+CondorPid::Kill( int sig )
+{
+	if( kill(m_pid, sig) < 0 ) {
 		fprintf( stderr, 
-				 "Error: can't kill pid %d with signal %d, errno: %d (%s)\n",
-				 pid, sig, errno, strerror(errno) );
+				 "Error: can't kill pid %ld with signal %d, errno: %d (%s)\n",
+				 (long)m_pid, sig, errno, strerror(errno) );
+		return false;
+	} else {
+		printf( "killed pid: %ld, %s\n", 
+				(long)m_pid, m_line );
+		syslog( LOG_INFO, "killed pid %ld, ps info: %s", 
+				(long)m_pid, m_line );
+		return true;
 	}
+}
+
+
+CondorPid::CondorPid( pid_t pid, char* line )
+{
+	assert( line );
+	m_pid = pid;
+	m_line = strdup( line );
+}
+
+
+CondorPid::~CondorPid()
+{
+	free( m_line );
 }
 
 
