e00e78c623b1682e684b2614eccbb8b7e2f6824a
wright <wright>
1998-06-13 22:20:52 +0000

    Optimistically, the non-beta 6.0.1 will be out on Monday, 6/15/98.

--- src/condor_c++_util/condor_version.C
+++ src/condor_c++_util/condor_version.C
@@ -22,7 +22,7 @@
 ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
 
 /* Here is the version string - update before a public release */
-static char* CondorVersionString = "$Version: 6.0.1 beta 1998/06/09 $";
+static char* CondorVersionString = "$Version: 6.0.1 1998/06/15 $";
 
 
 extern "C" {
