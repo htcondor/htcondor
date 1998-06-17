7f4569fb0080338716d73c1b07e9155cfe1a4805
unknown <unknown>
1998-06-17 20:37:59 +0000

    This commit was manufactured by cvs2svn to create branch 'V6_0-branch'.

--- src/condor_classad.V6/classad_package.h
+++
@@ -1,7 +0,0 @@
-#ifndef __CLASSAD_PACKAGE_H__
-#define __CLASSAD_PACKAGE_H__
-
-#include "common.h"
-#include "exprTree.h"
-
-#endif
--- src/condor_dagman/RecoveryLogConstants.h
+++
@@ -1,9 +0,0 @@
-
-#define RECOVER 0
-#define NORECOVER -1
-#define DAGMAN_Initialize 1000
-#define DAGMAN_AddDependency 1001
-#define DAGMAN_DoneInitialize 1002
-#define CONDOR_SubmitJob 1003
-#define CONDOR_JobTerminate 1004
-
