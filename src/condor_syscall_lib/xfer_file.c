68071a41c457e3e764c99137f8d794bfe9a32694
jbasney <jbasney>
1998-07-07 15:41:22 +0000

    use condor_common.h

--- src/condor_syscall_lib/xfer_file.c
+++ src/condor_syscall_lib/xfer_file.c
@@ -21,12 +21,7 @@
  * WI 53706-1685, (608) 262-0856 or miron@cs.wisc.edu.
 ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
 
-
-#include <stdio.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/file.h>
-#include <sys/param.h>
+#include "condor_common.h"
 #include "condor_sys.h"
 #include "ckpt_file.h"
 #include "debug.h"
