9e6444044e26f983a22470d63fe961ce285d3822
wright <wright>
1998-07-17 15:34:02 +0000

    For the daemonType enum, use "DT_" prefix on all enum values to avoid conflicts with other enums (e.g. "NEGOTIATOR" from DC permissions).

--- src/condor_c++_util/daemon_types.h
+++ src/condor_c++_util/daemon_types.h
@@ -1,7 +1,8 @@
 #ifndef _CONDOR_DAEMON_TYPES_H
 #define _CONDOR_DAEMON_TYPES_H
 
-enum daemonType {MASTER, SCHEDD, STARTD, COLLECTOR, NEGOTIATOR, KBDD};
+enum daemonType { DT_MASTER, DT_SCHEDD, DT_STARTD, DT_COLLECTOR, 
+				  DT_NEGOTIATOR, DT_KBDD };
 
 #ifdef __cplusplus
 extern "C" {
