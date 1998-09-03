32dc78010a41ba35016bef25bc5adfab31520a4a
stanis <stanis>
1998-09-03 01:25:38 +0000

    Final release for 6.0.1

--- src/condor_c++_util/condor_version.C
+++ src/condor_c++_util/condor_version.C
@@ -22,7 +22,7 @@
 ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
 
 /* Here is the version string - update before a public release */
-static char* CondorVersionString = "$Version: 6.0.1 rc1 1998/07/27 $";
+static char* CondorVersionString = "$Version: 6.0.1 1998/09/02 $";
 
 
 extern "C" {
