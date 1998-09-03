df70c7b9948af7e04325b8536e2b8bbb28f57407
wright <wright>
1998-09-03 22:10:13 +0000

    On the trunk, we're now onto the 6.1 development series.

--- src/condor_c++_util/condor_version.C
+++ src/condor_c++_util/condor_version.C
@@ -22,8 +22,7 @@
 ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
 
 /* Here is the version string - update before a public release */
-static char* CondorVersionString = "$Version: 6.0.1 1998/09/02 $";
-
+static char* CondorVersionString = "$Version: 6.1.0 alpha0 1998/09/03 $";
 
 extern "C" {
 
