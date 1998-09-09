4441c980f135c22a3d5825321bb0f7ec7597372b
mikeu <mikeu>
1998-09-09 22:29:12 +0000

    added InitializeConnectionAuth for connections over auth(enticated) sockets

--- src/condor_schedd.V6/qmgmt_constants.h
+++ src/condor_schedd.V6/qmgmt_constants.h
@@ -41,3 +41,4 @@
 #define CONDOR_GetJobByConstraint	10019
 #define CONDOR_GetNextJobByConstraint	10020
 #define	CONDOR_SetAttributeByConstraint	10021		/* Todd */
+#define	CONDOR_InitializeConnectionAuth	10022
