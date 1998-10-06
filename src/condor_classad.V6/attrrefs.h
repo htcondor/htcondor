ed27c6257a4b4da31d102d5f01f4e8ba42b5441a
raman <raman>
1998-10-06 22:48:59 +0000

    + Updated docs + Changed evaluate() methods to return bool

--- src/condor_classad.V6/attrrefs.h
+++ src/condor_classad.V6/attrrefs.h
@@ -3,27 +3,47 @@
 
 #include "stringSpace.h"
 
+/** Represents a attribute reference node in the expression tree
+*/
 class AttributeReference : public ExprTree 
 {
   	public:
-		// ctor/dtor	
+		/** Constructor
+		*/
     	AttributeReference ();
+
+		/**  Destructor
+		*/
     	~AttributeReference ();
 
-    	// override methods
-		virtual bool toSink (Sink &);
-		virtual void setParentScope( ClassAd* );
+        /** Sends the attribute reference object to a Sink.
+            @param s The Sink object.
+            @return false if the expression could not be successfully
+                unparsed into the sink.
+            @see Sink
+        */
+		virtual bool toSink (Sink &s);
 
-    	// specific methods
-    	void setReference (ExprTree *, char *, bool = false);
+		/** Set the attribute reference.
+			@param expr The expression part of the reference (i.e., in
+				case of expr.attr).  This parameter is NULL if the reference
+				is absolute (i.e., .attr) or simple (i.e., attr).
+			@param attrName The name of the reference.  This string is
+				duplicated internally.
+			@param absolute True if the reference is an absolute reference
+				(i.e., in case of .attr).  This parameter cannot be true if
+				expr is not NULL
+		*/
+    	void setReference (ExprTree *expr, char *attrName, bool absolute=false);
 
   	private:
 		// private ctor for internal use
 		AttributeReference( ExprTree*, char*, bool );
 
 		virtual ExprTree* _copy( CopyMode );
-    	virtual void _evaluate( EvalState & , Value & );
-    	virtual void _evaluate( EvalState & , Value &, ExprTree*& );
+		virtual void _setParentScope( ClassAd* p );
+    	virtual bool _evaluate( EvalState & , Value & );
+    	virtual bool _evaluate( EvalState & , Value &, ExprTree*& );
     	virtual bool _flatten( EvalState&, Value&, ExprTree*&, OpKind* );
 
 		int	findExpr( EvalState&, ExprTree*&, ExprTree*&, bool );
