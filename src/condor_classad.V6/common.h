d275031bf7b16523dba3e54f58c126e764684122
raman <raman>
1998-10-07 00:24:52 +0000

    +  Documented enumerations +  Added EVAL_FAIL constant

--- src/condor_classad.V6/common.h
+++ src/condor_classad.V6/common.h
@@ -9,71 +9,99 @@ enum MmMode
 	MEM_DISPOSE,
 };
 
+enum {
+	EVAL_FAIL,
+	EVAL_OK,
+	EVAL_UNDEF,
+	EVAL_ERROR
+};
+
+/// The modes in which an expression can be copied.
 enum CopyMode 
 { 
-	EXPR_DEFAULT_COPY,
-	EXPR_DEEP_COPY, 
-	EXPR_REF_COUNT 
+	/** Default copy mode (EXPR_DEEP_COPY or EXPR_REF_COUNT)*/EXPR_DEFAULT_COPY,
+	/** Make deep copies */ EXPR_DEEP_COPY, 
+	/** Use reference counts */ EXPR_REF_COUNT 
 };
 
+/// The multiplicative factors for a numeric literal.
 enum NumberFactor 
 { 
-    NO_FACTOR   = 1,
-    K_FACTOR    = 1024,             // kilo
-    M_FACTOR    = 1024*1024,        // mega
-    G_FACTOR    = 1024*1024*1024    // giga
+    /** No factor specified */ NO_FACTOR   = 1,
+    /** Kilo factor */ 	K_FACTOR = 1024,
+    /** Mega factor */ 	M_FACTOR = 1024*1024,
+    /** Giga factor */ 	G_FACTOR = 1024*1024*1024
 };
 
-// kinds of operators
+/// List of supported operators 
 enum OpKind 
 {
-    __NO_OP__,              // convenience
+    /** No op */ __NO_OP__,              // convenience
 
     __FIRST_OP__,
 
-    __COMPARISON_START__    = __FIRST_OP__,
-    LESS_THAN_OP            = __COMPARISON_START__,
-    LESS_OR_EQUAL_OP,       // (comparison)
-    NOT_EQUAL_OP,           // (comparison)
-    EQUAL_OP,               // (comparison)
-    META_EQUAL_OP,          // (comparison)
-	IS_OP					= META_EQUAL_OP,
-    META_NOT_EQUAL_OP,      // (comparison)
-	ISNT_OP					= META_NOT_EQUAL_OP,
-    GREATER_OR_EQUAL_OP,    // (comparison)
-    GREATER_THAN_OP,        // (comparison)
-    __COMPARISON_END__      = GREATER_THAN_OP,
+    __COMPARISON_START__   	= __FIRST_OP__,
+	/** @name Strict comparison operators */
+	//@{
+    /** Less than operator 	*/ LESS_THAN_OP            = __COMPARISON_START__,
+    /** Less or equal 		*/	LESS_OR_EQUAL_OP,       // (comparison)
+    /** Not equal 			*/ 	NOT_EQUAL_OP,           // (comparison)
+    /** Equal 				*/	EQUAL_OP,               // (comparison)
+    /** Greater or equal 	*/ 	GREATER_OR_EQUAL_OP,    // (comparison)
+    /** Greater than 		*/	GREATER_THAN_OP,        // (comparison)
+	//@}
+
+	/** @name Non-strict comparison operators */
+	//@{
+    /** Meta-equal (same as IS)*/ META_EQUAL_OP,        // (comparison)
+	/** Is 					*/ 	IS_OP= META_EQUAL_OP,	// (comparison)
+    /** Meta-not-equal (same as ISNT) */META_NOT_EQUAL_OP,//(comparison)
+	/** Isnt 				*/ 	ISNT_OP=META_NOT_EQUAL_OP,//(comparison)
+	//@}
+    __COMPARISON_END__     	= ISNT_OP,
 
     __ARITHMETIC_START__,     
-    UNARY_PLUS_OP           = __ARITHMETIC_START__,
-    UNARY_MINUS_OP,         // (arithmetic)
-    ADDITION_OP,            // (arithmetic)
-    SUBTRACTION_OP,         // (arithmetic)
-    MULTIPLICATION_OP,      // (arithmetic)
-    DIVISION_OP,            // (arithmetic)
-    MODULUS_OP,             // (arithmetic)
+	/** @name Arithmetic operators */
+	//@{
+    /** Unary plus 			*/	UNARY_PLUS_OP           = __ARITHMETIC_START__,
+    /** Unary minus 		*/	UNARY_MINUS_OP,         // (arithmetic)
+    /** Addition 			*/	ADDITION_OP,            // (arithmetic)
+    /** Subtraction 		*/	SUBTRACTION_OP,         // (arithmetic)
+    /** Multiplication 		*/	MULTIPLICATION_OP,      // (arithmetic)
+    /** Division 			*/	DIVISION_OP,            // (arithmetic)
+    /** Modulus 			*/	MODULUS_OP,             // (arithmetic)
+	//@}
     __ARITHMETIC_END__      = MODULUS_OP,
     
     __LOGIC_START__,
-    LOGICAL_NOT_OP          = __LOGIC_START__,
-    LOGICAL_OR_OP,          // (logical)
-    LOGICAL_AND_OP,         // (logical)
+	/** @name Logical operators */
+	//@{
+    /** Logical not 		*/	LOGICAL_NOT_OP          = __LOGIC_START__,
+    /** Logical or 			*/	LOGICAL_OR_OP,          // (logical)
+    /** Logical and 		*/	LOGICAL_AND_OP,         // (logical)
+	//@}
     __LOGIC_END__           = LOGICAL_AND_OP,
 
     __BITWISE_START__,
-    BITWISE_NOT_OP          = __BITWISE_START__,
-    BITWISE_OR_OP,          // (bitwise)
-    BITWISE_XOR_OP,         // (bitwise)
-    BITWISE_AND_OP,         // (bitwise)
-    LEFT_SHIFT_OP,          // (bitwise)
-    RIGHT_SHIFT_OP, 		// (bitwise) -- signed shift
-    URIGHT_SHIFT_OP,   		// (bitwise) -- unsigned shift
+	/** @name Bitwise operators */
+	//@{
+    /** Bitwise not 		*/	BITWISE_NOT_OP          = __BITWISE_START__,
+    /** Bitwise or 			*/	BITWISE_OR_OP,          // (bitwise)
+    /** Bitwise xor 		*/	BITWISE_XOR_OP,         // (bitwise)
+    /** Bitwise and 		*/	BITWISE_AND_OP,         // (bitwise)
+    /** Left shift 			*/	LEFT_SHIFT_OP,          // (bitwise)
+    /** Right shift 		*/	RIGHT_SHIFT_OP, 		// (bitwise)
+    /** Unsigned right shift */	URIGHT_SHIFT_OP,   		// (bitwise)
+	//@}
     __BITWISE_END__         = URIGHT_SHIFT_OP, 
 
     __MISC_START__,
-    PARENTHESES_OP          = __MISC_START__,
-    SUBSCRIPT_OP,           // (misc) -- subscripting elements from lists
-    TERNARY_OP,             // (misc)
+	/** @name Miscellaneous operators */
+	//@{
+    /** Parentheses 		*/	PARENTHESES_OP          = __MISC_START__,
+    /** Subscript 			*/	SUBSCRIPT_OP,           // (misc)
+    /** Conditional op 		*/	TERNARY_OP,             // (misc)
+	//@}
     __MISC_END__            = TERNARY_OP,
 
     __LAST_OP__             = __MISC_END__
