fa1b27bc4cc73367a2646562a7cc8905ee766357
raman <raman>
1998-10-23 16:29:54 +0000

    Added license and copyright headers

--- src/condor_classad.V6/lexer.h
+++ src/condor_classad.V6/lexer.h
@@ -1,3 +1,26 @@
+/***************************Copyright-DO-NOT-REMOVE-THIS-LINE**
+ * CONDOR Copyright Notice
+ *
+ * See LICENSE.TXT for additional notices and disclaimers.
+ *
+ * Copyright (c)1990-1998 CONDOR Team, Computer Sciences Department, 
+ * University of Wisconsin-Madison, Madison, WI.  All Rights Reserved.  
+ * No use of the CONDOR Software Program Source Code is authorized 
+ * without the express consent of the CONDOR Team.  For more information 
+ * contact: CONDOR Team, Attention: Professor Miron Livny, 
+ * 7367 Computer Sciences, 1210 W. Dayton St., Madison, WI 53706-1685, 
+ * (608) 262-0856 or miron@cs.wisc.edu.
+ *
+ * U.S. Government Rights Restrictions: Use, duplication, or disclosure 
+ * by the U.S. Government is subject to restrictions as set forth in 
+ * subparagraph (c)(1)(ii) of The Rights in Technical Data and Computer 
+ * Software clause at DFARS 252.227-7013 or subparagraphs (c)(1) and 
+ * (2) of Commercial Computer Software-Restricted Rights at 48 CFR 
+ * 52.227-19, as applicable, CONDOR Team, Attention: Professor Miron 
+ * Livny, 7367 Computer Sciences, 1210 W. Dayton St., Madison, 
+ * WI 53706-1685, (608) 262-0856 or miron@cs.wisc.edu.
+****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
+
 #ifndef __LEXER_H__
 #define __LEXER_H__
 
@@ -5,6 +28,8 @@
 #include "stringSpace.h"
 #include "common.h"
 #include "tokens.h"
+
+const int MAX_TOKEN_SIZE = 4096;
 
 struct IntValue {
     int             value;
@@ -21,7 +46,7 @@ union TokenValue
     IntValue    intValue;
     RealValue   realValue;
 	bool		boolValue;
-    char*       strValue;
+    char       	strValue[MAX_TOKEN_SIZE];
 };
 
 
@@ -47,7 +72,7 @@ class Lexer
 		TokenType consumeToken (TokenValue * = NULL);
 
 		// internal buffer for string, CEDAR and file token accumulation
-		enum 	{ LEX_BUFFER_SIZE = 4096 };		// starting size
+		enum 	{ LEX_BUFFER_SIZE = MAX_TOKEN_SIZE };// starting size
 		char   	lexBuffer[LEX_BUFFER_SIZE];		// the buffer itself
 
 		// miscellaneous functions
