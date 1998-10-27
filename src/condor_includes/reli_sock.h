0edf45c7482cff027e579d708e559517ce3e067a
mikeu <mikeu>
1998-10-27 21:37:23 +0000

    Changed dummy authentication routines to return false vice true to err on the side of caution.

--- src/condor_includes/reli_sock.h
+++ src/condor_includes/reli_sock.h
@@ -73,8 +73,9 @@ public:
 
 	//authenticate[_user] are dummy functions to allow upward compatibility
 	//with AuthSock where globus is not implemented for a platform
-	int authenticate() { return 1; }
-	int authenticate_user() { return 1; }
+	int authenticate() { return 0; }
+	int authenticate_user() { return -1; }
+	int isAuthenticated() { return FALSE; }
 
 	// Send & Recv Files: Takes buffer pointers and lengths as arguments, 
 	// sends the data in a bulk optimized method. Returns -1 on failure,
