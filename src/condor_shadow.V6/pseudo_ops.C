c21a28ac3d7c31d4fb2ff08fd355858f638a1dba
wright <wright>
1998-10-27 20:41:42 +0000

    pseudo getwd() now returns the CurrentWorkingDir instead of explicity calling getwd() -- this is a bug fix for folks using NFS automounters which need Condor to rely on the InitialDir they supply.  It also properly returns a 1 indicating success.  These fixes are already on the development trunk, but they need to be in the stable branch, too.

--- src/condor_shadow.V6/pseudo_ops.C
+++ src/condor_shadow.V6/pseudo_ops.C
@@ -245,7 +245,8 @@ pseudo_perm_error( const char *msg )
 int
 pseudo_getwd( char *path )
 {
-	return (int)getwd(path);
+        strncpy(path,CurrentWorkingDir,_POSIX_PATH_MAX);
+        return 1;
 }
 
 int
