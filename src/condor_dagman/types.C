4a7481c4cacd5829056d399166ca507e9245b50e
colbster <colbster>
1998-10-28 20:53:05 +0000

    Specify types used throughout DagMan code.

---
+++ src/condor_dagman/types.C
@@ -0,0 +1,15 @@
+#include "types.h"
+
+//---------------------------------------------------------------------------
+template<class TYPE> int compare(TYPE a, TYPE b) {
+  if (a == b) return 0;
+  return (a > b ? 1 : -1);
+}
+
+//---------------------------------------------------------------------------
+int CondorID_t::Compare (const CondorID_t condorID) const {
+  int result = compare (_cluster, condorID._cluster);
+  if (result == 0) result = compare (_proc, condorID._proc);
+  if (result == 0) result = compare (_subproc, condorID._subproc);
+  return result;
+}
---
+++ src/condor_dagman/types.h
@@ -0,0 +1,63 @@
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+#include <stdio.h>
+
+//---------------------------------------------------------------------------
+#ifndef __cplusplus
+typedef int bool;
+#define true  1
+#define false 0
+#endif
+
+//---------------------------------------------------------------------------
+#define     OK   true
+#define NOT_OK   false
+
+///
+typedef int JobID_t;
+
+//---------------------------------------------------------------------------
+///
+template<class TYPE> int compare (TYPE a, TYPE b);
+
+//---------------------------------------------------------------------------
+/** Condor uses three integers to identify jobs. This structure 
+    will be used to store those three numbers.  
+*/
+struct CondorID_t {
+  ///
+  CondorID_t () : _cluster(-1), _proc(-1), _subproc(-1) {}
+
+  ///
+  inline void Set (int cluster, int proc, int subproc) {
+    _cluster = cluster;
+    _proc    = proc;
+    _subproc = subproc;
+  }
+
+  ///
+  CondorID_t (int cluster, int proc, int subproc):
+    _cluster(cluster), _proc(proc), _subproc(subproc) {}
+
+  /** Compare this condorID's with another
+      @return zero if they match
+  */
+  int Compare (const CondorID_t condorID) const;
+
+  ///
+  inline bool operator == (const CondorID_t condorID) const {
+    return Compare (condorID) == 0;
+  }
+
+  ///
+  inline void Print (FILE * file = stdout) const {
+    fprintf (file, "(%d.%d.%d)", _cluster, _proc, _subproc);
+  }
+
+  /** */ int _cluster;
+  /** */ int _proc;
+  /** */ int _subproc;
+};
+
+#endif /* #ifndef _TYPES_H_ */
