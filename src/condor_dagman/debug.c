3637ec7eb01561129d8c100af3920541aedfdca8
colbster <colbster>
1998-10-28 20:52:14 +0000

    This debugging module provides a simple mechanism of conditionally printing a message to the screen depending on the output level specified by the user at run time.
    For example, if the debug level is set to 3 (VERBOSE), then messages above 3
    will not print.  So a level 4 (DEBUG) message won't print, because the user
    specified only messages up to 3 to print.  A level 3 (VERBOSE) or 2 (NORMAL)
    would print, being less than or equal to 3.
    
    This module allows the output level of the program to dynamically change without the need to recompile.  The small expense is that all debug messages are compiled in whether or not they are used during a given run.  I believe this is a small price to pay, for the convenience of dynamic adjustment of output level.

---
+++ src/condor_dagman/debug.c
@@ -0,0 +1,58 @@
+#include "debug.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+debug_level_t debug_level    = DEBUG_NORMAL;
+char *        debug_progname = NULL;
+
+/*--------------------------------------------------------------------------*/
+int debug_printf (debug_level_t level, const char *fmt, ...) {
+  int ret = 0;
+  if (DEBUG_LEVEL(level)) {
+    va_list args;
+    va_start (args, fmt);
+    ret = vprintf (fmt, args);
+    va_end (args);
+    if (DEBUG_LEVEL(DEBUG_DEBUG_1)) debug_fflush();
+  }
+  return ret;
+}
+
+/*--------------------------------------------------------------------------*/
+void debug_println (debug_level_t level, const char *fmt, ...) {
+  if (DEBUG_LEVEL(level)) {
+    printf ("%s: ", debug_progname);
+    {
+      va_list args;
+      va_start (args, fmt);
+      vprintf (fmt, args);
+      va_end (args);
+    }
+    printf ("\n");
+    if (DEBUG_LEVEL(DEBUG_DEBUG_1)) debug_fflush();
+  }
+}
+
+/*--------------------------------------------------------------------------*/
+void debug_error (int error, debug_level_t level, const char *fmt, ...) {
+  if (DEBUG_LEVEL(level)) {
+    printf ("%s: ", debug_progname);
+    {
+      va_list args;
+      va_start (args, fmt);
+      vprintf (fmt, args);
+      va_end (args);
+    }
+    printf ("\n");
+    if (DEBUG_LEVEL(DEBUG_DEBUG_1)) debug_fflush();
+  }
+  exit (error);
+}
+
+/*--------------------------------------------------------------------------*/
+void debug_perror (int error, debug_level_t level, char *s) {
+  if (DEBUG_LEVEL(level)) {
+    perror (s);
+    exit(error);
+  }
+}
---
+++ src/condor_dagman/debug.h
@@ -0,0 +1,142 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include "condor_header_features.h"
+
+BEGIN_C_DECLS   /* from condor_header_features.h */
+
+/** @name debug.h
+    
+    This debug module offers the user (or programmer) a way to dynamically
+    to debug messages on and off without having to recompile.
+    
+    My motivation for writing this stemmed from my annoyance of
+    #if defined(DEBUG) lines littering source code, making it quite
+    unreadable.  Beyond that, having to constantly change a Makefile and
+    recompile just to add or subtract debug message was a real hassle.
+    
+    This module gives the programmer a way to offer a dynamic mechanism
+    for printing debug messages.  Not only that, it offers a verbosity scale,
+    so to speak.  Message can be assigned a severity, and will only be
+    printed if it is as or more severe than the desired output level.
+    
+    Some pitfalls to avoid:  Because all messages are dynamic, they
+    are compiled in with your code, whether of not you see them.  So
+    even if you never run the program at level 5, message at level 5
+    and above will make your executable larger, even though they never print.
+    
+    As with assertions or any conditionally compiled/executed code, you
+    should never include side effects in your debug messages, since you
+    don't really know if they will execute.  Since my functions are
+    not conditional macros, side effects will probably be OK, but that
+    doesn't mean you should do it.
+    
+    debug_println (DEBUG_DEBUG, "Error reading element %d", x++);
+    
+    is an example of unwise programming.
+    
+    This implementation only address output to stdio.  I yet thought about
+    how to design it to work with stderr.  Should there be a debug_level
+    for each output stream?  I don't want it to get too complicated.
+    
+    @author Colby O'Donnell <colbster@cs.wisc.edu>
+*/
+//@{
+  
+/** The fundamental data type for the debug module.
+    A debug level is meant to express the verbosity of any print
+    operation to the screen.  The programmer is welcome to use
+    values greater than 4, as I could see a real benefit to having
+    multiple levels of debug.  The programmer could assign level 5
+    to printing only in outer loops, and level 6 to inner loops.
+*/
+enum debug_level {
+  /** NEVER output, except for usage info */ DEBUG_SILENT  = 0,
+  /** Very quiet, only servere errors */     DEBUG_QUIET   = 1,
+  /** Normal output, errors and warnings */  DEBUG_NORMAL  = 2,
+  /** Errors, and all warnings */            DEBUG_VERBOSE = 3,
+  /** Basic debug output */                  DEBUG_DEBUG_1 = 4,
+  /** Outer Loop Debug */                    DEBUG_DEBUG_2 = 5,
+  /** Inner Loop Debug */                    DEBUG_DEBUG_3 = 6,
+  /** Rarely Used Debug */                   DEBUG_DEBUG_4 = 7
+};
+
+///
+typedef enum debug_level debug_level_t;
+
+/** The current threshhold of debug output.  You output is only printed
+    if the current debug_level is equal or greater than the level of your
+    message.
+*/
+extern debug_level_t debug_level;
+
+/** The name of the current executable.  basename (argv[0]) from main().
+ */
+extern char *        debug_progname;
+
+/** Debug wrapper for the printf() function.  Your message will be printed
+    only if the debug_level of the program is equal or greater than
+    the level of your message.  For example, if you call:
+
+      debug_printf (DEBUG_VERBOSE, "Warning, this might cause a problem");
+
+    You are requesting this message to be printed if the current debug level
+    is at least DEBUG_VERBOSE.  If the debug_level was DEBUG_NORMAL or
+    below, you would not see the message printed.
+
+    @param level This message is intended for output at debug 'level' or above
+    @param fmt Refer to printf() documentation
+    @param ... Refer to printf() documentation
+    @return Refer to printf() documentation
+*/
+int  debug_printf  (debug_level_t level, const char *fmt, ...);
+
+/** The conditional expression that controls whether output is
+    actually printed.
+
+    Usage: if (DEBUG_LEVEL(DEBUG_VERBOSE)) function_call();
+ */
+#define DEBUG_LEVEL(level) (debug_level >= (level))
+
+/** Short for "Print Line".  Same as debug_printf(), except the progname and
+    a '\n' are automatically printed.
+
+    @param level This message is intended for output at debug 'level' or above
+    @param fmt Refer to printf() documentation
+    @param ... Refer to printf() documentation
+    @return nothing
+*/
+void debug_println (debug_level_t level, const char *fmt, ...);
+
+/** Print an error an exit.  Same as debug_println(), except the progname
+    will immediately called exit(error) and never return.
+
+    @param error After printing the message, call exit(error)
+    @param level This message is intended for output at debug 'level' or above
+    @param fmt Refer to printf() documentation
+    @param ... Refer to printf() documentation
+    @return This function never returns, because it calls exit(error)
+*/
+void debug_error   (int error, debug_level_t level, const char *fmt, ...);
+
+/** A simple wrapper to perror().  perror() is only called if level is
+    high enough.  Essential code is:
+
+      perror (s);
+      exit(error);
+
+    @param error The integer to exit with
+    @param level This message is intended for output at debug 'level' or above
+    @param s     String to pass to perror(), usually argv[0] or a filename
+*/
+void debug_perror (int error, debug_level_t level, char *s);
+
+///
+#define debug_fflush() fflush(stdout)
+
+//@}
+
+END_C_DECLS /* from condor_header_features.h */
+
+#endif /* ifndef DEBUG_H */
+
