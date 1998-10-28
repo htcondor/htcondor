71c7d5201e57d698e56edc92f2f09667cfb13b88
colbster <colbster>
1998-10-28 20:54:53 +0000

    Removed several unused functions.  Rewrote getline function (same interface).

--- src/condor_dagman/util.C
+++ src/condor_dagman/util.C
@@ -1,133 +1,116 @@
-
 #include "util.h"
-#include "DagMan.h"
-#include <ctype.h>
-#include <strstream.h>
+#include <ctype.h>    /* for isspace() */
+#include <assert.h>
 
-char *IntToString(int i)
-{
-    ostrstream os;
-    os << i << '\0';
-    return os.str();    
+//------------------------------------------------------------------------
+int getline(FILE *fp, char *line, int max) {
+  int c, i = 0;
+    
+  assert (EOF  == -1);
+  assert (fp   != NULL);
+  assert (line != NULL);
+  assert (max  > 0);      /* Need at least 1 slot for '\0' character */
+
+  for (c = getc(fp) ; c != '\n' && c != EOF ; c = getc(fp)) {
+    if ( !(i == 0 && isspace(c)) && (i+1 < max)) line[i++] = c;
+  }
+  line[i] = '\0';
+  return (i==0 && c==EOF) ? EOF : i;
 }
 
-int
-StringHash(char *str, int numBuckets)
-{
-    //
-    // Stole this hash function from DEVise
-    //
-    int remainder = 0;
-    char *s = str;
-    while (*s != '\0')
-    {
-	remainder = (remainder * MAX_CHARCODE + *s) % numBuckets;
-	s++;
-    }
-    return remainder;
+#if 0
+const int MAX_CHARCODE = 127;
+
+//---------------------------------------------------------------------------
+char *IntToString(int i) {
+  ostrstream os;
+
+  os << i << '\0';
+  return os.str();    
 }
 
-int
-StringCompare(char *s1, char *s2)
-{
-    return strcmp(s1, s2);
+//---------------------------------------------------------------------------
+int StringHash(char *str, int numBuckets) {
+  //
+  // Stole this hash function from DEVise
+  //
+  int remainder = 0;
+  char *s = str;
+  while (*s != '\0') {
+    remainder = (remainder * MAX_CHARCODE + *s) % numBuckets;
+    s++;
+  }
+  return remainder;
 }
 
-void
-StringDup(char *&dst, char *src)
-{
-    dst = new char [strlen(src) + 1];
-    strcpy(dst, src);
+//---------------------------------------------------------------------------
+int StringCompare(char *s1, char *s2) {
+  return strcmp(s1, s2);
 }
 
-int
-IntCompare(int a, int b)
-{
-    if (a < b)
-	return -1;
-    if (a > b)
-	return 1;
-    return 0;
+//---------------------------------------------------------------------------
+void StringDup(char *&dst, char *src) {
+  dst = new char [strlen(src) + 1];
+  strcpy(dst, src);
 }
 
-int
-BinaryHash(void *buffer, int nbytes, int numBuckets)
-{
-    int remainder = 0;
-    unsigned char *s = (unsigned char *) buffer;
-    int i;
-    for (i = 0; i < nbytes; i++)
-    {
-	remainder = (remainder * MAX_CHARCODE + s[i]) % numBuckets;
-    }
-    return remainder;
+//---------------------------------------------------------------------------
+int BinaryHash(void *buffer, int nbytes, int numBuckets) {
+  int remainder = 0;
+  unsigned char *s = (unsigned char *) buffer;
+  int i;
+  for (i = 0; i < nbytes; i++) {
+    remainder = (remainder * MAX_CHARCODE + s[i]) % numBuckets;
+  }
+  return remainder;
 }
 
 /* Read a \n-terminated line from fp into line, replacing \n by \0. Do
    not read more than maxc-1 characters. Do not write leading
    whitespace into the line buffer. Return the number of characters
    read (excluding \n). Returns -1 on EOF. */
-int
-getline(FILE *fp, char *line, int max)
-{
-    int i;
-    int lim;
-    int c;
-    bool done;
+//---------------------------------------------------------------------------
+int getline (FILE *fp, char *line, int max) {
+  int i;
+  int lim;
+  int c;
+  bool done;
     
-    lim = max - 1;
-    i = 0;
-    done = false;
+  lim = max - 1;
+  i = 0;
+  done = false;
     
-    //
-    // Discard all leading whitespace
-    //
-    while ((i < lim) && ((c = getc(fp)) != EOF))
-    {
-	if (c == '\n')
-	{
-	    done = true;
-	    break;
-	}
-	else if (isspace(c))
-	{
-	    continue;
-	}
-	line[i++] = c;
-	break;
-    }
+  //
+  // Discard all leading whitespace
+  //
+  while ((i < lim) && ((c = getc(fp)) != EOF)) {
+    if (c == '\n') {
+      done = true;
+      break;
+    } else if (isspace(c)) continue;
+    line[i++] = c;
+    break;
+  }
     
-    if (i == 0 && feof(fp))
-    {
-	return -1;
-    }
-    if (done)
-    {
-	line[i] = '\0';
-	return i;
-    }
-    
-    //
-    // Read to the end of the line
-    //
-    while ((i < lim) && ((c = getc(fp)) != EOF))
-    {
-	if (c == '\n')
-	{
-	    break;
-	}
-	line[i++] = c;
-    }
-    
-    if (i == 0 && feof(fp))
-    {
-	return -1;
-    }
+  if (i == 0 && feof(fp)) return -1;
 
+  if (done) {
     line[i] = '\0';
     return i;
+  }
+    
+  //
+  // Read to the end of the line
+  //
+  while ((i < lim) && ((c = getc(fp)) != EOF)) {
+    if (c == '\n') break;
+    line[i++] = c;
+  }
+    
+  if (i == 0 && feof(fp)) return -1;
+  line[i] = '\0';
+  return i;
     
 }
-
-
+#endif
 
--- src/condor_dagman/util.h
+++ src/condor_dagman/util.h
@@ -3,32 +3,22 @@
 
 #include <stdio.h>
 
-///
-const int MAX_CHARCODE = 127;
-
-/** @name Utility functions for hash tables */
-//@{
-///
+#if 0   /* These function are never used! */
 int StringHash(char *str, int numBuckets);
-///
 int StringCompare(char *s1, char *s2);
-///
-int IntCompare(int a, int b);
-///
 int BinaryHash(void *buffer, int nbytes, int numBuckets);
-///
 void StringDup(char *&dst, char *src);
-//@}
-
 /// Create a new string containing the ascii representation of an integer
-char *IntToString(int i);
+char *IntToString (int i);
+#endif
+
+const int UTIL_MAX_LINE_LENGTH = 1024;
 
 /** Read a \n-terminated line from fp into line, replacing \n by \0. Do
     not read more than maxc-1 characters. Do not write leading
     whitespace into the line buffer. Return the number of characters
     read (excluding \n). Returns -1 on EOF.
 */ 
-int getline(FILE *fp, char *line, int max);
+extern "C" int getline (FILE *fp, char *line, int max);
 
 #endif /* #ifndef _UTIL_H_ */
-
