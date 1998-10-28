238a72b61fd1ec063965e1087905223622cbc47d
colbster <colbster>
1998-10-28 20:35:23 +0000

    CondorSubmit.{h,C} replaced by submit.{h,C}
    Simplified and debuged submit_submit() function.  Still uses
    popen("condor_submit") meathod to submit jobs.  Needs to be replaced by a
    condor_submit API.

---
+++ src/condor_dagman/submit.C
@@ -0,0 +1,108 @@
+#include "submit.h"
+#include "util.h"
+#include "debug.h"
+#include "types.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>  /*   for sleep()  */
+
+static bool submit_try (const char * command) {
+  
+  FILE * fp = popen(command, "r");
+  if (fp == NULL) {
+    debug_println (DEBUG_QUIET, "%s: popen() failed!", command);
+    return false;
+  }
+  
+  //----------------------------------------------------------------------
+  // Parse Condor's return message for Condor ID.  This desperately
+  // needs to be replaced by a Condor Submit API.
+  //
+  // Typical condor_submit output for Condor v6 looks like:
+  //
+  //   Submitting job(s).
+  //   Logging submit event(s).
+  //   1 job(s) submitted to cluster 2267.
+  //----------------------------------------------------------------------
+
+  char buffer[UTIL_MAX_LINE_LENGTH];
+  buffer[0] = '\0';
+  
+  // Look for the line containing the word "cluster".
+  // If we get an EOF, then something went wrong with condor_submit, so
+  // we return false.  The caller of this function can retry the submit
+  // by repeatedly calling this function.
+
+  do {
+    if (getline(fp, buffer, UTIL_MAX_LINE_LENGTH) == EOF) {
+      pclose(fp);
+      return false;
+    }
+  } while (strstr(buffer, "cluster") == NULL);
+  
+  {
+    int status = pclose(fp);
+    if (status == -1) {
+      perror (command);
+      return false;
+    }
+    
+    debug_println (DEBUG_DEBUG_1, "%s exited with status %d", command, status);
+  }
+
+  if (DEBUG_LEVEL(DEBUG_DEBUG_1)) {
+    CondorID_t condorID (0,0,0);
+    if (1 == sscanf(buffer, "1 job(s) submitted to cluster %d",
+                    & condorID._cluster)) {
+      printf ("%s assigned condorID ", command);
+      condorID.Print();
+      putchar('\n');
+    }
+  }
+
+  return true;
+}
+
+//-------------------------------------------------------------------------
+void submit_submit (const char * cmdFile) {
+  // the '-p' parameter to condor_submit will now cause condor_submit
+  // to pause ~4 seconds after a successfull submit.  this prevents
+  // the race condition of condor_submit finishing before dagman
+  // does a pclose, which at least on SGI IRIX causes a nasty 'Broken Pipe'
+  // [joev] - took this out on Todd's advice ....
+  
+  // char * exe = "condor_submit -p";
+  const char * exe = "condor_submit";
+  
+  // 'subproc' is not used in newer versions of condor Version 6 doesn't
+  // use 'proc' either.  We set this to zero for now
+  
+  char * command = new char[strlen(exe) + strlen(cmdFile) + 10];
+  if (command == NULL) {
+    debug_error (1, DEBUG_QUIET, "%s %s: Out of memory", exe, cmdFile);
+  }
+  sprintf(command, "%s %s 2>&1", exe, cmdFile);
+  
+  bool success = false;
+  const int tries = 6;
+  
+  for (int i = 1 ; i <= tries && !success ; i++) {
+    success = submit_try(command);
+    if (!success) {
+      const int wait = 10;
+      debug_println (DEBUG_NORMAL, "condor_submit try %d/%d failed, "
+                     "will try again in %d seconds", i, tries, wait);
+      sleep(wait);
+    }
+  }
+  if (!success) {
+    debug_println (DEBUG_QUIET, "condor_submit failed after %d tries.", tries);
+    debug_println (DEBUG_QUIET, "Submit command was: %s", command);
+    delete [] command;
+    debug_error   (1, DEBUG_QUIET, "Rerun %s after %s is fixed",
+                   debug_progname, exe);
+  }
+  delete [] command;
+}
+
---
+++ src/condor_dagman/submit.h
@@ -0,0 +1,22 @@
+#ifndef CONDOR_SUBMIT_H
+#define CONDOR_SUBMIT_H
+
+/** Submits a job to condor using popen().  This is a very primitive method
+    to submitting a job, and SHOULD be replacable by a Condor Submit API.
+
+    In the mean time, this function executes the condor_submit command
+    via popen() and parses the output, sniffing for the CondorID assigned
+    to the submitted job.
+
+    Parsing the condor_submit output successfully depends on the current
+    version of Condor, and how it's condor_submit outputs results on the
+    command line.
+    
+    @error contain a description of the error that occurred
+    @param condorID will hold the ID for the submitted job (if successful)
+    @return true on success, false on failure
+*/
+
+void submit_submit (const char * cmdFile);
+
+#endif /* #ifndef CONDOR_SUBMIT_H */
