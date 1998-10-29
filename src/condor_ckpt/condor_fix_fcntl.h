7c9fc226447fdb386c6ae2d24e330a27f1b74617
wright <wright>
1998-10-29 22:30:04 +0000

    Removed dead code that came back in the branch merge for some reason.

--- src/condor_ckpt/condor_fix_fcntl.h
+++
@@ -1,8 +0,0 @@
-#if defined(AIX32) && defined(__cplusplus )
-#	include "fcntl.aix32.h"
-#elif defined(AIX32) && !defined(__cplusplus )
-	typedef unsigned short ushort;
-#	include <fcntl.h>
-#else
-#	include <fcntl.h>
-#endif
