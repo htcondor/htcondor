75e3da1e117855bc190f4649e6b4dc3d4a2514d2
wright <wright>
1998-11-09 23:37:01 +0000

    We don't want to ever have SIGTRAP or SIGFPE blocked.  With SIGFPE we want to get the core file right away.  On many platforms, gdb uses SIGTRAP to implement breakpoints, so we want those right away, too.

--- src/condor_daemon_core.V6/daemon_core.C
+++ src/condor_daemon_core.V6/daemon_core.C
@@ -967,6 +967,8 @@ void DaemonCore::Driver()
 		sigdelset(&fullset, SIGABRT);    // so assert() failures drop core right away
 		sigdelset(&fullset, SIGILL);     // so we get a core right away
 		sigdelset(&fullset, SIGBUS);     // so we get a core right away
+		sigdelset(&fullset, SIGFPE);     // so we get a core right away
+		sigdelset(&fullset, SIGTRAP);    // so gdb works when it uses SIGTRAP
 	sigemptyset( &emptyset );
 	char asyncpipe_buf[10];
 #endif
