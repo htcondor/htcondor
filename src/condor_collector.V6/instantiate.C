d4ae0c7e2fc0ea913c86d3f9306d9b213b9117c8
wright <wright>
1998-12-01 23:08:20 +0000

    Since the c++_util_lib already instantiates a Set<MyString> (for ClassAd Collections), we don't have to.

--- src/condor_collector.V6/instantiate.C
+++ src/condor_collector.V6/instantiate.C
@@ -1,10 +1,7 @@
 #include "condor_common.h"
 #include "MyString.h"
-#include "Set.h"
 #include "HashTable.h"
 #include "view_server.h"
 
-template class Set<MyString>;
-template class SetElem<MyString>;
 template class HashTable<MyString, GeneralRecord*>;
 template class HashBucket<MyString, GeneralRecord*>;
