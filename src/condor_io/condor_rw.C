dd08974ab5e91d27df17851877818bd4c01fc997
tannenba <tannenba>
1998-12-21 19:15:42 +0000

    + use type SOCKET for socket descriptors instead of ints (this is safe on both Unix and NT).
    + updated copyright notice

--- src/condor_io/condor_rw.C
+++ src/condor_io/condor_rw.C
@@ -1,3 +1,26 @@
+/***************************Copyright-DO-NOT-REMOVE-THIS-LINE**
+ * CONDOR Copyright Notice
+ *
+ * See LICENSE.TXT for additional notices and disclaimers.
+ *
+ * Copyright (c)1990-1998 CONDOR Team, Computer Sciences Department, 
+ * University of Wisconsin-Madison, Madison, WI.  All Rights Reserved.  
+ * No use of the CONDOR Software Program Source Code is authorized 
+ * without the express consent of the CONDOR Team.  For more information 
+ * contact: CONDOR Team, Attention: Professor Miron Livny, 
+ * 7367 Computer Sciences, 1210 W. Dayton St., Madison, WI 53706-1685, 
+ * (608) 262-0856 or miron@cs.wisc.edu.
+ *
+ * U.S. Government Rights Restrictions: Use, duplication, or disclosure 
+ * by the U.S. Government is subject to restrictions as set forth in 
+ * subparagraph (c)(1)(ii) of The Rights in Technical Data and Computer 
+ * Software clause at DFARS 252.227-7013 or subparagraphs (c)(1) and 
+ * (2) of Commercial Computer Software-Restricted Rights at 48 CFR 
+ * 52.227-19, as applicable, CONDOR Team, Attention: Professor Miron 
+ * Livny, 7367 Computer Sciences, 1210 W. Dayton St., Madison, 
+ * WI 53706-1685, (608) 262-0856 or miron@cs.wisc.edu.
+****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
+
 #define _POSIX_SOURCE
 
 #include "condor_common.h"
@@ -5,14 +28,12 @@
 #include "condor_io.h"
 #include "condor_debug.h"
 
-static char _FileName_[] = __FILE__;
-
 
 /* Generic read/write wrappers for condor.  These function emulate the 
  * read/write system calls under unix except that they are portable, use
  * a timeout, and make sure that all data is read or written.
  */
-int condor_read(int fd, char *buf, int sz, int timeout)
+int condor_read(SOCKET fd, char *buf, int sz, int timeout)
 {
 	int nr = 0, nro;
 	unsigned int start_time, cur_time;
@@ -90,7 +111,7 @@ int condor_read(int fd, char *buf, int sz, int timeout)
 	return nr;
 }
 
-int condor_write(int fd, char *buf, int sz, int timeout) {
+int condor_write(SOCKET fd, char *buf, int sz, int timeout) {
 
 	int nw = 0, nwo = 0;
 	unsigned int start_time = 0, cur_time = 0;
@@ -163,7 +184,12 @@ int condor_write(int fd, char *buf, int sz, int timeout) {
 		if( nwo <= 0 ) {
 			dprintf( D_ALWAYS, 
 					"send returned %d, errno = %d. Assuming failure.\n",
-					nwo, errno);
+					nwo, 
+#ifdef WIN32
+					WSAGetLastError() );
+#else
+					errno );
+#endif
 			return -1;
 		}
 		
@@ -171,7 +197,7 @@ int condor_write(int fd, char *buf, int sz, int timeout) {
 	}
 
 	/* POST conditions. */
-	assert( nw == sz ); /* Make sure that we wrote everything */
+	ASSERT( nw == sz ); /* Make sure that we wrote everything */
 	return nw;
 }
 
