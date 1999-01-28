87c97173eaa3e333078ac1bd52cc3d4ad99e1a85
raman <raman>
1999-01-28 19:20:45 +0000

    + Moved iterator declaration to classadItor.h + Removed CopyMode enumeration

--- src/condor_classad.V6/classad.h
+++ src/condor_classad.V6/classad.h
@@ -315,6 +315,9 @@ class ClassAd : public ExprTree
 		*/
 		static ClassAd *AugmentFromSource( Source &s, ClassAd &c);
 
+        /** Makes a deep copy of the expression tree
+         *  @return A deep copy of the expression, or NULL on failure.
+         */
 		virtual ClassAd* Copy( );
 
   	private:
@@ -336,79 +339,6 @@ class ClassAd : public ExprTree
 		int				last;
 };
 	
-
-/** An object for iterating over the attributes of a ClassAd.  Several
-	iterators may be active over the same ClassAd at any time, and the same
-	iterator object may be used to iterate over other ClassAds as well.
-*/
-class ClassAdIterator
-{
-	public:
-		/// Constructor
-		ClassAdIterator () { ad = NULL ; index = -1; }
-
-		/** Constructor which initializes the iterator to the given ClassAd.
-			@param ca The ClassAd to iterate over.
-			@see initialize
-		*/
-		ClassAdIterator (const ClassAd &ca) { ad = &ca; index = -1; }	
-
-		/// Destructor
-		~ClassAdIterator(){ }
-
-        /** Initializes the object to iterate over a ClassAd; the iterator 
-				begins at the "before first" position.  This method must be 
-				called before the iterator is usable.  (The iteration methods 
-				return false if the iterator has not been initialized.)  This 
-				method may be called any number of times, with different 
-				ClassAds as arguments.
-            @param l The expression list to iterate over (i.e., the iteratee).
-        */
-		inline void Initialize (const ClassAd &ca) { ad = &ca ; index = -1; }
-
-        /// Positions the iterator to the "before first" position.
-		inline void ToBeforeFirst () { index = -1; }
-
-        /// Positions the iterator to the "after last" position
-		inline void ToAfterLast ()	{ index = ad->last; }
-
-		/** Gets the next attribute in the ClassAd.
-			@param attr The name of the next attribute in the ClassAd.
-			@param expr The expression of the next attribute in the ClassAd.
-            @return false if the iterator has crossed the last attribute in the 
-				ClassAd, or true otherwise.
-        */
-		bool NextAttribute( const char*& attr, ExprTree*& expr );
-
-		/** Gets the attribute currently referred to by the iterator.
-            @param attr The name of the next attribute in the ClassAd.
-            @param expr The expression of the next attribute in the ClassAd.
-            @return false if the operation failed, true otherwise.
-		*/
-		bool CurrentAttribute( const char*& attr, ExprTree*& expr );
-
-		/** Gets the previous attribute in the ClassAd.
-			@param attr The name of the previous attribute in the ClassAd.
-			@param expr The expression of the previous attribute in the ClassAd.
-            @return false if the iterator has crossed the first attribute in 
-				the ClassAd, or true otherwise.
-        */
-		bool PreviousAttribute( const char *&attr, ExprTree *&expr );
-
-        /** Predicate to check the position of the iterator.
-            @return true iff the iterator is before the first element.
-        */
-		inline bool IsBeforeFirst () { return (index == -1); }
-
-        /** Predicate to check the position of the iterator.
-            @return true iff the iterator is after the last element.
-        */
-		inline bool IsAfterLast () { return (ad ? (index==ad->last) : false); }
-
-	private:
-		int		index;
-		const ClassAd	*ad;
-};
-
+#include "classadItor.h"
 
 #endif//__CLASSAD_H__
