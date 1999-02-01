1faac3e7c5534e06add0ac250112643db44fa9a3
colbster <colbster>
1999-02-01 21:28:43 +0000

    Explicit template instantiations for DAGMan.

---
+++ src/condor_dagman/instantiate.C
@@ -0,0 +1,9 @@
+#include "condor_common.h"
+#include "dag.h"
+template class List<TQI>;
+template class Item<TQI>;
+template class ListIterator<TQI>;
+template class List<Job>;
+template class Item<Job>;
+template class ListIterator<Job>;
+template class SimpleListIterator<JobID_t>;
