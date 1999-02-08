964f923f034266fcf481c61bfa7d74ed99aa032c
wright <wright>
1999-02-08 21:53:20 +0000

    File for miscellaneous utility functions that don't fit in anywhwere else.  Started this file for num_string(), which takes in an int and returns a string version of it w/ "st", "nd", "rd" or "th" tacked on the end, as appropriate, in a static buffer (callers beware).

---
+++ src/condor_c++_util/misc_utils.h
@@ -0,0 +1,37 @@
+/***************************Copyright-DO-NOT-REMOVE-THIS-LINE**
+ * CONDOR Copyright Notice
+ *
+ * See LICENSE.TXT for additional notices and disclaimers.
+ *
+ * Copyright (c)1990-1998 CONDOR Team, Computer Sciences Department, 
+ * University of Wisconsin-Madison, Madison, WI.  All Rights Reserved.  
+ * No use of the CONDOR Software Program Source Code is authorized 
+ * without the express consent of the CONDOR Team.  For more information 
+ * contact: CONDOR Team, Attention: Professor Miron Livny, 
+ * 7367 Computer Sciences, 1210 W. Dayton St., Madison, WI 53706-1685, 
+ * (608) 262-0856 or miron@cs.wisc.edu.
+ *
+ * U.S. Government Rights Restrictions: Use, duplication, or disclosure 
+ * by the U.S. Government is subject to restrictions as set forth in 
+ * subparagraph (c)(1)(ii) of The Rights in Technical Data and Computer 
+ * Software clause at DFARS 252.227-7013 or subparagraphs (c)(1) and 
+ * (2) of Commercial Computer Software-Restricted Rights at 48 CFR 
+ * 52.227-19, as applicable, CONDOR Team, Attention: Professor Miron 
+ * Livny, 7367 Computer Sciences, 1210 W. Dayton St., Madison, 
+ * WI 53706-1685, (608) 262-0856 or miron@cs.wisc.edu.
+****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
+#ifndef _MISC_UTILS_H
+#define _MISC_UTILS_H
+
+#if defined( __cplusplus )
+extern "C" {
+#endif
+
+/* Converts an int to a string w/ "st", "nd", "rd, or "th" added on. */
+char*	num_string( int );  
+
+#if defined( __cplusplus )
+}
+#endif
+
+#endif /* _MISC_UTILS_H */
