2382ed565e9ad71c27aa251b93f996999e74f3f3
yoderme <yoderme>
1999-02-11 04:46:10 +0000

    This class represents one user job.  The vanilla/standard shadow uses one of these, the MPI shadow will use several.  This version franks on unix.  Untested.

---
+++ src/condor_shadow.V6.1/remoteresource.C
@@ -0,0 +1,303 @@
+/***************************Copyright-DO-NOT-REMOVE-THIS-LINE**
+ * CONDOR Copyright Notice
+ *
+ * See LICENSE.TXT for additional notices and disclaimers.
+ *
+ * Copyright (c)1990-1998 CONDOR Team, Computer Sciences Department, 
+ * University of Wisconsin-Madison, Madison, WI.  All Rights Reserved.  
+ * No use of the CONDOR Software Program Source Code is authorized 
+ * without the express consent of the CONDOR Team.  For more information 
+ * contact: CONDOR Team, Attention: Professor Miron Livny, 
+ * 7367 Computer Sciences, 1210 W. Dayton St., Madison, WI 53706-1685, 
+ * (608) 262-0856 or miron@cs.wisc.edu.
+ *
+ * U.S. Government Rights Restrictions: Use, duplication, or disclosure 
+ * by the U.S. Government is subject to restrictions as set forth in 
+ * subparagraph (c)(1)(ii) of The Rights in Technical Data and Computer 
+ * Software clause at DFARS 252.227-7013 or subparagraphs (c)(1) and 
+ * (2) of Commercial Computer Software-Restricted Rights at 48 CFR 
+ * 52.227-19, as applicable, CONDOR Team, Attention: Professor Miron 
+ * Livny, 7367 Computer Sciences, 1210 W. Dayton St., Madison, 
+ * WI 53706-1685, (608) 262-0856 or miron@cs.wisc.edu.
+****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
+
+/* This file is the implementation of the RemoteResource class. */
+
+#include "condor_common.h"
+#include "remoteresource.h"
+#include "exit.h"             // for JOB_BLAH_BLAH exit reasons
+#include "condor_debug.h"     // for D_debuglevel #defines
+
+
+static char *_FileName_ = __FILE__;     /* Used by EXCEPT (see except.h) */
+
+// for remote syscalls, this is currently in NTreceivers.C.
+extern int do_REMOTE_syscall();
+
+// for remote syscalls...
+ReliSock *syscall_sock;
+RemoteResource *thisRemoteResource;
+
+RemoteResource::RemoteResource( BaseShadow *shad ) {
+	shadow = shad;
+	executingHost = NULL;
+	machineName = NULL;
+	capability = NULL;
+	claimSock = new ReliSock();
+	exitReason = exitStatus = -1;
+}
+
+RemoteResource::RemoteResource( BaseShadow *shad, const char * eHost, 
+								const char * cbility ) {
+
+	executingHost = new char[strlen(eHost)+1];
+	capability = new char[strlen(cbility)+1];
+
+	strcpy( executingHost, eHost );
+	strcpy( capability, cbility );
+
+	shadow = shad;
+	machineName = NULL;
+	claimSock = new ReliSock();
+	exitReason = exitStatus = -1;
+}
+
+RemoteResource::~RemoteResource() {
+	if ( executingHost ) delete [] executingHost;
+	if ( machineName   ) delete [] machineName;
+	if ( capability    ) delete [] capability;
+	if ( claimSock     ) delete claimSock;
+}
+
+int RemoteResource::requestIt( ClassAd *jobAd, int starterVersion ) {
+
+	int			reply;
+	
+	if ( !executingHost || !capability ) {
+		shadow->dprintf ( D_ALWAYS, "executingHost or capability not defined"
+				  "in requestIt.\n" );
+		setExitReason(JOB_SHADOW_USAGE);  // no better exit reason available
+		return -1;
+	}
+
+	claimSock->close();	// make sure ClaimSock is a virgin socket
+	claimSock->timeout(SHADOW_SOCK_TIMEOUT);
+	if (!claimSock->connect(executingHost, 0)) {
+		dprintf(D_ALWAYS, "failed to connect to execute host %s\n", 
+				executingHost);
+		setExitReason(JOB_NOT_STARTED);
+		return -1;
+	}
+
+	claimSock->encode();
+	if (!claimSock->put(ACTIVATE_CLAIM)  ||
+		!claimSock->code(capability)     ||
+		!claimSock->code(starterVersion) ||
+		!jobAd->put(*claimSock)          ||
+		!claimSock->end_of_message())
+	{
+		dprintf(D_ALWAYS, "failed to send ACTIVATE_CLAIM request to %s\n",
+				executingHost);
+		setExitReason(JOB_NOT_STARTED);
+		return -1;
+	}
+
+	claimSock->decode();
+	if (!claimSock->code(reply) || !claimSock->end_of_message()) {
+		dprintf(D_ALWAYS, "failed to receive ACTIVATE_CLAIM reply from %s\n",
+				executingHost);
+		setExitReason(JOB_NOT_STARTED);
+		return -1;
+	}
+
+		//XXX todo: also grab machine name from remote host here.
+	setMachineName( "<machine-name-not-gotten>" );
+
+	if (reply != OK) {
+		dprintf(D_ALWAYS, "Request to run on %s was REFUSED.\n",
+				executingHost);
+		setExitReason(JOB_NOT_STARTED);
+		return -1;
+	}
+
+	dprintf(D_ALWAYS, "Request to run on %s was ACCEPTED.\n",executingHost);
+	return 0;
+}
+
+int RemoteResource::killStarter() {
+	ReliSock	sock;
+
+	if ( !executingHost ) {
+		dprintf ( D_ALWAYS, "In killStarter, executingHost not defined.\n");
+		return -1;
+	}
+
+		// for pretty debugging messages, use machineName if possible.
+	char *name;
+	if ( !machineName )
+		name = executingHost;
+	else
+		name = machineName;
+
+	sock.timeout(SHADOW_SOCK_TIMEOUT);
+	if (!sock.connect(executingHost, 0)) {
+		dprintf(D_ALWAYS, "failed to connect to executing host %s\n",
+				name );
+		return -1;
+	}
+
+	sock.encode();
+	if (!sock.put(KILL_FRGN_JOB) || 
+		!sock.put(capability)    ||
+		!sock.end_of_message())
+	{
+		dprintf(D_ALWAYS, "failed to send KILL_FRGN_JOB to startd %s\n",
+				name );
+		return -1;
+	}
+
+	return 0;
+}
+
+void RemoteResource::dprintfSelf( int debugLevel ) {
+	shadow->dprintf ( debugLevel, "RemoteResource::dprintSelf printing "
+					  "host info:\n");
+	shadow->dprintf ( debugLevel, "\texecutingHost: %s\n", executingHost);
+	shadow->dprintf ( debugLevel, "\tcapability:    %s\n", capability);
+	shadow->dprintf ( debugLevel, "\tmachineName:   %s\n", machineName);
+	shadow->dprintf ( debugLevel, "\texitReason:    %d\n", exitReason);
+}
+
+int RemoteResource::handleSysCalls( Stream *sock ) {
+
+		// XXX I *think* this is what I want to do...
+		// change value of the syscall_sock to correspond with that of
+		// this claim sock right before do_REMOTE_syscall().
+
+	syscall_sock = claimSock;
+	thisRemoteResource = this;
+
+	if (do_REMOTE_syscall() < 0) {
+		dprintf(D_SYSCALLS, "Shadow: do_REMOTE_syscall returned < 0\n");
+			// we call our shadow's shutdown method:
+		shadow->shutDown(exitReason, exitStatus);
+	}
+
+	return KEEP_STREAM;
+}
+
+void RemoteResource::getExecutingHost( char *& eHost ) {
+		// yes, this looks yucky, but I wanted to cover all the cases...
+
+	if (!eHost) {
+		int len;
+		if ( executingHost )
+			len = strlen(executingHost)+1;
+		else
+			len = 1;
+
+		eHost = new char[len];
+	}
+	
+	if ( executingHost )
+		strcpy ( eHost, executingHost );
+	else
+		eHost[0] = '\0';
+}
+
+void RemoteResource::getMachineName( char *& mName ) {
+		// yes, this looks yucky, but I wanted to cover all the cases...
+
+	if (!mName) {
+		int len;
+		if ( machineName )
+			len = strlen(machineName)+1;
+		else
+			len = 1;
+
+		mName = new char[len];
+	}
+	
+	if ( machineName )
+		strcpy ( mName, machineName );
+	else
+		mName[0] = '\0';
+}
+
+void RemoteResource::getCapability( char *& cbility ) {
+		// yes, this looks yucky, but I wanted to cover all the cases...
+
+	if (!cbility) {
+		int len;
+		if ( capability )
+			len = strlen(capability)+1;
+		else
+			len = 1;
+
+		cbility = new char[len];
+	}
+	
+	if ( capability )
+		strcpy ( cbility, capability );
+	else
+		cbility[0] = '\0';
+}
+
+ReliSock* RemoteResource::getClaimSock() {
+	return claimSock;
+}
+
+int RemoteResource::getExitReason() {
+	return exitReason;
+}
+
+int RemoteResource::getExitStatus() {
+	return exitStatus;
+}
+
+void RemoteResource::setExecutingHost( const char * eHost ) {
+	if ( !eHost )
+		return;
+
+	if ( executingHost )
+		delete [] executingHost;
+	
+	executingHost = new char[strlen(eHost)+1];
+	strcpy( executingHost, eHost );
+}
+
+void RemoteResource::setMachineName( const char * mName ) {
+	if ( !mName )
+		return;
+
+	if ( machineName )
+		delete [] machineName;
+	
+	machineName = new char[strlen(mName)+1];
+	strcpy( machineName, mName );
+}
+
+void RemoteResource::setCapability( const char * cbility ) {
+	if ( !cbility )
+		return;
+
+	if ( capability )
+		delete [] capability;
+	
+	capability = new char[strlen(cbility)+1];
+	strcpy( capability, cbility );
+}
+
+void RemoteResource::setClaimSock( ReliSock * cSock ) {
+	claimSock = cSock;
+}
+
+void RemoteResource::setExitReason( int reason ) {
+// old code was { if (ExitReason != JOB_KILLED) ExitReason = reason; }
+// from Todd.  Why?
+	exitReason = reason;
+}
+
+void RemoteResource::setExitStatus( int status ) {
+	exitReason = status;
+}
---
+++ src/condor_shadow.V6.1/remoteresource.h
@@ -0,0 +1,190 @@
+/***************************Copyright-DO-NOT-REMOVE-THIS-LINE**
+ * CONDOR Copyright Notice
+ *
+ * See LICENSE.TXT for additional notices and disclaimers.
+ *
+ * Copyright (c)1990-1998 CONDOR Team, Computer Sciences Department, 
+ * University of Wisconsin-Madison, Madison, WI.  All Rights Reserved.  
+ * No use of the CONDOR Software Program Source Code is authorized 
+ * without the express consent of the CONDOR Team.  For more information 
+ * contact: CONDOR Team, Attention: Professor Miron Livny, 
+ * 7367 Computer Sciences, 1210 W. Dayton St., Madison, WI 53706-1685, 
+ * (608) 262-0856 or miron@cs.wisc.edu.
+ *
+ * U.S. Government Rights Restrictions: Use, duplication, or disclosure 
+ * by the U.S. Government is subject to restrictions as set forth in 
+ * subparagraph (c)(1)(ii) of The Rights in Technical Data and Computer 
+ * Software clause at DFARS 252.227-7013 or subparagraphs (c)(1) and 
+ * (2) of Commercial Computer Software-Restricted Rights at 48 CFR 
+ * 52.227-19, as applicable, CONDOR Team, Attention: Professor Miron 
+ * Livny, 7367 Computer Sciences, 1210 W. Dayton St., Madison, 
+ * WI 53706-1685, (608) 262-0856 or miron@cs.wisc.edu.
+****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
+
+#ifndef REMOTERESOURCE_H
+#define REMOTERESOURCE_H
+
+#include "condor_common.h"
+#include "../condor_daemon_core.V6/condor_daemon_core.h"
+#include "condor_classad.h"
+#include "reli_sock.h"
+#include "baseshadow.h"
+
+/// This is the timeout period to hear from a startd.  (90 seconds).
+const int SHADOW_SOCK_TIMEOUT = 90;
+
+/** This class represents one remotely running user job.  <p>
+
+	This class has a Socket to the remote job from which it can handle
+	syscalls.  It can also talk to the startd and request to start a
+	starter, and it can kill the starter as well.<p>
+
+	For all functions that start with 'get' and are passed a char*, 
+	the following applies: if a NULL value is passed in as the argument, 
+	space is new'ed for you.  If the parameter is not NULL, it is 
+	assumed to point to valid memory, and the char* will be copied in.<p>
+
+	More to come...<p>
+
+	@author Mike Yoder
+*/
+class RemoteResource : public Service {
+
+ public: 
+		/** Constructor.  Pass it a pointer to the shadow which creates
+			it, this is handy while in handleSysCalls.  
+		*/
+	RemoteResource( BaseShadow *shadow );
+
+		/** Constructor with parameters.
+			@param shadow A pointer to the shadow which created this instance.
+			@param executingHost The sinful string for the remote startd.
+			@param capability The capability that the remote startd wants.
+		*/
+	RemoteResource( BaseShadow *shadow, const char * executingHost, 
+					const char * capability );
+
+		/// Destructor
+	~RemoteResource();
+
+		/** This function connects to the executing host and does
+			an ACTIVATE_CLAIM command on it.  The capability, starternum
+			and Job ClassAd are pushed, and the executing host's 
+			full machine name and (hopefully) an OK are returned.
+			@param jobAd The classAd for this job.
+			@param starterVersion The version number of the starter wanted.
+			                  The default is 2.
+			@return 0 if everthing went ok, -1 if error.				  
+		 */ 
+	int requestIt( ClassAd *jobAd, int starterVersion = 2 );
+
+		/** Here we tell the remote starter to kill itself in a gentle manner.
+			@return 0 on success, -1 if a problem occurred.
+		*/
+	int killStarter();
+
+		/** Print out this representation of the remote resource.
+			@param debugLevel The dprintf debug level you wish to use 
+		*/
+	void dprintfSelf( int debugLevel);
+
+		/** Each of these remote resources can handle syscalls.  The 
+			claimSock gets registered with daemonCore, and it will
+			call this function to handle it.
+			@param sock Stuff comes in here
+			@return KEEP_STREAM
+		*/
+	int handleSysCalls( Stream *sock );
+
+		/** Return the sinful string of the remote host.  If it is NULL, 
+			a "" will be returned.
+			@param executingHost Will contain the host's sinful string.
+		*/ 
+	void getExecutingHost( char *& executingHost );
+
+		/** Return the machine name of the remote host.  If it is NULL, 
+			a "" will be returned.
+			@param machineName Will contain the host's machine name.
+		*/ 
+	void getMachineName( char *& machineName );
+
+		/** Return the capability for talking to the host.  If is is
+			NULL, a "" will be returned.
+			@param capability Will contain the capability for the host.
+		*/ 
+	void getCapability( char *& capability );
+
+		/** Return the claim socket associated with this remote host.  
+			@return The claim socket for this host.
+		*/ 
+	ReliSock* getClaimSock();
+
+		/** Return the reason this host exited.
+			@return The exit reason for this host.
+		*/ 
+	int  getExitReason();
+
+		/** Return the status this host exited with.
+			@return The exit status for this host.
+		*/ 
+	int  getExitStatus();
+	
+		/** Set the sinful string for this host.
+			@param executingHost The sinful string for this host.
+		*/
+	void setExecutingHost( const char *executingHost );
+
+		/** Set the capability for this host.
+			@param cabability The capability of this host.
+		*/
+	void setCapability( const char *capability );
+
+		/** Set the reason this host exited.  
+			@param reason Why did it exit?  Film at 11.
+		*/
+	void setExitReason( int reason );
+
+		/** Set the status this host exited with.  
+			@param status Host exit status.
+		*/
+	void setExitStatus( int status );
+
+ private:
+
+		/** Set the machine name for this host.
+			@param machineName The machine name of this host.
+		*/
+	void setMachineName( const char *machineName );
+
+		/** Set the claimSock associated with this host.  
+			XXX is this needed at all?  Time will tell.
+			@param claimSock The socket used for communication with
+			                 this host.
+		*/
+	void setClaimSock( ReliSock* claimSock);
+
+		/** Making the copy constructor private prevents people
+			from copying it. */
+	RemoteResource( const RemoteResource& );
+
+		/** Making the assignment operator private also prevents copying.
+		 */
+	RemoteResource& operator = ( const RemoteResource& );
+
+		/* internal data: if you can't figure the following out.... */
+	char *executingHost;
+	char *machineName;
+	char *capability;
+	ReliSock *claimSock;
+	int exitReason;
+	int exitStatus;
+
+		// we keep a pointer to the shadow class around.  This is useful
+		// in handleSysCalls if we have to panic and do a shutdown.
+	BaseShadow *shadow;
+};
+
+
+#endif
+
+
