cbbb3caa58e61312558d11fd590e86ad2e08c0fb
wright <wright>
1999-03-11 17:21:32 +0000

    Undoing revision 1.3, since that's now on its own branch.

--- src/condor_includes/file_table_interf.h
+++ src/condor_includes/file_table_interf.h
@@ -20,53 +20,18 @@
  * Livny, 7367 Computer Sciences, 1210 W. Dayton St., Madison, 
  * WI 53706-1685, (608) 262-0856 or miron@cs.wisc.edu.
 ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
-
- 
-
 #ifndef _FILE_TABLE_INTERF_H
 #define _FILE_TABLE_INTERF_H
-
-/*
-This file describes the simple C interface to file table
-operations that the startup and checkpoint code needs
-access to.
-*/
 
 #if defined(__cplusplus)
 extern "C" {
 #endif
 
-/** Display known info about the file table */
 void DumpOpenFds();
-
-/** Set up the file table if necessary.  Calling this function
-multiple times is harmless.  All system calls that access FileTab
-should call this function first. */
-void InitFileState();
-
-/** Perform a periodic checkpoint of the file state. */
-void CheckpointFileState();
-
-/** Checkpoint the file state in preparation for a vacation. */
-void SuspendFileState();
-
-/** Restore the file state after a checkpoint. */
-void ResumeFileState();
-
-/** Map a virtual fd to the same real fd.  This function generally
-is only called but the startup code to get a temporary working
-stdin/stdout. */
-int pre_open( int fd, int readable, int writeable, int is_remote );
-
-/** This function still works, but it will eventually go away.
-(It's not up to the switches to decide where to map a file to.
-When mapping is in effect, talk to the open file table.) */
-int MapFd( int user_fd );
-
-/** This function still works, but it will eventually go away.
-See MapFd above. */
-int LocalAccess( int user_fd );
-
+void SaveFileState();
+void RestoreFileState();
+int MapFd( int );
+void Set_CWD( const char *working_dir );
 
 #if defined(__cplusplus)
 }
