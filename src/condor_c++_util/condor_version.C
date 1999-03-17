745212679a90b1eca50dae8ed4cf2eae1d6faa9c
wright <wright>
1999-03-17 05:04:56 +0000

    Initial version string for 6.1.5 (this isn't the final date).

--- src/condor_c++_util/condor_version.C
+++ src/condor_c++_util/condor_version.C
@@ -22,7 +22,7 @@
 ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
 
 /* Here is the version string - update before a public release */
-static char* CondorVersionString = "$Version: 6.1.4 1999/03/03 $";
+static char* CondorVersionString = "$Version: 6.1.5 1999/03/16 $";
 
 extern "C" {
 
