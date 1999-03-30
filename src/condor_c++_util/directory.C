7f8375ee9b0309d3f0ce0c35e5dfcdb199a8214e
tannenba <tannenba>
1999-03-30 21:46:27 +0000

    Massive re-write.
    + Added support for WinNT.
    
    + Added priv_state awareness; constructor can now take a priv_state.
    
    + Added methods Remove_Current_File(), Remove_Entire_Directory(),
    GetAccessTime(), GetModifyTime(), GetCreateTime(), IsDirectory().
    
    + If requested priv_state is PRIV_ROOT, we also try to access as PRIV_CONDOR
    so file access across NFS (in case spool, execute, etc, is on NFS) is happy.
    
    + Doc++ happy header file

--- src/condor_c++_util/directory.C
+++ src/condor_c++_util/directory.C
@@ -26,34 +26,224 @@
 #include "condor_constants.h"
 #include "condor_debug.h"
 #include "directory.h"
+#include "condor_string.h"
 
-static char *_FileName_ = __FILE__;     /* Used by EXCEPT (see except.h)     */
+// Set DEBUG_DIRECTORY_CLASS to 1 to not actually remove
+// files, but instead print out to the log file what would get
+// removed.  Set DEBUG_DIRECTORY_CLASS to 0 to actually do
+// the work.  Good luck!
+#define DEBUG_DIRECTORY_CLASS 0
+
+// Declare space for static data members
+#ifdef WIN32
+struct _finddata_t Directory::filedata;
+#endif
+
+//  --- A macro to reset our priv state and return
+#define return_and_resetpriv(i) \
+	if ( want_priv_change ) 		\
+		set_priv(saved_priv);		\
+	return i;
+// -----------------------------------------------
 
 
-Directory::Directory( const char *name )
+Directory::Directory( const char *name, priv_state priv ) 
 {
+	curr_filename = NULL;
+	dirp = -1;
+
+#ifndef WIN32
+	// Unix
 	dirp = opendir( name );
 	if( dirp == NULL ) {
 		EXCEPT( "Can't open directory \"%s\"", name );
+	}
+#endif
+	
+	curr_dir = strnewp(name);
+	ASSERT(curr_dir);
+
+	if ( priv == PRIV_UNKNOWN ) {
+		want_priv_change = false;
+	} else {
+		want_priv_change = true;
+		desired_priv_state = priv;
 	}
 }
 
 Directory::~Directory()
 {
+	delete [] curr_dir;
+
+#ifndef WIN32
+	// Unix
 	(void)closedir( dirp );
+#else
+	// Win32
+	if ( dirp ) {
+		(void)_findclose(dirp);
+	}
+#endif
 }
 
-void
+inline void
+Directory::Set_Access_Priv()
+{
+	if ( want_priv_change ) {
+		saved_priv = set_priv(desired_priv_state);
+	}
+}
+
+bool
+Directory::Remove_Entire_Directory()
+{
+	const char* thefile = NULL;
+	bool ret_value = true;
+
+	Set_Access_Priv();
+
+	Rewind();
+
+	while ( (thefile=Next()) ) {
+		if ( Remove_Current_File() == false ) {
+			ret_value = false;
+		}
+	}
+
+	return_and_resetpriv(ret_value);
+}
+
+
+bool 
+Directory::Remove_Current_File()
+{
+	char buf[_POSIX_PATH_MAX];
+	bool ret_val = true;
+	priv_state priv;
+
+	if ( curr_filename == NULL ) {
+		// there is not current file; user probably did not call
+		// Next() yet.
+		return false;
+	}
+
+	Set_Access_Priv();
+
+	if ( IsDirectory() ) {
+		// the current file is a directory.
+		// instead of messing with recursion, worrying about
+		// stack overflows, ACLs, etc, we'll just call upon
+		// the shell to do the dirty work.
+		// TODO: we should really look return val from system()!
+#ifdef WIN32
+		sprintf(buf,"rmdir /s /q %s\\%s",curr_dir,curr_filename);
+#else
+		sprintf(buf,"/bin/rm -rf %s/%s",curr_dir,curr_filename);
+#endif
+
+#if DEBUG_DIRECTORY_CLASS
+		dprintf(D_ALWAYS,"Directory: about to call %s\n",buf);
+#else
+		system( buf );
+#endif
+
+		// for good measure, repeat the above operation a second
+		// time as user Condor.  We do this because if we currently
+		// have root priv, and we are accessing files across NFS, we
+		// will get re-mapped to user "nobody" by any NFS daemon worth
+		// its salt.  
+		if ( want_priv_change && (desired_priv_state == PRIV_ROOT) ) {
+			priv = set_condor_priv(); 
+
+#if DEBUG_DIRECTORY_CLASS
+			dprintf(D_ALWAYS,
+				"Directory: with condor priv about to call %s\n",buf);
+#else
+			system( buf );
+#endif
+
+			set_priv(priv);
+		}
+	} else {
+		// the current file is not a directory, just a file	
+		sprintf(buf,"%s%c%s",curr_dir,DIR_DELIM_CHAR,curr_filename);
+#if DEBUG_DIRECTORY_CLASS
+		dprintf(D_ALWAYS,"Directory: about to unlink %s\n",buf);
+#else
+		errno = 0;
+		if ( unlink(buf) < 0 ) {
+			ret_val = false;
+			if( errno == EACCES ) {
+				// Try again as Condor, in case we are going
+				// across NFS since root access would get mapped
+				// to nobody.  If on NT, remove
+				// read-only access if exists.
+#ifdef WIN32
+				// Make file read/write on NT.
+				_chmod(buf,_S_IWRITE);
+#endif
+				if ( want_priv_change && (desired_priv_state == PRIV_ROOT) ) {
+					priv = set_condor_priv(); 
+				}
+				
+				if ( unlink( path ) < 0 ) {
+					ret_val = false;
+				} else {
+					ret_val = true;
+				}
+
+				if ( want_priv_change && (desired_priv_state == PRIV_ROOT) ) {
+					set_priv(priv);
+				}
+			}	// end of if errno = EACCESS
+		}	// end of if unlink() < 0
+
+#endif  // of if !DEBUG_DIRECTORY_CLASS
+	}		// end of if file is a regular file, not a directory
+
+	// if the file simple no longer exits, return success
+	// since the file is gone anyway.
+	if( ret_val == false &&  errno == ENOENT ) {
+		ret_val = true;
+	} 
+
+	return_and_resetpriv(ret_val);
+}
+
+bool
 Directory::Rewind()
 {
+	curr_filename = NULL;
+
+	Set_Access_Priv();
+
+#ifndef WIN32
+	// Unix
 	rewinddir( dirp );
+#else
+	// Win32
+	if ( dirp != -1 ) {
+		(void)_findclose(dirp);
+	}
+	dirp = -1;
+#endif
+
+	return_and_resetpriv(true);
 }
 
-char *
+const char *
 Directory::Next()
 {
-	struct dirent *dirent;
+	char buf[_POSIX_PATH_MAX];
 
+	Set_Access_Priv();
+	curr_filename = NULL;
+
+#ifndef WIN32
+	// Unix
+	int stat_status;
+	struct dirent *dirent;
+	struct stat statbuf;	
 	while( dirent = readdir(dirp) ) {
 		if( strcmp(".",dirent->d_name) == MATCH ) {
 			continue;
@@ -61,7 +251,93 @@ Directory::Next()
 		if( strcmp("..",dirent->d_name) == MATCH ) {
 			continue;
 		}
-		return dirent->d_name;
+		curr_filename =  dirent->d_name;
+		if ( curr_filename ) {
+			sprintf(buf,"%s/%s",curr_dir,curr_filename);
+			stat_status = do_stat(buf,&statbuf);
+			if ( stat_status == FALSE ) {
+				// do_stat thinks the file disappeared; it was 
+				// probably deleted. just go on to the next file.
+				continue;
+			}
+			if ( stat_status < 0 ) {
+				// do_stat failed with an error!
+				dprintf(D_ALWAYS,"Directory:: stat() failed for file %s",buf);
+				access_time = modify_time = create_time = 0;
+				curr_filename = NULL;
+			} else {
+				// do_stat succeeded
+				access_time = statbuf.st_atime;
+				create_time = statbuf.st_ctime;
+				modify_time = statbuf.st_mtime;
+				curr_isdirectory = S_ISDIR(statbuf.st_mode);
+			}
+		}
+		break;
 	}
-	return NULL;
+#else 
+	// Win32
+	int result;
+	sprintf(buf,"%s\\*.*",curr_dir);
+	// do findfirst/findnext until we find a file which
+	// is not "." or ".." or until there are no more files.
+	do {
+		if ( dirp == -1 ) {
+			dirp = _findfirst(buf,&filedata);
+			result = dirp;
+		} else {
+			result = _findnext(dirp,&filedata);
+		}
+	} while ( (result != -1) && 
+		( strcmp(filedata.name,".") == MATCH ||
+		  strcmp(filedata.name,"..") == MATCH ) );
+	if ( result != -1 ) {
+		// findfirst/findnext succeeded
+		curr_filename = filedata.name;
+		access_time = filedata.time_access;
+		create_time = filedata.time_create;
+		modify_time = filedata.time_write;
+		curr_isdirectory = (filedata.attrib & _A_SUBDIR) != 0;
+	} else {
+		access_time = modify_time = create_time = 0;
+		curr_filename = NULL;
+	}
+#endif
+
+	return_and_resetpriv( curr_filename );		
 }
+
+#ifndef WIN32
+/*
+  We want to stat the given file. We may be operating as root, but root
+  == nobody across most NFS file systems, so we may have to do it as
+  condor.  If we succeed, we return TRUE, and if the file has already
+  been removed we return FALSE.  If we cannot do it as either root
+  or condor, we return -1.
+*/
+int
+do_stat( const char *path, struct stat *buf )
+{
+	priv_state priv;
+
+	if( stat(path,buf) < 0 ) {
+		switch( errno ) {
+		  case ENOENT:	// got rm'd while we were doing this
+			return FALSE;
+		  case EACCES:	// permission denied, try as condor
+			priv = set_condor_priv();
+			if( stat(path,buf) < 0 ) {
+				set_priv(priv);
+				if ( errno == ENOENT ) {
+					return FALSE;	// got rm'd while we were doing this
+				}
+				dprintf(D_ALWAYS,"Directory::do_stat(%s,0x%x) failed",
+					path, &buf );
+				return -1;
+			}
+			set_priv(priv);
+		}
+	}
+	return TRUE;
+}
+#endif
--- src/condor_c++_util/directory.h
+++ src/condor_c++_util/directory.h
@@ -23,6 +23,8 @@
 #ifndef DIRECTORY_H
 #define DIRECTORY_H
 
+#include "condor_uid.h"
+
 #if defined (ULTRIX42) || defined(ULTRIX43)
 	/* _POSIX_SOURCE should have taken care of this,
 		but for some reason the ULTRIX version of dirent.h
@@ -31,7 +33,9 @@
 #	define __POSIX
 #endif
 
-#include <dirent.h>
+#ifndef WIN32
+#	include <dirent.h>
+#endif
 
 #if defined(SUNOS41) && defined(_POSIX_SOURCE)
 	/* Note that function seekdir() is not required by POSIX, but the sun
@@ -41,15 +45,101 @@
 	extern "C" void seekdir( DIR *dirp, long loc );
 #endif
 
+/** Class to iterate filenames in a subdirectory.  Given a subdirectory
+	path, this class can iterate the names of the files in the directory,
+	remove files, and/or
+	report file access/modify/create times.  Also reports if
+	the filename represents another subdirectory or not.  
+	<p><b>Note:</b> This class does not recurse down into subdirectories.
+*/
 class Directory
 {
 public:
-	Directory( const char *name );
+
+	/** Constructor.  Upon instantiation, the user 
+		should call Next() to fetch information on the first file.  
+		If a priv_state is specified, this priv_state will be used 
+		whenever accessing the filesystem.
+		@param dirpath The full path to the subdirectory to operate upon
+		@param priv The priv_state used when accessing the filesystem.
+		If set to PRIV_UNKNOWN, then the class will use 
+		whatever priv_state is currently in effect.  If set to
+		PRIV_ROOT, note all operations will be tried as both PRIV_ROOT
+		and as PRIV_CONDOR, just in case the files are being accessed
+		over NFS (where root gets mapped to nobody).
+		@see Next()
+		@see priv_state
+	*/
+	Directory( const char *dirpath, priv_state priv = PRIV_UNKNOWN);
+
+	/// Destructor<p>
 	~Directory();
-	void Rewind();
-	char *Next();
+
+	/** Fetch information on the next file in the subdirectory and
+		make it the 'current' file.
+		@return The filename of the next file, or NULL if there are 
+		no more files.  Do not free or delete
+		this memory; the class handles all memory management. */
+	const char* Next();
+
+	/** Restart the iteration.  After calling Rewind(), the next 
+		call to Next() will return the first file in the directory again.
+		@return Always returns true (for now)
+		@see Next()
+	*/
+	bool Rewind();
+
+	/** Get last access time of current file.
+		@return time in seconds since 00:00:00
+			UTC, January 1, 1970 */
+	time_t GetAccessTime() { return access_time; }
+
+	/** Get last modification time of current file.
+		@return time in seconds since 00:00:00
+			UTC, January 1, 1970 */
+	time_t GetModifyTime() { return modify_time; }
+
+	/** Get creation time of current file.
+		@return time in seconds since 00:00:00
+			UTC, January 1, 1970 */
+	time_t GetCreateTime() { return create_time; }
+
+	/** Determine if the current file is the name of a subdirectory,
+		or just a file.
+		@return true if current file is a subdirectory name, false if not
+	*/
+	bool IsDirectory() { return curr_isdirectory; }
+
+	/** Remove the current file.
+		@return true on successful removal, otherwise false
+	*/
+	bool Remove_Current_File();
+
+	/** Remove the all the files and subdirectories in the directory
+		specified by the constructor.  Upon success, the subdirectory
+		will still exist, but will be empty.
+		@return true on successful removal, otherwise false
+	*/
+	bool Remove_Entire_Directory();
+
 private:
+	char *curr_dir;
+	char *curr_filename;
+	bool curr_isdirectory;
+	time_t access_time;
+	time_t modify_time;
+	time_t create_time;
+	bool want_priv_change;
+	priv_state desired_priv_state;
+	priv_state saved_priv;
+	inline void Directory::Set_Access_Priv();
+#ifdef WIN32
+	long dirp;
+	static struct _finddata_t filedata;
+#else
 	DIR *dirp;
+	int do_stat( const char *path, struct stat *buf );
+#endif
 };
 
 #endif
