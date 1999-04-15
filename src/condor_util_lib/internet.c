7e2b9f9382db7843d7c7ff07b5637e13bbde6e01
wright <wright>
1999-04-15 20:38:32 +0000

    + Removed old, dead APIs that don't do the right thing:   get_inet_address(), get_machine_name(), evaluate_job_id(), and   append_job_id().  *_job_id we #if 0'd out, and get_* should be   replaced with calls to my_sin_addr() and my_full_hostname(). + Added #include "my_hostname.h" to avoid implicit declarations. + Removed internal call to get_inet_address() that wasn't needed.

--- src/condor_util_lib/internet.c
+++ src/condor_util_lib/internet.c
@@ -34,106 +34,7 @@
 #include "proc.h"
 #include "internet.h"
 #include "expr.h"
-
-/* 
-**  returns the internet address of this host 
-**  returns 1 on success and -1 on failure 
-*/
-int 
-get_inet_address(buffer)
-struct in_addr*	buffer;
-{
-	char			host[MAXHOSTLEN];
-	struct hostent	*hostptr;
-
-	if ( gethostname(host, MAXHOSTLEN) == -1 )
-	{
-		dprintf(D_FULLDEBUG,"Error in gethostname\n");
-		return -1;
-	}
-	if ( (hostptr=gethostbyname(host)) == NULL)
-	{	
-		dprintf(D_FULLDEBUG,"Error in gethostbyname\n");
-		return -1;
-	}
-	switch( hostptr->h_addrtype)
-	{
-		case AF_INET:  *buffer = *(struct in_addr *)(hostptr->h_addr_list[0]);
-					   return 1;
-		default     :  dprintf(D_FULLDEBUG,"Unknown type\n");
-					   return -1;
-	}
-}
-
-/* 
-** returns the full internet name of this host 
-** returns 1 on success and -1 on failure
-*/
-int 
-get_machine_name(buffer)
-char*	buffer;
-{
-	char			host[MAXHOSTLEN];
-	struct hostent	*hostptr;
-	if ( gethostname(host, MAXHOSTLEN) == -1 )
-	{
-		dprintf(D_FULLDEBUG, "Error in gethostname\n");
-		return -1;
-	}
-	if ( (hostptr=gethostbyname(host)) == NULL)
-	{	
-		dprintf(D_FULLDEBUG, "Error in gethostbyname\n");
-		return -1;
-	}
-	strcpy(buffer, hostptr->h_name);
-	return 1;
-}
-
-#if 0 /* don't use CONTEXTs in V6 */
-/*
-** extract the internet-wide unique job_id from the job-context
-*/
-int
-evaluate_job_id( context, job_id)
-JOB_ID*		job_id;
-CONTEXT*	context;
-{
-	char *tmp;
-
-	if ( evaluate_string("JOB_ID", &tmp, context, NULL) < 0 )
-	{
-		dprintf(D_ALWAYS, "Can't evaluate JOB_ID\n");
-		return -1;
-	}
-	if ( sscanf(tmp,"(%d, %d, %d)", &(job_id->inet_addr),&job_id->id.cluster,
-												   &job_id->id.proc) != 3 )
-			return -1;
-	return 1;
-}	
-
-/*
-** appends the internet wide unique job_id to the job-context
-*/
-int
-append_job_id( context, cluster, proc)
-CONTEXT*		context;
-int				cluster;
-int				proc;
-{
-	 char    line[2048];
-	 struct in_addr   buffer;
-
-	 memset(&buffer, '\0', sizeof(buffer));
-	 if ( get_inet_address(&buffer) == -1 )
-	 {
-		  dprintf(D_ALWAYS, "Error in getting internet address\n");
-		  return -1;
-	 }
-     sprintf(line, "JOB_ID = \"(%d, %d, %d)\"", buffer.s_addr, cluster, proc);
-	 add_stmt(scan(line), context);
-	 return 1;
-}
-#endif
+#include "my_hostname.h"
 
 
 /* Convert a string of the form "<xx.xx.xx.xx:pppp>" to a sockaddr_in  TCP */
@@ -229,9 +130,6 @@ sock_to_string(SOCKET sockd)
 	addr_len = sizeof(addr);
 
 	if (getsockname(sockd, (struct sockaddr *)&addr, &addr_len) < 0) 
-		return mynull;
-
-	if ( get_inet_address( &addr.sin_addr ) == -1 )
 		return mynull;
 
 	return ( sin_to_string( &addr ) );
