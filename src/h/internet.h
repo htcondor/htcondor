07c932ca34dec39b776d43ab9e1e998578cedd27
wright <wright>
1999-04-15 20:37:14 +0000

    Removed old, dead APIs that don't do the right thing: get_inet_address(), get_machine_name(), evaluate_job_id(), and append_job_id().  *_job_id we #if 0'd out, and get_* should be replaced with calls to my_sin_addr() and my_full_hostname().

--- src/h/internet.h
+++ src/h/internet.h
@@ -51,28 +51,6 @@ typedef struct job_id
 extern "C" {
 #endif
 
-/* 
-**  returns the internet address of this host 
-**  returns 1 on success and -1 on failure 
-*/
-int get_inet_address(struct in_addr *buffer);
-
-/* 
-** returns the full internet name of this host 
-** returns 1 on success and -1 on failure
-*/
-int get_machine_name(char *buffer);
-
-/*
-** extract the internet-wide unique job_id from the job-context
-*/
-int evaluate_job_id(CONTEXT *context, JOB_ID *job_id);
-
-/*
-** appends the internet wide unique job_id to the job-context
-*/
-int append_job_id(CONTEXT *context, int cluster, int proc);
-
 /* Convert a string of the form "<xx.xx.xx.xx:pppp>" to a sockaddr_in  TCP */
 int string_to_sin(char *addr, struct sockaddr_in *sin);
 
