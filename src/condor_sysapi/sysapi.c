ebb95d8ea05906827f0dcd72187956e36b1011e1
psilord <psilord>
1999-04-28 20:40:45 +0000

    This function translates the old calc_* and other type calls to the new API. This is done so I can develop the API without having to change gobs of code elsewhere. When the API is done, then I can make the changes everywhere in the code at once.

---
+++ src/condor_sysapi/sysapi.c
@@ -0,0 +1,14 @@
+#include "sysapi.h"
+
+/* 
+	this is a temporary file to map all of the old function names to the new
+	API system before I go through all of the old code and replace them.
+*/
+
+int
+calc_phys_memory()
+{
+	sysapi_phys_memory();
+}
+
+
