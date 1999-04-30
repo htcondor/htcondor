04878fcf0061f6f3f576631cfe700d1a76eb9765
psilord <psilord>
1999-04-30 19:00:06 +0000

    Added free_fs_blocks map to the sysapi version.

--- src/condor_sysapi/sysapi.c
+++ src/condor_sysapi/sysapi.c
@@ -3,7 +3,10 @@
 /* 
 	this is a temporary file to map all of the old function names to the new
 	API system before I go through all of the old code and replace them.
+	The sysapi is a comletely c linkaged entity.
 */
+
+/* also not that these return the cooked versions of the functions */
 
 int
 calc_phys_memory()
@@ -12,3 +15,8 @@ calc_phys_memory()
 }
 
 
+int
+free_fs_blocks(const char *filename)
+{
+	sysapi_free_fs_blocks(filename);
+}
