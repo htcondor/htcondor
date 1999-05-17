e0946f7f71dc8882764e82200fc00e2d4c5ec146
psilord <psilord>
1999-05-17 19:46:07 +0000

    Added more functions that I have converted to sysapi.

--- src/condor_sysapi/sysapi.c
+++ src/condor_sysapi/sysapi.c
@@ -6,17 +6,49 @@
 	The sysapi is a comletely c linkaged entity.
 */
 
-/* also not that these return the cooked versions of the functions */
+/* also note that these return the cooked versions of the functions */
 
 int
 calc_phys_memory()
 {
-	sysapi_phys_memory();
+	return sysapi_phys_memory();
 }
-
 
 int
 free_fs_blocks(const char *filename)
 {
-	sysapi_free_fs_blocks(filename);
+	return sysapi_diskspace(filename);
 }
+
+int
+calc_ncpus()
+{
+	return sysapi_ncpus();
+}
+
+int
+calc_mips()
+{
+	return sysapi_mips();
+}
+
+int
+calc_kflops()
+{
+	return sysapi_kflops();
+}
+
+void
+calc_idle_time(time_t *m_idle, time_t *m_console_idle)
+{
+	sysapi_idle_time(m_idle, m_console_idle);
+}
+
+float
+calc_load_avg()
+{
+	return sysapi_load_avg();
+}
+
+
+
