9531bda7873c6e6b060ea9b9181e5f8ddafa4ac7
tannenba <tannenba>
1999-05-24 21:58:42 +0000

    added macros and data types for pipe(), struct rusage

--- src/condor_includes/condor_sys_nt.h
+++ src/condor_includes/condor_sys_nt.h
@@ -46,6 +46,7 @@ typedef DWORD pid_t;
 #define MAXPATHLEN 1024
 #define MAXHOSTNAMELEN 64
 #define	_POSIX_PATH_MAX 255
+#define pipe(fds) _pipe(fds,2048,_O_BINARY)
 #define popen _popen
 #define pclose _pclose
 #define strcasecmp _stricmp
@@ -95,6 +96,25 @@ typedef DWORD pid_t;
 #define rint(num) floor(num + .5)
 
 typedef fd_set *SELECT_FDSET_PTR;
+
+struct rusage {
+    struct timeval ru_utime;    /* user time used */
+    struct timeval ru_stime;    /* system time used */
+    long    ru_maxrss;      /* XXX: 0 */
+    long    ru_ixrss;       /* XXX: 0 */
+    long    ru_idrss;       /* XXX: sum of rm_asrss */
+    long    ru_isrss;       /* XXX: 0 */
+    long    ru_minflt;      /* any page faults not requiring I/O */
+    long    ru_majflt;      /* any page faults requiring I/O */
+    long    ru_nswap;       /* swaps */
+    long    ru_inblock;     /* block input operations */
+    long    ru_oublock;     /* block output operations */
+    long    ru_msgsnd;      /* messages sent */
+    long    ru_msgrcv;      /* messages received */
+    long    ru_nsignals;        /* signals received */
+    long    ru_nvcsw;       /* voluntary context switches */
+    long    ru_nivcsw;      /* involuntary " */
+};
 
 #endif /* CONDOR_SYS_NT_H */
 
