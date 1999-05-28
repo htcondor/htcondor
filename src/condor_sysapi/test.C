dd3468baab6d7df029befb6befe7f91f1a499dc0
psilord <psilord>
1999-05-28 19:18:17 +0000

    Added all of the test cases. If you call the entry functions, plenty of information will now go into the logs prepended by a 'SysAPI:' symbol.

--- src/condor_sysapi/test.C
+++ src/condor_sysapi/test.C
@@ -1,3 +1,27 @@
+/***************************Copyright-DO-NOT-REMOVE-THIS-LINE**
+ * CONDOR Copyright Notice
+ *
+ * See LICENSE.TXT for additional notices and disclaimers.
+ *
+ * Copyright (c)1990-1998 CONDOR Team, Computer Sciences Department,
+ * University of Wisconsin-Madison, Madison, WI.  All Rights Reserved.
+ * No use of the CONDOR Software Program Source Code is authorized
+ * without the express consent of the CONDOR Team.  For more information
+ * contact: CONDOR Team, Attention: Professor Miron Livny,
+ * 7367 Computer Sciences, 1210 W. Dayton St., Madison, WI 53706-1685,
+ * (608) 262-0856 or miron@cs.wisc.edu.
+ *
+ * U.S. Government Rights Restrictions: Use, duplication, or disclosure
+ * by the U.S. Government is subject to restrictions as set forth in
+ * subparagraph (c)(1)(ii) of The Rights in Technical Data and Computer
+ * Software clause at DFARS 252.227-7013 or subparagraphs (c)(1) and
+ * (2) of Commercial Computer Software-Restricted Rights at 48 CFR
+ * 52.227-19, as applicable, CONDOR Team, Attention: Professor Miron
+ * Livny, 7367 Computer Sciences, 1210 W. Dayton St., Madison,
+ * WI 53706-1685, (608) 262-0856 or miron@cs.wisc.edu.
+****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
+
+#include "condor_common.h"
 #include "condor_debug.h"
 #include "sysapi.h"
 #include "sysapi_externs.h"
@@ -12,15 +36,21 @@
 extern "C" void
 sysapi_test_dump_internal_vars(void)
 {
-	dprintf(D_ALWAYS, "_sysapi_config = %s\n", 
+	dprintf(D_ALWAYS, "SysAPI: Dumping %s internal variables\n", 
+		_sysapi_config==TRUE?"initialized":"uninitialized");
+
+	dprintf(D_ALWAYS, "SysAPI: _sysapi_config = %s\n", 
 		_sysapi_config==TRUE?"TRUE":"FALSE");
 
-	dprintf(D_ALWAYS, "_sysapi_console_devices = %p\n",_sysapi_console_devices);
-	dprintf(D_ALWAYS, "_sysapi_last_x_event = %d\n", _sysapi_last_x_event);
-	dprintf(D_ALWAYS, "_sysapi_reserve_afs_cache = %s\n", 
+	dprintf(D_ALWAYS, 
+		"SysAPI: _sysapi_console_devices = %p\n",_sysapi_console_devices);
+	dprintf(D_ALWAYS, 
+		"SysAPI: _sysapi_last_x_event = %d\n", _sysapi_last_x_event);
+	dprintf(D_ALWAYS, "SysAPI: _sysapi_reserve_afs_cache = %s\n", 
 		_sysapi_reserve_afs_cache==TRUE?"TRUE":"FALSE");
-	dprintf(D_ALWAYS, "_sysapi_reserve_disk = %d\n", _sysapi_reserve_disk);
-	dprintf(D_ALWAYS, "_sysapi_startd_has_bad_utmp = %s\n",
+	dprintf(D_ALWAYS, 
+		"SysAPI: _sysapi_reserve_disk = %d\n", _sysapi_reserve_disk);
+	dprintf(D_ALWAYS, "SysAPI: _sysapi_startd_has_bad_utmp = %s\n",
 		_sysapi_startd_has_bad_utmp==TRUE?"TRUE":"FALSE");
 }
 
@@ -34,61 +64,67 @@ sysapi_test_dump_functions(void)
 	char *qux = NULL;
 	time_t t0, t1;
 
-	foo = sysapi_phys_memory_raw();
-	dprintf(D_ALWAYS, "sysapi_phys_memory_raw() -> %d\n", foo);
-	foo = sysapi_phys_memory();
-	dprintf(D_ALWAYS, "sysapi_phys_memory() -> %d\n", foo);
+	dprintf(D_ALWAYS, "SysAPI: Calling SysAPI functions....\n");
 
-	foo = sysapi_diskspace_raw("/");
-	dprintf(D_ALWAYS, "sysapi_diskspace_raw() -> %d\n", foo);
-	foo = sysapi_diskspace("/");
-	dprintf(D_ALWAYS, "sysapi_diskspace() -> %d\n", foo);
+	foo = sysapi_phys_memory_raw();
+	dprintf(D_ALWAYS, "SysAPI: sysapi_phys_memory_raw() -> %d\n", foo);
+	foo = sysapi_phys_memory();
+	dprintf(D_ALWAYS, "SysAPI: sysapi_phys_memory() -> %d\n", foo);
+
+	foo = sysapi_disk_space_raw("/");
+	dprintf(D_ALWAYS, "SysAPI: sysapi_disk_space_raw() -> %d\n", foo);
+	foo = sysapi_disk_space("/");
+	dprintf(D_ALWAYS, "SysAPI: sysapi_disk_space() -> %d\n", foo);
 
 	foo = sysapi_ncpus_raw();
-	dprintf(D_ALWAYS, "sysapi_ncpus_raw() -> %d\n", foo);
+	dprintf(D_ALWAYS, "SysAPI: sysapi_ncpus_raw() -> %d\n", foo);
 	foo = sysapi_ncpus();
-	dprintf(D_ALWAYS, "sysapi_ncpus() -> %d\n", foo);
+	dprintf(D_ALWAYS, "SysAPI: sysapi_ncpus() -> %d\n", foo);
 
 	foo = sysapi_mips_raw();
-	dprintf(D_ALWAYS, "sysapi_mips_raw() -> %d\n", foo);
+	dprintf(D_ALWAYS, "SysAPI: sysapi_mips_raw() -> %d\n", foo);
 	foo = sysapi_mips();
-	dprintf(D_ALWAYS, "sysapi_mips() -> %d\n", foo);
+	dprintf(D_ALWAYS, "SysAPI: sysapi_mips() -> %d\n", foo);
 
 	foo = sysapi_kflops_raw();
-	dprintf(D_ALWAYS, "sysapi_kflops_raw() -> %d\n", foo);
+	dprintf(D_ALWAYS, "SysAPI: sysapi_kflops_raw() -> %d\n", foo);
 	foo = sysapi_kflops();
-	dprintf(D_ALWAYS, "sysapi_kflops() -> %d\n", foo);
+	dprintf(D_ALWAYS, "SysAPI: sysapi_kflops() -> %d\n", foo);
 
 	sysapi_idle_time_raw(&t0, &t1);
-	dprintf(D_ALWAYS,"sysapi_idle_time_raw() -> (%f,%f)\n",(float)t0,(float)t1);
+	dprintf(D_ALWAYS,"SysAPI: sysapi_idle_time_raw() -> (%f,%f)\n",(float)t0,(float)t1);
 	sysapi_idle_time(&t0, &t1);
-	dprintf(D_ALWAYS, "sysapi_idle_time() -> (%f,%f)\n", t0, t1);
+	dprintf(D_ALWAYS, "SysAPI: sysapi_idle_time() -> (%f,%f)\n", (float)t0, (float)t1);
 
 	bar = sysapi_load_avg_raw();
-	dprintf(D_ALWAYS, "sysapi_load_avg_raw() -> %f\n", bar);
+	dprintf(D_ALWAYS, "SysAPI: sysapi_load_avg_raw() -> %f\n", bar);
 	bar = sysapi_load_avg();
-	dprintf(D_ALWAYS, "sysapi_load_avg() -> %f\n", bar);
+	dprintf(D_ALWAYS, "SysAPI: sysapi_load_avg() -> %f\n", bar);
 
 	qux = sysapi_condor_arch();
-	dprintf(D_ALWAYS, "sysapi_condor_arch -> %s\n", qux);
-	free(qux);
-	qux = sysapi_uname_arch();
-	dprintf(D_ALWAYS, "sysapi_uname_arch -> %s\n", qux);
-	free(qux);
+	dprintf(D_ALWAYS, "SysAPI: sysapi_condor_arch -> %s\n", qux);
 
-	foo = sysapi_virt_memory_raw();
-	dprintf(D_ALWAYS, "sysapi_virt_memory_raw() -> %d\n", foo);
-	foo = sysapi_virt_memory();
-	dprintf(D_ALWAYS, "sysapi_virt_memory() -> %d\n", foo);
+	qux = sysapi_uname_arch();
+	dprintf(D_ALWAYS, "SysAPI: sysapi_uname_arch -> %s\n", qux);
+
+	qux = sysapi_opsys();
+	dprintf(D_ALWAYS, "SysAPI: sysapi_opsys -> %s\n", qux);
+
+	foo = sysapi_swap_space_raw();
+	dprintf(D_ALWAYS, "SysAPI: sysapi_swap_space_raw() -> %d\n", foo);
+	foo = sysapi_swap_space();
+	dprintf(D_ALWAYS, "SysAPI: sysapi_swap_space() -> %d\n", foo);
 }
 
 /* the main entry function, this will do all the magic */
 extern "C" void
-sysapi_test_dump(void)
+sysapi_test_dump_all(void)
 {
+	dprintf(D_ALWAYS, "SysAPI: BEGIN SysAPI DUMP!\n");
 	sysapi_test_dump_internal_vars();
 	sysapi_reconfig();
 	sysapi_test_dump_internal_vars();
 
 	sysapi_test_dump_functions();
+	dprintf(D_ALWAYS, "SysAPI: END SysAPI DUMP!\n");
 }
