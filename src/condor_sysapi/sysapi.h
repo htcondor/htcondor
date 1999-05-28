a511d54026cb4e0baa6860bc2f19ea6e826e1767
psilord <psilord>
1999-05-28 19:25:08 +0000

    + added (void) where there was () in the prototypes. + changed the name of sysapi_virt_mem* to sysapi_swap_space* 	because that is what you are actually calculating.

--- src/condor_sysapi/sysapi.h
+++ src/condor_sysapi/sysapi.h
@@ -29,28 +29,28 @@
 BEGIN_C_DECLS
 
 /* How to get the physical memory on a machine, answer in Megs */
-int sysapi_phys_memory_raw();
-int sysapi_phys_memory();
+int sysapi_phys_memory_raw(void);
+int sysapi_phys_memory(void);
 
 /* How to get the free disk blocks from a full pathname, answer in KB */
-int sysapi_diskspace_raw(const char *filename);
-int sysapi_diskspace(const char *filename);
+int sysapi_disk_space_raw(const char *filename);
+int sysapi_disk_space(const char *filename);
 
 /* return the number of cpus there on a machine */
-int sysapi_ncpus_raw();
-int sysapi_ncpus();
+int sysapi_ncpus_raw(void);
+int sysapi_ncpus(void);
 
 /* calculate the number of mips the machine is. Even though this is a user
 	thing and on platforms like NT it is done by hand, it goes in the sysapi.
 	The reasoning is that you are still asking for a fundamental parameter
 	from the machine, and even if there isn't OS support for it, it is
 	still a lowlevel number. */
-int sysapi_mips_raw();
-int sysapi_mips();
+int sysapi_mips_raw(void);
+int sysapi_mips(void);
 
 /* as above, but for kflops */
-int sysapi_kflops_raw();
-int sysapi_kflops();
+int sysapi_kflops_raw(void);
+int sysapi_kflops(void);
 
 /* return the idle time on the machine in the arguments */
 void sysapi_idle_time_raw(time_t *m_idle, time_t *m_console_idle);
@@ -61,17 +61,30 @@ void sysapi_reconfig(void);
 void sysapi_internal_reconfig(void);
 
 /* if this is called, then the sysapi knows that a last_x_event has happend
-   and records the time it happened, this is quite useful in idle_time.C */
+	and records the time it happened, this is quite useful in idle_time.C */
 void sysapi_last_xevent(void);
 
 /* return the one minute load average on a machine */
 float sysapi_load_avg_raw(void);
 float sysapi_load_avg(void);
 
-/* return information about the arch and operating system */
+/* return information about the arch and operating system.
+	The pointer returned points to a static buffer defined in the library,
+	do not free it */
 char* sysapi_condor_arch(void);
 char* sysapi_uname_arch(void);
 char* sysapi_opsys(void);
+
+/* return information about how many 1K blocks of swap space there are.
+	If there are more 1K blocks than INT_MAX, then INT_MAX is returned */
+int sysapi_swap_space_raw(void);
+int sysapi_swap_space(void);
+
+/* these are functions that spit out data about the library, useful for
+	testing or debugging purposes */
+void sysapi_test_dump_all(void);
+void sysapi_test_dump_internal_vars(void);
+void sysapi_test_dump_functions(void);
 
 END_C_DECLS
 
