54289c08bb63c306551312d0f86d7da368e76723
psilord <psilord>
1999-06-03 18:29:57 +0000

    + updated to use the sysapi library now

--- src/condor_tools/test_calc.C
+++ src/condor_tools/test_calc.C
@@ -1,20 +1,15 @@
 #include "condor_common.h"
 #include "condor_config.h"
 
-extern "C" {
-	int free_fs_blocks( const char* );
-	int	calc_phys_memory();
-}
-
 
 int
 main(int argc, char* argv[]) 
 {
 	config(0);
 	if( argc > 1 ) {
-		printf( "Free space in %s: %d\n", argv[1], free_fs_blocks(argv[1]) );
+		printf( "Free space in %s: %d\n", argv[1], sysapi_disk_space(argv[1]) );
 	}
-	printf( "Free memory: %d\n", calc_phys_memory() );
+	printf( "Free memory: %d\n", sysapi_phys_memory() );
 	return 0;
 
 }
