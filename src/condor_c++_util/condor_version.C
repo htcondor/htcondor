159732e9e7d1d916e84eacc59e82339c37604f48
wright <wright>
1999-06-10 18:03:30 +0000

    Use the __DATE__ cpp token to get the build date automagically.

--- src/condor_c++_util/condor_version.C
+++ src/condor_c++_util/condor_version.C
@@ -22,7 +22,7 @@
 ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
 
 /* Here is the version string - update before a public release */
-static char* CondorVersionString = "$Version: 6.1.6 1999/05/28 $";
+static char* CondorVersionString = "$Version: 6.1.6 " __DATE__ " $";
 
 extern "C" {
 
