993d75cdc3f02ce20ac904622425ee90f1aaa63b
wright <wright>
1999-06-10 18:38:36 +0000

    Moved from "$Version: ..." to "$CondorVersion: ..." to avoid possible naming conflicts and guarantee uniqueness.

--- src/condor_c++_util/condor_version.C
+++ src/condor_c++_util/condor_version.C
@@ -22,7 +22,7 @@
 ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
 
 /* Here is the version string - update before a public release */
-static char* CondorVersionString = "$Version: 6.1.6 " __DATE__ " $";
+static char* CondorVersionString = "$CondorVersion: 6.1.6 " __DATE__ " $";
 
 extern "C" {
 
