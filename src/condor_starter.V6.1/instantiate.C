c8be56de64a797fc36bf329139068c1e0dd60f50
tannenba <tannenba>
1999-07-02 21:56:07 +0000

    needed changes to compile new starter on Unix now that yet another hash table has been added to the file transfer object (to support asynchronous transfer).

--- src/condor_starter.V6.1/instantiate.C
+++ src/condor_starter.V6.1/instantiate.C
@@ -31,4 +31,6 @@
 template class List<UserProc>;
 template class HashTable<MyString, FileTransfer *>;
 template class HashBucket<MyString, FileTransfer *>;
+template class HashTable<int, FileTransfer *>;
+template class HashBucket<int, FileTransfer *>;
 template class Item<UserProc>;
