b0dbc8e95954aff6734f6625fdbe515dcaeb4caa
psilord <psilord>
1999-07-08 16:15:15 +0000

    I couldn't *stand* the indenting style anymore. It was driving me insane. I fixed it.

--- src/condor_kbdd/XInterface.C
+++ src/condor_kbdd/XInterface.C
@@ -38,8 +38,8 @@ static char *_FileName_ = __FILE__;
 
 extern int PollActivity();
 
-bool        g_connected;
-jmp_buf     jmp;
+bool		g_connected;
+jmp_buf	 jmp;
 
 
 // When a window has a very short life time, we get messages
@@ -52,23 +52,23 @@ jmp_buf     jmp;
 static int 
 CatchFalseAlarm(Display *display, XErrorEvent *err)
 {
-    char msg[80];
-    XGetErrorText(display, err->error_code, msg, 80);
-    dprintf(D_FULLDEBUG, "Caught Error code(%d): %s\n", err->error_code, msg);
-    return 0;
+	char msg[80];
+	XGetErrorText(display, err->error_code, msg, 80);
+	dprintf(D_FULLDEBUG, "Caught Error code(%d): %s\n", err->error_code, msg);
+	return 0;
 }
 
 static int 
 CatchIOFalseAlarm(Display *display, XErrorEvent *err)
 {
-    char msg[80];
-    g_connected = false;
-        
-    XGetErrorText(display, err->error_code, msg, 80);
-    dprintf(D_FULLDEBUG, "Caught Error code(%d): %s\n", err->error_code, msg);
+	char msg[80];
+	g_connected = false;
+		
+	XGetErrorText(display, err->error_code, msg, 80);
+	dprintf(D_FULLDEBUG, "Caught Error code(%d): %s\n", err->error_code, msg);
 
-    longjmp(jmp, 0);
-    return 0;
+	longjmp(jmp, 0);
+	return 0;
 }
 
 // Get the next utmp entry and set our XAUTHORITY environment
@@ -76,328 +76,326 @@ CatchIOFalseAlarm(Display *display, XErrorEvent *err)
 int
 XInterface::NextEntry()
 {
-    utmp *utmp_entry;
-    char *tmp;
+	utmp *utmp_entry;
+	char *tmp;
  
-    if(!_tried_root)
-    {
-	TryUser("root");
-	_tried_root = true;
-    }
-    else if(!_tried_utmp)
-    {
-	do
+	if(!_tried_root)
 	{
-	    utmp_entry = getutent();
-	    if(!utmp_entry)
-	    {
-		_tried_utmp = true;
-		dprintf(D_FULLDEBUG, "Tryed all utmp users, "
-			"now moving to XAUTHORITY_USERS param.\n");
-		return 0;  // Keep trying....
-	    }
-	} 
-	while(utmp_entry->ut_type != USER_PROCESS);
+		TryUser("root");
+		_tried_root = true;
+	}
+	else if(!_tried_utmp)
+	{
+		do
+		{
+			utmp_entry = getutent();
+			if(!utmp_entry)
+			{
+				_tried_utmp = true;
+				dprintf(D_FULLDEBUG, "Tryed all utmp users, "
+					"now moving to XAUTHORITY_USERS param.\n");
+				return 0;  // Keep trying....
+			}
+		} 
+		while(utmp_entry->ut_type != USER_PROCESS);
 
-	TryUser(utmp_entry->ut_user);
-    }
-    else  // Try the others
-    {
-	if(_xauth_users == NULL)
-	{
-	    dprintf(D_FULLDEBUG, "No XAUTHORITY_USERS specified.\n");
-	    return -1; // No more entries...
+		TryUser(utmp_entry->ut_user);
 	}
-	else
+	else  // Try the others
 	{
-	    if((tmp = _xauth_users->next()))
-	    {
-		dprintf(D_FULLDEBUG, "Will try XAUTHORITY_USER '%s'\n", 
-			tmp);
-		TryUser(tmp);
-	    }
-	    else
-	    {
-		return -1; // No more entries...
-	    }
+		if(_xauth_users == NULL)
+		{
+			dprintf(D_FULLDEBUG, "No XAUTHORITY_USERS specified.\n");
+			return -1; // No more entries...
+		}
+		else
+		{
+			if((tmp = _xauth_users->next()))
+			{
+				dprintf(D_FULLDEBUG, "Will try XAUTHORITY_USER '%s'\n", 
+					tmp);
+				TryUser(tmp);
+			}
+			else
+			{
+				return -1; // No more entries...
+			}
+		}
 	}
-    }
-    return 0; 
+	return 0; 
 }
 
 void
 XInterface::TryUser(const char *user)
 {
-    static char env[1024];
-    passwd *passwd_entry;
+	static char env[1024];
+	passwd *passwd_entry;
 
-    passwd_entry = getpwnam(user);
-    if(passwd_entry == NULL) {
-	// We couldn't find the current user in the passwd file?
-	dprintf( D_FULLDEBUG, 
-		 "Current user cannot be found in passwd file.\n" );
-	return;
-    } else {
-	fflush(stdout);
-	sprintf(env, "XAUTHORITY=%s/.Xauthority", passwd_entry->pw_dir);
-	if(putenv(env) != 0) {
-	    EXCEPT("Putenv failed!.");
+	passwd_entry = getpwnam(user);
+	if(passwd_entry == NULL) {
+		// We couldn't find the current user in the passwd file?
+		dprintf( D_FULLDEBUG, 
+		 	"Current user cannot be found in passwd file.\n" );
+		return;
+	} else {
+		fflush(stdout);
+		sprintf(env, "XAUTHORITY=%s/.Xauthority", passwd_entry->pw_dir);
+		if(putenv(env) != 0) {
+			EXCEPT("Putenv failed!.");
 	}
 	dprintf( D_FULLDEBUG, "Using %s's .Xauthority: \n", 
 		 passwd_entry->pw_name, env);
-    }
+	}
 }
-    
+	
 
 XInterface::XInterface(int id)
 {
-    char *tmp;
-    _daemon_core_timer = id;
-    
-    // We may need access to other user's home directories, so we must run
-    // as root.
-    
-    set_root_priv();
-    
-    if( geteuid() != 0 ) {
-	dprintf(D_FULLDEBUG, "NOTE: Daemon can't use Xauthority if not"
-			" running as root.\n");
-    }
-    
-    set_condor_priv();
+	char *tmp;
+	_daemon_core_timer = id;
+	
+	// We may need access to other user's home directories, so we must run
+	// as root.
+	
+	set_root_priv();
+	
+	if( geteuid() != 0 ) {
+		dprintf(D_FULLDEBUG, "NOTE: Daemon can't use Xauthority if not"
+				" running as root.\n");
+	}
+	
+	set_condor_priv();
 
-    g_connected = false;
+	g_connected = false;
 
-    tmp = param( "XAUTHORITY_USERS" );
-    if(tmp != NULL) {
+	tmp = param( "XAUTHORITY_USERS" );
+	if(tmp != NULL) {
 		_xauth_users = new StringList();
 		_xauth_users->initializeFromString( tmp );
 		free( tmp );
-    } else {
+	} else {
 		_xauth_users = NULL;
-    }
-    Connect();
+	}
+	Connect();
 }
 
 XInterface::~XInterface()
 {
-    if(_xauth_users != NULL) {
+	if(_xauth_users != NULL) {
 		delete _xauth_users;
-    }
+	}
 }
 
 bool
 XInterface::Connect()
 {
-    int rtn;
-    Window root;
-    int s;
+	int rtn;
+	Window root;
+	int s;
 
-    set_root_priv();
+	set_root_priv();
 
-    setutent(); // Reset utmp file to beginning.
+	setutent(); // Reset utmp file to beginning.
 
-    _tried_root = false;
-    _tried_utmp = false;
-    if(_xauth_users != NULL) {
-	_xauth_users->rewind();
-    }
-
-    while(!(_display = XOpenDisplay("localhost:0.0") ))
-    {
-	rtn = NextEntry();
-	if(rtn == -1)
-	{
-	    dprintf(D_FULLDEBUG, "Exausted all possible attempts to "
-		    "connect to X server, will try again in 60 seconds.\n");
-	    daemonCore->Reset_Timer( _daemon_core_timer, 60 ,60 );
-	    dprintf(D_FULLDEBUG, "Reset timer: %d\n", _daemon_core_timer);
-
-	    g_connected = false;
-	    set_condor_priv();
-	    return false;
+	_tried_root = false;
+	_tried_utmp = false;
+	if(_xauth_users != NULL) {
+		_xauth_users->rewind();
 	}
-    }
 
-    dprintf(D_ALWAYS, "Connected to X server: localhost:0.0\n");
-    g_connected = true;
+	while(!(_display = XOpenDisplay("localhost:0.0") ))
+	{
+		rtn = NextEntry();
+		if(rtn == -1)
+		{
+			dprintf(D_FULLDEBUG, "Exausted all possible attempts to "
+				"connect to X server, will try again in 60 seconds.\n");
+			daemonCore->Reset_Timer( _daemon_core_timer, 60 ,60 );
+			dprintf(D_FULLDEBUG, "Reset timer: %d\n", _daemon_core_timer);
+	
+			g_connected = false;
+			set_condor_priv();
+			return false;
+		}
+	}
 
-    dprintf(D_FULLDEBUG, "Reset timer: %d\n", _daemon_core_timer);
-    daemonCore->Reset_Timer( _daemon_core_timer, 5 ,5 );
-    
-    // See note above the function to see why we need to do this.
-    XSetErrorHandler((XErrorHandler) CatchFalseAlarm);
-    XSetIOErrorHandler((XIOErrorHandler) CatchIOFalseAlarm);
+	dprintf(D_ALWAYS, "Connected to X server: localhost:0.0\n");
+	g_connected = true;
 
-    //Select the events on each root window of each screen
-    for(s = 0; s < ScreenCount(_display); s++)
-    {
-	root = RootWindowOfScreen(ScreenOfDisplay(_display, s));
-	SelectEvents(root);
-    }
-    
-    // Query pointer stuff
-    _pointer_root = DefaultRootWindow(_display);
-    _pointer_screen = ScreenOfDisplay(_display, DefaultScreen(_display));
-    _pointer_prev_x = -1;
-    _pointer_prev_y = -1;
-    _pointer_prev_mask = 0;
+	dprintf(D_FULLDEBUG, "Reset timer: %d\n", _daemon_core_timer);
+	daemonCore->Reset_Timer( _daemon_core_timer, 5 ,5 );
+	
+	// See note above the function to see why we need to do this.
+	XSetErrorHandler((XErrorHandler) CatchFalseAlarm);
+	XSetIOErrorHandler((XIOErrorHandler) CatchIOFalseAlarm);
 
-    set_condor_priv();
-    return true;
+	//Select the events on each root window of each screen
+	for(s = 0; s < ScreenCount(_display); s++)
+	{
+		root = RootWindowOfScreen(ScreenOfDisplay(_display, s));
+		SelectEvents(root);
+	}
+	
+	// Query pointer stuff
+	_pointer_root = DefaultRootWindow(_display);
+	_pointer_screen = ScreenOfDisplay(_display, DefaultScreen(_display));
+	_pointer_prev_x = -1;
+	_pointer_prev_y = -1;
+	_pointer_prev_mask = 0;
+
+	set_condor_priv();
+	return true;
 }
 
 bool
 XInterface::CheckActivity()
 {
-    setjmp(jmp);
-    if(!g_connected)
-    {
-	// If we can't connect, we don't know anything....
-	if( Connect() == false )
+	setjmp(jmp);
+	if(!g_connected)
 	{
-	    return false;
+		// If we can't connect, we don't know anything....
+		if( Connect() == false )
+		{
+			return false;
+		}
 	}
-    }
-    
-    if(ProcessEvents())
-    {
-	return true;
-    }
-    else if(QueryPointer())
-    {
-	return true;
-    }
-    else
-    {
-	return false;
-    }
+	
+	if(ProcessEvents())
+	{
+		return true;
+	}
+	else if(QueryPointer())
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 
 bool 
 XInterface::ProcessEvents()
 {
-    XEvent event;
-    
-    while(XPending(_display))
-    {
-	if(XCheckMaskEvent(_display, SubstructureNotifyMask, &event))
+	XEvent event;
+	
+	while(XPending(_display))
 	{
-	    if(event.type == CreateNotify)
-	    {
-		SelectEvents(event.xcreatewindow.window);
-	    }
+		if(XCheckMaskEvent(_display, SubstructureNotifyMask, &event))
+		{
+			if(event.type == CreateNotify)
+			{
+			SelectEvents(event.xcreatewindow.window);
+			}
+		}
+		else
+		{
+			XNextEvent(_display, &event);
+		}
 	}
-	else
-	{
-	    XNextEvent(_display, &event);
-	}
-    }
-    if( (event.type == KeyPress || event.type == ButtonPress || 
+	if( (event.type == KeyPress || event.type == ButtonPress || 
 	 event.type == ButtonRelease || event.type == MotionNotify) 
 	&& !event.xany.send_event )
-    {
-	time(&_last_event);
-	return true;
-    }
-    return false;
+	{
+		time(&_last_event);
+		return true;
+	}
+	return false;
 }
 
 
 void 
 XInterface::SelectEvents(Window win)
 {
-    Window root;
-    Window parent;
-    Window *children;
-    unsigned int num_children = 0;
-    unsigned int i;
-    XWindowAttributes attribs;
+	Window root;
+	Window parent;
+	Window *children;
+	unsigned int num_children = 0;
+	unsigned int i;
+	XWindowAttributes attribs;
 
-    if(!XQueryTree(_display, win, &root, &parent, &children, &num_children))
-    {
-	return;
-    }
-    
-    if(parent == None) // The root of all evil!!!!!!!!!!!!!11!!1!!
-    {
-	attribs.all_event_masks = 
-	    attribs.do_not_propagate_mask = KeyPressMask;
-    }
-    else if(XGetWindowAttributes(_display, win, &attribs) == 0)
-    {
-	dprintf(D_ALWAYS, "XGetWindowAttributes() failed.\n");
-	return;
-    }
-    else
-    {
-	XSelectInput(_display, win, SubstructureNotifyMask | 
-		     ((attribs.all_event_masks | attribs.do_not_propagate_mask) 
-		      & KeyPressMask));
+	if(!XQueryTree(_display, win, &root, &parent, &children, &num_children))
+	{
+		return;
+	}
 	
-    }
-    
-    //Recursion in action
-    for(i = 0; i < num_children; i++)
-    {
-	    SelectEvents(children[i]);
-    }
-    if(num_children > 0)
-    {
-	XFree(children);
-    }
+	if(parent == None) // The root of all evil!!!!!!!!!!!!!11!!1!!
+	{
+		attribs.all_event_masks = 
+			attribs.do_not_propagate_mask = KeyPressMask;
+	}
+	else if(XGetWindowAttributes(_display, win, &attribs) == 0)
+	{
+		dprintf(D_ALWAYS, "XGetWindowAttributes() failed.\n");
+		return;
+	}
+	else
+	{
+		XSelectInput(_display, win, SubstructureNotifyMask | 
+			 	((attribs.all_event_masks | attribs.do_not_propagate_mask) 
+			  	& KeyPressMask));
+	
+	}
+	
+	//Recursion in action
+	for(i = 0; i < num_children; i++)
+	{
+		SelectEvents(children[i]);
+	}
+	if(num_children > 0)
+	{
+		XFree(children);
+	}
 }
 
 bool
 XInterface::QueryPointer()
 {
-    Window dummy_win;
-    int dummy;
-    unsigned int mask;
-    int root_x;
-    int root_y;
-    int i;
-    time_t now;
-    bool found = false;
+	Window dummy_win;
+	int dummy;
+	unsigned int mask;
+	int root_x;
+	int root_y;
+	int i;
+	time_t now;
+	bool found = false;
 
-    if(!XQueryPointer(_display, _pointer_root, &_pointer_root, &dummy_win, 
-		      &root_x, &root_y, &dummy, &dummy, &mask))
-    {
-	//Pointer has moved to another screen
-	for(i = 0; i < ScreenCount(_display); i++)
+	if(!XQueryPointer(_display, _pointer_root, &_pointer_root, &dummy_win, 
+			  &root_x, &root_y, &dummy, &dummy, &mask))
 	{
-	    if(_pointer_root == RootWindow(_display, i))
-	    {
-		_pointer_screen = ScreenOfDisplay(_display, i);
-		found = true;
-		break;
-	    }
+		//Pointer has moved to another screen
+		for(i = 0; i < ScreenCount(_display); i++)
+		{
+			if(_pointer_root == RootWindow(_display, i))
+			{
+				_pointer_screen = ScreenOfDisplay(_display, i);
+				found = true;
+				break;
+			}
+		}
+		if(!found)
+		{
+			dprintf(D_ALWAYS, "Lost connection to X server.\n");
+			g_connected = false;
+		}
 	}
-	if(!found)
-	{
-	    dprintf(D_ALWAYS, "Lost connection to X server.\n");
-	    g_connected = false;
-	}
-    }
 
-    if(root_x == _pointer_prev_x && root_y == _pointer_prev_y && 
-       mask == _pointer_prev_mask)
-    {
-	// Pointer has not moved.
-	return false;
-    }
-    else
-    {
-	// Pointer has indeed moved.
-	time(&now);
-	_last_event = now;
-	_pointer_prev_x = root_x;
-	_pointer_prev_y = root_y;
-	_pointer_prev_mask = mask;
-	return true;
-    }
-    
-        
+	if(root_x == _pointer_prev_x && root_y == _pointer_prev_y && 
+	   mask == _pointer_prev_mask)
+	{
+		// Pointer has not moved.
+		return false;
+	}
+	else
+	{
+		// Pointer has indeed moved.
+		time(&now);
+		_last_event = now;
+		_pointer_prev_x = root_x;
+		_pointer_prev_y = root_y;
+		_pointer_prev_mask = mask;
+		return true;
+	}
 }
 
