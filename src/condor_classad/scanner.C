558c07cbbe610c0f172dd3dc504c881d71ffa0b5
tannenba <tannenba>
1999-07-28 16:00:21 +0000

    + added a reset() method to the Token class.
    + re-arranged the scanner and added switch statements to minimize the
    number of calls we make to strncasecmp() inside of tight
    loops (optimization made after profiling).
    
    + moved some buffers from the heap to the stack

--- src/condor_classad/scanner.C
+++ src/condor_classad/scanner.C
@@ -38,18 +38,18 @@
 
 Token::Token()
 {
-	length = 0;
-	type = NOT_KEYWORD;
-	strVal = NULL;
-	isString = FALSE;
+	reset();
 }
 
-Token::~Token()
+void
+Token::reset()
 {
-	if(isString)
-	{
-		delete []strVal;
-	}
+	length = 0;
+	type = NOT_KEYWORD;
+	floatVal = 0.0;
+	intVal = 0;
+	strVal[0] = '\0';
+	isString = FALSE;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -58,7 +58,7 @@ Token::~Token()
 void Scanner(char*& s, Token& t)
 { 
     char	str[MAXVARNAME];
-    char	*tmp, *tmp2;		// working variables
+    char	*tmp;		// working variables
 
     // skip white space
     t.length = 0;
@@ -68,82 +68,109 @@ void Scanner(char*& s, Token& t)
 		t.length++;
     }
 
-	if(!strncasecmp(s, "ERROR", 5) && !isalpha(*(s+5)) && *(s+5) != '_')
+	tmp = str;
+
+    if(isalpha(*s) || *s == '_') 
 	{
-		s = s + 5;
-		t.length = t.length + 5;
-		t.type = LX_ERROR;
-		return;
-	}
-	if(!strncasecmp(s, "UNDEFINED", 9) && !isalpha(*(s+9)) && *(s+9) != '_')
-	{
-		s = s + 9;
-		t.length = t.length + 9;
-		t.type = LX_UNDEFINED;
-		return;
-	}
-    if(!strncasecmp(s, "TRUE", 4) && !isalpha(*(s+4)) && *(s+4) != '_')
-    // TRUE
-    {
-        s = s + 4;
-		t.length = t.length + 4;
-        t.intVal = 1;
-        t.type = LX_BOOL;
-		return;
-    }
-	if(!strncasecmp(s, "T", 1) && !isalpha(*(s+1)) && *(s+1) != '_')
-	// also TRUE
-	{
-		s = s + 1;
-		t.length = t.length + 1;
-		t.intVal = 1;
-		t.type = LX_BOOL;
-		return;
-	}
-    if(!strncasecmp(s, "FALSE", 5) && !isalpha(*(s+5)) && *(s+5) != '_')
-    // FALSE
-    {
-        s = s + 5;
-		t.length = t.length + 5;
-        t.intVal = 0;
-        t.type = LX_BOOL;
-		return;
-    }
-	if(!strncasecmp(s, "F", 1) && !isalpha(*(s+1)) && *(s+1) != '_')
-	// also FALSE
-	{
-		s = s + 1;
-		t.length = t.length + 1;
-		t.intVal = 0;
-		t.type = LX_BOOL;
-		return;
-	}
-    if(isalpha(*s) || *s == '_')
-    // token is a variable
-    {
-        tmp = str;
-        // prefix, if any.
-		if(!strncmp(s, "MY.", 3) && (isalpha(*(s+3)) || *(s+3) == '_'))
-		{
-			for(int i=0; i<3; i++)
+		// first check for keywords
+
+		switch (*s) {
+
+		case 'e':
+		case 'E':
+			if(!strncasecmp(s, "ERROR", 5) && !isalpha(*(s+5)) && *(s+5) != '_')
 			{
-				*tmp = *s;
-				s++;
-				tmp++;
-				t.length++;
+				s = s + 5;
+				t.length = t.length + 5;
+				t.type = LX_ERROR;
+				return;
 			}
-		}
-		else if(!strncmp(s, "TARGET.", 7) && (isalpha(*(s+7)) || *(s+7) == '_'))
-		{
-			for(int i=0; i<7; i++)
+			break;
+
+		case 'u':
+		case 'U':
+			if(!strncasecmp(s, "UNDEFINED", 9) && !isalpha(*(s+9)) && *(s+9) != '_')
 			{
-				*tmp = *s;
-				s++;
-				tmp++;
-				t.length++;
+				s = s + 9;
+				t.length = t.length + 9;
+				t.type = LX_UNDEFINED;
+				return;
 			}
-		}
-		// the rest of the variable.
+			break;
+
+		case 'T':
+			if(!strncmp(s, "TARGET.", 7) && (isalpha(*(s+7)) || *(s+7) == '_'))
+			{
+				for(int i=0; i<7; i++)
+				{
+					*tmp = *s;
+					s++;
+					tmp++;
+					t.length++;
+				}
+			}
+			// NOTE: no break, so we fall thru to 't' case.
+
+		case 't':
+			if(!strncasecmp(s, "TRUE", 4) && !isalpha(*(s+4)) && *(s+4) != '_')
+			// TRUE
+			{
+				s = s + 4;
+				t.length = t.length + 4;
+				t.intVal = 1;
+				t.type = LX_BOOL;
+				return;
+			}
+			if(!strncasecmp(s, "T", 1) && !isalpha(*(s+1)) && *(s+1) != '_')
+			// also TRUE
+			{
+				s = s + 1;
+				t.length = t.length + 1;
+				t.intVal = 1;
+				t.type = LX_BOOL;
+				return;
+			}
+			break;
+
+		case 'f':
+		case 'F':
+			if(!strncasecmp(s, "FALSE", 5) && !isalpha(*(s+5)) && *(s+5) != '_')
+			// FALSE
+			{
+				s = s + 5;
+				t.length = t.length + 5;
+				t.intVal = 0;
+				t.type = LX_BOOL;
+				return;
+			}
+			if(!strncasecmp(s, "F", 1) && !isalpha(*(s+1)) && *(s+1) != '_')
+			// also FALSE
+			{
+				s = s + 1;
+				t.length = t.length + 1;
+				t.intVal = 0;
+				t.type = LX_BOOL;
+				return;
+			}
+			break;
+
+		case 'M':
+			// prefix, if any.
+			if(!strncmp(s, "MY.", 3) && (isalpha(*(s+3)) || *(s+3) == '_'))
+			{
+				for(int i=0; i<3; i++)
+				{
+					*tmp = *s;
+					s++;
+					tmp++;
+					t.length++;
+				}
+			}
+			break;
+
+		}	// end of switch on first letter
+
+		// read in the rest of the variable.
         while(isalnum(*s) || *s == '_')
 		{ 
 			*tmp = *s;
@@ -152,13 +179,13 @@ void Scanner(char*& s, Token& t)
 			t.length++;
         }
         *tmp = '\0';
-        t.strVal = new char[strlen(str) + 1];
 		strcpy(t.strVal, str);
         t.type = LX_VARIABLE;
 		t.isString = TRUE;
 		return;
     }
-    if(isdigit(*s))
+    
+	if(isdigit(*s))
     // token is an integer or a floating point number
     {
 		// count the length of the number
@@ -184,11 +211,13 @@ void Scanner(char*& s, Token& t)
         }
 		return;
     }
+
     if(*s == '"')
     // token is a string
     {
         s++;
-		tmp2 = tmp = new char[strlen(s) + 1]; // make sure to have enough space
+		char tmp2[ATTRLIST_MAX_EXPRESSION];		
+		tmp = tmp2;
 		t.length++;
         while(*s != '"' && *s != '\0')
 		{
@@ -209,18 +238,15 @@ void Scanner(char*& s, Token& t)
         if(*s == '\0')
 		{
 			 t.type = LX_ERROR;
-			 t.length = 0;
-			 delete [] tmp2;
+			 t.length = 0;			 
 			 return;
 		}
         s++;
 		t.length++;
         *tmp = '\0';
-		t.strVal = new char[strlen(tmp2) + 1];
 		strcpy(t.strVal, tmp2);
         t.type = LX_STRING; 
 		t.isString = TRUE;
-		delete [] tmp2;
 		return;
     }
 
