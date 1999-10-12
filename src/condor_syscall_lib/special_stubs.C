af86c1499d17e10841c462cc7aeeb9cc9afb627d
psilord <psilord>
1999-10-12 19:12:52 +0000

    + Completed implementation of _condor_vfprintf_va().
    Supported things:
    %u, %d, %o, %x, %i, %c, %s, %p, %%, all escape characters except \ooo and \x.
    
    Unsupported things:
    %f, %X, %e, %E, %g, %G, the L and l modifiers, the position precision
    character '*', precision numbers, left justification, and printing
    leading zeros.
    
    I read the spec in the K&R book about what happens if you give a wierd
    conversion character, and it said "implementation defined". So I usually
    just copy the offending character into the output buffer.
    
    Please look in condor_includes/condor_debug.h to change how bug you wish
    the output buffer to be.
    
    + Added implementation of _condor_itoa(). This functions takes an int
    like quantity and a base, and converts it to an ascii representation.
    
    There were no libc functions called in the creation of this code. This is
    done to allow us to use this code ANYWHERE in checkpointing without regards
    to whether or not libc is currently mapped in or not. This is very important
    and must be preserved if the code gets modified for some reason.

--- src/condor_syscall_lib/special_stubs.C
+++ src/condor_syscall_lib/special_stubs.C
@@ -40,6 +40,11 @@ extern "C" {
 extern	int		DebugFlags;
 int		_condor_DebugFD = 0;
 
+static int _condor_vfprintf_va( int fd, char* fmt, va_list args );
+static int _condor_itoa(int quantity, char *out, int base);
+extern int SetSyscalls(int);
+extern int SYSCALL(int, ...);
+
 
 /*
   We need our own definition of my_ip_addr(), which is used by
@@ -105,16 +110,352 @@ _condor_dprintf_va( int flags, char* fmt, va_list args )
 int
 _condor_vfprintf_va( int fd, char* fmt, va_list args )
 {
-		/* 
-		   This is totally wrong.  It's just a stub that accomplishes
-		   the old functionality until this function is replaced by a
-		   real version that accomplishes the new goal. -Derek
-		*/
-	FILE* fp;
-	fp = fdopen( fd, "a" );
-	vfprintf( fp, fmt, args );
+	char out[VFPRINTF_LEN + 1];	/* the output buffer passed to SYS_write */
+	char *i, *o, *p; /* pointers into the fmt, out and %s, if any */
+	int c = 0;		/* number of output characters from conversion of fmt */
+	double f;		/* any float-like quantity from va_list */
+	int d;			/* any int-like quantity from va_list */
+	int index;		/* index usage in for loops */
+	int signedness;	/* is the int-like quantity signed or not? */
+	int val;		/* how many chars the ASCII rep is of a number */
+
+	/* how many digits you need to represent in ASCII base 10 a 64 bit 
+		quantity plus a minus sign and a null character. For those interested,
+		the equation is ceil(ln(numberinbase)/ln(base)) + minus sign + NULL.
+		If you are wondering why I did only 64, it is because the
+		values on the va_list are promoted to _at max_ that size. */
+	char d64[ASCII64DECIMAL];
+
+	/* how many ASCII digits in base 16 you need to represent a 64
+		bit number */
+	char x64[ASCII64HEXADECIMAL];
+
+	/* how many ASCII digits in base 8 you need to represent a 64 bit
+		number */
+	char o64[ASCII64OCTAL];
+
+
+	/* XXX I'm assuming I need to promote the types I use va_arg() on,
+		The ANSI C spec is a little unclear about this, but compiler tests
+		tell me that the promotion happens, even in "new-style" function
+		declarations */
+
+	for(index = 0; index < VFPRINTF_LEN+1; index++)
+	{
+		out[index] = 0;
+	}
+
+	i = fmt;
+	o = out;
+
+	while(*i != '\0' && c < VFPRINTF_LEN) /* minus one for the NULL */
+	{
+		/* if it isn't a conversion character delimiter, just copy it */
+		if (*i != '%' && *i != '\\')
+		{
+			*o++ = *i++;
+			c++;
+			continue;
+		}
+		else if (*i == '\\') /* must be an escape sequence */
+		{
+			switch(*++i)
+			{
+				case 'a':
+					*o = '\a';
+					break;
+				case 'b':
+					*o = '\b';
+					break;
+				case 'f':
+					*o = '\f';
+					break;
+				case 'n':
+					*o = '\n';
+					break;
+				case 'r':
+					*o = '\r';
+					break;
+				case 't':
+					*o = '\t';
+					break;
+				case 'v':
+					*o = '\v';
+					break;
+				case '\\':
+					*o = '\\';
+					break;
+				case '?':
+					*o = '?';
+					break;
+				case '\'':
+					*o = '\'';
+					break;
+				case '\"':
+					*o = '\"';
+					break;
+
+				/* XXX need to handle octal and hex numbers */
+
+				default:
+					/* not really correct behaviour, but close enough */
+					*o = *i;
+					break;
+			}
+			i++;
+			o++;
+			c++;
+			continue;
+		}
+
+		/* *i is a '%' at this point in the algorithm */
+
+		/* if it is a conversion character delimiter, then deal with it.
+			XXX skip all of the justifying and precision crap for now, if
+			we need it, we can add it later */
+		switch(*++i)
+		{
+			case 'c':
+				d = va_arg(args, int);
+				if (c < VFPRINTF_LEN)
+				{
+					*o++ = (char)d;
+					c++;
+				}
+				i++;
+				break;
+			case 's':
+				p = va_arg(args, char *);
+				while(*p != '\0' && c < VFPRINTF_LEN)
+				{
+					*o++ = *p++;
+					c++;
+				}
+				i++;
+				break;
+			case 'f':
+				f = va_arg(args, double);
+				i++;
+				break;
+			case 'p':
+			case 'x':
+				d = va_arg(args, int);
+				_condor_itoa(d, x64, HEXADECIMAL);
+				p = x64;
+				while(*p != '\0' && c < VFPRINTF_LEN)
+				{
+					*o++ = *p++;
+					c++;
+				}
+				i++;
+				break;
+			case 'o':
+				d = va_arg(args, int);
+				_condor_itoa(d, o64, OCTAL);
+				p = o64;
+				while(*p != '\0' && c < VFPRINTF_LEN)
+				{
+					*o++ = *p++;
+					c++;
+				}
+				i++;
+				break;
+			case 'i':
+			case 'd':
+				d = va_arg(args, int);
+				_condor_itoa(d, d64, DECIMAL);
+				p = d64;
+				while(*p != '\0' && c < VFPRINTF_LEN)
+				{
+					*o++ = *p++;
+					c++;
+				}
+				i++;
+				break;
+			case 'u':
+				d = va_arg(args, int);
+				_condor_itoa(d, d64, DECIMAL);
+				p = d64;
+				while(*p != '\0' && c < VFPRINTF_LEN)
+				{
+					*o++ = *p++;
+					c++;
+				}
+				i++;
+				break;
+			case '%':
+				*o++ = *i++;
+				c++;
+				break;
+			default: /* not really correct behaviour, but good enough */
+				*o++ = *i++;
+				c++;
+				break;
+		}
+	}
+
+	/* return the number of bytes actually written */
+	return SYSCALL(SYS_write, fd, out, c);
 }
 
+/* a helper function to _condor_vfprintf_va(). It will convert a binary
+	representation into an ASCII representation based upon the axes of
+	signed, unsigned, and hex, octal, decimal. If the base is
+	hex or octal, the signedness is ignored. */
+int
+_condor_itoa(int quantity, char *out, int base)
+{
+	int i;
+	char basemap[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a',
+						'b', 'c', 'd', 'e', 'f'};
+	unsigned int mask;
+	unsigned char byte, hi, lo;
+	unsigned int hexquant;
+	unsigned int octquant;
+	int numchars;
+	char *p, *q;
+	int div, sum, mod, neg;
+	
+	switch(base)
+	{
+		case HEXADECIMAL:
+			numchars = 0;
+			hexquant = (unsigned int)quantity;
+
+			mask = 0xff000000;
+
+			/* put the number into the out array, including leading zeros */
+			for(i = 0; i < sizeof(int); i++)
+			{
+				byte = (mask & hexquant) >> (sizeof(int)-i-1)*8;
+				out[i*2] = basemap[(byte&0xf0)>>4];
+				out[i*2+1] = basemap[byte&0x0f];
+				mask >>= 8;
+			}
+			out[8] = 0;
+
+			/* are there any leading zeros? */
+			p = out;
+			while(*p == '0') p++;
+
+			/* no leading zeros */
+			if (p == out)
+				return 8;
+
+			numchars = 8 - (p - out);
+
+			/* strip out the leading zeros */
+			q = out;
+			while(*p != '\0')
+			{
+				*q++ = *p++;
+			}
+			*q = 0;
+			return numchars;
+			break;
+
+		case OCTAL:
+			octquant = (unsigned int)quantity;
+			mask = 0xc0000000;
+			byte = (mask & octquant) >> 30;
+			out[0] = basemap[byte];
+			numchars = 1;
+
+			mask = 0x38000000;
+
+			for (i = 27; i >= 0; i-=3)
+			{
+				byte = (mask & octquant) >> i;
+				out[numchars++] = basemap[byte];
+				mask >>= 3;
+			}
+			out[numchars] = 0;
+
+			/* are there any leading zeros? */
+			p = out;
+			while(*p == '0') p++;
+
+			/* no leading zeros */
+			if (p == out)
+				return 8;
+
+			numchars = 8 - (p - out);
+
+			/* strip out the leading zeros */
+			q = out;
+			while(*p != '\0')
+			{
+				*q++ = *p++;
+			}
+			*q = 0;
+			return numchars;
+			
+			break;
+
+		case DECIMAL:
+			sum = 1;
+			numchars = 0;
+			
+			/* the simple case */
+			if (quantity == 0)
+			{
+				out[0] = '0';
+				out[1] = 0;
+				return 1;
+			}
+
+			/* create the ASCII representation backwards */
+			for (i = 1; i < 21; i++)
+			{
+				out[numchars] = 0;
+				div = quantity / sum;
+				sum *= 10;
+				mod = div % 10;
+				if (div == 0) break;
+				out[numchars] = basemap[ABS(mod)];
+				numchars++;
+			}
+			if (quantity < 0)
+			{
+				out[numchars++] = '-';
+			}
+			out[numchars] = 0;
+
+			/* now reverse the entire buffer */
+
+			/* find the start and end of the buffer */
+			p = q = out;
+			while(*q != '\0') q++;
+			q--;
+			
+			/* do the reversing operation, in place */
+			while(p != q && q > p)
+			{
+				/* swap two int-like quantities */
+				*p ^= *q;
+				*q ^= *p;
+				*p ^= *q;
+				*p++; *q--;
+			}
+
+			return numchars;
+			break;
+		default:
+			out[0] = 'N';
+			out[1] = '/';
+			out[2] = 'A';
+			out[3] = 0;
+			return 3;
+			break;
+	}
+
+	/* just in case I entered this function with garbage. */
+	out[0] = 'N';
+	out[1] = '/';
+	out[2] = 'A';
+	out[3] = 0;
+	return 3;
+}
 
 /* 
    This is called by the dprintf_init() linked in with the user job in
