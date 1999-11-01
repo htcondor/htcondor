8067d1c44ecfbb1b416dd15386de5b00dce46059
jbasney <jbasney>
1999-11-01 21:08:55 +0000

    added RemotePool, a job ClassAd attribute which contains the hostname of the negotiator which allowed a job to flock to the remote pool

--- src/condor_includes/condor_attributes.h
+++ src/condor_includes/condor_attributes.h
@@ -137,6 +137,7 @@ const char ATTR_Q_DATE                   [] = "QDate";
 const char ATTR_RANK					 [] = "Rank";
 const char ATTR_REAL_UID				 [] = "RealUid";
 const char ATTR_REMOTE_HOST				 [] = "RemoteHost";
+const char ATTR_REMOTE_POOL				 [] = "RemotePool";
 const char ATTR_REMOTE_USER              [] = "RemoteUser";
 const char ATTR_REMOTE_USER_PRIO         [] = "RemoteUserPrio";
 const char ATTR_REQUIREMENTS             [] = "Requirements";
