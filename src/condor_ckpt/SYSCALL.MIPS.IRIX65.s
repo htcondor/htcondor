21c741d9b18ba6da19fb801b317ba2dfeb209c1a
psilord <psilord>
2000-01-20 05:24:37 +0000

    Heh. Our extracted SYSCALL will try and call _cerror relative to itself, which is fine, if it lived in libc, but it doesn't. So if this call ever fails, a segfault will ensue. I fixed it, but at the cost of errno. This means errno is useless if this call detects a failure condition. However, it will return -1, so at least the code can detect a failure. Prolly should fix this under IRIX62 as well.

--- src/condor_ckpt/SYSCALL.MIPS.IRIX65.s
+++ src/condor_ckpt/SYSCALL.MIPS.IRIX65.s
@@ -1,25 +1,27 @@
-		.text
+/* This file has been rewritten to remove the fact that if the syscall failed
+	it would try and call _cerror relative to itself. errno now has no
+	meaning should this call fail. You will only get a -1 as a return 
+	value. If I have time, I'll come back and explore what I would need
+	to do to support an errno like thing.
+	-pete 01/11/00
+*/
+
+		#.file 1 "SYSCALL.MIPS.IRIX65.s"
+		.option pic2
+		.section .text
 		.align	2
 		.globl	SYSCALL
 		.ent	SYSCALL
 SYSCALL:
 		li	$2,1000
 		syscall 
+		nop
 		bnez	$7,SYSCALL_L1
-		nop 
+		nop
 		jr	$31
 		nop
 SYSCALL_L1:	
-		move	$24,$31
-		bal	SYSCALL_L2
-		nop
-SYSCALL_L2:	
-		lui	$28,8
-		addiu	$28,$28,17932
-		addu	$28,$28,$31
-		move	$31,$24
-		lw	$25,-29436($28)
-		nop 
-		jr	$25
+		li $2, -1
+		jr	$31
 		nop
 		.end	SYSCALL
