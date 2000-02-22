744e943a8b67fa1637369b1ad6db711930e81008
epaulson <epaulson>
2000-02-22 01:55:54 +0000

    Since the bloody CSL squashes mail, we'll just not use mail. dolog.pl now contacts raven directly, and there's now really a bonsai checkin daemon running on raven.

--- CVSROOT/dolog.pl
+++ CVSROOT/dolog.pl
@@ -236,23 +236,23 @@ sub do_commitinfo {
 
 sub get_response_code {
     my ($expecting) = @_;
-#     if ($flag_debug) {
-# 	print STDERR "SMTP: Waiting for code $expecting\n";
-#     }
+     if ($flag_debug) {
+ 	print STDERR "SMTP: Waiting for code $expecting\n";
+     }
     while (1) {
 	my $line = <S>;
-# 	if ($flag_debug) {
-# 	    print STDERR "SMTP: $line";
-# 	}
+ 	if ($flag_debug) {
+ 	    print STDERR "SMTP: $line";
+ 	}
 	if ($line =~ /^[0-9]*-/) {
 	    next;
 	}
 	if ($line =~ /(^[0-9]*) /) {
 	    my $code = $1;
 	    if ($code == $expecting) {
-# 		if ($flag_debug) {
-# 		    print STDERR "SMTP: got it.\n";
-# 		}
+ 		if ($flag_debug) {
+ 		    print STDERR "SMTP: got it.\n";
+ 		}
 		return;
 	    }
 	    die "Bad response from SMTP -- $line";
@@ -269,7 +269,7 @@ sub mail_notification {
     my ($remote,$port, $iaddr, $paddr, $proto, $line);
 
     $remote  = $mailhost;
-    $port    = 25;
+    $port    = 16;
     if ($port =~ /\D/) { $port = getservbyname($port, 'tcp') }
     die "No port" unless $port;
     $iaddr   = inet_aton($remote)               || die "no host: $remote";
@@ -280,17 +280,17 @@ sub mail_notification {
     connect(S, $paddr)    || die "connect: $!";
     select(S); $| = 1; select(STDOUT);
 
-    get_response_code(220);
-    print S "EHLO $hostname\n";
-    get_response_code(250);
-    print S "MAIL FROM: bonsai-daemon\@$hostname\n";
-    get_response_code(250);
-    foreach $i (@mailto) {
-	print S "RCPT TO: $i\n";
-	get_response_code(250);
-    }
-    print S "DATA\n";
-    get_response_code(354);
+    #get_response_code(220);
+    #print S "EHLO $hostname\n";
+    #get_response_code(250);
+    #print S "MAIL FROM: bonsai-daemon\@$hostname\n";
+    #get_response_code(250);
+    #foreach $i (@mailto) {
+	#print S "RCPT TO: $i\n";
+	#get_response_code(250);
+    #}
+    #print S "DATA\n";
+    #get_response_code(354);
     # Get one line starting with "354 ".
     if ($flag_tagcmd) {
 	print S "Subject:  cvs tag in $repository\n";
@@ -300,7 +300,7 @@ sub mail_notification {
     print S "\n";
     print S @outlist, "\n";
     print S ".\n";
-    get_response_code(250);
-    print S "QUIT\n";
+    #get_response_code(250);
+    #print S "QUIT\n";
     close(S);
 }
