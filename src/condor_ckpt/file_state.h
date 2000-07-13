3f2413f769b65c7e759acc6f1b096e6d859f7a38
psilord <psilord>
2000-07-13 21:30:54 +0000

    Added fstat(), lstat(), and stat() as to get the correct sizes of buffered files.

--- src/condor_ckpt/file_state.h
+++ src/condor_ckpt/file_state.h
@@ -210,6 +210,15 @@ public:
 	    perform a UNIX fsync/fdsync/fdatasync as appropriate. */
 	int	fsync( int fd );
 
+	/** Do a UNIX fstat() */
+	int fstat( int fd, struct stat *buf);
+
+	/** Do a UNIX stat() */
+	int stat( char *name, struct stat *buf);
+
+	/** Do a UNIX lstat() */
+	int lstat( char *name, struct stat *buf);
+
 	/** Return the max number of bytes to buffer per file */
 	int	get_buffer_size();
 
