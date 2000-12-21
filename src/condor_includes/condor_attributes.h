a812859ab027b00cd9e4a6c3314fd9c0b36780f4
wright <wright>
2000-12-21 21:53:53 +0000

    Added ATTR_REMOTE_OWNER

--- src/condor_includes/condor_attributes.h
+++ src/condor_includes/condor_attributes.h
@@ -162,6 +162,7 @@ const char ATTR_Q_DATE                   [] = "QDate";
 const char ATTR_RANK					 [] = "Rank";
 const char ATTR_REAL_UID				 [] = "RealUid";
 const char ATTR_REMOTE_HOST				 [] = "RemoteHost";
+const char ATTR_REMOTE_OWNER			 [] = "RemoteOwner";
 const char ATTR_REMOTE_POOL				 [] = "RemotePool";
 const char ATTR_REMOTE_USER              [] = "RemoteUser";
 const char ATTR_REMOTE_USER_PRIO         [] = "RemoteUserPrio";
