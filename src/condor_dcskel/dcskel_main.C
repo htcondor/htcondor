69bd3b00899dda2e800343382f3dbe1af0d92e54
psilord <psilord>
2001-02-15 23:01:23 +0000

    Initial revision

---
+++ src/condor_dcskel/dcskel_main.C
@@ -0,0 +1,51 @@
+#include "condor_common.h"
+#include "../condor_daemon_core.V6/condor_daemon_core.h"
+#include "condor_debug.h"
+
+/* Using daemoncore, you get the benefits of a logging system with dprintf
+	and you can read config files automatically. To start testing
+	your daemon, run it with "-f -t" until you start specifying
+	a config file to use(the daemon will automatically look in
+	/etc/condor_config, ~condor/condor_config, or the env var
+	CONDOR_CONFIG if -t is not specifed).  -f means run in the
+	foreground, -t means print the debugging output to the terminal.
+*/
+
+//-------------------------------------------------------------
+
+// about self
+char* mySubSystem = "DCSKEL";		// used by Daemon Core
+
+//-------------------------------------------------------------
+
+int main_init(int argc, char *argv[])
+{
+	dprintf(D_ALWAYS, "main_init() called\n");
+	return TRUE;
+}
+
+//-------------------------------------------------------------
+
+int main_config()
+{
+	dprintf(D_ALWAYS, "main_config() called\n");
+	return TRUE;
+}
+
+//-------------------------------------------------------------
+
+int main_shutdown_fast()
+{
+	dprintf(D_ALWAYS, "main_shutdown_fast() called\n");
+	DC_Exit(0);
+	return TRUE;	// to satisfy c++
+}
+
+//-------------------------------------------------------------
+
+int main_shutdown_graceful()
+{
+	dprintf(D_ALWAYS, "main_shutdown_graceful() called\n");
+	DC_Exit(0);
+	return TRUE;	// to satisfy c++
+}
