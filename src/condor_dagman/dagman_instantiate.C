cab0c7d115406be6d2421389a5f39f2fe07cd27a
pfc <pfc>
2001-03-21 03:15:05 +0000

    added SimpleList<Job*> template class for _readyQ

--- src/condor_dagman/dagman_instantiate.C
+++ src/condor_dagman/dagman_instantiate.C
@@ -2,9 +2,6 @@
 #include "dag.h"
 #include "job.h"
 #include "script.h"
-template class List<TQI>;
-template class Item<TQI>;
-template class ListIterator<TQI>;
 template class List<Job>;
 template class Item<Job>;
 template class ListIterator<Job>;
@@ -12,3 +9,4 @@ template class SimpleListIterator<JobID_t>;
 template class HashTable<int, Script*>;
 template class Queue<Script*>;
 template class Queue<Job*>;
+template class SimpleList<Job*>;
