d3887a44b16f8393e98a1ac0920c122c58d8410a
wright <wright>
2001-04-03 21:35:17 +0000

    Re-merged V6_2-branch as tagged at V6_2-merge-3 into V6_3-branch, now that all bandwidth control code is out of here.

--- src/condor_io/reli_sock.C
+++ src/condor_io/reli_sock.C
@@ -89,9 +89,18 @@ int
 ReliSock::listen()
 {
 	if (_state != sock_bound) return FALSE;
-	if (::listen(_sock, 5) < 0) return FALSE;
 
-	dprintf( D_NETWORK, "LISTEN %s\n", sock_to_string(_sock) );
+	// many modern OS's now support a >5 backlog, so we ask for 200,
+	// but since we don't know how they behave when you ask for too
+	// many, if 200 doesn't work we try 5 before returning failure
+	if( ::listen( _sock, 200 ) < 0 ) {
+		if( ::listen( _sock, 5 ) < 0 ) {
+			return FALSE;
+		}
+	}
+
+	dprintf( D_NETWORK, "LISTEN %s fd=%d\n", sock_to_string(_sock),
+			 _sock );
 
 	_state = sock_special;
 	_special_state = relisock_listen;
@@ -161,8 +170,14 @@ ReliSock::accept( ReliSock	&c )
 	int on = 1;
 	c.setsockopt(SOL_SOCKET, SO_KEEPALIVE, (char*)&on, sizeof(on));
 
-	dprintf( D_NETWORK, "ACCEPT %s ", sock_to_string(_sock) );
-	dprintf( D_NETWORK|D_NOHEADER, "%s\n", sin_to_string(c.endpoint()) );
+	if( DebugFlags & D_NETWORK ) {
+		char* src = strdup(	sock_to_string(_sock) );
+		char* dst = strdup( sin_to_string(c.endpoint()) );
+		dprintf( D_NETWORK, "ACCEPT src=%s fd=%d dst=%s\n",
+				 src, c._sock, dst );
+		free( src );
+		free( dst );
+	}
 	
 	return TRUE;
 }
