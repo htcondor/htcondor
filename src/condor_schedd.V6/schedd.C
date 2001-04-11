e42db11379961f62d693d2128cc6e8ec0630a5e8
wright <wright>
2001-04-11 14:54:03 +0000

    changed time(NULL) to time(0) to avoid compiler warnings.

--- src/condor_schedd.V6/schedd.C
+++ src/condor_schedd.V6/schedd.C
@@ -139,7 +139,7 @@ match_rec::match_rec(char* i, char* p, PROC_ID* id, ClassAd *match,
 	origcluster = cluster = id->cluster;
 	proc = id->proc;
 	status = M_UNCLAIMED;
-	entered_current_status = time(NULL);
+	entered_current_status = time( 0 );
 	shadowRec = NULL;
 	alive_countdown = 0;
 	num_exceptions = 0;
