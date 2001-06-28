b6f2bff77159462e53a39ad665c7174af4edee36
thain <thain>
2001-06-28 20:30:18 +0000

    Oops, fixed parameter in wrong position.

--- src/condor_c++_util/condor_query.C
+++ src/condor_c++_util/condor_query.C
@@ -335,7 +335,7 @@ fetchAds (ClassAdList &adList, const char *poolName)
 	}
 
 	// contact collector
-	Daemon my_collector(DT_COLLECTOR, NULL, pool);
+	Daemon my_collector(DT_COLLECTOR, pool, NULL);
 	if (!(sock = my_collector.startCommand(command, Stream::reli_sock, 0)) ||
 	    !queryAd.put (*sock) || !sock->end_of_message()) {
 
