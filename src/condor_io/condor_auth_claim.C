dac65cf244bd8aab9629718c2dd695e9d0a7dd9f
tannenba <tannenba>
2001-09-11 20:55:56 +0000

    include "condor_common.h"

--- src/condor_io/condor_auth_claim.C
+++ src/condor_io/condor_auth_claim.C
@@ -21,6 +21,8 @@
  * WI 53706-1685, (608) 262-0856 or miron@cs.wisc.edu.
 ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
 
+#include "condor_common.h"
+
 #if !defined(SKIP_AUTHENTICATION)
 
 #include "condor_auth_claim.h"
