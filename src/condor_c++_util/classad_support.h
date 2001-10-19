8b5995f1db7a28a8dcca6900a06e0860a7fcdc88
psilord <psilord>
2001-10-19 17:47:37 +0000

    added comments as to what the functions do.

--- src/condor_c++_util/classad_support.h
+++ src/condor_c++_util/classad_support.h
@@ -9,10 +9,19 @@
 
 extern char ATTR_DIRTY_ATTR_LIST [];
 
+/* marks the given attr in the classad as "dirty" */
 void SetAttrDirty(ClassAd *ad, char *attr);
+
+/* marks the given attr in the classad as "clean" */
 void SetAttrClean(ClassAd *ad, char *attr);
+
+/* returns true if given attr is dirty in the classad */
 bool IsAttrDirty(ClassAd *ad, char *attr);
+
+/* returns true if any attr is dirty in the classad */
 bool AnyAttrDirty(ClassAd *ad);
+
+/* Print out with specified dprintf mode the dirty list in the classad */
 void EmitDirtyAttrList(int mode, ClassAd *ad);
 
 #endif
