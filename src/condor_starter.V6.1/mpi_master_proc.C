690deed90dca9da61db912f6fbadbf9bacc9f04c
wright <wright>
2001-11-09 00:47:03 +0000

    Changes to support starting MPI on Windows w/o rsh: - Everything is controlled by #define MPI_USES_RSH, defined in   mpi_master_proc.h - Before the master node is spawned, the MPICH_EXTRA environment   variable is set to point to a file that is used to communicate the   port the master is listening on.  This file is created before we   spawn the root node, and is deleted in JobExit() before we transfer   files back to the user.  Once the master is spawned, we keep trying   to read the contents of this file until we either find the port, or   we exceed our max limit (defined in max_port_file_opens), in which   case we abort this job.

--- src/condor_starter.V6.1/mpi_master_proc.C
+++ src/condor_starter.V6.1/mpi_master_proc.C
@@ -23,35 +23,95 @@
 
 #include "condor_common.h"
 #include "mpi_master_proc.h"
+#include "starter.h"        // for proto of pseudo calls we use
 #include "condor_attributes.h"
 #include "condor_string.h"  // for strnewp
 #include "my_hostname.h"
 
+extern int main_shutdown_graceful();
+
 MPIMasterProc::MPIMasterProc( ClassAd * jobAd ) : MPIComradeProc( jobAd )
 {
+#if ! MPI_USES_RSH
+	port = 0;
+	port_check_tid = -1;
+	port_file = NULL;
+	num_port_file_opens = 0;
+		// Should we make either of these config file knobs?
+	max_port_file_opens = 10;
+	port_check_interval = 2;
+#endif
     dprintf ( D_FULLDEBUG, "Constructor of MPIMasterProc::MPIMasterProc\n" );
     dprintf ( D_PROTOCOL, "#4 - MPI MasterProc Started.\n" );
 }
 
 MPIMasterProc::~MPIMasterProc() {}
 
+
+#if ! MPI_USES_RSH
+// We only need our own version of this function if we're not using
+// rsh and have a port file to clean up.
+int 
+MPIMasterProc::JobExit( int pid, int status )
+{ 
+	dprintf(D_FULLDEBUG,"in MPIMasterProc::JobExit()\n");
+
+		// First, we've got to clean up the mpi port file, so that
+		// doesn't get transfered back to the user.
+	priv_state priv;
+	priv = set_user_priv();
+	unlink( port_file );
+	set_priv( priv );
+
+		// Now, just let our parent versions of this function do their
+		// magic. 
+    return VanillaProc::JobExit( pid, status );
+}
+#endif /* ! MPI_USES_RSH */
+
+
 int
-MPIMasterProc::StartJob() {
+MPIMasterProc::StartJob()
+{
+	int rval;
 
     dprintf ( D_FULLDEBUG, "MPIMasterProc::StartJob()\n" );
+
+#if MPI_USES_RSH
 
     if ( !alterEnv() ) {
         return FALSE;
     }
-
     dprintf ( D_PROTOCOL, "#5 - altered job environment, starting master:\n" );
 
+#else
+
+	preparePortFile();
+    dprintf ( D_PROTOCOL, "#5 - created port file, starting master:\n" );
+
+#endif /* ! MPI_USES_RSH */
+
         // The master starts like the comrades...
-    return MPIComradeProc::StartJob();
+	rval = MPIComradeProc::StartJob();
+
+#if ! MPI_USES_RSH
+		// now, we've got to check for the port file, grab the port,
+		// and send that back to the shadow as a pseudo syscall so it
+		// can spawn the rest of the workers.  If the port file isn't
+		// yet ready, this function will set a timer so we keep
+		// checking until we find it.
+	checkPortFile();
+#endif 
+
+	return rval;
 }
 
+
+#if MPI_USES_RSH
+
 int
-MPIMasterProc::alterEnv() {
+MPIMasterProc::alterEnv()
+{
 /* This routine is here in the starter because only here do we know 
    if there is a PATH environment variable set.  This is important
    if there is no Env in the JobAd, or there is no PATH in said
@@ -159,3 +219,140 @@ MPIMasterProc::alterEnv() {
 
     return 1;
 }
+
+#else /* ! MPI_USES_RSH */
+
+bool
+MPIMasterProc::preparePortFile( void )
+{
+	int fd;
+
+		// First, construct the appropriate filename to use.  Luckily
+		// for us here, the temporary execute directory has already
+		// been created, and it's already our cwd.  So, we can just
+		// use a filename of our choosing, and it'll already be unique
+		// across all VMs, it'll automatically get deleted, etc.
+	port_file = strdup( "mpi_port_file" );
+
+		// To create this file, we've got to be in the user's priv
+		// state. 
+	priv_state priv;
+	priv = set_user_priv();
+
+		// For this stuff to work, we've got to create the file and
+		// have it be 0 bytes.
+	fd = open( port_file, O_WRONLY|O_CREAT|O_TRUNC, 0666 );
+	if( fd < 0 ) {
+		dprintf( D_ALWAYS, "ERROR: Can't create port file (%s)\n",
+				 port_file );
+		return false;
+	}
+		// if that worked, we can just close the file, since we don't
+		// want to put anything in it, yet.
+	close( fd );
+
+		// We're done being the user...
+	set_priv( priv );
+
+		// Now that the file exists, we also need to hack the
+		// environment of our job ad so we set the right env var so
+		// MPICH puts the port in here.
+
+		// Eventually, we're going to need to reinsert this as a
+		// ClassAd attribute, so we might as well initialize it with 
+		// 'ATTR_JOB_ENVIRONMENT = "'...
+	char buf[1024];
+	sprintf( buf, "%s = \"", ATTR_JOB_ENVIRONMENT );
+	MyString env( buf );
+
+		// Now, grab the existing environment.  Do so without static
+		// buffers so we can handle really huge environments
+		// correctly.  
+	char* env_str = NULL;
+	if( JobAd->LookupString(ATTR_JOB_ENVIRONMENT, &env_str) ) {
+		env += env_str;
+		free( env_str );
+	} else {
+			// Maybe this is a little harsh, but it should never 
+			// happen.   
+		EXCEPT( "MPI Master node started w/o an environment!" );
+	}
+	sprintf( buf, "%cMPICH_EXTRA=%s\"", env_delimiter, port_file );
+	env += buf;
+
+	if( JobAd->Insert(env.GetCStr()) ) {
+		return true;
+	} 
+	return false;
+}
+
+
+
+bool
+MPIMasterProc::checkPortFile( void )
+{
+		// First, see if the file's got anything tasty in it. 
+	FILE* fp;
+	char buf[100];
+	char* rval;
+
+	fp = fopen( port_file, "r" );
+	if( fp ) {
+			// check if there's anything there...
+		rval = fgets( buf, 100, fp );
+		if( rval ) {
+				// cool, we read something.  stash the port in our
+				// local variable
+			port = atoi( buf );
+
+				// At this point, we can actually do our pseudo
+				// syscall to tell the shadow.  First, create the
+				// string we need: 
+			sprintf( buf, "%s:%d", inet_ntoa(*(my_sin_addr())), port );
+
+				// Now, do the call:
+			REMOTE_CONDOR_register_mpi_master_info( buf );
+
+				// clear our tid (since we're not going to reset the
+				// timer) 
+			port_check_tid = -1;
+
+				// we're done.
+			return true;
+		}
+	} else {
+			// Couldn't even open the file
+		num_port_file_opens++;
+		if( num_port_file_opens >= max_port_file_opens ) {
+			dprintf( D_ALWAYS, "ERROR: Can't open %s after %d attempts, "
+					 "aborting", port_file, num_port_file_opens );
+			main_shutdown_graceful();
+		} else {
+			dprintf( D_FULLDEBUG, "WARNING: Can't open %s, will try again\n", 
+					 port_file ); 
+		}
+	}
+				 
+		// if we got here, either: we couldn't open the file (and
+		// we're willing to keep trying), or we opened it and there
+		// was nothing in there.  so, we need to set a timer to call
+		// ourselves again in a few seconds...  if we've already done
+		// this already, all we have to do is reset the timer.  
+	if( port_check_tid >= 0 ) {
+			// we've already got a timer, just reset it.
+		daemonCore->Reset_Timer( port_check_tid, port_check_interval, 0 );
+		return false;
+	}
+		// if we get here, it means this is our first time, and we
+		// need to register our timer...
+	port_check_tid = daemonCore->
+		Register_Timer( port_check_interval, 0, 
+						(TimerHandlercpp)&MPIMasterProc::checkPortFile,
+						"MPIMasterProc::checkPortFile", this );
+	if( port_check_tid < 0 ) {
+		EXCEPT( "Can't register DaemonCore Timer!" );
+	}
+	return false;
+}
+
+#endif /* ! MPI_USES_RSH */
