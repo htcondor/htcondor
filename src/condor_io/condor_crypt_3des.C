d3d64bf127d6069b6f3f533b1409cb6de03a273a
zmiller <zmiller>
2001-11-15 22:21:37 +0000

    Added condor_common.h as the first include.

--- src/condor_io/condor_crypt_3des.C
+++ src/condor_io/condor_crypt_3des.C
@@ -21,6 +21,7 @@
  * WI 53706-1685, (608) 262-0856 or miron@cs.wisc.edu.
 ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
 
+#include "condor_common.h"
 #include "condor_crypt_3des.h"
 #include <malloc.h>
 #include <string.h>
