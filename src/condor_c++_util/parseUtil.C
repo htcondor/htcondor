d30f21a24056eb48cb87489ead8a99cfbaa7749f
ray <ray>
2001-11-20 22:00:15 +0000

    *** empty log message ***

---
+++ src/condor_c++_util/parseUtil.C
@@ -0,0 +1,21 @@
+#include "condor_common.h"
+#include "parseUtil.h"
+
+void breakStringByDelimiter(StringList &toAppendTo, const MyString &strToBreak, char cDelimiter)
+{
+	int iStartPos = 0;
+	int iDelimPos;
+	int iLength = strToBreak.Length();
+	while (iStartPos < iLength)
+	{
+		iDelimPos = strToBreak.FindChar(cDelimiter, iStartPos);
+		if (iDelimPos == -1)
+		{
+			toAppendTo.append(strToBreak.Substr(iStartPos, strToBreak.Length()-1).GetCStr());
+			break;
+		}	
+
+		toAppendTo.append(strToBreak.Substr(iStartPos, iDelimPos-1).GetCStr());
+		iStartPos = iDelimPos+1;
+	} 
+}
