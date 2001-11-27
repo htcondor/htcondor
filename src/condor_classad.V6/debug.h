2949ba4dfb1cdfa45165838584beb05d4697cd16
roy <roy>
2001-11-27 01:59:47 +0000

    First check-in. This is a clean-room implementation of EXCEPT. It is only used in the ClassAd distribution.

---
+++ src/condor_classad.V6/debug.h
@@ -0,0 +1,41 @@
+/*********************************************************************
+ *
+ * Condor ClassAd library
+ * Copyright (C) 1990-2001, CONDOR Team, Computer Sciences Department,
+ * University of Wisconsin-Madison, WI, and Rajesh Raman.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of version 2.1 of the GNU Lesser General
+ * Public License as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *********************************************************************/
+
+#ifndef __CLASSAD_DEBUG_H
+#define __CLASSAD_DEBUG_H
+
+#include "common.h"
+
+BEGIN_NAMESPACE( classad );
+
+#define EXCEPT \
+    _except_line_number = __LINE__, \
+    _except_file_name   = __FILE__, \
+    _classad_except
+
+extern int _except_line_number;
+extern char *_except_file_name;
+void _classad_except(char *format, ...);
+
+END_NAMESPACE
+
+#endif /* __CLASSAD_DEBUG_H */
