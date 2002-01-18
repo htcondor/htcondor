16814f35a1f32760b8bb460561e6e15a9cfb5b9c
thain <thain>
2002-01-18 00:20:46 +0000

    For backwards compatibility with old history files -- do not barf if REMOTE_WALL_CLOCK_TIME is missing.

--- src/condor_tools/history.C
+++ src/condor_tools/history.C
@@ -162,11 +162,16 @@ displayJobShort(AttrList* ad)
         float utime;
         char owner[64], cmd[2048], args[2048];
 
+	if(!ad->EvalFloat(ATTR_JOB_REMOTE_WALL_CLOCK,NULL,utime)) {
+		if(!ad->EvalFloat(ATTR_JOB_REMOTE_USER_CPU,NULL,utime)) {
+			utime = 0;
+		}
+	}
+
         if (!ad->EvalInteger (ATTR_CLUSTER_ID, NULL, cluster)           ||
                 !ad->EvalInteger (ATTR_PROC_ID, NULL, proc)             ||
                 !ad->EvalInteger (ATTR_Q_DATE, NULL, date)              ||
                 !ad->EvalInteger (ATTR_COMPLETION_DATE, NULL, CompDate)	||
-                !ad->EvalFloat   (ATTR_JOB_REMOTE_WALL_CLOCK, NULL, utime)||
                 !ad->EvalInteger (ATTR_JOB_STATUS, NULL, status)        ||
                 !ad->EvalInteger (ATTR_JOB_PRIO, NULL, prio)            ||
                 !ad->EvalInteger (ATTR_IMAGE_SIZE, NULL, image_size)    ||
