03fab839a0fc07d819d345aca75102f0d2f495f3
wright <wright>
2002-01-23 00:03:14 +0000

    added REMOTE_CONDOR_get_user_info(), which asks the shadow for information about the user which isn't already in the job ad (for now, this is just the numeric uid + gid).

--- src/condor_starter.V6.1/NTsenders.C
+++ src/condor_starter.V6.1/NTsenders.C
@@ -98,6 +98,36 @@ REMOTE_CONDOR_get_job_info(ClassAd *ad)
 	return rval;
 }
 
+
+int
+REMOTE_CONDOR_get_user_info(ClassAd *ad)
+{
+	condor_errno_t		terrno;
+	int		rval=-1;
+
+	dprintf ( D_SYSCALLS, "Doing CONDOR_get_user_info\n" );
+
+	CurrentSysCall = CONDOR_get_user_info;
+
+	syscall_sock->encode();
+	assert( syscall_sock->code(CurrentSysCall) );
+	assert( syscall_sock->end_of_message() );
+
+	syscall_sock->decode();
+	assert( syscall_sock->code(rval) );
+	if( rval < 0 ) {
+		assert( syscall_sock->code(terrno) );
+		assert( syscall_sock->end_of_message() );
+		errno = terrno;
+		dprintf ( D_SYSCALLS, "Return val problem, errno = %d\n", errno );
+		return rval;
+	}
+	assert( ad->initFromStream(*syscall_sock) );
+	assert( syscall_sock->end_of_message() );
+	return rval;
+}
+
+
 #if 0
 int
 REMOTE_CONDOR_get_executable(char *destination)
