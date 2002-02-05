6eecf2c12b9f0fa4adb9b13edb1e54c90bdbf5d8
wright <wright>
2002-02-05 02:34:54 +0000

    added the first derived child class of Daemon, DCShadow.  This shows you how you can derive classes to implement daemon-specific interfaces in addition to getting all the goodness from Daemon.  At first, this just implements the "updateJobInfo()" method that the starter will use to send udp updates back to the shadow.  we'll add more later.

---
+++ src/condor_daemon_client/dc_shadow.C
@@ -0,0 +1,139 @@
+/***************************Copyright-DO-NOT-REMOVE-THIS-LINE**
+ * CONDOR Copyright Notice
+ *
+ * See LICENSE.TXT for additional notices and disclaimers.
+ *
+ * Copyright (c)1990-1998 CONDOR Team, Computer Sciences Department, 
+ * University of Wisconsin-Madison, Madison, WI.  All Rights Reserved.  
+ * No use of the CONDOR Software Program Source Code is authorized 
+ * without the express consent of the CONDOR Team.  For more information 
+ * contact: CONDOR Team, Attention: Professor Miron Livny, 
+ * 7367 Computer Sciences, 1210 W. Dayton St., Madison, WI 53706-1685, 
+ * (608) 262-0856 or miron@cs.wisc.edu.
+ *
+ * U.S. Government Rights Restrictions: Use, duplication, or disclosure 
+ * by the U.S. Government is subject to restrictions as set forth in 
+ * subparagraph (c)(1)(ii) of The Rights in Technical Data and Computer 
+ * Software clause at DFARS 252.227-7013 or subparagraphs (c)(1) and 
+ * (2) of Commercial Computer Software-Restricted Rights at 48 CFR 
+ * 52.227-19, as applicable, CONDOR Team, Attention: Professor Miron 
+ * Livny, 7367 Computer Sciences, 1210 W. Dayton St., Madison, 
+ * WI 53706-1685, (608) 262-0856 or miron@cs.wisc.edu.
+****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
+
+#include "condor_common.h"
+#include "condor_debug.h"
+#include "condor_config.h"
+#include "condor_string.h"
+#include "condor_ver_info.h"
+#include "condor_attributes.h"
+
+#include "daemon.h"
+#include "dc_shadow.h"
+#include "internet.h"
+
+
+DCShadow::DCShadow( const char* name ) : Daemon( DT_SHADOW, name, NULL )
+{
+	is_initialized = false;
+	shadow_safesock = NULL;
+}
+
+
+DCShadow::~DCShadow( void )
+{
+	if( shadow_safesock ) {
+		delete shadow_safesock;
+	}
+}
+
+
+bool
+DCShadow::initFromClassAd( ClassAd* ad )
+{
+	char* tmp = NULL;
+
+	if( ! ad ) {
+		dprintf( D_ALWAYS, 
+				 "ERROR: DCShadow::initFromClassAd() called with NULL ad\n" );
+		return false;
+	}
+
+	ad->LookupString( ATTR_SHADOW_IP_ADDR, &tmp );
+	if( ! tmp ) {
+			// If that's not defined, try ATTR_MY_ADDRESS
+		ad->LookupString( ATTR_MY_ADDRESS, &tmp );
+	}
+	if( ! tmp ) {
+		dprintf( D_FULLDEBUG, "ERROR: DCShadow::initFromClassAd(): "
+				 "Can't find shadow address in ad\n" );
+		return false;
+	} else {
+		if( is_valid_sinful(tmp) ) {
+			New_addr( strnewp(tmp) );
+			is_initialized = true;
+		} else {
+			dprintf( D_FULLDEBUG, 
+					 "ERROR: DCShadow::initFromClassAd(): invalid %s in ad (%s)\n", 
+					 ATTR_SHADOW_IP_ADDR, tmp );
+		}
+		free( tmp );
+		tmp = NULL;
+	}
+
+	if( ad->LookupString(ATTR_SHADOW_VERSION, &tmp) ) {
+		New_version( strnewp(tmp) );
+		free( tmp );
+		tmp = NULL;
+	}
+
+	return is_initialized;
+}
+
+
+bool
+DCShadow::locate( void )
+{
+	return is_initialized;
+}
+
+
+bool
+DCShadow::updateJobInfo( ClassAd* ad )
+{
+	if( ! ad ) {
+		dprintf( D_FULLDEBUG, 
+				 "DCShadow::updateJobInfo() called with NULL ClassAd\n" );
+		return false;
+	}
+
+	if( ! shadow_safesock ) {
+		shadow_safesock = new SafeSock;
+		shadow_safesock->connect( _addr );
+	}
+
+	Sock* tmp;
+	tmp = startCommand( SHADOW_UPDATEINFO, 	(Sock*)shadow_safesock );
+	if( ! tmp ) {
+		dprintf( D_FULLDEBUG, 
+				 "Failed to send SHADOW_UPDATEINFO command to shadow\n" );
+		delete shadow_safesock;
+		shadow_safesock = NULL;
+		return false;
+	}
+	if( ! ad->put(*tmp) ) {
+		dprintf( D_FULLDEBUG, 
+				 "Failed to send SHADOW_UPDATEINFO ClassAd to shadow\n" );
+		delete shadow_safesock;
+		shadow_safesock = NULL;
+		return false;
+	}
+	if( ! tmp->end_of_message() ) {
+		dprintf( D_FULLDEBUG, 
+				 "Failed to send SHADOW_UPDATEINFO EOM to shadow\n" );
+		delete shadow_safesock;
+		shadow_safesock = NULL;
+		return false;
+	}
+	return true;
+}
