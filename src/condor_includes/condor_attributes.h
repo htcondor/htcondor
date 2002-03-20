44033273c973e95383ffd519e3f78c70a0baf6ec
wright <wright>
2002-03-20 21:05:56 +0000

    added ATTR_REMOVE_REASON

--- src/condor_includes/condor_attributes.h
+++ src/condor_includes/condor_attributes.h
@@ -206,6 +206,7 @@ const char ATTR_REMOTE_USER              [] = "RemoteUser";
 const char ATTR_REMOTE_USER_PRIO         [] = "RemoteUserPrio";
 const char ATTR_REMOTE_VIRTUAL_MACHINE_ID [] = "RemoteVirtualMachineID";
 const char ATTR_REMOVE_KILL_SIG          [] = "RemoveKillSig";
+const char ATTR_REMOVE_REASON            [] = "RemoveReason";
 const char ATTR_REQUIREMENTS             [] = "Requirements";
 const char ATTR_RSC_BYTES_SENT			 [] = "RSCBytesSent";
 const char ATTR_RSC_BYTES_RECVD			 [] = "RSCBytesRecvd";
