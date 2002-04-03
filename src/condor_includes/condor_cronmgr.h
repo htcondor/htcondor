e54dd7bc83f06d1fb081da958b70a79e462376fa
nleroy <nleroy>
2002-04-03 19:53:09 +0000

    Added "cron" classes to run things periodically; specifically targetted at Hawkeye (in the startd), but designed to be more generic than that.

---
+++ src/condor_includes/condor_cronmgr.h
@@ -0,0 +1,55 @@
+/***************************Copyright-DO-NOT-REMOVE-THIS-LINE**
+ * CONDOR Copyright Notice
+ *
+ * See LICENSE.TXT for additional notices and disclaimers.
+ *
+ * Copyright (c)1990-1998 CONDOR Team, Computer Sciences Department, 
+ * University of Wisconsin-Madison, Madison, WI.  All Rights Reserved.  
+ * No use of the CONDOR Software Program Source Code is authorized 
+ * without the express consent of the CONDOR Team.  For more information 
+ * contact: CONDOR Team, Attention: Professor Miron Livny, 
+ * 7367 Computer Sciences, 1210 W. Dayton St., Madison, WI 53706-1685, 
+ * (608) 262-0856 or miron@cs.wisc.edu.
+ *
+ * U.S. Government Rights Restrictions: Use, duplication, or disclosure 
+ * by the U.S. Government is subject to restrictions as set forth in 
+ * subparagraph (c)(1)(ii) of The Rights in Technical Data and Computer 
+ * Software clause at DFARS 252.227-7013 or subparagraphs (c)(1) and 
+ * (2) of Commercial Computer Software-Restricted Rights at 48 CFR 
+ * 52.227-19, as applicable, CONDOR Team, Attention: Professor Miron 
+ * Livny, 7367 Computer Sciences, 1210 W. Dayton St., Madison, 
+ * WI 53706-1685, (608) 262-0856 or miron@cs.wisc.edu.
+****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
+#ifndef _CONDOR_CRONMGR_H
+#define _CONDOR_CRONMGR_H
+
+#include "condor_common.h"
+#include "../condor_daemon_core.V6/condor_daemon_core.h"
+#include "condor_cron.h"
+
+// Define a simple class to run child tasks periodically.
+class CondorCronMgr
+{
+  public:
+	CondorCronMgr( const char *name );
+	virtual ~CondorCronMgr( );
+	int Reconfig( void );
+
+  protected:
+	virtual CondorCronJob *NewJob( const char *name );
+
+  private:
+	CondorCron	Cron;
+	const char	*Name;
+	const char	*Args;
+	int			NameLen;
+
+	// Private member functions
+	int ParseJobList( const char *JobListString );
+	char *GetParam( const char *paramName, 
+					const char *paramSep = NULL,
+					const char *paramName2 = NULL,
+					const char *paramName3 = NULL );
+};
+
+#endif /* _CONDOR_CRONMGR_H */
