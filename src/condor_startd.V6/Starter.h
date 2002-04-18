d27f8ade4d45518d64286c68bcfc7099a9427aa4
wright <wright>
2002-04-18 22:55:08 +0000

    Lots of changes related the starter ClassAds: - the constructor no longer takes in a pointer to a Resource, we deal   with that in setResource() now. - added a copy constructor for use *before* the starter is spawned. - changed name() to path() to be more specific about what it is. - removed all the type related alternate_starter_2 crap.  now, all the   useful info about this Starter object is set via setPath(), setAd()   and setIsDC(), which are used by the StarterMgr class - added satisfies() method, which is passed a job ad and a resource   ad.  the Starter then merges the resource ad w/ its own ad and tries   to satisfy the job's requirements expression. - added a provides() method which looks up the given ability in the   starter's ClassAd and sees if it's true. - added a publish() method that prints all of this starter's abilities   into the given ad, and their names into the given StringList. - re-organized the private data so it's clearer what data is valid   before the starter is spawned, and what is only used once running.

--- src/condor_startd.V6/Starter.h
+++ src/condor_startd.V6/Starter.h
@@ -42,42 +42,66 @@ typedef struct jobstartinfo {
 class Starter
 {
 public:
-	Starter( Resource* );
+	Starter();
+	Starter( const Starter& s );
 	~Starter();
+
 
 	void	dprintf( int, char* ... );
 
-	char*	name() {return s_name;};
+	char*	path() {return s_path;};
 	time_t	birthdate( void ) {return s_birthdate;};
 	time_t	last_snapshot( void ) {return s_last_snapshot;};
-	int		settype(int);
 	int		kill(int);
 	int		killpg(int);
 	void	killkids(int);
 	int		spawn( start_info_t*, time_t );
 	void	exited();
 	pid_t	pid() {return s_pid;};
-	int		is_dc() {return s_is_dc;};
+	bool	is_dc() {return s_is_dc;};
 	bool	active();
 	pid_t*	pidfamily() {return s_pidfamily;};
 	int		pidfamily_size() {return s_family_size;};
 	void	recompute_pidfamily( time_t now = 0 );
 	void	set_last_snapshot( time_t val ) { s_last_snapshot = val; };
 
+	void	publish( ClassAd* ad, amask_t mask, StringList* list );
+
+	bool	satisfies( ClassAd* job_ad, ClassAd* mach_ad );
+	bool	provides( const char* ability );
+
+	void	setAd( ClassAd* ad );
+	void	setPath( const char* path );
+	void	setIsDC( bool is_dc );
+
+	void	setResource( Resource* rip );
+
+	void	printInfo( int debug_level );
+
 private:
-	Resource*	rip;
-	pid_t	s_pid;
-	char*	s_name;
+
+		// methods
 	int		reallykill(int, int);
 	int		exec_starter(char*, char*, int, int);
 	int		exec_starter(char*, char*, Stream*);
+	void	initRunData( void );
+
+		// data that will be the same across all instances of this
+		// starter (i.e. things that are valid for copying)
+	ClassAd* s_ad;
+	char*	s_path;
+	bool 	s_is_dc;
+
+		// data that only makes sense once this Starter object has
+		// been assigned to a given resource and spawned.
+	Resource*	rip;
+	pid_t	s_pid;
 	pid_t*	s_pidfamily;
 	int		s_family_size;
 	ProcFamily*	s_procfam;
-	int 	s_type;
-	int 	s_is_dc;
 	time_t	s_birthdate;
 	time_t	s_last_snapshot;
+
 };
 
 #endif /* _CONDOR_STARTD_STARTER_H */
