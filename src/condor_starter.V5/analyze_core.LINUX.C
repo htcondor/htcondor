f884a37f4873726f66e5c70a3b219502b8d9f089
psilord <psilord>
2002-04-23 21:53:16 +0000

    Ripped out bfd usage and even checking for the validity of the core beyond existance. I think we were just being too smart here. If the OS gives us a core file, we will ship it back to the user corrupt or not. Let them figure it out corrution or not.

--- src/condor_starter.V5/analyze_core.LINUX.C
+++ src/condor_starter.V5/analyze_core.LINUX.C
@@ -31,49 +31,31 @@ Purpose:
 	This code requires a *correct* libbfd, liberty, bfd.h and ansidecl.h.
 	The code has been tested with libbfd.so.2.6.0.12.  Correct versions
 	of the heasers and libraries should be included with this distribution.
+
+Addendum:
+	Ripped out libbfd usage. Also, don't even check the core for validity
+	beyond its existance. Let the user figure it out when they 
+	use gdb on it. -psilord 4/22/2002
 ******************************************************************/
 
 #include "condor_common.h"
 #include "condor_debug.h"
 #include "condor_constants.h"
-#include <sys/file.h>
-#include <sys/user.h>
-#include <bfd.h>
-
 
 int
 core_is_valid( char *core_fn )
 {
-	bfd		*bfdp;
-	long	storage_needed;
+	struct stat buf;
 
-
-	dprintf( D_ALWAYS,
-		"Analyzing core file \"%s\" for existence and completeness\n", core_fn
-	);
-
-	// We only check the header of the core.  Most cores will be complete
-	// if they exist at all (unless the disk fills).  In any case, my
-	// opinion is that any core is better than nothing!  As long as the
-	// header is intact, gdb should try to read it and give the user
-	// some feedback. -- Greger
-	bfd_init();
-	if((bfdp=bfd_openr(core_fn, 0))!=NULL) {
-		dprintf(D_ALWAYS, "\n\nbfdopen(%s) succeeded\n", core_fn);
-		// Check if it is a core.
-		if(bfd_check_format(bfdp, bfd_core)) {
-			dprintf(D_ALWAYS, "File type=bfd_core\n");
-			bfd_close(bfdp);
-			return TRUE;
-		} else {
-			dprintf(D_ALWAYS, "File %s is not a valid core file\n", core_fn);
-			bfd_close(bfdp);
-			return FALSE;
-		}
-	} else {
-		dprintf(D_ALWAYS, "bfdopen(%s) failed\n", core_fn);
-		return -1;
+	if (stat(core_fn, &buf) < 0)
+	{
+		return FALSE;
 	}
+
+	/* if the OS gave us something, we should give it to the user and let
+		THEM figure out if it was corrupt or not. */
+
+	return TRUE;
 }
 
 
