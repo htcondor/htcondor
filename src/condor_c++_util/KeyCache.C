416c08e74b1a4a420a181aacd30c195b45615dfe
zmiller <zmiller>
2002-05-16 21:21:43 +0000

    fixed memory leak in remove

--- src/condor_c++_util/KeyCache.C
+++ src/condor_c++_util/KeyCache.C
@@ -203,7 +203,7 @@ void KeyCache::delete_storage()
 bool KeyCache::insert(KeyCacheEntry &e) {
 
 	// the key_table member is a HashTable which maps
-	// MyString's to KeyCacheEntry's.  (note the '*')
+	// MyString's to KeyCacheEntry*'s.  (note the '*')
 
 	// the map_table member is a HashTable which maps
 	// MyString's to MyString's.
@@ -258,18 +258,24 @@ bool KeyCache::lookup(const char *key_id, KeyCacheEntry *&e_ptr) {
 
 bool KeyCache::remove(const char *key_id) {
 	// to remove a key:
-	// you first need to do a lookup.  part of the KeyCacheEntry
-	// then tells you ALL key_id's that this key was inserted under.
+	// you first need to do a lookup, so we can get the pointer to delete.
+	KeyCacheEntry *tmp_ptr = NULL;
 
-	// NOTE: HashTable's remove returns ZERO on SUCCESS!!!
-	return (key_table->remove(key_id) == 0);
+	// NOTE: HashTable's lookup returns ZERO on SUCCESS
+	bool res = (key_table->lookup(key_id, tmp_ptr) == 0);
+
+	if (res) {
+		// ** HEY **
+		// key_id could be pointing to the string tmp_ptr->id.  so, we'd
+		// better finish using key_id *before* we delete tmp_ptr.
+		res = (key_table->remove(key_id) == 0);
+		delete tmp_ptr;
+	}
+
+	return res;
 }
 
 void KeyCache::expire(KeyCacheEntry *e) {
-	// ** HEY **
-	// the pointer they passed in could be pointing
-	// to the string within this object, so we need
-	// to keep the info we want before we delete it
 	char* key_id = strdup (e->id());
 	time_t key_exp = e->expiration();
 
