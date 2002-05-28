73f3e4e0bccd0b1d54ef8b0b99bf878fc60b3903
tannenba <tannenba>
2002-05-28 19:32:33 +0000

    Fixed many bugs.   Behold:
    + when writing to the user log, don't just flush stdio buffers... actually
    sync to disk before releasing the write lock.
    
    + when reading from the user log, for some reason we obtain a write
    lock on the log file.  however, we open the file O_RDONLY (because,
    after all, we do just read from it).  however, on RedHat 7.2, the
    kernel will never block on a *write* lock if the file was opened
    read-only.  thus the user log code attempted to read from the log
    file at the same time when a potential record was being written.  the
    current fix is to open the file O_RDWR so locking works on Linux.
    however, i think soon we will change this write lock to a read lock...
    
    + method readEvent() used to block for one second before returning
    ULOG_NO_EVENT.  now it returns right away if there is nothing to read.
    
    + if readEvent sees a "partial" event (normally should not happen, but
    it tries to be robust), the code used to sleep for 1 second and try
    again.  Changed so we first release the write lock before sleeping. :^).
    
    + if readEvent fails to read the event number, re-instantiate the
    event object after we try to read it a second time.
    
    + if readEvent() sees a partial event and successfully reads it
    a second time, we had a bug which would cause the *subsequent*
    readEvent() to always fail.  Now fixed by synchronizing the log
    before calling ULOG_OK after successfully reading the log on a retry.

--- src/condor_c++_util/user_log.C
+++ src/condor_c++_util/user_log.C
@@ -221,6 +221,9 @@ writeEvent (ULogEvent *event)
 	if (!retval) fputc ('\n', fp);
 	if (fprintf (fp, SynchDelimiter) < 0) retval = 0;
 	fflush(fp);
+	// Now that we have flushed the stdio stream, sync to disk
+	// *before* we release our write lock!
+	fsync( fileno(fp) );
 	lock->release ();
 	set_priv( priv );
 	return retval;
@@ -368,7 +371,11 @@ ReadUserLog (const char * filename)
 bool ReadUserLog::
 initialize (const char *filename)
 {	
-	if ((_fd = open (filename, O_RDONLY, 0)) == -1) return false;
+		// Note:  For whatever reason, we obtain a WRITE lock
+		// in method readEvent.  On Linux, of the file is opened
+		// O_RDONLY, then a WRITE_LOCK never blocks.  Thus
+		// open the file RDWR so the WRITE_LOCK below works correctly.
+	if ((_fd = open (filename, O_RDWR, 0)) == -1) return false;
 	if ((_fp = fdopen (_fd, "r")) == NULL) return false;
 
     lock = new FileLock( _fd, _fp );
@@ -392,6 +399,7 @@ readEvent (ULogEvent *& event)
 	// we obtain a write lock here not because we want to write
 	// anything, but because we want to ensure we don't read
 	// mid-way through someone else's write
+	// note: this seems bogus to me.  i think we want a READ_LOCK here.-Todd
 	if (!is_locked)
 		lock->obtain( WRITE_LOCK );
 
@@ -404,11 +412,34 @@ readEvent (ULogEvent *& event)
 		return ULOG_UNK_ERROR;
 	}
 
+
+#if 0
+	// TODD DEBUG
+	char smella[512];
+	if ( fgets(smella,512,_fp) ) {
+		printf("POS=%d BUFFER = '%s'\n",filepos,smella);
+	} else {
+		printf("POS=%d BUFFER = EOF\n",filepos);
+	}
+	fseek( _fp, filepos, SEEK_SET);
+	// END OF TODD DEBUG
+#endif
+
 	retval1 = fscanf (_fp, "%d", &eventnumber);
 
 	// so we don't dump core if the above fscanf failed
-	if (retval1 != 1) 
+	if (retval1 != 1) {
 		eventnumber = 1;
+			// check for end of file
+		if ( feof(_fp) ) {
+			event = NULL;  // To prevent FMR: Free memory read
+			clearerr (_fp);
+			if (!is_locked)
+				lock->release();
+			return ULOG_NO_EVENT;
+		}
+	}
+
 
 	// allocate event object; check if allocated successfully
 	event = instantiateEvent ((ULogEventNumber) eventnumber);
@@ -421,14 +452,21 @@ readEvent (ULogEvent *& event)
 
 	// read event from file; check for result
 	retval2 = event->getEvent (_fp);
-		
 	// check if error in reading event
 	if (!retval1 || !retval2)
 	{	
+		// we could end up here if file locking did not work for
+		// whatever reason (crappy NFS bugs, whatever).  so here
+		// try to wait a second until the current partially-written
+		// event has benn completely written.  the algorithm is 
 		// wait a second, rewind to our initial position (in case a
 		// buggy getEvent() slurped up more than one event), then
-		// synchronize the log
+		// again try to synchronize the log.
+		if (!is_locked)
+			lock->release();
 		sleep( 1 );
+		if (!is_locked)
+			lock->obtain( WRITE_LOCK );
 		if( fseek( _fp, filepos, SEEK_SET)) {
 			dprintf( D_ALWAYS, "fseek() failed in %s:%d", __FILE__, __LINE__ );
 			if (!is_locked)
@@ -448,7 +486,20 @@ readEvent (ULogEvent *& event)
 			
 			// ... attempt to read the event again
 			clearerr (_fp);
+			int oldeventnumber = eventnumber;
+			eventnumber = -1;
 			retval1 = fscanf (_fp, "%d", &eventnumber);
+			if ( eventnumber != oldeventnumber ) {
+				if (event) delete event;
+				// allocate event object; check if allocated successfully
+				event = instantiateEvent ((ULogEventNumber) eventnumber);
+				if (!event) 
+				{
+					if (!is_locked)
+						lock->release();
+					return ULOG_UNK_ERROR;
+				}
+			}
 			retval2 = event->getEvent (_fp);
 
 			// if failed again, we have a parse error
@@ -463,9 +514,24 @@ readEvent (ULogEvent *& event)
 			}
 			else
 			{
-				if (!is_locked)
-					lock->release();
-				return ULOG_OK;
+				// finally got the event successfully -- synchronize the log
+				if (synchronize ())
+				{
+					if (!is_locked)
+						lock->release();
+					return ULOG_OK;
+				}
+				else
+				{
+					// got the event, but could not synchronize!!  treat as incomplete
+					// event
+					delete event;
+					event = NULL;  // To prevent FMR: Free memory read
+					clearerr (_fp);
+					if (!is_locked)
+						lock->release();
+					return ULOG_NO_EVENT;
+				}
 			}
 		}
 		else
