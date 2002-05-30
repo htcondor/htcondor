83e210e9d50d273d0ed73749844c89bd84b25912
thain <thain>
2002-05-30 19:44:59 +0000

    Set up the signal mask correctly before forking off Java.

--- src/condor_starter.V6.1/java_detect.C
+++ src/condor_starter.V6.1/java_detect.C
@@ -10,11 +10,27 @@ ClassAd * java_detect()
 	char path[ATTRLIST_MAX_EXPRESSION];
 	char args[ATTRLIST_MAX_EXPRESSION];
 	char command[_POSIX_ARG_MAX];
+	sigset_t mask;
 
 	if(!java_config(path,args,0)) return 0;
 	int benchmark_time = param_integer("JAVA_BENCHMARK_TIME",0);
 
 	sprintf(command,"%s %s CondorJavaInfo old %d",path,args,benchmark_time);
+	
+	/*
+	N.B. Certain version of Java do not set up their own signal
+	masks correctly.  DaemonCore has already blocked off a bunch
+	of signals.  We have to reset them, otherwise some JVMs go
+	off into an infinite loop.  However, we leave SIGCHLD blocked,
+	as DaemonCore has already set up a handler, but isn't prepared
+	to actually handle it before the DC constructor completes.
+	*/
+
+#ifndef WIN32
+	sigemptyset(&mask);
+	sigaddset(&mask,SIGCHLD);
+	sigprocmask(SIG_SETMASK,&mask,0);
+#endif
 
 	FILE *stream = popen(command,"r");
 	if(!stream) return 0;
