4a1a117f5ac2a0a8c4fefa23da474775e5b76efe
epaulson <epaulson>
2002-06-25 20:11:43 +0000

    The big comment about the format is there for a reason... fix it so 6.4.1 has a valid version string.

--- src/condor_c++_util/condor_version.C
+++ src/condor_c++_util/condor_version.C
@@ -32,7 +32,7 @@
    Any questions?  See Todd or Derek.  Note: if you mess it up, DaemonCore
    will EXCEPT at startup time.  
 */
-static char* CondorVersionString = "$CondorVersion: 6.4.1 PRE-RELEASE-1 " __DATE__ " $";
+static char* CondorVersionString = "$CondorVersion: 6.4.1 " __DATE__ " PRE-RELEASE-1 $";
 
 /* 
    This is some wisdom from Cygnus's web page.  If you just try to use
