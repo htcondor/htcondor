e79665397164a2ad11cc7e393f480ace8b87d0ce
roy <roy>
2002-07-31 17:01:17 +0000

    Doug wrote this but forgot to add the copyright, so I did. Added "using namespace std;" so it compiles well in the standalone distribution with GCC 3.0

--- src/condor_classad.V6/cclassad.C
+++ src/condor_classad.V6/cclassad.C
@@ -1,3 +1,24 @@
+/*********************************************************************
+ *
+ * Condor ClassAd library
+ * Copyright (C) 1990-2001, CONDOR Team, Computer Sciences Department,
+ * University of Wisconsin-Madison, WI, and Rajesh Raman.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of version 2.1 of the GNU Lesser General
+ * Public License as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *********************************************************************/
 
 #include "common.h"
 #include "cclassad.h"
@@ -5,6 +26,8 @@
 #include "source.h"
 #include "sink.h"
 #include "xmlSink.h"
+
+using namespace std;
 
 struct cclassad {
 	ClassAd *ad;
