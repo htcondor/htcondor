e8a3eca8f0da7154dcf68bdae1da34af5b585a7b
roy <roy>
2002-07-31 22:37:22 +0000

    Changes for namespace support, added copyright notice.

--- src/condor_classad.V6/cclassad.h
+++ src/condor_classad.V6/cclassad.h
@@ -1,6 +1,27 @@
+/*********************************************************************
+ *
+ * Condor ClassAd library
+ * Copyright (C) 1990-2001, CONDOR Team, Computer Sciences Department,
+ * University of Wisconsin-Madison, WI, and Rajesh Raman.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of version 2.1 of the GNU Lesser General
+ * Public License as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *********************************************************************/
 
-#ifndef CCLASSAD_H
-#define CCLASSAD_H
+#ifndef __CCLASSAD_H_
+#define __CCLASSAD_H_
 
 /*
 This is a C interface to the classad library.
@@ -11,6 +32,7 @@ or other packages are necessary to use this interface.
 */
 
 #ifdef __cplusplus
+BEGIN_NAMESPACE( classad )
 extern "C" {
 #endif
 
@@ -82,6 +104,7 @@ int cclassad_evaluate_to_bool( struct cclassad *c, const char *expr, int *result
 
 #ifdef __cplusplus
 }
+END_NAMESPACE
 #endif
 
 #endif
