a01755550262da5002181c86950098f5c9267470
nleroy <nleroy>
2002-08-12 18:06:03 +0000

    Misc cleanups & bug fixes

--- src/hawkeye_modules/HawkeyeLib.pm
+++ src/hawkeye_modules/HawkeyeLib.pm
@@ -18,6 +18,8 @@ sub DoConfig( )
 
     chop( $ENV{OS_TYPE} = `uname -s` );
     chop( $ENV{OS_REV} = `uname -r` );
+    chop( $ENV{HOST} = `hostname` );
+    print STDERR "$ENV{HOST}\n";
 }
 
 # Get my name..
