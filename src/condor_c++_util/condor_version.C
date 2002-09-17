404fb4791780431dc0c26d3841438c938feb8084
epaulson <epaulson>
2002-09-17 03:26:51 +0000

    6.4.3 is basically done; Todd has to checkin a fix to the broken build and Colin has to sign off on a few things, and then we ship.

--- src/condor_c++_util/condor_version.C
+++ src/condor_c++_util/condor_version.C
@@ -32,7 +32,7 @@
    Any questions?  See Todd or Derek.  Note: if you mess it up, DaemonCore
    will EXCEPT at startup time.  
 */
-static char* CondorVersionString = "$CondorVersion: 6.4.3 " __DATE__ " PRE-RELEASE-1 $";
+static char* CondorVersionString = "$CondorVersion: 6.4.3 " __DATE__ " $";
 
 /* 
    This is some wisdom from Cygnus's web page.  If you just try to use
