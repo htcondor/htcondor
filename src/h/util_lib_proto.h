b9f579f7a28da9781ae981774b609a69c65c9bd0
jfrey <jfrey>
2003-03-13 21:23:31 +0000

    Added new copy_file() function to the util lib. It copies a file from one location to another, returning 0 on success or -1 on failure (and logs an appropriate log message). It doesn't not attempt to do an "atomic" update of the destination file. For that, use it in combination with rotate_file().

--- src/h/util_lib_proto.h
+++ src/h/util_lib_proto.h
@@ -45,6 +45,7 @@ int hash ( register char *string, register int size );
 char * getline ( FILE *fp );
 
 int rotate_file(const char *old_filename, const char *new_filename);
+int copy_file(const char *old_filename, const char *new_filename);
 
 void schedule_event ( int month, int day, int hour, int minute, int second, void (*func)() );
 void event_mgr ( void );
