d3975138c2fee3186cd62d1f065c4eae39e6a68e
psilord <psilord>
2003-03-18 22:19:36 +0000

    + Added AIX support that conveniently already wokred under linux.

--- src/condor_c++_util/event_handler.C
+++ src/condor_c++_util/event_handler.C
@@ -113,7 +113,7 @@ EventHandler::install()
 		signo = next_sig();
 		if( sigismember(&mask,signo) ) {
 			// explicit type cast to eliminate type check warning  -- Rajesh
-#if (defined(LINUX) && defined(GLIBC22))
+#if (defined(LINUX) && defined(GLIBC22)) || defined(AIX)
 			// bad craziness with the type cast --psilord
 			action.sa_handler = (void (*)(int)) func;
 #else
