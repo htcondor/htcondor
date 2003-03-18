19be06e599a00a855623c41d413157cae7167513
psilord <psilord>
2003-03-18 20:57:00 +0000

    + added inclusion of aix specific header file. + Moved inclusion of condor_snprintf.h and the typedef of the fd_set pointer 	to after the inclusion of the system header files because under AIX, 	those things aren't brought in through various other header files before 	the standard ones we bring in here.

--- src/condor_includes/condor_system.h
+++ src/condor_includes/condor_system.h
@@ -62,6 +62,8 @@
 #	include "condor_sys_solaris.h"
 #elif defined(OSF1)
 #	include "condor_sys_dux.h"
+#elif defined(AIX)
+#	include "condor_sys_aix.h"
 #else
 #   error "Don't know what Unix this is!"
 #endif
@@ -73,7 +75,6 @@
 #include "condor_fix_access.h"
 #include "condor_file_lock.h"
 #include "condor_fix_assert.h"
-#include "condor_snutils.h"
 
 
 /**********************************************************************
@@ -107,11 +108,6 @@
 #	define NFDS(x) (x)
 #endif
 
-/* select() on all our platforms takes an fd_set pointer, so we can
-   just define this here for everyone.  We don't really need it
-   anymore, but we might hit a platform that has a different select,
-   so the level of indirection is worth keeping around. */
-typedef fd_set *SELECT_FDSET_PTR;
 
 /* If WCOREFLAG is defined but WCOREFLG is not, define WCOREFLG since
    that's what we use in our code. */
@@ -176,7 +172,18 @@ typedef fd_set *SELECT_FDSET_PTR;
 #define stricmp strcasecmp		/* stricmp no longer exits in egcs, but strcasecmp does */
 #define strincmp strncasecmp	/* strincmp no longer exits in egcs, but strncasecmp does */
 
+#include "condor_snutils.h"
+
+/* select() on all our platforms takes an fd_set pointer, so we can
+   just define this here for everyone.  We don't really need it
+   anymore, but we might hit a platform that has a different select,
+   so the level of indirection is worth keeping around. */
+typedef fd_set *SELECT_FDSET_PTR;
+
 #endif /* UNIX */
 
 #endif /* CONDOR_SYSTEM_H */
+
+
+
 
