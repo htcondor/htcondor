4eecec09c2b071662ff4519dfd0cdd6e233e27c6
psilord <psilord>
2003-03-25 22:02:40 +0000

    + in create_tcp_port, getsockname must use the length types derived from 	condor_socket_types.h

--- src/condor_shadow.V6/pseudo_ops.C
+++ src/condor_shadow.V6/pseudo_ops.C
@@ -1392,7 +1392,9 @@ int
 create_tcp_port( u_short *port, int *fd )
 {
 	struct sockaddr_in	sin;
-	SOCKET_LENGTH_TYPE	addr_len = sizeof sin;
+	SOCKET_LENGTH_TYPE	addr_len;
+	
+	addr_len = sizeof(sin);
 
 		/* create a tcp socket */
 	if( (*fd=socket(AF_INET,SOCK_STREAM,0)) < 0 ) {
