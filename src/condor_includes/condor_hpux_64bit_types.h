6370a71892b7052f6fd18be560a8767f909fbed3
psilord <psilord>
2003-04-03 21:17:35 +0000

    + Added support for hpux11 for the types defined in this file.

--- src/condor_includes/condor_hpux_64bit_types.h
+++ src/condor_includes/condor_hpux_64bit_types.h
@@ -28,26 +28,56 @@
    defined.  We don't want that always defined on HPUX, so we just do
    this directly here. */
 
+#if defined(HPUX10)
 typedef long long int64_t;
 typedef unsigned long long uint64_t;
 typedef int64_t off64_t;
 typedef uint64_t rlim64_t;
 typedef int64_t blkcnt64_t;
+#endif
 
 #define __stat stat64
+#if defined(HPUX10)
 #define blkcnt_t blkcnt64_t
 #define off_t off64_t
 #define ad_long_t long
+#elif defined(HPUX11)
+#define _T_INO_T      ino_t
+#define _T_OFF_T      off64_t
+#define _T_BLKCNT_T   blkcnt64_t
+#else
+#error Inspect the 64 bit stat type specification!
+#endif
 #include <sys/_stat_body.h>
+#if defined(HPUX10)
 #undef ad_long_t
 #undef off_t
 #undef blkcnt_t
+#elif defined(HPUX11)
+#undef _T_INO_T
+#undef _T_OFF_T
+#undef _T_BLKCNT_T
+#else
+#error Inspect the 64 bit stat type specification!
+#endif
 #undef __stat
 
 #define __rlimit  rlimit64
+#if defined(HPUX10)
 #define rlim_t rlim64_t
+#elif defined(HPUX11)
+#define _T_RLIM_T rlim64_t
+#else
+#error Inspect the 64 bit rlimit type specification!
+#endif
 #include <sys/_rlimit_body.h>
+#if defined(HPUX10)
 #undef rlim_t
+#elif defined(HPUX11)
+#undef _T_RLIM_T
+#else
+#error Inspect the 64 bit rlimit type specification!
+#endif
 #undef __rlimit
 
 #endif /* CONDOR_HPUX_64BIT_TYPES_H */
