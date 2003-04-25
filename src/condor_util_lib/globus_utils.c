79de01e7e3fcf2d6f6d490de87def4f2c7f55fe9
adesmet <adesmet>
2003-04-25 21:51:10 +0000

    strstr in earlier versions of glibc (including the one that ships with Red Hat 6.x) don't handle null pointers.  Result: automatical url detection could cause core dumps in some cases.

--- src/condor_util_lib/globus_utils.c
+++ src/condor_util_lib/globus_utils.c
@@ -427,6 +427,8 @@ void parse_resource_manager_string( const char *string, char **host,
 int
 is_globus_friendly_url(const char * path)
 {
+	if(path == 0)
+		return 0;
 	// Should this be more aggressive and allow anything with ://?
 	return 
 		strstr(path, "http://") == path ||
