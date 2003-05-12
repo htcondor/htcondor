4ea260cd5b7f757fc0e2657f23cde438f5314477
roy <roy>
2003-05-12 21:32:13 +0000

    This file makes it easier to include hash_map and slist under different versions of gcc. And it should be easy to expand when we expand to other compilers, or use STLPort. Note that there are #defines that you use for hash_map and slist, so you get the right namespace.

---
+++ src/condor_classad.V6/classad_stl.h
@@ -0,0 +1,52 @@
+/*********************************************************************
+ *
+ * Condor ClassAd library
+ * Copyright (C) 1990-2003, CONDOR Team, Computer Sciences Department,
+ * University of Wisconsin-Madison, WI, and Rajesh Raman.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of version 2.1 of the GNU Lesser General
+ * Public License as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *********************************************************************/
+
+#ifndef __CLASSAD_STL_H__
+#define __CLASSAD_STL_H__
+
+#ifdef __GNUC__
+  #if (__GNUC__<3)
+    #include <hash_map>
+    #include <slist>
+  #else
+    #include <ext/hash_map>
+    #include <ext/slist>
+    using namespace __gnu_cxx;
+  #endif
+#else
+  #include <hash_map>
+#endif
+
+#ifdef __GNUC__
+  #if (__GNUC__ == 3 && __GNUC_MINOR__ > 0)
+    #define classad_hash_map __gnu_cxx::hash_map 
+    #define classad_slist    __gnu_cxx::slist
+  #else
+    #define classad_hash_map std::hash_map 
+    #define classad_slist    std::slist
+  #endif
+#else
+  #define classad_hash_map std::hash_map 
+  #define classad_slist    std::slist
+#endif
+
+#endif /* __CLASSAD_STL_H__ */
