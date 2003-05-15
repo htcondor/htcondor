9c1fab38212395110874852bd31dbfee1fe01179
psilord <psilord>
2003-05-15 21:17:36 +0000

    + Ripped out lower_case() and put it into its own .o file so it can be 	included in libcondorapi.a. You may now find it in the lower_case.C 	file in condor_c++_util.

--- src/condor_c++_util/config.C
+++ src/condor_c++_util/config.C
@@ -359,31 +359,6 @@ getline_implementation( FILE *fp, int requested_bufsize )
 	}
 }
 
-#ifndef _tolower
-#define _tolower(c) ((c) + 'a' - 'A')
-#endif
-/*
-** Transform the given string into lower case in place.
-*/
-void
-lower_case( register char	*str )
-{
-#if defined(AIX32)
-#	define ANSI_STYLE 0
-#else
-#	define ANSI_STYLE 1
-#endif
-
-	for( ; *str; str++ ) {
-		if( *str >= 'A' && *str <= 'Z' )
-#if ANSI_STYLE
-			*str = _tolower( (int)(*str) );
-#else
-			*str |= 040;
-#endif
-	}
-}
-	
 /*
 ** Expand parameter references of the form "left$(middle)right".  This
 ** is deceptively simple, but does handle multiple and or nested references.
