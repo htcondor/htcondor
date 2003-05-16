2c0b84f8d5c0b6bf50a29ada0a56f92f40ae8d44
wright <wright>
2003-05-16 16:03:37 +0000

    we do *NOT* want to bring in *any* condor header files, or we have to start with "condor_common.h" and then all hell breaks loose.  so long as we extern "C" the method explicitly, we don't need any headers.

--- src/condor_c++_util/lower_case.C
+++ src/condor_c++_util/lower_case.C
@@ -1,10 +1,18 @@
-#include "condor_config.h"
+/*
+  NOTE: we do *NOT* want to include "condor_common.h" and all the
+  associated header files for this, since it's a very simple function
+  and we want to be able to build it in a stand-alone version of the
+  UserLog parser for libcondorapi.a
+*/
 
 /* Define a version of lower_case which is happy on various platforms. */
 
 #ifndef _tolower
 #define _tolower(c) ((c) + 'a' - 'A')
 #endif
+
+extern "C" {
+
 /*
 ** Transform the given string into lower case in place.
 */
@@ -28,4 +36,5 @@ lower_case( register char   *str )
 }
 
 
+} /* extern "C" */
 
