0436ea0366a7622b7beed8cff29f6e6d2659d9f1
ckireyev <ckireyev>
2003-05-23 19:07:44 +0000

    Added commnents Made automatically detect version number

--- src/condor_scripts/make-condor-rpm.sh
+++ src/condor_scripts/make-condor-rpm.sh
@@ -1,12 +1,31 @@
 #!/bin/sh
 
-condor_version='6.5.1'
+#
+# This script takes a condor distribution .tar.gz file and 
+# creates an RPM from it in the current directory.
+#
+# Example:
+#   make-condor-rpm.sh condor-6.5.1.tar.gz "6.5.1"
+#
+# Notes:
+#  1) This script requires (the new) condor_configure at the root level 
+#  (i.e. in place of old condor_install)
+#  2) The default directory of installation is /opt/condor-<version>,
+#    e.g. /opt/condor-6.5.1
+#  3) Condor .tar.gz file should follow naming convention:
+#    condor-<ver>-....tar.gz
+#
+#
+#
 
-if [ -z "$1" ]
+if [ -z "$1" ] 
 then
     echo "Usage: make-condor-rpm.sh <Condor dirstribution .tar.gz>"
     exit 1
 fi
+
+# Figure out version from file name condor-<ver>-....tar.gz
+condor_version=`perl -e '@v=split(/-/, $ARGV[0]); print $v[1];' $1`;
 
 rm -rf /tmp/make-condor-rpm && mkdir /tmp/make-condor-rpm
 if [ $? != 0 ]
