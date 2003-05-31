7d4c777aaaf4eb59c54f00f8cd7c068e305d8daf
wright <wright>
2003-05-31 16:09:49 +0000

    added "HAWKEYE" to the default_dc_daemon_list since it *is* daemoncore, and was relying on the stupid master bug that always appended "-f".  now that the master doesn't do that, we've got to treat "HAWKEYE" as daemoncore by default to avoid people having to go out of their way to run hawkeye under their regular condor install.

--- src/condor_master.V6/master.C
+++ src/condor_master.V6/master.C
@@ -122,7 +122,7 @@ char	*default_daemon_list[] = {
 
 char	default_dc_daemon_list[] =
 "MASTER, STARTD, SCHEDD, KBDD, COLLECTOR, NEGOTIATOR, EVENTD, "
-"VIEW_SERVER, CONDOR_VIEW, VIEW_COLLECTOR";
+"VIEW_SERVER, CONDOR_VIEW, VIEW_COLLECTOR, HAWKEYE";
 
 // create an object of class daemons.
 class Daemons daemons;
