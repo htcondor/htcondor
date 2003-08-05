893774223a8761d9c19954b18176a3c3c7a1e2be
roy <roy>
2003-08-05 20:13:28 +0000

    The old contents of this file were moved to condorUtil.C, where they belong. I added double_to_hex and hex_to_double here: The signatures of the functions may seem surprising because one takes a string and one takes a char*. This may look odd, but it avoided extra conversions, and these may be called a lot.

--- src/condor_classad.V6/util.C
+++ src/condor_classad.V6/util.C
@@ -21,202 +21,41 @@
  * WI 53706-1685, (608) 262-0856 or miron@cs.wisc.edu.
 ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
 
-#include "condor_common.h"
-#include "condor_classad.h"
+#include "common.h"
+#include "util.h"
 
 using namespace std;
 
 BEGIN_NAMESPACE( classad )
 
-ClassAd* getOldClassAd( Stream *sock )
+union RealHexUnion
+{ 
+	double     real; 
+	long long  hex; 
+};
+
+bool hex_to_double(const string &hex, double &number)
 {
-	ClassAd *ad = new ClassAd( );
-	if( !ad ) { 
-		return (ClassAd*) 0;
+	RealHexUnion  u;
+	bool          success;
+
+	if (sscanf(hex.c_str(), "%llx", &u.hex) != 1) {
+		success = false;
+		number  = 0;
+	} else {
+		number  = u.real;
+		success = true;
 	}
-	if( !getOldClassAd( sock, *ad ) ) {
-		delete ad;
-		return NULL;
-	}
-	return ad;	
+	return success;
 }
 
-
-bool getOldClassAd( Stream *sock, ClassAd& ad )
+void double_to_hex(double number, char *hex)
 {
-	ClassAdParser	parser;
-	int 			numExprs;
-	string			buffer;
-	char			*tmp;
-	ClassAd			*upd=NULL;
-	static char		*inputLine = new char[ 10240 ];
+	RealHexUnion u;
 
-
-	ad.Clear( );
-	sock->decode( );
-	if( !sock->code( numExprs ) ) {
- 		return false;
-	}
-
-		// pack exprs into classad
-	buffer = "[";
-	for( int i = 0 ; i < numExprs ; i++ ) {
-		tmp = NULL;
-		if( !sock->get( inputLine ) ) { 
-			return( false );	 
-		}		
-
-		buffer += string(inputLine) + ";";
-		free( tmp );
-	}
-	buffer += "]";
-
-		// get type info
-	if (!sock->get(inputLine)||!ad.InsertAttr("MyType",(string)inputLine)) {
-		return false;
-	}
-	if (!sock->get(inputLine)|| !ad.InsertAttr("TargetType",
-											   (string)inputLine)) {
-		return false;
-	}
-
-		// parse ad
-	if( !( upd = parser.ParseClassAd( buffer ) ) ) {
-		return( false );
-	}
-
-		// put exprs into ad
-	ClassAd *tmpAd = new ClassAd( );
-	tmpAd->Update( *upd );
-	tmpAd = tmpAd->AddExplicitTargetRefs( );
-	ad.Update( *tmpAd );
-	delete tmpAd;
-	delete upd;
-
-	return true;
-}
-
-
-bool getOldClassAdNoTypes( Stream *sock, ClassAd& ad )
-{
-	ClassAdParser	parser;
-	int 			numExprs;
-	string			buffer;
-	char			*tmp;
-	ClassAd			*upd=NULL;
-	static char		*inputLine = new char[ 10240 ];
-
-
-	ad.Clear( );
-	sock->decode( );
-	if( !sock->code( numExprs ) ) {
- 		return false;
-	}
-
-		// pack exprs into classad
-	buffer = "[";
-	for( int i = 0 ; i < numExprs ; i++ ) {
-		tmp = NULL;
-		if( !sock->get( inputLine ) ) { 
-			return( false );	 
-		}		
-
-		buffer += string(inputLine) + ";";
-		free( tmp );
-	}
-	buffer += "]";
-
-		// parse ad
-	if( !( upd = parser.ParseClassAd( buffer ) ) ) {
-		return( false );
-	}
-
-		// put exprs into ad
-	ad.Update( *upd );
-	delete upd;
-
-	return true;
-}
-
-
-
-bool putOldClassAd ( Stream *sock, ClassAd& ad )
-{
-	ClassAdUnParser	unp;
-	string			buf;
-	ExprTree		*expr;
-
-	int numExprs=0;
-	ClassAdIterator itor(ad);
-	while( !itor.IsAfterLast( ) ) {
-		itor.CurrentAttribute( buf, (const ExprTree *)expr );
-		if( strcasecmp( "MyType", buf.c_str( ) ) != 0 && 
-				strcasecmp( "TargetType", buf.c_str( ) ) != 0 ) {
-			numExprs++;
-		}
-		itor.NextAttribute( buf, (const ExprTree *) expr );
-	}
-	
-	sock->encode( );
-	if( !sock->code( numExprs ) ) {
-		return false;
-	}
-		
-	for( itor.ToFirst(); !itor.IsAfterLast(); itor.NextAttribute(buf, (const ExprTree *) expr) ) {
-		itor.CurrentAttribute( buf, (const ExprTree *) expr );
-		if( strcasecmp( "MyType", buf.c_str( ) ) == 0 || 
-				strcasecmp( "TargetType", buf.c_str( ) ) == 0 ) {
-			continue;
-		}
-		buf += " = ";
-		unp.Unparse( buf, expr );
-		if (!sock->put((char*)buf.c_str())) return false;
-	}
-
-	// Send the type
-	if (!ad.EvaluateAttrString("MyType",buf)) {
-		buf="(unknown type)";
-	}
-	if (!sock->put((char*)buf.c_str())) {
-		return false;
-	}
-
-	if (!ad.EvaluateAttrString("TargetType",buf)) {
-		buf="(unknown type)";
-	}
-	if (!sock->put((char*)buf.c_str())) {
-		return false;
-	}
-
-	return true;
-}
-
-bool putOldClassAdNoTypes ( Stream *sock, ClassAd& ad )
-{
-	ClassAdUnParser	unp;
-	string			buf;
-	ExprTree		*expr;
-
-	int numExprs=0;
-	ClassAdIterator itor(ad);
-
-	while( !itor.IsAfterLast( ) ) {
-		numExprs++;
-	}	
-
-	sock->encode( );
-	if( !sock->code( numExprs ) ) {
-		return false;
-	}
-		
-	for( itor.ToFirst(); !itor.IsAfterLast(); itor.NextAttribute(buf, (const ExprTree *) expr) ) {
-		itor.CurrentAttribute( buf, (const ExprTree *) expr );
-		buf += " = ";
-		unp.Unparse( buf, expr );
-		if (!sock->put((char*)buf.c_str())) return false;
-	}
-
-	return true;
+	u.real = number;
+	snprintf(hex, 17, "%016llx", u.hex;);
+	return;
 }
 
 END_NAMESPACE // classad
