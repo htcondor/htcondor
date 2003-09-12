1dc8d8feca08f8147bd1f3936789cb59872e4543
wright <wright>
2003-09-12 03:23:13 +0000

    the version on the V6_7-branch should now be 6.7.0 PRERELEASE.

--- src/condor_c++_util/condor_version.C
+++ src/condor_c++_util/condor_version.C
@@ -33,7 +33,7 @@
    will EXCEPT at startup time.  
 */
 
-static char* CondorVersionString = "$CondorVersion: 6.5.5 " __DATE__ " $";
+static char* CondorVersionString = "$CondorVersion: 6.7.0 " __DATE__ " PRERELEASE $";
 
 /* 
    This is some wisdom from Cygnus's web page.  If you just try to use
