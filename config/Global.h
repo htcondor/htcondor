398b1dff6f20dbb7b0a6b27ed3465842636a8348
wright <wright>
2003-11-03 03:11:34 +0000

    moved all the #undef for compiler-defined symbols from Platform.select into Global.h, and moved Global.h to the very top of Imake.tmpl.  we were getting nailed because some pathnames that were set in configure.cf were getting mangled by compiler-defined symbols.  now, Global.h cleans all these up for us.  since it doesn't depend on anything else, it can be the very first file we include. Platform.select might be going away, since we can figure all that out and set it in configure.cf, if we want.

--- config/Global.h
+++ config/Global.h
@@ -20,3 +20,22 @@
 
 #define __CONCAT6(a,b,c,d,e,f) a ## b ## c ## d ## e ## f
 #define concat6(a,b,c,d,e,f) __CONCAT6(a,b,c,d,e,f)
+
+
+/***************************************************************************
+** Undefine any symbols the compiler might have defined for us since
+** they may trip us up later.
+***************************************************************************/
+#undef sun
+#undef sparc
+#undef i386		
+#undef i686
+#undef hp9000s800
+#undef __alpha
+#undef sgi
+#undef alpha
+#undef hpux
+#undef linux
+#undef mips
+#undef aix
+#undef ia64
