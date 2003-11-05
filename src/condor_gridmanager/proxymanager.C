8813918eb7a0b10095e3689b17f5882cbd1e5102
ckireyev <ckireyev>
2003-11-05 22:18:04 +0000

    MyProxy crap

--- src/condor_gridmanager/proxymanager.C
+++ src/condor_gridmanager/proxymanager.C
@@ -3,21 +3,21 @@
  *
  * See LICENSE.TXT for additional notices and disclaimers.
  *
- * Copyright (c)1990-1998 CONDOR Team, Computer Sciences Department, 
- * University of Wisconsin-Madison, Madison, WI.  All Rights Reserved.  
- * No use of the CONDOR Software Program Source Code is authorized 
- * without the express consent of the CONDOR Team.  For more information 
- * contact: CONDOR Team, Attention: Professor Miron Livny, 
- * 7367 Computer Sciences, 1210 W. Dayton St., Madison, WI 53706-1685, 
+ * Copyright (c)1990-1998 CONDOR Team, Computer Sciences Department,
+ * University of Wisconsin-Madison, Madison, WI.  All Rights Reserved.
+ * No use of the CONDOR Software Program Source Code is authorized
+ * without the express consent of the CONDOR Team.  For more information
+ * contact: CONDOR Team, Attention: Professor Miron Livny,
+ * 7367 Computer Sciences, 1210 W. Dayton St., Madison, WI 53706-1685,
  * (608) 262-0856 or miron@cs.wisc.edu.
  *
- * U.S. Government Rights Restrictions: Use, duplication, or disclosure 
- * by the U.S. Government is subject to restrictions as set forth in 
- * subparagraph (c)(1)(ii) of The Rights in Technical Data and Computer 
- * Software clause at DFARS 252.227-7013 or subparagraphs (c)(1) and 
- * (2) of Commercial Computer Software-Restricted Rights at 48 CFR 
- * 52.227-19, as applicable, CONDOR Team, Attention: Professor Miron 
- * Livny, 7367 Computer Sciences, 1210 W. Dayton St., Madison, 
+ * U.S. Government Rights Restrictions: Use, duplication, or disclosure
+ * by the U.S. Government is subject to restrictions as set forth in
+ * subparagraph (c)(1)(ii) of The Rights in Technical Data and Computer
+ * Software clause at DFARS 252.227-7013 or subparagraphs (c)(1) and
+ * (2) of Commercial Computer Software-Restricted Rights at 48 CFR
+ * 52.227-19, as applicable, CONDOR Team, Attention: Professor Miron
+ * Livny, 7367 Computer Sciences, 1210 W. Dayton St., Madison,
  * WI 53706-1685, (608) 262-0856 or miron@cs.wisc.edu.
 ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
 
@@ -30,8 +30,15 @@
 #include "condor_config.h"
 #include "classad_hashtable.h"
 #include "util_lib_proto.h"
+#include "env.h"
+#include "directory.h"
+#include "daemon.h"
+#include "internet.h"
+#include "simplelist.h"
+
 
 #include "proxymanager.h"
+//#include "myproxy_manager.h"
 #include "gahp-client.h"
 
 #define HASH_TABLE_SIZE			500
@@ -41,8 +48,18 @@
 template class HashTable<HashKey, Proxy *>;
 template class HashBucket<HashKey, Proxy *>;
 
+
+template class SimpleList<MyProxyEntry*>;
+
+
+//template class HashTable<HashKey, MyProxyManager *>;
+//template class HashBucket<HashKey, MyProxyManager *>;
+
 HashTable <HashKey, Proxy *> ProxiesByPath( HASH_TABLE_SIZE,
 											hashFunction );
+//HashTable <HashKey, MyProxyManager *> MyProxyManagersByPath ( HASH_TABLE_SIZE, hashFunction );
+
+//MyProxyManager myProxyManager;
 
 static bool proxymanager_initialized = false;
 static bool gahp_initialized = false;
@@ -54,6 +71,8 @@ static bool(*InitGahpFunc)(const char *) = NULL;
 
 int CheckProxies_interval = 600;		// default value
 int minProxy_time = 3 * 60;				// default value
+int myproxyGetDelegationReaperId = 0;
+
 
 static GahpClient my_gahp;
 static int next_gahp_proxy_id = 1;
@@ -141,7 +160,7 @@ bool UseMultipleProxies( const char *proxy_dir,
 	MasterProxy.proxy_filename = strdup( buf.Value() );
 
 	return true;
-}	
+}
 
 // Initialize the ProxyManager module in single-proxy mode. This is for
 // use with old (i.e. 6.4.x) schedds. proxy_path is the path to the single
@@ -170,6 +189,23 @@ UseSingleProxy( const char *proxy_path,
 
 	return true;
 }
+
+// Set the host:port of the MyProxy server for a given proxy
+// Proxymanager will talk to the given server when the proxy is about to expire
+/*int SetMyProxyHostForProxy ( const char* hostport, const char* server_dn, const char* myproxy_pwd, const Proxy * proxy ) {
+	if (hostport == NULL || proxy == NULL) {
+		return FALSE;
+	}
+
+	if (myProxyManager.AddMyProxyEntry (hostport, server_dn, myproxy_pwd, proxy->proxy_filename)) {
+
+		dprintf (D_ALWAYS, "Created MyProxy manager (%s) for %s\n", hostport, proxy->proxy_filename);
+
+		return TRUE;
+	}
+
+	return FALSE;
+}*/
 
 // An entity (e.g. GlobusJob, GlobusResource object) should call this
 // function when it wants to use a proxy managed by ProxyManager. proxy_path
@@ -242,6 +278,8 @@ AcquireProxy( const char *proxy_path, int notify_tid )
 
 	ProxiesByPath.insert(HashKey(proxy_path), proxy);
 
+
+
 	daemonCore->Reset_Timer( CheckProxies_tid, 0 );
 
 	return proxy;
@@ -283,6 +321,63 @@ ReleaseProxy( Proxy *proxy, int notify_tid )
 	}
 }
 
+void DeleteMyProxyEntry (MyProxyEntry *& myproxy_entry) {
+	if (myproxy_entry->get_delegation_pid != FALSE) {
+		// Kill the process
+		//daemonCore->Shutdown_Graceful (proxy->myproxy_entry->get_delegation_pid);
+		myproxy_entry->get_delegation_pid=FALSE;
+	}
+
+	if (myproxy_entry->myproxy_host) {
+		free (myproxy_entry->myproxy_host);
+	}
+
+	if (myproxy_entry->myproxy_server_dn) {
+		free (myproxy_entry->myproxy_server_dn);
+	}
+
+	if (myproxy_entry->myproxy_password) {
+		free (myproxy_entry->myproxy_password);
+	}
+
+	if (myproxy_entry->myproxy_credential_name) {
+		free (myproxy_entry->myproxy_credential_name);
+	}
+
+	if (myproxy_entry->get_delegation_err_filename) {
+		free (myproxy_entry->get_delegation_err_filename);
+	}
+
+	if (myproxy_entry->get_delegation_password_pipe[0] > -1) {
+		close (myproxy_entry->get_delegation_password_pipe[0]);
+	}
+
+	if (myproxy_entry->get_delegation_password_pipe[1] > -1) {
+		close (myproxy_entry->get_delegation_password_pipe[1]);
+	}
+
+
+}
+
+// Utility function to deep-delete the Proxy data structure
+void DeleteProxy (Proxy *& proxy) {
+
+	if (proxy->proxy_filename) {
+		free( proxy->proxy_filename );
+	}
+
+	MyProxyEntry * myproxy_entry;
+	proxy->myproxy_entries.Rewind();
+	proxy->myproxy_entries.Next(myproxy_entry);
+
+	while (proxy->myproxy_entries.Current(myproxy_entry) == true) {
+		DeleteMyProxyEntry (myproxy_entry);
+		delete myproxy_entry;
+	}
+
+	delete proxy;
+}
+
 void doCheckProxies()
 {
 	if ( CheckProxies_tid != TIMER_UNSET ) {
@@ -303,7 +398,8 @@ int CheckProxies()
 	int now = time(NULL);
 	int next_check = CheckProxies_interval + now;
 	int rc;
-dprintf(D_FULLDEBUG,"CheckProxies called\n");
+
+	dprintf(D_FULLDEBUG,"CheckProxies called\n");
 
 	if ( proxymanager_initialized == false ) {
 		daemonCore->Reset_Timer( CheckProxies_tid, CheckProxies_interval );
@@ -325,15 +421,57 @@ dprintf(D_FULLDEBUG,"CheckProxies called\n");
 
 		// Remove any proxies that are no longer being used by anyone
 		if ( next_proxy->num_references == 0 ) {
+
+			// If myproxy-get-delegation is still running, don't delete this just yet
+			next_proxy->myproxy_entries.Rewind();
+			MyProxyEntry * mpe;
+			int keep = FALSE;
+			while (next_proxy->myproxy_entries.Next(mpe)) {
+				if (mpe->get_delegation_pid != FALSE) {
+					keep = TRUE;
+					break;
+				}
+			}
+			if (keep) {
+				continue;
+			}
+
+
 dprintf(D_FULLDEBUG,"  removing old proxy %d\n",next_proxy->gahp_proxy_id);
 			ProxiesByPath.remove( HashKey(next_proxy->proxy_filename) );
 			if ( my_gahp.uncacheProxy( next_proxy->gahp_proxy_id ) == false ) {
 				EXCEPT( "GAHP uncache command failed!" );
 			}
-			free( next_proxy->proxy_filename );
-			delete next_proxy;
+			/*free( next_proxy->proxy_filename );
+			delete next_proxy;*/
+			DeleteProxy (next_proxy);
 			continue;
 		}
+
+		/*
+
+		This is moved to RefreshProxy
+
+		// If this proxy is renewable via myproxy
+		// and we don't have the password for it yet,
+		// ask the Schedd
+
+		next_proxy->myproxy_entries.Rewind();
+		MyProxyEntry * mpe = NULL;
+		while (next_proxy->myproxy_entries.Next(mpe)) {
+			if (!mpe->myproxy_password) {
+				if (!GetMyProxyPasswordFromSchedD (mpe->cluster_id,
+													mpe->proc_id,
+													&(mpe->myproxy_password))) {
+					dprintf (D_ALWAYS,
+						"Unable to retrieve MyProxy password from SchedD for proxy (job: %d.%d) %s\n",
+						mpe->cluster_id,
+						mpe->proc_id,
+						next_proxy->proxy_filename);
+					// This is unfortunate, let's hope the proxy doesn't expire before we finish the job
+				}
+			}
+		}*/
 
 		int new_expiration = x509_proxy_expiration_time( next_proxy->proxy_filename );
 		// If the proxy is valid, and either it hasn't been cached in the
@@ -363,8 +501,16 @@ dprintf(D_FULLDEBUG,"  (re)caching proxy %d\n",next_proxy->gahp_proxy_id);
 				daemonCore->Reset_Timer( tid, 0 );
 			}
 
-		} 
-		
+		}
+
+
+		// Check whether to renew the proxy (renew 4 hrs beforehand)
+		if ( (new_expiration <= now + 4*60*60) && (!next_proxy->myproxy_entries.IsEmpty()) ) {
+			dprintf (D_FULLDEBUG, "About to RefreshProxyThruMyProxy() for %s\n", next_proxy->proxy_filename);
+			RefreshProxyThruMyProxy (next_proxy);
+		}
+
+
 		if ( new_expiration <= now + minProxy_time ) {
 			// This proxy has expired or is about to expire. Mark it
 			// as such and notify everyone who cares.
@@ -454,5 +600,307 @@ dprintf(D_FULLDEBUG,"  refreshing master proxy in gahp\n");
 dprintf(D_FULLDEBUG,"  will call CheckProxies again in %d seconds\n",next_check-now);
 	daemonCore->Reset_Timer( CheckProxies_tid, next_check - now );
 
+	return TRUE;
+}
+
+int RefreshProxyThruMyProxy(Proxy * proxy) {
+
+	char * proxy_filename = proxy->proxy_filename;
+	MyProxyEntry * myProxyEntry = NULL;
+
+
+	// Starting from the most recent myproxy entry
+	// Find an entry with a password
+	int found = FALSE;
+	proxy->myproxy_entries.Rewind();
+	while (proxy->myproxy_entries.Next (myProxyEntry)) {
+		if (myProxyEntry->myproxy_password ||
+			GetMyProxyPasswordFromSchedD (myProxyEntry->cluster_id,
+												myProxyEntry->proc_id,
+												&(myProxyEntry->myproxy_password))) {
+			found=TRUE;
+
+			//. Now move it to the front of the list
+			proxy->myproxy_entries.DeleteCurrent();
+			proxy->myproxy_entries.Prepend(myProxyEntry);
+			break;
+		}
+	}
+
+	if (!found) {
+		// We're screwed - can't get MyProxy passwords for any entry
+		return FALSE;
+	}
+
+  // Make sure we're not called more often than necessary and if
+  time_t now=time(NULL);
+  if ((myProxyEntry->get_delegation_pid != FALSE) || (now - myProxyEntry->last_invoked_time < 30)) {
+  	dprintf (D_ALWAYS, "proxy %s too soon or myproxy-get-delegation already started\n",proxy_filename);
+	return FALSE;
+  }
+  myProxyEntry->last_invoked_time=now;
+
+
+  // If you don't have a myproxy password, ask SchedD for it
+  if (!myProxyEntry->myproxy_password) {
+  	// Will there ever be a case when there is no MyProxy password needed at all?
+  	return FALSE;
+  }
+
+	// Initialize reaper, if needed
+	if (myproxyGetDelegationReaperId == 0 ) {
+		myproxyGetDelegationReaperId = daemonCore->Register_Reaper(
+					   "GetDelegationReaper",
+					   (ReaperHandler) &MyProxyGetDelegationReaper,
+					   "GetDelegation Reaper");
+  	}
+
+	// Set up environnment for myproxy-get-delegation
+	Env myEnv;
+	char buff[_POSIX_PATH_MAX];
+
+	if (myProxyEntry->myproxy_server_dn) {
+		sprintf (buff, "MYPROXY_SERVER_DN=%s", myProxyEntry->myproxy_server_dn);
+		myEnv.Put (buff);
+		dprintf (D_FULLDEBUG, "%s\n", buff);
+	}
+
+
+	sprintf (buff,"X509_USER_PROXY=%s", proxy_filename);
+	myEnv.Put (buff);
+	dprintf (D_FULLDEBUG, "%s\n", buff);
+
+
+    // Print password (this will end up in stdin for myproxy-get-delegation)
+	pipe (myProxyEntry->get_delegation_password_pipe);
+	write (myProxyEntry->get_delegation_password_pipe[1],
+		myProxyEntry->myproxy_password,
+		strlen (myProxyEntry->myproxy_password));
+	write (myProxyEntry->get_delegation_password_pipe[1], "\n", 1);
+
+
+	// Figure out user name;
+	char * username = NULL;
+	struct passwd * userinfo = getpwuid (geteuid());
+	if (userinfo) {
+		username = strdup (userinfo->pw_name);
+		// For some reason this gags...
+		//delete userinfo;
+	} else {
+		username = getlogin();
+	}
+
+
+	// Figure out myproxy host and port
+	char * myproxy_host = getHostFromAddr (myProxyEntry->myproxy_host);
+	int myproxy_port = getPortFromAddr (myProxyEntry->myproxy_host);
+
+	// args
+	char args[1000];
+	sprintf(args, "-v -o %s -s %s -d -t %d -S -l %s",
+		proxy_filename,
+		myproxy_host,
+		12,
+		username);
+
+
+	// Optional port argument
+	if (myproxy_port) {
+		sprintf (buff, " -p %d ", myproxy_port);
+		strcat (args, buff);
+	}
+
+	// Optional credential name argument
+	if (myProxyEntry->myproxy_credential_name) {
+		sprintf (buff, " -k %s ", myProxyEntry->myproxy_credential_name);
+		strcat (args, buff);
+	}
+
+	free (username);
+	free (myproxy_host);
+
+	// Create temporary file to store myproxy-get-delegation's stderr
+	myProxyEntry->get_delegation_err_filename = create_temp_file();
+	chmod (myProxyEntry->get_delegation_err_filename, 0600);
+	myProxyEntry->get_delegation_err_fd = open (myProxyEntry->get_delegation_err_filename,O_RDWR);
+	if (myProxyEntry->get_delegation_err_fd == -1) {
+		dprintf (D_ALWAYS, "Error opening file %s\n",myProxyEntry->get_delegation_err_filename);
+	}
+
+
+	int arrIO[3];
+	arrIO[0]=myProxyEntry->get_delegation_password_pipe[0]; //stdin
+	arrIO[1]=-1; //myProxyEntry->get_delegation_err_fd;
+	arrIO[2]=myProxyEntry->get_delegation_err_fd; // stderr
+
+	char * myproxy_get_delegation_pgm = param ("MYPROXY_GET_DELEGATION");
+	if (!myproxy_get_delegation_pgm) {
+		dprintf (D_ALWAYS, "MYPROXY_GET_DELEGATION not defined in config file\n");
+		return FALSE;
+	}
+
+
+	dprintf (D_ALWAYS, "Calling %s %s\n", myproxy_get_delegation_pgm, args);
+
+	int pid = daemonCore->Create_Process (
+					myproxy_get_delegation_pgm,
+					args,
+					PRIV_USER_FINAL,
+					myproxyGetDelegationReaperId,
+					FALSE,
+					myEnv.getDelimitedString(),
+					NULL,	// cwd
+					FALSE, // new proc group
+					NULL,  // socket inherit
+					arrIO); // in/out/err streams
+
+	free (myproxy_get_delegation_pgm);
+
+  	if (pid == FALSE) {
+		dprintf (D_ALWAYS, "Failed to run myproxy-get-delegation\n");
+
+		myProxyEntry->get_delegation_err_fd=-1;
+		myProxyEntry->get_delegation_pid=FALSE;
+		close (myProxyEntry->get_delegation_err_fd);
+		unlink (myProxyEntry->get_delegation_err_filename);// Remove the temporary file
+		free (myProxyEntry->get_delegation_err_filename);
+
+		close (myProxyEntry->get_delegation_password_pipe[0]);
+		close (myProxyEntry->get_delegation_password_pipe[1]);
+		myProxyEntry->get_delegation_password_pipe[0]=-1;
+		myProxyEntry->get_delegation_password_pipe[1]=-1;
+
+		return FALSE;
+	}
+
+  myProxyEntry->get_delegation_pid = pid;
+
+  return TRUE;
+
+}
+
+
+int MyProxyGetDelegationReaper(Service *, int exitPid, int exitStatus) {
+
+	// Find the right MyProxyEntry
+	Proxy *proxy=NULL;
+	MyProxyEntry *matched_entry=NULL;
+	int found = FALSE;
+
+	// Iterate through each proxy
+	ProxiesByPath.startIterations();
+	while ( ProxiesByPath.iterate( proxy ) != 0 ) {
+		// Iterate through all myproxy entries for the proxy
+		proxy->myproxy_entries.Rewind();
+		while (proxy->myproxy_entries.Next(matched_entry)) {
+			if (matched_entry->get_delegation_pid == exitPid) {
+				found = TRUE;
+				break;
+			}
+		}
+		if (found) {
+			break;
+		}
+	}
+
+ 	if (!found) {
+		dprintf (D_ALWAYS, "WEIRD! MyProxyManager::GetDelegationReaper unable to find entry for pid %d", exitPid);
+		return FALSE;
+	}
+
+	if (exitStatus == 0) {
+		dprintf (D_ALWAYS, "myproxy-get-delegation for proxy %s exited successfully\n", proxy->proxy_filename);
+		close (matched_entry->get_delegation_err_fd);
+	} else {
+		// This myproxyEntry is no good, move it to the back of the list
+		MyProxyEntry * myProxyEntry = NULL;
+		proxy->myproxy_entries.Rewind();
+		if (proxy->myproxy_entries.Next (myProxyEntry)) {
+			proxy->myproxy_entries.DeleteCurrent();
+			proxy->myproxy_entries.Append (myProxyEntry);
+		}
+
+		// In the case of an error, append the stderr stream of myproxy-get-delegation to log
+		close (matched_entry->get_delegation_err_fd);
+
+		char buff[500];
+		buff[0]='\0';
+		int fd = open (matched_entry->get_delegation_err_filename, O_RDONLY);
+		if (fd != -1) {
+			int bytes_read = read (fd, buff, 499);
+			close (fd);
+			buff[bytes_read]='\0';
+		} else {
+			dprintf (D_ALWAYS, "WEIRD! Cannot read err file %s\n", matched_entry->get_delegation_err_filename);
+		}
+
+		dprintf (D_ALWAYS, "myproxy-get-delegation for proxy %s, for job (%d,%d) exited with code %d, output (top):\n%s\n\n",
+			proxy->proxy_filename,
+			matched_entry->cluster_id,
+			matched_entry->proc_id,
+			WEXITSTATUS(exitStatus),
+			buff);
+
+	}
+
+
+	// Clean up
+	close (matched_entry->get_delegation_password_pipe[0]);
+	close (matched_entry->get_delegation_password_pipe[1]);
+	matched_entry->get_delegation_password_pipe[0]=-1;
+	matched_entry->get_delegation_password_pipe[1]=-1;
+
+	matched_entry->get_delegation_err_fd=-1;
+	matched_entry->get_delegation_pid=FALSE;
+	unlink (matched_entry->get_delegation_err_filename);// Remove the temporary file
+	free (matched_entry->get_delegation_err_filename);
+	matched_entry->get_delegation_err_filename=NULL;
+
+   return TRUE;
+}
+
+int GetMyProxyPasswordFromSchedD (int cluster_id, int proc_id, char ** password) {
+
+	// This might seem not necessary, but it IS
+	// For some reason you can't just pass cluster_id to sock->code() directly!!!!
+	int cluster, proc;
+	cluster = cluster_id;
+	proc = proc_id;
+
+	dprintf ( D_FULLDEBUG, " GetMyProxyPasswordFromSchedD %d, %d\n", cluster_id, proc_id);
+
+	// Get At Schedd
+	Daemon	schedd( DT_SCHEDD );
+	if( ! schedd.locate() ) {
+		dprintf( D_ALWAYS, "GetMyProxyPasswordFromSchedD: Can't find address of local schedd\n" );
+		return FALSE;
+	}
+
+	// Start command
+	Sock* sock;
+	if (!(sock = schedd.startCommand( GET_MYPROXY_PASSWORD, Stream::reli_sock, 0))) {
+		dprintf( D_ALWAYS, "GetMyProxyPasswordFromSchedD: Could not connect to local schedd\n" );
+		return FALSE;
+	}
+
+	sock->encode();
+
+	if (!sock->code (cluster) || !sock->code(proc)) {
+		dprintf( D_ALWAYS, "GetMyProxyPasswordFromSchedD: Could not encode clusterId, procId\n" );
+		return FALSE;
+	}
+
+	sock->eom();
+	sock->decode();
+
+	if (!sock->code (*password)) {
+		dprintf( D_ALWAYS, "GetMyProxyPasswordFromSchedD: Can't retrieve password\n" );
+		return FALSE;
+
+	}
+
+	sock->eom();
+	sock->close();
+	delete sock;
 	return TRUE;
 }
--- src/condor_gridmanager/proxymanager.h
+++ src/condor_gridmanager/proxymanager.h
@@ -3,21 +3,21 @@
  *
  * See LICENSE.TXT for additional notices and disclaimers.
  *
- * Copyright (c)1990-1998 CONDOR Team, Computer Sciences Department, 
- * University of Wisconsin-Madison, Madison, WI.  All Rights Reserved.  
- * No use of the CONDOR Software Program Source Code is authorized 
- * without the express consent of the CONDOR Team.  For more information 
- * contact: CONDOR Team, Attention: Professor Miron Livny, 
- * 7367 Computer Sciences, 1210 W. Dayton St., Madison, WI 53706-1685, 
+ * Copyright (c)1990-1998 CONDOR Team, Computer Sciences Department,
+ * University of Wisconsin-Madison, Madison, WI.  All Rights Reserved.
+ * No use of the CONDOR Software Program Source Code is authorized
+ * without the express consent of the CONDOR Team.  For more information
+ * contact: CONDOR Team, Attention: Professor Miron Livny,
+ * 7367 Computer Sciences, 1210 W. Dayton St., Madison, WI 53706-1685,
  * (608) 262-0856 or miron@cs.wisc.edu.
  *
- * U.S. Government Rights Restrictions: Use, duplication, or disclosure 
- * by the U.S. Government is subject to restrictions as set forth in 
- * subparagraph (c)(1)(ii) of The Rights in Technical Data and Computer 
- * Software clause at DFARS 252.227-7013 or subparagraphs (c)(1) and 
- * (2) of Commercial Computer Software-Restricted Rights at 48 CFR 
- * 52.227-19, as applicable, CONDOR Team, Attention: Professor Miron 
- * Livny, 7367 Computer Sciences, 1210 W. Dayton St., Madison, 
+ * U.S. Government Rights Restrictions: Use, duplication, or disclosure
+ * by the U.S. Government is subject to restrictions as set forth in
+ * subparagraph (c)(1)(ii) of The Rights in Technical Data and Computer
+ * Software clause at DFARS 252.227-7013 or subparagraphs (c)(1) and
+ * (2) of Commercial Computer Software-Restricted Rights at 48 CFR
+ * 52.227-19, as applicable, CONDOR Team, Attention: Professor Miron
+ * Livny, 7367 Computer Sciences, 1210 W. Dayton St., Madison,
  * WI 53706-1685, (608) 262-0856 or miron@cs.wisc.edu.
 ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
 
@@ -30,6 +30,27 @@
 
 #include "gahp-client.h"
 
+struct MyProxyEntry {
+	char * myproxy_host;
+	//int myproxy_port;
+	char * myproxy_server_dn;
+	char * myproxy_credential_name;
+
+	char * myproxy_password;
+
+	// For requesting MyProxy password
+	int cluster_id;
+	int proc_id;
+
+	// get-delegation-invocation details
+	time_t last_invoked_time;
+	int get_delegation_pid;
+	int get_delegation_err_fd;
+	char  * get_delegation_err_filename ;
+	int get_delegation_password_pipe[2]; // to send pwd via stdin
+};
+
+
 struct Proxy {
 	char *proxy_filename;
 	int expiration_time;
@@ -37,6 +58,8 @@ struct Proxy {
 	int gahp_proxy_id;
 	int num_references;
 	SimpleList<int> notification_tids;
+	
+	SimpleList<MyProxyEntry*> myproxy_entries;
 };
 
 #define PROXY_NEAR_EXPIRED( p ) \
@@ -54,6 +77,15 @@ bool UseSingleProxy( const char *proxy_path,
 
 Proxy *AcquireProxy( const char *proxy_path, int notify_tid = -1 );
 void ReleaseProxy( Proxy *proxy, int notify_tid = -1 );
+void DeleteProxy (Proxy *& proxy);
+void DeleteMyProxyEntry (MyProxyEntry *& proxy);
+
+int RefreshProxyThruMyProxy (Proxy * proxy);
+int MyProxyGetDelegationReaper(Service *, int exitPid, int exitStatus);
+int GetMyProxyPasswordFromSchedD (int cluster_id, int proc_id, char ** password);
+extern int myproxyGetDelegationReaperId;
+
+//int SetMyProxyHostForProxy ( const char* hostport, const char * server_dn, const char * myproxy_pwd, const Proxy * proxy );
 
 void doCheckProxies();
 
