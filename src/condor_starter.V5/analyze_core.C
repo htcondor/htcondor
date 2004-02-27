c9298799cd0b90fcf417352b93d2e8d87f7177ea
psilord <psilord>
2004-02-27 23:11:45 +0000

    + Initial revision.
    + This file is how we detect of a core file is valid. For our purposes,
    valid means present and not a symlink.
    
    + This file replaces the behavior of:
    analyze_core.Darwin.C
    analyze_core.HPUX.C
    analyze_core.IRIX.C
    analyze_core.IRIX53.C
    analyze_core.IRIX62.C
    analyze_core.LINUX.C
    analyze_core.OSF1.C
    analyze_core.Solaris.C
    
    Which used to be in the condor_starter.V5/ directory.
    
    + Some of the above files actually were this implementation that is
    in this file, but I consolidated it all. Other files did complicated
    magic to figure out if the core file was truly valid as to not ship over
    incomplete or corrupt core files. However, I've made the decision that
    the extra complexity isn't worth it. Simplicity is better.
    
    + Fixed a security hole in the previous implementations of this code
    where if a standard universe job could make a symlink of a core file
    to another file on the machine in the execute/dir_<pid> directory,
    Condor(as root) would happily bring it back for you.

---
+++ src/condor_starter.V5/analyze_core.C
@@ -0,0 +1,59 @@
+/***************************Copyright-DO-NOT-REMOVE-THIS-LINE**
+  *
+  * Condor Software Copyright Notice
+  * Copyright (C) 1990-2004, Condor Team, Computer Sciences Department,
+  * University of Wisconsin-Madison, WI.
+  *
+  * This source code is covered by the Condor Public License, which can
+  * be found in the accompanying LICENSE.TXT file, or online at
+  * www.condorproject.org.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  * AND THE UNIVERSITY OF WISCONSIN-MADISON "AS IS" AND ANY EXPRESS OR
+  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  * WARRANTIES OF MERCHANTABILITY, OF SATISFACTORY QUALITY, AND FITNESS
+  * FOR A PARTICULAR PURPOSE OR USE ARE DISCLAIMED. THE COPYRIGHT
+  * HOLDERS AND CONTRIBUTORS AND THE UNIVERSITY OF WISCONSIN-MADISON
+  * MAKE NO MAKE NO REPRESENTATION THAT THE SOFTWARE, MODIFICATIONS,
+  * ENHANCEMENTS OR DERIVATIVE WORKS THEREOF, WILL NOT INFRINGE ANY
+  * PATENT, COPYRIGHT, TRADEMARK, TRADE SECRET OR OTHER PROPRIETARY
+  * RIGHT.
+  *
+  ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
+#include "condor_common.h"
+#include "condor_debug.h"
+#include "condor_constants.h"
+
+int
+core_is_valid( char *core_fn )
+{
+	struct stat buf;
+
+	/* be cognizant of the file that we are checking isn't a
+		symlink. If it is, then the user is most likely trying
+		to perform an exploit to have Condor retrieve any file
+		on the system. Condor must return the core file via root
+		privledges because a user doesn't have search permissions
+		to the sand box of the execute directory where the core
+		file will end up */
+
+	if (lstat(core_fn, &buf) == 0)
+	{
+		/* Hmm... Something is here, make sure it isn't a symlink */
+		if ( ! S_ISLNK(buf.st_mode) )
+		{
+			/* looks like something real.  If it turns out the file is
+				corrupt, then let the user figure it out when it they
+				inspect it. This is no different than if the core file was
+				corrupt to begin with and created outside of Condor 
+				altogether. */
+			return TRUE;
+		}
+	}
+
+	/* If this file was a symlink or not present, mark it as not valid. */
+	return FALSE;
+}
+
+
+
