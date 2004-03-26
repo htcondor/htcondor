523a772cb26edca6ede6542263c0fc0ffd18c187
nleroy <nleroy>
2004-03-26 21:23:19 +0000

    Defines system level types; currently just the (u,)int64_t types, but hopefully more in the future (as required).

---
+++ src/condor_includes/condor_sys_types.h
@@ -0,0 +1,44 @@
+/***************************Copyright-DO-NOT-REMOVE-THIS-LINE**
+  *
+  * Condor Software Copyright Notice
+  * Copyright (C) 1990-2004, Condor Team, Computer Sciences Department,
+  * University of Wisconsin-Madison, WI.
+  *
+  * This source code is covered by the Condor Public License, which can
+  * be found in the accompanying LICENSE.TXT file, or online at
+  * www.condorproject.org.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  * AND THE UNIVERSITY OF WISCONSIN-MADISON "AS IS" AND ANY EXPRESS OR
+  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  * WARRANTIES OF MERCHANTABILITY, OF SATISFACTORY QUALITY, AND FITNESS
+  * FOR A PARTICULAR PURPOSE OR USE ARE DISCLAIMED. THE COPYRIGHT
+  * HOLDERS AND CONTRIBUTORS AND THE UNIVERSITY OF WISCONSIN-MADISON
+  * MAKE NO MAKE NO REPRESENTATION THAT THE SOFTWARE, MODIFICATIONS,
+  * ENHANCEMENTS OR DERIVATIVE WORKS THEREOF, WILL NOT INFRINGE ANY
+  * PATENT, COPYRIGHT, TRADEMARK, TRADE SECRET OR OTHER PROPRIETARY
+  * RIGHT.
+  *
+  ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
+#ifndef CONDOR_SYS_TYPES_H
+#define CONDOR_SYS_TYPES_H
+
+
+/* Define 64 bit integer types */
+# if defined( HAS_INT64_T )
+   /* int64_t already defined; do nothing */
+
+# elif defined( HAS___INT64 )
+   /* Use the compiler's __int64 type for int64_t */
+   typedef __int64 int64_t;
+   typedef unsigned __int64 uint64_t;
+#  define HAS_INT64_T
+
+# elif defined( HAS_LONGLONG )
+   /* Use the compiler's long long type for int64_t */
+   typedef long long int64_t;
+   typedef unsigned long long uint64_t;
+#  define HAS_INT64_T
+# endif
+
+#endif /* CONDOR_SYS_TYPES_H */
