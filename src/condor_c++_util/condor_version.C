1dcfac1c7e0653451727c929a7b0aaa82c617f13
epaulson <epaulson>
2004-04-29 19:39:34 +0000

    6.7.0 is out the door...

--- src/condor_c++_util/condor_version.C
+++ src/condor_c++_util/condor_version.C
@@ -33,7 +33,7 @@
    will EXCEPT at startup time.  
 */
 
-static char* CondorVersionString = "$CondorVersion: 6.7.0 " __DATE__ " $";
+static char* CondorVersionString = "$CondorVersion: 6.7.1 " __DATE__ " PRE-RELEASE-UWCS-INTERNAL $";
 
 /* 
    This is some wisdom from Cygnus's web page.  If you just try to use
