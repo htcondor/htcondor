27d4a9bd79a0a980d555b04de8f63fee96f430e1
wright <wright>
2004-05-14 20:54:32 +0000

    cleaned up and fixed carey's last set of changes: - iwdIsChanged() is always going to return the same thing during a   given instances of the starter, so there's no reason to go through   all the effort to loop over the files, call basename(), do all sorts   of static buffer manipulations, etc, etc if it's false.  instead,   just test it once, and only if it's true do we go through the work   of changing the paths... - removed some unnecessary (and dangerous) static buffers - put things on the stack whenever humanly possible

--- src/condor_starter.V6.1/java_proc.C
+++ src/condor_starter.V6.1/java_proc.C
@@ -52,7 +52,7 @@ int JavaProc::StartJob()
 	
 	char java_cmd[_POSIX_PATH_MAX];
 	char java_args[_POSIX_ARG_MAX];
-	char jarfiles[ATTRLIST_MAX_EXPRESSION];
+	char* jarfiles = NULL;
 
 	ExprTree  *tree;
 	char	  *tmp_args;
@@ -64,41 +64,51 @@ int JavaProc::StartJob()
 	// (in the execute directory)
 	// otherwise use the original name
 
+	StringList jarfiles_orig_list;
+	StringList jarfiles_local_list;
+	StringList* jarfiles_final_list = NULL;
 
-	StringList jarfiles_final_list;
-	if(JobAd->LookupString(ATTR_JAR_FILES,jarfiles)==1) {
-		StringList * jarfiles_list = new StringList(jarfiles);
-		
+	if( JobAd->LookupString(ATTR_JAR_FILES,&jarfiles) ) {
+		jarfiles_orig_list.initializeFromString( jarfiles );
+		free( jarfiles );
+		jarfiles = NULL;
+
 		char * jarfile_name;
 		char * base_name;
-		char local_name[_POSIX_ARG_MAX];
 		struct stat stat_buff;
+		if( Starter->jic->iwdIsChanged() ) {
+				// If the job's IWD has been changed (because we're
+				// running in the sandbox due to file transfer), we
+				// need to use a local version of the path to the jar
+				// files, not the full paths from the submit machine. 
+			jarfiles_orig_list.rewind();
+			while( (jarfile_name = jarfiles_orig_list.next()) ) {
+					// Construct the local name
+				base_name = basename( jarfile_name );
+				MyString local_name = execute_dir;
+				local_name += DIR_DELIM_CHAR;
+				local_name += base_name; 
 
-		jarfiles_list->rewind();
-		while ((jarfile_name = jarfiles_list->next())) {
-		  base_name = basename (jarfile_name);
+				if( stat(local_name.Value(), &stat_buff) == 0 ) {
+						// Jar file exists locally, use local name
+					jarfiles_local_list.append( local_name.Value() );
+				} else {
+						// Use the original name
+					jarfiles_local_list.append (jarfile_name);
+				}
+			} // while(jarfiles_orig_list)
 
-		  // Construct the local name
-		  sprintf (local_name, "%s%c%s",
-			   execute_dir,
-			   DIR_DELIM_CHAR,
-			   base_name);
+				// jarfiles_local_list is our real copy...
+			jarfiles_final_list = &jarfiles_local_list;
 
-		  if ((Starter->jic->iwdIsChanged()) && (stat (local_name, &stat_buff) == 0)) {
-		    // Jar file exists locally, use local name
-		    jarfiles_final_list.append (local_name);
+		} else {  // !iwdIsChanged()
 
-		  } else {
-		    // Use the original name
-		    jarfiles_final_list.append (jarfile_name);
-		  }
-		
-		} // elihw jarfiles_list
-		
-		delete jarfiles_list;
+				// just use jarfiles_orig_list as our real copy...
+			jarfiles_final_list = &jarfiles_orig_list;
+		}			
 	}
 
-	if(!java_config(java_cmd,java_args,&jarfiles_final_list)) {
+	if( !java_config(java_cmd,java_args,jarfiles_final_list) ) {
 		dprintf(D_FAILURE|D_ALWAYS,"JavaProc: Java is not configured!\n");
 		return 0;
 	}
