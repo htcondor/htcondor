d4db17c8b04b060ac5346fc818f6eaa7f514f122
wright <wright>
2004-05-17 05:56:35 +0000

    big changes to the OSX version.  unfortunately, some of the things that worked fine on my 10.3.3 laptop didn't work at all on 10.2.x and we ended up dying with SIGBUS.  now, things are a lot better: - dynamically check to see if we've got the symbol for the happy   method that does all the work for us. - if not, we now have a fall-back method that should work on all   versions of darwin, but requires more effort (using the   kernel-supplied argv table, which also includes the environment and   the full path that was given to exec()) - use MAXPATHLEN (1024) everywhere instead of _POSIX_PATH_MAX (255) - cleaned up header file junk by adding support to condor_constants.h   for _CONDOR_NO_TRUE_FALSE so that we don't define TRUE/FALSE and can   use the proper system headers without conflict.  unfortunately, that   meant i had to move condor_common.h to inside the per-platform code. - more careful call to strdup(), and return value handling in general.

--- src/condor_util_lib/get_exec_path.c
+++ src/condor_util_lib/get_exec_path.c
@@ -21,9 +21,6 @@
   *
   ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
 
-#include "condor_common.h" 
-#include "condor_debug.h" 
-
 /*
 
   This file defines getExecPath(), which tries to return the full path
@@ -42,20 +39,24 @@
 */
 
 #if defined( LINUX )
+
+#include "condor_common.h" 
+#include "condor_debug.h" 
+
 char*
 linux_getExecPath()
 {
 	int rval;
 	char* full_path;
-	char path_buf[_POSIX_PATH_MAX];
-	rval = readlink( "/proc/self/exe", path_buf, _POSIX_PATH_MAX );
+	char path_buf[MAXPATHLEN];
+	rval = readlink( "/proc/self/exe", path_buf, MAXPATHLEN );
 	if( rval < 0 ) {
 		dprintf( D_ALWAYS,"getExecPath: "
 				 "readlink(\"/proc/self/exe\") failed: errno %d (%s)\n",
 				 errno, strerror(errno) );
 		return NULL;
 	}
-	if( rval == _POSIX_PATH_MAX ) {
+	if( rval == MAXPATHLEN ) {
 		dprintf( D_ALWAYS,"getExecPath: "
 				 "unable to find full path from /proc/self/exe\n" );
 		return NULL;
@@ -70,6 +71,10 @@ linux_getExecPath()
 
 
 #if defined( Solaris )
+
+#include "condor_common.h" 
+#include "condor_debug.h" 
+
 char*
 solaris_getExecPath()
 {
@@ -87,7 +92,7 @@ solaris_getExecPath()
 		  man pages say we can prepend the output of getcwd() to find
 		  the real value...
 		*/ 
-	cwd = getcwd( NULL, _POSIX_PATH_MAX );
+	cwd = getcwd( NULL, MAXPATHLEN );
 	size = strlen(cwd) + strlen(name) + 2;
 	full_path = malloc(size);
 	snprintf( full_path, size, "%s/%s", cwd, name );
@@ -100,6 +105,8 @@ solaris_getExecPath()
 
 #if defined( WIN32 )
 
+#include "condor_common.h" 
+#include "condor_debug.h" 
 #include <psapi.h>
 
 char*
@@ -150,44 +157,123 @@ win32_getExecPath()
 
 #if defined( Darwin )
 
-/* We should include <mach-o/dyld.h> for this prototype, but
+/* We need to include <mach-o/dyld.h> for some prototypes, but
    unfortunately, that header conflicts with our definitions of FALSE
-   and TRUE. :( So, we just prototype it directly here.  This is lame,
-   but it's easier than dealing with header file incompatibilities.
-   Also, this method only exists on OS X 10.2 and higher.  So, this
-   code will not work on older versions of OS X.
+   and TRUE. :( So, we hack around that by defining a special thing
+   that tells our condor_constants.h header not to mess with the
+   definition of FALSE and TRUE.  It doesn't effect anything in the
+   rest of this file, and it's important to get the right stuff from
+   the system headers for what we're doing here for OS X.
    Derek Wright <wright@cs.wisc.edu> 2004-05-13
 */
-extern int _NSGetExecutablePath( char *buf, size_t *bufsize );
+
+#include <mach-o/dyld.h>
+#define _CONDOR_NO_TRUE_FALSE 1
+#include "condor_common.h" 
+#include "condor_debug.h" 
+#include <crt_externs.h>
+
+typedef int (*NSGetExecutablePathProcPtr)(char *buf, size_t *bufsize);
+
+int
+darwin_NSGetExecutablePath_getExecPath( char* buf, size_t* bufsize )
+{
+	return ((NSGetExecutablePathProcPtr)NSAddressOfSymbol(NSLookupAndBindSymbol("__NSGetExecutablePath")))(buf, bufsize);
+}
+
+int
+darwin_NSGetArgv_getExecPath( char* buf, size_t* bufsize )
+{
+	int len = MAXPATHLEN;
+	int argc = 0;
+    char *path;
+    char **ptr;
+
+		/* _NSGetArgv() returns a pointer to the argv array.  After
+		   all the args, there should be a NULL in the table.  Next
+		   comes the environment, and another NULL.  After that, the
+		   next entry will be the value of the path given to exec().
+		*/
+	ptr = (char**)(*(_NSGetArgv()));
+		/* Get the value of our original argc. */
+	argc = *(_NSGetArgc());
+		/* skip over all the args: */
+	ptr += argc;
+		/* ptr should be pointing at the NULL terminating the args. */
+	if( *ptr != NULL ) {
+		return -1;
+	} else {
+			/* skip the NULL */
+		ptr += 1;
+	}
+		/* now, skip the environment, searching for the next NULL. */
+	while( *ptr != NULL ) {
+		ptr += 1;
+	}
+		/* ptr should be pointing at the NULL terminating the env. */
+	if( *ptr != NULL ) {
+		return -1;
+	} else { 
+			/* skip the NULL */
+		ptr += 1;
+	}
+		/* Now we're finally pointing at the thing we want */
+	path = *ptr;
+	len = strlen( path ) + 1;
+	if( len > *bufsize ) {
+		return -1;
+	}
+	strncpy( buf, path, len );
+	*bufsize = len;
+	return len;
+}
+
 
 char*
 darwin_getExecPath()
 {
-	char buf[_POSIX_PATH_MAX];
-	char full_buf[_POSIX_PATH_MAX];
+	char buf[MAXPATHLEN];
+	char full_buf[MAXPATHLEN];
 	char* path = NULL;
-	size_t size = _POSIX_PATH_MAX;
+	size_t size = MAXPATHLEN;
 	int rval = 0;
 
-	memset( buf, '\0', _POSIX_PATH_MAX );
-	memset( full_buf, '\0', _POSIX_PATH_MAX );
+    memset( buf, '\0', MAXPATHLEN );
+    memset( full_buf, '\0', MAXPATHLEN );
 
-	rval = _NSGetExecutablePath( buf, &size );
-	if( rval < 0 ) {
-		dprintf( D_ALWAYS, 
-				 "getExecPath(): _NSGetExecutablePath() failed: "
-				 "%s (errno %d)\n", strerror(errno), errno );
-		return NULL;
+		// Figure out dynamically if we can use the special method or
+		// if we have to do it ourselves...
+	if( NSIsSymbolNameDefined("__NSGetExecutablePath") ) {
+			// We've got the handy function that does all this work
+			// for us, so use it.
+		rval = darwin_NSGetExecutablePath_getExecPath( buf, &size );
+	    if( rval < 0 ) {
+			dprintf( D_ALWAYS, "getExecName(): NSGetExecutablePath() "
+					 "returned failure (%d): %s (errno %d)\n", 
+					 rval, strerror(errno), errno );
+			return NULL;
+	    }
+	} else {
+			// No handy function that does it for us, so instead we
+			// use a slightly more convoluted method...
+		rval = darwin_NSGetArgv_getExecPath( buf, &size );
+		if( rval < 0 ) {		
+			dprintf( D_ALWAYS, "getExecName(): NSGetArgv() "
+					 "returned failure (%d): %s (errno %d)\n", 
+					 rval, strerror(errno), errno );
+			return NULL;
+	    }
 	}
-
 		/* 
-		  Convert to a canonicalized path, resolving '/./' and '/../'
+		   Either way, the resulting path might still have some
+		   relative paths in it.  So, convert to a canonicalized path,
+		   resolving '/./' and '/../'
 		*/
 	path = realpath( buf, full_buf );
 	if( path ) {
-		return strdup( path );
+		path = strdup( full_buf );
+		return path;
 	}
-
 	dprintf( D_ALWAYS,
 			 "getExecName(): realpath() returned failure on \"%s\": "
 			 "%s (errno %d)\n", full_buf, strerror(errno), errno );
@@ -195,6 +281,7 @@ darwin_getExecPath()
 }
 #endif /* defined(Darwin) */
 
+#include "condor_common.h" 
 
 /*
   Now, the public method that just invokes the right platform-specific
@@ -203,16 +290,16 @@ darwin_getExecPath()
 char*
 getExecPath( void )
 {
+	char* rval = NULL;
 #if defined( LINUX )
-	return linux_getExecPath();
+	rval = linux_getExecPath();
 #elif defined( Solaris )
-	return solaris_getExecPath();
+	rval= solaris_getExecPath();
 #elif defined( Darwin )
-	return darwin_getExecPath();
+	rval = darwin_getExecPath();
 #elif defined( WIN32 )
-	return win32_getExecPath();
-#else
-	return NULL;
+	rval = win32_getExecPath();
 #endif
+	return rval;
 }
 
