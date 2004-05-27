dc760f284cab02463bbdf66735f5e3e2bac4a8e8
roy <roy>
2004-05-27 22:48:32 +0000

    Changed string to std::string, so we don't need to declare that we are using the std namespace.

--- src/condor_classad.V6/collectionClient.h
+++ src/condor_classad.V6/collectionClient.h
@@ -32,7 +32,7 @@ class ClientTransaction;
 
 BEGIN_NAMESPACE( classad )
 
-typedef hash_map<string,ClientTransaction*,StringHash> LocalXactionTable;
+typedef hash_map<std::string,ClientTransaction*,StringHash> LocalXactionTable;
 
 class ClassAdCollectionClient : public ClassAdCollectionInterface {
 public:
@@ -40,59 +40,59 @@ public:
 	virtual ~ClassAdCollectionClient( );
 
 		// Initialization and recovery
-	virtual bool InitializeFromLog( const string &filename );
+	virtual bool InitializeFromLog( const std::string &filename );
 	void Recover( int &committed, int &aborted, int &unknown );
 
 		// View management
 	virtual bool CreateSubView( const ViewName &viewName,
 				const ViewName &parentViewName,
-				const string &constraint, const string &rank,
-				const string &partitionExprs );
+				const std::string &constraint, const std::string &rank,
+				const std::string &partitionExprs );
 	virtual bool CreatePartition( const ViewName &viewName,
 				const ViewName &parentViewName,
-				const string &constraint, const string &rank,
-				const string &partitionExprs, ClassAd *rep );
+				const std::string &constraint, const std::string &rank,
+				const std::string &partitionExprs, ClassAd *rep );
 	virtual bool DeleteView( const ViewName &viewName );
 	virtual bool SetViewInfo( const ViewName &viewName, 
-				const string &constraint, const string &rank, 
-				const string &partitionAttrs );
+				const std::string &constraint, const std::string &rank, 
+				const std::string &partitionAttrs );
 	virtual bool GetViewInfo( const ViewName &viewName, ClassAd *&viewInfo );
 		// Child view interrogation
 	virtual bool GetSubordinateViewNames( const ViewName &viewName,
-				vector<string>& views );
+				vector<std::string>& views );
 	virtual bool GetPartitionedViewNames( const ViewName &viewName,
-				vector<string>& views );
+				vector<std::string>& views );
 	virtual bool FindPartitionName( const ViewName &viewName, ClassAd *rep, 
 				ViewName &partition );
 
 
 		// ClassAd manipulation
-	virtual bool AddClassAd( const string &key, ClassAd *newAd );
-	virtual bool UpdateClassAd( const string &key, ClassAd *updateAd );
-	virtual bool ModifyClassAd( const string &key, ClassAd *modifyAd );
-	virtual bool RemoveClassAd( const string &key );
+	virtual bool AddClassAd( const std::string &key, ClassAd *newAd );
+	virtual bool UpdateClassAd( const std::string &key, ClassAd *updateAd );
+	virtual bool ModifyClassAd( const std::string &key, ClassAd *modifyAd );
+	virtual bool RemoveClassAd( const std::string &key );
 		// ClassAd interrogation
-	virtual ClassAd *GetClassAd(const string &key );
+	virtual ClassAd *GetClassAd(const std::string &key );
 
 
 		// Transaction management
-	virtual bool OpenTransaction( const string &transactionName );
-	virtual bool CloseTransaction(const string &transactionName,bool commit,
+	virtual bool OpenTransaction( const std::string &transactionName );
+	virtual bool CloseTransaction(const std::string &transactionName,bool commit,
 				int &outcome);
 
-	virtual bool IsMyActiveTransaction( const string &transactionName );
-	virtual void GetMyActiveTransactions( vector<string>& );
-	virtual bool IsActiveTransaction( const string &transactionName );
-	virtual bool GetAllActiveTransactions( vector<string>& );
-	virtual bool IsMyPendingTransaction( const string &transactionName );
-	virtual void GetMyPendingTransactions( vector<string>& );
-	virtual bool IsCommittedTransaction( const string &transactionName );
-	virtual bool IsMyCommittedTransaction( const string &transactionName );
-	virtual void GetMyCommittedTransactions( vector<string>& );
-	virtual bool GetAllCommittedTransactions( vector<string>& );
+	virtual bool IsMyActiveTransaction( const std::string &transactionName );
+	virtual void GetMyActiveTransactions( vector<std::string>& );
+	virtual bool IsActiveTransaction( const std::string &transactionName );
+	virtual bool GetAllActiveTransactions( vector<std::string>& );
+	virtual bool IsMyPendingTransaction( const std::string &transactionName );
+	virtual void GetMyPendingTransactions( vector<std::string>& );
+	virtual bool IsCommittedTransaction( const std::string &transactionName );
+	virtual bool IsMyCommittedTransaction( const std::string &transactionName );
+	virtual void GetMyCommittedTransactions( vector<std::string>& );
+	virtual bool GetAllCommittedTransactions( vector<std::string>& );
 
 		// Server connection and disconnection
-	bool Connect( const string &address, int port, bool UseTCP=true );
+	bool Connect( const std::string &address, int port, bool UseTCP=true );
 	bool Disconnect( );
 
 
@@ -106,22 +106,22 @@ private:
 
 		// helper functions
 	bool SendOpToServer( int, ClassAd *, int, ClassAd *& );
-	bool _GetViewNames( int, const ViewName &, vector<string>& );
-	bool _CheckTransactionState( int, const string &, Value & );
-	bool _GetAllxxxTransactions( int, vector<string>& );
-	bool DoCommitProtocol( const string &, int &, ClientTransaction * );
-	bool GetServerXactionState( const string &, int & );
+	bool _GetViewNames( int, const ViewName &, vector<std::string>& );
+	bool _CheckTransactionState( int, const std::string &, Value & );
+	bool _GetAllxxxTransactions( int, vector<std::string>& );
+	bool DoCommitProtocol( const std::string &, int &, ClientTransaction * );
+	bool GetServerXactionState( const std::string &, int & );
 
 		// info about transactions
 	LocalXactionTable	localXactionTable;
 
 		// current connection to the server
-	string	serverAddr;
+	std::string	serverAddr;
 	int		serverPort;
 	Sock	*serverSock;
 
 		// to manage the log file ...
-	string	logFileName;
+	std::string	logFileName;
 	FILE	*log_fp;
 };
 
