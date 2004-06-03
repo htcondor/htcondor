fa1d980dde61a3aa9d9ae92982ebc88fab8c220d
psilord <psilord>
2004-06-03 17:41:12 +0000

    + merged V6_6-imake-branch as tagged at V6_6-imake-merge-1 into V6_6-branch

--- config/Global.h
+++ config/Global.h
@@ -6,20 +6,75 @@
 #define YES	1
 #define NO	0
 
-#define __CONCAT(a,b) a ## b
-#define concat(a,b) __CONCAT(a,b)
+/* SimpleConcat - concatenates two strings.  */
+#ifndef SimpleConcat
+#if (__STDC__ && !defined(UnixCpp)) || defined(AnsiCpp) || defined(__ANSI_CPP__)
+#define SimpleConcat(a,b)a##b
+#else
+#define SimpleConcat(a,b)a/**/b
+#endif
+#endif /* SimpleConcat */
 
-#define __CONCAT3(a,b,c) a ## b ## c
-#define concat3(a,b,c) __CONCAT3(a,b,c)
+/* The actual Concat rule which is robust to preprocessor semantics */
+#ifndef Concat
+#define Concat(a,b)SimpleConcat(a,b)
+#endif /* Concat */
 
-#define __CONCAT4(a,b,c,d) a ## b ## c ## d
-#define concat4(a,b,c,d) __CONCAT4(a,b,c,d)
+/* SimpleConcat3 - concatenates three strings.  */
+#ifndef Concat3
+#if (__STDC__ && !defined(UnixCpp)) || defined(AnsiCpp) || defined(__ANSI_CPP__)
+#define SimpleConcat3(a,b,c)a##b##c
+#else
+#define SimpleConcat3(a,b,c)a/**/b/**/c
+#endif
+#endif /* SimpleConcat3 */
 
-#define __CONCAT5(a,b,c,d,e) a ## b ## c ## d ## e
-#define concat5(a,b,c,d,e) __CONCAT5(a,b,c,d,e)
+/* The actual Concat3 rule which is robust to preprocessor semantics */
+#ifndef Concat3
+#define Concat3(a,b,c)SimpleConcat3(a,b,c)
+#endif /* Concat3 */
 
-#define __CONCAT6(a,b,c,d,e,f) a ## b ## c ## d ## e ## f
-#define concat6(a,b,c,d,e,f) __CONCAT6(a,b,c,d,e,f)
+/* SimpleConcat4 - concatenates four strings.  */
+#ifndef SimpleConcat4
+#if (__STDC__ && !defined(UnixCpp)) || defined(AnsiCpp) || defined(__ANSI_CPP__)
+#define SimpleConcat4(a,b,c,d)a##b##c##d
+#else
+#define SimpleConcat4(a,b,c,d)a/**/b/**/c/**/d
+#endif
+#endif /* SimpleConcat4 */
+
+/* The actual Concat4 rule which is robust to preprocessor semantics */
+#ifndef Concat4
+#define Concat4(a,b,c,d)SimpleConcat4(a,b,c,d)
+#endif /* Concat4 */
+
+/* SimpleConcat5 - concatenates five strings.  */
+#ifndef SimpleConcat5
+#if (__STDC__ && !defined(UnixCpp)) || defined(AnsiCpp) || defined(__ANSI_CPP__)
+#define SimpleConcat5(a,b,c,d,e)a##b##c##d##e
+#else
+#define SimpleConcat5(a,b,c,d,e)a/**/b/**/c/**/d/**/e
+#endif
+#endif /* SimpleConcat5 */
+
+/* The actual Concat5 rule which is robust to preprocessor semantics */
+#ifndef Concat5
+#define Concat5(a,b,c,d,e)SimpleConcat5(a,b,c,d,e)
+#endif /* Concat5 */
+
+/* SimpleConcat6 - concatenates six strings.  */
+#ifndef SimpleConcat6
+#if (__STDC__ && !defined(UnixCpp)) || defined(AnsiCpp) || defined(__ANSI_CPP__)
+#define SimpleConcat6(a,b,c,d,e,f)a##b##c##d##e##f
+#else
+#define SimpleConcat6(a,b,c,d,e,f)a/**/b/**/c/**/d/**/e/**/f
+#endif
+#endif /* SimpleConcat6 */
+
+/* The actual Concat6 rule which is robust to preprocessor semantics */
+#ifndef Concat6
+#define Concat6(a,b,c,d,e,f)SimpleConcat6(a,b,c,d,e,f)
+#endif /* Concat6 */
 
 
 /***************************************************************************
