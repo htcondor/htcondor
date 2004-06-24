531472ce2b0127435b80b136c32e20c342c385ea
wright <wright>
2004-06-24 20:57:09 +0000

    first major round of debugging/cleanup: - added lots of error checking and dying if there's trouble - renamed MakeDir to SrcDir for consistency - formatted/indented code to be more legible - we now exit w/ 2 if we failed to build a given test - cleaned up how we invoke batch_test.pl, and ensured that we create   the prerequisite perl modules - cleaned up how we copy results:   - make sure all the files are there or we die   - copy them to results/compiler so we don't clobber files - removed verbose_system() since we're not using it

--- nmi_glue/test/remote_task.pl
+++ nmi_glue/test/remote_task.pl
@@ -1,24 +1,27 @@
 #!/usr/bin/perl
 
 ######################################################################
-# $Id: remote_task.pl,v 1.1.2.3 2004-06-24 19:39:08 wright Exp $
+# $Id: remote_task.pl,v 1.1.2.4 2004-06-24 20:57:09 wright Exp $
 # run a test in the Condor testsuite
 # return val is the status of the test
 # 0 = good
 # 1 = build failed
 # 2 = run failed
+# 3 = internal fatal error (a.k.a. die)
 ######################################################################
 
-my $fulltestname = $ARGV[0];
-my $teststatus = 2; # set to failed by default
+my $fulltestname = $ARGV[0] || die "Task name not passed as argument!\n";
+
+###
+### We want to re-define die to exit with status 3 !!!!
+###
 
 ######################################################################
 # set up path 
 ######################################################################
 
-my $BaseDir = $ENV{BASE_DIR};
-my $MakeDir = $ENV{SRC_DIR};
-print "PATH is $ENV{PATH}\n";
+my $BaseDir = $ENV{BASE_DIR} || die "BASE_DIR is not in environment!\n";
+my $SrcDir = $ENV{SRC_DIR} || die "SRC_DIR is not in environment!\n";
 
 ######################################################################
 # get the testname and group
@@ -27,100 +30,116 @@ print "PATH is $ENV{PATH}\n";
 @testinfo = split(/\//, $fulltestname);
 my $compiler = @testinfo[0];
 my $testname = @testinfo[1];
+if( ! $compiler || ! $testname ) {
+    die "Invalid input for compiler/testname\n";
+}
 print "compiler is $compiler\n";
 print "testname is $testname\n";
 
-if ( ($compiler =~ m/gcc/) || ($compiler =~ m/g\+\+/) || ($compiler =~ /cc/) || ($compiler =~ /CC/) ){
-  $testdir = "condor_test_suite_C.V5";
-} elsif ( ($compiler =~ /g77/) || ($compiler =~ /f77/) || ($compiler =~ /f90/) ){
-  $testdir = "condor_test_suite_F.V5";
+if( ($compiler =~ m/gcc/) || ($compiler =~ m/g\+\+/) || 
+    ($compiler =~ /cc/) || ($compiler =~ /CC/) ) {
+    $testdir = "condor_test_suite_C.V5";
+} elsif ( ($compiler =~ /g77/) || ($compiler =~ /f77/) || 
+	  ($compiler =~ /f90/) ) {
+    $testdir = "condor_test_suite_F.V5";
 } else {
-  die "compiler $compiler is not valid\n";
+    die "compiler $compiler is not valid\n";
 }
 
 ######################################################################
 # build the test
 ######################################################################
 
-chdir("$MakeDir/$testdir/$compiler");
-open ( TESTBUILD, "make $testname 2>&1 |");
-while ( <TESTBUILD> ) {
-  print $_;
+chdir( "$SrcDir/$testdir/$compiler" ) || 
+    die "Can't chdir($SrcDir/$testdir/$compiler): $!\n";
+
+open( TESTBUILD, "make $testname 2>&1 |" ) || 
+    die "Can't run make $testname\n";
+while( <TESTBUILD> ) {
+    print $_;
 }
-close (TESTBUILD);
+close( TESTBUILD );
 $buildstatus = $?;
-print "BUILD TEST returned $buildstatus\n";
-if ($buildstatus != 0) {
-  $teststatus = 1;
-} else {
-  $teststatus = 0;
-} 
+print "BUILD TEST for $testname returned $buildstatus\n";
+if( $buildstatus != 0 ) {
+    print "Build failed for $testname\n";
+    exit 2;
+}
+
 
 ######################################################################
 # run the test using batch_test.pl
 ######################################################################
 
-if ($buildstatus == 0){
-  print "RUNNING $compiler/$testname\n";
-  chdir("$MakeDir/$testdir");
+print "RUNNING $compiler/$testname\n";
+chdir("$SrcDir/$testdir") || die "Can't chdir($SrcDir/$testdir): $!\n";
 
-  if ($testdir eq "condor_test_suite_C.V5") {
-      open(BATCHTEST, "perl ./batch_test.pl -d $compiler -t $testname 2>&1 |" );
-  } elsif ($testdir eq "condor_test_suite_F.V5") {
-      open( BATCHTEST, "perl ../condor_test_suite_C.V5/batch_test.pl -d $compiler -t $testname 2>&1 |" );
-  }
-
-  while ( <BATCHTEST> ) {
-    print $_;
-  }
-  close (BATCHTEST);
-  $batchteststatus = $?;
+system( "make batch_test.pl" );
+if( $? >> 8 ) {
+    die "Can't build batch_test.pl\n";
 }
+system( "make CondorTest.pm" );
+if( $? >> 8 ) {
+    die "Can't build CondorTest.pm\n";
+}
+system( "make Condor.pm" );
+if( $? >> 8 ) {
+    die "Can't build Condor.pm\n";
+}
+
+open(BATCHTEST, "perl ./batch_test.pl -d $compiler -t $testname 2>&1 |" ) || 
+    die "Can't open \"batch_test.pl -d $compiler -t $testname\": $!\n";
+while ( <BATCHTEST> ) {
+    print $_;
+}
+close (BATCHTEST);
+$batchteststatus = $?;
 
 # figure out here if the test passed or failed.  
-if ($batchteststatus == 0) {
-  $teststatus = 2;
+if( $batchteststatus != 0 ) {
+    $teststatus = 2;
+} else {
+    $teststatus = 0;
 }
+
 
 ######################################################################
 # copy test results to results dir
 ######################################################################
 
-system ("mkdir -p $BaseDir/results");
-chdir ("$MakeDir/$testdir");
-system ("cp $compiler/$testname.out $BaseDir/results");
-system ("cp $compiler/$testname.error $BaseDir/results");
-system ("cp $compiler/$testname.log $BaseDir/results");
+if( ! -d "$BaseDir/results" ) {
+    mkdir( "$BaseDir/results" ) || 
+	die "Can't mkdir($BaseDir/results): $!\n";
+}
+if( ! -d "$BaseDir/results/$compiler" ) {
+    mkdir( "$BaseDir/results/$compiler" ) || 
+	die "Can't mkdir($BaseDir/results/$compiler): $!\n";
+}
+chdir( "$SrcDir/$testdir/$compiler" ) || 
+    die "Can't chdir($SrcDir/$testdir/$compiler): $!\n";
 
+$copy_failure = 0;
 
-sub verbose_system {
+safe_copy( "$testname.out", "$BaseDir/results/$compiler" );
+safe_copy( "$testname.err", "$BaseDir/results/$compiler" );
+safe_copy( "$testname.log", "$BaseDir/results/$compiler" );
+safe_copy( "$testname.run.out", "$BaseDir/results/$compiler" );
+safe_copy( "$testname.cmd.out", "$BaseDir/results/$compiler" );
 
-  my @args = @_;
-  my $rc = 0xffff & system @args;
-
-  printf "system(%s) returned %#04x: ", @args, $rc;
-
-  if ($rc == 0) {
-   print "ran with normal exit\n";
-   return $rc;
-  }
-  elsif ($rc == 0xff00) {
-   print "command failed: $!\n";
-   return $rc;
-  }
-  elsif (($rc & 0xff) == 0) {
-   $rc >>= 8;
-   print "ran with non-zero exit status $rc\n";
-   return $rc;
-  }
-  else {
-   print "ran with ";
-   if ($rc &   0x80) {
-       $rc &= ~0x80;
-       print "coredump from ";
-   return $rc;
-   }
-   print "signal $rc\n"
-  }
+if( $copy_failure ) {
+    die "Failed to copy some output to results!\n";
 }
 
+exit $teststatus;
+
+
+sub safe_copy {
+    my( $src, $dest ) = @_;
+    system( "cp $src $dest" );
+    if( $? >> 8 ) {
+	print "Can't copy $src to $dest: $!\n";
+	$copy_failure = 1;
+    } else {
+	print "Copied $src to $dest\n";
+    }
+}
