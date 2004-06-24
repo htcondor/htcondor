5aacae3105368bd8db8fd61c11ef4a858feab05c
wright <wright>
2004-06-24 19:44:44 +0000

    removed implementation of verbose_system() since we don't use it.

--- nmi_glue/test/remote_post_always.pl
+++ nmi_glue/test/remote_post_always.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 ######################################################################
-# $Id: remote_post_always.pl,v 1.1.2.4 2004-06-24 19:39:08 wright Exp $
+# $Id: remote_post_always.pl,v 1.1.2.5 2004-06-24 19:44:44 wright Exp $
 # post script for Condor testsuite that is run regardless of the
 # testsuite end status  
 ######################################################################
@@ -26,38 +26,7 @@ while (<PIDFILE>){
 }
 close PIDFILE;
 
-# probably try to stop more gracefully, then wait 30 seconds and kill if necessary
+# probably try to stop more gracefully, then wait 30 seconds and kill
+# if necessary
 print "KILLING Personal Condor master daemon (pid: $master_pid)\n";
 kill 15, $master_pid;
-
-sub verbose_system {
-
-  my @args = @_;
-  my $rc = 0xffff & system @args;
-
-  printf "system(%s) returned %#04x: ", @args, $rc;
-
-  if ($rc == 0) {
-   print "ran with normal exit\n";
-   return $rc;
-  }
-  elsif ($rc == 0xff00) {
-   print "command failed: $!\n";
-   return $rc;
-  }
-  elsif (($rc & 0xff) == 0) {
-   $rc >>= 8;
-   print "ran with non-zero exit status $rc\n";
-   return $rc;
-  }
-  else {
-   print "ran with ";
-   if ($rc &   0x80) {
-       $rc &= ~0x80;
-       print "coredump from ";
-   return $rc;
-   }
-   print "signal $rc\n"
-  }
-}
-
