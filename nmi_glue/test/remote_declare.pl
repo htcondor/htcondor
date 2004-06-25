c3718c12b2fd3d03da3c633c7302810c1500eea0
wright <wright>
2004-06-25 00:03:49 +0000

    added brains to understand a command-line argument which decides what tests to declare.  for now, there's only 1 real option: "quick".  if that's passed on the command-line, we only declare a small subset of tests which should run quickly, yet still give a good cross-section.

--- nmi_glue/test/remote_declare.pl
+++ nmi_glue/test/remote_declare.pl
@@ -1,14 +1,31 @@
 #!/usr/bin/perl
 
 ######################################################################
-# $Id: remote_declare.pl,v 1.1.2.6 2004-06-24 23:54:41 wright Exp $
+# $Id: remote_declare.pl,v 1.1.2.7 2004-06-25 00:03:49 wright Exp $
 # generate list of all tests to run
 ######################################################################
 
-use Cwd;
-
 my $BaseDir = $ENV{BASE_DIR};
 my $SrcDir = $ENV{SRC_DIR};
+
+my $decl_type;
+my $args = $ARGV[0];
+if( $args ) {
+    # passed special black-box args, do something smart with them.
+    $decl_type = $args;
+} else {
+    $decl_type = "full";
+}
+
+# make sure that what we were given is valid and we recognize it
+if( $decl_type =~ /^quick$/ ||
+    $decl_type =~ /^full$/ )
+{
+    print "Declaring tests for type '$decl_type'\n";
+} else {
+    die "Unknown declare-type: '$decl_type'!\n";
+}
+
 
 open( TASKLIST, ">$BaseDir/tasklist.nmi" ) || 
     die "Can't open $BaseDir/tasklist.nmi: $!\n"; 
@@ -35,7 +52,6 @@ foreach $testdir (@testdirs) {
 	chdir( "$SrcDir/$testdir" ) || 
 	    die "Can't chdir($SrcDir/$testdir): $!\n";
 	print "Working on $compiler\n";
-	print "cwd is: " . getcwd() . "\n";
 	system( "make $compiler/Makefile" );
 	chdir( $compiler ) || die "Can't chdir($compiler): $!\n";
 	system( "make run_list" );
@@ -43,7 +59,25 @@ foreach $testdir (@testdirs) {
 	while( <RUNLIST> ) {
 	    chomp;
 	    $testname = $_;
-	    print TASKLIST "$compiler.$testname\n";
+	    if( $decl_type =~ /^full$/ ) {
+		# In 'full' mode, print all tests we fine
+		print TASKLIST "$compiler.$testname\n";
+	    } elsif( $decl_type =~ /^quick$/ ) {
+		# In 'quick' mode, only use a subset for quick testing
+		if( $compiler =~ /g77/ || 
+		    $testname =~ /^env/ || 
+		    $testname =~ /floats/ || 
+		    $testname =~ /fgets/ || 
+		    $testname =~ /fcntl/ )
+		{
+		    print TASKLIST "$compiler.$testname\n";
+		} else {
+		    print "using quick tests, ignoring $compiler.$testname\n";
+		}
+	    } else {
+		die "Impossible: unknown declare type: $decl_type after " .
+		    "we already recognized it!\n";
+	    }
 	}
 	close( RUNLIST );
     } 
