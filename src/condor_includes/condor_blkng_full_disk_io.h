ac6e7ee1fbc6d69e80d1052d4c62f045ee192c08
psilord <psilord>
2004-10-07 21:51:35 +0000

    + Fixed the signature so it is POSIX. + Added two more functions. the full_write/full_read functions are to be used 	in normal condor_code. The _condor_* versions are to be used in the 	remote i/o and checkpointing libraries.

--- src/condor_includes/condor_blkng_full_disk_io.h
+++ src/condor_includes/condor_blkng_full_disk_io.h
@@ -1,16 +1,23 @@
 #ifndef _CONDOR_BLKNG_FULL_DISK_IO_H
 #define _CONDOR_BLKNG_FULL_DISK_IO_H
 
-/* This file implementes blocking read/write operations on regular files.
-	The main purpose is to read/write however many bytes you actually
-	specified. These functions are NOT drop in replacements for 
-	read() and write(), please read the comments in the implementation
-	file as to their usage. */
+/* This file implements blocking read/write operations on a regular
+	files only.  The main purpose is to read/write however many
+	bytes you actually specified ans these functions will try very
+	hard to do just that. */
 
 BEGIN_C_DECLS
 
-ssize_t condor_blkng_full_disk_read(int fd, char *ptr, int nbytes);
-ssize_t condor_blkng_full_disk_write(int fd, void *ptr, int nbytes);
+/* These functions are the actual implementation, and must be used if
+	you want this functionality in the remote i/o or condor_checkpointing
+	libraries. Be warned, however, that they do require libc to be mapped
+	correctly. */
+ssize_t _condor_full_read(int fd, void *ptr, size_t nbytes);
+ssize_t _condor_full_write(int fd, void *ptr, size_t nbytes);
+
+/* For generic Condor code not in the above libraries, use these functions */
+ssize_t full_read(int filedes, void *ptr, size_t nbyte);
+ssize_t full_write(int filedes, void *ptr, size_t nbyte);
 
 END_C_DECLS
 
