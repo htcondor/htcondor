b211bd6b7ab729528b063ae45616038d4147e94d
wright <wright>
2004-10-08 23:54:42 +0000

    Initial commit to the new test suite directory: this used to be called: src/condor_test_suite_C.V5/hello.*

---
+++ src/condor_tests/job_rsc_hello_std.cmd
@@ -0,0 +1,10 @@
+universe     = standard
+executable   = job_rsc_hello_std.cndr.exe.$$(OPSYS).$$(ARCH)
+input        = x_small.in
+output       = job_rsc_hello_std.out
+error        = job_rsc_hello_std.err
+log          = job_rsc_hello_std.log
+arguments    = -_condor_aggravate_bugs
+priority     = 0
+notification = Never
+queue
