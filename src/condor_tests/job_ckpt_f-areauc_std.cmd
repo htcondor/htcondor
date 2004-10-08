aefdf2e2a30978e98969e7a48a471521e3861d57
wright <wright>
2004-10-08 23:55:52 +0000

    Initial commit to the new test suite directory: this used to be called: src/condor_test_suite_F.V5/areauc.*

---
+++ src/condor_tests/job_ckpt_f-areauc_std.cmd
@@ -0,0 +1,7 @@
+executable      = job_ckpt_f-areauc_std.cndr.exe.$$(OPSYS).$$(ARCH)
+error           = job_ckpt_f-areauc_std.err
+output          = job_ckpt_f-areauc_std.out
+log             = job_ckpt_f-areauc_std.log
+arguments       = -_condor_aggravate_bugs
+Notification    = Never
+queue
