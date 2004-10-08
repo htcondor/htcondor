8a5048d550c8e630ee41cd5fe5f1945b0f1e1275
wright <wright>
2004-10-08 23:55:27 +0000

    Initial commit to the new test suite directory: this used to be called: src/condor_test_suite_F.V5/direct_write.*

---
+++ src/condor_tests/job_rsc_f-direct-write_std.cmd
@@ -0,0 +1,16 @@
+####################
+##
+## Test Condor command file
+##
+####################
+
+executable      = job_rsc_f-direct-write_std.cndr.exe.$$(OPSYS).$$(ARCH)
+error           = job_rsc_f-direct-write_std.err
+output          = job_rsc_f-direct-write_std.out
+log		= job_rsc_f-direct-write_std.log
+arguments = -_condor_aggravate_bugs
+
+
+Notification = Never
+
+queue
