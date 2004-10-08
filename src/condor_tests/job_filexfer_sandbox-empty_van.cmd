114ed1645f98a7ecf2269178b245258ee48d9a6c
wright <wright>
2004-10-08 23:57:59 +0000

    Initial commit to the new test suite directory: this used to be called: src/condor_test_suite_C.V5/sandbox_empty.*

---
+++ src/condor_tests/job_filexfer_sandbox-empty_van.cmd
@@ -0,0 +1,10 @@
+Universe   = vanilla
+Executable = job_filexfer_sandbox-empty_van.pl
+Log        = job_filexfer_sandbox-empty_van.log
+Output     = job_filexfer_sandbox-empty_van.out
+Error      = job_filexfer_sandbox-empty_van.err
+Notification = Never
+Priority   = 0
+should_transfer_files = YES
+when_to_transfer_output = ON_EXIT
+Queue
