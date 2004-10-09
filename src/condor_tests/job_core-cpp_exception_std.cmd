7634dbae6e9740b5408ef210d85112b1b15ffa37
wright <wright>
2004-10-09 12:00:37 +0000

    renamed "job_rsc_exception_std" to "job_core-cpp_exception_std"

--- src/condor_tests/job_core-cpp_exception_std.cmd
+++ src/condor_tests/job_core-cpp_exception_std.cmd
@@ -1,8 +1,8 @@
 universe = standard
-executable = job_rsc_exception_std.cndr.exe.$$(OPSYS).$$(ARCH)
-output = job_rsc_exception_std.out
-error = job_rsc_exception_std.err
-log = job_rsc_exception_std.log
+executable = job_core-cpp_exception_std.cndr.exe.$$(OPSYS).$$(ARCH)
+output = job_core-cpp_exception_std.out
+error = job_core-cpp_exception_std.err
+log = job_core-cpp_exception_std.log
 arguments = -_condor_aggravate_bugs
 priority = 0
 
