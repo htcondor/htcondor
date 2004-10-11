c9d62eaf4fb8ecb4677b70a8eaf5eec89f78ffb8
stolley <stolley>
2004-10-11 20:05:15 +0000

    V6_6_7 has been tagged; Moved version string up to 6.6.8 pre-release.

--- src/condor_c++_util/condor_version.C
+++ src/condor_c++_util/condor_version.C
@@ -33,7 +33,7 @@
    will EXCEPT at startup time.  
 */
 
-static char* CondorVersionString = "$CondorVersion: 6.6.7 " __DATE__ " $";
+static char* CondorVersionString = "$CondorVersion: 6.6.8 " __DATE__ " PRE-RELEASE-UWCS $";
 
 /* 
    This is some wisdom from Cygnus's web page.  If you just try to use
