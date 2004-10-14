15696091a7f2693722508f2f42c29a89815ae563
wright <wright>
2004-10-14 23:38:24 +0000

    Initial commit to the new test suite directory: this used to be called: src/condor_test_suite_C.V5/lseek.*

---
+++ src/condor_tests/job_ckpt_memory-file_std.cmd
@@ -0,0 +1,9 @@
+universe        = standard
+executable      = job_ckpt_memory-file_std.remote.$$(OPSYS).$$(ARCH)
+output          = job_ckpt_memory-file_std.out
+error           = job_ckpt_memory-file_std.error
+log             = job_ckpt_memory-file_std.log
+priority        = 6
+arguments       = -f x_job_ckpt_memory-file_std.t1 -s 10000 -o 25000 -_condor_aggravate_bugs
+notification   = never
+queue
