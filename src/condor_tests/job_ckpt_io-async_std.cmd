c6898e09d5a2dd5621bd099d9740ee5bf075f817
wright <wright>
2004-10-14 23:33:56 +0000

    Initial commit to the new test suite directory: this used to be called: src/condor_test_suite_C.V5/io.*

---
+++ src/condor_tests/job_ckpt_io-async_std.cmd
@@ -0,0 +1,9 @@
+universe	= standard
+executable      = job_ckpt_io-async_std.cndr.exe.$$(OPSYS).$$(ARCH)
+output          = job_ckpt_io-async_std.out
+error           = job_ckpt_io-async_std.err
+log             = job_ckpt_io-async_std.log
+arguments       = 2048 -_condor_aggravate_bugs
+priority        = 8
+notification    = never
+queue
