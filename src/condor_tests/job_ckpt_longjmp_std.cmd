b98b6faf8567b6cfa577a8bf5391f9cdf70b1a06
wright <wright>
2004-10-15 06:44:19 +0000

    Initial commit to the new test suite directory: this used to be called: src/condor_test_suite_C.V5/threads.*  NOTE: this test was previously a C++ only test, but it was only 1 line that was C++-specific code, so i converted that back to valid C so we can run this test with more compilers.

---
+++ src/condor_tests/job_ckpt_longjmp_std.cmd
@@ -0,0 +1,9 @@
+universe        = standard
+executable      = job_ckpt_longjmp_std.cndr.exe.$$(OPSYS).$$(ARCH)
+error           = job_ckpt_longjmp_std.error
+output          = job_ckpt_longjmp_std.out
+log             = job_ckpt_longjmp_std.log
+arguments       = -_condor_D_CKPT -_condor_aggravate_bugs
+priority        = 0
+notification    = never
+queue
