deed030f6827de8146e2de0e7b4dd5f6318452a3
wright <wright>
2004-10-20 06:30:38 +0000

    fixed the error file to end in '.err' to match the other tests.

--- src/condor_tests/job_ckpt_longjmp_std.cmd
+++ src/condor_tests/job_ckpt_longjmp_std.cmd
@@ -1,6 +1,6 @@
 universe        = standard
 executable      = job_ckpt_longjmp_std.cndr.exe.$$(OPSYS).$$(ARCH)
-error           = job_ckpt_longjmp_std.error
+error           = job_ckpt_longjmp_std.err
 output          = job_ckpt_longjmp_std.out
 log             = job_ckpt_longjmp_std.log
 arguments       = -_condor_D_CKPT -_condor_aggravate_bugs
