45e5ab82fdcdcef0cb53dbfa37484bd827cbb276
stolley <stolley>
2004-10-28 18:54:03 +0000

    This is a fix that allows dagman to be submitted as a non-administrator user. The theory behind this patch is that if we can't get a process handle to our parent because of an ACCESS_DENIED error, we shouldn't blow up, but instead just operate without the benefit of being able to detect if our parent goes away (like all user jobs).
    Opening a process handle with lower access rights won't work either--we
    need SYNCHRONIZE in order to pass the handle to WaitForMultipleObjects(),
    and normal users can't open process handles to processes they don't own
    with this level of priviledge.

--- src/condor_daemon_core.V6/daemon_core.C
+++ src/condor_daemon_core.V6/daemon_core.C
@@ -5658,24 +5658,41 @@ DaemonCore::Inherit( void )
 #ifdef WIN32
 		pidtmp->deallocate = 0L;
 
-		// I don't see why we need STANDARD_RIGHTS_REQUIRED, so I'm dropping it
-		// for now since its screwing up DAGman. Ask Colin if you care.
+		pidtmp->hProcess = ::OpenProcess( SYNCHRONIZE |
+				PROCESS_QUERY_INFORMATION, FALSE, ppid );
 
-		pidtmp->hProcess = ::OpenProcess( SYNCHRONIZE | 
-//						PROCESS_QUERY_INFORMATION | STANDARD_RIGHTS_REQUIRED , 
-						PROCESS_QUERY_INFORMATION, 
-						FALSE, ppid );
+		
+		// We want to avoid trying to watch the ppid if it turns out
+		// that we can't open a handle to it because we have insufficient
+		// permissions. In the case of dagman, it runs as a user, which
+		// doesn't necessarily have the perms to open a handle to the
+		// schedd process. If we fail to get the handle for some reason
+		// other than ACCESS_DENIED however, we want to try to watch the
+		// pid, and consequently cause the assert() to blow.
+
+		bool watch_ppid = true;
+
 		if ( pidtmp->hProcess == NULL ) {
-			dprintf(D_ALWAYS, "OpenProcess() failed - Error %d\n", GetLastError());
+			if ( GetLastError() == ERROR_ACCESS_DENIED ) {
+				dprintf(D_FULLDEBUG, "OpenProcess() failed - "
+						"ACCESS DENIED. We can't watch parent process.\n");
+				watch_ppid = false;
+			} else { 
+				dprintf(D_ALWAYS, "OpenProcess() failed - Error %d\n",
+					   	GetLastError());
+			}
 		}
-		assert(pidtmp->hProcess);
+		
 		pidtmp->hThread = NULL;		// do not allow child to suspend parent
 		pidtmp->deallocate = 0L;
 #endif
 		int insert_result = pidTable->insert(ppid,pidtmp);
 		assert( insert_result == 0 );
 #ifdef WIN32
-		WatchPid(pidtmp);
+		if ( watch_ppid ) {
+			assert(pidtmp->hProcess);
+			WatchPid(pidtmp);
+		}
 #endif
 
 		// inherit cedar socks
