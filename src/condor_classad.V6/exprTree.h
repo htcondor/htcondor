8aabfea061b088d3b7dca89761ac9256598d0877
roy <roy>
2004-11-22 21:11:48 +0000

    We select ostream.h if we are using old versions of gcc, otherwise ostream

--- src/condor_classad.V6/exprTree.h
+++ src/condor_classad.V6/exprTree.h
@@ -27,7 +27,12 @@
 #include "classad_stl.h"
 #include "common.h"
 #include "value.h"
+
+#if defined (__GNUC__) && (__GNUC__<3)
+#include <ostream.h>
+#else
 #include <ostream>
+#endif
 
 BEGIN_NAMESPACE( classad )
 
