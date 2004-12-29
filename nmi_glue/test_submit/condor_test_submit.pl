d536fda5f0a37503172739c49c28449e92ad1dcc
wright <wright>
2004-12-29 21:44:32 +0000

    instead of just using split() to grab the version numbers out of the tag, we now use a regexp to be safer about only grabbing what we want/expect (namely digits, not anything that happens to be included in the branch name that's delimited with '_' chars).  This now works properly for tags like "BUILD-V6_7-negotiator_ad-branch-2004-12-29".

--- nmi_glue/test_submit/condor_test_submit.pl
+++ nmi_glue/test_submit/condor_test_submit.pl
@@ -114,9 +114,14 @@ sub generate_cmdfile() {
     my $desc = $release;
     $release =~ s/-branch-.*//;
     $release =~ s/V//;
-    my @versions = split("_", $release);
-    if( ! $versions[2] ) {
-        $versions[2] = "x";
+    if( $release =~ /(\d+)(\D*)_(\d+)(\D*)_?(\d+)?(\D*)/ ) {
+        $versions[0] = $1;
+        $versions[1] = $3;
+	if( $5 ) {
+            $versions[2] = $5;
+        } else {
+            $versions[2] = "x";
+	}
     }
     my $vers_string = "$versions[0], $versions[1], $versions[2]";
 
