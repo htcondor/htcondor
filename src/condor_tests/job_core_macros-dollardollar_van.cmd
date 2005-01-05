1de0d13217d7237fcedd1f0f09aaa82b55e9e89f
wright <wright>
2005-01-05 01:25:52 +0000

    removed misspelled, confusing, and misleading comment.

--- src/condor_tests/job_core_macros-dollardollar_van.cmd
+++ src/condor_tests/job_core_macros-dollardollar_van.cmd
@@ -4,7 +4,6 @@ output = job_core_macros-dollardollar_van.out
 error = job_core_macros-dollardollar_van.err
 log = job_core_macros-dollardollar_van.log
 env = MEMORY=$$(memory);MACHINE=$$(machine)
-# out put file has herader line making these $lines[1], $lines[2] ...
 arguments = $$(memory) $$(machine)
 
 Notification = NEVER
