4ad5795d371cb3845936a180de2cee84af9f52ef
bt <bt>
2005-02-01 23:11:22 +0000

    Add files for compressfiles, copytospool, and corefile tests to the test suite initially in v6_6-branch.

---
+++ src/condor_tests/job_core_copytospool_van.cmd
@@ -0,0 +1,11 @@
+
+Executable           = /bin/sleep
+Universe             = vanilla
+log = job_core_copytospool_van.log
+output = job_core_copytospool_van.out
+error = job_core_copytospool_van.err
+hold = true
+copy_to_spool = true
+Notification         = NEVER
+getenv               = false
+Queue
