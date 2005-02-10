fcb6388c11fcff30d66ace2ac0ecf482e7ad0e03
parag <parag>
2005-02-10 21:29:32 +0000

    Initialise creates temporary workspace based on the arg supplied

--- nmi_glue/lib/CondorGlue.pm
+++ nmi_glue/lib/CondorGlue.pm
@@ -3,7 +3,7 @@
 # build and test "glue" scripts for use with the NMI-NWO framework.
 #
 # Originally written by Derek Wright <wright@cs.wisc.edu> 2004-12-30
-# $Id: CondorGlue.pm,v 1.1.2.14 2005-01-10 21:52:38 parag Exp $
+# $Id: CondorGlue.pm,v 1.1.2.15 2005-02-10 21:29:32 parag Exp $
 #
 ######################################################################
 
@@ -37,10 +37,12 @@ my %tags;
 
 sub Initialize
 {
+    my ($type) = @_;
+    
     $ENV{PATH} = "/nmi/bin:/usr/local/condor/bin:/usr/local/condor/sbin:"
         . $ENV{PATH};
 
-    $workspace = "/tmp/condor_build." . "$$";
+    $workspace = "/tmp/condor_$type." . "$$";
 
     $init_cwd = &getcwd();
     mkdir($workspace) || die "Can't create workspace $workspace: $!\n";
