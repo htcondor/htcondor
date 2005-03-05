7364fccb62add0e7eeaa14f93eed336c79873003
psilord <psilord>
2005-03-05 07:33:09 +0000

    + After a closer inspection, I realized that this program was setting up the struct sigaction very incorrectly. I fixed it to be what was intended (as far as I could figure from the code base).

--- src/condor_tests/x_trapsig.c
+++ src/condor_tests/x_trapsig.c
@@ -1,8 +1,14 @@
-#include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 
-int signalcaught = 0;
+volatile int signalcaught = 0;
+
+void catchsig(int sig);
+
+struct sigaction sa;
+struct sigaction old;
 
 void catchsig( int sig )
 {
@@ -18,91 +24,90 @@ void catchsig( int sig )
 		exit(sig);
 }
 
-struct sigaction new = 
-{
-	&catchsig,
-	0,
-	SA_NODEFER,
-	SA_NODEFER,
-};
-
-struct sigaction old;
-
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	int res = 0;
+
+	/* do not assign sa_handler and sa_sigaction at the same time since on some
+		architectures those two are a union */
+	sa.sa_handler = catchsig;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_NODEFER;
+
 	/*signal( 1, (sighandler_t)&catchsig);*/
-	res = sigaction( 1, &new, &old);
+	res = sigaction( 1, &sa, &old);
 	if(res != 0 )
 	{
 		printf("failed to replace handler intr 1\n");
 	}
-	res = sigaction( 2, &new, &old);
+	res = sigaction( 2, &sa, &old);
 	if(res != 0 )
 	{
 		printf("failed to replace handler intr 2\n");
 	}
-	res = sigaction( 3, &new, &old);
+	res = sigaction( 3, &sa, &old);
 	if(res != 0 )
 	{
 		printf("failed to replace handler intr 3\n");
 	}
-	res = sigaction( 4, &new, &old);
+	res = sigaction( 4, &sa, &old);
 	if(res != 0 )
 	{
 		printf("failed to replace handler intr 4\n");
 	}
-	res = sigaction( 5, &new, &old);
+	res = sigaction( 5, &sa, &old);
 	if(res != 0 )
 	{
 		printf("failed to replace handler intr 5\n");
 	}
-	res = sigaction( 6, &new, &old);
+	res = sigaction( 6, &sa, &old);
 	if(res != 0 )
 	{
 		printf("failed to replace handler intr 6\n");
 	}
-	res = sigaction( 7, &new, &old);
+	res = sigaction( 7, &sa, &old);
 	if(res != 0 )
 	{
 		printf("failed to replace handler intr 7\n");
 	}
-	res = sigaction( 8, &new, &old);
+	res = sigaction( 8, &sa, &old);
 	if(res != 0 )
 	{
 		printf("failed to replace handler intr 8\n");
 	}
 	/*
-	**res = sigaction( 9, &new, &old);
+	**res = sigaction( 9, &sa, &old);
 	**if(res != 0 )
 	**
 		**printf("failed to replace handler intr 9\n");
 	**
 	*/
-	res = sigaction( 10, &new, &old);
+	res = sigaction( 10, &sa, &old);
 	if(res != 0 )
 	{
 		printf("failed to replace handler intr 10\n");
 	}
-	res = sigaction( 11, &new, &old);
+	res = sigaction( 11, &sa, &old);
 	if(res != 0 )
 	{
 		printf("failed to replace handler intr 11\n");
 	}
-	res = sigaction( 12, &new, &old);
+	res = sigaction( 12, &sa, &old);
 	if(res != 0 )
 	{
 		printf("failed to replace handler intr 12\n");
 	}
-	res = sigaction( 13, &new, &old);
+	res = sigaction( 13, &sa, &old);
 	if(res != 0 )
 	{
 		printf("failed to replace handler intr 13\n");
 	}
-	res = sigaction( 14, &new, &old);
+	res = sigaction( 14, &sa, &old);
 	if(res != 0 )
 	{
 		printf("failed to replace handler intr 14\n");
 	}
 	while(1);
+
+	return 0;
 }
