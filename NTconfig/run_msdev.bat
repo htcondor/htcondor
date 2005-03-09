d5503c72ff27334a97bfbbf3ee49e65e7634a8a0
stolley <stolley>
2005-03-09 21:39:10 +0000

    Added a few extra awk commands to clean up the project files in the same manner that we clean up the .mak files. VC++ can be stupid sometimes.

--- NTconfig/run_msdev.bat
+++ NTconfig/run_msdev.bat
@@ -3,6 +3,23 @@ REM Prepare to build Condor.
 call dorenames.bat > NUL
 if not errorlevel 2 call dorenames.bat > NUL
 
+REM This fixes the wierdness in condor_cpp_util.dsp
+awk "{ gsub(/\.\\\.\.\\Debug/, \"..\\Debug\") } { gsub(/\.\\\.\.\\Release/, \"..\\Release\") } { print }" condor_cpp_util.dsp > ~temp.dsp
+del condor_cpp_util.dsp
+ren ~temp.dsp condor_cpp_util.dsp
+
+REM InputDir is stupidly defined by an absolute path by MSVS. So we
+REM have to have awk clean up by making everything relative.
+for %%f in ( *.dsp ) do awk "{ b = gensub(/InputDir=(.*)\\src\\(.+)/, \"InputDir=..\\\\src\\\\\\2\", $0); print b }" %%f > %%f.tmp
+
+if exist condor_util_lib.dsp.tmp (
+	del *.dsp
+	ren *.tmp *.
+		) ELSE (
+			echo awk makefile cleanup failed!
+		   	exit /b 1
+		)
+
 REM Build the externals and bail if it fails.
 call make_win32_externals.bat
 
