b1e46d8ada5563c278da09161b0f7c25d9ab09a9
stolley <stolley>
2005-03-09 20:39:08 +0000

    Beefed up dorelease.bat to create a more unix-style releasedir. It also grabs the wsdl files , .jar files and config files and places them in the appropriate directories.

--- NTconfig/dorelease.bat
+++ NTconfig/dorelease.bat
@@ -1,20 +1,33 @@
 @echo off
 if exist %1 goto process1
 echo Error - directory %1 does not exist
-echo Usage: release.bat path_to_release_subdirectory
+echo Usage: dorelease.bat path_to_release_subdirectory
 goto end
 :process1
 if exist ..\Release goto process2
 echo Compile a Release Build of Condor first.
 goto end
 :process2
+REM Set up release-dir directories
+echo Creating Release Directory...
+if not exist %1\bin\NUL mkdir %1\bin
+if not exist %1\lib\NUL mkdir %1\lib
+if not exist %1\lib\webservice\NUL mkdir %1\lib\webservice
+if not exist %1\etc\NUL mkdir %1\etc
 echo Copying files...
-copy ..\Release\*.exe %1
-copy ..\Release\*.dll %1
-copy ..\src\condor_starter.V6.1\*.class %1
-copy msvcrt.dll %1
-copy msvcirt.dll %1
-copy pdh.dll %1
+copy ..\Release\*.exe %1\bin
+copy ..\Release\*.dll %1\bin
+copy ..\src\condor_starter.V6.1\*.class %1\lib
+copy ..\src\condor_starter.V6.1\*.jar %1\lib
+copy ..\src\condor_chirp\Chirp.jar %1\lib
+copy ..\src\condor_examples\condor_config.* %1\etc
+copy msvcrt.dll %1\bin
+copy msvcirt.dll %1\bin
+copy pdh.dll %1\bin
+pushd .
+cd ..\src
+for /R %%f in (*.wsdl) do copy %%f %1\lib\webservice
+popd
 echo Stripping out debug symbols from files...
 pushd %1
 for %%f in (*.exe) do rebase -b 0x400000 -x . -a %%f
