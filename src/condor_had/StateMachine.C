e202dd1137cb71abdf1a037666472ad543e1da93
nleroy <nleroy>
2005-03-09 17:21:13 +0000

    Added the Condor Copyright notice and a "developed by Technion" notice to the head of the source files

--- src/condor_had/StateMachine.C
+++ src/condor_had/StateMachine.C
@@ -1,3 +1,34 @@
+/***************************Copyright-DO-NOT-REMOVE-THIS-LINE**
+  *
+  * Condor Software Copyright Notice
+  * Copyright (C) 1990-2005, Condor Team, Computer Sciences Department,
+  * University of Wisconsin-Madison, WI.
+  *
+  * This source code is covered by the Condor Public License, which can
+  * be found in the accompanying LICENSE.TXT file, or online at
+  * www.condorproject.org.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  * AND THE UNIVERSITY OF WISCONSIN-MADISON "AS IS" AND ANY EXPRESS OR
+  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  * WARRANTIES OF MERCHANTABILITY, OF SATISFACTORY QUALITY, AND FITNESS
+  * FOR A PARTICULAR PURPOSE OR USE ARE DISCLAIMED. THE COPYRIGHT
+  * HOLDERS AND CONTRIBUTORS AND THE UNIVERSITY OF WISCONSIN-MADISON
+  * MAKE NO MAKE NO REPRESENTATION THAT THE SOFTWARE, MODIFICATIONS,
+  * ENHANCEMENTS OR DERIVATIVE WORKS THEREOF, WILL NOT INFRINGE ANY
+  * PATENT, COPYRIGHT, TRADEMARK, TRADE SECRET OR OTHER PROPRIETARY
+  * RIGHT.
+  *
+  ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
+
+/***************************Copyright-DO-NOT-REMOVE-THIS-LINE**
+ *
+ * The Condor High Availablility Daemon (HAD) softare was developed by
+ * by the Distributed Systems Laboratory at the Technion Israel
+ * Institute of Technology, and contributed to to the Condor Project.
+ *
+  ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
+
 /*
   StateMachine.cpp: implementation of the HADStateMachine class.
 */
