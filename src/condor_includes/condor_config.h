8e5dfa3be6df12e9cb8bb0254dbca4ca0356f131
epaulson <epaulson>
2005-04-14 16:50:20 +0000

    Added support for STARTD_EXPRS to be on a per-VM basis.
    fill_config_ad takes a new paramater, that defalts to NULL

--- src/condor_includes/condor_config.h
+++ src/condor_includes/condor_config.h
@@ -59,7 +59,7 @@ typedef struct bucket {
 extern "C" {
 	void config( int wantsQuiet=0 );
 	void config_host( char* host=NULL );
-	void config_fill_ad( ClassAd* );
+	void config_fill_ad( ClassAd*, const char* prefix=NULL );
         int  param_boolean_int( const char *name, int default_value );  
 	int  set_persistent_config(char *admin, char *config);
 	int  set_runtime_config(char *admin, char *config);
