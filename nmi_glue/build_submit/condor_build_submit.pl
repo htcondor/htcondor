46c1ee977ea67d4ab8a252c9aac01c71391dcd3a
wright <wright>
2005-05-11 23:33:21 +0000

    irix 6.5 and dux 5.1 are now in the NWO pool, so try building on them.

--- nmi_glue/build_submit/condor_build_submit.pl
+++ nmi_glue/build_submit/condor_build_submit.pl
@@ -37,6 +37,8 @@ my $default_platforms =
     "ppc_aix_5.2, " .
     "x86_winnt_5.1, " .
     "hppa_hpux_B.10.20, " .
+    "alpha_osf_V5.1, " .
+    "irix_6.5, " .
     "ppc_macosx";
 CondorGlue::ProcessOptions( $default_platforms );
 
