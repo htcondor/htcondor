543e94d5167ef50e0dea2a918485920b9f502ebe
adesmet <adesmet>
2005-06-08 20:43:16 +0000

    build_job_env will consolidate figuring out what environment variables we need to set for a job.  At the moment it frets about X509_USER_PROXY, though it's planned to handle a few more things "soonish".

---
+++ src/condor_c++_util/build_job_env.C
@@ -0,0 +1,80 @@
+/***************************Copyright-DO-NOT-REMOVE-THIS-LINE**
+  *
+  * Condor Software Copyright Notice
+  * Copyright (C) 1990-2004, Condor Team, Computer Sciences Department,
+  * University of Wisconsin-Madison, WI.
+  *
+  * This source code is covered by the Condor Public License, which can
+  * be found in the accompanying LICENSE.TXT file, or online at
+  * www.condorproject.org.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  * AND THE UNIVERSITY OF WISCONSIN-MADISON "AS IS" AND ANY EXPRESS OR
+  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  * WARRANTIES OF MERCHANTABILITY, OF SATISFACTORY QUALITY, AND FITNESS
+  * FOR A PARTICULAR PURPOSE OR USE ARE DISCLAIMED. THE COPYRIGHT
+  * HOLDERS AND CONTRIBUTORS AND THE UNIVERSITY OF WISCONSIN-MADISON
+  * MAKE NO MAKE NO REPRESENTATION THAT THE SOFTWARE, MODIFICATIONS,
+  * ENHANCEMENTS OR DERIVATIVE WORKS THEREOF, WILL NOT INFRINGE ANY
+  * PATENT, COPYRIGHT, TRADEMARK, TRADE SECRET OR OTHER PROPRIETARY
+  * RIGHT.
+  *
+  ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
+
+#include "condor_common.h"
+#include "condor_classad.h"
+#include "condor_string.h"
+#include "condor_attributes.h"
+#include "condor_auth_x509.h"
+#include "basename.h"
+#include "directory.h"
+#include "build_job_env.h"
+
+StringMap build_job_env(const ClassAd & ad) {
+	StringMap results(1,MyStringHash,updateDuplicateKeys);// rejectDuplicateKeys?
+
+	MyString Iwd;
+	if( ! ad.LookupString(ATTR_JOB_IWD, Iwd) ) {
+		ASSERT(0);
+		dprintf(D_ALWAYS, "Job ClassAd lacks required attribute %s.  Job's environment may be incorrect.\n", ATTR_JOB_IWD);
+		return results;
+	}
+
+	MyString X509Path;
+	if(ad.LookupString(ATTR_X509_USER_PROXY, X509Path)) {
+		if( ! fullpath(X509Path.Value()) ) {
+			// It's not a full path, so glob on the IWD onto the front
+			char * newpath = dircat(Iwd.Value(), X509Path.Value());
+			X509Path = newpath;
+			delete [] newpath; // dircat returnned newed memory.
+
+		}
+		results.insert(STR_GSI_USER_PROXY, X509Path.Value());
+	}
+
+	return results;
+}
+
+int Insert(ClassAd & ad, const char * lhs, const char * rhs) {
+	MyString s;
+	s.sprintf("%s=\"%s\"",lhs,rhs);
+	return ad.Insert(s.Value());
+}
+
+#if 0
+int main() {
+	ClassAd ad;
+	Insert(ad,ATTR_JOB_IWD,"/path/to/iwd");
+	Insert(ad,ATTR_X509_USER_PROXY,"/path/bob");
+	StringMap sm = build_job_env(ad);
+
+	sm.startIterations();
+	MyString key,value;
+	while(sm.iterate(key,value)) {
+		printf("%s=%s\n",key.Value(), value.Value());
+	}
+
+	
+	return 0;
+}
+#endif
---
+++ src/condor_c++_util/build_job_env.h
@@ -0,0 +1,56 @@
+/***************************Copyright-DO-NOT-REMOVE-THIS-LINE**
+  *
+  * Condor Software Copyright Notice
+  * Copyright (C) 1990-2004, Condor Team, Computer Sciences Department,
+  * University of Wisconsin-Madison, WI.
+  *
+  * This source code is covered by the Condor Public License, which can
+  * be found in the accompanying LICENSE.TXT file, or online at
+  * www.condorproject.org.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  * AND THE UNIVERSITY OF WISCONSIN-MADISON "AS IS" AND ANY EXPRESS OR
+  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  * WARRANTIES OF MERCHANTABILITY, OF SATISFACTORY QUALITY, AND FITNESS
+  * FOR A PARTICULAR PURPOSE OR USE ARE DISCLAIMED. THE COPYRIGHT
+  * HOLDERS AND CONTRIBUTORS AND THE UNIVERSITY OF WISCONSIN-MADISON
+  * MAKE NO MAKE NO REPRESENTATION THAT THE SOFTWARE, MODIFICATIONS,
+  * ENHANCEMENTS OR DERIVATIVE WORKS THEREOF, WILL NOT INFRINGE ANY
+  * PATENT, COPYRIGHT, TRADEMARK, TRADE SECRET OR OTHER PROPRIETARY
+  * RIGHT.
+  *
+  ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
+#ifndef _build_job_env_H_
+#define _build_job_env_H_
+
+#include "condor_common.h"
+#include "condor_classad.h"
+#include "condor_string.h"
+
+typedef HashTable<MyString,MyString> StringMap;
+/** Given a job's classad, return environment variables job should get.
+
+This includes:
+- X509_USER_PROXY
+
+This should eventually include:
+- Condor provided environment like _CONDOR_SCRATCH_DIR
+- "POSIX" stuff (HOME, USERNAME, TEMP?)
+
+This might, but may not eventually include:
+- Contents of the ATTR_JOB_ENVIRONMENT
+
+Typical use might look something like this (taken from condor_starter.V6.1/user_proc.C)
+		Env * proc_env; // Comes from outside
+		StringMap classenv = build_job_env(JobAd);
+		sm.startIterations();
+		MyString key,value;
+		while(sm.iterate(key,value)) {
+			proc_env->Put(key.Value(),value.Value());
+		}
+
+
+*/
+StringMap build_job_env(const ClassAd & ad);
+
+#endif /* _build_job_env_H_ */
