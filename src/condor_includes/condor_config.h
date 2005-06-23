2a46d0fba202ebe2a915ddb6dda1cb8b271751d4
adesmet <adesmet>
2005-06-23 19:46:23 +0000

    Delicious comments.

--- src/condor_includes/condor_config.h
+++ src/condor_includes/condor_config.h
@@ -63,6 +63,23 @@ extern "C" {
         int  param_boolean_int( const char *name, int default_value );  
 	int  set_persistent_config(char *admin, char *config);
 	int  set_runtime_config(char *admin, char *config);
+	/** Expand parameter references of the form "left$(middle)right".  
+	
+	This is deceptively simple, but does handle multiple and or nested
+	references.  If self is not NULL, then we only expand references to to the
+	parameter specified by self.  This is used when we want to expand
+	self-references only.
+
+	table is a hash table (managed with insert() and macro_lookup)
+	that contains the various $(middle)s.
+
+	Also expand references of the form "left$ENV(middle)right",
+	replacing $ENV(middle) with getenv(middle).
+
+	Also expand references of the form "left$RANDOM_CHOICE(middle)right".
+
+	NOTE: Returns malloc()ed memory; caller is responsible for calling free().
+	*/
 	char * expand_macro ( const char *value, BUCKET *table[], int table_size,
 						  char *self=NULL );
 	int get_var( register char *value, register char **leftp,
@@ -71,14 +88,33 @@ extern "C" {
 	int get_special_var( const char *prefix, bool only_id_chars, register char *value, register char **leftp,
 				 register char **namep, register char **rightp);
 }
+
 #endif
 
 BEGIN_C_DECLS
 
 	char * get_tilde();
 	char * param ( const char *name );
+	/** Insert a value into a hash table.
+
+	The value of 'value' is associated with the name 'name'.  This is
+	inserted into the table 'table' which is 'table_size' big.
+	insert keeps copies of the name and value.
+	*/
 	void insert ( const char *name, const char *value, BUCKET *table[], int table_size );
+
+	/** Look up a value by the name 'name' from the table 'table' which is table_size big.
+	
+	Values should have been inserted with insert() (above).
+	Treats name case insensitively.  Returns NULL if the name isn't in the
+	table.  On success returns a pointer to the associated value.  The
+	value is owned by the table; do not free it.
+	*/
 	char * lookup_macro ( const char *name, BUCKET *table[], int table_size );
+	/*
+	As expand_macro() (above), but assumes the table 'ConfigTab' which is
+	of size TABLESIZE.
+	*/
 	char * macro_expand ( const char *name );
 	int param_in_pattern ( char *parameter, char *pattern);
 	void init_config ( void );
