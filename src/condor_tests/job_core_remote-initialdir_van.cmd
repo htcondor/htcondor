40b995442a5a83aadcfe5b0f95bc355ca16e24da
danb <danb>
2005-07-27 20:01:07 +0000

    Added a test of remote_initialdir in vanilla universe.

---
+++ src/condor_tests/job_core_remote-initialdir_van.cmd
@@ -0,0 +1,14 @@
+#Turn on transfer-files mode and pass some input to /bin/sh, just to
+#make sure remote_initialdir does not break that.
+
+universe = vanilla
+executable = /bin/sh
+remote_initialdir = /tmp
+log = job_core_remote-initialdir_van.log
+output = job_core_remote-initialdir_van.out
+error = job_core_remote-initialdir_van.err
+input = job_core_remote-initialdir_van.in
+Notification = NEVER
+should_transfer_files = true
+when_to_transfer_output = on_exit
+queue
