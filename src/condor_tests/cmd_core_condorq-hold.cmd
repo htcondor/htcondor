a3921cfbef0dfdeb8381c3c4239add373662de71
bt <bt>
2005-07-28 21:50:18 +0000

    Adding prototype/first test for condor_q

---
+++ src/condor_tests/cmd_core_condorq-hold.cmd
@@ -0,0 +1,9 @@
+universe   = vanilla
+executable = x_sleep.pl
+log = cmd_core_condorq-hold.log
+output = cmd_core_condorq-hold.out
+error = cmd_core_condorq-hold.err
+Notification = NEVER
+arguments  = 40
+queue
+
