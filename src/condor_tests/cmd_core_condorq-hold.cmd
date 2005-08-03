7a1a3d2651103f90167c9351dd4dc6ad23683c9f
bt <bt>
2005-08-03 19:21:38 +0000

    renaming cmd_condorq-hold test to cmd_q_shows-hold.

--- src/condor_tests/cmd_core_condorq-hold.cmd
+++
@@ -1,9 +0,0 @@
-universe   = vanilla
-executable = x_sleep.pl
-log = cmd_core_condorq-hold.log
-output = cmd_core_condorq-hold.out
-error = cmd_core_condorq-hold.err
-Notification = NEVER
-arguments  = 40
-queue
-
