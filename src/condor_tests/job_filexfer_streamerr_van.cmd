de724ce0519bbd9d63066d62f54a9d97dcdf0c42
bt <bt>
2005-08-04 16:22:49 +0000

    these 4 files replacxe all the old files for the old job_ccore_streamXXX_van  tests

---
+++ src/condor_tests/job_filexfer_streamerr_van.cmd
@@ -0,0 +1,12 @@
+universe = vanilla
+executable = x_job_filexfer_stream.pl
+log = job_filexfer_streamerr_van.log
+output = job_filexfer_streamerr_van.out
+error = job_filexfer_streamerr_van.err
+when_to_transfer_output = ON_EXIT_OR_EVICT
+stream_error = true
+arguments = stderr 100 300
+getenv=false
+Notification = NEVER
+queue
+
---
+++ src/condor_tests/job_filexfer_streamout_van.cmd
@@ -0,0 +1,12 @@
+universe = vanilla
+executable = x_job_filexfer_stream.pl
+log = job_filexfer_streamout_van.log
+output = job_filexfer_streamout_van.out
+error = job_filexfer_streamout_van.err
+when_to_transfer_output = ON_EXIT_OR_EVICT
+stream_output = true
+arguments = stdout 100 300
+getenv=false
+Notification = NEVER
+queue
+
