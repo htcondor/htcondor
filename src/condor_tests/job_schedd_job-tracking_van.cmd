80e8b52b1999b190cf8207d5f1535a1ea8f57b78
bt <bt>
2005-08-04 22:35:04 +0000

    Test to see if schedd is loosing track of jobs

---
+++ src/condor_tests/job_schedd_job-tracking_van.cmd
@@ -0,0 +1,9 @@
+universe   = scheduler
+executable = x_returnquick.pl
+log = job_schedd_job-tracking_van.log
+error = job_schedd_job-tracking_van.err
+output = job_schedd_job-tracking_van.out
+Notification = NEVER
+arguments = hello
+queue 400
+
