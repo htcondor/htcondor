1d4b9b893de7c38eb056aa278592db41734c5626
bt <bt>
2005-08-17 21:32:40 +0000

    Make the sleep job be a sleep forever to remove a race condition relative to the job completing before it is possible to place it on hold.

--- src/condor_tests/job_core_holdrelease_sched.cmd
+++ src/condor_tests/job_core_holdrelease_sched.cmd
@@ -4,6 +4,6 @@ log = job_core_holdrelease_sched.log
 output = job_core_holdrelease_sched.out
 error = job_core_holdrelease_sched.err
 Notification = NEVER
-arguments  = 40
+arguments  = 0
 queue
 
--- src/condor_tests/job_core_holdrelease_van.cmd
+++ src/condor_tests/job_core_holdrelease_van.cmd
@@ -4,6 +4,6 @@ log = job_core_holdrelease_van.log
 output = job_core_holdrelease_van.out
 error = job_core_holdrelease_van.err
 Notification = NEVER
-arguments  = 40
+arguments  = 0
 queue
 
