9310945cb1988c81901aef092a930885c2e7a1c6
jfrey <jfrey>
2005-08-23 20:26:38 +0000

    Fixed a bug that caused the gridmanager to go into an infinite loop if an entry in the job's environment was missing an equals sign. (Version history entry for globusjob.C in 6.6.11 covers this as well.)

--- src/condor_gridmanager/gt3job.C
+++ src/condor_gridmanager/gt3job.C
@@ -1604,7 +1604,7 @@ MyString *GT3Job::buildSubmitRSL()
 		if ( env_vec[0] ) {
 			*rsl += ")(environment=";
 		}
-		while (env_vec[i]) {
+		for ( i = 0; env_vec[i]; i++ ) {
 			char *equals = strchr(env_vec[i],'=');
 			if ( !equals ) {
 				// this environment entry has no equals sign!?!?
@@ -1614,7 +1614,6 @@ MyString *GT3Job::buildSubmitRSL()
 			buff.sprintf( "(%s %s)", env_vec[i],
 							 rsl_stringify(equals + 1) );
 			*rsl += buff;
-			i++;
 		}
 	}
 	if ( attr_value ) {
