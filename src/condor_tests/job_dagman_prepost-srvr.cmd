0227df17ee426106bdab3cbaea900e4b77c2e2e8
bt <bt>
2005-09-30 16:32:02 +0000

    The check-in server is now started and stopped from within the main test and not started or removed as a condor job making these file not be needed anymore.

--- src/condor_tests/job_dagman_prepost-srvr.cmd
+++
@@ -1,9 +0,0 @@
-universe   = scheduler
-executable = ./x_general_server.pl
-log = job_dagman_prepost-srvr.log
-output = job_dagman_prepost-srvr.out
-error = job_dagman_prepost-srvr.err
-arguments = /tmp/prepostsock job_dagman_prepost-srvr-log.log
-Notification = NEVER
-queue
-
