b279466649e4339e24d7b2a3e4459d7dd5f139b5
bt <bt>
2005-09-30 15:20:50 +0000

    These tests are now responsible for starting and stopping their check-in server due to problems on HPUX running the server as a condor job. It also simplifies the test and the files needed for it.

--- src/condor_tests/job_dagman_maxpostscripts-srvr.cmd
+++
@@ -1,9 +0,0 @@
-universe   = scheduler
-executable = ./x_general_server.pl
-log = job_dagman_maxpostscripts-srvr.log
-output = job_dagman_maxpostscripts-srvr.out
-error = job_dagman_maxpostscripts-srvr.err
-arguments = /tmp/maxpostsock job_dagman_maxpostscripts-srvr-log.log
-Notification = NEVER
-queue
-
