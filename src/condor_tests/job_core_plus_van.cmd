1c2a468d9acc556f16af223f84a201c4c4a002fa
wright <wright>
2005-10-13 22:35:50 +0000

    merged V6_6-branch as tagged at V6_6-merge-49 into V6_7-branch

--- src/condor_tests/job_core_plus_van.cmd
+++ src/condor_tests/job_core_plus_van.cmd
@@ -3,11 +3,12 @@ executable = x_sleep.pl
 log = job_core_plus_van.log
 output = job_core_plus_van.out
 error = job_core_plus_van.err
+hold = true
 +foo		= "bar"
 +bar		= "foo"
 +last		= "first"
 +done		= FALSE
 Notification = NEVER
-arguments  = 10
+arguments  = 0
 queue
 
