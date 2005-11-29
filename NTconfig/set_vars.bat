0eebf114feaf5518db03c56ed5dfc45ee04d2b95
tannenba <tannenba>
2005-11-29 21:02:55 +0000

    changes to upgrade the version of gsoap used in Condor to gsoap 2.7.6c.  this change was made to clear up some problems when trying to use Microsoft .NET to talk to the Condor daemons.

--- NTconfig/set_vars.bat
+++ NTconfig/set_vars.bat
@@ -16,7 +16,7 @@ set EXT_TRIGGERS=%EXTERN_DIR%\triggers
 REM Specify which versions of the externals we're using. To add a 
 REM new external, just add its version here, and add that to the 
 REM EXTERNALS_NEEDED variable defined below.
-set EXT_GSOAP_VERSION=gsoap-2.7
+set EXT_GSOAP_VERSION=gsoap-2.7.6c
 set EXT_OPENSSL_VERSION=openssl-0.9.8
 set EXT_POSTGRESQL_VERSION=postgresql-8.0.2
 set EXT_KERBEROS_VERSION=
