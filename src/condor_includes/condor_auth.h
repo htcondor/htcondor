6b59a3d815fd12195e3510166f8e33db42afaf9e
tannenba <tannenba>
2006-03-17 17:16:33 +0000

    change the value for CAUTH_PASSWORD to make certain it does not conflict with earlier protocol implementations. define CAUTH_SSL.

--- src/condor_includes/condor_auth.h
+++ src/condor_includes/condor_auth.h
@@ -40,7 +40,8 @@ const int CAUTH_NTSSPI                  = 16;
 const int CAUTH_GSI                     = 32;
 const int CAUTH_KERBEROS                = 64;
 const int CAUTH_ANONYMOUS               = 128;
-const int CAUTH_PASSWORD				= 256;
+const int CAUTH_SSL                     = 256;
+const int CAUTH_PASSWORD                = 512;
 
 const char STR_DEFAULT_CONDOR_USER[]    = "condor";    // Default condor user
 const char STR_DEFAULT_CONDOR_SPOOL[]   = "SPOOL";
