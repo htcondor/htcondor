2e20abc14820936aaf569c0a43e7d74df6adeb92
nleroy <nleroy>
2006-03-23 15:37:50 +0000

    Backed out the changes to the collector for SOAP; these have instead been incorporated into 6.7.18, and will be merged back into 6.7 later.

--- src/condor_collector.V6/soap_collectorStub.C
+++ src/condor_collector.V6/soap_collectorStub.C
@@ -33,120 +33,6 @@
 
 #include "../condor_c++_util/soap_helpers.cpp"
 
-extern CollectorDaemon* Daemon;
-
-//struct timeval convert_start_time, convert_end_time, convert_time_diff;
-
-int
-condor__insertAd(struct soap *soap,
-				 enum condor__ClassAdType type,
-				 struct condor__ClassAdStruct adStruct,
-				 struct condor__insertAdResponse &result)
-{
-	dprintf(D_ALWAYS, "Entering condor__insertAd...\n");
-
-		// Instead of a big switch an array index could be used, but
-		// it would mean keeping the order of things in the
-		// condor__ClassAdType enum and this code in sync.
-	int command;
-	switch (type) {
-	case STARTD_AD_TYPE:     command = UPDATE_STARTD_AD;     break;
-	case QUILL_AD_TYPE:      command = UPDATE_QUILL_AD;      break;
-	case SCHEDD_AD_TYPE:     command = UPDATE_SCHEDD_AD;     break;
-	case SUBMITTOR_AD_TYPE:  command = UPDATE_SUBMITTOR_AD;  break;
-	case LICENSE_AD_TYPE:    command = UPDATE_LICENSE_AD;    break;
-	case MASTER_AD_TYPE:     command = UPDATE_MASTER_AD;     break;
-	case CKPTSRVR_AD_TYPE:   command = UPDATE_CKPT_SRVR_AD;  break;
-	case COLLECTOR_AD_TYPE:  command = UPDATE_COLLECTOR_AD;  break;
-	case STORAGE_AD_TYPE:    command = UPDATE_STORAGE_AD;    break;
-	case NEGOTIATOR_AD_TYPE: command = UPDATE_NEGOTIATOR_AD; break;
-	case HAD_AD_TYPE:        command = UPDATE_HAD_AD;        break;
-	default:
-		command = -1; // Let's hope a command is never -1
-		result.status.code = FAIL;
-		result.status.message = "Unknown type";
-		return SOAP_OK;
-		break;
-	}
-
-	ClassAd *ad = new ClassAd;
-	sockaddr_in *from = NULL;	// We don't need this ONLY because
-								// it is ignored in all hash
-								// functions in hashkey.C
-	int insert;	// Return value from CollectorEngine::collect
-	Sock *sock = NULL; // Updating of Startd ads will fail without
-					   // this. It is needed to retrieve the
-					   // startd private ad.
-	if (!ad) {
-		result.status.code = FAIL;
-		result.status.message = "Collestor has run out of memory, sorry";
-		return SOAP_OK;
-	}
-
-	if (!convert_adStruct_to_ad(soap, ad, &adStruct)) {
-		dprintf(D_FULLDEBUG,
-				"Failed to convert ClassAdStruct to ClassAd.\n");
-
-		result.status.code = FAIL;
-		result.status.message = "Failed to parse the ClassAdStruct";
-		delete ad;
-		return SOAP_OK;
-	}
-
-	if (!Daemon->getCollector().collect(command,
-										ad,
-										from,
-										insert,
-										sock)) {
-		result.status.code = FAIL;
-		result.status.message = "Failed to parse the ClassAdStruct";
-		delete ad;
-		return SOAP_OK;
-	}
-
-		// Finally, do some real work.
-	switch (insert) {
-	case 0:
-		result.status.code = SUCCESS;
-		result.status.message = "Updated ad";
-		break;
-	case 1:
-		result.status.code = SUCCESS;
-		result.status.message = "New ad inserted";
-		break;
-	case -3:
-		result.status.code = FAIL;
-		result.status.message = "Internal update failure";
-		break;
-	case -2:
-		result.status.code = FAIL;
-		result.status.message = "Unsupported internal command";
-		break;
-	case -1:
-		result.status.code = FAIL;
-		result.status.message = "Illegal internal command";
-		break;
-	default:
-		result.status.code = FAIL;
-		result.status.message = "Unknown internal error code";
-		break;
-	}
-
-		// Don't leak a ClassAd on error, otherwise it becomes the
-		// CollectorEngine's responsibility
-	if (FAIL == result.status.code) {
-		if (ad) {
-			delete ad;
-			ad = NULL;
-		}
-	}
-
-	dprintf(D_ALWAYS, "Leaving condor__insertAd...\n");
-
-	return SOAP_OK;
-}
-
-
 static int receive_query_soap(int command,struct soap *s,char *constraint,
 	struct condor__ClassAdStructArray &ads)
 {
@@ -174,18 +60,10 @@ static int receive_query_soap(int command,struct soap *s,char *constraint,
 	query_ad.fPrint(stderr);
 	CollectorDaemon::process_query_public (whichAds, &query_ad, &adList);
 
-//	ASSERT(0 == gettimeofday(&convert_start_time, NULL));
 	// and fill in our soap struct response
 	if ( !convert_adlist_to_adStructArray(s,&adList,&ads) ) {
 		dprintf(D_ALWAYS, "receive_query_soap: convert_adlist_to_adStructArray failed!\n");
 	}
-//	ASSERT(0 == gettimeofday(&convert_end_time, NULL));
-//	timersub(&convert_end_time, &convert_start_time, &convert_time_diff);
-
-//	fprintf(stdout,
-//			"TIMING: CONVERT: %ld.%.6ld\n",
-//			convert_time_diff.tv_sec, convert_time_diff.tv_usec);
-
 	return SOAP_OK;
 }
 
