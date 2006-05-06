2cee50c2e14acc12b400930ac1c5ce80fa2d0d0b
jfrey <jfrey>
2006-05-06 15:40:48 +0000

    Set GLITE_LOCATION in the blahp's environment, not the gridmanager's. Otherwise, the blahp won't work.

--- src/condor_gridmanager/gahp-client.C
+++ src/condor_gridmanager/gahp-client.C
@@ -536,6 +536,7 @@ GahpServer::Startup()
 	int low_port;
 	int high_port;
 	Env newenv;
+	char *tmp_char;
 
 		// Check if we already have spawned a GAHP server.  
 	if ( m_gahp_pid != -1 ) {
@@ -584,6 +585,13 @@ GahpServer::Startup()
 		MyString buff;
 		buff.sprintf( "%d,%d", low_port, high_port );
 		newenv.SetEnv( "GLOBUS_TCP_SOURCE_RANGE", buff.Value() );
+	}
+
+		// GLITE_LOCATION needs to be set for the blahp
+	tmp_char = param("GLITE_LOCATION");
+	if ( tmp_char ) {
+		newenv.SetEnv( "GLITE_LOCATION", tmp_char );
+		free( tmp_char );
 	}
 
 		// Now register a reaper, if we haven't already done so.
