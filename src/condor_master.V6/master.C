4872b2a0d9e8d77488a0d30ee2f21d4d8b451427
adesmet <adesmet>
2006-07-17 21:01:14 +0000

    Updating master to recognize QUILL as a DaemonCore Daemon.
    Updated various configuration files to all QUILL and CREDD to DC_DAEMON_LIST.
    In the process ensured all were commented out as most people should
    default to the hard coded version in the master.
    
    (Not documenting seperately as this is just part of the Quill and Credd work)

--- src/condor_master.V6/master.C
+++ src/condor_master.V6/master.C
@@ -127,9 +127,12 @@ char	*default_daemon_list[] = {
 #endif
 	0};
 
+// NOTE: When adding something here, also add it to the various condor_config
+// examples in src/condor_examples
 char	default_dc_daemon_list[] =
 "MASTER, STARTD, SCHEDD, KBDD, COLLECTOR, NEGOTIATOR, EVENTD, "
-"VIEW_SERVER, CONDOR_VIEW, VIEW_COLLECTOR, HAWKEYE, CREDD, HAD";
+"VIEW_SERVER, CONDOR_VIEW, VIEW_COLLECTOR, HAWKEYE, CREDD, HAD, "
+"QUILL";
 
 // create an object of class daemons.
 class Daemons daemons;
