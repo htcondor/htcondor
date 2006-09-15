c420fca18c6f8fc711efd881179bb6b2a628e15e
bt <bt>
2006-09-15 20:48:27 +0000

    Initial working GSI authentication test prior to verify of owner and user in the job ad.

---
+++ src/condor_tests/lib_auth_protocol-gsi.cmd
@@ -0,0 +1,9 @@
+universe   = vanilla
+executable = x_sleep.pl
+log = lib_auth_protocol-gsi.log
+output = lib_auth_protocol-gsi.out
+error = lib_auth_protocol-gsi.err
+Notification = NEVER
+arguments  = 45
+queue
+
