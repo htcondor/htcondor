c4d01565ce39a977d7e3547fc88e1b1e5eba0f8f
zmiller <zmiller>
2006-09-25 16:31:28 +0000

    6.8.1 has been released.  version is now "6.8.2 PRE-RELEASE".

--- src/condor_c++_util/condor_version.C
+++ src/condor_c++_util/condor_version.C
@@ -33,7 +33,7 @@
    will EXCEPT at startup time.  
 */
 
-static char* CondorVersionString = "$CondorVersion: 6.8.1 " __DATE__ "  $";
+static char* CondorVersionString = "$CondorVersion: 6.8.2 " __DATE__ " PRE-RELEASE $";
 
 /* 
    This is some wisdom from Cygnus's web page.  If you just try to use
