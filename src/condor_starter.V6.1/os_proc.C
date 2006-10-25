9e08272961ac2b94858b50a38b45fffbb476f5f7
danb <danb>
2006-10-25 21:10:24 +0000

    Added JOB_INHERITS_STARTER_ENVIRONMENT.

--- src/condor_starter.V6.1/os_proc.C
+++ src/condor_starter.V6.1/os_proc.C
@@ -328,7 +328,10 @@ OsProc::StartJob()
 
 	// Check to see if we need to start this process paused, and if
 	// so, pass the right flag to DC::Create_Process().
-	int job_opt_mask = DCJOBOPT_NO_ENV_INHERIT;
+	int job_opt_mask = 0;
+	if (!param_boolean("JOB_INHERITS_STARTER_ENVIRONMENT",false)) {
+		job_opt_mask = 	DCJOBOPT_NO_ENV_INHERIT;
+	}
 	int suspend_job_at_exec = 0;
 	JobAd->LookupBool( ATTR_SUSPEND_JOB_AT_EXEC, suspend_job_at_exec);
 	if( suspend_job_at_exec ) {
