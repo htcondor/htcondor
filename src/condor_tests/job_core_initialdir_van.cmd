8c440f9335a25e0781415e6346cb1b6545f8fadb
bt <bt>
2006-12-07 21:01:12 +0000

    Redid this test. This test now expects 4 basic lines of output. There is a call to the default output checking function which looks in "initialdir" if it is set. It fails if the job does not work and it fails if the output is not in initialdir and correct.

--- src/condor_tests/job_core_initialdir_van.cmd
+++ src/condor_tests/job_core_initialdir_van.cmd
@@ -1,6 +1,7 @@
 universe = vanilla
-executable =  x_sleep.pl
+executable =  x_echo1234.pl
 initialdir = initd_van
+#log = $ENV(PWD)/job_core_initialdir_van.log
 log = job_core_initialdir_van.log
 output = job_core_initialdir_van.out
 error = job_core_initialdir_van.err
