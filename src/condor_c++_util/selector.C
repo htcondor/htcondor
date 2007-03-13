06559511148c3b44c8fd8c254f1682cdde20db0f
jfrey <jfrey>
2007-03-13 22:54:38 +0000

    Ressurect the Selector class that wraps select() for use throughout Condor. Before, it was only used by the pvm starter. Now, it is imbued with the ability to use select() with file descriptors larger than FD_SETSIZE. We do this by dynamically allocating buffers large enough to hold the largest file descriptor openable by the process.

--- src/condor_c++_util/selector.C
+++ src/condor_c++_util/selector.C
@@ -27,38 +27,145 @@
 #include "condor_debug.h"
 #include "selector.h"
 
-#if defined(IRIX)
-#	include<bstring.h>
-#endif
-
 
 void display_fd_set( char *msg, fd_set *set, int max );
 
+int Selector::_fd_select_size = -1;
+
+fd_set *Selector::cached_read_fds = NULL;
+fd_set *Selector::cached_write_fds = NULL;
+fd_set *Selector::cached_except_fds = NULL;
+
+fd_set *Selector::cached_save_read_fds = NULL;
+fd_set *Selector::cached_save_write_fds = NULL;
+fd_set *Selector::cached_save_except_fds = NULL;
+
 Selector::Selector()
 {
+#if defined(WIN32)
+		// On Windows, we can't treat fd_set as an open-ended bit array.
+		// fd_set can take up to FD_SETSIZE sockets (not any socket whose
+		// fd is <= FD_SETSIZE) and that's it. Currently, we set
+		// FD_SETSIZE to 1024. I'm not sure what we can do if we ever
+		// have more than 1024 sockets to select on.
+	fd_set_size = 1;
+#else
+	int nfdbits = 8 * sizeof(fd_set);
+	fd_set_size = ( fd_select_size() + (nfdbits - 1) ) / nfdbits;
+#endif
+
+	if ( cached_read_fds ) {
+		read_fds = cached_read_fds;
+		write_fds = cached_write_fds;
+		except_fds = cached_except_fds;
+
+		save_read_fds = cached_save_read_fds;
+		save_write_fds = cached_save_write_fds;
+		save_except_fds = cached_save_except_fds;
+
+		cached_read_fds = NULL;
+		cached_write_fds = NULL;
+		cached_except_fds = NULL;
+		cached_save_read_fds = NULL;
+		cached_save_write_fds = NULL;
+		cached_save_except_fds = NULL;
+	} else {
+		read_fds = (fd_set *)calloc( fd_set_size, sizeof(fd_set) );
+		write_fds = (fd_set *)calloc( fd_set_size, sizeof(fd_set) );
+		except_fds = (fd_set *)calloc( fd_set_size, sizeof(fd_set) );
+
+		save_read_fds = (fd_set *)calloc( fd_set_size, sizeof(fd_set) );
+		save_write_fds = (fd_set *)calloc( fd_set_size, sizeof(fd_set) );
+		save_except_fds = (fd_set *)calloc( fd_set_size, sizeof(fd_set) );
+	}
+
+	reset();
+}
+
+Selector::~Selector()
+{
+	if ( cached_read_fds == NULL ) {
+		cached_read_fds = read_fds;
+		cached_write_fds = write_fds;
+		cached_except_fds = except_fds;
+
+		cached_save_read_fds = save_read_fds;
+		cached_save_write_fds = save_write_fds;
+		cached_save_except_fds = save_except_fds;
+	} else {
+		free( read_fds );
+		free( write_fds );
+		free( except_fds );
+
+		free( save_read_fds );
+		free( save_write_fds );
+		free( save_except_fds );
+	}
+}
+
+void
+Selector::reset()
+{
+	_select_retval = -2;
 	state = VIRGIN;
 	timeout_wanted = FALSE;
 	max_fd = -1;
-	FD_ZERO( &save_read_fds );
-	FD_ZERO( &save_write_fds );
-	FD_ZERO( &save_except_fds );
+#if defined(WIN32)
+	FD_ZERO( save_read_fds );
+	FD_ZERO( save_write_fds );
+	FD_ZERO( save_except_fds );
+#else
+	memset( save_read_fds, 0, fd_set_size * sizeof(fd_set) );
+	memset( save_write_fds, 0, fd_set_size * sizeof(fd_set) );
+	memset( save_except_fds, 0, fd_set_size * sizeof(fd_set) );
+#endif
+}
+
+int
+Selector::fd_select_size()
+{
+	if ( _fd_select_size < 0 ) {
+#if defined(WIN32)
+		// On Windows, fd_set is not a bit-array, but a structure that can
+		// hold up to 1024 different file descriptors (not just file 
+		// descriptors whose value is less than 1024). We set max_fd to
+		// 1024 (FD_SETSIZE) as a reasonable approximation.
+		_fd_select_size = FD_SETSIZE;
+#elif defined(Solaris)
+		// Solaris's select() can't handle fds greater than FD_SETSIZE.
+		int max_fds = getdtablesize();
+		if ( max_fds < FD_SETSIZE ) {
+			_fd_select_size = max_fds;
+		} else {
+			_fd_select_size = FD_SETSIZE;
+		}
+#else
+		_fd_select_size = getdtablesize();
+#endif
+	}
+	return _fd_select_size;
 }
 
 void
 Selector::add_fd( int fd, IO_FUNC interest )
 {
+	if ( fd < 0 || fd > fd_select_size() ) {
+		EXCEPT( "Selector::add_fd(): fd %d outside valid range 0-%d",
+				fd, _fd_select_size-1 );
+	}
+
 	switch( interest ) {
 
 	  case IO_READ:
-		FD_SET( fd, &save_read_fds );
+		FD_SET( fd, save_read_fds );
 		break;
 
 	  case IO_WRITE:
-		FD_SET( fd, &save_write_fds );
+		FD_SET( fd, save_write_fds );
 		break;
 
 	  case IO_EXCEPT:
-		FD_SET( fd, &save_except_fds );
+		FD_SET( fd, save_except_fds );
 		break;
 
 	}
@@ -71,41 +178,43 @@ Selector::add_fd( int fd, IO_FUNC interest )
 void
 Selector::delete_fd( int fd, IO_FUNC interest )
 {
+	if ( fd < 0 || fd > fd_select_size() ) {
+		EXCEPT( "Selector::delete_fd(): fd %d outside valid range 0-%d",
+				fd, _fd_select_size-1 );
+	}
+
 	switch( interest ) {
 
 	  case IO_READ:
-		FD_CLR( fd, &save_read_fds );
+		FD_CLR( fd, save_read_fds );
 		break;
 
 	  case IO_WRITE:
-		FD_CLR( fd, &save_write_fds );
+		FD_CLR( fd, save_write_fds );
 		break;
 
 	  case IO_EXCEPT:
-		FD_CLR( fd, &save_except_fds );
+		FD_CLR( fd, save_except_fds );
 		break;
 
 	}
 }
 
 void
-Selector::set_timeout( clock_t ticks )
+Selector::set_timeout( time_t sec, long usec )
 {
-	static long clock_tick;
-
-	if( ticks < 0 ) {
-		ticks = 0;
-	}
-
 	timeout_wanted = TRUE;
 
-	if( !clock_tick ) {
-		clock_tick = sysconf( _SC_CLK_TCK );
-	}
+	timeout.tv_sec = sec;
+	timeout.tv_usec = usec;
+}
 
-	timeout.tv_sec = ticks / clock_tick;
-	timeout.tv_usec = ticks % clock_tick;
-	timeout.tv_usec *= 1000000 / clock_tick;
+void
+Selector::set_timeout( timeval tv )
+{
+	timeout_wanted = TRUE;
+
+	timeout = tv;
 }
 
 void
@@ -120,9 +229,9 @@ Selector::execute()
 	int		nfds;
 	struct timeval	*tp;
 
-	read_fds = save_read_fds;
-	write_fds = save_write_fds;
-	except_fds = save_except_fds;
+	memcpy( read_fds, save_read_fds, fd_set_size * sizeof(fd_set) );
+	memcpy( write_fds, save_write_fds, fd_set_size * sizeof(fd_set) );
+	memcpy( except_fds, save_except_fds, fd_set_size * sizeof(fd_set) );
 
 	if( timeout_wanted ) {
 		tp = &timeout;
@@ -131,48 +240,62 @@ Selector::execute()
 	}
 
 	nfds = select( max_fd + 1, 
-				  (SELECT_FDSET_PTR) &read_fds, 
-				  (SELECT_FDSET_PTR) &write_fds, 
-				  (SELECT_FDSET_PTR) &except_fds, 
+				  (SELECT_FDSET_PTR) read_fds, 
+				  (SELECT_FDSET_PTR) write_fds, 
+				  (SELECT_FDSET_PTR) except_fds, 
 				  tp );
+	_select_retval = nfds;
 
 	if( nfds < 0 ) {
+#if !defined(WIN32)
 		if( errno == EINTR ) {
 			state = SIGNALLED;
 			return;
-		} else {
-			EXCEPT( "select" );
 		}
+#endif
+		state = FAILED;
+		return;
 	}
 
-	if( NFDS(nfds) == 0 ) {
+	if( nfds == 0 ) {
 		state = TIMED_OUT;
 	} else {
 		state = FDS_READY;
 	}
+	return;
+}
+
+int
+Selector::select_retval()
+{
+	return _select_retval;
 }
 
 BOOLEAN
 Selector::fd_ready( int fd, IO_FUNC interest )
 {
-	if( state != FDS_READY ) {
+	if( state != FDS_READY && state != TIMED_OUT ) {
 		EXCEPT(
 			"Selector::fd_ready() called, but selector not in FDS_READY state"
 		);
 	}
 
+	if ( fd < 0 || fd > fd_select_size() ) {
+		return FALSE;
+	}
+
 	switch( interest ) {
 
 	  case IO_READ:
-		return FD_ISSET( fd, &read_fds );
+		return FD_ISSET( fd, read_fds );
 		break;
 
 	  case IO_WRITE:
-		return FD_ISSET( fd, &write_fds );
+		return FD_ISSET( fd, write_fds );
 		break;
 
 	  case IO_EXCEPT:
-		return FD_ISSET( fd, &except_fds );
+		return FD_ISSET( fd, except_fds );
 		break;
 
 	}
@@ -191,6 +314,12 @@ BOOLEAN
 Selector::signalled()
 {
 	return state == SIGNALLED;
+}
+
+BOOLEAN
+Selector::failed()
+{
+	return state == FAILED;
 }
 
 BOOLEAN
@@ -221,20 +350,23 @@ Selector::display()
 		dprintf( D_ALWAYS, "State = SIGNALLED\n" );
 		break;
 
+	  case FAILED:
+		dprintf( D_ALWAYS, "State = FAILED\n" );
+		break;
 	}
 
 	dprintf( D_ALWAYS, "max_fd = %d\n", max_fd );
 
 	dprintf( D_ALWAYS, "Selection FD's\n" );
-	display_fd_set( "\tRead", &save_read_fds, max_fd );
-	display_fd_set( "\tWrite", &save_write_fds, max_fd );
-	display_fd_set( "\tExcept", &save_except_fds, max_fd );
+	display_fd_set( "\tRead", save_read_fds, max_fd );
+	display_fd_set( "\tWrite", save_write_fds, max_fd );
+	display_fd_set( "\tExcept", save_except_fds, max_fd );
 
 	if( state == FDS_READY ) {
 		dprintf( D_ALWAYS, "Ready FD's\n" );
-		display_fd_set( "\tRead", &read_fds, max_fd );
-		display_fd_set( "\tWrite", &write_fds, max_fd );
-		display_fd_set( "\tExcept", &except_fds, max_fd );
+		display_fd_set( "\tRead", read_fds, max_fd );
+		display_fd_set( "\tWrite", write_fds, max_fd );
+		display_fd_set( "\tExcept", except_fds, max_fd );
 	}
 	if( timeout_wanted ) {
 		dprintf( D_ALWAYS,
--- src/condor_c++_util/selector.h
+++ src/condor_c++_util/selector.h
@@ -24,36 +24,51 @@
 #ifndef SELECTOR_H
 #define SELECTOR_H
 
-typedef enum {
-	IO_READ, IO_WRITE, IO_EXCEPT
-} IO_FUNC;
-
-typedef enum {
-	VIRGIN, FDS_READY, TIMED_OUT, SIGNALLED
-} SELECTOR_STATE;
+#include "condor_common.h"
 
 class Selector {
 public:
 	Selector();
+	~Selector();
+	static int fd_select_size();
+
+	enum IO_FUNC {
+		IO_READ, IO_WRITE, IO_EXCEPT
+	};
+
+	enum SELECTOR_STATE {
+		VIRGIN, FDS_READY, TIMED_OUT, SIGNALLED, FAILED
+	};
+
+	void reset();
 	void add_fd( int fd, IO_FUNC interest );
 	void delete_fd( int fd, IO_FUNC interest );
-	void set_timeout( clock_t ticks );
+	void set_timeout( time_t sec, long usec = 0 );
+	void set_timeout( timeval tv );
 	void unset_timeout();
-	void	execute();
+	void execute();
+	int select_retval();
 	BOOLEAN	has_ready();
 	BOOLEAN	timed_out();
 	BOOLEAN	signalled();
+	BOOLEAN failed();
 	BOOLEAN fd_ready( int fd, IO_FUNC interest );
 	void display();
 
 private:
-	fd_set	read_fds, save_read_fds;
-	fd_set	write_fds, save_write_fds;
-	fd_set	except_fds, save_except_fds;
+	static int _fd_select_size;
+	static fd_set *cached_read_fds, *cached_save_read_fds;
+	static fd_set *cached_write_fds, *cached_save_write_fds;
+	static fd_set *cached_except_fds, *cached_save_except_fds;
+	fd_set	*read_fds, *save_read_fds;
+	fd_set	*write_fds, *save_write_fds;
+	fd_set	*except_fds, *save_except_fds;
+	int		fd_set_size;
 	int		max_fd;
 	BOOLEAN			timeout_wanted;
 	struct timeval	timeout;
 	SELECTOR_STATE	state;
+	int		_select_retval;
 };
 
 #endif
