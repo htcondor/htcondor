b71d747ddf05f72e4fae6d773656ed5fc3b3d85a
bt <bt>
2007-04-03 20:43:44 +0000

    It was not using its own log file but the restart-holdjobs one....

--- src/condor_tests/job_schedd_restart-runningjobs-ok.cmd
+++ src/condor_tests/job_schedd_restart-runningjobs-ok.cmd
@@ -1,8 +1,8 @@
 universe   = vanilla
 executable = x_sleep.pl
-output = job_schedd_restart-holdjobs-ok.out
-error = job_schedd_restart-holdjobs-ok.err
-log = job_schedd_restart-holdjobs-ok.log
+output = job_schedd_restart-running-ok.out
+error = job_schedd_restart-running-ok.err
+log = job_schedd_restart-running-ok.log
 Notification = NEVER
 arguments = 0
 queue 10
