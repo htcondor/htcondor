55199e93ed1c789411e6e2d234071197f9e99024
gthain <gthain>
2007-04-09 21:37:30 +0000

    try to work around VC++ ICE

--- src/condor_includes/condor_system.h
+++ src/condor_includes/condor_system.h
@@ -86,7 +86,10 @@
 #include "condor_fix_access.h"
 #include "condor_file_lock.h"
 #include "condor_fix_assert.h"
-//#include "condor_fix_iomanip.h"
+
+#if defined(WIN32)
+#include "condor_fix_iomanip.h"
+#endif
 
 
 /**********************************************************************
