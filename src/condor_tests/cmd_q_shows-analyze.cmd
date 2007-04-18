e414d8d37e11a8e747d7067bfa92ca5ae61bd32e
bt <bt>
2007-04-18 23:40:57 +0000

    Add tests for the 2 kinds 0f analyze.

---
+++ src/condor_tests/cmd_q_shows-analyze.cmd
@@ -0,0 +1,10 @@
+universe   = vanilla
+executable = x_sleep.pl
+log = cmd_q_shows-analyze.log
+output = cmd_q_shows-analyze.out
+error = cmd_q_shows-analyze.err
+Notification = NEVER
+requirements = Memory >= 3000
+arguments  = 0
+queue
+
