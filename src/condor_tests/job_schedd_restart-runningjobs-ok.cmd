fd0757ca235c0457a6489408480fa422537f33ae
danb <danb>
2007-06-19 01:23:55 +0000

    Fixed job_schedd_restart-runningjobs test to use job_lease_duration=0, since new behavior of starter is not to immediately give up when gracefully shutting down a disconnected job.

--- src/condor_tests/job_schedd_restart-runningjobs-ok.cmd
+++ src/condor_tests/job_schedd_restart-runningjobs-ok.cmd
@@ -1,5 +1,7 @@
 universe   = vanilla
 executable = x_sleep.pl
+# Avoid having the starter wait around for a reconnect
+job_lease_duration = 0
 output = job_schedd_restart-running-ok.out
 error = job_schedd_restart-running-ok.err
 log = job_schedd_restart-running-ok.log
