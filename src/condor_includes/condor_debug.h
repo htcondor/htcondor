ffe75ef50e2f0228187ed13a3f68c51d2344a945
jfrey <jfrey>
2007-09-25 19:53:30 +0000

    Document a dprintf level available for reuse.

--- src/condor_includes/condor_debug.h
+++ src/condor_includes/condor_debug.h
@@ -77,6 +77,7 @@
 */ 
 #define D_PID           (1<<28)
 #define D_FDS           (1<<29)
+/* D_SECONDS is no longer used */
 #define D_SECONDS       (1<<30)
 #define D_NOHEADER      (1<<31)
 #define D_ALL           (~(0) & (~(D_NOHEADER)))
