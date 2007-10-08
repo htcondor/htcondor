9e81e39ab71b72e340d7b6a5255b69fb72e40215
bt <bt>
2007-10-08 16:28:32 +0000

    Change from a shell based job to a perl based job.

---
+++ src/condor_tests/job_condorc_xxx_van.pl
@@ -0,0 +1,11 @@
+#! /usr/bin/env perl
+
+open(IN,"<job_condorc_xxx_van.in") || die "can not open job_condorc_xxx_van.in:$!\n";
+open(OUT,">outfile1") || die "can not open outfile1:$!\n";
+while(<IN>) {
+	print OUT "$_\n";
+}
+close(IN);
+$cmd = `pwd`;
+print OUT "$cmd\n";
+close(OUT);
