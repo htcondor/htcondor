3d91d5389a5071ebfc09d3a993543e7d94ea368f
bt <bt>
2007-12-18 18:00:30 +0000

    Redid this two part test and broke it into 4 tests of 4 personal condor runs to make the test viable for windows and other places without timeouts. Used to be 2 tests which each created 8 personal condors.

---
+++ src/condor_tests/lib_auth_protocol-negot-prt3.cmd
@@ -0,0 +1,9 @@
+universe   = vanilla
+executable = x_sleep.pl
+log = lib_auth_protocol-negot-prt3.log
+output = lib_auth_protocol-negot-prt3.out
+error = lib_auth_protocol-negot-prt3.err
+Notification = NEVER
+arguments  = 45
+queue
+
