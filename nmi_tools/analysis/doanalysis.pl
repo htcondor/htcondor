0f16c921ace2f8e98fa3c3d5526b3fcad8c49759
bt <bt>
2008-02-12 20:56:55 +0000

    Tuned blog analysis scripts more easily fliped between releases.

--- nmi_tools/analysis/doanalysis.pl
+++ nmi_tools/analysis/doanalysis.pl
@@ -25,25 +25,25 @@ if( $datafile ) {
 $branch1 = "7_0";
 $branch2 = "7_1";
 
-print "Transition file is <<7_0regdata>>\n";
+#print "Transition file is <<7_0regdata>>\n";
 $outfile = $datalocation . $branch1 . "regdata";
 system("./readtestdata.pl --data=$data --view=testsum --branch=$branch1 --out=$outfile");
 $outimage = $outfile . ".png";
 system("./make-graphs  --daily --input $outfile --output $outimage");
 
-print "Transition file is <<7_0detaildata>>\n";
+#print "Transition file is <<7_0detaildata>>\n";
 $outfile = $datalocation . $branch1 . "detaildata";
 system("./readtestdata.pl --data=$data --view=testdetail  --branch=$branch1 --out=$outfile");
 $outimage = $outfile . ".png";
 system("./make-graphs  --daily --detail analdetail --input $outfile --output $outimage");
 
-print "Transition file is <<7_1regdata>>\n";
+#print "Transition file is <<7_1regdata>>\n";
 $outfile = $datalocation . $branch2 . "regdata";
 system("./readtestdata.pl --data=$data --view=testsum --branch=$branch2 --out=$outfile");
 $outimage = $outfile . ".png";
 system("./make-graphs  --daily --input $outfile --output $outimage");
 
-print "Transition file is <<7_1detaildata>>\n";
+#print "Transition file is <<7_1detaildata>>\n";
 $outfile = $datalocation . $branch2 . "detaildata";
 system("./readtestdata.pl --data=$data --view=testdetail  --branch=$branch2 --out=$outfile");
 $outimage = $outfile . ".png";
