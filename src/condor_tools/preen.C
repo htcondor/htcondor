2f4ec782fd4695512c82b7097c2f4e36a5f33c10
jfrey <jfrey>
2008-03-06 21:04:31 +0000

    Teach condor_preen to recognize MyProxy password files in the spool directory (so that it won't delete them).

--- src/condor_tools/preen.C
+++ src/condor_tools/preen.C
@@ -78,6 +78,7 @@ BOOLEAN is_v2_ckpt( const char *name );
 BOOLEAN is_v3_ckpt( const char *name );
 BOOLEAN cluster_exists( int );
 BOOLEAN proc_exists( int, int );
+BOOLEAN is_myproxy_file( const char *name );
 
 /*
   Tell folks how to use this program.
@@ -272,6 +273,12 @@ check_spool_dir()
 			continue;
 		}
 
+			// See if it's a legimate MyProxy password file
+		if ( is_myproxy_file( f ) ) {
+			good_file( Spool, f );
+			continue;
+		}
+
 			// We think it's bad.  For now, just append it to a
 			// StringList, instead of actually deleting it.  This way,
 			// if DisconnectQ() fails, we can abort and not actually
@@ -382,6 +389,24 @@ is_v3_ckpt( const char *name )
 	} else {
 		return proc_exists( cluster, proc );
 	}
+}
+
+/*
+  Check whether the given file could be a valid MyProxy password file
+  for a queued job.
+*/
+BOOLEAN
+is_myproxy_file( const char *name )
+{
+	int cluster, proc;
+
+		// TODO This will accept files that look like a valid MyProxy
+		//   password file with extra characters on the end of the name.
+	int rc = sscanf( name, "mpp.%d.%d", &cluster, &proc );
+	if ( rc != 2 ) {
+		return FALSE;
+	}
+	return proc_exists( cluster, proc );
 }
 
 /*
