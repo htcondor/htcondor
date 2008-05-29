htcondor/htcondor

commit d63d73c9cab7b6b23a0ca7a579914343250e083d

Fixed Java detection to work with spaces in the path to java.

src/condor_starter.V6.1/starter_v61_main.C
@@ -100,9 +100,6 @@ printClassAd( void )
 	printf( "%s = True\n", ATTR_HAS_JIC_LOCAL_CONFIG );
 	printf( "%s = True\n", ATTR_HAS_JIC_LOCAL_STDIN );
 
-		// Java stuff
-	config(true);
-
 	ClassAd *ad = java_detect();
 	if(ad) {
 		int gotone=0;
@@ -210,6 +207,12 @@ main_pre_dc_init( int argc, char* argv[] )
 		// need to do this *after* we set mySubSystem, since this ends
 		// up calling functions that rely on it being defined...  
 	if( argc == 2 && strincmp(argv[1],"-cla",4) == MATCH ) {
+			// needed for Java stuff
+		config(true);
+			// log any errors to stderr
+		Termlog = 1;
+		dprintf_config(mySubSystem);
+
 		printClassAd();
 		exit(0);
 	}

src/condor_starter.V6.1/java_detect.C
@@ -23,6 +23,7 @@
 #include "condor_classad.h"
 #include "java_detect.h"
 #include "java_config.h"
+#include "my_popen.h"
 
 ClassAd * java_detect()
 {
@@ -39,18 +40,10 @@ ClassAd * java_detect()
 	if(!java_config(path,&args,0)) return 0;
 	int benchmark_time = param_integer("JAVA_BENCHMARK_TIME",0);
 
-	// NOTE: this is not quite right.  If any of the jvm arguments
-	// contain spaces, then V1 syntax cannot handle it, and we
-	// need to pass something more sophisticated to popen that
-	// works under both unix and windows.
-
-	if(!args.GetArgsStringV1Raw(&args_string,&args_error)) {
-		dprintf(D_ALWAYS,"java_detect: failed to produce jvm arguments: %s\n",
-				args_error.Value());
-		return 0;
-	}
-
-	command.sprintf("%s %s CondorJavaInfo old %d",path.Value(),args_string.Value(),benchmark_time);
+	args.InsertArg(path.Value(),0);
+	args.AppendArg("CondorJavaInfo");
+	args.AppendArg("old");
+	args.AppendArg(benchmark_time);
 
 	/*
 	N.B. Certain version of Java do not set up their own signal
@@ -67,13 +60,24 @@ ClassAd * java_detect()
 	sigprocmask(SIG_SETMASK,&mask,0);
 #endif
 
-	FILE *stream = popen(command.Value(),"r");
-	if(!stream) return 0;
+	FILE *stream = my_popen(args,"r",0);
+	if(!stream) {
+		MyString arg_str;
+		args.GetArgsStringForDisplay(&arg_str);
+		dprintf(D_ALWAYS,"JavaDetect: failed to execute %s\n",arg_str.Value());
+		return 0;
+	}
 
 	int eof=0,error=0,empty=0;
 	ClassAd *ad = new ClassAd(stream,"***",eof,error,empty);
 
-	pclose(stream);
+	int rc = my_pclose(stream);
+	if( rc!=0 ) {
+		MyString arg_str;
+		args.GetArgsStringForDisplay(&arg_str);
+		dprintf(D_ALWAYS,"JavaDetect: failure status %d when executing %s\n",rc,arg_str.Value());
+		error = 1;
+	}
 
 	if(error || empty) {
 		delete ad;

commit 6d61fe7c2c1850e476b2aceade52245e12296899

Fixed dprintf error in condor_starter -classad. This fixes a problem internal to 7.0.2 caused by my patch d63d73c9cab7b6b23a0ca7a579914343250e083d.  This was causing the test lib_auth_protocol-negot to fail.

src/condor_starter.V6.1/starter_v61_main.C
@@ -209,8 +209,19 @@ main_pre_dc_init( int argc, char* argv[] )
 	if( argc == 2 && strincmp(argv[1],"-cla",4) == MATCH ) {
 			// needed for Java stuff
 		config(true);
-			// log any errors to stderr
-		Termlog = 1;
+
+			// Would like to log errors to stderr if stderr is not
+			// /dev/null to make it easier for users to debug, but not
+			// sure how to do that on windows.  On Windows, when
+			// condor_starter is run by the startd, setting Termlog=1
+			// causes a dprintf to abort with an error if any calls to
+			// dprintf() are made in a debug level that is turned on.
+			// I have not found a way to detect when stderr is in this
+			// state, so I am just leaving Termlog turned off in all
+			// cases.
+
+		//Termlog = 1;
+
 		dprintf_config(mySubSystem);
 
 		printClassAd();