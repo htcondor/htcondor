115301629c802a22199a66aa6a41e7291fcefd77
Bill Taylor <bt@cs.wisc.edu>
2008-06-23 16:54:27 -0500

    Add handling of the large test repository into the nightly builds and tests. First test going here is the almost ready VM Universe smoke test

--- nmi_tools/nightly.pl
+++ nmi_tools/nightly.pl
@@ -24,6 +24,7 @@ $GIT_SRC_ROOT="/space/git/CONDOR_SRC.git";
 $GIT_EXT_ROOT="/space/git/CONDOR_EXT.git";
 $GIT_DOC_ROOT="/space/git/CONDOR_DOC.git";
 $GIT_TSTCNF_ROOT="/space/git/CONDOR_TEST_CNFDTL.git";
+$GIT_TSTLRG_ROOT="/space/git/CONDOR_TEST_LRG.git";
 
 
 # Open log file and setup autoflushing
@@ -52,7 +53,7 @@ chdir $home || die "Can't chdir($home): $!\n";
 # We need to make sure the clones are current, since they aren't
 # rsync'd constantly anymore
 if (defined $opt_git) {
-    @roots = ($GIT_SRC_ROOT, $GIT_DOC_ROOT, $GIT_EXT_ROOT, $GIT_TSTCNF_ROOT);
+    @roots = ($GIT_SRC_ROOT, $GIT_DOC_ROOT, $GIT_EXT_ROOT, $GIT_TSTCNF_ROOT, $GIT_TSTLRG_ROOT);
     foreach $root (@roots) {
 	open(OUT, "$GIT --git-dir=$root fetch 2>&1|")
 	    || die "Can't fetch $root: $!\n";
