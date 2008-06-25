1b94f4441360c099ffa1a1204052b8abdb510b71
Greg Thain <gthain@cs.wisc.edu>
2008-06-25 10:31:11 -0500

    Replace strnicmp with strncasecomp, as the latter is available on unix

--- src/condor_tools/tool.C
+++ src/condor_tools/tool.C
@@ -1710,7 +1710,7 @@ printAdToFile(ClassAd *ad, char* filename) {
 
 int strncmp_auto(const char *s1, const char *s2)
 {
-    return strnicmp(s1, s2, strlen(s2));
+    return strncasecmp(s1, s2, strlen(s2));
 }
 
 extern "C" int SetSyscalls() {return 0;}
