0e30f2ad68f6d2575420fd4cd8e1c109455896d6
Matthew Farrellee <matt@redhat.com>
2008-11-19 08:56:06 -0600

    Report Start and Status failure error messages in VMGahpLog, avoids needing to look at the StarterLog, where the messages are also logged

--- src/condor_vm-gahp/xen_type.cpp
+++ src/condor_vm-gahp/xen_type.cpp
@@ -152,6 +152,7 @@ XenType::Start()
 		// TODO Should we grab more than just the first line?
 		cmd_out.rewind();
 		m_result_msg = cmd_out.next();
+		vmprintf(D_ALWAYS, "ERROR: %s\n", m_result_msg.GetCStr());
 		return false;
 	}
 
@@ -507,6 +508,7 @@ XenType::Status()
 		// TODO Should we grab more than just the first line?
 		cmd_out.rewind();
 		m_result_msg = cmd_out.next();
+		vmprintf(D_ALWAYS, "ERROR: %s\n", m_result_msg.GetCStr());
 		return false;
 	}
 
