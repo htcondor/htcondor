4fd55a7985698ba7fcb4f21401e7539d7c750989
Peter Keller <psilord@cs.wisc.edu>
2008-11-25 10:24:17 -0600

    Changed all of the splice tests to use scheduler universe jobs instead of vanilla universe jobs to try and speed up the time they take to run. At this time, dagman has a hardcoded 5 second timeout on when it'll try and submit the next batch of ready jobs, so if a dag has long chains in it, the time to execute it can go up noticably.

--- src/condor_tests/job_dagman_splice-N-subdir1/b/b_submit.cmd
+++ src/condor_tests/job_dagman_splice-N-subdir1/b/b_submit.cmd
@@ -1,6 +1,6 @@
 executable   = /bin/echo
 arguments    = OK
-universe     = vanilla
+universe     = scheduler
 output       = job_dagman_splice-N-subdir1/b/$(job).out
 error        = job_dagman_splice-N-subdir1/b/$(job).err
 log          = job_dagman_splice-N-subdir1/b/b_submit.log
