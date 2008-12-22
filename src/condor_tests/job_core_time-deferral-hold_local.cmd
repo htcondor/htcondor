62668c087f86d27f686c5d41fcd4a1f46d3ed2bb
Bill Taylor <bt@cs.wisc.edu>
2008-12-22 09:11:39 -0600

    These files quit haveing the deferral tests from sharing a log so they can run in parallel.

---
+++ src/condor_tests/job_core_time-deferral-hold_local.cmd
@@ -0,0 +1,9 @@
+Executable   = ./x_time.pl
+Notification = NEVER
+DeferralPrep = 600
+Universe	   = local
+Log		   = job_core_time-deferral-hold_local.log
+Output	   = job_core_time-deferral-hold_local.out
+Error		   = job_core_time-deferral-hold_local.err
+DeferralTime = 1229727371
+Queue
---
+++ src/condor_tests/job_core_time-deferral-hold_vanilla.cmd
@@ -0,0 +1,9 @@
+Executable   = ./x_time.pl
+Notification = NEVER
+DeferralPrep = 600
+Universe	   = vanilla
+Log		   = job_core_time-deferral-hold_vanilla.log
+Output	   = job_core_time-deferral-hold_vanilla.out
+Error		   = job_core_time-deferral-hold_vanilla.err
+DeferralTime = 1229727167
+Queue
