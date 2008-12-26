cb88c42675409feb7046c5f7f9044066d7aa734c
Bill Taylor <bt@cs.wisc.edu>
2008-12-26 12:06:59 -0600

    Remove precautionary removal of the symlink into the saveme dir and the creation of that same symlink.

--- src/condor_scripts/CondorPersonal.pm
+++ src/condor_scripts/CondorPersonal.pm
@@ -1646,16 +1646,16 @@ sub SaveMeSetup
 	# make a symbolic link for personal condor module to use
 	# if we get pid recycling, blow the symbolic link 
 	# This might not be a symbolic link, so use -r to be sure
-	$res = CondorTest::verbose_system("rm -fr $mypid");
-	if($res != 0) {
-		print "SaveMeSetup: Could not remove prior pid directory\n";
-		return(0);
-	}
-	$res = CondorTest::verbose_system("ln -s $mypiddir $mypid");
-	if($res != 0) {
-		print "SaveMeSetup: Could not link to pid dir in  \"saveme\" directory\n";
-		return(0);
-	}
+	#$res = CondorTest::verbose_system("rm -fr $mypid");
+	#if($res != 0) {
+		#print "SaveMeSetup: Could not remove prior pid directory\n";
+		#return(0);
+	#}
+	#$res = CondorTest::verbose_system("ln -s $mypiddir $mypid");
+	#if($res != 0) {
+		#print "SaveMeSetup: Could not link to pid dir in  \"saveme\" directory\n";
+		#return(0);
+	#}
 	return($mypid);
 }
 
