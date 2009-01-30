4149ceae5800da3332e245166ee60f873a89975b
Jaime Frey <jfrey@cs.wisc.edu>
2009-01-29 18:25:04 -0600

    Use some version of openssl on windows as unix.

--- msconfig/set_vars.bat
+++ msconfig/set_vars.bat
@@ -61,7 +61,7 @@ REM Specify which versions of the externals we're using. To add a
 REM new external, just add its version here, and add that to the 
 REM EXTERNALS_NEEDED variable defined below.
 set EXT_GSOAP_VERSION=gsoap-2.7.6c-p2
-set EXT_OPENSSL_VERSION=openssl-0.9.8
+set EXT_OPENSSL_VERSION=openssl-0.9.8h
 set EXT_POSTGRESQL_VERSION=postgresql-8.0.2
 set EXT_KERBEROS_VERSION=krb5-1.4.3-p0
 set EXT_PCRE_VERSION=pcre-7.6
