17b5ead375d0fb4a1a22b0b585642628394a4683
Bill Taylor <bt@cs.wisc.edu>
2009-02-10 16:12:47 -0600

    Make sure the createlang works and that we know when it does not.

--- src/condor_tests/x_quill_pgsqlinstall.pl
+++ src/condor_tests/x_quill_pgsqlinstall.pl
@@ -282,10 +282,13 @@ if($docreatedb != 0) {
 }
 system("date");
 
-$docreatelang = system("$pgsql_dir/bin/createlang plpgsql");
-if($docreatedb != 0) {
-	die "Failed to create test db\n";
+$docreatelang = system("$pgsql_dir/bin/createlang plpgsql test --port $startpostmasterport");
+if($docreatelang != 0) {
+	die "Failed to createlang plpgsql\n";
+} else {
+	print "$pgsql_dir/bin/createlang plpgsql test --port $startpostmasterport Worked!!\n";
 }
+
 system("date");
 
 
