957af1bbaf95730b53d5d36c452d985c9d132b71
Jaime Frey <jfrey@cs.wisc.edu>
2009-02-11 12:47:16 -0600

    Fix debug messages in job_core_time-deferral-hold test

--- src/condor_tests/job_core_time-deferral-hold.pl
+++ src/condor_tests/job_core_time-deferral-hold.pl
@@ -159,10 +159,11 @@ $timed = sub {
 	$job = $info{"job"};
 
 	if ( !defined( $info{"cluster"} ) || !defined( $info{"job"} ) ) {
-		CondorTest::debug("Haven't seen submit event yet at timeout!\n,1");
-		CondorTest::debug("stat and contents of user log ".$info{"log"}.":\n",1);
-		system( "stat " . $info{"log"} );
-		system( "cat " . $info{"log"} );
+		my $ulog = $info{"log"};
+		CondorTest::debug("Haven't seen submit event yet at timeout!\n",1);
+		CondorTest::debug("stat and contents of user log $ulog:\n",1);
+		CondorTest::debug(`stat $ulog`,1);
+		CondorTest::debug(`cat $ulog`,1);
 	}
 
 	##
