htcondor/htcondor

commit 7f193be99c3c75bafbee2b126ddc5f6b1787d62f

Revert "Get rid of "caching" of sandbox ownership in CondorPrivSepHelper" Without this state in CondorPrivSepHelper, the starter in priv-sep mode was trying to chown the sandbox to condor multiple times on job cleanup. The root switchboard fails when trying to chown from user to condor ownership if the files already have condor ownership.  Fixing that in the switchboard complicates the switchboard code unnecessarily. This was not fatal to the job, but it resulted in ERROR messages in the starter log.
This reverts commit 073f32f9dec16cad15d6bbc2732113089c3a706e.

src/condor_starter.V6.1/condor_privsep_helper.h
@@ -100,6 +100,10 @@ private:
 	// the sandbox directory name
 	//
 	char* m_sandbox_path;
+
+	// true when the sandbox is owned by the user, not condor
+	//
+	bool m_sandbox_owned_by_user;
 };
 
 #endif

src/condor_starter.V6.1/condor_privsep_helper.UNIX.cpp
@@ -31,7 +31,8 @@ CondorPrivSepHelper::CondorPrivSepHelper() :
 	m_user_initialized(false),
 	m_sandbox_initialized(false),
 	m_uid(0),
-	m_sandbox_path(NULL)
+	m_sandbox_path(NULL),
+	m_sandbox_owned_by_user(false)
 {
 	ASSERT(!s_instantiated);
 	s_instantiated = true;
@@ -86,6 +87,7 @@ CondorPrivSepHelper::initialize_sandbox(const char* path)
 
 	m_sandbox_path = strdup(path);
 	ASSERT(m_sandbox_path);
+	m_sandbox_owned_by_user = false;
 
 	m_sandbox_initialized = true;
 }
@@ -102,10 +104,19 @@ CondorPrivSepHelper::chown_sandbox_to_user()
 {
 	ASSERT(m_sandbox_initialized);
 
+	if (m_sandbox_owned_by_user) {
+		dprintf(D_FULLDEBUG,
+		        "CondorPrivSepHelper::chown_sandbox_to_user: "
+		        	"sandbox already user-owned\n");
+		return;
+	}
+
 	dprintf(D_FULLDEBUG, "changing sandbox ownership to the user\n");
 	if (!privsep_chown_dir(m_uid, get_condor_uid(), m_sandbox_path)) {
 		EXCEPT("error changing sandbox ownership to the user");
 	}
+
+	m_sandbox_owned_by_user = true;
 }
 
 void
@@ -113,10 +124,19 @@ CondorPrivSepHelper::chown_sandbox_to_condor()
 {
 	ASSERT(m_sandbox_initialized);
 
+	if (!m_sandbox_owned_by_user) {
+		dprintf(D_FULLDEBUG,
+		        "CondorPrivSepHelper::chown_sandbox_to_condor: "
+		        	"sandbox already condor-owned\n");
+		return;
+	}
+
 	dprintf(D_FULLDEBUG, "changing sandbox ownership to condor\n");
 	if (!privsep_chown_dir(get_condor_uid(), m_uid, m_sandbox_path)) {
 		EXCEPT("error changing sandbox ownership to condor");
 	}
+
+	m_sandbox_owned_by_user = false;
 }
 
 int

commit 3b5a3e79740d54aa235eca5f35498892c2d9ea3c

Fix problem with privsep and vmgahp (internal to 7.2.2). My patch 7f193be99c introduced a problem.  When checkpointing vm jobs, the starter had the wrong state about the ownership of the sandbox.

src/condor_starter.V6.1/baseStarter.cpp
@@ -745,11 +745,15 @@ CStarter::jobEnvironmentReady( void )
 		// file(s)
 		//
 	if (m_privsep_helper != NULL) {
-		int univ;
+		int univ = -1;
 		if (!jic->jobClassAd()->LookupInteger(ATTR_JOB_UNIVERSE, univ) ||
 		    (univ != CONDOR_UNIVERSE_VM))
 		{
 			m_privsep_helper->chown_sandbox_to_user();
+		}
+		else if( univ == CONDOR_UNIVERSE_VM ) {
+				// the vmgahp will chown the sandbox to the user
+			m_privsep_helper->set_sandbox_owned_by_user();
 		}
 	}
 

src/condor_starter.V6.1/condor_privsep_helper.h
@@ -59,6 +59,9 @@ public:
 	//
 	void chown_sandbox_to_user();
 
+	// change our state to "sandbox is owned by user"
+	void set_sandbox_owned_by_user() { m_sandbox_owned_by_user=true; }
+
 	// change ownership of the sandbox to condor
 	//
 	void chown_sandbox_to_condor();

src/condor_starter.V6.1/glexec_privsep_helper.h
@@ -40,6 +40,9 @@ public:
 	//
 	void chown_sandbox_to_user();
 
+	// change our state to "sandbox is owned by user"
+	void set_sandbox_owned_by_user() { m_sandbox_owned_by_user=true; }
+
 	// change ownership of the sandbox to condor
 	//
 	void chown_sandbox_to_condor();

src/condor_starter.V6.1/privsep_helper.h
@@ -36,6 +36,9 @@ public:
 	//
 	virtual void chown_sandbox_to_condor() = 0;
 
+	// change our state to "sandbox is owned by user"
+	virtual void set_sandbox_owned_by_user() = 0;
+
 	// launch the job as the user
 	//
 	virtual int create_process(const char* path,