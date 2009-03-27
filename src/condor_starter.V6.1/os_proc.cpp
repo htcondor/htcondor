636229b91c9a51782e0dfd367011705e2cf79750
Ben Burnett <burnett@cs.wisc.edu>
2009-03-27 16:29:06 -0500

    Ticket #353: Changed all =condor_close()= calls to =daemonCore->Close_Pipe()= calls, since it does "the right thing".

--- src/condor_starter.V6.1/os_proc.cpp
+++ src/condor_starter.V6.1/os_proc.cpp
@@ -40,7 +40,6 @@
 #include "perm.h"
 #include "profile.WINDOWS.h"
 #endif
-#include "condor_close.h"
 
 extern CStarter *Starter;
 
@@ -281,7 +280,7 @@ OsProc::StartJob(FamilyInfo* family_info)
 			/* only close ones that had been opened correctly */
 		for( int fdindex=0; fdindex<=2; fdindex++ ) {
 			if( fds[fdindex] >= 0 ) {
-				condor_close(fds[fdindex]);
+				daemonCore->Close_Pipe(fds[fdindex]);
 			}
 		}
 		dprintf(D_ALWAYS, "Failed to open some/all of the std files...\n");
@@ -470,7 +469,7 @@ OsProc::StartJob(FamilyInfo* family_info)
 	// reason.
 	for( int fdindex=0; fdindex<=2; fdindex++ ) {
 		if( fds[fdindex] >= 0 ) {
-			condor_close(fds[fdindex]);
+			daemonCore->Close_Pipe(fds[fdindex]);
 		}
 	}
 
