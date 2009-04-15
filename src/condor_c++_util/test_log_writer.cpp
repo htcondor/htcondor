e12b19066e646b89b2d461ae3dcbcaff4205f71f
Nick LeRoy <nleroy@cs.wisc.edu>
2009-04-15 13:18:06 -0500

    Fixed random event generation in the log writer test

--- src/condor_c++_util/test_log_writer.cpp
+++ src/condor_c++_util/test_log_writer.cpp
@@ -129,6 +129,7 @@ public:
 
 	ULogEvent *GenEvent( void );
 	ULogEvent *GenEvent( ULogEventNumber );
+	ULogEvent *GenEventRandom( void );
 
 	bool WriteEvent( UserLog &log );
 
@@ -697,15 +698,30 @@ WriteEvents( Options &opts, UserLogTest &writer, int &events, int &sequence )
 			break;
 		}
 
-		event.GenEvent( );
+		event.GenEventExecute( );
 		if ( event.WriteEvent( writer ) ) {
 			error = true;
 		}
 		else {
 			events++;
-			event.NextProc( );
 		}
 		event.Reset( );
+
+		if ( !error && ( get_random_float() < opts.m_randomProb )  ) {
+			event.GenEventRandom( );
+			if ( event.WriteEvent( writer ) ) {
+				printf( "Error writing event type %d\n",
+						(int) event.GetEventNumber() );
+				error = true;
+			}
+			else {
+				events++;
+			}
+			event.Reset( );
+		}
+		if ( !error ) {
+			event.NextProc( );
+		}
 
 		if ( ( opts.m_maxGlobalSize > 0 ) && 
 			 ( writer.getGlobalLogSize() > opts.m_maxGlobalSize ) ) {
@@ -801,10 +817,17 @@ EventInfo::GenEvent( void )
 	double	randval = get_random_float( );
 
 	// Special case: execute event
-	if ( randval > m_opts.m_randomProb ) {
+	if ( randval < m_opts.m_randomProb ) {
 		return GenEventExecute( );
 	}
+	else {
+		return GenEventRandom( );
+	}
+}
 
+ULogEvent *
+EventInfo::GenEventRandom( void )
+{
 	ULogEvent	*eventp = NULL;
 	while( NULL == eventp ) {
 		unsigned	rand_event = randint(29);
