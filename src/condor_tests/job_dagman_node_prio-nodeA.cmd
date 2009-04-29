a8e9fd43f60691c1810fe31b5eaf1400cf038376
Kent Wenger <wenger@cs.wisc.edu>
2009-04-29 13:53:16 -0500

    Added test for DAGMan node priorities -- makes sure nodes run in the correct order.

---
+++ src/condor_tests/job_dagman_node_prio-nodeA.cmd
@@ -0,0 +1,8 @@
+executable   = ./job_dagman_node_prio-nodeA.csh
+arguments    = $(nodename)
+universe     = scheduler
+output       = $(job).out
+error        = $(job).err
+log          = job_dagman_node_prio.log
+Notification = NEVER
+queue
