ffdb901af7fad9811625931ad2f42528909ace9d
Kent Wenger <wenger@cs.wisc.edu>
2009-05-20 16:25:25 -0500

    Added job_dagman_log_path test to make sure that the log reading code works right with "tricky" log file paths (see gittrac #499).

---
+++ src/condor_tests/job_dagman_log_path-nodeB.cmd
@@ -0,0 +1,8 @@
+executable   = ./job_dagman_log_path-node.pl
+universe     = scheduler
+output       = job_dagman_log_path.out
+error        = job_dagman_log_path.err
+# Note funky path to log file.
+log          = ../condor_tests/job_dagman_log_path.log
+Notification = NEVER
+queue
