htcondor/htcondor

commit 3924ee46d836c7742ef3a7b07396959fc71cdc6c

Fixed shutdown race condition in master with HA daemons
HA daemons in the master would never get notified that they were
shutting down, because they did not have a pid yet. The result was
during a shutdown, an HA daemon could acquire its lock and start
running. Once running it would prevent the master from completely
shutting down (a good thing). This was further problematic because the
master would ignore the signal it had received for shutdown. A
workaround was to send a different signal, e.g. if first sent QUIT
second time send TERM.

src/condor_master.V6/masterDaemon.cpp
@@ -509,6 +509,13 @@ int daemon::RealStart( )
 			// Already running
 		return TRUE;
 	}
+	if( stop_state != NONE ) {
+			// Currently trying to shutdown, this might happen for an
+			// HA daemon who acquires a lock just before shutdown.
+		dprintf( D_FULLDEBUG, "::RealStart; %s stop_state=%d, ignoring\n",
+				 name_in_config_file, stop_state );
+		return FALSE;
+	}
 
 	shortname = condor_basename( process_name );
 
@@ -786,15 +793,17 @@ daemon::Stop( bool never_forward )
 		daemonCore->Cancel_Timer( start_tid );
 		start_tid = -1;
 	}
-	if( !pid ) {
-			// We're not running, just return.
-		return;
-	}
 	if( stop_state == GRACEFUL ) {
 			// We've already been here, just return.
 		return;
 	}
 	stop_state = GRACEFUL;
+		// Test for pid after setting state so HA daemons that aren't
+		// running get notified that they are shutting down
+	if( !pid ) {
+			// We're not running, just return.
+		return;
+	}
 
 	Kill( SIGTERM );
 
@@ -822,15 +831,17 @@ daemon::StopPeaceful()
 		daemonCore->Cancel_Timer( start_tid );
 		start_tid = -1;
 	}
-	if( !pid ) {
-			// We're not running, just return.
-		return;
-	}
 	if( stop_state == PEACEFUL ) {
 			// We've already been here, just return.
 		return;
 	}
 	stop_state = PEACEFUL;
+		// Test for pid after setting state so HA daemons that aren't
+		// running get notified that they are shutting down
+	if( !pid ) {
+			// We're not running, just return.
+		return;
+	}
 
 	// Ideally, we would somehow tell the daemon to die peacefully
 	// (only currently applies to startd).  However, we only have
@@ -875,15 +886,17 @@ daemon::StopFast( bool never_forward )
 		daemonCore->Cancel_Timer( start_tid );
 		start_tid = -1;
 	}
-	if( !pid ) {
-			// We're not running, just return.
-		return;
-	}
 	if( stop_state == FAST ) {
 			// We've already been here, just return.
 		return;
 	}
 	stop_state = FAST;
+		// Test for pid after setting state so HA daemons that aren't
+		// running get notified that they are shutting down
+	if( !pid ) {
+			// We're not running, just return.
+		return;
+	}
 
 	if( stop_fast_tid != -1 ) {
 		dprintf( D_ALWAYS, 
@@ -907,15 +920,17 @@ daemon::HardKill()
 			// Never want to stop master.
 		return;
 	}
-	if( !pid ) {
-			// We're not running, just return.
-		return;
-	}
 	if( stop_state == KILL ) {
 			// We've already been here, just return.
 		return;
 	}
 	stop_state = KILL;
+		// Test for pid after setting state so HA daemons that aren't
+		// running get notified that they are shutting down
+	if( !pid ) {
+			// We're not running, just return.
+		return;
+	}
 
 	if( hard_kill_tid != -1 ) {
 		dprintf( D_ALWAYS, 
@@ -1654,7 +1669,10 @@ Daemons::StopAllDaemons()
 	daemons.SetAllReaper();
 	int running = 0;
 	for( int i=0; i < no_daemons; i++ ) {
-		if( daemon_ptr[i]->pid && daemon_ptr[i]->runs_here ) {
+			// Need to stop HA daemons from trying to start during
+			// shutdown
+		if( ( daemon_ptr[i]->pid || daemon_ptr[i]->IsHA() ) &&
+			daemon_ptr[i]->runs_here ) {
 			daemon_ptr[i]->Stop();
 			running++;
 		}
@@ -1671,7 +1689,10 @@ Daemons::StopFastAllDaemons()
 	daemons.SetAllReaper();
 	int running = 0;
 	for( int i=0; i < no_daemons; i++ ) {
-		if( daemon_ptr[i]->pid && daemon_ptr[i]->runs_here ) {
+			// Need to stop HA daemons from trying to start during
+			// shutdown
+		if( ( daemon_ptr[i]->pid || daemon_ptr[i]->IsHA() ) &&
+			daemon_ptr[i]->runs_here ) {
 			daemon_ptr[i]->StopFast();
 			running++;
 		}
@@ -1687,7 +1708,10 @@ Daemons::StopPeacefulAllDaemons()
 	daemons.SetAllReaper();
 	int running = 0;
 	for( int i=0; i < no_daemons; i++ ) {
-		if( daemon_ptr[i]->pid && daemon_ptr[i]->runs_here ) {
+			// Need to stop HA daemons from trying to start during
+			// shutdown
+		if( ( daemon_ptr[i]->pid || daemon_ptr[i]->IsHA() ) &&
+			daemon_ptr[i]->runs_here ) {
 			daemon_ptr[i]->StopPeaceful();
 			running++;
 		}
@@ -1704,7 +1728,10 @@ Daemons::HardKillAllDaemons()
 	daemons.SetAllReaper();
 	int running = 0;
 	for( int i=0; i < no_daemons; i++ ) {
-		if( daemon_ptr[i]->pid && daemon_ptr[i]->runs_here ) {
+			// Need to stop HA daemons from trying to start during
+			// shutdown
+		if( ( daemon_ptr[i]->pid || daemon_ptr[i]->IsHA() ) &&
+			daemon_ptr[i]->runs_here ) {
 			daemon_ptr[i]->HardKill();
 			running++;
 		}

src/condor_master.V6/master.h
@@ -82,6 +82,8 @@ public:
 	int		SetupController( void );
 	int		RegisterControllee( class daemon * );
 
+	bool	IsHA( void ) { return is_ha; };
+
 private:
 
 	int		runs_on_this_host();

commit 89697cc17f1f9c21ca1f944b5e75a9ce0ad45915

Fixed master to let HAD start daemons, broken by 3924ee4
Issue was 3924ee4 prevented starting daemons that had been
stopped. Well, HAD works by stopping daemons immediately and then
starting them again. The issue did not prevent _off then _on from
working, only HAD.

src/condor_master.V6/masterDaemon.cpp
@@ -509,7 +509,13 @@ int daemon::RealStart( )
 			// Already running
 		return TRUE;
 	}
-	if( stop_state != NONE ) {
+		// Check for a controller. If one exists, e.g. HAD, it is
+		// likely this daemon has stop_state = FAST, but we want to
+		// let it start anyway. The stop_state != NONE check is to
+		// avoid letting the master start HA daemons it is
+		// maintaining. A controller indicates something else is
+		// maintaining the daemon.
+	if( !controller && stop_state != NONE ) {
 			// Currently trying to shutdown, this might happen for an
 			// HA daemon who acquires a lock just before shutdown.
 		dprintf( D_FULLDEBUG, "::RealStart; %s stop_state=%d, ignoring\n",
@@ -748,6 +754,10 @@ int daemon::RealStart( )
 
 		// Since we just started it, we know it's not a new executable. 
 	newExec = FALSE;
+
+		// Be sure to reset the stop_state, if the daemon is HAD
+		// managed it will have been set to FAST on startup
+	stop_state = NONE;
 
 		// If starting the collector, give it a few seconds to get
 		// going before starting other daemons or talking to ti