e995aa2d53c3c1dfbbab7ce9acbc5ae7f402acc0
Dan Bradley <dan@hep.wisc.edu>
2009-06-12 15:18:37 -0500

    Fixed version string so that pre_all can parse it again.

--- src/condor_c++_util/condor_version.cpp
+++ src/condor_c++_util/condor_version.cpp
@@ -67,8 +67,7 @@
    will EXCEPT at startup time.  
 */
 
-static char* CondorVersionString =
-	"$CondorVersion: 7.2.4 " __DATE__ BUILDIDSTR " $";
+static char* CondorVersionString = "$CondorVersion: 7.2.4 " __DATE__ BUILDIDSTR " $";
 
 /* Here is the platform string.  You don't need to edit this */
 static char* CondorPlatformString = "$CondorPlatform: " xstr(PLATFORM) " $";
