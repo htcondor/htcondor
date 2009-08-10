58bcdacba38746e3dedcfea3657e344293bf2f1c
Jaime Frey <jfrey@cs.wisc.edu>
2009-08-10 11:24:59 -0500

    Bump version number of gahp_server.

--- src/gt2_gahp/gahp_server.c
+++ src/gt2_gahp/gahp_server.c
@@ -127,7 +127,7 @@ ptr_ref_count * current_cred = NULL;
    to be escaped or the gahp server gets confused. :(
    !!! BEWARE !!!
 */ 
-static char *VersionString ="$GahpVersion: 1.0.16 " __DATE__ " UW\\ Gahp $";
+static char *VersionString ="$GahpVersion: 1.1.0 " __DATE__ " UW\\ Gahp $";
 
 volatile int ResultsPending;
 volatile int AsyncResults;
