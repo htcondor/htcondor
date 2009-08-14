684c99028cbd15ee162ad2673cc3d014fd9092eb
Kent Wenger <wenger@cs.wisc.edu>
2009-08-14 10:01:17 -0500

    Added the job_dagman_usedagdir and job_dagman_node_dir tests.

---
+++ src/condor_tests/job_dagman_node_dir-subdir/job_dagman_node_dir-pre.pl
@@ -0,0 +1,8 @@
+#! /usr/bin/env perl
+
+$file = "job_dagman_node_dir-" . $ARGV[0] . "-pre.out";
+open(OUT, ">$file") or die "Can't open $file\n";
+
+print OUT "$ARGV[0] pre script\n";
+
+close(OUT);
