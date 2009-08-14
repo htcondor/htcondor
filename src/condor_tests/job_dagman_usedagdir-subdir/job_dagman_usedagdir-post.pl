953c4110fc8e1a8e450b748d3bf4f9e415e2c91b
Kent Wenger <wenger@cs.wisc.edu>
2009-08-14 16:17:29 -0500

    Made improvements to the job_dagman_usedagdir test -- post scripts now check $RESULT.

--- src/condor_tests/job_dagman_usedagdir-subdir/job_dagman_usedagdir-post.pl
+++ src/condor_tests/job_dagman_usedagdir-subdir/job_dagman_usedagdir-post.pl
@@ -3,6 +3,8 @@
 $file = "job_dagman_usedagdir-" . $ARGV[0] . "-post.out";
 open(OUT, ">$file") or die "Can't open $file\n";
 
-print OUT "$ARGV[0] post script\n";
+print OUT "$ARGV[0] post script ($ARGV[1])\n";
 
 close(OUT);
+
+exit($ARGV[1]);
