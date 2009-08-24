b403f112e4f4e27c1a0b0a427edb23b04a24caa8
Jaime Frey <jfrey@cs.wisc.edu>
2009-08-24 10:11:29 -0500

    Fix windows build script for hadoop external.

--- externals/bundles/hadoop/0.20.0-p2/build_hadoop-0.20.0-p2.bat
+++ externals/bundles/hadoop/0.20.0-p2/build_hadoop-0.20.0-p2.bat
@@ -3,7 +3,7 @@ rm -rf ..\..\install\hdfs
 mkdir ..\..\install\hdfs
 mkdir ..\..\install\hdfs\conf
 
-cp -r %PACKAGE_NAME%\webapps ..\..\install\hdfs
-cp -r %PACKAGE_NAME%\lib ..\..\install\hdfs
-cp %PACKAGE_NAME%\hadoop-0.20.0-p1-core.jar ..\..\install\hdfs\lib
-cp %PACKAGE_NAME%\src\hdfs\hdfs-default.xml ..\..\install\hdfs\conf
+cp -r %PACKAGE_SRC_NAME%\webapps ..\..\install\hdfs
+cp -r %PACKAGE_SRC_NAME%\lib ..\..\install\hdfs
+cp %PACKAGE_SRC_NAME%\hadoop-0.20.0-p1-core.jar ..\..\install\hdfs\lib
+cp %PACKAGE_SRC_NAME%\src\hdfs\hdfs-default.xml ..\..\install\hdfs\conf
