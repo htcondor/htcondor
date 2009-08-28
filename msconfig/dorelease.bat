c5b1c1c5bbc17734d2c90036ad5e0f1ca9a994e9
Ziliang Guo <ziliang@cs.wisc.edu>
2009-08-28 12:19:47 -0500

    Readd the correct file copying for hadoop JAR files in the dorelease script in preparation for packaging.

--- msconfig/dorelease.bat
+++ msconfig/dorelease.bat
@@ -115,8 +115,8 @@ for %%f in (master startd quill dbmsd had credd schedd collector negotiator shad
     copy ..\Release\condor_%%f.pdb %1\bin
 )
 
-REM echo. & echo Copying hadoop files...
-REM xcopy ..\externals\bundle\hadoop %1\hdfs /e
+echo. & echo Copying hadoop files...
+xcopy ..\externals\install\hdfs %1\hdfs /E
 
 echo. & echo Making some aliases...
 pushd %1\bin
