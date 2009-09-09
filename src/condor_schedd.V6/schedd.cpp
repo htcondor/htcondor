htcondor/htcondor

commit c491238ea627ff601be573c5c669c1866a8660a3

Sped up startup of newly submitted jobs in some cases. One problem was that because of various timers delaying things, the schedd would sometimes send a reschedule command to the negotiator before it published an ad showing that it had idle jobs.
Another problem was that on startup, the runnable job array would
sometimes not be built the first time it was needed, because it
thought it had just built one.

src/condor_schedd.V6/schedd.cpp
@@ -372,6 +372,8 @@ Scheduler::Scheduler() :
 	LocalUnivExecuteDir = NULL;
 	ReservedSwap = 0;
 	SwapSpace = 0;
+	m_need_reschedule = false;
+	m_send_reschedule_timer = -1;
 
 		//
 		// ClassAd attribute for evaluating whether to start
@@ -440,7 +442,6 @@ Scheduler::Scheduler() :
 
 	CronMgr = NULL;
 
-	last_reschedule_request = 0;
 	jobThrottleNextJobDelay = 0;
 #ifdef WANT_QUILL
 	prevLHF = 0;
@@ -614,6 +615,7 @@ Scheduler::timeout()
 				 "Preempting %d jobs due to MAX_JOBS_RUNNING change\n",
 				 (real_jobs - MaxJobsRunning) );
 		preempt( real_jobs - MaxJobsRunning );
+		m_need_reschedule = false;
 	}
 
 	if( LocalUniverseJobsIdle > 0 || SchedUniverseJobsIdle > 0 ) {
@@ -634,6 +636,10 @@ Scheduler::timeout()
 	scheduler.rescheduleContactQueue();
 
 	SchedDInterval.setFinishTimeNow();
+
+	if( m_need_reschedule ) {
+		sendReschedule();
+	}
 
 	/* Reset our timer */
 	time_to_next_run = SchedDInterval.getTimeToNextRun();
@@ -4440,7 +4446,7 @@ Scheduler::actOnJobs(int, Stream* s)
 		for( i=0; i<num_matches; i++ ) {
 			WriteReleaseToUserLog( jobs[i] );		
 		}
-		sendReschedule();
+		needReschedule();
 		break;
 
 	case JA_REMOVE_X_JOBS:
@@ -4628,7 +4634,13 @@ Scheduler::negotiate(int command, Stream* s)
 	// we've been waiting for.  If only Todd can say the same about
 	// his life in general.  ;)
 	NegotiationRequestTime = 0;
-	last_reschedule_request = 0; // Our request was fulfilled.
+	m_need_reschedule = false;
+	if( m_send_reschedule_timer != -1 ) {
+		daemonCore->Cancel_Timer( m_send_reschedule_timer );
+	}
+
+		// set stop/start times on the negotiate timeslice object
+	ScopedTimesliceStopwatch negotiate_stopwatch( &m_negotiate_timeslice );
 
 	// BIOTECH
 	bool	biotech = false;
@@ -10139,6 +10151,17 @@ Scheduler::Init()
 		WalkJobQueue( (int(*)(ClassAd *))::updateSchedDInterval );
 	}
 
+		// Delay sending negotiation request if we are spending more
+		// than this amount of time negotiating.  This is currently an
+		// intentionally undocumented knob, because it's behavior is
+		// not at all well defined, given that the negotiator can
+		// initiate negotiation at any time.  We also hope to
+		// upgrade the negotiation protocol to avoid blocking the
+		// schedd, which should make this all unnecessary.
+	m_negotiate_timeslice.setTimeslice( param_double("SCHEDD_NEGOTIATE_TIMESLICE",0.1) );
+		// never delay negotiation request longer than this amount of time
+	m_negotiate_timeslice.setMaxInterval( SchedDInterval.getMaxInterval() );
+
 	// default every 24 hours
 	QueueCleanInterval = param_integer( "QUEUE_CLEAN_INTERVAL",24*60*60 );
 
@@ -10992,31 +11015,52 @@ Scheduler::reschedule_negotiator(int, Stream *s)
 		return 0;
 	}
 
-		// Update the central manager now.
-		// This also starts local jobs etc.
-	timeout();
-
-	dprintf( D_ALWAYS, "Called reschedule_negotiator()\n" );
-
-	sendReschedule();
+	needReschedule();
 
 	return 0;
 }
 
+void
+Scheduler::needReschedule()
+{
+	m_need_reschedule = true;
+
+		// Update the central manager and request a reschedule.  We
+		// don't call sendReschedule() directly below, because
+		// timeout() has internal logic to avoid doing its work too
+		// frequently, and we want to send the reschedule after
+		// updating our ad in the collector, not before.
+	timeout();
+}
 
 void
-Scheduler::sendReschedule( bool checkRecent /* = true */ )
+Scheduler::sendReschedule()
 {
-		// MIN_RESCHEDULE_GAP: If we've sent a reschedule request in the
-		// last MIN_RESCHEDULE_GAP second
-	if( checkRecent) {
-		const int MIN_RESCHEDULE_GAP = 30;
-		time_t now = time(0);
-		if( (last_reschedule_request + MIN_RESCHEDULE_GAP) > now ) {
-			dprintf( D_FULLDEBUG, "Skipping RESCHEDULE as optimization: sent one %d seconds ago and still haven't heard from negotiatator.  Will not reschedule more frequently than every %d seconds\n", (int)(now - last_reschedule_request), MIN_RESCHEDULE_GAP);
-			return;
+	if( !m_negotiate_timeslice.isTimeToRun() ) {
+			// According to our negotiate timeslice object, we are
+			// spending too much of our time negotiating, so delay
+			// sending the reschedule command to the negotiator.  That
+			// _might_ help, but there is no guarantee, since the
+			// negotiator can decide to initiate negotiation at any
+			// time.
+
+		if( m_send_reschedule_timer == -1 ) {
+			m_send_reschedule_timer = daemonCore->Register_Timer(
+				m_negotiate_timeslice.getTimeToNextRun(),
+				(TimerHandlercpp)&Scheduler::sendReschedule,
+				"Scheduler::sendReschedule",
+				this);
 		}
+		dprintf( D_FULLDEBUG,
+				 "Delaying sending RESCHEDULE to negotiator for %d seconds.\n",
+				 m_negotiate_timeslice.getTimeToNextRun() );
+		return;
 	}
+
+	if( m_send_reschedule_timer != -1 ) {
+		daemonCore->Cancel_Timer( m_send_reschedule_timer );
+	}
+
 	dprintf( D_FULLDEBUG, "Sending RESCHEDULE command to negotiator(s)\n" );
 
 	Daemon negotiator(DT_NEGOTIATOR);
@@ -11039,8 +11083,6 @@ Scheduler::sendReschedule( bool checkRecent /* = true */ )
 			}
 		}
 	}
-
-	last_reschedule_request = time(0);
 }
 
 
@@ -12236,7 +12278,7 @@ releaseJob( int cluster, int proc, const char* reason,
 		}
 	}
 
-	scheduler.sendReschedule();
+	scheduler.needReschedule();
 
 	return result;
 }

src/condor_c++_util/condor_timeslice.h
@@ -43,7 +43,6 @@ The following functions can then be used to query the timer:
 
 */
 
-
 #ifndef _CONDOR_TIMESLICE_H_
 #define _CONDOR_TIMESLICE_H_
 
@@ -173,6 +172,24 @@ class Timeslice {
 	time_t m_next_start_time;  // utc second number when to run next time
 	bool m_never_ran_before;   // true if never ran before
 	bool m_expedite_next_run;  // true if should ignore default interval
+};
+
+// Use this class to set the start/stop times on a timeslice object
+// automatically when this object is created and when it goes out of scope.
+// Assumes that the timeslice object does not go out of scope before this
+// object.
+class ScopedTimesliceStopwatch {
+ public:
+	ScopedTimesliceStopwatch(Timeslice *timeslice):
+		m_timeslice( timeslice )
+	{
+		m_timeslice->setStartTimeNow();
+	}
+	~ScopedTimesliceStopwatch() {
+		m_timeslice->setFinishTimeNow();
+	}
+ private:
+	Timeslice *m_timeslice;
 };
 
 #endif

src/condor_schedd.V6/dedicated_scheduler.cpp
@@ -3502,7 +3502,7 @@ DedicatedScheduler::requestResources( void )
 			// to ask to negotiate for them...
 		displayResourceRequests();
 		publishRequestAd();
-		scheduler.sendReschedule(false);
+		scheduler.needReschedule();
 	} else {
 			// We just want to publish another add to let the
 			// negotiator know we're satisfied.

src/condor_schedd.V6/qmgmt.cpp
@@ -3669,6 +3669,9 @@ bool BuildPrioRecArray(bool no_match_found /*default false*/) {
 		return false;
 	}
 
+		// run without any delay the first time
+	PrioRecArrayTimeslice.setInitialInterval( 0 );
+
 	PrioRecArrayTimeslice.setMaxInterval( PrioRecRebuildMaxInterval );
 	if( no_match_found ) {
 		PrioRecArrayTimeslice.setTimeslice( PrioRecRebuildMaxTimeSliceWhenNoMatchFound );

src/condor_schedd.V6/scheduler.h
@@ -262,7 +262,12 @@ class Scheduler : public Service
 	int				reschedule_negotiator_timer() { return reschedule_negotiator(0, NULL); }
 	void			release_claim(int, Stream *);
 	AutoCluster		autocluster;
-	void			sendReschedule( bool checkRecent = true );
+		// send a reschedule command to the negotiatior unless we
+		// have recently sent one and not yet heard from the negotiator
+	void			sendReschedule();
+		// call this when state of job queue has changed in a way that
+		// requires a new round of negotiation
+	void            needReschedule();
 
 	// job managing
 	int				abort_job(int, Stream *);
@@ -547,8 +552,6 @@ private:
 	char*			AccountantName;
     char*			UidDomain;
 
-	time_t last_reschedule_request;
-
 	// connection variables
 	struct sockaddr_in	From;
 	int					Len; 
@@ -660,6 +663,10 @@ private:
 		// A bit that says wether or not we've sent email to the admin
 		// about a shadow not starting.
 	int sent_shadow_failure_email;
+
+	bool m_need_reschedule;
+	int m_send_reschedule_timer;
+	Timeslice m_negotiate_timeslice;
 
 	// some stuff about Quill that should go into the ad
 #ifdef WANT_QUILL

commit a9aeab797e4833a300dda84a19ffbd1f3680ff3c

Fixed a problem with the schedd's reschedule timer. This bug was introduced in my c491238e commit to 7.3.0. When the timer was canceled, the timer id was not cleared, causing the schedd to think there was still a registered timer with that id.

src/condor_schedd.V6/schedd.cpp
@@ -4734,6 +4734,7 @@ Scheduler::negotiate(int command, Stream* s)
 	m_need_reschedule = false;
 	if( m_send_reschedule_timer != -1 ) {
 		daemonCore->Cancel_Timer( m_send_reschedule_timer );
+		m_send_reschedule_timer = -1;
 	}
 
 		// set stop/start times on the negotiate timeslice object
@@ -11143,6 +11144,7 @@ Scheduler::sendReschedule()
 
 	if( m_send_reschedule_timer != -1 ) {
 		daemonCore->Cancel_Timer( m_send_reschedule_timer );
+		m_send_reschedule_timer = -1;
 	}
 
 	dprintf( D_FULLDEBUG, "Sending RESCHEDULE command to negotiator(s)\n" );