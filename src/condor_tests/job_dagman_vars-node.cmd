529c821fa34423f45e85c6a920c874be5d927252
Kent Wenger <wenger@cs.wisc.edu>
2009-10-13 16:01:17 -0500

    Added DAGMan VARS test -- this should be updated once the "no spaces in VARS" bug (gittrac #725) is fixed.

---
+++ src/condor_tests/job_dagman_vars-node.cmd
@@ -0,0 +1,10 @@
+executable   = ./job_dagman_vars-node.pl
+arguments    = $(nodename) $$([DAGParentNodeNames]) $(first) $(second) $(third)
+# Note: this needs to be vanilla universe for the $$([DAGParentNodeNames])
+# expansion to work above.
+universe     = vanilla
+output       = job_dagman_vars-$(nodename).out
+error        = job_dagman_vars-$(nodename).err
+log          = job_dagman_vars.log
+Notification = NEVER
+queue
