6f40081e238a21259b98a485df00e0f34b7ee5cd
Kent Wenger <wenger@cs.wisc.edu>
2009-10-16 15:04:48 -0500

    Added new job_dagman_rescue-A test (tests the rescue DAG problem noted in gittrac #851).

---
+++ src/condor_tests/job_dagman_rescue-A-nodeLA.cmd
@@ -0,0 +1,7 @@
+executable   = ./job_dagman_rescue-A-nodeLA.pl
+universe     = scheduler
+output       = job_dagman_rescue-A-nodeLA.out
+error        = job_dagman_rescue-A-nodeLA.err
+log          = job_dagman_rescue-A-node.log
+Notification = NEVER
+queue
