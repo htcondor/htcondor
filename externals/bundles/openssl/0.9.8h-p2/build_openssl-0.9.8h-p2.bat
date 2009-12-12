c8d04987a67c2ff40d8fa5c771cef7c4cd45f481
Jaime Frey <jfrey@cs.wisc.edu>
2009-12-12 17:22:00 -0600

    Back-port openssl renegotiation fix to 0.9.8h. #1043
    To address the recent openssl session renegotiation vulnerability, we
    changed the openssl external to 0.9.8l. Then, we discovered that some
    change between 0.9.8h and 0.9.8l breaks gsi authentication to servers
    using openssl 0.9.7. So now, we're reverting to openssl 0.9.8, but
    including the patch for the renegotiation problem.

--- externals/bundles/openssl/0.9.8h-p2/build_openssl-0.9.8h-p2.bat
+++ externals/bundles/openssl/0.9.8h-p2/build_openssl-0.9.8h-p2.bat
@@ -18,6 +18,10 @@ REM Now patch the build system to get the Multi-threaded runtime library
 REM For now, this is disabled, since I don't think we need it to make
 REM condor happy. -stolley
 
+touch zerolength
+call :patch %PACKAGE_BUILD_DIR%\renegotiate.patch
+if not %ERRORLEVEL%A == 0A goto failure
+
 cd %PACKAGE_SRC_NAME%
 rem touch zerolength
 rem patch -p1 -i ..\openssl-0.9.8-patch < zerolength
@@ -33,11 +37,11 @@ rem We do this because all warnings are treated as errors in this build.
 if /i A%NEED_MANIFESTS_IN_EXTERNALS%==Atrue ( 
     pushd util
     touch zerolength
-    call :patch %PACKAGE_BUILD_DIR%\mk1mf.pl-0.9.8l.patch
+    call :patch %PACKAGE_BUILD_DIR%\mk1mf.pl-0.9.8h-patch
     if not %ERRORLEVEL%A == 0A goto failure
     pushd pl
     touch zerolength
-    call :patch %PACKAGE_BUILD_DIR%\VC-32.pl-0.9.8l.patch
+    call :patch %PACKAGE_BUILD_DIR%\VC-32.pl-0.9.8h-patch
     if not %ERRORLEVEL%A == 0A goto failure
     popd && popd
 )
@@ -86,6 +90,6 @@ rem ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 patch --verbose -i %1 < zerolength
 if ERRORLEVEL 3 ( 
   set ERRORLEVEL=0
-  cdb -o -c "g; !gle; kb; q" patch --verbose -i %1 < zerolength
+  cdb -o -c "g; !gle; kb; q" patch --verbose -p0 -i %1 < zerolength
 )
 goto :EOF
