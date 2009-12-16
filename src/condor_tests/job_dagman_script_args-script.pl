5735e7955389eae12809864e2597dd775b44991f
Kent Wenger <wenger@cs.wisc.edu>
2009-12-16 16:16:31 -0600

    Added checking of macro substition ($JOB, $RETRY, $JOBID, $RETURN) to job_dagman_script_args; cleaned up some really ugly code structure in Script::BackgroundRun() and added warnings if $JOBID and $RETURN are used with a PRE script.

--- src/condor_tests/job_dagman_script_args-script.pl
+++ src/condor_tests/job_dagman_script_args-script.pl
@@ -6,7 +6,7 @@ $name = "job_dagman_script_args-" . $node . "-" . $script . ".out";
 
 open(OUT, ">$name") or die "Can't open file $name";
 
-while ($arg = shift) {
+while (defined($arg = shift)) {
 	print OUT "<$arg> ";
 }
 print OUT "\n";
