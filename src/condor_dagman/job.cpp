1e3b9e83108ce574782eeb1515dedd0df98e556d
Kent Wenger <wenger@cs.wisc.edu>
2010-01-29 12:12:36 -0600

    Temporarily changed Job::NOOP_NODE_PROCID back to 1000000000, because MAXINT broke the build on Windows.

--- src/condor_dagman/job.cpp
+++ src/condor_dagman/job.cpp
@@ -30,7 +30,9 @@
 
 //---------------------------------------------------------------------------
 JobID_t Job::_jobID_counter = 0;  // Initialize the static data memeber
-int Job::NOOP_NODE_PROCID = MAXINT;
+// Windows doesn't like MAXINT...
+// int Job::NOOP_NODE_PROCID = MAXINT;
+int Job::NOOP_NODE_PROCID = 1000000000;
 
 //---------------------------------------------------------------------------
 // NOTE: this must be kept in sync with the queue_t enum
