7973cee8d776316ba4f871dc0a08f7efcfd86eb9
Dan Bradley <dan@hep.wisc.edu>
2010-02-04 16:37:12 -0600

    Fixed fstat() failures in procapi when the inode is too big for 32-bit. #1184

--- src/condor_procapi/procapi.cpp
+++ src/condor_procapi/procapi.cpp
@@ -2741,9 +2741,15 @@ ProcAPI::printProcInfo(FILE* fp, piPTR pi){
 uid_t 
 ProcAPI::getFileOwner(int fd) {
 	
+#if HAVE_FSTAT64
+	// If we do not use fstat64(), fstat() fails if the inode number
+	// is too big and possibly for a few other reasons as well.
+	struct stat64 si;
+	if ( fstat64(fd, &si) != 0 ) {
+#else
 	struct stat si;
-
 	if ( fstat(fd, &si) != 0 ) {
+#endif
 		dprintf(D_ALWAYS, 
 			"ProcAPI: fstat failed in /proc! (errno=%d)\n", errno);
 		return 0; 	// 0 is probably wrong, but this should never
