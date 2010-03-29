b10ef3e02a331bb737e7f3010d75df398e02cb1d
Peter Keller <psilord@cs.wisc.edu>
2010-03-29 15:31:25 -0500

    updated version string for 7.5.3

--- src/condor_c++_util/condor_version.cpp
+++ src/condor_c++_util/condor_version.cpp
@@ -70,7 +70,7 @@
    will EXCEPT at startup time.  
 */
 
-static char* CondorVersionString = "$CondorVersion: 7.5.2 " __DATE__ BUILDIDSTR " PRE-RELEASE $";
+static char* CondorVersionString = "$CondorVersion: 7.5.3 " __DATE__ BUILDIDSTR " PRE-RELEASE $";
 
 /* Here is the platform string.  You don't need to edit this */
 static char* CondorPlatformString = "$CondorPlatform: " xstr(PLATFORM) " $";
