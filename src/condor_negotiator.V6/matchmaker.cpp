01c2ad1c0133c0e3412b9d51a0b577d7e9e56af6
Greg Thain <gthain@cs.wisc.edu>
2010-05-24 09:24:21 -0500

    Fix one-time leak in the negotiator

--- src/condor_negotiator.V6/matchmaker.cpp
+++ src/condor_negotiator.V6/matchmaker.cpp
@@ -160,6 +160,7 @@ Matchmaker::
 	if (publicAd) delete publicAd;
     if (DynQuotaMachConstraint) delete DynQuotaMachConstraint;
 	if (groupQuotasHash) delete groupQuotasHash;
+	if (stashedAds) delete stashedAds;
 }
 
 
