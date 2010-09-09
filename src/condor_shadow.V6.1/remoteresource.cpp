e4abcbd58d84bdea4b09fe49458dcb6e4841cf36
Jaime Frey <jfrey@cs.wisc.edu>
2010-09-09 17:20:13 -0500

    Start using new JobStatus STAGING_OUT. #1641
    The starter now sends a job ad update indicating the job has exited to
    the shadow before starting the transfer of output files. When the shadow
    receives this update, it changes the job's status to STAGING_OUT in the
    schedd. This change only affects vanilla, java, and vm universe jobs.

--- src/condor_shadow.V6.1/remoteresource.cpp
+++ src/condor_shadow.V6.1/remoteresource.cpp
@@ -1070,6 +1070,8 @@ RemoteResource::updateFromStarter( ClassAd* update_ad )
 			new_state = RR_EXECUTING;
 		} else if ( strcasecmp(job_state, "Checkpointed") == MATCH ) {
 			new_state = RR_CHECKPOINTED;
+		} else if ( strcasecmp(job_state, "Exited") == MATCH ) {
+			jobAd->Assign( ATTR_JOB_STATUS, STAGING_OUT );
 		} else { 
 				// For our purposes in here, we don't care about any
 				// other possible states at the moment.  If the job
