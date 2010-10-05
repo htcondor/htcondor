8be18ca859c70c64076db40501041e6fe618477f
Kent Wenger <wenger@cs.wisc.edu>
2010-10-05 09:24:00 -0500

    Modified jobstate.log test to also test rescue DAG.

--- src/condor_tests/job_dagman_jobstate_log-nodeD.pl
+++ src/condor_tests/job_dagman_jobstate_log-nodeD.pl
@@ -1,19 +1,29 @@
 #! /usr/bin/env perl
 
-open (OUTPUT, "condor_hold $ARGV[0] 2>&1 |") or die "Can't fork: $!";
-while (<OUTPUT>) {
-	print "$_";
+my $file = "job_dagman_jobstate_log-nodeD.fails";
+
+if (-e $file) {
+	unlink $file or die "Unlink failed: $!";
+
+	open (OUTPUT, "condor_hold $ARGV[0] 2>&1 |") or die "Can't fork: $!";
+	while (<OUTPUT>) {
+		print "$_";
+	}
+	close (OUTPUT) or die "Condor_hold failed: $?";
+
+	sleep 10;
+
+	open (OUTPUT, "condor_release $ARGV[0] 2>&1 |") or die "Can't fork: $!";
+	while (<OUTPUT>) {
+		print "$_";
+	}
+	close (OUTPUT) or die "Condor_release failed: $?";
+
+	# Sleep here so DAGMan restarts before this node ends.
+	sleep 20;
+
+	print "Node D failed\n";
+	exit(1);
+} else {
+	print "Node D succeeded\n";
 }
-close (OUTPUT) or die "Condor_hold failed: $?";
-
-sleep 10;
-
-open (OUTPUT, "condor_release $ARGV[0] 2>&1 |") or die "Can't fork: $!";
-while (<OUTPUT>) {
-	print "$_";
-}
-close (OUTPUT) or die "Condor_release failed: $?";
-
-sleep 20;
-
-print "Node D succeeded\n";
