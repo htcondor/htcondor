htcondor/htcondor

commit b3ef628910d6b6073ec64aa930e392313a1b43b0

This fixes a problem in Condor's file transfer semantics.
The problem is that if someone specifies transfer_output_files and is
using file transfer, then if the job doesn't produce all of the files
in question and exits (suppose by segfault), Condor will put the job
on hold. In this circumstance there will be no record of how the job
terminated. This is problematic because: it is hard to debug simply by
looking at the job ad of the held job, and the user policy expression
like the on_exit_* and periodic_* expressions can't be used to make a
judgment about the job.

I fixed it so that when the file transfer fails because not all of
the files were present, we "mock terminate" the job via a new remote
system call from the starter to the shadow which happens after the
failed transfer, but before the starter tells the shadow to put the job
on hold due to the failed transfer. This catches things like the job
segfaulting--which presumably is why it didn't produce all of its output
file. This new remote system call in the starter will only happen for
7.4.4 shadows or later. A mock terminate simply means to commit into the
schedd's authoritative copy of the job ad with how the job terminated,
but don't write a terminate event into the job log or otherwise act as
if the job has been terminated. Even though we mock terminate the job,
the job will still go on hold in these situations. The job ad will still
contain the hold information and additionally the termination information
of the job.

This is for ticket #1587 and implements only Number 1 in the list.

src/condor_starter.V6.1/baseStarter.cpp
@@ -2273,6 +2273,7 @@ CStarter::Reaper(int pid, int exit_status)
 				// so, we can directly call allJobsDone() to do final
 				// cleanup.
 			if( !allJobsDone() ) {
+				dprintf(D_ALWAYS, "Returning from CStarter::JobReaper()\n");
 				return 0;
 			}
 		}
@@ -2282,6 +2283,7 @@ CStarter::Reaper(int pid, int exit_status)
 		dprintf(D_ALWAYS,"Last process exited, now Starter is exiting\n");
 		StarterExit(0);
 	}
+
 	return 0;
 }
 
@@ -2314,7 +2316,34 @@ CStarter::allJobsDone( void )
 bool
 CStarter::transferOutput( void )
 {
-	if (!jic->transferOutput()) {
+	char *ver;
+	UserProc *job;
+
+	if (jic->transferOutput() == false) {
+
+		// Send, if the JIC thinks it is talking to a shadow of the right
+		// version, a message about the termination of the job to the shadow in
+		// the event of a file transfer failure.  The UserProc's classad which
+		// has an ATTR_JOB_PID attribute is the actual job the starter ran on
+		// behalf of the user. For other types of jobs like pre/post scripts,
+		// that attribute is name mangled and won't be present as ATTR_JOB_PID
+		// in the published ad.
+		//
+		// See the usage of the "name" variable in user_proc.h/cpp
+
+		m_reaped_job_list.Rewind();
+		while ((job = m_reaped_job_list.Next()) != NULL) {
+			ClassAd ad;
+			int pid;
+			job->PublishUpdateAd(&ad);
+			if (ad.LookupInteger(ATTR_JOB_PID, pid)) {
+				jic->notifyJobTermination(job);
+				break;
+			}
+		}
+
+		jic->transferOutputMopUp();
+
 			/*
 			  there was an error with the JIC in this step.  at this
 			  point, the only possible reason is if we're talking to a
@@ -2327,6 +2356,8 @@ CStarter::transferOutput( void )
 				 "lease to expire or for a reconnect attempt\n" );
 		return false;
 	}
+
+	jic->transferOutputMopUp();
 
 		// If we're here, the JIC successfully transfered output.
 		// We're ready to move on to the next cleanup stage.

src/condor_starter.V6.1/jic_local.h
@@ -128,6 +128,7 @@ public:
 		   just return true so we move directly onto the next stage.
 		*/
 	bool transferOutput( void ) { return true; };
+	bool transferOutputMopUp( void ) { return true; };
 
 		/** The last job this starter is controlling has been
    			completely cleaned up.  Since there's no shadow to tell us
@@ -155,6 +156,8 @@ public:
 		*/
 	bool notifyJobExit( int exit_status, int reason, 
 						UserProc* user_proc );  
+
+	int notifyJobTermination( UserProc* user_proc ) { return 0; };
 
 	virtual bool notifyStarterError( const char* err_msg, bool critical, int hold_reason_code, int hold_reason_subcode );
 

src/condor_starter.V6.1/jic_shadow.cpp
@@ -66,6 +66,7 @@ JICShadow::JICShadow( const char* shadow_name ) : JobInfoCommunicator()
 	shadow = NULL;
 	shadow_version = NULL;
 	filetrans = NULL;
+	m_did_transfer = false;
 	m_filetrans_sec_session = NULL;
 	m_reconnect_sec_session = NULL;
 	
@@ -370,11 +371,13 @@ JICShadow::Continue( void )
 bool
 JICShadow::transferOutput( void )
 {
-		// make sure we only do this step once.
-	static bool did_transfer = false;
-	if (did_transfer) {
+	dprintf(D_FULLDEBUG, "Inside JICShadow::transferOutput(void)\n");
+
+	if (m_did_transfer) {
 		return true;
 	}
+
+	dprintf(D_FULLDEBUG, "JICShadow::transferOutput(void): Transferring...\n");
 
 		// transfer output files back if requested job really
 		// finished.  may as well do this in the foreground,
@@ -416,44 +419,22 @@ JICShadow::transferOutput( void )
 			// by the user, so set_user_priv here.
 		priv_state saved_priv = set_user_priv();
 
+		dprintf( D_FULLDEBUG, "Begin transfer of sandbox to shadow.\n");
 			// this will block
-		bool rval = filetrans->UploadFiles( true, final_transfer );
+		m_ft_rval = filetrans->UploadFiles( true, final_transfer );
+		dprintf( D_FULLDEBUG, "End transfer of sandbox to shadow.\n");
 		set_priv(saved_priv);
 
-		if( ! rval ) {
-				// Failed to transfer.  See if there is a reason to put
-				// the job on hold.
-			FileTransfer::FileTransferInfo ft_info = filetrans->GetInfo();
-			if(!ft_info.success && !ft_info.try_again) {
-				ASSERT(ft_info.hold_code != 0);
-				notifyStarterError(ft_info.error_desc.Value(), true,
-				                   ft_info.hold_code,ft_info.hold_subcode);
-				return false;
+		if (! m_ft_rval) {
+			dprintf( D_FULLDEBUG, "Sandbox transfer failed.\n");
+			// Failed to transfer.  Record if there is a reason to put
+			// the job on hold. Look to JICShadow::transferOutputMopUp()
+			// for what you should call if this function returns false.
+			m_ft_info = filetrans->GetInfo();
+			if(!m_ft_info.success) {
+				ASSERT(m_ft_info.hold_code != 0);
 			}
-
-				// Some other kind of error.  Would like to know
-				// for sure whether this really means we are disconnected
-				// from the shadow, but for now, force the socket to
-				// disconnect by closing it.
-
-				// Please forgive us these hacks
-				// as we forgive those who hack against us
-			static int timesCalled = 0;
-			timesCalled++;
-			if (timesCalled < 5) {
-					dprintf(D_ALWAYS,"File transfer failed, forcing disconnect.\n");
-
-					if (syscall_sock != NULL) {
-							syscall_sock->close();
-					}
-
-					job_cleanup_disconnected = true;
-					return false;
-			}
-
-				// We tried 5 times and kept failing
-				// now just tell the user we're giving up
-			notifyStarterError("Repeated attempts to transfer output failed for unknown reasons", true,0,0);
+			m_did_transfer = false;
 			return false;
 		}
 	}
@@ -461,7 +442,63 @@ JICShadow::transferOutput( void )
 		// In both cases, we should record that we were successful so
 		// that if we ever come through here again to retry the whole
 		// job cleanup process we don't attempt to transfer again.
-	did_transfer = true;
+	m_did_transfer = true;
+	return true;
+}
+
+bool
+JICShadow::transferOutputMopUp(void)
+{
+	static int timesCalled = 0;
+
+	dprintf(D_FULLDEBUG, "Inside JICShadow::transferOutputMopUp(void)\n");
+
+	if (m_did_transfer) {
+		/* nothing was marked wrong if we were able to do the transfer */
+		return true;
+	}
+
+	// We saved the return value of the last filetransfer attempt...
+	if( ! m_ft_rval ) {
+		dprintf(D_FULLDEBUG, "JICShadow::transferOutputMopUp(void): "
+			"Mopping up failed transfer...\n");
+
+		// Failed to transfer.  See if there is a reason to put
+		// the job on hold.
+		if(!m_ft_info.success && !m_ft_info.try_again) {
+			ASSERT(m_ft_info.hold_code != 0);
+			// The shadow will immediately cut the connection to the
+			// starter when this is called. 
+			notifyStarterError(m_ft_info.error_desc.Value(), true,
+			                   m_ft_info.hold_code,m_ft_info.hold_subcode);
+			return false;
+		}
+
+		// Some other kind of error.  Would like to know
+		// for sure whether this really means we are disconnected
+		// from the shadow, but for now, force the socket to
+		// disconnect by closing it.
+
+		// Please forgive us these hacks
+		// as we forgive those who hack against us
+		timesCalled++;
+		if (timesCalled < 5) {
+				dprintf(D_ALWAYS,"File transfer failed, forcing disconnect.\n");
+
+				if (syscall_sock != NULL) {
+						syscall_sock->close();
+				}
+
+				job_cleanup_disconnected = true;
+				return false;
+		}
+
+		// We tried 5 times and kept failing
+		// now just tell the user we're giving up
+		notifyStarterError("Repeated attempts to transfer output failed for unknown reasons", true,0,0);
+		return false;
+	}
+
 	return true;
 }
 
@@ -717,6 +754,26 @@ JICShadow::notifyJobExit( int exit_status, int reason, UserProc*
 	}
 
 	return true;
+}
+
+int
+JICShadow::notifyJobTermination( UserProc *user_proc )
+{
+	int rval = 0;
+	ClassAd ad;
+
+	dprintf(D_FULLDEBUG, "Inside JICShadow::notifyJobTermination()\n");
+
+	if (shadow_version && shadow_version->built_since_version(7,4,4)) {
+		dprintf(D_ALWAYS, "JICShadow::notifyJobTermination(): "
+			"Sending mock terminate event.\n");
+
+		user_proc->PublishUpdateAd( &ad );
+
+		rval = REMOTE_CONDOR_job_termination(&ad);
+	}
+
+	return rval;
 }
 
 void

src/condor_starter.V6.1/jic_shadow.h
@@ -113,8 +113,21 @@ public:
 
 		/** Once all the jobs are done, and after the optional
 			HOOK_JOB_EXIT has returned, initiate the file transfer.
+			If you call this, you MUST use transferOutputMopUp() afterwards
+			to handle problems the file transfer may have had.
 		*/
 	bool transferOutput( void );
+
+		/** After transferOutput returns, we need to handle what happens
+			if the transfer actually failed. This call is separate from the
+			one above since it allows us to talk to the shadow in the time
+			between the failure of the transfer and the disconnection of the
+			shadow when the starter notifies it of the file transfer error.
+			This call will put the job on hold and cause the shadow to
+			disconnect from the starter if something went wrong. If 
+			the file transfer went right, then it is a noop.
+		*/
+	bool transferOutputMopUp( void );
 
 		/** The last job this starter is controlling has been
    			completely cleaned up.  We don't care, since we just wait
@@ -142,13 +155,31 @@ public:
 		 */
 	void notifyJobPreSpawn( void );
 
-		/** Notify the shadow that the job exited
+		/** Notify the shadow that the job exited. This will not only
+			update the job ad with the termination information of the job,
+			but also write terminate events into the job log, and do the
+			rest of the machinery for when a job finishes and is likely to
+			leave the queue.
+
 			@param exit_status The exit status from wait()
 			@param reason The Condor-defined exit reason
 			@param user_proc The UserProc that was running the job
 		*/
 	bool notifyJobExit( int exit_status, int reason,
 						UserProc* user_proc );
+	
+		/** The sutble distinction between notifyJobTermination() and
+			notifyJobExit() is that notifyJobTermination() will only update the
+			U_TERMINATE attributes in the job ad on the submit machine based
+			upon the exit_status and reason and do nothing else! An example
+			use is when a job coredumps and due to this causes the 
+			file transfer to fail since some files might not exist if 
+			directly specified in transfer_output_files. This allows the
+			job ad to contain the exit status of the job while it is on hold.
+			Basically this is used to preserve the termination of the job in
+			the face of Condor then messing stuff up terribly around it.
+		*/
+	int notifyJobTermination( UserProc* user_proc );
 
 		/** Tell the shadow to log an error event in the user log.
 			@param err_msg A desription of the error
@@ -403,6 +434,10 @@ private:
 	IOProxy io_proxy;
 
 	FileTransfer *filetrans;
+	bool m_ft_rval;
+	FileTransfer::FileTransferInfo m_ft_info;
+	bool m_did_transfer;
+
 
 		// specially made security sessions if we are doing
 		// SEC_ENABLE_MATCH_PASSWORD_AUTHENTICATION

src/condor_starter.V6.1/job_info_communicator.h
@@ -212,6 +212,7 @@ public:
 			in all JICs so that the code path during cleanup is sane.
 		*/
 	virtual bool transferOutput( void ) = 0;
+	virtual bool transferOutputMopUp( void ) = 0;
 
 		/** The last job this starter is controlling has been
 			completely cleaned up.  Do whatever final work we want to
@@ -258,6 +259,8 @@ public:
 		*/
 	virtual bool notifyJobExit( int exit_status, int reason, 
 								UserProc* user_proc ) = 0;
+
+	virtual int notifyJobTermination( UserProc* user_proc ) = 0;
 
 	virtual bool notifyStarterError( const char* err_msg, bool critical, int hold_reason_code, int hold_reason_subcode ) = 0;
 

src/condor_shadow.V6.1/NTreceivers.cpp
@@ -52,6 +52,7 @@ static const char * shadow_syscall_name(int condor_sysnum)
         case CONDOR_get_job_info: return "get_job_info";
         case CONDOR_get_user_info: return "get_user_info";
         case CONDOR_job_exit: return "job_exit";
+        case CONDOR_job_termination: return "job_termination";
         case CONDOR_begin_execution: return "begin_execution";
         case CONDOR_open: return "open";
         case CONDOR_close: return "close";
@@ -324,6 +325,31 @@ do_REMOTE_syscall()
 		result = ( syscall_sock->end_of_message() );
 		ASSERT( result );
 		return -1;
+	}
+
+	case CONDOR_job_termination:
+	{
+		ClassAd ad;
+		result = ( ad.initFromStream(*syscall_sock) );
+		ASSERT( result );
+		result = ( syscall_sock->end_of_message() );
+		ASSERT( result );
+
+		errno = 0;
+		rval = pseudo_job_termination( &ad );
+		terrno = (condor_errno_t)errno;
+		dprintf( D_SYSCALLS, "\trval = %d, errno = %d\n", rval, terrno );
+
+		syscall_sock->encode();
+		result = ( syscall_sock->code(rval) );
+		ASSERT( result );
+		if( rval < 0 ) {
+			result = ( syscall_sock->code( terrno ) );
+			ASSERT( result );
+		}
+		result = ( syscall_sock->end_of_message() );
+		ASSERT( result );
+		return 0;
 	}
 
 	case CONDOR_begin_execution:

src/condor_shadow.V6.1/baseshadow.cpp
@@ -423,6 +423,51 @@ BaseShadow::holdJob( const char* reason, int hold_reason_code, int hold_reason_s
 	DC_Exit( JOB_SHOULD_HOLD );
 }
 
+void
+BaseShadow::mockTerminateJob( MyString exit_reason, 
+		bool exited_by_signal, int exit_code, int exit_signal, 
+		bool core_dumped )
+{
+	if (exit_reason == "") {
+		exit_reason = "Exited normally";
+	}
+	
+	dprintf( D_ALWAYS, "Mock terminating job %d.%d: "
+			"exited_by_signal=%s, exit_code=%d OR exit_signal=%d, "
+			"core_dumped=%s, exit_reason=\"%s\"\n", 
+			 getCluster(),
+			 getProc(), 
+			 exited_by_signal ? "TRUE" : "FALSE",
+			 exit_code,
+			 exit_signal,
+			 core_dumped ? "TRUE" : "FALSE",
+			 exit_reason.Value());
+
+	if( ! jobAd ) {
+		dprintf(D_ALWAYS, "BaseShadow::mockTerminateJob(): NULL JobAd! "
+			"Holding Job!");
+		DC_Exit( JOB_SHOULD_HOLD );
+	}
+
+	// Insert the various exit attributes into our job ad.
+	jobAd->Assign( ATTR_JOB_CORE_DUMPED, core_dumped );
+	jobAd->Assign( ATTR_ON_EXIT_BY_SIGNAL, exited_by_signal );
+
+	if (exited_by_signal) {
+		jobAd->Assign( ATTR_ON_EXIT_SIGNAL, exit_signal );
+	} else {
+		jobAd->Assign( ATTR_ON_EXIT_CODE, exit_code );
+	}
+
+	jobAd->Assign( ATTR_EXIT_REASON, exit_reason );
+
+		// update the job queue for the attributes we care about
+	if( !updateJobInQueue(U_TERMINATE) ) {
+			// trouble!  TODO: should we do anything else?
+		dprintf( D_ALWAYS, "Failed to update job queue!\n" );
+	}
+}
+
 
 void
 BaseShadow::removeJob( const char* reason )

src/condor_shadow.V6.1/baseshadow.h
@@ -176,6 +176,19 @@ class BaseShadow : public Service
 		*/
 	void terminateJob( update_style_t kind = US_NORMAL );
 
+		/** The job exited, but it _isn't_ ready to leave the queue.
+
+			For example, the job segfaulted and didn't produce all of the
+			output files specifically labeled in transfer_output_files, so
+			this causes file transfer to fail which puts the job on hold
+			(as of 7.4.4). However, the user would like to write periodic or
+			exit policies about the job and for that we need to know exactly
+			how the job exited. So this call places how the job exited into
+			the jobad, but doesn't write any events about it.
+		*/
+	void mockTerminateJob( MyString exit_reason, bool exited_by_signal, 
+		int exit_code, int exit_signal, bool core_dumped );
+
 		/** Set a timer to call terminateJob() so we retry
 		    our attempts to update the job queue etc.
 		*/

commit 7fc8355533ee984567ff8026e8dd24294836132e

Restored handling of disconnect during file transfer. #1750 The problem introduced in b3ef6289 (#1587) is that the starter would abort if the file transfer failed with a transient failure, such as a disconnect.

src/condor_starter.V6.1/baseStarter.cpp
@@ -2318,8 +2318,14 @@ CStarter::transferOutput( void )
 {
 	char *ver;
 	UserProc *job;
+	bool transient_failure = false;
 
-	if (jic->transferOutput() == false) {
+	if (jic->transferOutput(transient_failure) == false) {
+
+		if( transient_failure ) {
+				// we will retry the transfer when (if) the shadow reconnects
+			return false;
+		}
 
 		// Send, if the JIC thinks it is talking to a shadow of the right
 		// version, a message about the termination of the job to the shadow in

src/condor_starter.V6.1/jic_local.h
@@ -127,7 +127,7 @@ public:
 		   happen.  We don't do any file transfer for JICLocal, so
 		   just return true so we move directly onto the next stage.
 		*/
-	bool transferOutput( void ) { return true; };
+	bool transferOutput( bool &transient_failure ) { return true; };
 	bool transferOutputMopUp( void ) { return true; };
 
 		/** The last job this starter is controlling has been

src/condor_starter.V6.1/jic_shadow.cpp
@@ -369,9 +369,11 @@ JICShadow::Continue( void )
 
 
 bool
-JICShadow::transferOutput( void )
+JICShadow::transferOutput( bool &transient_failure )
 {
 	dprintf(D_FULLDEBUG, "Inside JICShadow::transferOutput(void)\n");
+
+	transient_failure = false;
 
 	if (m_did_transfer) {
 		return true;
@@ -431,9 +433,35 @@ JICShadow::transferOutput( void )
 			// the job on hold. Look to JICShadow::transferOutputMopUp()
 			// for what you should call if this function returns false.
 			m_ft_info = filetrans->GetInfo();
-			if(!m_ft_info.success) {
-				ASSERT(m_ft_info.hold_code != 0);
+
+			if( !m_ft_info.success && m_ft_info.try_again ) {
+				// Some kind of transient error, such as a timeout or
+				// disconnect.  Would like to know for sure whether
+				// this really means we are disconnected from the
+				// shadow, but for now, force the socket to disconnect
+				// by closing it.
+
+				// Please forgive us these hacks
+				// as we forgive those who hack against us
+
+				static int timesCalled = 0;
+				timesCalled++;
+				if (timesCalled < 5) {
+					dprintf(D_ALWAYS,"File transfer failed, forcing disconnect.\n");
+
+					if (syscall_sock != NULL) {
+						syscall_sock->close();
+					}
+
+						// trigger retransfer on reconnect
+					job_cleanup_disconnected = true;
+
+						// inform our caller that transfer will be retried
+						// when the shadow reconnects
+					transient_failure = true;
+				}
 			}
+
 			m_did_transfer = false;
 			return false;
 		}
@@ -449,7 +477,6 @@ JICShadow::transferOutput( void )
 bool
 JICShadow::transferOutputMopUp(void)
 {
-	static int timesCalled = 0;
 
 	dprintf(D_FULLDEBUG, "Inside JICShadow::transferOutputMopUp(void)\n");
 
@@ -474,27 +501,8 @@ JICShadow::transferOutputMopUp(void)
 			return false;
 		}
 
-		// Some other kind of error.  Would like to know
-		// for sure whether this really means we are disconnected
-		// from the shadow, but for now, force the socket to
-		// disconnect by closing it.
-
-		// Please forgive us these hacks
-		// as we forgive those who hack against us
-		timesCalled++;
-		if (timesCalled < 5) {
-				dprintf(D_ALWAYS,"File transfer failed, forcing disconnect.\n");
-
-				if (syscall_sock != NULL) {
-						syscall_sock->close();
-				}
-
-				job_cleanup_disconnected = true;
-				return false;
-		}
-
-		// We tried 5 times and kept failing
-		// now just tell the user we're giving up
+		// We hit some "transient" error, but we've retried too many times,
+		// so tell the shadow we are giving up.
 		notifyStarterError("Repeated attempts to transfer output failed for unknown reasons", true,0,0);
 		return false;
 	}

src/condor_starter.V6.1/jic_shadow.h
@@ -113,10 +113,11 @@ public:
 
 		/** Once all the jobs are done, and after the optional
 			HOOK_JOB_EXIT has returned, initiate the file transfer.
-			If you call this, you MUST use transferOutputMopUp() afterwards
-			to handle problems the file transfer may have had.
+			If you call this, and transient_failure is not true, you
+			MUST use transferOutputMopUp() afterwards to handle
+			problems the file transfer may have had.
 		*/
-	bool transferOutput( void );
+	bool transferOutput( bool &transient_failure );
 
 		/** After transferOutput returns, we need to handle what happens
 			if the transfer actually failed. This call is separate from the

src/condor_starter.V6.1/job_info_communicator.h
@@ -210,8 +210,11 @@ public:
 			internal file transfer for the output.  This only makes
 			sense for JICShadow, but we need this step to be included
 			in all JICs so that the code path during cleanup is sane.
+			Returns false on failure and sets transient_failure to
+			true if the failure is deemed transient and will therefore
+			be automatically tried again (e.g. when the shadow reconnects).
 		*/
-	virtual bool transferOutput( void ) = 0;
+	virtual bool transferOutput( bool &transient_failure ) = 0;
 	virtual bool transferOutputMopUp( void ) = 0;
 
 		/** The last job this starter is controlling has been