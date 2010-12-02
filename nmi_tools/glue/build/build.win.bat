ba0e2bf438f5fd548277a9d161856e5c5ae339d1
John (TJ) Knoeller <johnkn@cs.wisc.edu>
2010-12-02 11:42:39 -0600

    Change Windows NMI build to build via build.win.bat rather than invoking devenv directly in remote_task

---
+++ nmi_tools/glue/build/build.win.bat
@@ -0,0 +1,25 @@
+@echo CD=%CD%
+@echo HOME=%HOME%
+@echo LIB=%LIB%
+@echo INCLUDE=%INCLUDE%
+@echo PATH=%PATH%
+
+for /D %%I in ("%VS90COMNTOOLS%..") do set VS90ROOT=%%~sdpI
+set VS_DIR=%VS90ROOT:~0,-1%
+set VC_DIR=%VS_DIR%\VC
+set VC_BIN=%VS_DIR%\bin
+set DOTNET_PATH=%SystemRoot%\Microsoft.NET\Framework\v3.5;%SystemRoot%\Microsoft.NET\Framework\v2.0.50727
+set PERL_PATH=c:\prereq\ActivePerl-5.10.1\bin;c:\perl\bin
+set ZIP_PATH=%ProgramFiles%\7-Zip
+set WIX_PATH=%WIX%
+set MSCONFIG_TOOLS_DIR=%HOME%\userdir\msconfig
+set CMAKE_BIN=%ProgramFiles%\CMake 2.8\bin
+
+set PATH=%SystemRoot\system32;%SystemRoot%;%PERL_PATH%;%MSCONFIG_TOOLS_DIR%;%VS_DIR%\Common7\IDE;%VC_BIN%;%CMAKE_BIN%;%ZIP_PATH%;%WIX_PATH%
+
+@echo ----  build.win.bat ENVIRONMENT --------------------------------
+env
+@echo ----  end build.win.bat ENVIRONMENT ----------------------------
+
+@echo devenv CONDOR.sln /Rebuild RelWithDebInfo /project PACKAGE
+devenv CONDOR.sln /Rebuild RelWithDebInfo /project PACKAGE
