88980770a7b674d6998d2d5c7b08e4eb29eb489b
Jaime Frey <jfrey@cs.wisc.edu>
2010-12-20 14:29:39 -0600

    Disable optimizations on ppc_macos_10.4.
    With optimizations enabled, the compile crashes when building
    condor_submit. The previous attempt to do this only changed a comment.

--- build/cmake/CondorConfigure.cmake
+++ build/cmake/CondorConfigure.cmake
@@ -89,9 +89,9 @@ if( NOT WINDOWS)
 	  # Using -O2 crashes the compiler on ppc mac os x when compiling
 	  # condor_submit
 	  if ( ${OS_NAME} STREQUAL "DARWIN" AND ${SYS_ARCH} STREQUAL "POWERPC" )
-	    set( CMAKE_BUILD_TYPE RelWithDebInfo ) # = -g (package will strip the info)
+	    set( CMAKE_BUILD_TYPE Debug ) # = -g (package may strip the info)
 	  else()
-	    set( CMAKE_BUILD_TYPE RelWithDebInfo ) # = -O2 -g (package will strip the info)
+	    set( CMAKE_BUILD_TYPE RelWithDebInfo ) # = -O2 -g (package may strip the info)
 	  endif()
 	endif()
 
