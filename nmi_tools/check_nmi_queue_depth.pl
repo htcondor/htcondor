2abf00c04db3bb36a2e82e1cc0a41ed8c057a414
Scot Kronenfeld <kronenfe@cs.wisc.edu>
2011-01-18 21:15:19 -0600

    Use absolute path to condor commands because when run from cron the PATH will not be set.  Also added a header with comments about what the script does.

--- nmi_tools/check_nmi_queue_depth.pl
+++ nmi_tools/check_nmi_queue_depth.pl
@@ -3,6 +3,11 @@ use strict;
 use warnings;
 use Getopt::Long;
 
+# This script runs condor_q to find all the queued jobs and what platform they want.
+# It prints all platforms that have more than <THRESHOLD> jobs queued (THRESHOLD is
+# a command line parameter).  It also runs condor_status for each of those platforms
+# to try to get more information about why so many jobs are queued, such as the
+# machine is not in the pool or no jobs are matching.
 
 my $THRESHOLD;
 my $SEND_EMAIL;
@@ -14,7 +19,7 @@ if(not $THRESHOLD) {
     exit 1;
 }
 
-my @out = `condor_q -format '%s\n' nmi_target_platform | sort | uniq -c | sort -nr`;
+my @out = `/usr/local/condor/bin/condor_q -format '%s\n' nmi_target_platform | sort | uniq -c | sort -nr`;
 
 my @email;
 my @platforms;
@@ -31,7 +36,7 @@ push @email, "\n\n\nIndividual platform status:\n";
 
 my @missing;
 foreach my $platform (@platforms) {
-    my $output = `condor_status -const 'nmi_platform=="$platform"'`;
+    my $output = `/usr/local/condor/bin/condor_status -const 'nmi_platform=="$platform"'`;
     $output =~ s/\n\n/\n/g;
     
     # If we get nothing back (except whitespace) then the machine is probably not in 
