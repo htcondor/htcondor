bb0dbde20ab75660675c9ae3edd3c95809a5ff6a
Greg Thain <gthain@cs.wisc.edu>
2011-01-31 09:38:56 -0600

    cmd_q_shows_sub test was failing, as it had a bogus executable
    Of all the tests, this was the only one using CondorTest::RunTest2
    
    While RunTest2 was a pleasant idea (passing a perl hash representing
    the submit file instead of another file), having only one out of hundred
    test use it is not.  I changed this to use the same CondorTest::RunTest
    that everyone else uses.

---
+++ src/condor_tests/cmd_q_shows-sub.cmd
@@ -0,0 +1,9 @@
+universe = vanilla
+Notification = NEVER
+executable = /bin/sleep
+arguments = 0
+error = cmd_q_shows-sub.err
+hold = true
+log = cmd_q_shows-sub.log
+output = cmd_q_shows-sub.out
+queue 1
