htcondor/htcondor

commit 2aeaf46ba91eecf9173f1ccf267ab298e4d4f86d

Fixed building of Condor w/o SSL

src/condor_utils/condor_base64.cpp
@@ -23,15 +23,18 @@
 #include "condor_base64.h"
 
 // For base64 encoding
-#include <openssl/sha.h>
-#include <openssl/hmac.h>
-#include <openssl/evp.h>
-#include <openssl/bio.h>
-#include <openssl/buffer.h>
+#if HAVE_EXT_OPENSSL
+# include <openssl/sha.h>
+# include <openssl/hmac.h>
+# include <openssl/evp.h>
+# include <openssl/bio.h>
+# include <openssl/buffer.h>
+#endif
 
 // Caller needs to free the returned pointer
 char* condor_base64_encode(const unsigned char *input, int length)
 {
+#if HAVE_OPENSSL
 	BIO *bmem, *b64;
 	BUF_MEM *bptr;
 
@@ -49,11 +52,18 @@ char* condor_base64_encode(const unsigned char *input, int length)
 	BIO_free_all(b64);
 
 	return buff;
+#else
+	(void) input;
+	(void) length;
+	EXCEPT( "condor_base64_encode() not available: HAVE_OPENSSL is false" );
+	return NULL;
+#endif
 }
 
 // Caller needs to free *output if non-NULL
 void condor_base64_decode(const char *input,unsigned char **output, int *output_length)
 {
+#if HAVE_OPENSSL
 	BIO *b64, *bmem;
 
 	ASSERT( input );
@@ -78,4 +88,11 @@ void condor_base64_decode(const char *input,unsigned char **output, int *output_
 	}
 
 	BIO_free_all(bmem);
+#else
+	(void) input;
+	(void) output;
+	(void) output_length;
+	EXCEPT( "condor_base64_encode() not available: HAVE_OPENSSL is false" );
+#endif
 }
+

src/condor_utils/condor_dh.cpp
@@ -1,6 +1,6 @@
 /***************************************************************
  *
- * Copyright (C) 1990-2007, Condor Team, Computer Sciences Department,
+ * Copyright (C) 1990-2011, Condor Team, Computer Sciences Department,
  * University of Wisconsin-Madison, WI.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you
@@ -22,6 +22,9 @@
 #include "condor_dh.h"
 #include "condor_debug.h"
 #include "condor_config.h"
+
+#if HAVE_EXT_OPENSSL
+
 //#include <openssl/pem.h>
 //#include <openssl/bn.h>
 
@@ -197,3 +200,5 @@ int Condor_Diffie_Hellman :: initialize()
     }
     return 0;
 }
+
+#endif

src/condor_utils/condor_dh.h
@@ -21,6 +21,9 @@
 #define CONDOR_DH
 
 #include "condor_common.h"
+
+#if HAVE_EXT_OPENSSL
+
 #include <openssl/ssl.h>
 //#include <openssl/rand.h>
 
@@ -65,6 +68,6 @@ class Condor_Diffie_Hellman {
     unsigned char * secret_;
     int keySize_;
 };
-
+#endif
 
 #endif

commit ed0148d3d621c6a43f40e002a9bdbb8f42a6f7f2

Fixed ssh to job broken by 2aeaf46ba91eecf9173f1ccf267ab298e4d4f86d

src/condor_utils/condor_base64.cpp
@@ -34,7 +34,7 @@
 // Caller needs to free the returned pointer
 char* condor_base64_encode(const unsigned char *input, int length)
 {
-#if HAVE_OPENSSL
+#if HAVE_EXT_OPENSSL
 	BIO *bmem, *b64;
 	BUF_MEM *bptr;
 
@@ -55,7 +55,7 @@ char* condor_base64_encode(const unsigned char *input, int length)
 #else
 	(void) input;
 	(void) length;
-	EXCEPT( "condor_base64_encode() not available: HAVE_OPENSSL is false" );
+	EXCEPT( "condor_base64_encode() not available: HAVE_EXT_OPENSSL is false" );
 	return NULL;
 #endif
 }
@@ -63,7 +63,7 @@ char* condor_base64_encode(const unsigned char *input, int length)
 // Caller needs to free *output if non-NULL
 void condor_base64_decode(const char *input,unsigned char **output, int *output_length)
 {
-#if HAVE_OPENSSL
+#if HAVE_EXT_OPENSSL
 	BIO *b64, *bmem;
 
 	ASSERT( input );
@@ -92,7 +92,7 @@ void condor_base64_decode(const char *input,unsigned char **output, int *output_
 	(void) input;
 	(void) output;
 	(void) output_length;
-	EXCEPT( "condor_base64_encode() not available: HAVE_OPENSSL is false" );
+	EXCEPT( "condor_base64_encode() not available: HAVE_EXT_OPENSSL is false" );
 #endif
 }
 