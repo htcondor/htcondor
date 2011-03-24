4429485cc385f4055c602490a8a74d83b2bb5148
Scot Kronenfeld <kronenfe@cs.wisc.edu>
2011-03-24 15:50:47 -0500

    Disable kerberos on Debian 6 for now.  The externals do not build with the current version of Kerberos on this platform.

--- nmi_tools/glue/SubmitInfo.pm
+++ nmi_tools/glue/SubmitInfo.pm
@@ -328,6 +328,7 @@ our %submit_info = (
 		'build' => {
 			'configure_args' => { @default_build_configure_args,
 				'-DCLIPPED:BOOL=OFF' => undef,
+                                '-DWITH_KRB5:BOOL=OFF' => undef,
 			 },
 			'prereqs'	=> [ 'libtool-1.5.26', 'cmake-2.8.3' ],
 			'xtests'	=>  [ 'x86_64_ubuntu_10.04', ],
