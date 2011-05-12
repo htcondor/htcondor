4bdd101c370e1744cd5462b4bf7b537f62826e72
Nathan W. Panike <nwp@cs.wisc.edu>
2011-05-12 15:14:22 -0500

    Test to make sure PRE_SKIP kills POST script
    ===GT=== #2122

--- src/condor_tests/job_dagman_pre_skip-C.cmd
+++ src/condor_tests/job_dagman_pre_skip-C.cmd
@@ -1,6 +1,6 @@
 executable = /bin/sh
 output = $(job).out
 error = $(job).err
-arguments = "-c ''echo ""B is done""; exit 0;''"
+arguments = "-c ''echo ""C is done""; exit 0;''"
 universe = scheduler
 queue
