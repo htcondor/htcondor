510ccc4e897cb616a5f7a2625808880d4f8f10c6
Nathan M. Mitchell <nmitchel@cs.wisc.edu>
2011-05-26 09:34:51 -0500

    more stuff for the receive

--- src/cftp_tool/cftp_recieve.h
+++ src/cftp_tool/cftp_recieve.h
@@ -2,69 +2,21 @@
 #define CFTP_SERVICE_RECIEVE_H_
 
 #include "cftp_service.h"
-
-#include "frames.h"
-#include "simple_parameters.h"
-#include "utilities.h"
-
-enum ProtocolPhase { DISCOVERY, NEGOTIATION, TRANSFER, TEARDOWN }; 
-enum Errors { NO_ERROR, };	
-	
-/* Used by main to communicate with parse_opt. */
-typedef struct _arguments
-{
-	int verbose, itimeout, ptimeout, debug, announce;
-	long quota;
-	char* aport;
-	char* lport;
-	char* ahost;
-	char* lhost;
-	char* tpath;
-	char* uuid;
-} ServerArguments;
-
-typedef struct _ServerState
-{
-	int          phase;
-	int 		 state;
-	int          last_error;
-	char         error_string[256];
-
-	FileRecord   local_file;
-	ServerRecord server_info;
-	ServerRecord oob_info;
-	ClientRecord client_info;
-	ServerArguments* arguments;
-
-	int          session_token;
-	int          parameter_length;
-	int          parameter_format;
-	char*        session_parameters;
-
-	long         current_chunk;
-	long         data_written;
-	int          retry_count;
-
-	int          recv_rdy;
-    int		     send_rdy;
-
-	cftp_frame   fsend_buf;
-	cftp_frame   frecv_buf;
-
-	char*        data_buffer;
-	int          data_buffer_size;
-
-
-} ServerState;
-
-typedef int (*StateAction)(ServerState*);
+#include "server_sm_lib.h"
 
 class CFTP_RecieveService : public Service
 {
+ public:
     CFTP_RecieveService( int handle );
     virtual ~CFTP_RecieveService();
 
     virtual void step();
+
+ private:
+	int transition_table( int condition );
+
+
+	ServerState _state;
 }
 
 #endif
