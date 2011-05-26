acc300402278cb6aee608d92ac73e2d0dd0025cf
Scott Kronenfeld <kronenfe@nmi-s006.cs.wisc.edu>
2011-05-26 14:05:09 -0500

    Changed return type of debug_print from "void *" to "void" so that this function will compile under Windows.
    I think void is what Greg meant, but I'll send this to him for review.

--- src/classad/exprTree.cpp
+++ src/classad/exprTree.cpp
@@ -30,7 +30,7 @@ extern int exprHash( const ExprTree* const&, int );
 
 static const int MAX_CLASSAD_RECURSION = 1000;
 
-void *ExprTree::debug_print(const char *message) {
+void ExprTree::debug_print(const char *message) {
 /* need to call the callback here instead
 	FILE *file = fopen("debug.out", "a");
 	if(file) {
