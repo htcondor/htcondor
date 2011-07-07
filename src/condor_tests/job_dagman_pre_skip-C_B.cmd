8086293da1e142c7792326854b60a3bba363457a
Kent Wenger <wenger@cs.wisc.edu>
2011-07-07 18:18:54 -0500

    Fixed up the job_dagman_pre_skip-C test.

--- src/condor_tests/job_dagman_pre_skip-C_B.cmd
+++ src/condor_tests/job_dagman_pre_skip-C_B.cmd
@@ -1,6 +1,6 @@
 executable = /bin/echo
-output = job_dagman_pre_skip-C_$(job).out
-error = job_dagman_pre_skip-C_$(job).err
+output = job_dagman_pre_skip-C_B.out
+error = job_dagman_pre_skip-C_B.err
 arguments = OK done with B
 universe = scheduler
 queue
--- src/condor_tests/job_dagman_pre_skip-C_C.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env sh
-echo "ACCHHH! C_C should not run" > C_C.out
-exit 0
