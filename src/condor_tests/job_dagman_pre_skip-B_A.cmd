a9d8ab28c365b2a8b63276f0cde854f03f805964
Kent Wenger <wenger@cs.wisc.edu>
2011-07-07 17:05:14 -0500

    Got job_dagman_pre_skip-B to work; a little more cleanup of job_dagman_pre_skip-A.

--- src/condor_tests/job_dagman_pre_skip-B_A.cmd
+++ src/condor_tests/job_dagman_pre_skip-B_A.cmd
@@ -1,6 +1,6 @@
 executable = /bin/echo
-output = job_dagman_pre_skip-B_$(job).out
-error = job_dagman_pre_skip-B_$(job).err
+output = job_dagman_pre_skip-B_A.out
+error = job_dagman_pre_skip-B_A.err
 arguments = OK done with A
 universe = scheduler
 queue
--- src/condor_tests/job_dagman_pre_skip-B_B.cmd
+++ src/condor_tests/job_dagman_pre_skip-B_B.cmd
@@ -1,6 +1,6 @@
 executable = /bin/echo
-output = job_dagman_pre_skip-B_$(job).out
-error = job_dagman_pre_skip-B_$(job).err
+output = job_dagman_pre_skip-B_B.out
+error = job_dagman_pre_skip-B_B.err
 arguments = OK done with B
 universe = scheduler
 queue
