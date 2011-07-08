3d1eeba4252a9c5b998ae8450120a6ddbc316d7b
Kent Wenger <wenger@cs.wisc.edu>
2011-07-07 20:53:56 -0500

    Fixed up the job_dagman_pre_skip-D test, a few more fixes to job_dagman_pre_skip-A.

--- src/condor_tests/job_dagman_pre_skip-D_A.cmd
+++ src/condor_tests/job_dagman_pre_skip-D_A.cmd
@@ -1,6 +1,6 @@
 executable = /bin/echo
-output = job_dagman_pre_skip-D_$(job).out
-error = job_dagman_pre_skip-D_$(job).err
+output = job_dagman_pre_skip-D_A.out
+error = job_dagman_pre_skip-D_A.err
 arguments = OK done with A
 universe = scheduler
 queue
--- src/condor_tests/job_dagman_pre_skip-D_B.cmd
+++ src/condor_tests/job_dagman_pre_skip-D_B.cmd
@@ -1,6 +1,6 @@
 executable = /bin/echo
-output = job_dagman_pre_skip-D_$(job).out
-error = job_dagman_pre_skip-D_$(job).err
+output = job_dagman_pre_skip-D_B.out
+error = job_dagman_pre_skip-D_B.err
 arguments = OK done with B
 universe = scheduler
 queue
