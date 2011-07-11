79595f67c369d6c5f4160b48dcbe9fc07b39e083
Chris Lalancette <clalance@redhat.com>
2011-07-11 18:28:44 -0500

    Bail out of GM_SAVE_INSTANCE_NAME if the job is HELD.
    A missing break statement meant that we would still try to
    do GM_SAVE_INSTANCE_NAME, even if the job was in HELD
    or REMOVED.  Fix that here.
    ===GT=== #2264

--- src/condor_gridmanager/dcloudjob.cpp
+++ src/condor_gridmanager/dcloudjob.cpp
@@ -433,6 +433,7 @@ void DCloudJob::doEvaluateState()
 
 				if ( (condorState == REMOVED) || (condorState == HELD) ) {
 					gmState = GM_DELETE;
+					break;
 				}
 
 				// Once RequestSubmit() is called at least once, you must
