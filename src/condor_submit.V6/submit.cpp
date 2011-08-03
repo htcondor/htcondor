00b64a263d92cb23a615c2928ac4c8813172f31e
Dan Bradley <dan@hep.wisc.edu>
2011-08-03 14:36:17 -0500

    Fixed a problem with overriding the default RequestMemory requirements.
    ===GT:Fixed=== #2348

--- src/condor_submit.V6/submit.cpp
+++ src/condor_submit.V6/submit.cpp
@@ -6181,7 +6181,6 @@ check_requirements( char const *orig, MyString &answer )
 				// If it wasn't 't', we must have found it...
 			if( *(aptr-1) == 't' || *(aptr-1) == 'T' ) {
 					// Must be RequestMemory, keep searching...
-				checks_reqmem = true;
 				continue;
 			}	
 		
@@ -6189,7 +6188,9 @@ check_requirements( char const *orig, MyString &answer )
 			break;
 		}
 	}
- 
+
+	checks_reqmem = findClause(answer,ATTR_REQUEST_MEMORY);
+
 	if( JobUniverse == CONDOR_UNIVERSE_JAVA ) {
 		if( answer[0] ) {
 			answer += " && ";
