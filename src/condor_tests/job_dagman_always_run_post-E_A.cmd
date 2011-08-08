13ffba584f2d84527b3655d77c2d219f2bda136f
Nathan W. Panike <nwp@cs.wisc.edu>
2011-08-08 16:36:02 -0500

    Add test job_dagman_always_run_post-E
    ===GT=== #2057
    We want to make sure that always running POST does not mess up RETRY

---
+++ src/condor_tests/job_dagman_always_run_post-E_A.cmd
@@ -0,0 +1,5 @@
+executable = /bin/bash
+output = E_A.out
+error = E_A.err
+arguments = "-c 'echo ''OK we are running''; exit 0'"
+queue
---
+++ src/condor_tests/job_dagman_always_run_post-E_A_post.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env sh
+echo "POST E_A running"
+exit 0
