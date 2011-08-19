d01e3aec3134189ce43e22531a857c17fc23b711
Kent Wenger <wenger@cs.wisc.edu>
2011-08-19 17:14:37 -0500

    Gittrac #2402: initial version of DAG in place, but the test script isn't yet -- I'm committing so I can cherry pick this over to V7_7-always_run_post_script_even_if_pre_script_fails-branch to test things there...

---
+++ src/condor_tests/job_dagman_retry-B-nodeA.cmd
@@ -0,0 +1,8 @@
+executable		= job_dagman_retry-B-nodeA.pl
+arguments		=
+universe		= scheduler
+notification	= NEVER
+output			= job_dagman_retry-B-nodeA.out
+error			= job_dagman_retry-B-nodeA.err
+log				= job_dagman_retry-B-nodeA.log
+queue
