5e37113476f5962d0e4115bd7791858cec6b549e
Peter Keller <psilord@cs.wisc.edu>
2011-09-21 14:49:50 -0500

    Fixed a single scheduler universe test to use a relative path for an output file to down and dirty test a file transfer codepath inside of Condor.

--- src/condor_tests/job_core_queue_sched.cmd
+++ src/condor_tests/job_core_queue_sched.cmd
@@ -2,7 +2,7 @@ universe   = scheduler
 executable = job_core_queue.pl
 log = job_core_queue_sched.log
 error = job_core_queue_sched.err
-output = job_core_queue_sched.out
+output = ./job_core_queue_sched.out
 Notification = NEVER
 arguments = hello
 queue 40
