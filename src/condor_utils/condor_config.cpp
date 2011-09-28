25a400cc29dd8ef6358fcfc60b029e60ed4079df
Matthew Farrellee <matt@redhat.com>
2011-09-28 06:33:48 -0400

    Added param_boolean_crufy, which allows for only checking the first char of a value instead of the full string

--- src/condor_utils/condor_config.cpp
+++ src/condor_utils/condor_config.cpp
@@ -1879,6 +1879,31 @@ param_double( const char *name, double default_value,
 }
 
 /*
+ * Like param_boolean, but allow for 'T' or 'F' (no quotes, case
+ * insensitive) to mean True/False, respectively.
+ */
+bool
+param_boolean_crufty( const char *name, bool default_value )
+{
+	char *tmp = param(name);
+	if (tmp) {
+		char c = *tmp;
+		free(tmp);
+
+		if ('t' == c || 'T' == c) {
+			return true;
+		} else if ('f' == c || 'F' == c) {
+			return false;
+		} else {
+			return param_boolean(name, default_value);
+		}
+	} else {
+		return param_boolean(name, default_value);
+	}
+}
+
+
+/*
 ** Return the boolean value associated with the named paramter.
 ** The parameter value is expected to be set to the string
 ** "TRUE" or "FALSE" (no quotes, case insensitive).
