22d09534dcbf0a522ffee65ea5792ee3716983fa
Kent Wenger <wenger@cs.wisc.edu>
2011-10-16 20:48:14 -0500

    Some cleanup of the DAG final node tests (the tests themselves, plus the actual functionality, still need fixes...).

--- src/condor_tests/job_dagman_final-A-nodeB.cmd
+++ src/condor_tests/job_dagman_final-A-nodeB.cmd
@@ -2,6 +2,6 @@ executable = ./job_dagman_final-A-node.pl
 output = job_dagman_final-A-nodeB.out
 error = job_dagman_final-A-nodeB.err
 log = job_dagman_final-A-nodeB.log
-arguments = "'B is done' 0"
+arguments = "'OK done with B' 0"
 universe = scheduler
 queue
