0dedc0095b8f4444aae196d104f998346e7aca16
Kent Wenger <wenger@cs.wisc.edu>
2011-12-05 18:29:30 -0600

    Gittrac #1482: got job_dagman_final-E working except that I think we should get a rescue DAG; minor cleanups to other tests.

--- src/condor_tests/job_dagman_final-E-nodeB1.cmd
+++ src/condor_tests/job_dagman_final-E-nodeB1.cmd
@@ -1,4 +1,4 @@
-executable = ./job_dagman_final-E-node.pl
+executable = ./job_dagman_final-E-nodeB1.pl
 output = job_dagman_final-E-nodeB1.out
 error = job_dagman_final-E-nodeB1.err
 arguments = "E_B1 0 $(DAG_STATUS) $(FAILED_COUNT)"
--- src/condor_tests/job_dagman_final-E-nodeB1.pl
+++ src/condor_tests/job_dagman_final-E-nodeB1.pl
@@ -8,7 +8,6 @@ system("echo '  DAG_STATUS=$ARGV[2]' >> $outfile");
 system("echo '  FAILED_COUNT=$ARGV[3]' >> $outfile");
 
 # Time for sibling node to run...
-#TEMPTEMP -- this doesn't seem to be working...
 sleep(600);
 
 system("echo '  OK done with $ARGV[0]' >> $outfile");
