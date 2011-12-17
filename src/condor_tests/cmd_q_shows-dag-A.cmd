79d9cef1561055f42e822ae8610cec0a34dbeeb9
Nathan W. Panike <nwp@cs.wisc.edu>
2011-12-17 13:46:27 -0600

    Make the output filenames longer to guard against collisions
    ===GT=== #1281

--- src/condor_tests/cmd_q_shows-dag-A.cmd
+++ src/condor_tests/cmd_q_shows-dag-A.cmd
@@ -1,7 +1,7 @@
 executable = cmd_q_shows-dag.sh
 universe = vanilla
 arguments = $(JOB) 120
-output = $(JOB).out
-error = $(JOB).err
+output = cmd_q_shows-dag-A.out
+error = cmd_q_shows-dag-A.out
 log = cmd_q_shows-dag.log
 queue
