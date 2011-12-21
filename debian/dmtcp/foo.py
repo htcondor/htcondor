8fb10444c643fd1702a133eed9fbe1cc312b29fc
Peter Keller <psilord@cs.wisc.edu>
2011-12-21 20:28:34 +0100

    Sync DMTCP support with version 0.4

--- debian/dmtcp/foo.py
+++ debian/dmtcp/foo.py
@@ -4,16 +4,31 @@
 import sys
 import time
 
+min_delay = 10
+
 runtime = int(sys.argv[1])
+if runtime < min_delay:
+	print 'Delay has a minimum of', min_delay, 'seconds'
+	runtime = min_delay
+
 outfile = open(sys.argv[2], 'w')
 
+print 'Dumping stdin:'
 outfile.write('Dumping stdin:\n')
+
 for line in sys.stdin:
-    print 'stdin: %s' % line
+    print 'stdin: %s' % line,
     outfile.write('stdin: %s' % line)
+
+print 'Doing work for', runtime, 'seconds'
 outfile.write('Doing work for %i seconds\n' % runtime)
+
 for t in xrange(runtime):
     print 'i = %i' % t
     outfile.write('i = %i\n' % t)
     time.sleep(1)
+
+print 'Done.'
 outfile.write('Done.\n')
+
+sys.exit(0);
