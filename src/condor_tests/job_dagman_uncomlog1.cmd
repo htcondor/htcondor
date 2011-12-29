95893a1abf4f58fc113127735a6a9a19089f11ac
Greg Thain <gthain@cs.wisc.edu>
2011-12-29 10:51:51 -0600

    Remove bogus test files

--- src/condor_tests/job_dagman_uncomlog1.cmd
+++
@@ -1,9 +0,0 @@
-Executable           	= ./job_dagman_uncomlog.pl
-Universe             	= vanilla
-log = job_dagman_uncomlog1.log
-Notification         	= NEVER
-getenv               	= true
-output = job_dagman_uncomlog1.out
-error = job_dagman_uncomlog1.err
-Queue
-
--- src/condor_tests/job_dagman_uncomlog4.cmd
+++
@@ -1,9 +0,0 @@
-Executable           	= ./job_dagman_uncomlog.pl
-Universe             	= vanilla
-log = job_dagman_uncomlog4.log
-Notification         	= NEVER
-getenv               	= true
-output = job_dagman_uncomlog4.out
-error = job_dagman_uncomlog4.err
-Queue
-
