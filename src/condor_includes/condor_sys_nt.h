1915ac2d61212e95e92403e1db3653e3f61e4fee
Alan De Smet <adesmet@cs.wisc.edu>
2012-01-04 17:45:03 -0600

    A key feature of an int32_t versus a uint32_t is that the former should probably be signed.

--- src/condor_includes/condor_sys_nt.h
+++ src/condor_includes/condor_sys_nt.h
@@ -86,7 +86,7 @@ typedef int socklen_t;
 typedef DWORD pid_t;
 typedef	unsigned __int16 uint16_t;
 typedef unsigned __int32 uint32_t;
-typedef unsigned __int32 int32_t;
+typedef __int32 int32_t;
 #define stat _fixed_windows_stat
 #define fstat _fixed_windows_fstat
 #define MAXPATHLEN 1024
