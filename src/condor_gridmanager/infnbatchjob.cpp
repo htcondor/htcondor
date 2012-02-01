0530a3d4f7742e0f2e7fe6bc43c628754c3a6905
Jaime Frey <jfrey@cs.wisc.edu>
2012-02-01 15:06:48 -0600

    Add INFNBatchResource class for blahp jobs in the gridmanager. #2421
    This gives us gridmanager ads and job submission throttles for blahp
    jobs.

--- src/condor_gridmanager/infnbatchjob.cpp
+++ src/condor_gridmanager/infnbatchjob.cpp
@@ -24,14 +24,10 @@
 #include "condor_debug.h"
 #include "condor_string.h"	// for strnewp and friends
 #include "condor_daemon_core.h"
-#include "spooled_job_files.h"
-#include "daemon.h"
-#include "dc_schedd.h"
-#include "setenv.h"
+#include "condor_config.h"
 
 #include "gridmanager.h"
 #include "infnbatchjob.h"
-#include "condor_config.h"
 
 
 // GridManager job states
@@ -147,7 +143,6 @@ INFNBatchJob::INFNBatchJob( ClassAd *classad )
 	std::string buff;
 	std::string error_string = "";
 	char *gahp_path;
-	MyString args_str;
 	ArgList gahp_args;
 
 	gahpAd = NULL;
@@ -160,6 +155,7 @@ INFNBatchJob::INFNBatchJob( ClassAd *classad )
 	remoteJobId = NULL;
 	lastPollTime = 0;
 	pollNow = false;
+	myResource = NULL;
 	gahp = NULL;
 	jobProxy = NULL;
 	remoteProxyExpireTime = 0;
@@ -220,12 +216,22 @@ INFNBatchJob::INFNBatchJob( ClassAd *classad )
 		}
 	}
 
-	gahp_args.GetArgsStringForDisplay( &args_str );
-	sprintf( buff, "%s/%s", batchType, args_str.Value() );
+	buff = batchType;
+	if ( gahp_args.Count() > 0 ) {
+		sprintf_cat( buff, "/%s", gahp_args.GetArg( 0 ) );
+	}
 	gahp = new GahpClient( buff.c_str(), gahp_path, &gahp_args );
 	free( gahp_path );
 
+	myResource = INFNBatchResource::FindOrCreateResource( batchType,
+														  gahp_args.GetArg(0) );
+	myResource->RegisterJob( this );
+	if ( remoteJobId ) {
+		myResource->AlreadySubmitted( this );
+	}
+
 		// Does this have to be lower-case for SetRemoteJobId()?
+
 	strlwr( batchType );
 
 	gahp->setNotificationTimerId( evaluateStateTid );
@@ -254,6 +260,9 @@ INFNBatchJob::~INFNBatchJob()
 {
 	if ( jobProxy != NULL ) {
 		ReleaseProxy( jobProxy, (TimerHandlercpp)&BaseJob::SetEvaluateState, this );
+	}
+	if ( myResource ) {
+		myResource->UnregisterJob( this );
 	}
 	if ( batchType != NULL ) {
 		free( batchType );
@@ -383,6 +392,13 @@ void INFNBatchJob::doEvaluateState()
 			// another one.
 			if ( now >= lastSubmitAttempt + submitInterval ) {
 				char *job_id_string = NULL;
+
+				// Once RequestSubmit() is called at least once, you must
+				// CancelRequest() once you're done with the request call
+				if ( myResource->RequestSubmit( this ) == false ) {
+					break;
+				}
+
 				if ( gahpAd == NULL ) {
 					gahpAd = buildSubmitAd();
 				}
@@ -390,6 +406,7 @@ void INFNBatchJob::doEvaluateState()
 					gmState = GM_HOLD;
 					break;
 				}
+
 				rc = gahp->blah_job_submit( gahpAd, &job_id_string );
 				if ( rc == GAHPCLIENT_COMMAND_NOT_SUBMITTED ||
 					 rc == GAHPCLIENT_COMMAND_PENDING ) {
@@ -411,6 +428,7 @@ void INFNBatchJob::doEvaluateState()
 							 procID.cluster, procID.proc,
 							 gahp->getErrorString() );
 					errorString = gahp->getErrorString();
+					myResource->CancelSubmit( this );
 					gmState = GM_UNSUBMITTED;
 					reevaluate_state = true;
 				}
@@ -548,6 +566,7 @@ void INFNBatchJob::doEvaluateState()
 				// cleared in GM_CLEAR_REQUEST (it might go to GM_HOLD first).
 				SetRemoteJobId( NULL );
 				requestScheddUpdate( this, false );
+				myResource->CancelSubmit( this );
 				gmState = GM_CLEAR_REQUEST;
 			}
 			} break;
@@ -573,6 +592,7 @@ void INFNBatchJob::doEvaluateState()
 					break;
 				}
 				SetRemoteJobId( NULL );
+				myResource->CancelSubmit( this );
 			}
 			if ( condorState == REMOVED ) {
 				gmState = GM_DELETE;
@@ -600,6 +620,7 @@ void INFNBatchJob::doEvaluateState()
 			}
 			errorString = "";
 			SetRemoteJobId( NULL );
+			myResource->CancelSubmit( this );
 			JobIdle();
 			if ( submitLogged ) {
 				JobEvicted();
@@ -800,7 +821,7 @@ void INFNBatchJob::ProcessRemoteAd( ClassAd *remote_ad )
 
 BaseResource *INFNBatchJob::GetResource()
 {
-	return (BaseResource *)NULL;
+	return (BaseResource *)myResource;
 }
 
 ClassAd *INFNBatchJob::buildSubmitAd()
