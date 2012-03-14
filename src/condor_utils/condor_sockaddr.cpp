41d6e1b9bf9f98163ffa6354e4effdfe72fd2404
Alan De Smet <adesmet@cs.wisc.edu>
2012-03-14 18:09:18 -0500

    Revert "Fix IPv4 private network tests. #2853" - Breaks some platforms on link
    This reverts commit 02a71a9536f770c544e0ba4d6ffc091085505d19.

--- src/condor_utils/condor_sockaddr.cpp
+++ src/condor_utils/condor_sockaddr.cpp
@@ -1,7 +1,6 @@
 #include "condor_common.h"
 #include "MyString.h"
 #include "condor_sockaddr.h"
-#include "condor_netaddr.h"
 #include "ipv6_hostname.h"
 
 condor_sockaddr condor_sockaddr::null;
@@ -408,18 +407,10 @@ bool condor_sockaddr::is_valid() const
 bool condor_sockaddr::is_private_network() const
 {
 	if (is_ipv4()) {
-		static bool initialized = false;
-		static condor_netaddr p10;
-		static condor_netaddr p172_16;
-		static condor_netaddr p192_168;
-		if(!initialized) {
-			p10.from_net_string("10.0.0.0/8");
-			p172_16.from_net_string("172.16.0.0/12");
-			p192_168.from_net_string("192.168.0.0/16");
-			initialized = true;
-		}
-
-		return p10.match(*this) || p172_16.match(*this) || p192_168.match(*this);
+		uint32_t ip = (uint32_t)v4.sin_addr.s_addr;
+		return ((ip & 0xFF000000) == 0x0A000000 ||      // 10/8
+				(ip & 0xFFF00000) == 0xAC100000 ||      // 172.16/12
+				(ip & 0xFFFF0000) == 0xC0A80000);       // 192.168/16
 	}
 	else if (is_ipv6()) {
 		return IN6_IS_ADDR_LINKLOCAL(&v6.sin6_addr);
