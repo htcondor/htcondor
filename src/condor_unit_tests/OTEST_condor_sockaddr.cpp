99d4bf9e47b71e70ea5017b44f9c7eecaf930c35
Alan De Smet <adesmet@cs.wisc.edu>
2012-03-14 15:03:50 -0500

    Initial condor_sockaddr tests

--- src/condor_unit_tests/OTEST_condor_sockaddr.cpp
+++ src/condor_unit_tests/OTEST_condor_sockaddr.cpp
@@ -27,6 +27,22 @@
 #include "emit.h"
 
 
+static bool sockaddr_equivalance(const condor_sockaddr & s,
+	MyString expected_ip_string, int expected_port) {
+
+	int port = s.get_port();
+	MyString ip_string = s.to_ip_string();
+
+	emit_output_expected_header();
+	emit_param("get_port()", "%d", expected_port);
+	emit_param("to_ip_string()", "%s", expected_ip_string.Value());
+	emit_output_actual_header();
+	emit_param("get_port()", "%d", port);
+	emit_param("to_ip_string()", "%s", ip_string.Value());
+
+	return (port == expected_port) && (ip_string == expected_ip_string);
+}
+
 static bool test_from_sinful_simple_v4() {
 	const char * sinful = "<1.2.3.4:567>";
 	const int expected_port = 567;
@@ -37,25 +53,18 @@ static bool test_from_sinful_simple_v4() {
 	emit_param("sinful", "%s", sinful);
 
 	condor_sockaddr s;
-	bool parsed = s.from_sinful("<1.2.3.4:567>");
-	int port = s.get_port();
-	MyString ip_string = s.to_ip_string();
+	bool parsed = s.from_sinful(sinful);
 
 	emit_output_expected_header();
 	emit_param("Was able to parse?", "%s", "yes");
-	emit_param("get_port()", "%d", expected_port);
-	emit_param("to_ip_string()", "%s", "1.2.3.4");
 	emit_output_actual_header();
 	emit_param("Was able to parse?", "%s", parsed?"yes":"NO");
-	emit_param("get_port()", "%d", port);
-	emit_param("to_ip_string()", "%s", ip_string.Value());
-
 	if(!parsed) FAIL;
-	if(port != expected_port) FAIL;
-	if(ip_string != expected_ip_string) FAIL;
+
+	bool match = sockaddr_equivalance(s, expected_ip_string, expected_port);
+	if(!match) FAIL;
 	PASS;
 }
-
 
 bool OTEST_condor_sockaddr() {
 	emit_object("condor_sockaddr");
