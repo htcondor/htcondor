13c1e5a290c010743384b4aae731fb0e53ac030d
Jaime Frey <jfrey@cs.wisc.edu>
2012-07-12 13:09:10 -0500

    Handle zeros properly in hashed directory names in ft-gahp. #3033

--- src/condor_ft-gahp/io_loop.cpp
+++ src/condor_ft-gahp/io_loop.cpp
@@ -833,7 +833,7 @@ define_sandbox_path(std::string sid, std::string &path)
 
 	char c_hex_md5[MD5_DIGEST_LENGTH*2+1];
 	for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
-		sprintf(&(c_hex_md5[i*2]), "%0x", hash_buf[i]);
+		sprintf(&(c_hex_md5[i*2]), "%02x", hash_buf[i]);
 	}
 	c_hex_md5[MD5_DIGEST_LENGTH*2] = 0;
 
