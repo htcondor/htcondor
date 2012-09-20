48fc027c69406169d33fd18a8c928c6957e1f0bf
Erik Erlandson <eerlands@redhat.com>
2012-09-20 16:38:02 -0700

    Provide a first class accounting group via submit files ===GT:Fixed=== #2728

--- src/condor_submit.V6/submit.cpp
+++ src/condor_submit.V6/submit.cpp
@@ -375,6 +375,10 @@ const char	*LoadProfile = "load_profile";
 // Concurrency Limit parameters
 const char    *ConcurrencyLimits = "concurrency_limits";
 
+// Accounting Group parameters
+const char* AcctGroup = "accounting_group";
+const char* AcctGroupUser = "accounting_group_user";
+
 //
 // VM universe Parameters
 //
@@ -527,6 +531,7 @@ void SetMaxJobRetirementTime();
 bool mightTransfer( int universe );
 bool isTrue( const char* attr );
 void SetConcurrencyLimits();
+void SetAccountingGroup();
 void SetVMParams();
 void SetVMRequirements();
 bool parse_vm_option(char *value, bool& onoff);
@@ -6533,6 +6538,7 @@ queue(int num)
 		SetJavaVMArgs();
 		SetParallelStartupScripts(); //JDB
 		SetConcurrencyLimits();
+        SetAccountingGroup();
 		SetVMParams();
 		SetLogNotes();
 		SetUserNotes();
@@ -8088,6 +8094,35 @@ SetConcurrencyLimits()
 		}
 	}
 }
+
+
+void SetAccountingGroup() {
+    // is a group setting in effect?
+    char* group = condor_param(AcctGroup);
+    if (NULL == group) return;
+
+    // look for the group user setting, or default to owner
+    std::string group_user;
+    char* gu = condor_param(AcctGroupUser);
+    if (NULL == gu) {
+        ASSERT(owner);
+        group_user = owner;
+    } else {
+        group_user = gu;
+        free(gu);
+    }
+
+    // set attributes AcctGroup, AcctGroupUser and AccountingGroup on the job ad:
+    std::string assign;
+    formatstr(assign, "%s = \"%s.%s\"", ATTR_ACCOUNTING_GROUP, group, group_user.c_str()); 
+    InsertJobExpr(assign.c_str());
+    formatstr(assign, "%s = \"%s\"", ATTR_ACCT_GROUP, group);
+    InsertJobExpr(assign.c_str());
+    formatstr(assign, "%s = \"%s\"", ATTR_ACCT_GROUP_USER, group_user.c_str());
+    InsertJobExpr(assign.c_str());
+    free(group);
+}
+
 
 // this function must be called after SetUniverse
 void
