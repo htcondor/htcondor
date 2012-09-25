4534fca34ac41bad183d9e82f67b80b72d3553fa
Brian Bockelman <bbockelm@cse.unl.edu>
2012-09-25 09:22:59 -0500

    More netfilter comments.

--- src/condor_utils/network_netfilter.c
+++ src/condor_utils/network_netfilter.c
@@ -12,6 +12,8 @@
 #include <stdlib.h>
 
 // Names are taken from libiptc.c
+// TODO: this is going to be a maintenance nightmare!  I'm assuming this is
+// part of the kernel's stable interface.
 static const char *hooknames[] = {
 	[NF_IP_PRE_ROUTING]      = "PREROUTING",
 	[NF_IP_LOCAL_IN]         = "INPUT",
@@ -20,18 +22,24 @@ static const char *hooknames[] = {
 	[NF_IP_POST_ROUTING]     = "POSTROUTING",
 };
 
-static int find_rules(struct ipt_entry * entry, int (*match_fcn)(const unsigned char *, long long, void *), void * callback_data) {
+/**
+ * Parse an iptables rule, looking for the comments and the number of bytes which matched.
+ */
+static int parse_rule(struct ipt_entry * entry, int (*match_fcn)(const unsigned char *, long long, void *), void * callback_data) {
 
 	unsigned char *elems = entry->elems;
 	
 	size_t idx = 1, offset = 0;
 	//printf("Target offset: %d\n", entry->target_offset);
 	struct xt_entry_match * match = NULL;
+	// The rule is composed of multiple matches.  We iterate through all
+	// of them until we find the comment.
 	while (offset < entry->target_offset) {
 		match = (struct xt_entry_match *)(elems + offset);
 		if (strcmp(match->u.user.name, "comment") == 0) {
 			match_fcn(match->data, entry->counters.bcnt, callback_data);
 		}
+		// End of the matches is detected by a zero-sized match->u.match_size
 		size_t next_jump = match->u.match_size;
 		if (next_jump)
 			offset += next_jump;
@@ -87,17 +95,25 @@ int perform_accounting(const char * chain, int (*match_fcn)(const unsigned char 
 	strcpy(entries->name, TABLE_NAME);
 	entries->size = info.size;
 
-	// Finally, read out the firewall.
+	// Read out the firewall from the kernel
 	if (getsockopt(sockfd, IPPROTO_IP, IPT_SO_GET_ENTRIES, entries, &info_size) < 0) {
 		fprintf(stderr, "Unable to get table entries. (errno=%d) %s\n", errno, strerror(errno));
 		return errno;
 	}
 
+	// Iterate through the table binary blob, searching for the desired chain
 	size_t offset = 0, idx = 1;
+	// The entry pointer will point at the currne rule, and hold the
+	// offset of the next rule - we will iterate through this like a linked-list.
 	struct ipt_entry * entry = entries->entrytable;
 	int hook_idx;
 	const unsigned char * chain_name = NULL, *old_chain_name = NULL;
+	// Note that the table is a list of rules, grouped into chains.  We will iterate through
+	// the rules, taking special notice when the chain name changes.
 	while (1) {
+		// There are two types of chains - user-defined and built-in.
+		// The user-defined ones match the if statement below, while the
+		// built-in ones have offsets as specified in the "info" object.
 		struct xt_entry_target *my_target = ipt_get_target(entry);
 		if (strcmp(my_target->u.user.name, XT_ERROR_TARGET) == 0) {
 			// User chain
@@ -106,25 +122,29 @@ int perform_accounting(const char * chain, int (*match_fcn)(const unsigned char 
 				break;
 			}
 		} else {
-			// See if this is start of a new chain
+			// See if this is start of a built-in chain.
 			for (hook_idx=0; hook_idx < NF_IP_NUMHOOKS; hook_idx++) {
-				if (info.valid_hooks & (1 << hook_idx)) {
-					if  (info.hook_entry[hook_idx] == offset) {
+				if (info.valid_hooks & (1 << hook_idx)) { // Make sure this hook is valid, AND
+					if  (info.hook_entry[hook_idx] == offset) { // Make sure the offset matches.
+						// If true, then we look up the name in the table.
 						chain_name = (const unsigned char *)hooknames[hook_idx];
 					}
 				}
 			}
 		}
 
+		// We will parse each rule if we are in the correct chain *and* we have skipped past the header
+		// entry which denotes the start of the chain.
 		if ((chain_name == old_chain_name) && (strcmp((const char *)chain, (const char *)chain_name) == 0)) {
 			fprintf(stderr, "Entry %ld, offset %ld, chain %s\n", idx, offset, chain_name);
-			find_rules(entry, match_fcn, callback_data);
+			parse_rule(entry, match_fcn, callback_data);
 
 		} else {
 			old_chain_name = chain_name;
 		}
 
-		// Calculate the next entry.
+		// Calculate the beginning of the next rule.
+		// Make sure to detect the end of the chain.
 		if (entry->next_offset)
 			offset += entry->next_offset;
 		else
