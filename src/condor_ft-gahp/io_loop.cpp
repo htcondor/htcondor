7951e86d2f91a03ea3976e50f890bdcf5aa9b593
Jaime Frey <jfrey@cs.wisc.edu>
2012-09-27 12:01:51 -0500

    Fix win32 pipe code in the ft-gahp.

--- src/condor_ft-gahp/io_loop.cpp
+++ src/condor_ft-gahp/io_loop.cpp
@@ -101,6 +101,47 @@ read_from_pipe( int fd, char **msg )
 	}
 }
 
+#if defined(WIN32)
+int forwarding_pipe = -1;
+unsigned __stdcall pipe_forward_thread(void *)
+{
+	const int FORWARD_BUFFER_SIZE = 4096;
+	char buf[FORWARD_BUFFER_SIZE];
+	
+	// just copy everything from stdin to the forwarding pipe
+	while (true) {
+
+		// read from stdin
+		int bytes = read(0, buf, FORWARD_BUFFER_SIZE);
+		if (bytes == -1) {
+			dprintf(D_ALWAYS, "pipe_forward_thread: error reading from stdin\n");
+			daemonCore->Close_Pipe(forwarding_pipe);
+			return 1;
+		}
+
+		// close forwarding pipe and exit on EOF
+		if (bytes == 0) {
+			daemonCore->Close_Pipe(forwarding_pipe);
+			return 0;
+		}
+
+		// write to the forwarding pipe
+		char* ptr = buf;
+		while (bytes) {
+			int bytes_written = daemonCore->Write_Pipe(forwarding_pipe, ptr, bytes);
+			if (bytes_written == -1) {
+				dprintf(D_ALWAYS, "pipe_forward_thread: error writing to forwarding pipe\n");
+				daemonCore->Close_Pipe(forwarding_pipe);
+				return 1;
+			}
+			ptr += bytes_written;
+			bytes -= bytes_written;
+		}
+	}
+
+}
+#endif
+
 int
 default_reaper(Service *, int pid, int exit_status)
 {
@@ -115,7 +156,7 @@ main_init( int, char ** const)
 	dprintf(D_SECURITY | D_FULLDEBUG, "FT-GAHP IO thread\n");
 
 	int stdin_pipe = -1;
-#if defined(WIN32) && 0
+#if defined(WIN32)
 	// if our parent is not DaemonCore, then our assumption that
 	// the pipe we were passed in via stdin is overlapped-mode
 	// is probably wrong. we therefore create a new pipe with the
