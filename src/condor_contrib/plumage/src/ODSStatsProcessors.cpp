f0bfede3185b20462fe5099eed56cb0afa375410
Peter MacKinnon <pmackinn@redhat.com>
2012-10-16 13:45:38 -0400

    Plumage contrib - linkage fix; history script enhancements; misc.

--- src/condor_contrib/plumage/src/ODSStatsProcessors.cpp
+++ src/condor_contrib/plumage/src/ODSStatsProcessors.cpp
@@ -27,7 +27,7 @@
 #include "stat_wrapper.h"
 
 // local includes
-#include "ODSProcessors.h"
+#include "ODSStatsProcessors.h"
 #include "ODSDBNames.h"
 #include "ODSUtils.h"
 #include "ODSHistoryFile.h"
@@ -36,6 +36,7 @@ using namespace std;
 using namespace compat_classad;
 using namespace mongo;
 using namespace plumage::etl;
+using namespace plumage::stats;
 using namespace plumage::util;
 
 // helpers, note expected bob & p vars
@@ -48,7 +49,7 @@ if (strcmp(X,"")) bob.append(#X,X);
 #define DATE(X,Y) bob.appendDate(#X,Y);
 
 void
-plumage::etl::processSubmitterStats(ODSMongodbOps* ops, Date_t& ts) {
+plumage::stats::processSubmitterStats(ODSMongodbOps* ops, Date_t& ts) {
     dprintf(D_FULLDEBUG, "ODSCollectorPlugin::processSubmitterStats called...\n");
     DBClientConnection* conn =  ops->m_db_conn;
     conn->ensureIndex(DB_RAW_ADS, BSON( ATTR_MY_TYPE << 1 ));
@@ -72,7 +73,7 @@ plumage::etl::processSubmitterStats(ODSMongodbOps* ops, Date_t& ts) {
 }
 
 void
-plumage::etl::processMachineStats(ODSMongodbOps* ops, Date_t& ts) {
+plumage::stats::processMachineStats(ODSMongodbOps* ops, Date_t& ts) {
     dprintf(D_FULLDEBUG, "ODSCollectorPlugin::processMachineStats() called...\n");
     DBClientConnection* conn =  ops->m_db_conn;
     conn->ensureIndex(DB_RAW_ADS, BSON( ATTR_MY_TYPE << 1 ));
@@ -104,7 +105,7 @@ plumage::etl::processMachineStats(ODSMongodbOps* ops, Date_t& ts) {
 }
 
 void
-plumage::etl::processSchedulerStats(ODSMongodbOps* ops, Date_t& ts) {
+plumage::stats::processSchedulerStats(ODSMongodbOps* ops, Date_t& ts) {
     dprintf(D_FULLDEBUG, "ODSCollectorPlugin::processSchedulerStats() called...\n");
     DBClientConnection* conn =  ops->m_db_conn;
     conn->ensureIndex(DB_RAW_ADS, BSON( ATTR_MY_TYPE << 1 ));
@@ -135,7 +136,7 @@ plumage::etl::processSchedulerStats(ODSMongodbOps* ops, Date_t& ts) {
 
 // liberally cribbed from user_prio.cpp
 void 
-plumage::etl::processAccountantStats(ClassAd* ad, ODSMongodbOps* ops, Date_t& ts)
+plumage::stats::processAccountantStats(ClassAd* ad, ODSMongodbOps* ops, Date_t& ts)
 {
     // attr%d holders...sadly reverting back to MyString for convenience of sprintf
     MyString  attrName, attrPrio, attrResUsed, attrWtResUsed, attrFactor, attrBeginUsage, attrAccUsage;
@@ -226,122 +227,4 @@ plumage::etl::processAccountantStats(ClassAd* ad, ODSMongodbOps* ops, Date_t& ts
         conn->insert(DB_STATS_SAMPLES_ACCOUNTANT,bob.obj());
     }
     
-}
-
-typedef set<long unsigned int> HistoryFileListType;
-static HistoryFileListType m_historyFiles;
-MyString m_path;
-
-// force a reset of history processing
-void plumage::etl::initHistoryFiles() {
-    m_historyFiles.clear();
-    processHistoryDirectory();
-    processCurrentHistory(true);
-}
-
-/**
- * Process the history directory and maintain the history file map
- *
- * Only handle rotated history files. 
- * For each one that is not in the history file map, create a
- * new HistoryFile, poll it for entries to process, and add it to the
- * map.
- */
-void
-plumage::etl::processHistoryDirectory()
-{
-    const char *file = NULL;
-
-    Directory dir ( m_path.Value() );
-    dir.Rewind();
-    while ( ( file = dir.Next() ) )
-    {
-        // Skip all non-history files, e.g. not history.<datestamp>
-        if ( strncmp ( file, "history.", 8 ) ) {
-            continue;
-        }
-
-        ODSHistoryFile h_file ( ( m_path + DIR_DELIM_STRING + file ).Value() );
-        CondorError errstack;
-        if ( !h_file.init ( errstack ) )
-        {
-            dprintf ( D_ALWAYS, "%s\n", errstack.getFullText() );
-            return;
-        }
-        errstack.clear();
-
-        long unsigned int id;
-        ASSERT ( h_file.getId ( id ) );
-        HistoryFileListType::iterator entry = m_historyFiles.find ( id );
-        if ( m_historyFiles.end() == entry )
-        {
-            h_file.poll ( errstack );
-            m_historyFiles.insert ( id );
-        }
-    }
-}
-
-/**
- * Process the current history file.
- *
- * 1) check to see if it is properly initialized, recording id (inode)
- * 2) stat the current history file
- * 3) poll for new entries and process them
- * 4) detect rotations
- */
-void
-plumage::etl::processCurrentHistory(bool force_reset)
-{
-    static MyString currentHistoryFilename = m_path + DIR_DELIM_STRING + "history";
-    static ODSHistoryFile currentHistory ( currentHistoryFilename.Value() );
-
-    CondorError errstack;
-
-    if (force_reset) {
-       currentHistory.cleanup();
-    }
-
-    // (1)
-    long unsigned int id;
-    if ( !currentHistory.getId ( id ) || force_reset)
-    {
-        if ( !currentHistory.init ( errstack ) )
-        {
-            dprintf ( D_ALWAYS, "%s\n", errstack.getFullText() );
-            return;
-        }
-        ASSERT ( currentHistory.getId ( id ) );
-        m_historyFiles.insert ( id );
-    }
-
-    // (2)
-    // Stat before poll to handle race of: poll + write + rotate + stat
-    StatWrapper stat_wrapper;
-    if ( stat_wrapper.Stat ( currentHistoryFilename ) )
-    {
-        dprintf ( D_ALWAYS, "Failed to stat %s: %d (%s)\n",
-                  currentHistoryFilename.Value(),
-                  stat_wrapper.GetErrno(), strerror ( stat_wrapper.GetErrno() ) );
-        return;
-    }
-    const StatStructType *stat = stat_wrapper.GetBuf();
-    ASSERT ( currentHistory.getId ( id ) );
-
-    // (3)
-    errstack.clear();
-    currentHistory.poll ( errstack );
-
-    // (4)
-    // If different the file has rotated
-    if ( id != stat->st_ino )
-    {
-        currentHistory = ODSHistoryFile ( currentHistoryFilename.Value() );
-        if ( !currentHistory.init ( errstack ) )
-        {
-            dprintf ( D_ALWAYS, "%s\n", errstack.getFullText() );
-            return;
-        }
-        ASSERT ( currentHistory.getId ( id ) );
-        m_historyFiles.insert ( id );
-    }
 }
