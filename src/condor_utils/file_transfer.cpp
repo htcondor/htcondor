ae6ebf99c4a14434e3258412e436db27d19e54b1
Dan Bradley <dan@hep.wisc.edu>
2012-10-18 17:18:15 -0500

    Fixed DELEGATE_JOB_GSI_CREDENTIALS_REFRESH, which had no effect. #3282

--- src/condor_utils/file_transfer.cpp
+++ src/condor_utils/file_transfer.cpp
@@ -4196,7 +4196,7 @@ GetDelegatedProxyRenewalTime(time_t expiration_time)
 
 	time_t now = time(NULL);
 	time_t lifetime = expiration_time - now;
-	double lifetime_frac = param_double( "DELEGATE_JOB_GSI_CREDENTIALS_RENEWAL", 0.25,0,1);
+	double lifetime_frac = param_double( "DELEGATE_JOB_GSI_CREDENTIALS_REFRESH", 0.25,0,1);
 	return now + (time_t)floor(lifetime*lifetime_frac);
 }
 
