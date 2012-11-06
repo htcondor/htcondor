23947dabb5ef7236470cd358200a2e6d5b0a1024
Derek Weitzel <djw8605@gmail.com>
2012-11-06 12:15:52 -0600

    updating glidein_jobs for campus factory

--- src/condor_contrib/campus_factory/share/glidein_jobs/glidein_wrapper.sh
+++ src/condor_contrib/campus_factory/share/glidein_jobs/glidein_wrapper.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/sh 
 
-campus_factory_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
+starting_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+eval campus_factory_dir=$_campusfactory_CAMPUSFACTORY_LOCATION
 
 # Make the temporary directory
 if [ ! -d $_campusfactory_wntmp ]
@@ -14,13 +14,13 @@ cd $local_dir
 
 # Copy the exec tar file
 cp $campus_factory_dir/glideinExec.tar.gz $local_dir
-cp $campus_factory_dir/passwdfile $local_dir
+cp $starting_dir/passwdfile $local_dir
 
 # Untar the executables
 tar xzf $campus_factory_dir/glideinExec.tar.gz
 
 # All late-binding configurations
-export CONDOR_CONFIG=$campus_factory_dir/glidein_condor_config
+export CONDOR_CONFIG=$starting_dir/glidein_condor_config
 export _condor_LOCAL_DIR=$local_dir
 export _condor_SBIN=$local_dir/glideinExec
 export _condor_LIB=$local_dir/glideinExec
@@ -29,7 +29,7 @@ export LD_LIBRARY_PATH=$_condor_LIB
 
 if [ -e `pwd`/user_job_wrapper.sh ]
 then
-export _condor_USER_JOB_WRAPPER=$campus_factory_dir/user_job_wrapper.sh
+export _condor_USER_JOB_WRAPPER=`pwd`/user_job_wrapper.sh
 fi
 
 ./glideinExec/glidein_startup -dyn -f -r 1200
