011459ef3a0174b2bbdecaf82456d40f5c47322f
Zach Miller <zmiller@cs.wisc.edu>
2012-11-15 15:12:27 -0600

    remove debugging info and clarify comments

--- src/condor_starter.V6.1/baseStarter.cpp
+++ src/condor_starter.V6.1/baseStarter.cpp
@@ -1385,16 +1385,12 @@ CStarter::createTempExecuteDir( void )
 
 	CondorPrivSepHelper* cpsh = condorPrivSepHelper();
 	if (cpsh != NULL) {
-		// ZKM TODO FIX
-		// the privsep switchboard ALWAYS creates directories with
-		// permissions 0755.  that change goes deeper than anything
-		// i want to do in this first round of fixes.
-		dprintf( D_ALWAYS, "ZKM: right now: initialize_sandbox(%s)\n", WorkingDir.Value());
+		// the privsep switchboard now ALWAYS creates directories with
+		// permissions 0700.
 		cpsh->initialize_sandbox(WorkingDir.Value());
 		WriteAdFiles();
 	} else {
 		// we can only get here if we are not using PrivSep.
-		dprintf( D_ALWAYS, "ZKM: right now: mkdir(%s, 0700)\n", WorkingDir.Value());
 		if( mkdir(WorkingDir.Value(), 0700) < 0 ) {
 			dprintf( D_FAILURE|D_ALWAYS,
 			         "couldn't create dir %s: %s\n",
