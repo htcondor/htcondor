d21254deb81be1388205652efc118e02ce0ab696
Kent Wenger <wenger@cs.wisc.edu>
2012-12-07 11:31:21 -0600

    Gittrac #3279: created new job_negotiator_restart test that makes sure user priority factor change survives across a Condor restart.

---
+++ src/condor_tests/job_negotiator_restart-nodeB.cmd
@@ -0,0 +1,8 @@
+executable = job_negotiator_restart-nodeB.pl
+universe = scheduler
+getenv = true
+output = job_negotiator_restart-nodeB.out
+error = job_negotiator_restart-nodeB.err
+log = job_negotiator_restart-nodeB.log
+notification = NEVER
+queue
