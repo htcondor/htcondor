dc0d239308a051499ca131f9237000cbc129e0e6
Kent Wenger <wenger@cs.wisc.edu>
2013-02-04 16:17:08 -0600

    Gittrac #3334: added an explicitly-defined log file using a macro in the file name to the job_dagman_default_log test, to make sure that doesn't goof things up (because you should be able to have a macro in the user-specified log file name if you're using the workflow log file for DAGMan).

--- src/condor_tests/job_dagman_default_log-subdir/job_dagman_default_log-nodeA.cmd
+++ src/condor_tests/job_dagman_default_log-subdir/job_dagman_default_log-nodeA.cmd
@@ -2,6 +2,9 @@ executable   = ./job_dagman_default_log-nodeA.pl
 universe     = scheduler
 output       = job_dagman_default_log-nodeA.out
 error        = job_dagman_default_log-nodeA.err
-# No log file specified; default should be used.
+# Adding this to make sure that a macro in the log file name
+# doesn't goof things up (even in recovery mode) when we're using the
+# workflow log.
+log          = $(LogFile)
 Notification = NEVER
 queue
