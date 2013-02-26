d8d50b6b0e5bcf83dddf9da638b9f3b69a8741c8
Kent Wenger <wenger@cs.wisc.edu>
2013-02-26 10:22:25 -0600

    Gittrac #3512: added job_starter_script-A test.

---
+++ src/condor_tests/job_starter_script-A-pre.pl
@@ -0,0 +1,11 @@
+#! /usr/bin/env perl
+
+$outfile = "job_starter_script-A.out2";
+
+open(OUT, ">>$outfile") or die "Can't open file $outfile";
+print OUT "Pre: ";
+foreach $arg (@ARGV) {
+	print OUT "<$arg> ";
+}
+print OUT "\n";
+close(OUT);
