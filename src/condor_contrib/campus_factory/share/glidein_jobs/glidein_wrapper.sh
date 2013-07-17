a26b1303651c5081e880ff8ee4db56d3b5590d8e
Marco Mambelli <marco@hep.uchicago.edu>
2013-07-17 14:56:59 -0500

    Fixed wrong reference to user_job_wrapper.sh in Campus Factory.
    From OSG Jira CAMPUS-125

--- src/condor_contrib/campus_factory/share/glidein_jobs/glidein_wrapper.sh
+++ src/condor_contrib/campus_factory/share/glidein_jobs/glidein_wrapper.sh
@@ -1,12 +1,11 @@
 #!/bin/sh 
 
 starting_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+eval campus_factory_dir=$_campusfactory_CAMPUSFACTORY_LOCATION
 
 # BLAHP does weird things with home directory
 unset HOME
 export HOME
-
-eval campus_factory_dir=$_campusfactory_CAMPUSFACTORY_LOCATION
 
 # Make the temporary directory
 if [ ! -d $_campusfactory_wntmp ]
@@ -31,6 +30,12 @@ export _condor_SBIN=$local_dir/glideinExec
 export _condor_LIB=$local_dir/glideinExec
 
 export LD_LIBRARY_PATH=$_condor_LIB
+
+# Copy the user job wrapper
+if [ -e $starting_dir/user_job_wrapper.sh ]
+then
+cp $starting_dir/user_job_wrapper.sh `pwd`
+fi
 
 if [ -e `pwd`/user_job_wrapper.sh ]
 then
