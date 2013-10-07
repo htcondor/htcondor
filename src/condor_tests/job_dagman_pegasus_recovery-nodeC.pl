505f14776925f8cf3cced7d70d28414595a8b012
Kent Wenger <wenger@cs.wisc.edu>
2013-10-07 16:45:21 -0500

    Gittrac #3890:  A little more cleanup of the test.

--- src/condor_tests/job_dagman_pegasus_recovery-nodeC.pl
+++ src/condor_tests/job_dagman_pegasus_recovery-nodeC.pl
@@ -6,6 +6,8 @@ while (<OUTPUT>) {
 }
 close (OUTPUT) or die "Condor_hold failed: $?";
 
+# Make sure DAGMan can't read the individual node job log files
+# when it goes into recovery mode.
 unlink "job_dagman_pegasus_recovery-nodeA.log" or die "Unlink failed: $!\n";
 unlink "job_dagman_pegasus_recovery-nodeB.log" or die "Unlink failed: $!\n";
 
