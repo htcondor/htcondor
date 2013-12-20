1d8afc7cf5f6c38ff0f767cf0e83250974362e5b
Kent Wenger <wenger@cs.wisc.edu>
2013-12-20 10:28:14 -0600

    Gittrac #4102:  Added job_dagman_prio_in_rescue test for this problem.

---
+++ src/condor_tests/job_dagman_prio_in_rescue-node.cmd
@@ -0,0 +1,4 @@
+executable = /bin/true
+output = job_dagman_prio_in_rescue-node.$(cluster).out
+error = job_dagman_prio_in_rescue-node.$(cluster).err
+queue
---
+++ src/condor_tests/job_dagman_prio_in_rescue-nodeE.cmd
@@ -0,0 +1,4 @@
+executable = /bin/false
+output = job_dagman_prio_in_rescue-node.$(cluster).out
+error = job_dagman_prio_in_rescue-node.$(cluster).err
+queue
