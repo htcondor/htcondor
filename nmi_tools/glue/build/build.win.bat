f052127c7fde2ee74f65785c72ad0b3b1414513f
Ziliang Guo <ziliang@cs.wisc.edu>
2013-12-20 10:35:54 -0600

    Copy yet more files into condor_tests to accommodate unit tests. All except consumption should now pass. Ticket #4022

--- nmi_tools/glue/build/build.win.bat
+++ nmi_tools/glue/build/build.win.bat
@@ -257,6 +257,8 @@ move src\condor_utils\tests\RelWithDebInfo\*.exe src\condor_tests
 move src\condor_utils\tests\RelWithDebInfo\*.pdb src\condor_tests
 move src\condor_collector.V6\tests\RelWithDebInfo\*.exe src\condor_tests
 move src\condor_collector.V6\tests\RelWithDebInfo\*.pdb src\condor_tests
+copy bld_external\pcre-8.33\install\lib\pcre.dll src\condor_tests
+copy src\classad\tests\testdata.txt src\condor_tests
 goto finis
 
 REM common exit
