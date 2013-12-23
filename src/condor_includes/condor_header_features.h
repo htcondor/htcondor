4400b5d8254304abf6f1b68b6d3f0697a3f5ee46
John (TJ) Knoeller <johnkn@cs.wisc.edu>
2013-12-23 11:01:30 -0600

    GCC_NORETURN needs a definition for non-gcc platforms to avoid compile errors. ===VersionHistory:None===

--- src/condor_includes/condor_header_features.h
+++ src/condor_includes/condor_header_features.h
@@ -78,6 +78,8 @@ b - Argument position of the "..."
 
 #if defined(__GNUC__)
 # define GCC_NORETURN __attribute__((__noreturn__))
+#else
+# define GCC_NORETURN
 #endif
 
 #endif /* CONDOR_SYS_FEATURES_H */
