1b1d402662530e042b3ed7beb14a9921c22ac2d0
Todd L Miller <tlmiller@cs.wisc.edu>
2014-01-02 16:12:39 -0600

    (#4113) Request a ping of the resource when a command fails with E_CURL_IO.

--- src/condor_gridmanager/ec2job.cpp
+++ src/condor_gridmanager/ec2job.cpp
@@ -693,6 +693,7 @@ void EC2Job::doEvaluateState()
 						m_keypair_created = true;
 						gmState = gmTargetState;
 					} else {
+						if( gahp_error_code == "E_CURL_IO" ) { myResource->RequestPing( this ); }
 
 						// May need to add back retry logic, but why?
 						errorString = gahp->getErrorString();
@@ -800,6 +801,8 @@ void EC2Job::doEvaluateState()
 						daemonCore->Reset_Timer( evaluateStateTid,
 												 submitInterval );
 					} else {
+						if( gahp_error_code == "E_CURL_IO" ) { myResource->RequestPing( this ); }
+
 						errorString = gahp->getErrorString();
 						dprintf(D_ALWAYS,"(%d.%d) job submit failed: %s: %s\n",
 								procID.cluster, procID.proc,
@@ -1217,6 +1220,7 @@ void EC2Job::doEvaluateState()
 							strstr( errorString.c_str(), "InvalidInstanceID.NotFound" ) ) {
 							remoteJobState = EC2_VM_STATE_PURGED;
 						} else {
+							if( gahp_error_code == "E_CURL_IO" ) { myResource->RequestPing( this ); }
 							dprintf( D_ALWAYS, "(%d.%d) job cancel failed: %s: %s\n",
 									 procID.cluster, procID.proc,
 									 gahp_error_code.c_str(),
@@ -1373,6 +1377,7 @@ void EC2Job::doEvaluateState()
 						// deleted the keypair ID yet, it'll still be in
 						// the job ad...
 						if( rc != 0 ) {
+							if( gahp_error_code == "E_CURL_IO" ) { myResource->RequestPing( this ); }
 							errorString = gahp->getErrorString();
 							dprintf( D_ALWAYS, "(%d.%d) job destroy keypair (%s) failed: %s: %s\n",
 									 procID.cluster, procID.proc, m_key_pair.c_str(),
@@ -1459,6 +1464,7 @@ void EC2Job::doEvaluateState()
 					gmState = GM_SPOT_SUBMITTED;
 					break;
 				} else {
+					if( gahp_error_code == "E_CURL_IO" ) { myResource->RequestPing( this ); }
 					errorString = gahp->getErrorString();
 					dprintf( D_ALWAYS, "(%d.%d) spot instance request failed: %s: %s\n",
 								procID.cluster, procID.proc,
@@ -1501,6 +1507,7 @@ void EC2Job::doEvaluateState()
 					}
 
 					if( rc != 0 ) {
+						if( gahp_error_code == "E_CURL_IO" ) { myResource->RequestPing( this ); }
 						errorString = gahp->getErrorString();
 						dprintf( D_ALWAYS, "(%d.%d) spot request stop failed: %s: %s\n",
 									procID.cluster, procID.proc,
@@ -1586,6 +1593,7 @@ void EC2Job::doEvaluateState()
 
 				// If the command fails, put the job on hold.
 				if( rc != 0 ) {
+					if( gahp_error_code == "E_CURL_IO" ) { myResource->RequestPing( this ); }
 					errorString = gahp->getErrorString();
 					dprintf( D_ALWAYS, "(%d.%d) spot request probe failed: %s: %s\n",
 								procID.cluster, procID.proc,
@@ -2104,6 +2112,7 @@ void EC2Job::associate_n_attach()
 			if ((condorState == REMOVED) || (condorState == HELD))
 				gmState = GM_DELETE;
 		default:
+			if( gahp_error_code == "E_CURL_IO" ) { myResource->RequestPing( this ); }
 			dprintf(D_ALWAYS,
 					"Failed ec2_create_tags returned %s continuing w/job\n",
 					gahp_error_code.c_str());
@@ -2134,6 +2143,7 @@ void EC2Job::associate_n_attach()
 				if ( (condorState == REMOVED) || (condorState == HELD) )
 					gmState = GM_DELETE;
 			default:
+				if( gahp_error_code == "E_CURL_IO" ) { myResource->RequestPing( this ); }
 				dprintf(D_ALWAYS,
 						"Failed ec2_associate_address returned %s continuing w/job\n",
 						gahp_error_code.c_str());
@@ -2178,6 +2188,7 @@ void EC2Job::associate_n_attach()
 					if ( (condorState == REMOVED) || (condorState == HELD) )
 						gmState = GM_DELETE;
 				default:
+					if( gahp_error_code == "E_CURL_IO" ) { myResource->RequestPing( this ); }
 					bcontinue=false;
 					dprintf(D_ALWAYS,
 							"Failed ec2_attach_volume returned %s continuing w/job\n",
