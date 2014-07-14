765c9b27d4d1f4221ac4a256dfaecf85c09600bd
Todd Tannenbaum <tannenba@cs.wisc.edu>
2014-07-14 14:22:17 -0500

    Include value of job rank expression itself in autoclustering. #4403

--- src/condor_schedd.V6/autocluster.cpp
+++ src/condor_schedd.V6/autocluster.cpp
@@ -224,6 +224,7 @@ int AutoCluster::getAutoClusterid( ClassAd *job )
 		job->GetReferences(ATTR_RANK,internal_refs,external_refs);
 		internal_refs.remove_anycase(ATTR_CURRENT_TIME);	// never want this attr
 		internal_refs.append(ATTR_REQUIREMENTS);	// always want these attrs
+		internal_refs.append(ATTR_RANK);
 		internal_refs.append(ATTR_NICE_USER);
 		internal_refs.append(ATTR_CONCURRENCY_LIMITS);
 
