43955439b5731f9ceae2fb2ccef65fbad9129d03
Todd Miller <tlmiller@exec-9.batlab.org>
2014-07-28 17:01:33 -0500

    Added some implementation notes.

--- src/condor_starter.V6.1/docker_proc.cpp
+++ src/condor_starter.V6.1/docker_proc.cpp
@@ -5,6 +5,33 @@ DockerProc::DockerProc( ClassAd * jobAd ) : VanillaProc( jobAd ) {
 	// Ask Docker which cgroup it's going to use?
 	// Steal code from os_proc::StartJob() to do the stdio mangling
 	// ...
+
+	// We can run 'docker pull' to verify that it can find the image
+	// before we run 'docker run' to actually start it.  In fact, if
+	// we feel like being clever, we could have the startd use config
+	// to do the pull before we run a docker job at all, and advertise
+	// which ones we will know will work...
+
+	// To see currently-installed images, run 'docker images'.
+
+	// If we supply a name to 'docker run', we can use 'docker ps' to
+	// link the name to the (short) container ID.  We can then pass that
+	// to 'docker inspect' to get the (long) container ID, among other
+	// interesting bits.  The (long) container ID will let use grovel
+	// around in /sys/fs/cgroup if we need to (for usage information?).
+
+	// The 'docker run' command hangs around for the duration of the
+	// instance, which means we can probably hand its PID off to UserProc
+	// and/or OSProc like we hand off the VM GAHP's PID.
+
+	// We should be able to leverage the procd for usage-gathering if
+	// we're clever -- assuming the (long) container ID suffices for
+	// its cgroup magic.
+
+	// We should run with '-v /path/to/sandbox:/inner-path/to/sandbox'.
+	// .. and the IWD should probably be /inter-path/to/sandbox.  Use '-w'.
+
+	// TODO for laterz: figure out this self-hosting magic.
 }
 
 DockerProc::~DockerProc() { }
