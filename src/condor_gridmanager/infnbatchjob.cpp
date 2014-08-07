c7525d9e4ed3b0c12d4704e48f7dbc20a9fa3dff
Alan De Smet <adesmet@cs.wisc.edu>
2014-08-07 14:23:36 -0500

    Mark two hand-rolling sinful sites that must change soon

--- src/condor_gridmanager/infnbatchjob.cpp
+++ src/condor_gridmanager/infnbatchjob.cpp
@@ -558,6 +558,7 @@ void INFNBatchJob::doEvaluateState()
 				while ( *old_addr != '\0' && *old_addr != '?' && *old_addr != '>' ) {
 					old_addr++;
 				}
+				// TODO IPV6: Rolling your own sinfuls WILL break soon.
 				formatstr( new_addr, "<127.0.0.1:%d%s",
 						   m_xfer_gahp->getSshForwardPort(), old_addr );
 				gahpAd->Assign( ATTR_TRANSFER_SOCKET, new_addr );
@@ -814,6 +815,7 @@ void INFNBatchJob::doEvaluateState()
 				while ( *old_addr != '\0' && *old_addr != '?' && *old_addr != '>' ) {
 					old_addr++;
 				}
+				// TODO IPV6: Rolling your own sinfuls WILL break soon.
 				formatstr( new_addr, "<127.0.0.1:%d%s",
 						   m_xfer_gahp->getSshForwardPort(), old_addr );
 				gahpAd->Assign( ATTR_TRANSFER_SOCKET, new_addr );
