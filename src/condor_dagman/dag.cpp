7a13d0b06e19445493b93b720771b60129c0cc64
Kent Wenger <wenger@cs.wisc.edu>
2014-12-02 13:01:46 -0600

    Gittrac #4727:  Tentative fix for this, plus lots of temporary debug node (I need to do a bunch of thinking and testing to make sure the fix doesn't break anything else).

--- src/condor_dagman/dag.cpp
+++ src/condor_dagman/dag.cpp
@@ -351,6 +351,50 @@ bool Dag::Bootstrap (bool recovery)
 		_jobstateLog.WriteRecoveryFinished();
         debug_printf( DEBUG_NORMAL, "...done with RECOVERY mode "
 					"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n" );
+//TEMPTEMP -- I should also figure out why DAGMan didn't think the DAG was finished, even though all of the nodes were finished...
+// Do we need so much complexity to reproduce this?  Or maybe just hold the DAG while the final node is running and release it after the final node has finished? -- can I do that with a periodic release expression for DAGMan itself?
+// Should we write an event when all submit attempts have failed?
+//TEMPTEMP?>>>
+// I'm not sure if I really want to do this, but I want to see if it
+// fixes gittrac #4727.
+// Hmm -- maybe something pretty simple -- if the whole DAG isn't done, the final node can't be done...
+// What if failed node was submitted and failed, rather than not getting submitted? -- probably need to make a test for that...
+		if ( _final_job && _final_job->GetStatus() == Job::STATUS_DONE ) {
+#if 0 //TEMPTEMP
+			//TEMPTEMP -- this will cause problems if last non-final node finished while DAGMan was held...  Hmm -- maybe check if all non-final nodes are done?
+			_final_job->SetStatus( Job::STATUS_NOT_READY );
+			--_numNodesDone;
+        	debug_printf( DEBUG_NORMAL, "Set final node status to STATUS_NOT_READY\n" );
+#endif //TEMPTEMP
+#if 0 //TEMPTEMP
+			//TEMPTEMP -- I *really* don't think I want to do this, but I want to see what effect it has
+			_finalNodeRun = true;
+        	debug_printf( DEBUG_NORMAL, "Set _finalNodeRun to true\n" );
+#endif //TEMPTEMP
+#if 0 //TEMPTEMP
+			if ( !FinishedRunning( true ) ) {
+				--_numNodesDone;
+				//TEMPTEMP -- why is FinishedRunning true when NodeA is unready?!?
+				if ( FinishedRunning( false ) ) {
+					_final_job->SetStatus( Job::STATUS_READY );
+        			debug_printf( DEBUG_NORMAL, "Set final node status to STATUS_READY\n" );
+				} else {
+					_final_job->SetStatus( Job::STATUS_NOT_READY );
+        			debug_printf( DEBUG_NORMAL, "Set final node status to STATUS_NOT_READY\n" );
+				}
+			}
+#endif //TEMPTEMP
+
+#if 1 //TEMPTEMP
+			if ( !FinishedRunning( true ) ) {
+				--_numNodesDone;
+				//TEMPTEMP -- why is FinishedRunning true when NodeA is unready?!?
+				_final_job->SetStatus( Job::STATUS_NOT_READY );
+        		debug_printf( DEBUG_NORMAL, "Set final node status to STATUS_NOT_READY\n" );
+			}
+#endif //TEMPTEMP
+		}
+//<<<TEMPTEMP
 		print_status();
 
 		_recovery = false;
@@ -1991,21 +2035,27 @@ Dag::FinishedRunning( bool includeFinalNode ) const
 bool
 Dag::DoneSuccess( bool includeFinalNode ) const
 {
+debug_printf( DEBUG_NORMAL, "DIAG 1010\n" );//TEMPTEMP
 	if ( !FinishedRunning( includeFinalNode ) ) {
+debug_printf( DEBUG_NORMAL, "DIAG 1020\n" );//TEMPTEMP
 			// Note: if final node is running we should get to here...
 		return false;
 	} else if ( NumNodesDone( includeFinalNode ) ==
 				NumNodes( includeFinalNode ) ) {
+debug_printf( DEBUG_NORMAL, "DIAG 1030\n" );//TEMPTEMP
 			// This is the normal case.
 		return true;
 	} else if ( includeFinalNode && _final_job &&
 				_final_job->GetStatus() == Job::STATUS_DONE ) {
+debug_printf( DEBUG_NORMAL, "DIAG 1040\n" );//TEMPTEMP
 			// Final node can override the overall DAG status.
 		return true;
 	} else if ( _dagIsAborted && _dagStatus == DAG_STATUS_OK ) {
+debug_printf( DEBUG_NORMAL, "DIAG 1050\n" );//TEMPTEMP
 			// Abort-dag-on can override the overall DAG status.
 		return true;
 	}
+debug_printf( DEBUG_NORMAL, "DIAG 1060\n" );//TEMPTEMP
 
 	return false;
 }
