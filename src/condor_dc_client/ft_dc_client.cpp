f76443bc241d0576ba2e375ecee68a6ed39aae60
Carl Vuosalo <covuosalo@wisc.edu>
2014-12-23 10:17:02 -0600

    Test FT client that uses full FT:Init().

--- src/condor_dc_client/ft_dc_client.cpp
+++ src/condor_dc_client/ft_dc_client.cpp
@@ -18,17 +18,84 @@
  ***************************************************************/
 
  
+#include "condor_common.h"
 #include "daemon.h"
-#include "condor_md.h"
-#include "classad/classad.h"
+#include "condor_debug.h"
+#include "condor_daemon_core.h"
+#include "subsystem_info.h"
 #include "file_transfer.h"
 
-
-#include <map>
-#include <limits.h>
-#include <stdio.h>
-
 using namespace std;
+
+/*
+class DCCached : public Daemon
+{
+	DCCached() :
+	Daemon(DT_GENERIC)
+	{}
+	
+	~DCCached() {}
+	
+	bool CreateCacheDir(string &cacheDir, time_t &leaseExpiration,
+		string version);
+	bool RemoveCacheDir(string cacheDir, string version);
+	bool UpdateLease(string leaseID, time_t &leaseExpiration,
+		string version);
+};
+
+*/
+
+class CacheDaemon : public Service
+{
+
+  public:
+        // ctor/dtor
+        CacheDaemon( void ) {}
+        ~CacheDaemon( void ) {}
+
+         // Command handlers
+        int commandHandler_Test(int command, Stream *stream);
+        int reaper_handler(int pid, int exit_status);
+};
+
+// static ClassAd jobad;
+// static FileTransfer filetrans;
+
+
+int
+CacheDaemon::commandHandler_Test(int command, Stream *stream)
+{
+	dprintf(D_ALWAYS, "entered command handler = %d\n", command);
+	char *data = NULL;
+	int sendval = 59;
+	stream->decode();
+	stream->code(data);
+	stream->end_of_message();
+	stream->encode();
+	stream->code(&sendval);
+	stream->end_of_message();
+	dprintf(D_ALWAYS, "received data = %s\n", data);
+
+	typedef struct stat Stat;
+	Stat st;
+	const char *path = "/scratch/cvuosalo/receive";
+	 if (stat(path, &st) != 0) {
+        /* Directory does not exist. EEXIST for race condition */
+        if (mkdir(path, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) != 0 && errno != EEXIST)
+            dprintf(D_ALWAYS, "cannot create dir = %s\n", path);
+     }
+	char fname[500];
+	strcpy(fname, path);
+	strcat(fname, "/file.out");
+	int fdesc = open(fname, O_RDWR | O_CREAT, S_IRUSR | S_IRGRP | S_IROTH);
+	if (fdesc > -1)
+	{
+		write(fdesc, data, strlen(data));
+		close(fdesc);
+	}
+	return (TRUE);
+}
+
 
 typedef map<string, unsigned char *> stringMap;
 
@@ -40,7 +107,7 @@ void getJobAd(ClassAd *const jobad, stringMap *const hashmap)
 	char filelist[MAXLNLEN];
 	while (jobadln.readLine(stdin) && jobadln != "***") {
 		if (jobadln.size() > 1) {	// More than just newline
-			printf("line = %s.\n", jobadln.Value());
+			// printf("line = %s.\n", jobadln.Value());
 			if (jobadln[0] != '#' && jobad->Insert(jobadln.Value()) == false) {
 				printf("ClassAd Insert failed. Exiting.\n");
 				return;
@@ -49,19 +116,19 @@ void getJobAd(ClassAd *const jobad, stringMap *const hashmap)
 				if (sscanf(jobadln.Value(), "TransferInput = \"%s", filelist) == 1) {
 					if (filelist[strlen(filelist) - 1] == '"')
 						filelist[strlen(filelist) - 1] = '\0';
-					printf("filelist = %s.\n", filelist);
+					// printf("filelist = %s.\n", filelist);
 					char filenam[PATH_MAX + 1], restlist[MAXLNLEN];
 					restlist[0] = '\0';
 					while(sscanf(filelist, "%[^,],%s", filenam, restlist) == 2 ||
 						sscanf(filelist, "%[^,]", filenam) == 1) {
-						printf("file = %s, rest = %s.\n", filenam, restlist);
+						// printf("file = %s, rest = %s.\n", filenam, restlist);
 						strcpy(filelist, restlist);
 						restlist[0] = '\0';
 						unsigned char result[100];
 						memcpy(result,
 							Condor_MD_MAC::computeOnce((unsigned char *) filenam, strlen(filenam)),
 							17);
-						printf("output = ");
+						// printf("output = ");
 						for (int ind = 0; ind < 16; ++ind)
 							printf("%x", result[ind]);
 						printf("\n");
@@ -78,50 +145,120 @@ void getJobAd(ClassAd *const jobad, stringMap *const hashmap)
 }
 
 
-int main( int argc, char **argv )
+//-------------------------------------------------------------
+
+void usage(char* name)
 {
-	ReliSock sock;
-	if (argc != 2) {
-		printf("Need sinful string\n");
-		return (1);
-	} else printf("opening = %s \n", argv[1]);
-	sock.timeout(100);
-	int retval = sock.connect(argv[1]);
-	printf("connect = %d \n", retval);
-	if (retval != 1)
-		return (1);
+	dprintf(D_ALWAYS,"Usage: %s [-f] [-b] [-t] [-p <port>]\n",name );
+	exit( 1 );
+}
+
+//-------------------------------------------------------------
+
+int CacheDaemon::reaper_handler(int pid, int exit_status)
+{
+	if( WIFSIGNALED(exit_status) ) {
+	   dprintf( D_ALWAYS, "Unknown process exited, pid=%d, signal=%d\n", pid,
+						WTERMSIG(exit_status) );
+	} else {
+		dprintf( D_ALWAYS, "Unknown process exited, pid=%d, status=%d\n", pid,
+						WEXITSTATUS(exit_status) );
+	}
+	return TRUE;
+}
 	
-	ClassAd jobad;
+	
+void main_init(int argc, char *argv[])
+{
+	if(argc > 3)
+	{
+		usage(argv[0]);
+	}
+	dprintf(D_ALWAYS, "main_init() called\n");
+	static CacheDaemon service;
+	if (daemonCore != NULL) {
+		dprintf(D_ALWAYS, "register ret = %d\n",
+			daemonCore->Register_Command(53, "TEST_COMMAND",
+			(CommandHandlercpp)&CacheDaemon::commandHandler_Test,
+			"command_handler", (Service *) &service));
+	} else dprintf(D_ALWAYS, "null pointer\n");
+	
+	daemonCore->Register_Reaper("Reaper",
+        (ReaperHandlercpp)&CacheDaemon::reaper_handler, "Reaper",
+        (Service *) &service);
+
+	static ClassAd jobad;
 	stringMap hashmap;
 	getJobAd(&jobad, &hashmap);
+	string tkey;
+	MyString tkeyin = "TransferKey = \"";
+	if (argc >= 2) {
+		tkeyin += argv[1];
+		tkeyin += "\"";
+		dprintf(D_ALWAYS, "Adding transfer key %s\n", tkeyin.c_str());
+	} else {
+		dprintf(D_ALWAYS, "Need transfer key argument.\n");
+		return;
+	}
+	if (jobad.Insert(tkeyin.Value()) == false) {
+		printf("tkey Insert failed. Exiting.\n");
+		return;
+	}
+	
+	if (jobad.LookupString( ATTR_TRANSFER_KEY, tkey ))
+		dprintf(D_ALWAYS, "Transfer key = %s\n", tkey.c_str());
+	else dprintf(D_ALWAYS, "No transfer key\n");
 
-	/*
-	Daemon server(DT_GENERIC, argv[1]); 
-	printf("daemon state = %s \n", server.error());
-	printf("startCommand = %d \n", server.startCommand(53,
-		(Sock *) &s));
-	printf("daemon state = %s \n", server.error());
-*/
-
+	dprintf(D_ALWAYS, "Beginning FT:Init()\n");
 	FileTransfer filetrans;
-	if (filetrans.SimpleInit(&jobad, false, false, &sock) == 0) {
-		printf("Can't init filetrans.\n");
-		return (1);
+	if (filetrans.Init(&jobad, false, PRIV_USER, false) == 0) {
+		// Don't check perms, don't use file catalog.
+		dprintf(D_ALWAYS, "Can't init filetrans.\n");
+		return;
 	}
-	if (filetrans.UploadFiles(true, false) == 0) { // blocking, not final xfer
+	int retval;
+	if ((retval = filetrans.UploadFiles()) == 0) { // blocking
 		printf("Can't upload.\n");
-		return (1);
-	}
-/*
-	sock.encode();
-	printf("code = %d \n", sock.code(jobadsz));
-	printf("eom = %d \n", sock.end_of_message());
-	// Receive the result 
-	sock.decode();
-	printf("code = %d \n", sock.code(result));
-	printf("eom = %d \n", sock.end_of_message());
-	printf("Result = %d \n", result);
-*/
-	sock.close();
-	return (0);
+		return;
+	} else printf("Upload OK = %d\n", retval);
+	
+
+	// Need full init for shadow/starter
+}
+
+//-------------------------------------------------------------
+
+void main_config()
+{
+	dprintf(D_ALWAYS, "main_config() called\n");
+}
+
+//-------------------------------------------------------------
+
+void main_shutdown_fast()
+{
+	dprintf(D_ALWAYS, "main_shutdown_fast() called\n");
+	DC_Exit(0);
+}
+
+//-------------------------------------------------------------
+
+void main_shutdown_graceful()
+{
+	dprintf(D_ALWAYS, "main_shutdown_graceful() called\n");
+	DC_Exit(0);
+}
+
+
+int
+main( int argc, char **argv )
+{
+	set_mySubSystem("DAEMONCORECLIENT", SUBSYSTEM_TYPE_DAEMON );
+
+	dc_main_init = main_init;
+	dc_main_config = main_config;
+	dc_main_shutdown_fast = main_shutdown_fast;
+	dc_main_shutdown_graceful = main_shutdown_graceful;
+
+	return dc_main( argc, argv );
 }
