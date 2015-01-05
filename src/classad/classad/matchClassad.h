2f9dcd7a246df2a609f449b21b334ddef39d7c13
Brian Bockelman <bbockelm@cse.unl.edu>
2015-01-05 08:22:57 -0600

    Allow the MatchClassAdDoesNotOwn understand non-strict eval. #2640

--- src/classad/classad/matchClassad.h
+++ src/classad/classad/matchClassad.h
@@ -224,13 +224,29 @@ class MatchClassAdDoesNotOwn : public MatchClassAd
 {
 public:
 	MatchClassAdDoesNotOwn() : MatchClassAd() {}
-	MatchClassAdDoesNotOwn(ClassAd* al, ClassAd* ar) : MatchClassAd(al, ar) {}
+	MatchClassAdDoesNotOwn(ClassAd* al, ClassAd* ar, bool strictEvaluation=true) : MatchClassAd(al, ar),
+		m_origLeftAltScope(al->alternateScope),
+		m_origRightAltScope(ar->alternateScope)
+	{
+		if (!strictEvaluation)
+		{
+			GetLeftAd()->alternateScope = ar;
+			GetRightAd()->alternateScope = al;
+		}
+	}
 
 	virtual ~MatchClassAdDoesNotOwn()
 	{
-		RemoveLeftAd();
-		RemoveRightAd();
+		classad::ClassAd *ad;
+		ad = RemoveLeftAd();
+		ad->alternateScope = m_origLeftAltScope;
+		ad = RemoveRightAd();
+		ad->alternateScope = m_origRightAltScope;
 	}
+
+private:
+	classad::ClassAd *m_origLeftAltScope;
+	classad::ClassAd *m_origRightAltScope;
 };
 
 } // classad
