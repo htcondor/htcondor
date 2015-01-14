9525b2f3374fd7c289a02cd54ba550c5cf4c2ffe
John (TJ) Knoeller <johnkn@cs.wisc.edu>
2015-01-14 14:13:08 -0600

    add x86_64_MacOSX10 to nmi_tools SubmitInfo.pm as a default build

--- nmi_tools/glue/SubmitInfo.pm
+++ nmi_tools/glue/SubmitInfo.pm
@@ -448,6 +448,7 @@ our %submit_info = (
 	'x86_64_MacOSX7',	=> 'x86_64_MacOSX',
 	'x86_64_MacOSX8',	=> 'x86_64_MacOSX',
 	'x86_64_MacOSX9',	=> 'x86_64_MacOSX',
+	'x86_64_MacOSX10',	=> 'x86_64_MacOSX',
 
 	#
 	# The SWAMP platforms.
