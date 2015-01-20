4853eafc1e380c88d34ec360ed4204430e02e18e
Greg Thain <gthain@cs.wisc.edu>
2015-01-20 16:51:02 -0600

    Fix more off by one errors

--- src/condor_daemon_core.V6/daemon_core.cpp
+++ src/condor_daemon_core.V6/daemon_core.cpp
@@ -5344,7 +5344,7 @@ public:
 		const char *the_executable_fullpath,
 		const int *the_std,
 		int the_numInheritFds,
-		const int (&the_inheritFds)[MAX_INHERIT_FDS],
+		const int (&the_inheritFds)[MAX_INHERIT_FDS + 1],
 		int the_nice_inc,
 		const priv_state &the_priv,
 		int the_want_command_port,
@@ -5415,7 +5415,7 @@ private:
 	const char *m_executable_fullpath;
 	const int *m_std;
 	const int m_numInheritFds;
-	const int (&m_inheritFds)[MAX_INHERIT_FDS];
+	const int (&m_inheritFds)[MAX_INHERIT_FDS + 1];
 	int m_nice_inc;
 	const priv_state &m_priv;
 	const int m_want_command_port;
