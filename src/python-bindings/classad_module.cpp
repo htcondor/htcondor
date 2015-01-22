52b18534aae9c3abb9a140f66ece2b58a1dc123d
Ivan Calderon <calderon@cs.wisc.edu>
2015-01-22 12:17:35 -0600

    Implemented #4836 enabling pyhon bindings for Windows. VS2012 and Python 2.7 are pre-requisets.
    *added proper project configurations to Cmake for python_bindings. *refactored some python-bindings sources as unix specific code was used. *added a new boost static library to the boost tar ball used in externals this is so that boost python can link in as this component is not header only on windows
    
    Caveats: Python2.7 only Visual Studio 2012 only boost_python-vc110-1_54.dll must be present in the condor bin dir
    
    Try 2 with fixed line endings

--- src/python-bindings/classad_module.cpp
+++ src/python-bindings/classad_module.cpp
@@ -23,7 +23,7 @@ std::string ClassadLibraryVersion()
 
 
 static
-std::string GetLastError()
+std::string GetLastCondorError()
 {
     return classad::CondorErrMsg;
 }
@@ -124,7 +124,7 @@ BOOST_PYTHON_MODULE(classad)
 
     def("version", ClassadLibraryVersion, "Return the version of the linked ClassAd library.");
 
-    def("lastError", GetLastError, "The last error that occurred in the ClassAd library.");
+    def("lastError", GetLastCondorError, "The last error that occurred in the ClassAd library.");
     def("registerLibrary", RegisterLibrary, "Register a shared library of ClassAd functions.");
 
     def("parse", parseString, return_value_policy<manage_new_object>());
