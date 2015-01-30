12e0c74b73c09dc9b647a91332dd585f16ff01e5
Greg Thain <gthain@cs.wisc.edu>
2015-01-30 09:16:21 -0600

    Fix socket leak in ckpt_server

--- src/condor_ckpt_server/server2.cpp
+++ src/condor_ckpt_server/server2.cpp
@@ -1350,6 +1350,7 @@ void Server::SendStatus(int data_conn_sd)
 				 sizeof(socket_bufsize));
   }
   imds.TransferFileInfo(xfer_sd);
+  close(xfer_sd);
 }
 
 /* check to make sure something being used as a filename that we will
