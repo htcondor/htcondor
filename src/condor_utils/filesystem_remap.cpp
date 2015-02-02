eeec609ff847fcd263cc7df24bb40db224f3f6e7
Todd Tannenbaum <tannenba@cs.wisc.edu>
2015-02-02 14:40:16 -0600

    Fix compile warning on Fedora (check fscanf return val). #4856

--- src/condor_utils/filesystem_remap.cpp
+++ src/condor_utils/filesystem_remap.cpp
@@ -328,9 +328,9 @@ int FilesystemRemap::AddEncryptedMapping(std::string mountpoint, std::string pas
 		// output is "blah blah ... [sig1] blah blah [sig2] ...", and
 		// we want to grab the sig1 and sig2 strings contained with the
 		// square brackets.  so just use fscanf with ToddT's mad scanf skilzzz.
-		fscanf(stream,"%*[^[][%79[^]]%*[^[][%79[^]]",sig1,sig2);
+		int sfret = fscanf(stream,"%*[^[][%79[^]]%*[^[][%79[^]]",sig1,sig2);
 		ret = my_pclose(stream); // ret now has exit status from ecryptfs-add-passphrase
-		if (ret != 0 || !sig1[0] || !sig2[0]) {
+		if (ret != 0 || sfret != 2 || !sig1[0] || !sig2[0]) {
 			dprintf(D_ALWAYS,
 					"%s failed to store encyption and file name encryption keys (%d,%s,%s)\n",
 					cmdargs.GetArg(0),ret,sig1,sig2);
