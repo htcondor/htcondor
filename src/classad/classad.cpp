540b481fac64d88971b87193dfbf8b185e8ab37e
Jaime Frey <jfrey@cs.wisc.edu>
2015-02-03 16:13:31 -0600

    Add #defines for special attribute name strings. #4858

--- src/classad/classad.cpp
+++ src/classad/classad.cpp
@@ -30,6 +30,12 @@ extern "C" void to_lower (char *);	// from util_lib (config.c)
 
 namespace classad {
 
+#define ATTR_TOPLEVEL "toplevel"
+#define ATTR_ROOT "root"
+#define ATTR_SELF "self"
+#define ATTR_PARENT "parent"
+#define ATTR_MY "my"
+
 // This flag is only meant for use in Condor, which is transitioning
 // from an older version of ClassAds with slightly different evaluation
 // semantics. It will be removed without warning in a future release.
@@ -71,10 +77,10 @@ void ClassAdLibraryVersion(string &version_string)
 static References *specialAttrNames = NULL;
 static void init_specialAttrNames() {
 	specialAttrNames = new References;
-	specialAttrNames->insert( "toplevel" );
-	specialAttrNames->insert( "root" );
-	specialAttrNames->insert( "self" );
-	specialAttrNames->insert( "parent" );
+	specialAttrNames->insert( ATTR_TOPLEVEL );
+	specialAttrNames->insert( ATTR_ROOT );
+	specialAttrNames->insert( ATTR_SELF );
+	specialAttrNames->insert( ATTR_PARENT );
 }
 
 void SetOldClassAdSemantics(bool enable)
@@ -84,9 +90,10 @@ void SetOldClassAdSemantics(bool enable)
 		init_specialAttrNames();
 	}
 	if ( enable ) {
-		specialAttrNames->insert( "my" );
+		specialAttrNames->insert( ATTR_MY );
+		}
 	} else {
-		specialAttrNames->erase( "my" );
+		specialAttrNames->erase( ATTR_MY );
 	}
 }
 
@@ -668,24 +675,25 @@ LookupInScope(const string &name, ExprTree*& expr, EvalState &state) const
 			if( current == this ) {		// NAC - simple loop checker
 				return( EVAL_UNDEF );
 			}
-		} else if(strcasecmp(name.c_str( ),"toplevel")==0 ||
-				strcasecmp(name.c_str( ),"root")==0){
+		} else if(strcasecmp(name.c_str( ),ATTR_TOPLEVEL)==0 ||
+				strcasecmp(name.c_str( ),ATTR_ROOT)==0){
 			// if the "toplevel" attribute was requested ...
 			expr = (ClassAd*)state.rootAd;
 			if( expr == NULL ) {	// NAC - circularity so no root
 				return EVAL_FAIL;  	// NAC
 			}						// NAC
 			return( expr ? EVAL_OK : EVAL_UNDEF );
-		} else if( strcasecmp( name.c_str( ), "self" ) == 0 ||
-				   strcasecmp( name.c_str( ), "my" ) == 0 ) {
+		} else if( strcasecmp( name.c_str( ), ATTR_SELF ) == 0 ||
+				   strcasecmp( name.c_str( ), ATTR_MY ) == 0 ) {
 			// if the "self" ad was requested
 			expr = (ClassAd*)state.curAd;
 			return( expr ? EVAL_OK : EVAL_UNDEF );
-		} else if( strcasecmp( name.c_str( ), "parent" ) == 0 ) {
+		} else if( strcasecmp( name.c_str( ), ATTR_PARENT ) == 0 ) {
 			// the lexical parent
 			expr = (ClassAd*)superScope;
 			return( expr ? EVAL_OK : EVAL_UNDEF );
 		}
+
 	}	
 
 	return( EVAL_UNDEF );
