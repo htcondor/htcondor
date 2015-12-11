90e03344098642169a179343869515bcc72b65da
Kent Wenger <wenger@cs.wisc.edu>
2015-12-11 12:17:24 -0600

    Gittrac #5107:  Finshed test; added version history item.

--- src/condor_dagman/condor_submit_dag.cpp
+++ src/condor_dagman/condor_submit_dag.cpp
@@ -1,5 +1,3 @@
-//TEMPTEMP -- -append on command line should override attr from DAG? or maybe warning or error...
-//TEMPTEMP -- warning or error if same attr is multiply-defined?
 /***************************************************************
  *
  * Copyright (C) 1990-2007, Condor Team, Computer Sciences Department,
--- src/condor_tests/job_dagman_set_attr-nodeA.cmd
+++ src/condor_tests/job_dagman_set_attr-nodeA.cmd
@@ -1,4 +1,5 @@
-#TEMPTEMP -- do we need scheduler?
+# Note:  Maybe change this to local universe once gittrac #5299 is
+# fixed.  wenger 2015-12-11
 universe	= scheduler
 executable	= ./job_dagman_set_attr-node.pl
 arguments	= $(DAGManJobId)
