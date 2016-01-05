6626d64960755f5f14b970f323f878180c315d4b
Kent Wenger <wenger@cs.wisc.edu>
2016-01-05 12:43:50 -0600

    Gittrac #5439:  Added a test for this functionality.

---
+++ src/condor_tests/job_dagman_event_timestamp-node.cmd
@@ -0,0 +1,9 @@
+executable           	= ./x_echostring.pl
+arguments		= 1 2 3
+universe             	= vanilla
+log			= job_dagman_event_timestamp-node.log
+notification         	= NEVER
+getenv               	= true
+output			= job_dagman_event_timestamp-node.out
+error			= job_dagman_event_timestamp-node.err
+queue
