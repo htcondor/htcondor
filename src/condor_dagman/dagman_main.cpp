85fc1bc9c3359648472d6fc42433b0454cc3115c
Kent Wenger <wenger@cs.wisc.edu>
2016-02-17 13:30:19 -0600

    Gittrac #5477:  Changed DAGMAN_ALWAYS_RUN_POST default to false; going to do a build/test and see what tests fail...

--- src/condor_dagman/dagman_main.cpp
+++ src/condor_dagman/dagman_main.cpp
@@ -343,7 +343,7 @@ Dagman::Config()
 	debug_printf( DEBUG_NORMAL, "DAGMAN_SUBMIT_DEPTH_FIRST setting: %s\n",
 				submitDepthFirst ? "True" : "False" );
 
-	_runPost = param_boolean( "DAGMAN_ALWAYS_RUN_POST", true );
+	_runPost = param_boolean( "DAGMAN_ALWAYS_RUN_POST", false );
 	debug_printf( DEBUG_NORMAL, "DAGMAN_ALWAYS_RUN_POST setting: %s\n",
 			_runPost ? "True" : "False" );
 
