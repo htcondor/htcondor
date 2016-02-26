0a62d662f66df0ef766db1a4bdf6e4ff4615a2a9
Greg Thain <gthain@cs.wisc.edu>
2016-02-25 18:57:32 -0600

    Correctly compute disk fraction for dslots #5535

--- src/condor_startd.V6/Resource.cpp
+++ src/condor_startd.V6/Resource.cpp
@@ -3349,7 +3349,7 @@ Resource * initialize_resource(Resource * rip, ClassAd * req_classad, Claim* &le
                 if (j != consumption.begin()) type += " ";
                 if (MATCH == strcasecmp(j->first.c_str(),"disk")) {
                     // if it weren't for special cases, we'd have no cases at all
-                    type.formatstr_cat("disk=%3.3g%%", max(0, (0.001 + 100 * j->second / (double)rip->r_attr->get_total_disk())));
+                    type.formatstr_cat("disk=%3.3g%%", max(0, (0.001 + 100 * j->second / (double)rip->r_attr->get_disk())));
                 } else {
                     type.formatstr_cat("%s=%d", j->first.c_str(), int(j->second));
                 }
