7856a8e7ae4a25097d34fc501f59798c6e4212a6
John (TJ) Knoeller <johnkn@cs.wisc.edu>
2016-03-15 15:35:31 -0500

    condor_tests: remove unix'isms from cmd_q_shows-dag test and change it to create dag and submit files that it needs when the test is run. ===VersionHistory:None===

--- src/condor_tests/cmd_q_shows-dag-A.cmd
+++
@@ -1,7 +0,0 @@
-executable = cmd_q_shows-dag.sh
-universe = vanilla
-arguments = $(JOB) 120
-output = cmd_q_shows-dag-A.out
-error = cmd_q_shows-dag-A.err
-log = cmd_q_shows-dag.log
-queue
--- src/condor_tests/cmd_q_shows-dag-B_C.cmd
+++
@@ -1,7 +0,0 @@
-executable = cmd_q_shows-dag.sh
-universe = vanilla
-arguments = $(JOB) 100
-output = cmd_q_shows-dag-B_C.out
-error = cmd_q_shows-dag-B_C.err
-log = cmd_q_shows-dag-B.log
-queue
