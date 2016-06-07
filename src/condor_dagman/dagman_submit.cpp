e68d60bd28e96f427bfcd7182dd7c080e5636f07
Kent Wenger <wenger@cs.wisc.edu>
2016-06-07 17:22:33 -0500

    Gittrac #5077:  Some cleanup of the submit code.

--- src/condor_dagman/dagman_submit.cpp
+++ src/condor_dagman/dagman_submit.cpp
@@ -331,7 +331,22 @@ condor_submit( const Dagman &dm, const char* cmdFile, CondorID& condorID,
 	var += dm.dag->NumNodesFailed();
 	args.AppendArg( var.Value() );
 
-	//TEMPTEMP -- accounting group stuff here
+	if( hold_claim ){
+		args.AppendArg( "-a" );
+		MyString holdit = MyString("+") + MyString(ATTR_JOB_KEEP_CLAIM_IDLE) + " = "
+			+ dm._claim_hold_time;
+		args.AppendArg( holdit.Value() );	
+	}
+	
+	if (dm._submitDagDeepOpts.suppress_notification) {
+		args.AppendArg( "-a" );
+		MyString notify = MyString("notification = never");
+		args.AppendArg( notify.Value() );
+	}
+
+		//
+		// Add accounting group and user if we have them.
+		//
 	if ( dm._submitDagDeepOpts.acctGroup != "" ) {
 		args.AppendArg( "-append" );
 		MyString arg = "accounting_group=";
@@ -346,7 +361,14 @@ condor_submit( const Dagman &dm, const char* cmdFile, CondorID& condorID,
 		args.AppendArg( arg );
 	}
 
-		// how big is the command line so far
+		//
+		// Add parents of this node to arguments, if we have room.
+		//
+		// This should be the last thing in the arguments, except
+		// for the submit file name!!!
+		//
+
+		// how big is the command line so far?
 	MyString display;
 	args.GetArgsStringForDisplay( &display );
 	int cmdLineSize = display.Length();
@@ -369,20 +391,6 @@ condor_submit( const Dagman &dm, const char* cmdFile, CondorID& condorID,
 		check_warning_strictness( DAG_STRICT_3 );
 	} else {
 		args.AppendArgsFromArgList( parentNameArgs );
-	}
-
-	//TEMPTEMP -- this should probably get moved to before parent names!
-	if( hold_claim ){
-		args.AppendArg( "-a" );
-		MyString holdit = MyString("+") + MyString(ATTR_JOB_KEEP_CLAIM_IDLE) + " = "
-			+ dm._claim_hold_time;
-		args.AppendArg( holdit.Value() );	
-	}
-	
-	if (dm._submitDagDeepOpts.suppress_notification) {
-		args.AppendArg( "-a" );
-		MyString notify = MyString("notification = never");
-		args.AppendArg( notify.Value() );
 	}
 
 	args.AppendArg( cmdFile );
