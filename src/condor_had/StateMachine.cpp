75c961e5fce092951604e78fcaad30df73356b73
Todd L Miller <tlmiller@cs.wisc.edu>
2016-06-24 16:44:41 -0500

    (#5754)  Clear the HAD StringLists before repeatedly inserting to construct them.

--- src/condor_had/StateMachine.cpp
+++ src/condor_had/StateMachine.cpp
@@ -827,6 +827,10 @@ HADStateMachine::getHadList( const char *str,
 	Sinful my_addr( daemonCore->InfoCommandSinfulString() );
 	ASSERT( daemonCore->InfoCommandSinfulString() && my_addr.valid() );
 
+       // Don't add to the HAD list on each reconfig.
+       otherIps.clearAll();
+       allIps.clearAll();
+
     bool iAmPresent = false;
     while( (try_address = had_list.next()) ) {
         char *sinful_addr = utilToSinful( try_address );
