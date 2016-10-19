e58c0ffbbd0ee008847c932db1a14dd21681a01a
shuyang <shuayng@cs.wisc.edu>
2016-10-19 16:03:43 -0500

    Added a test for config JOB_TRANSFORM_NAMES. Ticket: #5959

--- src/condor_scripts/CheckOutputFormats.pm
+++ src/condor_scripts/CheckOutputFormats.pm
@@ -951,12 +951,25 @@ sub check_status {
 sub convert_timestamp_date_hour_min {
 	my $timestamp = $_[0];
 	my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime($timestamp);
-	$mon = sprintf("%02d",$mon + 1);
-	$mday = sprintf("%02d", $mday);
-	$hour = sprintf("%02d", $hour);
-	$min = sprintf("%02d", $min);
+	$mon = sprintf("%2d",$mon + 1);
+	$mday = sprintf("%2d", $mday);
+	$hour = sprintf("%2d", $hour);
+	$min = sprintf("%2d", $min);
 	return "$mon/$mday $hour:$min";
 }
+
+sub how_many_entries {
+	my @content = @{$_[0]};
+	my $num = $_[1];
+	my $index = 0;
+	for my $i (0..(scalar @content)-1){
+		if ($content[$i] =~ /\S/){
+			$index++;
+		}
+	}
+	return ($index - 3);
+}
+
 
 sub count_status_state {
 	my %Attr = %{$_[0]};
