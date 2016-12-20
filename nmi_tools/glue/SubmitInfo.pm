3ab1b5368e83e95b5cdd875190444e19182b0b0d
Tim Theisen <tim@cs.wisc.edu>
2016-12-20 13:27:44 -0600

    Build on CentOS7

--- nmi_tools/glue/SubmitInfo.pm
+++ nmi_tools/glue/SubmitInfo.pm
@@ -303,6 +303,10 @@ our %submit_info = (
 		},
 	},
 
+	'x86_64_CentOS7'	=> 'x86_64_RedHat7',
+	'x86_64_SL7'		=> 'x86_64_RedHat7',
+
+
 	##########################################################################
 	# Platform RedHat and SL
 	##########################################################################
@@ -332,8 +336,6 @@ our %submit_info = (
 
 	# Add the SWAMP's (temporary) platform name
 	'swamp:rhel-6.4-64'	=> 'x86_64_RedHat6',
-
-	'x86_64_SL7'	=> 'x86_64_SL6',
 
 	# for now SL6 is the same as RedHat6
 	'x86_64_SL6'	=> 'x86_64_RedHat6',
