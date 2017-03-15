0d7425ccb4aee421abf11516cbcccd9f0740b1f8
Tim Theisen <tim@cs.wisc.edu>
2017-03-15 08:16:58 -0500

    (#6192) Make Fedora compiler happy

--- src/condor_daemon_core.V6/daemon_core.cpp
+++ src/condor_daemon_core.V6/daemon_core.cpp
@@ -8298,8 +8298,8 @@ DaemonCore::Create_Thread(ThreadStartFunc start_func, void *arg, Stream *sock,
 		priv_state saved_priv = get_priv();
 		int exit_status = start_func(arg,s);
 
-		if (s) delete s; s=NULL;
-		if (arg) free(arg);	arg=NULL;		// arg should point to malloc()'ed data
+		if (s) { delete s; s=NULL; }
+		if (arg) { free(arg);	arg=NULL; }		// arg should point to malloc()'ed data
 
 #ifndef WIN32
 			// In unix, we need to make exit_status like wait waitpid() returns
