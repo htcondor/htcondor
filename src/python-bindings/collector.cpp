f0802db6d2a82000af4ed85420f01a548823b27b
Carl Edquist <edquist@cs.wisc.edu>
2017-07-05 09:50:06 -0500

    (#6326) make PyString_Check available for python 3 in collector.cpp

--- src/python-bindings/collector.cpp
+++ src/python-bindings/collector.cpp
@@ -14,6 +14,12 @@
 
 #include "module_lock.h"
 
+// http://docs.python.org/3/c-api/apiabiversion.html#apiabiversion
+#if PY_MAJOR_VERSION >= 3
+   #define PyInt_Check(op)  PyNumber_Check(op)
+   #define PyString_Check(op)  PyBytes_Check(op)
+#endif
+
 using namespace boost::python;
 
 
