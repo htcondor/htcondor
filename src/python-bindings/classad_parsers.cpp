80b65605ad7f1db1eb18cd75870e4b9d1b9b0044
Carl Edquist <edquist@cs.wisc.edu>
2017-07-19 13:34:20 -0500

    (#6326) clear StopIteration error here on success
    starting in Python 3.5, a SystemError is raised if the function is
    returned with an exception set: https://bugs.python.org/issue23571
    
    ... note this makes the code immediately above meaningless, which saves
    and restores the error state...

--- src/python-bindings/classad_parsers.cpp
+++ src/python-bindings/classad_parsers.cpp
@@ -269,6 +269,9 @@ OldClassAdIterator::next()
                     m_source.attr("seek")(end_ptr);
                     PyErr_Restore(ptype, pvalue, ptraceback);
                 }
+#if PY_MAJOR_VERSION >= 3
+                PyErr_Clear();
+#endif
                 return result;
             }
             boost::python::throw_error_already_set();
