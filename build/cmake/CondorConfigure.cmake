1a5ca764d2258191f23a270022edebcc4fbba41b
Tim Theisen <tim@cs.wisc.edu>
2017-08-09 20:50:56 -0500

    (#6369) Update boost external to 1.64.0 on stable
    Squashed commit of the following:
    
    commit c098cd7ad940a321fed27e1a871014fa9b2e4d6b
    Author: Tim Theisen <tim@cs.wisc.edu>
    Date:   Wed Aug 9 20:47:32 2017 -0500
    
        (#6369) Merge in TJ's windows updates
    
    commit 1b6a2f77a0a7c55ab8bc3d1a114c0448685afbab
    Author: Jaime Frey <jfrey@cs.wisc.edu>
    Date:   Wed Aug 9 13:15:07 2017 -0500
    
        Patch for boost support of python numpy. #6369
    
        This patch is taken from boost 1.65.0, currently in beta. The build
        fails without it, when numpy is installed.
    
    commit d83d84f4a1088e1bc8d62d758d9b0395e7dcf07c
    Author: Tim Theisen <tim@cs.wisc.edu>
    Date:   Wed Aug 9 11:08:52 2017 -0500
    
        (#6369) Fix boost build
    
    commit 746e035b12b8ecca84111b8596d93da6caf9f14b
    Author: Tim Theisen <tim@cs.wisc.edu>
    Date:   Tue Aug 8 11:57:14 2017 -0500
    
        New boost external

--- build/cmake/CondorConfigure.cmake
+++ build/cmake/CondorConfigure.cmake
@@ -814,7 +814,7 @@ else ()
 
   add_subdirectory(${CONDOR_EXTERNAL_DIR}/bundles/drmaa/1.6.2)
   add_subdirectory(${CONDOR_EXTERNAL_DIR}/bundles/qpid/0.8-RC3)
-  add_subdirectory(${CONDOR_EXTERNAL_DIR}/bundles/boost/1.49.0)
+  add_subdirectory(${CONDOR_EXTERNAL_DIR}/bundles/boost/1.64.0)
 
   add_subdirectory(${CONDOR_EXTERNAL_DIR}/bundles/curl/7.31.0-p1 )
   add_subdirectory(${CONDOR_EXTERNAL_DIR}/bundles/openssl/1.0.1e)
