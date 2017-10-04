d16e12cc03916c53fdd229c7902e0e93b18f8cba
Mark Coatsworth <coatsworth@cs.wisc.edu>
2017-10-04 17:06:01 -0500

    Fixed Windows tests (#6274)

--- src/condor_tests/job_dagman_latemat-A-nodeA.cmd
+++ src/condor_tests/job_dagman_latemat-A-nodeA.cmd
@@ -1,4 +1,4 @@
-executable          = /bin/echo
+executable          = x_sleep.pl
 output              = job_dagman_latemat-A.out
 error               = job_dagman_latemat-A.err
 max_materialize     = 2
