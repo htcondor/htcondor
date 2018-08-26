69b849cc3b74b4a7f53ba2d935cbdf447366c959
Todd L Miller <tlmiller@cs.wisc.edu>
2018-08-25 20:18:39 -0500

    (#6736)  Update CondorJob (python class) to use the new JobEventLog.  Fix the new JobEventLog to release the GIL (global interpreter lock) so that the Python-threaded cmd_curl_* tests will work.

--- src/condor_tests/pytest/CondorJob.py
+++ src/condor_tests/pytest/CondorJob.py
@@ -7,23 +7,28 @@ from PersonalCondor import PersonalCondor
 from Globals import *
 from Utils import Utils
 
+from htcondor import JobEventLog
+from htcondor import JobEventType
+
 class CondorJob(object):
 
     def __init__(self, job_args):
         self._cluster_id = None
         self._job_args = job_args
+        self._log = None
+        self._callbacks = { }
 
-    def FailureCallback(self):
-        Utils.TLog("Job cluster " + str(self._cluster_id) + " failure callback invoked")
-
-    def SubmitCallback(self):
-        Utils.TLog("Job cluster " + str(self._cluster_id) + " submit callback invoked")
-
-    def SuccessCallback(self):
-        Utils.TLog("Job cluster " + str(self._cluster_id) + " success callback invoked")
-
+    def clusterID(self):
+        return self._cluster_id
 
     def Submit(self, wait=False, count=1):
+        # It's easier to smash the case of the keys (since ClassAds and the
+        # submit language don't care) than to do the case-insensitive compare.
+        self._job_args = dict([(k.lower(), v) for k, v in self._job_args.items()])
+
+        # Extract the event log filename, or insert one if none.
+        self._log = self._job_args.setdefault( "log", "test-{0}.log".format( os.getpid() ) )
+        self._log = os.path.abspath( self._log )
 
         # Submit the job defined by submit_args
         Utils.TLog("Submitting job with arguments: " + str(self._job_args))
@@ -36,48 +41,89 @@ class CondorJob(object):
             print( "Job submission failed for an unknown error: " + str(e) )
             return JOB_FAILURE
 
-        Utils.TLog("Job running on cluster " + str(self._cluster_id))
+        Utils.TLog("Job submitted succeeded with cluster ID " + str(self._cluster_id))
+
+        # We probably don't need self._log, but it seems like it may be
+        # handy for log messages at some point.
+        self._jel = JobEventLog( self._log )
+        if not self._jel.isInitialized():
+            print( "Unable to initialize job event log " + self._log )
+            return JOB_FAILURE
 
         # Wait until job has finished running?
         if wait is True:
-            submit_result = self.WaitForFinish()
-            return submit_result
+            return self.WaitUntilJobTerminated()
 
         # If we aren't waiting for finish, return None
         return None
 
+    #
+    # The timeout for these functions is in seconds, and applies to the
+    # whole process, not any individual read.
+    #
 
-    def WaitForFinish(self, timeout=240):
+    def WaitUntilJobTerminated( self, timeout = 240, proc = 0 ):
+        return self.WaitUntil( [ JobEventType.JOB_TERMINATED ],
+            [ JobEventType.EXECUTE, JobEventType.SUBMIT,
+              JobEventType.IMAGE_SIZE ], timeout, proc )
 
-        schedd = htcondor.Schedd()
-        for i in range(timeout):
-            ads = schedd.query("ClusterId == %d" % self._cluster_id, ["JobStatus"])
-            Utils.TLog("Ads = " + str(ads))
+    def WaitUntilExecute( self, timeout = 240, proc = 0 ):
+        return self.WaitUntil( [ JobEventType.EXECUTE ],
+            [ JobEventType.SUBMIT ], timeout, proc )
 
-            # When the job is complete, ads will be empty
-            if len(ads) == 0:
-                self.SuccessCallback()
-                return JOB_SUCCESS
+    def WaitUntilJobHeld( self, timeout = 240, proc = 0 ):
+        return self.WaitUntil( [ JobEventType.JOB_HELD ],
+            [ JobEventType.EXECUTE, JobEventType.SUBMIT,
+              JobEventType.IMAGE_SIZE, JobEventType.SHADOW_EXCEPTION ],
+            timeout, proc )
+
+        # FIXME: "look ahead" five seconds to see if we find the
+        # hold event, o/w fail.  TODO: file a bug to prevent the
+        # shadow exception event from entering the user log when
+        # a job goes on hold.
+
+
+    # An event type not listed in successEvents or ignoreeEvents is a failure.
+    def WaitUntil( self, successEvents, ignoreEvents, timeout = 240, proc = 0 ):
+        deadline = time.time() + timeout;
+        Utils.TLog( "[cluster " + str(self._cluster_id) + "] Waiting for " + ",".join( [str(x) for x in successEvents] ) )
+
+        for event in self._jel.follow( int(timeout * 1000) ):
+            if event.cluster == self._cluster_id and event.proc == proc:
+                if( not self._callbacks.get( event.type ) is None ):
+                    self._callbacks[ event.type ]()
+
+                if( event.type == JobEventType.NONE ):
+                    Utils.TLog( "[cluster " + str(self._cluster_id) + "] Found relevant event of type " + str(event.type) + " (ignore)" )
+                    pass
+                elif( event.type in successEvents ):
+                    Utils.TLog( "[cluster " + str(self._cluster_id) + "] Found relevant event of type " + str(event.type) + " (succeed)" )
+                    return True
+                elif( event.type in ignoreEvents ):
+                    Utils.TLog( "[cluster " + str(self._cluster_id) + "] Found relevant event of type " + str(event.type) + " (ignore)" )
+                    pass
+                else:
+                    Utils.TLog( "[cluster " + str(self._cluster_id) + "] Found relevant event of disallowed type " + str(event.type) + " (fail)" )
+                    return False
+
+            difference = deadline - time.time();
+            if difference <= 0:
+                Utils.TLog( "[cluster " + str(self._cluster_id) + "] Timed out waiting for " + ",".join( [str(x) for x in successEvents] ) )
+                return False
             else:
-                status = ads[0]["JobStatus"]
-                if status == 5:
-                    Utils.TLog("Job was placed on hold. Aborting.")
-                    self.FailureCallback()
-                    return JOB_FAILURE
-            time.sleep(1)
+                self._jel.setFollowTimeout( int(difference * 1000) )
+                continue
 
-        # If we got this far, we hit the timeout and job did not complete
-        Utils.TLog("Job failed to complete with timeout = " + str(timeout))
-        return JOB_FAILURE
+    #
+    # The callback-based interface.  The first set respond to events.  We'll
+    # add semantic callbacks if it turns out those are useful to anyone.
+    #
 
+    def RegisterSubmit( self, submit_callback_fn ):
+        self._callbacks[ JobEventType.SUBMIT ] = submit_callback_fn
 
-    def RegisterSubmit(self, submit_callback_fn):
-        self.SubmitCallback = submit_callback_fn
+    def RegisterJobTerminated( self, job_terminated_callback_fn ):
+        self._callbacks[ JobEventType.JOB_TERMINATED ] = job_terminated_callback_fn
 
-
-    def RegisterSuccess(self, success_callback_fn):
-        self.SuccessCallback = success_callback_fn
-
-
-    def RegisterFailure(self, failure_callback_fn):
-        self.FailureCallback = failure_callback_fn
+    def RegisterJobHeld( self, job_held_callback_fn ):
+        self._callbacks[ JobEventType.JOB_HELD ] = job_held_callback_fn
