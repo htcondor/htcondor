007cdfbe6d6d0a0695c6f000f25f54c8275412a4
Derek Weitzel <djw8605@gmail.com>
2018-09-10 13:33:05 -0500

    Changing argument to transferOutputSandbox
    The argument to transferOutputSandbox should be a list
    of tuples.  Each tuple has 2 elements, the job classad
    and the location of the output sandbox on the local host.

--- src/python-bindings/schedd.cpp
+++ src/python-bindings/schedd.cpp
@@ -1721,6 +1721,8 @@ struct Schedd {
     
     void transferOutputSandbox(object jobs)
     {
+        // Jobs is a list of tuples, or should be
+        // I'm not sure how to sanity check this though
         int len = py_len(jobs);
         std::vector<compat_classad::ClassAd*> job_array;
         std::vector<boost::shared_ptr<compat_classad::ClassAd> > job_tmp_array;
@@ -1728,10 +1730,20 @@ struct Schedd {
         job_tmp_array.reserve(len);
         for (int i=0; i<len; i++)
         {
-            const ClassAdWrapper wrapper = extract<ClassAdWrapper>(jobs[i]);
+            // Check the length of the item, make sure it's a tuple of exactly length 2
+            if (py_len(jobs[i]) != 2) {
+                PyErr_SetString(PyExc_ValueError, "Jobs object is not a list of tuples with exactly length 2");
+                throw_error_already_set();
+            }
+            
+            // Get the tuple
+            const boost::python::tuple job_info_tuple = extract<boost::python::tuple>(jobs[i]);
+            const ClassAdWrapper wrapper = extract<ClassAdWrapper>(job_info_tuple[0]);
+            const std::string path = extract<std::string>(job_info_tuple[1]);
             boost::shared_ptr<compat_classad::ClassAd> tmp_ad(new compat_classad::ClassAd());
             job_tmp_array.push_back(tmp_ad);
             tmp_ad->CopyFrom(wrapper);
+            tmp_ad->InsertAttr(ATTR_JOB_IWD, path);
             job_array.push_back(job_tmp_array[i].get());
         }
         CondorError errstack;
@@ -3204,7 +3216,7 @@ void export_schedd()
             ":param jobs: A expression string matching the list of job output sandboxes to retrieve.\n"
             ":param destination: Destination of the input sandbox.")
         .def("transferOutputSandbox", &Schedd::transferOutputSandbox, "Upload the output sandbox for multiple jobs\n"
-            ":param ads: A python list containing one or more job ads to upload their ouptut sandbox.\n")
+            ":param ads: A python list of tuples in the format (ClassAd, path) where path is the path to the output sandbox on the local machine.\n")
         .def("edit", &Schedd::edit, "Edit one or more jobs in the queue.\n"
             ":param job_spec: Either a list of jobs (CLUSTER.PROC) or a string containing a constraint to match jobs against.\n"
             ":param attr: Attribute name to edit.\n"
