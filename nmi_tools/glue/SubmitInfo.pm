5b539bbe15699253e61a44a84d3d6c8f96d0f006
Tim Theisen <tim@cs.wisc.edu>
2018-09-25 08:45:35 -0500

    Streamline 32-bit CentOS 7 build, remove duplicate Fedora27 #6783

--- nmi_tools/glue/SubmitInfo.pm
+++ nmi_tools/glue/SubmitInfo.pm
@@ -325,9 +325,7 @@ our %submit_info = (
 	# 32 bit CentOS 7
 	'x86_CentOS7'		=> {
 		'build' => {
-			'configure_args' => { @default_build_configure_args,
-								  '-DWITH_CREAM:BOOL' => 'OFF',
-			 },
+			'configure_args' => { @minimal_build_configure_args },
 			'prereqs'	=> [ ],
 			'xtests'	=> undef,
 		},
@@ -520,8 +518,7 @@ our %submit_info = (
 	##########################################################################
 	'x86_64_Fedora'	=> {
 		'build' => {
-			'configure_args' => { @minimal_build_configure_args,
-			},
+			'configure_args' => { @minimal_build_configure_args },
 			'prereqs'	=> [ @default_prereqs ],
 			'xtests'	=> undef,
 		},
@@ -557,20 +554,8 @@ our %submit_info = (
 	'x86_64_fedora_24'				=> 'x86_64_Fedora',
 	'x86_64_fedora_25'				=> 'x86_64_Fedora',
 	'x86_64_fedora_27'				=> 'x86_64_Fedora',
+	'x86_64_fedora_28'				=> 'x86_64_Fedora',
 
-	'x86_64_Fedora27'	=> {
-		'build' => {
-			'configure_args' => { @minimal_build_configure_args },
-			'prereqs'	=> [ @default_prereqs ],
-			'xtests'	=> undef,
-		},
-
-		'test' => {
-			'configure_args' => { @default_test_configure_args },
-			'prereqs'	=> [ @default_prereqs ],
-			'testclass' => [ @default_testclass ],
-		},
-	},
 
 	##########################################################################
 	# Platform Solaris 11 on x86_64
