8c3681f381b97cebe4620cdad5f599c9f1f6d543
Todd Tannenbaum <tannenba@cs.wisc.edu>
2018-11-09 07:04:30 -0600

    Do not change effectuve uid when using python bindings. #6817

--- src/python-bindings/_htcondor.cpp
+++ src/python-bindings/_htcondor.cpp
@@ -1,8 +1,10 @@
 
 #include "python_bindings_common.h"
 
+#include "condor_common.h"
 #include "old_boost.h"
 #include "export_headers.h"
+#include "condor_uid.h"
 
 using namespace boost::python;
 
@@ -14,6 +16,11 @@ BOOST_PYTHON_MODULE(_htcondor)
 
     // TODO: old boost doesn't have this; conditionally compile only one newer systems.
     //docstring_options local_docstring_options(true, false, false);
+
+    // Disable all priv switching from Python Bindings, even if the process
+    // invoking the bindings has root.  This prevents any possibility of returning
+    // to the calling code with a different effective uid than the caller expects.
+    set_priv_ignore_all_requests();
 
     export_config();
     export_daemon_and_ad_types();
