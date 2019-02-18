5110f23b0981216950fab0f98aa6db5f17cfca66
Todd Tannenbaum <tannenba@cs.wisc.edu>
2019-02-18 17:08:02 -0600

    Python Schedd.submit() now considers CPUs and GPUs in Requirements. #6918
    Also ensure the job ad given to submit() includes resource requests attrs
    before editing Requirements expression referencing them.

--- src/python-bindings/schedd.cpp
+++ src/python-bindings/schedd.cpp
@@ -33,6 +33,10 @@
 
 using namespace boost::python;
 
+#ifndef ATTR_REQUEST_GPUS
+#  define ATTR_REQUEST_GPUS "RequestGPUs"
+#endif
+
 #define DO_ACTION(action_name) \
     reason_str = extract<std::string>(reason); \
     { \
@@ -150,7 +154,7 @@ make_spool(classad::ClassAd& ad)
 }
 
 std::unique_ptr<ExprTree>
-make_requirements(ExprTree *reqs, ShouldTransferFiles_t stf)
+make_requirements(const ClassAd &jobAd, ExprTree *reqs, ShouldTransferFiles_t stf)
 {
     // Copied ideas from condor_submit.  Pretty lame.
     classad::ClassAdUnParser printer;
@@ -180,9 +184,24 @@ make_requirements(ExprTree *reqs, ShouldTransferFiles_t stf)
         ADD_REQUIREMENT(HAS_FILE_TRANSFER, "(TARGET." ATTR_HAS_FILE_TRANSFER " || (TARGET." ATTR_FILE_SYSTEM_DOMAIN " == MY." ATTR_FILE_SYSTEM_DOMAIN"))");
         break;
     }
-    ADD_REQUIREMENT(REQUEST_DISK, "TARGET.Disk >= " ATTR_REQUEST_DISK);
-    ADD_REQUIREMENT(REQUEST_MEMORY, "TARGET.Memory >= " ATTR_REQUEST_MEMORY);
-    return result;
+
+	if (jobAd.Lookup(ATTR_REQUEST_DISK)) {
+		ADD_REQUIREMENT(REQUEST_DISK, "TARGET.Disk >= " ATTR_REQUEST_DISK);
+	}
+    
+	if (jobAd.Lookup(ATTR_REQUEST_MEMORY)) {
+		ADD_REQUIREMENT(REQUEST_MEMORY, "TARGET.Memory >= " ATTR_REQUEST_MEMORY);
+	}
+
+	if (jobAd.Lookup(ATTR_REQUEST_CPUS)) {
+		ADD_REQUIREMENT(REQUEST_CPUS, "TARGET.Cpus >= " ATTR_REQUEST_CPUS);
+	}
+
+	if (jobAd.Lookup(ATTR_REQUEST_GPUS)) {
+		ADD_REQUIREMENT(REQUEST_GPUS, "TARGET.Gpus >= " ATTR_REQUEST_GPUS);
+	}
+    
+	return result;
 }
 
 bool
@@ -1559,7 +1578,7 @@ struct Schedd {
         }
 
         ExprTree *old_reqs = cluster_ad.Lookup(ATTR_REQUIREMENTS);
-        ExprTree *new_reqs = make_requirements(old_reqs, should).release();
+        ExprTree *new_reqs = make_requirements(cluster_ad, old_reqs, should).release();
         cluster_ad.Insert(ATTR_REQUIREMENTS, new_reqs);
 
         if (spool)
@@ -1604,7 +1623,7 @@ struct Schedd {
                 else if (should_str == "NO") {should = STF_NO;}
             }
 
-            ExprTree *new_reqs = make_requirements(old_reqs, should).release();
+            ExprTree *new_reqs = make_requirements(proc_ad, old_reqs, should).release();
             proc_ad.Insert(ATTR_REQUIREMENTS, new_reqs);
         }
 
