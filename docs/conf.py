7ad59d955bd39fbb07d9f57e1bda8c02b0ac66af
Mark Coatsworth <coatsworth@cs.wisc.edu>
2019-03-07 12:22:54 -0600

    Added custom CSS (#6908)

--- docs/conf.py
+++ docs/conf.py
@@ -257,3 +257,6 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+def setup(app):
+    app.add_stylesheet('css/htcondor-manual.css')
