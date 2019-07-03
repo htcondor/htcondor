fd3f36258a78903b0e20e3ab9ec421cac23810be
Brian Bockelman <bbockelm@cse.unl.edu>
2019-07-02 22:51:18 -0500

    Round out the initial logic for directory state.

--- src/condor_utils/data_reuse.cpp
+++ src/condor_utils/data_reuse.cpp
@@ -1,7 +1,13 @@
 
+#include "condor_debug.h"
 #include "data_reuse.h"
 
+#include "CondorError.h"
+#include "file_lock.h"
 #include "directory.h"
+#include "directory_util.h"
+
+#include <stdio.h>
 
 using namespace htcondor;
 
@@ -15,7 +21,9 @@ DataReuseDirectory::~DataReuseDirectory()
 
 DataReuseDirectory::DataReuseDirectory(const std::string &dirpath, bool owner) :
 	m_owner(owner),
-	m_dirpath(dirpath)
+	m_dirpath(dirpath),
+	m_log(dircat(m_dirpath.c_str(), "use.log", m_logname)),
+	m_rlog(dircat(m_dirpath.c_str(), "use.log", m_logname))
 {
 	if (m_owner) {
 		Cleanup();
@@ -55,4 +63,165 @@ void
 DataReuseDirectory::Cleanup() {
 	Directory dir(m_dirpath.c_str());
 	dir.Remove_Entire_Directory();
+}
+
+
+DataReuseDirectory::LogSentry::LogSentry(DataReuseDirectory &parent)
+	: m_parent(parent)
+{
+	m_lock = m_parent.m_log.getLock();
+	if (m_lock == nullptr) {return;}
+
+	m_acquired = m_lock->obtain(WRITE_LOCK);
+}
+
+
+DataReuseDirectory::LogSentry::~LogSentry()
+{
+	if (m_acquired) {
+		m_lock->release();
+	}
+}
+
+
+DataReuseDirectory::LogSentry &&
+DataReuseDirectory::LockLog(CondorError &err)
+{
+	LogSentry sentry(*this);
+	if (!sentry.acquired()) {
+		err.push("DataReuse", 3, "Failed to acquire data reuse directory lockfile.");
+	}
+	return std::move(sentry);
+}
+
+
+std::string
+DataReuseDirectory::FileEntry::fname() const
+{
+	MyString hash_dir;
+	dircat(m_parent.m_dirpath.c_str(), m_checksum_type.c_str(), hash_dir);
+
+	char hash_substring[3];
+	hash_substring[2] = '\0';
+	hash_substring[0] = m_checksum[0];
+	hash_substring[1] = m_checksum[1];
+
+	MyString file_dir;
+	dircat(hash_dir.c_str(), hash_substring, file_dir);
+
+	MyString fname;
+	std::string hash_name(m_checksum.c_str() + 2, m_checksum.size()-2);
+	hash_name += "." + m_tag;
+	dircat(file_dir.c_str(), hash_name.c_str(), fname);
+
+	return std::string(fname.c_str());
+}
+
+
+bool
+DataReuseDirectory::ReserveSpace(uint64_t size, uint32_t time, std::string &tag,
+	std::string &id, CondorError &err)
+{
+	LogSentry sentry = LockLog(err);
+	if (!sentry.acquired()) {
+		return false;
+	}
+
+	UpdateState(sentry, err);
+
+	if ((m_reserved_space + size > m_allocated_space) && !ClearSpace(size, sentry, err))
+	{
+		err.push("DataReuse", 1, "Unable to allocate space");
+		return false;
+	}
+
+	ReserveSpaceEvent event;
+	auto now = std::chrono::system_clock::now();
+	event.setExpirationTime(now + std::chrono::seconds(time));
+	event.setReservedSpace(size);
+	event.setTag(tag);
+	std::string uuid_result = event.generateUUID();
+	if (!m_log.writeEvent(&event)) {
+		err.push("DataReuse", 2, "Failed to write space reservation");
+		return false;
+	}
+	id = uuid_result;
+
+	return true;
+}
+
+
+bool
+DataReuseDirectory::ClearSpace(uint64_t size, LogSentry &sentry, CondorError &err)
+{
+	if (!sentry.acquired()) {return false;}
+
+	if (m_reserved_space + size <= m_allocated_space) {
+		return true;
+	}
+	auto it = m_contents.begin();
+	while (it != m_contents.end()) {
+		auto &file_entry = **it;
+		if (-1 == unlink(file_entry.fname().c_str())) {
+			err.pushf("DataReuse", 4, "Failed to unlink cache entry: %s", strerror(errno));
+			return false;
+		}
+		m_reserved_space -= file_entry.size();
+
+		FileRemovedEvent event;
+		event.setSize(file_entry.size());
+		event.setChecksumType(file_entry.checksum_type());
+		event.setChecksum(file_entry.checksum());
+		event.setTag(file_entry.tag());
+		it = m_contents.erase(it);
+		if (!m_log.writeEvent(&event)) {
+			err.push("DataReuse", 5, "Faild to write file deletion");
+			return false;
+		}
+
+		if (m_reserved_space + size <= m_allocated_space) {
+			return true;
+		}
+	}
+	return false;
+}
+
+
+bool
+DataReuseDirectory::UpdateState(LogSentry &sentry, CondorError &err)
+{
+	if (!sentry.acquired()) {return false;}
+
+	bool all_done = false;
+	do {
+		ULogEvent *event;
+		auto outcome = m_rlog.readEventWithLock(event, *sentry.lock());
+
+		switch (outcome) {
+		case ULOG_OK:
+			if (!HandleEvent(*event, err)) {return false;}
+			break;
+		case ULOG_NO_EVENT:
+			all_done = true;
+			break;
+		case ULOG_RD_ERROR:
+		case ULOG_UNK_ERROR:
+		case ULOG_INVALID:
+			dprintf(D_ALWAYS, "Failed to read reuse directory state file event.\n");
+			return false;
+		case ULOG_MISSED_EVENT:
+			dprintf(D_ALWAYS, "Missed an event in the directory state file.\n");
+			return false;
+		};
+	} while (!all_done);
+
+	std::sort(m_contents.begin(), m_contents.end(), [](std::unique_ptr<FileEntry> &left, std::unique_ptr<FileEntry> &right) {
+		return left->last_use() < right->last_use();
+	});
+}
+
+bool
+DataReuseDirectory::HandleEvent(ULogEvent & /*event*/, CondorError & /*err*/)
+{
+	return false;
 }
--- src/condor_utils/data_reuse.h
+++ src/condor_utils/data_reuse.h
@@ -1,5 +1,11 @@
 
+#include "read_user_log.h"
+#include "write_user_log.h"
+#include "MyString.h"
+
 #include <string>
+
+class CondorError;
 
 namespace htcondor {
 
@@ -12,15 +18,92 @@ public:
 	DataReuseDirectory(const DataReuseDirectory&) = delete;
 	DataReuseDirectory& operator=(const DataReuseDirectory*) = delete;
 
-	const std::string &getDirectory() const {return m_dirpath;}
+	const std::string &GetDirectory() const {return m_dirpath;}
+
+	bool ReserveSpace(uint64_t size, uint32_t time, std::string &tag,
+		std::string &id, CondorError &err);
+
+	bool Renew(uint32_t time, std::string &tag, const std::string &uuid,
+		CondorError &err);
+
+	bool ReleaseSpace(const std::string &uuid, CondorError &err);
+
+	bool CacheFile(const std::string &source, const std::string &checksum,
+		const std::string &checksum_type, const std::string &uuid,
+		CondorError &err);
+
+	void UsedFile(const std::string &checksum, const std::string &checksum_type,
+		const std::string &tag);
 
 private:
+	class LogSentry {
+	public:
+		LogSentry(DataReuseDirectory &parent);
+		~LogSentry();
+		LogSentry(const LogSentry&) = delete;
+		LogSentry(LogSentry&&) = default;
+		LogSentry& operator=(const LogSentry*) = delete;
+
+		bool acquired() const {return m_acquired;}
+		FileLockBase *lock() const {return m_lock;}
+	private:
+		bool m_acquired{false};
+		DataReuseDirectory &m_parent;
+		FileLockBase *m_lock{nullptr};
+	};
+
+	class FileEntry {
+	public:
+		FileEntry(DataReuseDirectory &parent, const std::string &checksum,
+			const std::string &checksum_type, const std::string &tag,
+			uint64_t size)
+		: m_size(size),
+		m_checksum(checksum),
+		m_checksum_type(checksum_type),
+		m_tag(tag),
+		m_parent(parent)
+		{}
+
+		uint64_t size() const {return m_size;}
+		const std::string &checksum() const {return m_checksum;}
+		const std::string &checksum_type() const {return m_checksum_type;}
+		const std::string &tag() const {return m_tag;}
+		std::string fname() const;
+		time_t last_use() const {return m_last_use;}
+		void update_last_use(time_t current) {m_last_use = current > m_last_use ? current : m_last_use;}
+
+	private:
+		uint64_t m_size{0};
+		time_t m_last_use{0};
+		std::string m_checksum;
+		std::string m_checksum_type;
+		std::string m_tag;
+		const DataReuseDirectory &m_parent;
+	};
+
+	bool ClearSpace(uint64_t size, LogSentry &sentry, CondorError &err);
+	bool UpdateState(LogSentry &sentry, CondorError &err);
+	bool HandleEvent(ULogEvent &event, CondorError &err);
+
+	LogSentry &&LockLog(CondorError &err);
+	bool UnlockLog(LogSentry sentry, CondorError &err);
+
 	void CreatePaths();
 	void Cleanup();
 
 	bool m_owner{true};
 	bool m_valid{false};
+
+	uint64_t m_reserved_space{0};
+	uint64_t m_allocated_space{0};
+
 	std::string m_dirpath;
+	MyString m_logname;
+
+	WriteUserLog m_log;
+	ReadUserLog m_rlog;
+
+	std::vector<std::unique_ptr<FileEntry>> m_contents;
 };
 
 }
