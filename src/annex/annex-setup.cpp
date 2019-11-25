30257cc9ea80e15263bc65fd3149cc12d32e6d31
Todd L Miller <tlmiller@cs.wisc.edu>
2019-11-25 15:33:33 -0600

    (#7400)  Update annex setup to version 9 -- update runtime to Node.js 12.x.

--- src/annex/annex-setup.cpp
+++ src/annex/annex-setup.cpp
@@ -258,7 +258,7 @@ setup( const char * region, const char * pukf, const char * prkf, const char * c
 	EC2GahpClient * ec2Gahp = startOneGahpClient( publicKeyFile, ec2URL );
 
 	// FIXME: Do something cleverer for versioning.
-	std::string bucketStackURL = "https://s3.amazonaws.com/condor-annex/bucket-8.json";
+	std::string bucketStackURL = "https://s3.amazonaws.com/condor-annex/bucket-9.json";
 	std::string bucketStackName = "HTCondorAnnex-ConfigurationBucket";
 	std::string bucketStackDescription = "configuration bucket (this takes less than a minute)";
 	std::map< std::string, std::string > bucketParameters;
@@ -272,7 +272,7 @@ setup( const char * region, const char * pukf, const char * prkf, const char * c
 		commandState, commandID );
 
 	// FIXME: Do something cleverer for versioning.
-	std::string lfStackURL = "https://s3.amazonaws.com/condor-annex/template-8.json";
+	std::string lfStackURL = "https://s3.amazonaws.com/condor-annex/template-9.json";
 	std::string lfStackName = "HTCondorAnnex-LambdaFunctions";
 	std::string lfStackDescription = "Lambda functions (this takes about a minute)";
 	std::map< std::string, std::string > lfParameters;
@@ -287,7 +287,7 @@ setup( const char * region, const char * pukf, const char * prkf, const char * c
 		commandState, commandID );
 
 	// FIXME: Do something cleverer for versioning.
-	std::string rStackURL = "https://s3.amazonaws.com/condor-annex/role-8.json";
+	std::string rStackURL = "https://s3.amazonaws.com/condor-annex/role-9.json";
 	std::string rStackName = "HTCondorAnnex-InstanceProfile";
 	std::string rStackDescription = "instance profile (this takes about two minutes)";
 	std::map< std::string, std::string > rParameters;
@@ -302,7 +302,7 @@ setup( const char * region, const char * pukf, const char * prkf, const char * c
 		commandState, commandID );
 
 	// FIXME: Do something cleverer for versioning.
-	std::string sgStackURL = "https://s3.amazonaws.com/condor-annex/security-group-8.json";
+	std::string sgStackURL = "https://s3.amazonaws.com/condor-annex/security-group-9.json";
 	std::string sgStackName = "HTCondorAnnex-SecurityGroup";
 	std::string sgStackDescription = "security group (this takes less than a minute)";
 	std::map< std::string, std::string > sgParameters;
