4d7507130c78c153da72cd63226409372779f588
Matyas Selmeci <matyas@cs.wisc.edu>
2020-02-21 12:25:45 -0600

    Don't let daemons try to increase the number of FDs if they can't
    Attempting to increase the ulimit and failing (due to lack of
    permissions) cause daemons (at least the shared port daemon) to go into
    an infinite crashing loop.  Set the MAX_FILE_DESCRIPTORS knobs to the
    current maximum if the user lacks the permission to increase them.

--- build/docker/services/base/start.sh
+++ build/docker/services/base/start.sh
@@ -8,18 +8,53 @@ fail () {
     exit 1
 }
 
+add_values_to () {
+    config=$1
+    shift
+    printf "%s=%s\n" >> /etc/condor/config.d/$config "$@"
+}
+
 # Create a config file from the environment.
 # The config file needs to be on disk instead of referencing the env
 # at run time so condor_config_val can work.
-printf "%s=%s\n" > /etc/condor/config.d/01-env.conf \
+echo "# This file was created by $prog" > /etc/condor/config.d/01-env.conf
+add_values_to 01-env.conf \
     CONDOR_HOST "${CONDOR_HOST:-\$(FULL_HOSTNAME)}" \
     NUM_CPUS "${NUM_CPUS:-1}" \
     MEMORY "${MEMORY:-1024}" \
     RESERVED_DISK "${RESERVED_DISK:-1024}" \
     USE_POOL_PASSWORD "${USE_POOL_PASSWORD:-no}"
 
+
 "$progdir/update-secrets"
 "$progdir/update-config"
+
+
+# Bug workaround: daemons will die if they can't raise the number of FD's;
+# cap the request if we can't raise it.
+
+hard_max=$(ulimit -Hn)
+
+# Try to raise the hard limit ourselves
+if ! ulimit -Hn $(( hard_max + 1 )) &>/dev/null; then
+    # We can't raise it.  Lower the limits in the condor config to the
+    # maximum allowable.
+    echo "# This file was created by $prog" > /etc/condor/config.d/01-fdfix.conf
+
+    for attr in COLLECTOR_MAX_FILE_DESCRIPTORS \
+                SHARED_PORT_MAX_FILE_DESCRIPTORS \
+                SCHEDD_MAX_FILE_DESCRIPTORS \
+                MAX_FILE_DESCRIPTORS; do
+        config_max=$(condor_config_val $attr 2>/dev/null)
+        if [[ -n $config_max && $config_max -gt $hard_max ]]; then
+            add_values_to 01-fdfix.conf "$attr" "$hard_max"
+        fi
+    done
+else
+    # We can raise it; reset it to default and let the daemons take care of
+    # raising it.
+    ulimit -Hn "$hard_max"
+fi
 
 # The master will crash if run as pid 1 (bug?) plus supervisor can restart
 # it if it dies, and gives us the ability to run other services.
