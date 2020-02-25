034e7669e8036c14208872107ecce06c82db17d9
Josh Karpel <karpel@wisc.edu>
2020-02-25 15:13:31 -0600

    (#7501) more classad unit tests

--- src/condor_tests/test_python_bindings_classad.py
+++ src/condor_tests/test_python_bindings_classad.py
@@ -5,6 +5,9 @@ import datetime
 
 import classad
 
+# to guarantee correct evaluation semantics
+import htcondor
+
 
 # this test replicates python_bindings_classad.run
 
@@ -94,7 +97,7 @@ def test_exprtree_eval(ad, expression, expected):
     assert ad["key"].eval() == expected
 
 
-def test_exprtree_eval_with_references(ad, ):
+def test_exprtree_eval_with_references(ad,):
     ad["ref"] = 1
     ad["key"] = classad.ExprTree("2 + ref")
 
@@ -161,10 +164,10 @@ def test_update_overwrites_existing_values(ad):
 
 
 def test_abstime_evals_to_datetime():
-    dt = classad.ExprTree('absTime("2013-11-12T07:50:23")').eval()
-
-    assert dt == datetime.datetime(
-        year = 2013, month = 11, day = 12, hour = 7, minute = 50, second = 23
+    assert classad.ExprTree(
+        'absTime("2013-11-12T07:50:23")'
+    ).eval() == datetime.datetime(
+        year=2013, month=11, day=12, hour=7, minute=50, second=23
     )
 
 
@@ -177,7 +180,7 @@ def test_quote(input, expected):
     assert classad.quote(input) == expected
 
 
-@pytest.mark.parametrize("input", ["foo", '"foo', r'\"foo'])
+@pytest.mark.parametrize("input", ["foo", '"foo', r"\"foo"])
 def test_quote_unquote_is_symmetric(input):
     assert classad.unquote(classad.quote(input)) == input
 
@@ -189,3 +192,194 @@ def test_register_custom_function():
     classad.register(concatenateLists)
 
     assert classad.ExprTree("concatenateLists({1, 2}, {3, 4})").eval() == [1, 2, 3, 4]
+
+
+def test_register_with_exception():
+    def bad(a, b):
+        raise Exception("oops")
+
+    classad.register(bad)
+
+    with pytest.raises(Exception) as e:
+        classad.ExprTree("bad(1, 2)").eval()
+
+    assert str(e.value) == "oops"
+
+
+def test_custom_function_can_see_python_values():
+    local = 1
+
+    def add(a):
+        return a + local
+
+    classad.register(add)
+
+    assert classad.ExprTree("add(1)").eval() == 2
+
+
+def test_evaluate_in_context_of_ad_with_matching_key(ad):
+    expr = classad.ExprTree("foo")
+    ad["foo"] = "bar"
+
+    assert expr.eval(ad) == "bar"
+
+
+def test_evaluate_in_context_of_ad_without_matching_key(ad):
+    expr = classad.ExprTree("foo")
+    ad["no"] = "bar"
+
+    assert expr.eval(ad) == classad.Value.Undefined
+
+
+@pytest.mark.parametrize(
+    "expr, expected",
+    [
+        (classad.Literal(1) + 2, 3),
+        (classad.Literal(1) + classad.Literal(2), 3),
+        (classad.Literal(1) - 2, -1),
+        (classad.Literal(1) * 2, 2),
+        (classad.Literal(2) / 2, 1),
+        (classad.Literal(1) & 2, 0),
+        (classad.Literal(1) | 2, 3),
+        (classad.Literal(1).and_(2), True),
+        (classad.Literal(1).and_(classad.Literal(2)), True),
+        (classad.Attribute("foo").is_(classad.Value.Undefined), True),
+    ],
+)
+def test_operators_produce_exprtrees_and_eval_as_expected(expr, expected):
+    assert isinstance(expr, classad.ExprTree)
+    assert expr.eval() == expected
+
+
+def test_subscript(ad):
+    ad["foo"] = [0, 1, 2, 3]
+    expr = classad.Attribute("foo")._get(2)
+
+    assert expr.eval(ad) == 2
+
+
+def test_function():
+    expr = classad.Function("strcat", "hello", " ", "world")
+
+    assert expr.eval() == "hello world"
+
+
+def test_flatten(ad):
+    ad["bar"] = 1
+    expr = classad.Attribute("foo") == classad.Attribute("bar")
+
+    assert ad.flatten(expr).sameAs(classad.ExprTree("foo == 1"))
+
+
+@pytest.mark.parametrize(
+    "left, right, matches",
+    [
+        (
+            classad.ClassAd("[foo = 3]"),
+            classad.ClassAd("[requirements = other.foo == 3]"),
+            True,
+        ),
+        (
+            classad.ClassAd("[requirements = other.foo == 3]"),
+            classad.ClassAd("[foo = 3]"),
+            False,
+        ),
+    ],
+)
+def test_matches(left, right, matches):
+    assert left.matches(right) is matches
+
+
+@pytest.mark.parametrize(
+    "left, right, matches",
+    [
+        (
+            classad.ClassAd("[foo = 3]"),
+            classad.ClassAd("[requirements = other.foo == 3]"),
+            False,
+        ),
+        (
+            classad.ClassAd("[requirements = other.foo == 3]"),
+            classad.ClassAd("[foo = 3]"),
+            False,
+        ),
+        (
+            classad.ClassAd("[requirements = other.foo == 3]"),
+            classad.ClassAd("[requirements = other.bar == 1; foo = 3]"),
+            False,
+        ),
+        (
+            classad.ClassAd("[requirements = other.foo == 3; bar = 1]"),
+            classad.ClassAd("[requirements = other.bar == 1; foo = 3]"),
+            True,
+        ),
+    ],
+)
+def test_symmetric_match(left, right, matches):
+    assert left.symmetricMatch(right) is matches
+
+
+@pytest.mark.parametrize(
+    "expr, value",
+    [
+        (classad.ExprTree("true || true"), True),
+        (classad.ExprTree("true || false"), True),
+        (classad.ExprTree("false || true"), True),
+        (classad.ExprTree("false || false"), False),
+        (classad.ExprTree("true && true"), True),
+        (classad.ExprTree("false && true"), False),
+        (classad.ExprTree("true && false"), False),
+        (classad.ExprTree("false && false"), False),
+        (classad.Literal(True).and_(True), True),
+        (classad.Literal(True).and_(False), False),
+        (classad.Literal(True).or_(False), True),
+        (classad.Literal(True).or_(False), True),
+        (classad.Literal(False).or_(False), False),
+    ],
+)
+def test_bool(expr, value):
+    assert bool(expr) is value
+
+
+def test_internal_refs(ad):
+    ad["bar"] = 2
+    expr = classad.ExprTree("foo =?= bar")
+
+    assert ad.internalRefs(expr) == ["bar"]
+
+
+def test_external_refs(ad):
+    ad["bar"] = 2
+    expr = classad.ExprTree("foo =?= bar")
+
+    assert ad.externalRefs(expr) == ["foo"]
+
+
+@pytest.mark.parametrize(
+    "expr, value",
+    [
+        (classad.ExprTree("1 + 3"), 4),
+        (classad.ExprTree("1.0 + 3.4"), 4),
+        (classad.ExprTree("1.0 + 3.5"), 4),
+        (classad.ExprTree("1.0 + 3.6"), 4),
+        (classad.ExprTree('"34"'), 34),
+    ],
+)
+def test_int(expr, value):
+    assert int(expr) == value
+
+
+@pytest.mark.parametrize(
+    "expr", [classad.ExprTree("foo"), classad.ExprTree('"34.foo"')]
+)
+def test_int_with_bad_values(expr):
+    with pytest.raises(ValueError):
+        print(int(expr))
+
+
+@pytest.mark.parametrize(
+    "expr, value",
+    [(classad.ExprTree("1.0 + 3.5"), 4.5), (classad.ExprTree("35.5"), 35.5)],
+)
+def test_float(expr, value):
+    assert float(expr) == value
