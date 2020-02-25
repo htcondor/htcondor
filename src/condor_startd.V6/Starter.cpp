236b39afbc08c9dea2e04b6c92123b9a0151d246
Todd L Miller <tlmiller@cs.wisc.edu>
2020-02-25 13:52:02 -0600

    (#7510)  Use the slot ID rather than 'starter' for the starter's shared port ID.

--- src/condor_startd.V6/Starter.cpp
+++ src/condor_startd.V6/Starter.cpp
@@ -1140,7 +1140,9 @@ int Starter::execDCStarter(
 	FamilyInfo fi;
 	fi.max_snapshot_interval = pid_snapshot_interval;
 
-	MyString daemon_sock = SharedPortEndpoint::GenerateEndpointName( "starter" );
+	std::string sockBaseName( "starter" );
+	if( claim ) { sockBaseName = claim->rip()->r_id_str; }
+	MyString daemon_sock = SharedPortEndpoint::GenerateEndpointName( sockBaseName.c_str() );
 	s_pid = daemonCore->
 		Create_Process( final_path, *final_args, PRIV_ROOT, reaper_id,
 		                TRUE, TRUE, env, NULL, &fi, inherit_list, std_fds,
