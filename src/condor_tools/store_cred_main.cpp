3223599e0d2aa68fcacb3d474e770d5e2d73382d
Greg Thain <gthain@cs.wisc.edu>
2020-03-10 08:03:13 -0500

    Check for negative return value from condor_full_read #6992

--- src/condor_tools/store_cred_main.cpp
+++ src/condor_tools/store_cred_main.cpp
@@ -217,6 +217,10 @@ int main(int argc, const char *argv[]) {
 					_setmode(_fileno(stdin), _O_BINARY);
 					#endif
 					credlen = _condor_full_read(fileno(stdin), cred.ptr(), max_len);
+					if (credlen < 0) {
+						fprintf(stderr, "ERROR: could read from stdin: %s\n", strerror(errno));
+						goto cleanup;
+					}
 				} else {
 					char *buf = NULL;
 					bool read_as_root = false;
