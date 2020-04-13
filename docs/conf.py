5f29f2752fa709d835db2e2d8d89ec5085af715e
Tim Theisen <tim@cs.wisc.edu>
2020-04-12 23:03:35 -0500

    Update version number in manual and add version history section.

--- docs/conf.py
+++ docs/conf.py
@@ -88,7 +88,7 @@ Licensed under the Apache License, Version 2.0.'
 # The short X.Y version.
 version = '8.9'
 # The full version, including alpha/beta/rc tags.
-release = '8.9.7'
+release = '8.9.8'
 
 rst_epilog = """
 .. |release_date| replace:: Month Day, 2020
