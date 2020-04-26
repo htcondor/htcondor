cc2bf0e49044753e1eb53a7bf2cca4cc5cc3532b
Tim Theisen <tim@cs.wisc.edu>
2020-04-26 07:53:56 -0500

    Update manual for 8.8.10

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ Licensed under the Apache License, Version 2.0.'
 # The short X.Y version.
 version = '8.8'
 # The full version, including alpha/beta/rc tags.
-release = '8.8.9'
+release = '8.8.10'
 
 rst_epilog = """
 .. |release_date| replace:: Month Day, 2020
