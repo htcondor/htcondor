c3ca289c4a994bb80c9cd9a0715647f9f558cee8
Josh Karpel <josh.karpel@gmail.com>
2020-06-04 16:45:23 -0500

    (#7501) tweak test_scheduler_priority.py to use more ornithology helpers

--- src/condor_tests/test_scheduler_priority.py
+++ src/condor_tests/test_scheduler_priority.py
@@ -1,87 +1,93 @@
 #!/usr/bin/env pytest
 
-import htcondor
 import logging
 
+import htcondor
 
 from ornithology import (
-    config,
-    standup,
     action,
     JobID,
-    SetAttribute,
-    SetJobStatus,
-    JobStatus,
-    in_order,
     SCRIPTS,
-    ClusterState
+    ClusterState,
 )
-
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+NUM_JOBS = 5
 
 @action
-def submit_equal_priority_jobs(test_dir, default_condor):
+def submit_equal_priority_jobs(default_condor):
     cluster = default_condor.submit(
         {
             "executable": SCRIPTS["sleep"],
             "arguments": "1",
             "universe": "scheduler",
-            "log": "scheduler_priority-equal.log"
+            "log": "scheduler_priority-equal.log",
         },
-        count=5
+        count=NUM_JOBS,
     )
     cluster.wait(condition=ClusterState.all_terminal)
     return cluster
 
+
 @action
-def submit_unequal_priority_jobs(test_dir, default_condor):
+def submit_unequal_priority_jobs(default_condor):
     cluster = default_condor.submit(
         {
             "executable": SCRIPTS["sleep"],
             "arguments": "1",
             "universe": "scheduler",
             "log": "scheduler_priority-unequal.log",
-            "priority": "$(process)"
+            "priority": "$(process)",
         },
-        count=5
+        count=NUM_JOBS,
     )
     cluster.wait(condition=ClusterState.all_terminal)
     return cluster
 
-@action
-def equal_priority_events(submit_equal_priority_jobs):
-    jel = htcondor.JobEventLog("scheduler_priority-equal.log")
-    execute_events = []
-    for event in jel.events(0):
-        if event.type == htcondor.JobEventType.EXECUTE:
-            execute_events.append(event)
-    return execute_events
 
 @action
-def unequal_priority_events(submit_unequal_priority_jobs):
-    jel = htcondor.JobEventLog("scheduler_priority-unequal.log")
-    execute_events = []
-    for event in jel.events(0):
-        if event.type == htcondor.JobEventType.EXECUTE:
-            execute_events.append(event)
-    return execute_events
+def equal_priority_execute_events(submit_equal_priority_jobs):
+    return submit_equal_priority_jobs.event_log.filter(
+        lambda event: event.type is htcondor.JobEventType.EXECUTE
+    )
+
+
+@action
+def unequal_priority_execute_events(submit_unequal_priority_jobs):
+    return submit_unequal_priority_jobs.event_log.filter(
+        lambda event: event.type is htcondor.JobEventType.EXECUTE
+    )
 
 
 class TestCanRunSleepJob:
+    def test_ran_all_equal_priority_jobs(self, equal_priority_execute_events):
+        assert len(equal_priority_execute_events) == NUM_JOBS
 
-    # We expect equal priority jobs to run in the order they were submitted.
-    # So each execution procid in the list to be lesser than the one after it.
-    def test_equal_priority_jobs_run_in_submit_order(self, equal_priority_events):
-        for i in range(1,4):
-            assert JobID.from_job_event(equal_priority_events[i]).proc > JobID.from_job_event(equal_priority_events[i-1]).proc
+    def test_equal_priority_jobs_run_in_submit_order(self, equal_priority_execute_events):
+        """
+        We expect equal priority jobs to run in the order they were submitted,
+        which means they should run in job-id-order.
+        """
+        assert (
+            sorted(equal_priority_execute_events, key=lambda event: JobID.from_job_event(event))
+            == equal_priority_execute_events
+        )
 
+    def test_ran_all_unequal_priority_jobs(self, unequal_priority_execute_events):
+        assert len(unequal_priority_execute_events) == NUM_JOBS
 
-    # We expect unequal priority jobs to run in the order of priority.
-    # So each execution procid in the list to be greater than the one after it.
-    def test_unequal_priority_jobs_run_in_priority_order(self, unequal_priority_events):
-        for i in range(1,4):
-            assert JobID.from_job_event(unequal_priority_events[i]).proc < JobID.from_job_event(unequal_priority_events[i-1]).proc
-
+    def test_unequal_priority_jobs_run_in_priority_order(self, unequal_priority_execute_events):
+        """
+        We expect unequal priority jobs to run in the order of priority,
+        which for the set up above, means they should run in reverse-job-id-order.
+        """
+        assert (
+            sorted(
+                unequal_priority_execute_events,
+                key=lambda event: JobID.from_job_event(event),
+                reverse=True,
+            )
+            == unequal_priority_execute_events
+        )
