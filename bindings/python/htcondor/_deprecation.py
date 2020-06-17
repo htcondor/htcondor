df66b9acd037fa556fee1c207e044cc6ae507690
Todd L Miller <tlmiller@cs.wisc.edu>
2020-06-17 15:19:23 -0500

    (#7690)  Final tweaks.

--- bindings/python/htcondor/_deprecation.py
+++ bindings/python/htcondor/_deprecation.py
@@ -47,7 +47,6 @@ def deprecate_method(message, cls, method=None):
         method = cls.__init__
 
     setattr(cls, method.__name__, deprecate(message)(method))
-
     return cls
 
 
@@ -55,34 +54,46 @@ def deprecate_class(message, cls):
     add_deprecation_doc_warning(cls, message)
     return deprecate_method(message, cls, None)
 
+
 # deprecations by version of HTCondor; call these in __init__.py
 
-def deprecate_8_9_8():
-    deprecate_class(
-        "LogReader is deprecated since v8.9.8 and will be removed in a future release.",
-        htcondor.LogReader,
-    )
 
+def deprecate_8_9_8():
+    ## from python-bindings/negotiator.cpp
     deprecate_class(
         "Negotiator is deprecated since v8.9.8 and will be removed in a future release.",
         htcondor.Negotiator,
     )
 
-    # These classes can't be constructed from Python, so maybe we don't need this?
+    ## from python-bindings/log_reader.cpp
+
+    # FIXME: deprecate htcondor.EntryType enum
+
+    deprecate_class(
+        "LogReader is deprecated since v8.9.8 and will be removed in a future release.",
+        htcondor.LogReader,
+    )
+
+    ## from python-bindings/event.cpp
+
+    # FIXME: deprecate htcondor.LockType enum
+
+    # This class can't be constructed from Python, so this is documentary.
     deprecate_class(
         "EventIterator is deprecated since v8.9.8 and will be removed in a future release.",
         htcondor.EventIterator,
     )
+
+    # This class can't be constructed from Python, so this is documentary.
     deprecate_class(
         "FileLock is deprecated since v8.9.8 and will be removed in a future release.",
         htcondor.EventIterator,
     )
 
-    htcondor.lock = deprecate("lock is deprecated since v8.9.8 and will be removed in a future release.")(htcondor.lock)
+    htcondor.lock = deprecate(
+        "lock() is deprecated since v8.9.8 and will be removed in a future release."
+    )(htcondor.lock)
 
     htcondor.read_events = deprecate(
-        "read_events is deprecated since v8.9.8 and will be removed in a future release; use JobEventLog instead."
+        "read_events() is deprecated since v8.9.8 and will be removed in a future release; use JobEventLog instead."
     )(htcondor.read_events)
-
-    # FIXME: deprecate htcondor.EntryType enum
-    # FIXME: deprecate htcondor.LockType enum
