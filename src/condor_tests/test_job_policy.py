a1bada541a773e3cfbf51ac03bc7af6be5e4060a
Todd L Miller <tlmiller@cs.wisc.edu>
2020-07-02 12:32:23 -0500

    (#7501)  The great refactoring.  (Also uncomment the two newer tests.)

--- src/condor_tests/test_job_policy.py
+++ src/condor_tests/test_job_policy.py
@@ -5,108 +5,30 @@
 #
 
 import logging
-import textwrap
-from pathlib import Path
-
-from ornithology import (
-    config,
-    standup,
-    action,
-    JobStatus,
-    ClusterState,
-    Condor,
-)
-
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+from ornithology import (
+    action,
+    JobStatus,
+    ClusterState,
+)
 
-# This layer of indirection may not be necessary if we're not parameterizing.
-@config
-def condor_config():
-    config = {
-        "STARTD_DEBUG": "D_SUB_SECOND D_FULLDEBUG D_JOB",
-        "MAXJOBRETIREMENTTIME": 300,
-    }
-    raw_config = textwrap.dedent(
-        """
-        use feature : PartitionableSlot
-        use policy : HOLD_IF_MEMORY_EXCEEDED
-    """
-    )
-    return {"config": config, "raw_config": raw_config}
+from job_policy_testing import (
+    condor_config,
+    condor,
+    job_file,
+    kill_file,
+    job_parameters,
+    job,
 
-
-@standup
-def condor(condor_config, test_dir):
-    with Condor(test_dir / "condor", **condor_config) as condor:
-        yield condor
-
-
-@standup
-def job_file(test_dir):
-    job_file = test_dir / "condition_sleep.py"
-    contents = textwrap.dedent(
-        """
-        import os
-        import sys
-        import time
-        import signal
-
-        signal.signal( signal.SIGTERM, signal.SIG_IGN )
-        killFile = sys.argv[1]
-        maxSleep = sys.argv[2]
-
-        for i in range( 0, int(maxSleep) ):
-            if os.path.isfile( killFile ):
-                memoryWaste = list( range( 0, 1024 * 1024 * 16 ) )
-            time.sleep( 1 )
-
-        sys.exit( 0 )
-        """
-    )
-    with open(str(job_file), "w") as f:
-        f.write(contents)
-    return job_file
+    ask_to_misbehave,
+)
 
 
 @action
-def kill_file(test_dir):
-    return test_dir / "killfile"
-
-
-@action
-def job_parameters(job_file, kill_file):
-    return {
-        "executable": "/usr/bin/env",
-        "transfer_executable": "false",
-        "should_transfer_files": "true",
-        "transfer_input_files": str(job_file),
-        "universe": "vanilla",
-        "arguments": "python {} {}.$(CLUSTER).$(PROCESS) 3600".format(
-            str(job_file), kill_file
-        ),
-        "request_memory": 1,
-        "log": "test_job_policy.log",
-    }
-
-
-@action
-def job(condor, job_parameters):
-    job = condor.submit(job_parameters, count=1)
-
-    yield job
-
-    job.remove()
-
-
-def ask_to_misbehave(job, proc, kill_file):
-    Path("{}.{}.{}".format(kill_file, job.clusterid, proc)).touch()
-
-
-@action
-def job_after_signal(job, kill_file):
+def misbehaving_job(job, kill_file):
     assert job.wait(
         condition=ClusterState.all_running,
         timeout=60,
@@ -118,8 +40,8 @@ def job_after_signal(job, kill_file):
 
 
 class TestHoldIfMemoryExceeded:
-    def test_job_is_held(self, job_after_signal):
-        assert job_after_signal.wait(
+    def test_job_is_held(self, misbehaving_job):
+        assert misbehaving_job.wait(
             condition=ClusterState.all_held,
             timeout=60,
             fail_condition=lambda self: self.any_status(
--- src/condor_tests/test_job_policy_while_draining.py
+++ src/condor_tests/test_job_policy_while_draining.py
@@ -4,141 +4,33 @@
 # Does HOLD_IF_MEMORY_EXCEEDED work on jobs while the startd is draining?
 #
 
-import time
 import logging
-import textwrap
-from pathlib import Path
-
-import htcondor
-
-from ornithology import (
-    config,
-    standup,
-    action,
-    JobStatus,
-    ClusterState,
-    Condor,
-)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+from ornithology import (
+    action,
+    JobStatus,
+    ClusterState,
+)
 
-# This layer of indirection may not be necessary if we're not parameterizing.
-@config
-def condor_config():
-    config = {
-        "STARTD_DEBUG": "D_SUB_SECOND D_FULLDEBUG D_JOB",
-        "MAXJOBRETIREMENTTIME": 300,
-    }
-    raw_config = textwrap.dedent(
-        """
-        use feature : PartitionableSlot
-        use policy : HOLD_IF_MEMORY_EXCEEDED
-    """
-    )
-    return {"config": config, "raw_config": raw_config}
+from job_policy_testing import (
+    condor_config,
+    condor,
+    job_file,
+    kill_file,
+    job_parameters,
+    job,
 
-
-@standup
-def condor(condor_config, test_dir):
-    with Condor(test_dir / "condor", **condor_config) as condor:
-        yield condor
-
-
-@standup
-def job_file(test_dir):
-    job_file = test_dir / "condition_sleep.py"
-    contents = textwrap.dedent(
-        """
-        import os
-        import sys
-        import time
-        import signal
-
-        signal.signal( signal.SIGTERM, signal.SIG_IGN )
-        killFile = sys.argv[1]
-        maxSleep = sys.argv[2]
-
-        for i in range( 0, int(maxSleep) ):
-            if os.path.isfile( killFile ):
-                memoryWaste = list( range( 0, 1024 * 1024 * 16 ) )
-            time.sleep( 1 )
-
-        sys.exit( 0 )
-        """
-    )
-    with open(str(job_file), "w") as f:
-        f.write(contents)
-    return job_file
+    ask_to_misbehave,
+    issue_drain_command,
+    wait_for_draining_to_start,
+)
 
 
 @action
-def kill_file(test_dir):
-    return test_dir / "killfile"
-
-
-@action
-def job_parameters(job_file, kill_file):
-    return {
-        "executable": "/usr/bin/env",
-        "transfer_executable": "false",
-        "should_transfer_files": "true",
-        "transfer_input_files": str(job_file),
-        "universe": "vanilla",
-        "arguments": "python {} {}.$(CLUSTER).$(PROCESS) 3600".format(
-            str(job_file), kill_file
-        ),
-        "request_memory": 1,
-        "log": "test_job_policy.log",
-    }
-
-
-@action
-def job(condor, job_parameters):
-    job = condor.submit(job_parameters, count=1)
-
-    yield job
-
-    job.remove()
-
-
-def ask_to_misbehave(job, proc, kill_file):
-    Path("{}.{}.{}".format(kill_file, job.clusterid, proc)).touch()
-
-
-def issue_drain_command(condor):
-    with condor.use_config():
-        hostname = htcondor.param["FULL_HOSTNAME"]
-
-    rv = condor.run_command(["condor_drain", "-start", "IsBackfill == true", hostname])
-    assert rv.returncode == 0
-
-
-def wait_for_draining_to_start(condor):
-    collector = condor.get_local_collector()
-    for delay in range(1, 20):
-        time.sleep(1)
-        ads = collector.query(
-            htcondor.AdTypes.Startd, True, ["Name", "State", "Activity"]
-        )
-
-        foundClaimedRetiring = False
-        for ad in ads:
-            if ad["Name"].startswith("slot1@"):
-                continue
-            if ad["State"] == "Claimed" and ad["Activity"] == "Retiring":
-                foundClaimedRetiring = True
-                break
-
-        if foundClaimedRetiring:
-            break
-
-    assert foundClaimedRetiring
-
-
-@action
-def misbehaving_job(condor, job, kill_file):
+def misbehaving_draining_job(condor, job, kill_file):
     assert job.wait(
         condition=ClusterState.all_running,
         timeout=60,
@@ -153,8 +45,8 @@ def misbehaving_job(condor, job, kill_file):
 
 
 class TestJob:
-    def test_misbehaving_job_held(self, misbehaving_job):
-        assert misbehaving_job.wait(
+    def test_misbehaving_job_held(self, misbehaving_draining_job):
+        assert misbehaving_draining_job.wait(
             condition=ClusterState.all_held,
             timeout=60,
             fail_condition=lambda self: self.any_status(
