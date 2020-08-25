8fa1f037eb9fa360befe7a0fb1d438eaba669200
Todd L Miller <tlmiller@cs.wisc.edu>
2020-08-25 09:17:33 -0500

    (#7501)  Fix new manual build issues.

--- src/condor_tests/ornithology-docs/source/conf.py
+++ src/condor_tests/ornithology-docs/source/conf.py
@@ -9,12 +9,10 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
 
-import dask_chtc
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
@@ -61,7 +59,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 # -- Extension Configuration -------------------------------------------------
 
