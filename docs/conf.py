e9d39462c0879ff63d516bbc9291be2a5e351a70
Tim Theisen <tim@cs.wisc.edu>
2020-09-21 13:19:49 -0500

    Add version history for 8.9.10

--- docs/conf.py
+++ docs/conf.py
@@ -62,7 +62,7 @@ Licensed under the Apache License, Version 2.0.'
 # The short X.Y version.
 version = '8.9'
 # The full version, including alpha/beta/rc tags.
-release = '8.9.9'
+release = '8.9.10'
 
 rst_epilog = """
 .. |release_date| replace:: Month Day, 2020
