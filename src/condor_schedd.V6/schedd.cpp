9f32e0ddde3d8356c206dc5a3a8f5610f3a937b9
John (TJ) Knoeller <johnkn@cs.wisc.edu>
2020-10-01 15:15:30 -0500

    add null check to schedd.lookup_owner_const, since callers might pass null here, #6992

--- src/condor_schedd.V6/schedd.cpp
+++ src/condor_schedd.V6/schedd.cpp
@@ -3162,6 +3162,7 @@ Scheduler::find_ownerinfo(const char * owner)
 const OwnerInfo *
 Scheduler::lookup_owner_const(const char * owner)
 {
+	if ( ! owner) return NULL;
 	return find_ownerinfo(owner);
 }
 
