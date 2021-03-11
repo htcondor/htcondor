31c7b49ea2cbc231e25b38e7109c10ff89c8fda8
John (TJ) Knoeller <johnkn@cs.wisc.edu>
2021-03-11 09:41:49 -0600

    Revert "Fix array bounds read error in config HTCONDOR-316"
    This reverts commit 9c79712e4de81f58b4b540d259ea5f6ba2066382.

--- src/condor_utils/config.cpp
+++ src/condor_utils/config.cpp
@@ -2778,7 +2778,7 @@ char * strcpy_quoted(char* out, const char* str, int cch, char quoted) {
 
 	// copy, adding quotes or not as requested.
 	if (quoted) { *p++ = quoted; }
-	memcpy(p, str, std::min(cch, (int)strlen(str)));
+	memcpy(p, str, cch*sizeof(str[0]));
 	if (quoted) { p[cch++] = quoted; }
 	p[cch] = 0;
 
