b340b90eb74f5971bc1f76a133ce90133fe778cc
Tim Theisen <tim@cs.wisc.edu>
2021-04-07 15:52:55 -0500

    Bump version number to 9.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ Licensed under the Apache License, Version 2.0.'
 # The short X.Y version.
 version = '9.0'
 # The full version, including alpha/beta/rc tags.
-release = '9.0.0'
+release = '9.0.1'
 
 rst_epilog = """
 .. |release_date| replace:: Month Day, 2021
