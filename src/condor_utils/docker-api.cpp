6aa82d03e95fb79011155c07930b82ad25b65ec7
Greg Thain <gthain@cs.wisc.edu>
2021-04-30 14:39:28 -0500

    Run Docker Universe jobs under init HTCONDOR-462

--- src/condor_utils/docker-api.cpp
+++ src/condor_utils/docker-api.cpp
@@ -158,6 +158,10 @@ int DockerAPI::createContainer(
 		runArgs.AppendArg("--security-opt");
 		runArgs.AppendArg("no-new-privileges");
 	}
+	// Tell docker to run this with an init program
+	if (param_boolean("DOCKER_RUN_UNDER_INIT", true)) {
+		runArgs.AppendArg("--init");
+	}
 
 	// Give the container a useful name
 	std::string hname = makeHostname(&machineAd, &jobAd);
