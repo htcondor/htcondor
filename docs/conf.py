2bf1e71a8b7888a63714d2f41481026d7f92717f
Tim Theisen <tim@cs.wisc.edu>
2021-05-11 08:00:26 -0500

    Update version in manual to 9.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ Licensed under the Apache License, Version 2.0.'
 # The short X.Y version.
 version = '9.1'
 # The full version, including alpha/beta/rc tags.
-release = '9.1.0'
+release = '9.1.1'
 
 rst_epilog = """
 .. |release_date| replace:: Month Day, 2021
