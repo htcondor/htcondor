36b4716f28c38f0a55fd9a9b42a50ad9b54707d5
Todd L Miller <tlmiller@cs.wisc.edu>
2021-05-14 11:40:38 -0500

    Fix problem building manual on systems with the JIRA API installed.

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,10 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-# sys.path.insert(0, os.path.abspath('exts'))
-sys.path.append(os.path.abspath('extensions'))
+#
+# If this is not inserted at the beginning of the list, our 'jira' ticket
+# extension is hidden by the system 'jira' API, if it exists.
+sys.path.insert(0, os.path.abspath('extensions'))
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'src', 'condor_tests'))
 
 # -- General configuration ------------------------------------------------
