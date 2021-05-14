ce344745622ab05a00c1f341f29f8389a735f07a
Jaime Frey <jfrey@cs.wisc.edu>
2021-05-14 15:08:53 -0500

    Remove unneeded NULL check. HTCONDOR-416

--- src/condor_schedd.V6/grid_universe.cpp
+++ src/condor_schedd.V6/grid_universe.cpp
@@ -426,7 +426,7 @@ GridUniverseLogic::StartOrFindGManager(const char* owner, const char* domain,
 
 
 #ifndef WIN32
-	if (owner && strcasecmp(owner, "root") == 0 ) {
+	if (strcasecmp(owner, "root") == 0 ) {
 		dprintf(D_ALWAYS, "Tried to start condor_gmanager as root.\n");
 		return NULL;
 	}
