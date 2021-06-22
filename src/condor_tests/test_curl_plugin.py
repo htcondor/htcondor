2f55661547f1d89232fd0b0949165147662a86b3
Mark Coatsworth <coatsworth@cs.wisc.edu>
2021-06-22 16:10:24 -0500

    Added multiple file check to test_curl_plugin (HTCONDOR-546)

--- src/condor_tests/test_curl_plugin.py
+++ src/condor_tests/test_curl_plugin.py
@@ -31,6 +31,14 @@ def server():
         yield httpserver
 
 
+@action
+def start_log(test_dir):
+    start_log = open(test_dir / "condor" / "log" / "StartLog", "r")
+    contents = start_log.readlines()
+    start_log.close()
+    return contents
+
+
 @action(params={"404": 404, "500": 500})
 def bad_url(server, request):
     server.expect_request("/badurl").respond_with_data(status=request.param)
@@ -40,6 +48,20 @@ def bad_url(server, request):
 @action
 def good_url(server):
     server.expect_request("/goodurl").respond_with_data("Great success!")
+    return "http://localhost:{}/goodurl".format(server.port)
+
+
+@action
+def multiple_urls(server):
+    server.expect_request("/url1").respond_with_data("url1")
+    server.expect_request("/url2").respond_with_data("url2")
+    server.expect_request("/url3").respond_with_data("url3")
+    return "http://localhost:{}/url1, http://localhost:{}/url2, http://localhost:{}/url3".format(server.port, server.port, server.port)
+
+
+@action
+def partial_content(server):
+    server.expect_request("/partialcontent").r
     return "http://localhost:{}/goodurl".format(server.port)
 
 
@@ -73,6 +95,21 @@ def job_with_bad_url(default_condor, bad_url, test_dir, path_to_sleep):
     assert job.wait(condition=ClusterState.all_terminal)
     return job
 
+@action
+def job_with_multiple_urls(default_condor, multiple_urls, test_dir, path_to_sleep):
+    job = default_condor.submit(
+        {
+            "executable": path_to_sleep,
+            "arguments": "1",
+            "log": (test_dir / "multiple_urls.log").as_posix(),
+            "transfer_input_files": multiple_urls,
+            "transfer_output_files": "url1, url2, url3",
+            "should_transfer_files": "YES",
+        }
+    )
+    assert job.wait(condition=ClusterState.all_terminal)
+    return job
+
 
 class TestCurlPlugin:
     def test_job_with_good_url_succeeds(self, job_with_good_url):
@@ -85,3 +122,15 @@ class TestCurlPlugin:
 
     def test_job_with_bad_url_holds(self, job_with_bad_url):
         assert job_with_bad_url.state[0] == JobStatus.HELD
+
+    def test_job_with_multiple_urls_succeeds(self, job_with_multiple_urls):
+        assert job_with_multiple_urls.state[0] == JobStatus.COMPLETED
+
+    def test_job_with_multiple_urls_invokes_plugin_once(self, job_with_multiple_urls, start_log):
+        plugin_invocations = 0
+        for line in start_log:
+            if "invoking" in line:
+                plugin_invocations += 1
+        # We actually expect to see 5 invocations of the curl_plugin because all the other tests are logged in the same file.
+        # TODO: Figure out a better way to isolate this so we're only looking for a single invocation
+        assert plugin_invocations == 5
