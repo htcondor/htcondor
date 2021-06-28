9a639c5caf8e16764038d51ef69dcac0ab912f09
Todd Tannenbaum <tannenba@cs.wisc.edu>
2021-06-28 12:54:15 -0500

    Improvements to hold aggregates regression test.
    Better test: Have the job go on hold twice to make sure aggregates
    increment as expected.  Also assert correctness of more hold attributes.
    Faster test: Instead of starting all daemons in the personal condor,
    just start a schedd.  Also use scheduler universe job - no need
    to wait for matchmaking etc.
    HTCONDOR-554

--- src/condor_tests/test_num_holds.py
+++ src/condor_tests/test_num_holds.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env pytest
 
-from ornithology import (
-    config,
-    standup,
-    action,
-    Condor,
-    SetAttribute,
-    SetJobStatus,
-    JobStatus,
-    in_order,
-)
+# Test that when a job goes on hold that attributes such as
+# NumHolds, NumHoldsByReason, and HoldReasonCode are set properly.
+# To allow this test to run faster, just setup a pool with a schedd alone
+# and submit a scheduler universe job.
 
+from ornithology import *
+
+#
+# Setup a personal condor with just a schedd - no need for anything else
+#
 @standup
 def condor(test_dir):
     with Condor(
@@ -22,49 +21,52 @@ def condor(test_dir):
     ) as condor:
         yield condor
 
+#
+# Submit a job that will immediately go on hold due to job policy expressions.
+# We use scheduler universe so it runs quickly; no need to wait for matches etc.
+#
 @action
-def job_queue_events_for_sleep_job(condor, path_to_sleep):
-    # Submit a single job that sleeps for 0 seconds.
+def job(condor, path_to_sleep):
     handle = condor.submit(
-            description={"executable": path_to_sleep, "arguments": "1", 
-                "universe": "local",
-                "on_exit_hold": "true"
+            description={"executable": "/bin/true",
+                "universe": "scheduler",
+                "on_exit_hold": "true",
+                "log": "job_events.log"
                 },
         count=1,
     )
 
-    # The id (cluster.proc) of the first (and only) job in the submit.
-    jobid = handle.job_ids[0]
+    # Wait for job to go on hold the first time...
+    assert handle.wait(condition=ClusterState.any_held,timeout=60)
+    # ...and then release it....
+    handle.release()
+    # and now wait for job to be held a second time... this way we can confirm hold counters 
+    # are incrementing as they should.
+    assert handle.wait(condition=ClusterState.any_held,timeout=60)
 
-    # Wait for (and react to) events in the job queue log.
-    condor.job_queue.wait_for_events(
-        {  
-            jobid: [
-                # wait for the job to go on hold
-                SetJobStatus(JobStatus.HELD),
-            ]
-        },
-        # All waits must have a timeout.
-        timeout=120,
-    )
+    # Return the first (and only) job ad in the cluster for testing class to reference
+    return handle.query()[0]
 
-    # This returns all of the job events in the job queue log for the jobid,
-    # making that information available to downstream fixtures/tests.
-    return condor.job_queue.by_jobid[jobid]
-
-
-# All tests must be methods of classes; the name of the class must be Test*
+#
+# The tests.... assertions on hold aggregates in the job ad
+#
 class TestJobHoldAggregates:
     # Methods that begin with test_* are tests.
 
-    # Test to check that NumReasonCode=3 (JobPolicy)
-    def test_hold_reason_code_was_3(self, job_queue_events_for_sleep_job):
-        assert SetAttribute("HoldReasonCode", "3") in job_queue_events_for_sleep_job
-    
-    # Test to check that NumHoldsByReason = [ JobPolicy = 1 ]
-    def test_hold_num_holds_was_1(self, job_queue_events_for_sleep_job):
-        assert SetAttribute("NumHoldsByReason", "[ JobPolicy = 1 ]") in job_queue_events_for_sleep_job
+    def test_holdreasoncode(self, job):
+        assert job["HoldReasonCode"] == 3
 
-    # Test to check that NumHolds = 1
-    def test_hold_num_holds_was_1(self, job_queue_events_for_sleep_job):
-        assert SetAttribute("NumHolds", "1") in job_queue_events_for_sleep_job
+    def test_lastholdreasoncode(self, job):
+        assert job["LastHoldReasonCode"] == 3
+
+    def test_holdreasonsubcode(self, job):
+        assert job["HoldReasonSubCode"] == 0
+
+    def test_lastholdsubreasoncode(self, job):
+        assert job["LastHoldReasonSubCode"] == 0
+
+    def test_hold_numholds(self, job):
+        assert job["NumHolds"] == 2
+
+    def test_hold_numholdsbyreason_was_policy(self, job):
+        assert job["NumHoldsByReason"] == { 'JobPolicy' : 2 }
