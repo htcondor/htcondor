5f3c945c56c382ee987098c9036e65ca4718d397
Greg Thain <gthain@cs.wisc.edu>
2021-08-05 10:34:37 -0500

    Fix job_dagman_rm test by changing job exe to not depend on CondorTest.pm, as it installs signal handlers HTCONDOR-536

--- src/condor_tests/job_dagman_rm-wait.pl
+++ src/condor_tests/job_dagman_rm-wait.pl
@@ -1,11 +1,12 @@
 #! /usr/bin/env perl
 
-use lib '.';
-use CondorTest;
-use CondorUtils;
-
 print "$ARGV[0] started\n";
-runcmd("touch $ARGV[0].started");
+
+# Touch file, portably, without external command
+open(F, ">>$ARGV[0].started");
+close(F);
+
+print ">>$ARGV[0].started\n";
 
 print "$ARGV[0] waiting for DAGMan to exit\n";
 $lockfile = "job_dagman_rm.dag.lock";
@@ -25,7 +26,8 @@ sleep(1000);
 
 
 # We should get condor_rm'ed before we get to here...
-runcmd("touch $ARGV[0].finished");
+open(G, ">>$ARGV[0].finished");
+close(G);
 print "$ARGV[0] finished\n";
 
 exit(0);
