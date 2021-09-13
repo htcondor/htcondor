9cb7be9dd4f3489fc83b344bc74944a0dcd4dc19
Jason Patton <jpatton@cs.wisc.edu>
2021-09-13 10:46:59 -0500

    Refactor htcondor CLI tool HTCONDOR-721
    - Use argparse subparsers to handle nouns and verbs
    - Create noun and verb base classes
    - Use logging library to print messages
    - Raise errors handled by instead of exiting (all exiting handled in cli.main())
    - Renamed htcondor_cli.py to cli.py to disambiguate library imports
    - Put useful globals in __init__.py
    - Remove unnecessary file close statements

--- src/condor_tools/htcondor_cli/conf.py
+++ src/condor_tools/htcondor_cli/conf.py
@@ -1,8 +1,2 @@
-import os
-import tempfile
-import time
 
-from pathlib import Path
 
-TMP_DIR = Path(os.environ.get("_CONDOR_TMPDIR", Path(tempfile.gettempdir()))
-    / Path("htcondor_cli") / Path(str(time.time())))
