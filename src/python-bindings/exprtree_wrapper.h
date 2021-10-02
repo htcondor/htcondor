2e4b50417181e9b3b846e8e58199dde43ea0349a
Todd L Miller <tlmiller@cs.wisc.edu>
2021-10-01 19:39:57 -0500

    (HTCONDR-707)  Add -target-file option to `classad_eval`. Extends simplify() to allow specification of a TARGET ad.

--- src/python-bindings/exprtree_wrapper.h
+++ src/python-bindings/exprtree_wrapper.h
@@ -27,7 +27,8 @@ struct ExprTreeHolder
     boost::python::object Evaluate(
         boost::python::object scope=boost::python::object() ) const;
     ExprTreeHolder simplify(
-        boost::python::object scope=boost::python::object() ) const;
+        boost::python::object scope=boost::python::object(),
+        boost::python::object target=boost::python::object() ) const;
 
     std::string toRepr() const;
 
@@ -76,7 +77,7 @@ struct ExprTreeHolder
     static void init();
 
 private:
-    void eval( boost::python::object scope, classad::Value & v ) const;
+    void eval( boost::python::object scope, classad::Value & v, boost::python::object target = boost::python::object() ) const;
 
     classad::ExprTree *m_expr;
     boost::shared_ptr<classad::ExprTree> m_refcount;
