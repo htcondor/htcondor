971432740ef0ff78fbd25c7c816018cb32768894
Todd L Miller <tlmiller@cs.wisc.edu>
2021-12-20 16:26:47 -0600

    (HTCONDOR-815)  Implement the third (self-checkpointing) test from the ticket.

--- src/condor_tests/test_htcondor_809.py
+++ src/condor_tests/test_htcondor_809.py
@@ -18,6 +18,12 @@ logger.setLevel(logging.DEBUG)
 # See HTCONDOR-815 for a detailed explanation of what we're testing here.
 #
 
+# -----------------------------------------------------------------------------
+
+#
+# If we specify a file twice, does the second version win?
+#
+
 @action
 def path_to_ordering_script(test_dir):
     script="""
@@ -208,6 +214,132 @@ def directory_job_lines(directory_job_handle, default_condor, test_dir):
 
 # -----------------------------------------------------------------------------
 
+#
+# Test a self-checkpointing job with preserve_relative_paths set and which
+# names the same directory in transfer_*_files.  Like the previous test,
+# the job here vacates itself.  At some point in the glorious future, we
+# will no longer have to run `condor_reschedule` after that happens, and
+# we'll be able to run this test concurrently with the others, in a fire-
+# and-forget manner.
+#
+
+@action
+def path_to_prp_script(default_condor, test_dir):
+    script=f"""
+    #!/bin/bash
+    export CONDOR_CONFIG={default_condor.config_file}
+    export PATH=$PATH:{os.environ["PATH"]}
+
+    DATA=`tail -n 1 {test_dir}/prp-test-file`
+    echo "Starting up..." >> {test_dir}/prp-test-file
+
+    if [[ $DATA == "" ]]; then
+        echo "step one" >> {test_dir}/prp-test-file
+        echo "step one" >> prp/data/data_file
+        exit 85
+    fi
+    if [[ $DATA == "step one" ]]; then
+        echo "step two" >> {test_dir}/prp-test-file
+        echo "step two" >> prp/data/data_file
+        condor_vacate_job $1
+        # Don't exit before we've been vacated.
+        sleep 60
+        # We did not succeed.
+        exit 1
+    fi
+    if [[ $DATA == "step two" ]]; then
+        echo "step three" >> {test_dir}/prp-test-file
+        echo "step three" >> prp/data/data_file
+        exit 0
+    fi
+    echo "step never-never" >> {test_dir}/prp-test-file
+    echo "step never-never" >> prp/data/data_file
+    exit 1
+    """
+
+    path = test_dir / "prp.sh"
+    write_file(path, format_script(script))
+
+    return path
+
+
+@action
+def prp_job_description(path_to_prp_script, test_dir):
+    return {
+        "LeaveJobInQueue":              "true",
+
+        "executable":                   path_to_prp_script,
+        "arguments":                    "$(ClusterID).$(ProcID)",
+        "transfer_executable":          "true",
+        "should_transfer_files":        "true",
+        "when_to_transfer_output":      "ON_EXIT",
+        "preserve_relative_paths":      "true",
+
+        "output":                       "prp.out",
+        "stream_output":                "true",
+        "error":                        "prp.err",
+        "stream_error":                 "true",
+        "log":                          test_dir / "prp.log",
+
+        "transfer_input_files":         "prp/data",
+        "transfer_checkpoint_files":    "prp/data",
+        "transfer_output_files":        "prp/data",
+        "checkpoint_exit_code":         "85",
+    }
+
+
+@action
+def prp_job_handle(default_condor, prp_job_description):
+    os.mkdir("prp")
+    os.mkdir("prp/data")
+
+    with open("prp/data/data_file", "w") as data_file:
+        data_file.write("original input file\n")
+
+    prp_job_handle = default_condor.submit(
+        description = prp_job_description,
+        count = 1,
+    )
+
+    yield prp_job_handle
+
+    prp_job_handle.remove()
+
+
+@action
+def prp_job_lines(prp_job_handle, default_condor, test_dir):
+    #
+    # With only a little work, this function and directory_job_lines()
+    # could share a common implementation.
+    #
+    prp_job_handle.wait(
+        timeout=60,
+        condition=ClusterState.all_running,
+        fail_condition=ClusterState.any_terminal,
+    )
+
+    prp_job_handle.wait(
+        timeout=60,
+        condition=ClusterState.all_idle,
+        fail_condition=ClusterState.any_terminal,
+    )
+
+    default_condor.run_command(['condor_reschedule'])
+
+    prp_job_handle.wait(
+        timeout=60,
+        condition=ClusterState.all_complete,
+        fail_condition=ClusterState.any_held,
+    )
+
+    with open(test_dir / "prp-test-file", "r") as output:
+        return output.readlines()
+
+    return None
+
+
+# -----------------------------------------------------------------------------
+
 class TestFileTransfer:
 
     def test_input_ordering(self, ordering_job_output):
@@ -224,5 +356,20 @@ class TestFileTransfer:
             assert line == "second job modification\n"
 
 
-    def test_preserve_spool_relative_paths(self):
-        pass
+    def test_preserve_spool_relative_paths(self, prp_job_lines):
+        assert prp_job_lines == [
+            "Starting up...\n",
+            "step one\n",
+            "Starting up...\n",
+            "step two\n",
+            "Starting up...\n",
+            "step three\n",
+        ]
+
+        with open("prp/data/data_file", "r") as final_output:
+            lines = final_output.readlines()
+            assert lines == [
+                "original input file\n",
+                "step one\n",
+                "step three\n",
+            ]
