529b00dd645955093d9c9f3491fbeebf21bbb806
Tim Theisen <tim@cs.wisc.edu>
2021-12-28 14:46:23 -0600

    HTCONDOR-94: Bootstrap bullseye and bookworm

--- nmi_tools/glue/build/build_uw_deb.sh
+++ nmi_tools/glue/build/build_uw_deb.sh
@@ -56,6 +56,12 @@ elif $(grep -qi bullseye /etc/os-release); then
     mv debian/htcondor.install.focal debian/htcondor.install
     mv debian/rules.focal debian/rules
     mv debian/patches/series.focal debian/patches/series
+elif $(grep -qi bookworm /etc/os-release); then
+    suffix='n3'
+    mv debian/control.focal debian/control
+    mv debian/htcondor.install.focal debian/htcondor.install
+    mv debian/rules.focal debian/rules
+    mv debian/patches/series.focal debian/patches/series
 elif $(grep -qi bionic /etc/os-release); then
     suffix=''
 elif $(grep -qi focal /etc/os-release); then
@@ -106,6 +112,12 @@ elif [ "$suffix" = 'n1' ]; then
     dpkg-buildpackage --build=$build -uc -us
 elif [ "$suffix" = 'n2' ]; then
     build='full'
+    dch --distribution $dist --nmu 'place holder entry'
+    dch --distribution $dist --nmu 'place holder entry'
+    dpkg-buildpackage --build=$build -uc -us
+elif [ "$suffix" = 'n3' ]; then
+    build='full'
+    dch --distribution $dist --nmu 'place holder entry'
     dch --distribution $dist --nmu 'place holder entry'
     dch --distribution $dist --nmu 'place holder entry'
     dpkg-buildpackage --build=$build -uc -us
