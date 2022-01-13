7a562dc407e1cd3a524936043fc656b043d6af8e
Jaime Frey <jfrey@cs.wisc.edu>
2022-01-13 12:28:19 -0600

    Set environment variables for ARC grid jobs. HTCONDOR-932
    Include environment variables set in the job ad in the ADL description
    we send to the ARC service.

--- src/condor_gridmanager/arcjob.cpp
+++ src/condor_gridmanager/arcjob.cpp
@@ -993,6 +993,18 @@ void ArcJob::SetRemoteJobId( const char *job_id )
 	BaseJob::SetRemoteJobId( full_job_id.c_str() );
 }
 
+static
+bool AppendEnvVar(void* pv, const std::string & var, const std::string & val)
+{
+	std::string &rsl = *(std::string*)pv;
+	rsl += "<Environment><Name>";
+	rsl += escapeXML(var);
+	rsl += "</Name><Value>";
+	rsl += escapeXML(val);
+	rsl += "</Value></Environment>";
+	return true;
+}
+
 bool ArcJob::buildJobADL()
 {
 	bool transfer_exec = true;
@@ -1091,7 +1103,17 @@ bool ArcJob::buildJobADL()
 		}
 	}
 
-	// TODO Add setting of environment variables
+	Env envobj;
+	std::string env_errors;
+	if (!envobj.MergeFrom(jobAd, env_errors)) {
+		dprintf(D_ALWAYS,"(%d.%d) Failed to read job environment: %s\n",
+		        procID.cluster, procID.proc, env_errors.c_str());
+		formatstr(errorString, "Failed to read job environment: %s\n",
+		          arg_errors.c_str());
+		RSL.clear();
+		return false;
+	}
+	envobj.Walk(&AppendEnvVar, &RSL);
 
 	RSL += "</Application>";
 
