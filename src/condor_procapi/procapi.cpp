522c781cfc0845737d25d618c3c0398c8d031de9
Todd L Miller <tlmiller@cs.wisc.edu>
2022-02-04 15:48:46 -0600

    (HTCONDOR-944)  Final (hopefully) formatting tweak.

--- src/condor_procapi/procapi.cpp
+++ src/condor_procapi/procapi.cpp
@@ -1983,8 +1983,9 @@ build_pid_list( std::vector<pid_t> & newPidList ) {
 						dprintf( D_ALWAYS, "/proc was mounted without hidepid, assuming default of 0.\n" );
 						hidepid = false;
 					}
-				// We found `proc`, we can quit reading now.
-				break;
+
+					// We found `proc`, we can quit reading now.
+					break;
 				}
 			}
 
