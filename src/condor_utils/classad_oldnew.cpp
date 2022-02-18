b6a9294b527fcb25b43e1a2a06e628853d650001
JaimeFrey <jfrey@cs.wisc.edu>
2022-02-18 15:53:57 -0600

    Update version check. HTCONDOR-638

--- src/condor_utils/classad_oldnew.cpp
+++ src/condor_utils/classad_oldnew.cpp
@@ -687,7 +687,7 @@ int _putClassAd( Stream *sock, const classad::ClassAd& ad, int options,
 	bool excludeTypes = (options & PUT_CLASSAD_NO_TYPES) == PUT_CLASSAD_NO_TYPES;
 	bool exclude_private = (options & PUT_CLASSAD_NO_PRIVATE) == PUT_CLASSAD_NO_PRIVATE;
 	auto *verinfo = sock->get_peer_version();
-	bool exclude_private_v2 = exclude_private || !verinfo || !verinfo->built_since_version(9, 1, 1);
+	bool exclude_private_v2 = exclude_private || !verinfo || !verinfo->built_since_version(9, 8, 0);
 
 	classad::ClassAdUnParser	unp;
 	std::string					buf;
