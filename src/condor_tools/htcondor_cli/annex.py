8ae3ee68f04fee66ae8ed431ec0702c1266be4a5
Todd L Miller <tlmiller@cs.wisc.edu>
2022-03-01 20:57:31 -0600

    (HTCONDOR-982)  The output format needs a lot of help, but this suffices for the necessary demo(s).

--- src/condor_tools/htcondor_cli/annex.py
+++ src/condor_tools/htcondor_cli/annex.py
@@ -6,6 +6,110 @@ from htcondor_cli.noun import Noun
 from htcondor_cli.verb import Verb
 
 
+def increment(hash, key, subkey, subsubkey, number):
+    value = hash[key][subkey].get(subsubkey, 0)
+    value += number
+    hash[key][subkey][subsubkey] = value
+
+
+class Status(Verb):
+    """
+    Summarize the usage of an annex.
+    """
+
+    options = {
+        "annex_name": {
+            "args": ("--annex-name",),
+            "help": "annex name",
+        },
+    }
+
+
+    # Unlike `shutdown`, status should probably not require a name.
+    def __init__(self, logger, **options):
+        the_annex_name = options["annex_name"];
+
+        schedd = htcondor.Schedd()
+        if the_annex_name is None:
+            annex_jobs = schedd.query(f'hpc_annex_name =!= undefined')
+        else:
+            annex_jobs = schedd.query(f'hpc_annex_name == "{the_annex_name}"')
+
+        # Each annex request is represented by its own job, but we want
+        # to present aggregate information for each annex name.
+
+        status = { job["hpc_annex_name"]: {} for job in annex_jobs }
+        for job in annex_jobs:
+            annex_name = job["hpc_annex_name"]
+            request_id = job["hpc_annex_request_id"]
+
+            status[annex_name][request_id] = "requested"
+            if job.get("hpc_annex_PID") is not None:
+                status[annex_name][request_id] = "submitting (1)"
+            if job.get("hpc_annex_PILOT_DIR") is not None:
+                status[annex_name][request_id] = "submitting (2)"
+            if job.get("hpc_annex_JOB_ID") is not None:
+                status[annex_name][request_id] = "submitted"
+            if job["JobStatus"] == htcondor.JobStatus.IDLE:
+                status[annex_name][request_id] = "granted"
+
+        annex_collector = htcondor.param.get("ANNEX_COLLECTOR", "htcondor-cm-hpcannex.osgdev.chtc.io")
+        collector = htcondor.Collector(annex_collector)
+        if the_annex_name is None:
+            annex_slots = collector.query(constraint=f'AnnexName =!= undefined', ad_type=htcondor.AdTypes.Startd)
+        else:
+            annex_slots = collector.query(constraint=f'AnnexName == "{the_annex_name}"', ad_type=htcondor.AdTypes.Startd)
+
+        for slot in annex_slots:
+            annex_name = slot["AnnexName"]
+            request_id = slot["hpc_annex_request_id"]
+            status[annex_name][request_id] = {}
+
+        for slot in annex_slots:
+            annex_name = slot["AnnexName"]
+            request_id = slot["hpc_annex_request_id"]
+
+            # Ignore dynamic slot, since we just care about aggregates.
+            # We could write a similar check for static slots, but we
+            # don't ever start any annexes with static slots.  (Bad
+            # admin, no cookie!)
+            if slot.get("PartitionableSlot", False):
+                increment(status, annex_name, request_id, "TotalCPUs", slot["TotalSlotCPUs"])
+                increment(status, annex_name, request_id, "BusyCPUs", len(slot["ChildCPUs"]))
+
+        #
+        # This is obviously the wrong format, but there's only so much
+        # work I'm willing to do before I get word on what it should be.
+        #
+        #   - This probably shouldn't be a table.
+        #   - It may make sense to report on the (non-active) requests
+        #     in their own section.
+        #   - It may be helpful to report how many jobs (owned by this
+        #     user) are targetting each annex, and of those, how many
+        #     are running.
+        #
+        print(f'          \t\t           REQUESTS\t        CPUS')
+        print(f'ANNEX NAME\t\tPENDING/ACTIVE/DONE\t  BUSY/TOTAL')
+        for annex_name, annex_status in status.items():
+            requests = len(annex_status)
+
+            busy_CPUs = 0
+            total_CPUs = 0
+            requested_and_left = 0
+            requested_but_not_joined = 0
+            for request_ID, values in annex_status.items():
+                if isinstance(values, str):
+                    if values == "granted":
+                        requested_and_left += 1
+                    else:
+                        requested_but_not_joined += 1
+                else:
+                    total_CPUs += values["TotalCPUs"]
+                    busy_CPUs += values["BusyCPUs"]
+
+            print(f'{annex_name}\t\t    {requested_but_not_joined}/{requests - requested_but_not_joined - requested_and_left}/{requested_and_left}\t\t    {busy_CPUs}/{total_CPUs}')
+
+
 class Shutdown(Verb):
     """
     Shut an HPC annex down.
@@ -75,7 +179,11 @@ class Annex(Noun):
         pass
 
 
+    class status(Status):
+        pass
+
+
     @classmethod
     def verbs(cls):
-        return [cls.shutdown,]
+        return [cls.shutdown, cls.status]
 
