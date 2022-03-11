f12ec4967b4f652ff258cb1a433d2f9d5a76c9c7
Tim Theisen <tim@cs.wisc.edu>
2022-03-11 09:43:46 -0600

    Set version number to 8.8.17 in V8_8-branch

--- docs/conf.py
+++ docs/conf.py
@@ -68,7 +68,7 @@ Licensed under the Apache License, Version 2.0.'
 # The short X.Y version.
 version = '8.8'
 # The full version, including alpha/beta/rc tags.
-release = '8.8.16'
+release = '8.8.17'
 
 rst_epilog = """
 .. |release_date| replace:: Month Day, 2021
