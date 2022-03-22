d177c92d5eff289530214be50ce7285bf8462d01
Jaime Frey <jfrey@cs.wisc.edu>
2022-03-22 12:58:28 -0500

    Have "the tool" issue a locate query. HTCONDOR-638
    Only locate queries get the remote admin capability now.
    The tool doesn't need the full ads, either.

--- src/condor_tools/tool.cpp
+++ src/condor_tools/tool.cpp
@@ -1276,7 +1276,7 @@ resolveNames( DaemonList* daemon_list, StringList* name_list, StringList* unreso
 	if (constraint!=NULL) {
 	  query.addANDConstraint(constraint);
 	}
-
+	query.setLocationLookup("tool", false);
 
 	if (pool_addr) {
 		q_result = query.fetchAds(ads, pool_addr, &errstack);
