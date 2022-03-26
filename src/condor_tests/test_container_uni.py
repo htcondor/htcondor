d20e1cd89db0786e91af3422763c337dee3b9ee8
Greg Thain <gthain@cs.wisc.edu>
2022-03-26 10:19:17 -0500

    HTCONDOR-884 have test create the mock singularity on the fly

--- src/condor_tests/test_container_uni.py
+++ src/condor_tests/test_container_uni.py
@@ -8,24 +8,108 @@
 
 import logging
 import htcondor
+import os,sys,stat
 
 from ornithology import *
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+# Write out a file that mocks the singularity cli
+@standup
+def mock_singularity_file(test_dir):
+    mock_singularity = """#!/bin/bash
+
+# ignore leading - options, except for --version
+while [[ "$1" =~ "-".* ]]
+do
+	if [ "$1" == "--version" ]
+	then
+		echo "3.0.0"
+		exit 0
+	fi
+
+	shift
+done
+
+#  Have test command always return true
+if [ "$1" = "test" ]
+then
+	echo 0
+	exit 0
+fi
+
+# Just run the command without a container
+if [ "$1" = "exec" ]
+then
+	# Shift off the exec
+	shift
+
+	while [[ "$1" =~ "-".* ]]
+	do
+		case $1 in
+		 -S)
+			 # scratch dir has an argument
+			 shift
+			 shift
+			 ;;
+		 -B)
+			 # scratch dir has an argument
+			 shift
+			 shift
+			 ;;
+		 --pwd)
+			 shift
+			 shift
+			 ;;
+		 --no-home)
+			 shift
+			 ;;
+		 -C)
+			 shift
+			 ;;
+		 -*)
+			 echo "Unknown argument at $@"
+			 exit 1
+			 break
+			 ;;
+		esac
+	done
+
+	# Shift off the container name
+	shift
+
+	# special case for test job
+	if [ "$1" = "/exit_37" ]
+	then
+		exit 37
+	fi
+
+	# And run the command
+	eval "$@"
+	exit $?
+fi
+
+echo "Unknown/unsupported singularity command: $@"
+exit 0
+    """
+    filename = test_dir / 'singularity_tester.sh'
+    print(filename)
+    write_file(filename, mock_singularity)
+    os.chmod(filename, stat.S_IRWXU)
+    return filename
+
 # Setup a personal condor with SINGULARITY pointing at the mocking wrapper
 @standup
-def condor(test_dir):
+def condor(test_dir, mock_singularity_file):
     # Singularity wrapper is in the root of the per-test directory, two
     # levels above the condor config
-    path_to_singularity = test_dir / "../../singularity_tester.sh"
-    with Condor(test_dir / "condor", config={"SINGULARITY": path_to_singularity}) as condor:
+    with Condor(test_dir / "condor", config={"SINGULARITY": mock_singularity_file}) as condor:
         yield condor
 
 # The actual job will cat a file
 @action
-def test_job_hash(test_dir):
+def test_job_hash():
     return {
             "universe": "container",
             "container_image": "ignored.sif",
@@ -38,6 +122,9 @@ def test_job_hash(test_dir):
 
 @action
 def completed_test_job(condor, test_job_hash):
+    os.system("/bin/pwd")
+    os.system("ls -CFR ../..")
+
     ctj = condor.submit(
         {**test_job_hash}, count=1
     )
