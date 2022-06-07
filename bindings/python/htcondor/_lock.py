d0ffdb8345461f999d11be998069ab3467c8f9b6
Jason Patton <jpatton@cs.wisc.edu>
2022-06-07 11:43:10 -0500

    HTCONDOR-924 Only exempt SecMan ctxmgr from lock

--- bindings/python/htcondor/_lock.py
+++ bindings/python/htcondor/_lock.py
@@ -38,7 +38,7 @@ DO_NOT_LOCK = {
     htcondor.JobEvent,
     htcondor.SubmitResult,
     htcondor.Transaction.__enter__,  # avoids recursion (this method returns self!)
-    htcondor.SecMan,
+    htcondor.SecMan.__enter__,
 }
 
 
