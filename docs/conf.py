ea2645f62b17cb1ffe9f0a06081f350ca70abe8e
Tim Theisen <tim@cs.wisc.edu>
2022-11-07 14:37:11 -0600

    HTCONDOR-1415: Set version number for 10.1.1 branch

--- docs/conf.py
+++ docs/conf.py
@@ -65,7 +65,7 @@ Licensed under the Apache License, Version 2.0.'
 # The short X.Y version.
 version = '10.0'
 # The full version, including alpha/beta/rc tags.
-release = '10.1.0'
+release = '10.1.1'
 
 rst_epilog = """
 .. |release_date| replace:: Month Day, 2022
