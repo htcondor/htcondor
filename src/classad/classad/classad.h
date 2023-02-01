0222a650aebcee5d623a5d399059fb80ee2756e7
Greg Thain <gthain@cs.wisc.edu>
2023-02-01 14:35:11 -0600

    HTCONODR-1571 Add classad transparent comparators

--- src/classad/classad/classad.h
+++ src/classad/classad/classad.h
@@ -286,7 +286,37 @@ class ClassAd : public ExprTree
 			@return The expression bound to the name in the ClassAd, or NULL
 				otherwise.
 		*/
-		ExprTree *Lookup( const std::string &attrName ) const;
+		ExprTree * Lookup( const std::string &name ) const {
+			ExprTree *tree;
+			AttrList::const_iterator itr;
+
+			itr = attrList.find( name );
+			if (itr != attrList.end()) {
+				tree = itr->second;
+			} else if (chained_parent_ad != NULL) {
+				tree = chained_parent_ad->Lookup(name);
+			} else {
+				tree = NULL;
+			}
+			return tree;
+		}
+
+		// The char * overload of the above
+		ExprTree * Lookup( const char *name ) const {
+			ExprTree *tree;
+			AttrList::const_iterator itr;
+
+			itr = attrList.find( name );
+			if (itr != attrList.end()) {
+				tree = itr->second;
+			} else if (chained_parent_ad != NULL) {
+				tree = chained_parent_ad->Lookup(name);
+			} else {
+				tree = NULL;
+			}
+			return tree;
+		}
+
 		ExprTree* LookupExpr(const std::string &name) const
 		{ return Lookup( name ); }
 
