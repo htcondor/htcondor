8dc34ae76349a1c0263f4cd033553e78b2837401
Tim Theisen <tim@cs.wisc.edu>
2023-02-01 17:50:12 -0600

    HTCONDOR-1576: Set up 10.2.2 branch

--- docs/conf.py
+++ docs/conf.py
@@ -65,7 +65,7 @@ Licensed under the Apache License, Version 2.0.'
 # The short X.Y version.
 version = '10.2'
 # The full version, including alpha/beta/rc tags.
-release = '10.2.1'
+release = '10.2.2'
 
 rst_epilog = """
 .. |release_date| replace:: January 24, 2023
