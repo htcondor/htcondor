3aec2c4255208c4f616be0aef1a43f29380bee19
Greg Thain <gthain@cs.wisc.edu>
2023-02-22 19:06:01 -0600

    HTCONDOR-1601 more const correctness

--- src/condor_daemon_core.V6/condor_softkill.WINDOWS.cpp
+++ src/condor_daemon_core.V6/condor_softkill.WINDOWS.cpp
@@ -214,7 +214,7 @@ wWinMain(__in HINSTANCE, __in_opt HINSTANCE, __in wchar_t*, __in int)
 	// see if a debug output file was given
 	//
 	if (__argc > 2) {
-		wchar_t * opt = L"a";
+		const wchar_t * opt = L"a";
 		wchar_t * pszFile = __wargv[2];
 		debug_fp = _wfopen(pszFile, opt);
 		if (debug_fp == NULL) {
