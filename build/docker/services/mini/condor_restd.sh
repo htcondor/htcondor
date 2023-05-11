d8e9c602b86d56deaab7a8e70f429e227cac9ea8
Matyas Selmeci <matyas@cs.wisc.edu>
2023-05-11 16:31:01 -0500

    HTCONDOR-1785: Use a locale that is actually available on the system for the condor_restd in the minicondor container
    Flask needs a UTF locale so get a list of locales with the "locale -a" command and pick one.

--- build/docker/services/mini/condor_restd.sh
+++ build/docker/services/mini/condor_restd.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
+# Flask needs a UTF locale; if one is not set, find one.
+# Prefer C, then en_US, then just pick one.
+# Depending on distro, a locale may have a ".utf8" or a ".UTF-8" suffix.
+if [[ $LANG != *[Uu][Tt][Ff]* ]]; then
+    utf_locales=$(locale -a | grep -i utf)
+    c_utf_locale=$(printf "%s\n" "$utf_locales" | grep '^C\.' | head -n 1)
+    en_us_utf_locale=$(printf "%s\n" "$utf_locales" | grep '^en_US\.' | head -n 1)
+    last_resort_utf_locale=$(printf "%s\n" "$utf_locales" | head -n 1)
 
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
+    if [[ $c_utf_locale ]]; then
+        LANG=$c_utf_locale
+    elif [[ $en_us_utf_locale ]]; then
+        LANG=$en_us_utf_locale
+    elif [[ $last_resort_utf_locale ]]; then
+        LANG=$last_resort_utf_locale
+    else
+        echo 1>&2 "No UTF locale found - bailing."
+        exit 1
+    fi
+fi
+LC_ALL=$LANG
+
+export LC_ALL LANG
+
 export FLASK_APP=condor_restd
 export FLASK_ENV=dev
 
