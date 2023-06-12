ea76522e75ed5ff26b14b020be09fb79e69815de
Brian Bockelman <bbockelman@morgridge.org>
2023-06-11 21:39:25 -0500

    Pass config-specified arguments to hooks.  HTCONDOR-1851
    Allows both the shadow and starter prep hooks to have custom arguments.

--- src/condor_daemon_core.V6/HookClientMgr.h
+++ src/condor_daemon_core.V6/HookClientMgr.h
@@ -86,6 +86,7 @@ protected:
 	virtual bool reconfig() = 0;
 	virtual const std::string paramPrefix() const = 0;
 	bool getHookPath(HookType hook_type, std::string &path);
+	bool getHookArgs(HookType hook_type, ArgList &args, CondorError &err);
 
 	std::string m_hook_keyword;
 };
