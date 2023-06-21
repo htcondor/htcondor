98e859be7c13d027ffbc9ec0906f0320dd367ae1
Todd L Miller <tlmiller@cs.wisc.edu>
2023-06-21 18:01:56 -0500

    (HTCONDOR-1887)  Don't use deprecated methods in htcondor CLI.  Revert test that accepted doing so.

--- src/condor_tests/test_htcondor_cli.py
+++ src/condor_tests/test_htcondor_cli.py
@@ -56,13 +56,7 @@ class TestHtcondorCli:
         assert submit_failure.stderr == "Error while trying to run job submit:\nCould not find file: does_not_exist.sub"
 
     def test_submit_success(self, test_dir, submit_success):
-        lines = submit_success.stderr.splitlines()
-        assert lines[0].endswith("FutureWarning: Schedd.transaction() was deprecated in version 10.7.0 and will be removed in a future release.  Use Schedd.submit() instead.")
-        assert lines[1].endswith("warnings.warn(message, FutureWarning)")
-        assert lines[2].endswith("FutureWarning: Submit.queue() was deprecated in version 10.7.0 and will be removed in a future release.  Use Schedd.submit() instead.")
-        assert lines[3].endswith("warnings.warn(message, FutureWarning)")
-        assert lines[4] == "Job 1 was submitted."
-
+        assert submit_success.stderr == "Job 1 was submitted."
         jel = htcondor.JobEventLog((test_dir / "helloworld.log").as_posix())
         # Wait for the job to finish by watching its event log
         for event in jel.events(stop_after=None):
