5f8eaa6f8f9869d2e050d1ca4f4cfc4747b2056d
Todd L Miller <tlmiller@cs.wisc.edu>
2023-08-09 17:03:32 -0500

    (HTCONDOR-1942)  Either sometimes we round up, or something else that the test is doing takes a little more time on the emulated problems.

--- src/condor_tests/test_checkpoint_ct.py
+++ src/condor_tests/test_checkpoint_ct.py
@@ -335,9 +335,9 @@ class TestCheckpointCommittedTime:
         committed_time = int(rv.stdout.strip())
 
         # There are three intervals of nominally 5 seconds and one interval
-        # of nominally 4 seconds.  Because of truncation (!), each interval
-        # can be recorded as a full second shorter.
-        assert(15 <= committed_time and committed_time <= 19)
+        # of nominally 4 seconds.  Because of rounding, each interval
+        # can be recorded as a full second shorter or longer.
+        assert(15 <= committed_time and committed_time <= 23)
 
         # We also look at the job queue log, which will not have been rotated
         # because with the default_condor as a parameter, it can't be garbage-
