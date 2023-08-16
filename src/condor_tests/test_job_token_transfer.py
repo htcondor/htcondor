abf7a9f7f0eaab6f42fb13ac4221686c42174f54
Jason Patton <jpatton@cs.wisc.edu>
2023-08-16 16:04:06 -0500

    HTCONDOR-1972 Expand test to other universes

--- src/condor_tests/test_job_token_transfer.py
+++ src/condor_tests/test_job_token_transfer.py
@@ -19,21 +19,45 @@ logger.setLevel(logging.DEBUG)
 USER = getuser()
 
 
+# This test scripts prints out the cat'd contents
+# of $_CONDOR_CREDS/{$1,$2,...,$n}
+TEST_SCRIPT = """#!/bin/bash
+args=($@)
+cat ${args[@]/#/$_CONDOR_CREDS/}
+"""
+
+
 @standup
 def condor(pytestconfig, test_dir):
+
+    # Prepare credmon
     credmon = pytestconfig.invocation_dir / "condor_credmon_oauth_dummy"
     credmon.chmod(0o755)  # make sure the credmon is executable
+
+    # Prepare top level credential directory
     cred_dir = test_dir / "oauth_credentials"
     cred_dir.mkdir(parents=True, exist_ok=True)
     cred_dir.chmod(0o2770)
+
+    # Prepare test user credential directory
     user_cred_dir = cred_dir / USER
     user_cred_dir.mkdir(parents=True, exist_ok=True)
-    secret_file = test_dir / "dummy.secret"
-    secret_file.open("w").write("dummy_secret")
-    secret_file.chmod(0o600)
+
+    # Create fake tokens in user credential directory
     for token_name in ["dummy", "dummy_A", "dummy_B"]:
         (user_cred_dir / token_name).with_suffix(".top").open("w").write(f"refresh_{token_name}")
         (user_cred_dir / token_name).with_suffix(".use").open("w").write(f"access_{token_name}")
+
+    # Prepare fake OAuth secret file
+    secret_file = test_dir / "dummy.secret"
+    secret_file.open("w").write("dummy_secret")
+    secret_file.chmod(0o600)
+
+    # Prepare test executable
+    test_executable = test_dir / "test_wrapper.sh"
+    test_executable.open("w").write(TEST_SCRIPT)
+    test_executable.chmod(0o755)
+
     with Condor(
         local_dir=test_dir / "condor",
         config={
@@ -55,13 +79,14 @@ def condor(pytestconfig, test_dir):
 
 
 @action
-def one_service_no_handles(condor, test_dir):
-    outfile = test_dir / "one_service_no_handles.out"
+def vanilla_one_service_no_handles(condor, test_dir):
+    test_executable = test_dir / "test_wrapper.sh"
+    outfile = test_dir / "vanilla_one_service_no_handles.out"
     job = condor.submit(
         {
-            "executable": "/bin/cat",
-            "arguments": ".condor_creds/dummy.use",
-            "should_transfer_files": "yes",
+            "universe": "vanilla",
+            "executable": str(test_executable),
+            "arguments": "dummy.use",
             "use_oauth_services": "dummy",
             "output": str(outfile),
             "error": str(outfile.with_suffix(".err")),
@@ -73,13 +98,94 @@ def one_service_no_handles(condor, test_dir):
 
 
 @action
-def one_service_two_handles(condor, test_dir):
-    outfile = test_dir / "one_service_two_handles.out"
+def vanilla_one_service_two_handles(condor, test_dir):
+    test_executable = test_dir / "test_wrapper.sh"
+    outfile = test_dir / "vanilla_one_service_two_handles.out"
     job = condor.submit(
         {
-            "executable": "/bin/cat",
-            "arguments": ".condor_creds/dummy_A.use .condor_creds/dummy_B.use",
-            "should_transfer_files": "yes",
+            "universe": "vanilla",
+            "executable": str(test_executable),
+            "arguments": "dummy_A.use dummy_B.use",
+            "use_oauth_services": "dummy",
+            "dummy_oauth_permissions_A": "",
+            "dummy_oauth_permissions_B": "",
+            "output": str(outfile),
+            "error": str(outfile.with_suffix(".err")),
+            "log": str(outfile.with_suffix(".log")),
+        }
+    )
+    assert job.wait(condition=ClusterState.all_terminal)
+    return outfile.open("r").read().strip()
+
+
+@action
+def local_one_service_no_handles(condor, test_dir):
+    test_executable = test_dir / "test_wrapper.sh"
+    outfile = test_dir / "local_one_service_no_handles.out"
+    job = condor.submit(
+        {
+            "universe": "local",
+            "executable": str(test_executable),
+            "arguments": "dummy.use",
+            "use_oauth_services": "dummy",
+            "output": str(outfile),
+            "error": str(outfile.with_suffix(".err")),
+            "log": str(outfile.with_suffix(".log")),
+        }
+    )
+    assert job.wait(condition=ClusterState.all_terminal)
+    return outfile.open("r").read().strip()
+
+
+@action
+def local_one_service_two_handles(condor, test_dir):
+    test_executable = test_dir / "test_wrapper.sh"
+    outfile = test_dir / "local_one_service_two_handles.out"
+    job = condor.submit(
+        {
+            "universe": "local",
+            "executable": str(test_executable),
+            "arguments": "dummy_A.use dummy_B.use",
+            "use_oauth_services": "dummy",
+            "dummy_oauth_permissions_A": "",
+            "dummy_oauth_permissions_B": "",
+            "output": str(outfile),
+            "error": str(outfile.with_suffix(".err")),
+            "log": str(outfile.with_suffix(".log")),
+        }
+    )
+    assert job.wait(condition=ClusterState.all_terminal)
+    return outfile.open("r").read().strip()
+
+
+@action
+def scheduler_one_service_no_handles(condor, test_dir):
+    test_executable = test_dir / "test_wrapper.sh"
+    outfile = test_dir / "scheduler_one_service_no_handles.out"
+    job = condor.submit(
+        {
+            "universe": "scheduler",
+            "executable": str(test_executable),
+            "arguments": "dummy.use",
+            "use_oauth_services": "dummy",
+            "output": str(outfile),
+            "error": str(outfile.with_suffix(".err")),
+            "log": str(outfile.with_suffix(".log")),
+        }
+    )
+    assert job.wait(condition=ClusterState.all_terminal)
+    return outfile.open("r").read().strip()
+
+
+@action
+def scheduler_one_service_two_handles(condor, test_dir):
+    test_executable = test_dir / "test_wrapper.sh"
+    outfile = test_dir / "scheduler_one_service_two_handles.out"
+    job = condor.submit(
+        {
+            "universe": "scheduler",
+            "executable": str(test_executable),
+            "arguments": "dummy_A.use dummy_B.use",
             "use_oauth_services": "dummy",
             "dummy_oauth_permissions_A": "",
             "dummy_oauth_permissions_B": "",
@@ -94,8 +200,20 @@ def one_service_two_handles(condor, test_dir):
 
 class TestJobTokenTransfer:
 
-    def test_one_service_no_handles(self, one_service_no_handles):
-        assert one_service_no_handles == "access_dummy"
+    def vanilla_test_one_service_no_handles(self, vanilla_one_service_no_handles):
+        assert vanilla_one_service_no_handles == "access_dummy"
 
-    def test_one_service_two_handles(self, one_service_two_handles):
-        assert one_service_two_handles == "access_dummy_Aaccess_dummy_B"
+    def vanilla_test_one_service_two_handles(self, vanilla_one_service_two_handles):
+        assert vanilla_one_service_two_handles == "access_dummy_Aaccess_dummy_B"
+
+    def local_test_one_service_no_handles(self, local_one_service_no_handles):
+        assert local_one_service_no_handles == "access_dummy"
+
+    def local_test_one_service_two_handles(self, local_one_service_two_handles):
+        assert local_one_service_two_handles == "access_dummy_Aaccess_dummy_B"
+
+    def scheduler_test_one_service_no_handles(self, scheduler_one_service_no_handles):
+        assert scheduler_one_service_no_handles == "access_dummy"
+
+    def scheduler_test_one_service_two_handles(self, scheduler_one_service_two_handles):
+        assert scheduler_one_service_two_handles == "access_dummy_Aaccess_dummy_B"
