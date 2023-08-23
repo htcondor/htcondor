936623414e9fb1cbc4cda5fc6aae7679b62b88e7
Greg Thain <gthain@cs.wisc.edu>
2023-08-23 14:10:23 -0500

    HTCONDOR-1992 remove ancient unused code that causes spurious memory leak warnings in valgrind

--- src/condor_schedd.V6/schedd.cpp
+++ src/condor_schedd.V6/schedd.cpp
@@ -13222,25 +13222,6 @@ Scheduler::Init()
 
 	dprintf( D_FULLDEBUG, "Using name: %s\n", Name );
 
-		// Put SCHEDD_NAME in the environment, so the shadow can use
-		// it.  (Since the schedd's name may have been set on the
-		// command line, the shadow can't compute the schedd's name on
-		// its own.)  
-		// Only put in in the env if it is not already there, so 
-		// we don't leak memory without reason.		
-
-#define SCHEDD_NAME_LHS "SCHEDD_NAME"
-	
-	if ( NameInEnv == NULL || strcmp(NameInEnv,Name) ) {
-		free( NameInEnv );
-		NameInEnv = strdup( Name );
-		if ( SetEnv( SCHEDD_NAME_LHS, NameInEnv ) == FALSE ) {
-			dprintf(D_ALWAYS, "SetEnv(%s=%s) failed!\n", SCHEDD_NAME_LHS,
-					NameInEnv);
-		}
-	}
-
-
 	if( AccountantName ) free( AccountantName );
 	if( ! (AccountantName = param("ACCOUNTANT_HOST")) ) {
 		dprintf( D_FULLDEBUG, "No Accountant host specified in config file\n" );
