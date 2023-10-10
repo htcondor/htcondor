f9441f8be9525e86de6fe85eaf5c66664168953a
John (TJ) Knoeller <johnkn@cs.wisc.edu>
2023-10-10 16:04:24 -0500

    HTCONDOR-2079 Add collector support for Slot ads and StartDaemon ads including synthesizing StartDaemon ads from Machine ads. Also add STARTD support for sending Slot and StartDaemon ads if ENABLE_STARTD_DAEMON_AD is set to True. currently it is set to false.

--- src/condor_collector.V6/collector.cpp
+++ src/condor_collector.V6/collector.cpp
@@ -22,11 +22,7 @@
 #include "condor_config.h"
 #include "condor_attributes.h"
 #include "condor_email.h"
-
 #include "condor_daemon_core.h"
-#include "status_types.h"
-#include "totals.h"
-
 #include "collector_engine.h"
 #include "hashkey.h"
 
@@ -491,6 +487,8 @@ int CollectorDaemon::receive_query_cedar(int command,
 	pending_query_entry_t *query_entry = NULL;
 	bool handle_in_proc;
 	bool is_locate;
+	const unsigned int BIG_TABLE_MASK = (1<<GENERIC_AD) | (1<<ANY_AD) | (1<<MASTER_AD) | (1<<STARTDAEMON_AD)
+	                                  | (1<<STARTD_AD) | (1<<STARTD_PVT_AD) | (1<<SLOT_AD);
 	KnownSubsystemId clientSubsys = SUBSYSTEM_ID_UNKNOWN;
 	AdTypes whichAds;
 	ClassAd *cad = new ClassAd();
@@ -518,9 +516,19 @@ int CollectorDaemon::receive_query_cedar(int command,
 
 	// Initial query handler
 	whichAds = receive_query_public( command );
-
+	if (whichAds == STARTD_AD) {
+		// for QUERY_STARTD_ADS, we look at the target type to figure which startd ad table to use
+		std::string target;
+		if (cad->LookupString(ATTR_TARGET_TYPE, target)) {
+			whichAds = get_real_startd_ad_type(target.c_str());
+		}
+	}
 	is_locate = cad->Lookup(ATTR_LOCATION_QUERY) != NULL;
-	if (is_locate) { rt.runtime = &HandleLocate_runtime; }
+	if (is_locate) {
+		rt.runtime = &HandleLocate_runtime;
+		// do startd ad locates in the daemon ad table
+		if (whichAds == STARTD_AD) { whichAds = STARTDAEMON_AD; }
+	}
 
 	// Figure out whether to handle the query inline or to fork.
 	handle_in_proc = false;
@@ -529,7 +537,7 @@ int CollectorDaemon::receive_query_cedar(int command,
 	} else if (HandleQueryInProcPolicy == HandleQueryInProcNever) {
 		handle_in_proc = false;
 	} else {
-		bool is_bigtable = ((whichAds == GENERIC_AD) || (whichAds == ANY_AD) || (whichAds == STARTD_PVT_AD) || (whichAds == STARTD_AD) || (whichAds == MASTER_AD));
+		bool is_bigtable = ((1<<whichAds) & BIG_TABLE_MASK) != 0;
 		if (HandleQueryInProcPolicy == HandleQueryInProcSmallTable) {
 			handle_in_proc = !is_bigtable;
 		} else {
