49c3bbd84e42850ec85c681986f4dd2717bc1c2f
Todd L Miller <tlmiller@cs.wisc.edu>
2023-10-10 15:28:29 -0500

    (HTCONDOR-1598)  Finish up Schedd.submit() testing.

--- src/condor_tests/test_schedd_submit.py
+++ src/condor_tests/test_schedd_submit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env pytest
 
+import os
 import logging
 import htcondor2
 
@@ -62,6 +63,55 @@ step_item_extra_expected = """{ClusterID} {ProcID} 0 10
 {ClusterID} {ProcID} 1 40
 {ClusterID} {ProcID} 0 50
 {ClusterID} {ProcID} 1 50
+"""
+
+
+test26_subtext = """
+    executable              = /path/to/sleep
+    arguments               = $(ClusterID) $(ProcID) $(step) $(y) $(x)
+    transfer_executable     = false
+    should_transfer_files   = true
+    universe                = vanilla
+    hold                    = true
+"""
+
+test26_qtext = """2 x y FROM [1:4] (
+    10 foo
+    20 bar
+    30 goto 10
+    40 rem unreachable
+    50 baz
+    )
+"""
+
+test26_expected = """{ClusterID} {ProcID} 0 bar 20
+{ClusterID} {ProcID} 1 bar 20
+{ClusterID} {ProcID} 0 goto 10 30
+{ClusterID} {ProcID} 1 goto 10 30
+{ClusterID} {ProcID} 0 rem unreachable 40
+{ClusterID} {ProcID} 1 rem unreachable 40
+"""
+
+
+test27_qtext = """2 x y FROM (
+    10 foo
+    20 bar
+    30 goto 10
+    40 rem unreachable
+    50 baz
+    )
+"""
+
+test27_expected = """{ClusterID} {ProcID} 0 foo 10
+{ClusterID} {ProcID} 1 foo 10
+{ClusterID} {ProcID} 0 bar 20
+{ClusterID} {ProcID} 1 bar 20
+{ClusterID} {ProcID} 0 goto 10 30
+{ClusterID} {ProcID} 1 goto 10 30
+{ClusterID} {ProcID} 0 rem unreachable 40
+{ClusterID} {ProcID} 1 rem unreachable 40
+{ClusterID} {ProcID} 0 baz 50
+{ClusterID} {ProcID} 1 baz 50
 """
 
 
@@ -131,6 +181,15 @@ test_cases = {
     },
 
     # Multi-variate FROM (table) with slice.
+    "test26_a": {
+        "subtext":  test26_subtext + "\n queue " + test26_qtext,
+        "expected": test26_expected,
+    },
+    "test26_b": {
+        "subtext":  test26_subtext,
+        "queue":    test26_qtext,
+        "expected": test26_expected,
+    },
 
     ## We test this variant specifically because it's the one we use internally,
     ## but otherwise, from here on, the tests will be with-slice and assume
@@ -140,19 +199,103 @@ test_cases = {
     ## in all the random combinations that are allowed.  If we miss a problem
     ## as a result, it will only be tedious to make the tests exhaustive.
     # Multi-variate FROM (table) without slice.
+    "test27_a": {
+        "subtext":  test26_subtext + "\n queue " + test27_qtext,
+        "expected": test27_expected,
+    },
+    "test27_b": {
+        "subtext":  test26_subtext,
+        "queue":    test27_qtext,
+        "expected": test27_expected,
+    },
+
 
     # IN (list) with slice.
-
-    # MATCHING dirs with slice.
+    "in_list": {
+        "subtext":  test26_subtext,
+        "queue":    "2 x IN [1:4] (a b c d e)",
+        "expected": "{ClusterID} {ProcID} 0 b\n"
+                    "{ClusterID} {ProcID} 1 b\n"
+                    "{ClusterID} {ProcID} 0 c\n"
+                    "{ClusterID} {ProcID} 1 c\n"
+                    "{ClusterID} {ProcID} 0 d\n"
+                    "{ClusterID} {ProcID} 1 d\n"
+    },
 
     # MATCHING files with slice.
+    "matching_files": {
+        "subtext":  test26_subtext,
+        "queue":    "2 x MATCHING FILES [1:4] *",
+        "expected": "{ClusterID} {ProcID} 0 b\n"
+                    "{ClusterID} {ProcID} 1 b\n"
+                    "{ClusterID} {ProcID} 0 c\n"
+                    "{ClusterID} {ProcID} 1 c\n"
+                    "{ClusterID} {ProcID} 0 d\n"
+                    "{ClusterID} {ProcID} 1 d\n"
+    },
+
+    # MATCHING dirs with slice.
+    "matching_dirs": {
+        "subtext":  test26_subtext,
+        "queue":    "2 x MATCHING DIRS [1:4] *",
+        "expected": "{ClusterID} {ProcID} 0 bb/\n"
+                    "{ClusterID} {ProcID} 1 bb/\n"
+                    "{ClusterID} {ProcID} 0 cc/\n"
+                    "{ClusterID} {ProcID} 1 cc/\n"
+                    "{ClusterID} {ProcID} 0 dd/\n"
+                    "{ClusterID} {ProcID} 1 dd/\n"
+    },
 
     # MATCHING external-file with slice.
+    "external_file": {
+        "subtext":  test26_subtext,
+        "queue":    "2 y x z FROM [1:4] external-file",
+        "expected": "{ClusterID} {ProcID} 0 20 b\n"
+                    "{ClusterID} {ProcID} 1 20 b\n"
+                    "{ClusterID} {ProcID} 0 30 c\n"
+                    "{ClusterID} {ProcID} 1 30 c\n"
+                    "{ClusterID} {ProcID} 0 40 d\n"
+                    "{ClusterID} {ProcID} 1 40 d\n"
+    },
 
-    # Tests for `itemdata` verify (a) that it generates the expected results
-    # and (b) that it wins out over both the original submit text and
-    # setQArgs().  We'll test both string- and dictionary-based itemdata.
-
+    "itemdata_list": {
+        "subtext":  step_item_extra_subtext,
+        "expected": "{ClusterID} {ProcID} 0 b\n"
+                    "{ClusterID} {ProcID} 1 b\n"
+                    "{ClusterID} {ProcID} 0 c\n"
+                    "{ClusterID} {ProcID} 1 c\n"
+                    "{ClusterID} {ProcID} 0 d\n"
+                    "{ClusterID} {ProcID} 1 d\n",
+        "itemdata": iter([ 'b', 'c', 'd' ]),
+        "count":    2,
+    },
+    "itemdata_dict": {
+        "subtext":  test26_subtext,
+        "expected": "{ClusterID} {ProcID} 0 20 b\n"
+                    "{ClusterID} {ProcID} 1 20 b\n"
+                    "{ClusterID} {ProcID} 0 30 c\n"
+                    "{ClusterID} {ProcID} 1 30 c\n"
+                    "{ClusterID} {ProcID} 0 40 d\n"
+                    "{ClusterID} {ProcID} 1 40 d\n",
+        "count":    2,
+        "itemdata": iter([
+            {"y": "20", "x": "b"},
+            {"y": "30", "x": "c"},
+            {"y": "40", "x": "d"},
+        ]),
+    },
+    "itemdata_priority": {
+        "subtext":  step_item_extra_subtext + "\n queue 3 IN (x y z)\n",
+        "queue":    "2 IN (b c d)",
+        "expected": "{ClusterID} {ProcID} 0 b\n"
+                    "{ClusterID} {ProcID} 1 b\n"
+                    "{ClusterID} {ProcID} 0 c\n"
+                    "{ClusterID} {ProcID} 1 c\n"
+                    "{ClusterID} {ProcID} 0 d\n"
+                    "{ClusterID} {ProcID} 1 d\n",
+        "itemdata": iter([ 'b', 'c', 'd' ]),
+        "count":    2,
+    },
 }
 
 
@@ -174,9 +317,38 @@ def the_condor(test_dir):
         yield the_condor
 
 
+@standup
+def the_test_dir(test_dir):
+    (test_dir / "test").mkdir()
+
+    (test_dir / "test" / "aa").mkdir()
+    (test_dir / "test" / "bb").mkdir()
+    (test_dir / "test" / "cc").mkdir()
+    (test_dir / "test" / "dd").mkdir()
+    (test_dir / "test" / "ee").mkdir()
+
+    (test_dir / "test" / "a").write_text("a")
+    (test_dir / "test" / "b").write_text("b")
+    (test_dir / "test" / "c").write_text("c")
+    (test_dir / "test" / "d").write_text("d")
+    (test_dir / "test" / "e").write_text("e")
+
+    (test_dir / "test" / "external-file").write_text("""10 a
+        20 b
+        30 c
+        40 d
+        50 e
+    """)
+
+    return (test_dir / "test").as_posix()
+
+
 @action
-def results(test_case, the_condor):
+def results(test_case, the_condor, test_dir, the_test_dir):
+    os.chdir(the_test_dir)
+
     submit = htcondor2.Submit(test_case["subtext"])
+    print(test_case["subtext"])
 
     queue = test_case.get("queue")
     count = test_case.get("count")
