5047e2960105afcd9b0e777c2dc622008b67c541
Greg Thain <gthain@cs.wisc.edu>
2023-11-06 09:36:19 -0600

    HTCONDOR-2129 Remove debugging output

--- build/cmake/macros/CloneInstall.cmake
+++ build/cmake/macros/CloneInstall.cmake
@@ -18,26 +18,24 @@
  # 
  ############################################################### 
 
-if (WINDOWS)
 
 MACRO (CLONE_INSTALL _ORIG_TARGET _ORIG_INSTALL _NEWNAMES _INSTALL_LOC )
+if (WINDOWS)
+
 	set(WIN_EXE_EXT .exe)
 	set(${_ORIG_TARGET}_loc ${CMAKE_CURRENT_BINARY_DIR}/\${CMAKE_INSTALL_CONFIG_NAME}/${_ORIG_TARGET}${WIN_EXE_EXT})
 	foreach ( new_target ${_NEWNAMES} )
 		install (CODE "FILE(INSTALL \"${${_ORIG_TARGET}_loc}\" DESTINATION \"\${CMAKE_INSTALL_PREFIX}/${_INSTALL_LOC}\" USE_SOURCE_PERMISSIONS RENAME \"${new_target}${WIN_EXE_EXT}\")")
 	endforeach(new_target)
-ENDMACRO (CLONE_INSTALL)
 
 else()
 
-MACRO (CLONE_INSTALL _ORIG_TARGET _ORIG_INSTALL _NEWNAMES _INSTALL_LOC )
 	foreach ( new_target ${_NEWNAMES} )
 		# If the link target is not in the same directory as the link source
 		# we need to make the link a relative path to the target.  Packaging
 		# tools complain if we use absolute paths.
 		# file(RELATIVE_PATH computes the relative path from one dir to the other
 		file(RELATIVE_PATH PATH_DIFF "${CMAKE_INSTALL_PREFIX}/${_INSTALL_LOC}" "${CMAKE_INSTALL_PREFIX}/${_ORIG_INSTALL}")
-		message(STATUS "echo GGT GGT GGT path diff from ${CMAKE_INSTALL_PREFIX}/${_INSTALL_LOC} to ${CMAKE_INSTALL_PREFIX}/${_ORIG_INSTALL} is ${PATH_DIFF}")
 
 		# If the orig and target dir are the same, file(RELATIVE_PATH returns empty string
 		if ("${PATH_DIFF}" STREQUAL "")
@@ -45,6 +43,7 @@ MACRO (CLONE_INSTALL _ORIG_TARGET _ORIG_INSTALL _NEWNAMES _INSTALL_LOC )
 		endif()
 		install (CODE "execute_process(COMMAND \"${CMAKE_COMMAND}\" -E create_symlink \"${PATH_DIFF}/${_ORIG_TARGET}\" \$ENV{DESTDIR}\${CMAKE_INSTALL_PREFIX}/${_INSTALL_LOC}/${new_target})")
 	endforeach(new_target)
+endif(WINDOWS)
+
 ENDMACRO (CLONE_INSTALL)
 
-endif(WINDOWS)
