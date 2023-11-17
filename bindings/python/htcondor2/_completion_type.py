508cce960af83e92850df7eaec92fdf82512e8b9
Todd L Miller <tlmiller@cs.wisc.edu>
2023-11-17 13:25:43 -0600

    (HTCONDOR-1598)  The enumerations.

--- bindings/python/htcondor2/_completion_type.py
+++ bindings/python/htcondor2/_completion_type.py
@@ -1,1 +1,28 @@
+import sys
 
+if sys.version_info < (3, 5, 0):
+    import compat_enum as enum
+else:
+    import enum
+
+
+class CompletionType(enum.IntEnum):
+    """
+    .. attribute:: Nothing
+    .. attribute:: Resume
+
+    Added in 8.9.11:
+
+    .. attribute:: Exit
+    .. attribute:: Restart
+
+    Added in 9.11.0:
+
+    .. attribute:: Reconfig
+    """
+
+    Nothing  = 0
+    Resume   = 1
+    Exit     = 2
+    Restart  = 3
+    Reconfig = 4
