fe77aafb32832f256763292416e23622bf131c97
Tim Theisen <tim@cs.wisc.edu>
2023-12-09 09:54:11 -0600

    HTCONDOR-2156: Add opsys and ver detection for openSUSE

--- src/condor_sysapi/arch.cpp
+++ src/condor_sysapi/arch.cpp
@@ -598,6 +598,26 @@ sysapi_get_linux_info(void)
 	}
 	}
 	
+    // For now get the pretty name for the info string from /etc/os-release
+    // This is a minimal change for LTS
+    // In the future, just process /etc/os-release
+    if (!info_str) {
+        my_fp = safe_fopen_wrapper_follow("/etc/os-release", "r");
+        if ( my_fp != NULL ) {
+            char tmp_str[200] = {0};
+            while (fgets(tmp_str, sizeof(tmp_str), my_fp)) {
+                dprintf(D_FULLDEBUG, "Result of reading /etc/os-release:  %s \n", tmp_str);
+                if (strstr(tmp_str, "PRETTY_NAME")) {
+                    if (char *equal = strchr(tmp_str, '=')) {
+                        info_str = strdup( equal+1 );
+                        break;
+                    }
+                }
+            }
+            fclose(my_fp);
+        }
+    }
+
 	if (!info_str) {
 		info_str = strdup( "Unknown" );
 	}
