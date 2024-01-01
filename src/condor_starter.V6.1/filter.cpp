d271345e9aee7a66fbcfd5e3b5e4adf4f831cb76
Todd L Miller <tlmiller@cs.wisc.edu>
2024-01-01 16:43:54 -0600

    (HTCONDOR-2122)  Filter the plugin result ClassAds, phase 2.

--- src/condor_starter.V6.1/filter.cpp
+++ src/condor_starter.V6.1/filter.cpp
@@ -37,22 +37,22 @@ class FirstIterator {
 // The caller is responsible for delete'ing the return ClassAd.
 classad::ClassAd *
 filterPluginResults( const classad::ClassAd & ad ) {
-	using namespace classad;
+    using namespace classad;
 
-	std::map<std::string, classad::Value::ValueType> LegalTransferStats {
-		{ "TransferSuccess",        Value::BOOLEAN_VALUE },
-		{ "TransferError",          Value::STRING_VALUE },
-		{ "TransferProtocol",       Value::STRING_VALUE },
-		{ "TransferType",           Value::STRING_VALUE },
-		{ "TransferFileName",       Value::STRING_VALUE },
-		{ "TransferFileBytes",      Value::INTEGER_VALUE },
-		{ "TransferTotalBytes",     Value::INTEGER_VALUE },
-		{ "TransferStartTime",      Value::INTEGER_VALUE },
-		{ "TransferEndTime",        Value::INTEGER_VALUE },
-		{ "ConnectionTimeSeconds",  Value::INTEGER_VALUE },
-		{ "TransferURL",            Value::STRING_VALUE },
-		{ "TransferErrorData",      Value::CLASSAD_VALUE }
-	};
+    std::map<std::string, classad::Value::ValueType, CaseIgnLTStr> LegalTransferStats {
+        { "TransferSuccess",        Value::BOOLEAN_VALUE },
+        { "TransferError",          Value::STRING_VALUE },
+        { "TransferProtocol",       Value::STRING_VALUE },
+        { "TransferType",           Value::STRING_VALUE },
+        { "TransferFileName",       Value::STRING_VALUE },
+        { "TransferFileBytes",      Value::INTEGER_VALUE },
+        { "TransferTotalBytes",     Value::INTEGER_VALUE },
+        { "TransferStartTime",      Value::INTEGER_VALUE },
+        { "TransferEndTime",        Value::INTEGER_VALUE },
+        { "ConnectionTimeSeconds",  Value::REAL_VALUE },
+        { "TransferURL",            Value::STRING_VALUE },
+        { "TransferErrorData",      Value::CLASSAD_VALUE },
+    };
 
 
     // Although FirstIterator() _works_ on ad.begin() and ad.end(),
@@ -60,21 +60,43 @@ filterPluginResults( const classad::ClassAd & ad ) {
     classad::References attributes;
     sGetAdAttrs( attributes, ad );
 
-	std::vector<std::string> intersection;
-	std::set_intersection(
-		attributes.begin(), attributes.end(),
-		FirstIterator(LegalTransferStats.begin()),
-		FirstIterator(LegalTransferStats.end()),
-		std::back_inserter( intersection ),
-		CaseIgnLTStr()
-	);
+    std::vector<std::string> intersection;
+    std::set_intersection(
+        attributes.begin(), attributes.end(),
+        FirstIterator(LegalTransferStats.begin()),
+        FirstIterator(LegalTransferStats.end()),
+        std::back_inserter( intersection ),
+        CaseIgnLTStr()
+    );
 
 
-	if( intersection.empty() ) { return NULL; }
-	classad::ClassAd * filteredAd = new classad::ClassAd();
-	for( const auto & attribute : intersection ) {
-		// FIXME: Filter the internal structure of 'TransferErrorData'.
-		filteredAd->Insert( attribute, ad.Lookup( attribute )->Copy() );
-	}
-	return filteredAd;
+    if( intersection.empty() ) { return NULL; }
+
+    classad::ClassAd * filteredAd = new classad::ClassAd();
+    for( const auto & attribute : intersection ) {
+        classad::Value v;
+        ExprTree * e = ad.Lookup( attribute );
+        if( e->Evaluate(v) ) {
+                // FIXME: Filter the internal structure of 'TransferErrorData'.
+                if( v.GetType() == LegalTransferStats[attribute] ) {
+                filteredAd->Insert( attribute, e->Copy() );
+            } else {
+                dprintf( D_NEVER, "illegal type %d for attribute %s\n", (int)v.GetType(), attribute.c_str() );
+
+                std::string buffer;
+                classad::ClassAdUnParser caup;
+                caup.Unparse(buffer, e);
+                dprintf( D_NEVER, "expression was '%s'\n", buffer.c_str() );
+            }
+        } else {
+            dprintf( D_NEVER, "failed to evaluate attribute %s\n", attribute.c_str() );
+
+            std::string buffer;
+            classad::ClassAdUnParser caup;
+            caup.Unparse(buffer, e);
+            dprintf( D_NEVER, "expression was '%s'\n", buffer.c_str() );
+        }
+    }
+
+    return filteredAd;
 }
