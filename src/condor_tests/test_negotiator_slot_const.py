fe5384c3c8cfde3f992975b5d5a69c695f03e6a9
Greg Thain <gthain@cs.wisc.edu>
2024-01-25 10:06:45 -0600

    HTCONDOR-2250 Add NEGOTIATOR_SLOT_CONSTRAINT test

---
+++ src/condor_tests/test_negotiator_slot_const.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env pytest
+
+
+import logging
+
+from ornithology import *
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# Plan for this test is to stand up a condor with 20
+# static slots, and configure the NEGOTIATOR_SLOT_CONSTRAINT
+# to only look at slot 7 (randomly chosen).  We then
+# submit a sleep 0 job, wait for it to complete, leave
+# it in the queue, and check to see that it ran on slot7.
+
+@standup
+def condor(test_dir):
+    with Condor(test_dir / "condor", 
+            config={
+                "NUM_CPUS": "20",
+                # What? You got a better hack to handle this?
+                "USE FEATURE : StaticSlots\n#": "",
+                "NEGOTIATOR_SLOT_CONSTRAINT": "SlotID == 7",
+            }) as condor:
+        yield condor
+
+@action
+def job_one_handle(condor, test_dir, path_to_sleep):
+    handle = condor.submit(
+        description={
+            "log":                      test_dir / "job.log",
+            "leave_in_queue":           True,
+
+            "universe":                 "vanilla",
+            "should_transfer_files":    False,
+            "executable":               path_to_sleep,
+            "arguments":                0,
+            "Request_Cpus":             1,
+            "Request_Memory":           32,
+            "Request_Disk":             10240,
+        },
+        count=1,
+    )
+
+    handle.wait(
+        condition=ClusterState.all_complete,
+        fail_condition=ClusterState.any_held,
+        verbose=True,
+        timeout=60,
+    )
+
+    yield handle
+
+@action
+def job_one_ad(job_one_handle):
+    for ad in job_one_handle.query():
+        return ad
+
+class TestNegotiatorSlotConstraint:
+    def test_internal_consistency(self, job_one_ad):
+        assert job_one_ad["LastRemoteHost"].startswith("slot7@")
