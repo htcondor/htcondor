38f7e7e108757f15646a55eeb3cec2e2a5d55668
Jaime Frey <jfrey@cs.wisc.edu>
2024-02-06 12:23:56 -0600

    HTCONDOR-2272 Fix EC key generation for LibreSSL
    Like with older OpenSSL, Elliptic Curve key generation with LibreSSL
    requires a call to EC_KEY_set_asn1_flag() to indicate a named curve
    is being used. Otherwise, the key will be rejected by any peers.

--- src/condor_io/condor_secman.cpp
+++ src/condor_io/condor_secman.cpp
@@ -3013,7 +3013,7 @@ SecMan::GenerateKeyExchange(CondorError *errstack)
 	}
 	pkey.reset(pkey_raw);
 
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
+#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
 	std::unique_ptr<EC_KEY, decltype(&EC_KEY_free)> key(EVP_PKEY_get1_EC_KEY(pkey.get()), &EC_KEY_free);
 	if (key) {
 		EC_KEY_set_asn1_flag(key.get(), OPENSSL_EC_NAMED_CURVE);
