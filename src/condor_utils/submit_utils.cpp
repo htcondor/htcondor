af59c2c010f7cac1d94ca4f3851e601232c1bc39
Greg Thain <gthain@cs.wisc.edu>
2024-03-01 15:44:41 -0600

    HTCONDOR-2319 code review

--- src/condor_utils/submit_utils.cpp
+++ src/condor_utils/submit_utils.cpp
@@ -2251,7 +2251,7 @@ int SubmitHash::SetIWD()
 	if (ComputeIWD()) { ABORT_AND_RETURN(1); }
 	AssignJobString(ATTR_JOB_IWD, JobIwd.c_str());
 	if (!SubmitMacroSet.sources.empty()) {
-		const char *filename = submit_param("SUBMIT_FILE");
+		auto_free_ptr filename = submit_param("SUBMIT_FILE");
 		if (filename) {
 			AssignJobString(ATTR_JOB_SUBMIT_FILE, filename);
 		}
