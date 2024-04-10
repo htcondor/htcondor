3de5707ddb7c5822080fa3b9e0bcb6efa11976dc
Greg Thain <gthain@cs.wisc.edu>
2024-04-10 14:46:01 -0500

    HTCONDOR-2364 Changes per code review

--- src/condor_schedd.V6/qmgmt.cpp
+++ src/condor_schedd.V6/qmgmt.cpp
@@ -4268,9 +4268,6 @@ int DestroyProc(int cluster_id, int proc_id)
 		return DESTROYPROC_SUCCESS_DELAY;
 	}
 
-	// Remove related Jobs
-	removeOtherJobs(cluster_id, proc_id);
-
 		// should we leave the job in the queue?
 	bool leave_job_in_q = false;
 	{
