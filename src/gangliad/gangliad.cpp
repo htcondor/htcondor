0451317d6d169e7fffb5e2400965c01cd09ca6a5
Todd Tannenbaum <tannenba@cs.wisc.edu>
2024-05-07 15:25:37 -0500

    Improve some misc dprintf debug log messages. HTCONDOR-2417
    Set the correct subsystem, add additional info, etc.  Did this while
    cleaning up the condor_gangliad debug log.

--- src/gangliad/gangliad.cpp
+++ src/gangliad/gangliad.cpp
@@ -353,9 +353,9 @@ GangliaD::publishMetric(Metric const &m)
 	int slope = metric.gangliaSlope();
 
     m_ganglia_metrics_sent++;
-	dprintf(D_FULLDEBUG,"%spublishing %s=%s, group=%s, units=%s, derivative=%d, type=%s, title=%s, desc=%s, cluster=%s, spoof_host=%s, lifetime=%d\n",
+	dprintf(D_FULLDEBUG,"%spublishing %s=%s, machine=%s <%s>, group=%s, units=%s, derivative=%d, type=%s, title=%s, desc=%s, cluster=%s, spoof_host=%s, lifetime=%d\n",
 			m_ganglia_noop ? "noop mode: " : "",
-			metric.name.c_str(), value.c_str(), metric.group.c_str(),  metric.units.c_str(), metric.derivative, metric.gangliaMetricType(), metric.title.c_str(),
+			metric.name.c_str(), value.c_str(), metric.machine.c_str(), metric.ip.c_str(), metric.group.c_str(),  metric.units.c_str(), metric.derivative, metric.gangliaMetricType(), metric.title.c_str(),
 			metric.desc.c_str(), metric.cluster.c_str(), spoof_host.c_str(), metric.lifetime < 0 ? m_dmax : metric.lifetime);
 	if( !m_ganglia_noop ) {
 		bool ok = ganglia_send(
