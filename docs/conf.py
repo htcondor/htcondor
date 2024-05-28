628946acaedbd7d7dc89a554707d40e5b2707775
Tim Theisen <tim@cs.wisc.edu>
2024-05-28 07:12:08 -0500

    HTCONDOR-2468: Update to 23.0.13 in LTS dev branch

--- docs/conf.py
+++ docs/conf.py
@@ -70,7 +70,7 @@ Licensed under the Apache License, Version 2.0.'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '23.0.11'
+release = '23.0.13'
 
 rst_epilog = """
 .. |release_date| replace:: Month Day, 2024
