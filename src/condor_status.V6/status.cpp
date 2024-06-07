b1dee54dfa44a28d0f5db1de67bc016aa1eee2bf
Jaime Frey <jfrey@cs.wisc.edu>
2024-06-07 16:57:31 -0500

    HTCONDOR-2475 Redo data structures in AdCluster

--- src/condor_status.V6/status.cpp
+++ src/condor_status.V6/status.cpp
@@ -1622,9 +1622,9 @@ main (int argc, char *argv[])
 	if (dash_group_by) {
 		if ( ! dashAttributes.empty()) {
 			std::string attrs = JoinAttrNames(dashAttributes, ",");
-			ad_groups.setSigAttrs(attrs.c_str(), true, true);
+			ad_groups.setSigAttrs(attrs.c_str(), true);
 		} else {
-			ad_groups.setSigAttrs("Cpus Memory GPUs IOHeavy START", false, true);
+			ad_groups.setSigAttrs("Cpus Memory GPUs IOHeavy START", true);
 		}
 		ad_groups.keepAdKeys(get_ad_name_string);
 	}
