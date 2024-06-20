b233dd8365563f913cfb307b1b927abfbb8dc2de
Greg Thain <gthain@cs.wisc.edu>
2024-06-20 16:17:15 -0500

    HTCONDOR-2513 Fixed for test_drain_policies to run on unprivileged cgroups
    Turn off per-job swap, to force the OOM killer to fire.
    
    On Ubuntu machine without memory.peak_usage, correctly hold jobs
    that OOM very early on.

--- src/condor_starter.V6.1/vanilla_proc.cpp
+++ src/condor_starter.V6.1/vanilla_proc.cpp
@@ -1025,10 +1025,15 @@ VanillaProc::outOfMemoryEvent() {
 	// lower than the limit when the OOM killer fired.
 	// So have some slop, just in case.
 	if (usageMB < (0.9 * (m_memory_limit / (1024 * 1024)))) {
-		dprintf(D_ALWAYS, "Evicting job because system is out of memory, even though the job is below requested memory: Usage is %lld Mb limit is %lld\n", (long long)usageMB, (long long)m_memory_limit);
-		Starter->jic->notifyStarterError("Worker node is out of memory", true, 0, 0);
-		Starter->jic->allJobsGone(); // and exit to clean up more memory
-		return 0;
+		// But sometimes the job dies before we can poll for memory on systems 
+		// that don't have a memory.peak, and we get 0 MB. Assume job should go
+		// on hold in that case 
+		if (usageMB > 0) {
+			dprintf(D_ALWAYS, "Evicting job because system is out of memory, even though the job is below requested memory: Usage is %lld Mb limit is %lld\n", (long long)usageMB, (long long)m_memory_limit);
+			Starter->jic->notifyStarterError("Worker node is out of memory", true, 0, 0);
+			Starter->jic->allJobsGone(); // and exit to clean up more memory
+			return 0;
+		}
 	}
 
 	std::string ss;
