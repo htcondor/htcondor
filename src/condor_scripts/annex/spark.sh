aee3327256b457b401ad9facaac0fce866e08487
Todd L Miller <tlmiller@cs.wisc.edu>
2024-06-27 16:30:25 -0500

    (HTCONDOR-2446)  Turn clean-up back on for Spark.

--- src/condor_scripts/annex/spark.sh
+++ src/condor_scripts/annex/spark.sh
@@ -183,7 +183,7 @@ echo "${CONTROL_PREFIX} PILOT_DIR ${PILOT_DIR}"
 
 function cleanup() {
     echo "Cleaning up temporary directory..."
-    # rm -fr ${PILOT_DIR}
+    rm -fr ${PILOT_DIR}
 }
 trap cleanup EXIT
 
