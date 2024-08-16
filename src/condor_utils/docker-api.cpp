477a08b8694872531fd4eafbc9590ec8c50338c7
Greg Thain <gthain@cs.wisc.edu>
2024-08-16 08:40:08 -0500

    HTCONDOR-2591 docker universe jobs with GPUs should pass --device when the AssignedGPUs has the "stable" name as well as the CUDA name

--- src/condor_utils/docker-api.cpp
+++ src/condor_utils/docker-api.cpp
@@ -9,6 +9,7 @@
 #include "file_lock.h"
 #include "condor_rw.h"
 #include "basename.h"
+#include "nvidia_utils.h"
 
 #include "docker-api.h"
 #include <algorithm>
@@ -199,8 +200,12 @@ int DockerAPI::createContainer(
 #else
 	// if the startd has assigned us a gpu, add in the
 	// nvidia devices.  AssignedGPUS looks like CUDA0, CUDA1, etc.
+	// when we don't use the "stable" uuids.
 	// map these to /dev/nvidia0, /dev/nvidia1...
 	// arguments to mount the nvidia devices
+	//
+	// Otherwise, with the "stable" uuids, they will
+	// look like GPU-XXXXX.
 	std::string assignedGpus;
 	machineAd.LookupString("AssignedGPUs", assignedGpus);
 	if  (assignedGpus.length() > 0) {
@@ -212,18 +217,43 @@ int DockerAPI::createContainer(
 		runArgs.AppendArg("--device");
 		runArgs.AppendArg("/dev/nvidia-uvm");
 
-		size_t offset = 0;
+		// If the startd is configured for the "CUDA" style
+		// GPU names...
+		if (assignedGpus.starts_with("CUDA")) {
+			size_t offset = 0;
 
 			// For each CUDA substring in assignedGpus...
-		while ((offset = assignedGpus.find("CUDA", offset)) != std::string::npos) {
-			offset += 4; // strlen("CUDA")
-			size_t comma = assignedGpus.find(",", offset);
+			while ((offset = assignedGpus.find("CUDA", offset)) != std::string::npos) {
+				offset += 4; // strlen("CUDA")
+				size_t comma = assignedGpus.find(",", offset);
 
-			// ...append to command line args -device /dev/nvidiaXX
-			std::string deviceName("/dev/nvidia");
-			deviceName += assignedGpus.substr(offset, comma - offset);
-			runArgs.AppendArg("--device");
-			runArgs.AppendArg(deviceName);
+				// ...append to command line args -device /dev/nvidiaXX
+				std::string deviceName("/dev/nvidia");
+				deviceName += assignedGpus.substr(offset, comma - offset);
+				runArgs.AppendArg("--device");
+				runArgs.AppendArg(deviceName);
+			}
+		} else {
+			// Get a map of long-form uuid device minor.  Assume that device minor
+			// matches the last digit of /dev/nvidiaX
+			bool map_all_gpus = param_boolean("DOCKER_MAP_ALL_GPUS", false);
+			auto uuid_dev_map = make_nvidia_uuid_to_dev_map();
+			
+			// For each of our short-named assigned GPU, find the device number
+			// n-squared, but should be small.
+			for (const auto &gpu: StringTokenIterator(assignedGpus)) {
+				for (const auto &[uuid, dev]: uuid_dev_map) {
+					if (map_all_gpus || uuid.starts_with(gpu)) {
+						std::string deviceName;
+
+						// and finally append it to our command line args
+						formatstr(deviceName, "/dev/nvidia%u", minor(dev));
+						runArgs.AppendArg("--device");
+						runArgs.AppendArg(deviceName);
+						break; // assume only one matches
+					}
+				}
+			}
 		}
 	}
 #endif
--- src/condor_utils/nvidia_utils.h
+++ src/condor_utils/nvidia_utils.h
@@ -4,6 +4,10 @@
 #ifdef LINUX
 #include "sys/sysmacros.h" // for dev_t
 
+#include <string>
+#include <vector>
+
+std::vector<std::pair<std::string, dev_t>> make_nvidia_uuid_to_dev_map();
 std::vector<dev_t> nvidia_env_var_to_exclude_list(const std::string &visible_devices_env);
 #endif
 #endif
