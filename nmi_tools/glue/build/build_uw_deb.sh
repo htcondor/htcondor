bf4bef00d2fce96562283be9bdbb33882b2e86b9
Tim Theisen <tim@cs.wisc.edu>
2024-10-03 11:21:55 -0500

    HTCONDOR-2520: Add SYS99 for release builds

--- nmi_tools/glue/build/build_uw_deb.sh
+++ nmi_tools/glue/build/build_uw_deb.sh
@@ -72,6 +72,7 @@ echo "Distribution is $dist"
 if [ "$PRE_RELEASE" = 'OFF' ]; then
     # Changelog entry is present for final release build
     dch --release --distribution $dist ignored
+    sed -i s/$condor_version-[0-9]*/\&+SYS99/
 else
     # Generate a changelog entry
     dch --distribution $dist --newversion "$condor_version-0.$condor_build_id+SYS99" "Automated build"
