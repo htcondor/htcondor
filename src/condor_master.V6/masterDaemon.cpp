5fa4df6638cc1b868d7284b6a4dd93409ec2425d
Todd L Miller <tlmiller@cs.wisc.edu>
2024-10-18 14:32:44 -0500

    Quick hack to make *_ADDRESS_FILE work as expected for snaked.

--- src/condor_master.V6/masterDaemon.cpp
+++ src/condor_master.V6/masterDaemon.cpp
@@ -809,6 +809,11 @@ int daemon::RealStart( )
 						}
 						break;
 					}
+					if( strcmp( configArg, "-genus" ) == 00 ) {
+					    foundLocalName = true;
+					    setLocalName = false;
+					    break;
+					}
 				}
 				if(! foundLocalName) {
 					args.AppendArg( "-local-name" );
