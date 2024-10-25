da2fa710b1241c841d09f82f511cc59c9362a74a
Cole Bollig <cabollig@wisc.edu>
2024-10-25 08:30:25 -0500

    HTCONDOR-2699: Add console logging

--- docs/_static/js/tool-mindmap.js
+++ docs/_static/js/tool-mindmap.js
@@ -2,6 +2,7 @@ window.addEventListener('load', () => {
     document.querySelectorAll('*[class*="link:"]').forEach(node => {
         node.classList.forEach(className => {
             if (className.startsWith('link:')) {
+                console.log(node)
                 const tool = className.split(':')[1];
                 node.addEventListener('click', () => {
                     window.open(`./${tool}.html`, '_self');
