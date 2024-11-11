26427cb162c93692328e585048688acff5ac3c98
Greg Thain <gthain@cs.wisc.edu>
2024-11-11 10:43:44 -0600

    HTCONDOR-2723 Remove uncalled functions

--- src/condor_utils/passwd_cache.unix.h
+++ src/condor_utils/passwd_cache.unix.h
@@ -88,12 +88,6 @@ class passwd_cache {
 		 * returns true on success. */
 		bool init_groups(const char* user, gid_t additional_gid = 0);
 		
-		/* for testing purposes only. 
-		 * returns the age of a given cache entry.
-		 * returns -1 on failure. */
-		int get_uid_entry_age(const char *user);
-		int get_group_entry_age(const char *user);
-
 		/* also for testing.
 		 * Returns maximum lifetime of a cache entry, in seconds.
 		 */
