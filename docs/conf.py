ac87447263cf7fb15dd222202cebfbd91b6a0fa3
Tim Theisen <tim@cs.wisc.edu>
2024-12-05 08:05:31 -0600

    HTCONDOR-2778: Bump version to 23.10.20

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ Licensed under the Apache License, Version 2.0.'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '23.10.19'
+release = '23.10.20'
 
 rst_epilog = """
 .. |release_date| replace:: Month Day, 2024
