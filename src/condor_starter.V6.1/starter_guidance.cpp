059daa9c2a83fb65982b811096a6ebf5392483a0
Todd L Miller <tlmiller@cs.wisc.edu>
2025-01-24 15:38:12 -0600

    (HTCONDOR-2687)  Add some more logging messages about guidance.

--- src/condor_starter.V6.1/starter_guidance.cpp
+++ src/condor_starter.V6.1/starter_guidance.cpp
@@ -200,6 +200,7 @@ Starter::requestGuidanceJobEnvironmentReady( Starter * s ) {
 	ClassAd request;
 	ClassAd guidance;
 	request.InsertAttr(ATTR_REQUEST_TYPE, RTYPE_JOB_ENVIRONMENT);
+    dprintf( D_ALWAYS, "Requesting guidance from shadow about job environment being ready.\n" );
 
 	GuidanceResult rv = GuidanceResult::Invalid;
 	if( s->jic->genericRequestGuidance( request, rv, guidance ) ) {
@@ -297,6 +298,7 @@ Starter::requestGuidanceJobEnvironmentUnready( Starter * s ) {
 	ClassAd request;
 	ClassAd guidance;
 	request.InsertAttr(ATTR_REQUEST_TYPE, RTYPE_JOB_ENVIRONMENT);
+    dprintf( D_ALWAYS, "Requesting guidance from shadow about job environment being unready.\n" );
 
 	GuidanceResult rv = GuidanceResult::Invalid;
 	if( s->jic->genericRequestGuidance( request, rv, guidance ) ) {
