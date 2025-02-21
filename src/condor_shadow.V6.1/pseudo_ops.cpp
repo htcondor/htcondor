ca7e07e8d3ffe528e1dbdf94bef45127a9ad38e0
Todd L Miller <tlmiller@cs.wisc.edu>
2025-02-21 09:36:55 -0600

    (HTCONDOR-2914)  Avoid the race condition better.

--- src/condor_shadow.V6.1/pseudo_ops.cpp
+++ src/condor_shadow.V6.1/pseudo_ops.cpp
@@ -1125,36 +1125,33 @@ pseudo_request_guidance( const ClassAd & request, ClassAd & guidance ) {
 
 	if( requestType == RTYPE_JOB_ENVIRONMENT ) {
 		dprintf( D_ALWAYS, "Received request for guidance about the job environment.\n" );
-dprintf( D_ALWAYS, "DOWNLOAD STATUS %d == UNKNOWN %s\n",
-    thisRemoteResource->download_transfer_info.xfer_status,
-    thisRemoteResource->download_transfer_info.xfer_status == XFER_STATUS_UNKNOWN ? "true" : "false"
-);
 		if( thisRemoteResource->download_transfer_info.xfer_status == XFER_STATUS_UNKNOWN ) {
-dprintf( D_ALWAYS, "UPLOAD STATUS %d == XFER_STATUS_DONE %s\n",
-    thisRemoteResource->upload_transfer_info.xfer_status,
-    thisRemoteResource->upload_transfer_info.xfer_status == XFER_STATUS_DONE ? "true" : "false"
-);
-dprintf( D_ALWAYS, "UPLOAD SUCCESS %s\n",
-    thisRemoteResource->upload_transfer_info.success == true ? "true" : "false"
-);
-			if ( thisRemoteResource->upload_transfer_info.xfer_status == XFER_STATUS_ACTIVE ) {
+			// This isn't copied into thisRemoteResource->download_transfer_info
+			// until the FTO reaper fires, which might be a a while.
+			const FileTransfer::FileTransferInfo current = thisRemoteResource->filetrans.GetInfo();
+			if ( current.xfer_status == XFER_STATUS_ACTIVE ) {
 				// We haven't gotten an update from our side's FTO yet.  What
 				// I would like to do is go back into the event loop and re-
 				// check after a few seconds, but the syscall socket code is
 				// _very_ synchronous.  Instead, since we'll want to have
 				// this command for other purposes later, ask the stater to
 				// call us back in a few seconds.
+				//
+				// Because we're now checking the live copy of the FTO status
+				// variables, this should never happen, but since we have a
+				// coded work-around and it's still at least possible, let's
+				// leave it in.
 				guidance.InsertAttr(ATTR_COMMAND, COMMAND_RETRY_REQUEST);
 				guidance.InsertAttr(ATTR_RETRY_DELAY, 5);
-			} else if( thisRemoteResource->upload_transfer_info.xfer_status == XFER_STATUS_DONE
-			 && thisRemoteResource->upload_transfer_info.success == true
+			} else if( current.xfer_status == XFER_STATUS_DONE
+			 && current.success == true
 			) {
 				guidance.InsertAttr(ATTR_COMMAND, COMMAND_START_JOB);
 			} else if (
-				thisRemoteResource->upload_transfer_info.xfer_status == XFER_STATUS_DONE
-			 && thisRemoteResource->upload_transfer_info.success == false
+				current.xfer_status == XFER_STATUS_DONE
+			 && current.success == false
 			) {
-				if( thisRemoteResource->upload_transfer_info.try_again == true ) {
+				if( current.try_again == true ) {
 					// I'm not sure this case ever actually happens in practice,
 					// but in case it does, this seems like the right thing to do.
 					guidance.InsertAttr(ATTR_COMMAND, COMMAND_RETRY_TRANSFER);
@@ -1189,6 +1186,8 @@ dprintf( D_ALWAYS, "UPLOAD SUCCESS %s\n",
 				guidance.InsertAttr( ATTR_COMMAND, COMMAND_CARRY_ON );
 			}
 		} else {
+			// The starter asked for guidance about the job environment, but
+			// we think we're doing output transfer.
 			guidance.InsertAttr(ATTR_COMMAND, COMMAND_CARRY_ON);
 		}
 
