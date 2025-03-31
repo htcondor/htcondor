a74196b0e1d8f331a350b1e9b7c046888940b7d5
Todd Tannenbaum <tannenba@cs.wisc.edu>
2025-03-31 17:01:39 -0500

    Fix CE Dashboard Health Error msg. HTCONDOR-2969
    Fix typo resulting in incomplete CE health error message on the admin
    landing page.

--- view/ce_dashboard/flask/blueprints/landing.py
+++ view/ce_dashboard/flask/blueprints/landing.py
@@ -130,7 +130,7 @@ def ce_info_from_collectors(resource_info_by_fqdn):
                 info.healthInfo="CE Collector unreachable"
                 continue
             for gridAd in gridAds:
-                if "GridResourceUnavailableTime" in gridAd and "GriqdResourceUnavailableTimeReason" in gridAd:
+                if "GridResourceUnavailableTime" in gridAd and "GridResourceUnavailableTimeReason" in gridAd:
                     hinfo = classad.ExprTree('strcat("Since ",formatTime(GridResourceUnavailableTime),": ",GridResourceUnavailableTimeReason)').eval(gridAd)
                     info.health="Poor"
                     info.healthInfo=hinfo
