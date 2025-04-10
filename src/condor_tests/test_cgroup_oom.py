144a668151cdb054fcd3562d3a65160d7896af5e
Greg Thain <gthain@cs.wisc.edu>
2025-04-10 00:36:27 -0500

    HTCONDOR-3007 Add process escape test and touch up comments

--- src/condor_tests/test_cgroup_oom.py
+++ src/condor_tests/test_cgroup_oom.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env pytest
 
-
-# Test that we can set environment variables in the submit
-# file, and they make it to the job, along with the magic
-# $$(CondorScratchDir) token expanded properly
+# Test that we can create sub-cgroups.  If not, skip the test
+# if so, turn on the secret unprivileged cgroup option,
+# and submit a job that uses more memory than we have
+# requested.  Verify that it goes on hold.
 
 import logging
 import pytest
@@ -20,7 +20,8 @@ def condor(test_dir):
     with Condor(test_dir / "condor", config={"CREATE_CGROUP_WITHOUT_ROOT": "true"}) as condor:
         yield condor
 
-# The actual job will just print environment vars of interest to stdout
+# The actual job will allocate 200 Mb in python, but request 50Mb.
+# The 50Mb might get rounded up to 128, so be careful.
 @action
 def job_script_contents():
     return format_script( """
@@ -69,6 +70,37 @@ def completed_test_job(condor, test_job_hash):
     )
     return ctj
 
+@action
+def escaping_job_hash(test_dir, path_to_python, test_script):
+    # A job which spawns "sleep 1234", with all the condor tracking env vars removed
+    # and nohup'd, to avoid all non-cgroup tracking mechanisms.  Writes the pid
+    # to pid file, which lives in the test_dir.  If the pid in pidfile exists
+    # after the job exits, the sleep escapes the job, and the test should fail.
+    return {
+            "shell": "nohup /usr/bin/env -i /usr/bin/sleep 1234 & echo $! > pidfile",
+            "universe": "vanilla",
+            "output": "output",
+            "error": "error",
+            "log": "log",
+            "request_memory": "50m",
+            "request_cpus": "1",
+            "request_disk": "200m"
+            }
+
+@action
+def escaping_test_job(condor, escaping_job_hash):
+    ej = condor.submit(
+        {**escaping_job_hash}, count=1
+    )
+    assert ej.wait(
+        condition=ClusterState.all_complete,
+        timeout=120,
+        verbose=True,
+        fail_condition=ClusterState.any_held,
+    )
+    return ej
+
+
 # This test only works if we start in a writeable (delegated)
 # cgroup.  Return true if this is so
 def CgroupIsWriteable():
@@ -89,4 +121,10 @@ def CgroupIsWriteable():
 class TestCgroupOOM:
     @pytest.mark.skipif(not CgroupIsWriteable(), reason="Test was not born in a writeable cgroup v2")
     def test_cgroup_oom(self, completed_test_job):
-        assert True
+        assert completed_test_job.state.all_held()
+
+    @pytest.mark.skipif(not CgroupIsWriteable(), reason="Test was not born in a writeable cgroup v2")
+    def test_cgroup_escape(self, escaping_test_job):
+        with open('pidfile', 'r') as file:
+            pid = file.read().rstrip()
+        assert not os.path.exists("/proc/" + pid)
