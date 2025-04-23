8ff686aba6b6fccaa57987e2502e1f5d1375cfe2
Todd L Miller <tlmiller@cs.wisc.edu>
2025-04-23 12:35:50 -0500

    (HTCONDOR-2887)  This test has all of the race conditions.

--- src/condor_tests/test_cif.py
+++ src/condor_tests/test_cif.py
@@ -48,7 +48,6 @@ def completed_cif_job(the_condor, path_to_sleep, test_dir):
         "transfer_executable":      False,
         "should_transfer_files":    True,
 
-        "arguments":                5,
         "log":                      "cif_job.log",
         "output":                   "cif_job.output",
         "error":                    "cif_job.error",
@@ -75,6 +74,26 @@ def completed_cif_job(the_condor, path_to_sleep, test_dir):
     return job_handle
 
 
+@action
+def the_big_lock_dir(test_dir):
+    return test_dir / "big.lock.d"
+
+
+@action
+def the_big_condor(test_dir, the_big_lock_dir):
+    local_dir = test_dir / "big-condor"
+
+    with Condor(
+        local_dir=local_dir,
+        config={
+            "SHADOW_DEBUG":     "D_CATEGORY D_ZKM D_SUB_SECOND D_PID",
+            "LOCK":             the_big_lock_dir.as_posix(),
+            "NUM_CPUS":         4,
+        },
+    ) as the_condor:
+        yield the_condor
+
+
 def lock_dir_is_clean(the_lock_dir):
     files = list(the_lock_dir.iterdir())
     for file in files:
@@ -82,15 +101,56 @@ def lock_dir_is_clean(the_lock_dir):
     assert len(files) == 2
 
 
-def output_is_as_expected(completed_cif_job):
-    ad = completed_cif_job.query(
-        projection=['Out'],
-        limit=1,
-    )[0];
+@action
+def completed_cif_jobs(the_big_condor, path_to_sleep, test_dir):
+    os.chdir(test_dir.as_posix())
+    (test_dir / "big_input1.txt").write_text("input A\n")
+    (test_dir / "big_input2.txt").write_text("II input\n");
+    (test_dir / "big_input3.txt").write_text("input line 3\n" );
 
-    out = Path(ad['Out'])
-    text = out.read_text()
-    assert text == "input A\nII input\ninput line 3\n"
+    job_description = {
+        "universe":                 "vanilla",
+
+        "shell":                    "cat big_input1.txt big_input2.txt big_input3.txt; sleep 20",
+        "transfer_executable":      False,
+        "should_transfer_files":    True,
+
+        "log":                      "cif_job.log",
+        "output":                   "cif_job.output.$(CLUSTER).$(PROCESS)",
+        "error":                    "cif_job.error",
+
+        "request_cpus":             1,
+        "request_memory":           1,
+
+        "MY.CommonInputFiles":      '"big_input1.txt, big_input2.txt"',
+        "transfer_input_files":     "big_input3.txt",
+
+        "leave_in_queue":           True,
+    }
+
+    job_handle = the_big_condor.submit(
+        description=job_description,
+        count=8,
+    )
+
+    assert job_handle.wait(
+        timeout=180,
+        condition=ClusterState.all_complete,
+    )
+
+    return job_handle
+
+
+# -----------------------------------------------------------------------------
+
+
+def output_is_as_expected(completed_cif_job):
+    ads = completed_cif_job.query( projection=['Out'] )
+
+    for ad in ads:
+        out = Path(ad['Out'])
+        text = out.read_text()
+        assert text == "input A\nII input\ninput line 3\n"
 
 
 def count_shadow_log_lines(the_condor, the_phrase):
@@ -102,26 +162,32 @@ def count_shadow_log_lines(the_condor, the_phrase):
     return len(lines)
 
 
-def shadow_log_is_as_expected(the_condor):
+def shadow_log_is_as_expected(the_condor, count):
     staging_commands_sent = count_shadow_log_lines(
         the_condor, "StageCommonFiles"
     )
-    assert staging_commands_sent == 1
+    assert staging_commands_sent == count
 
     successful_staging_commands = count_shadow_log_lines(
         the_condor, "Staging successful"
     )
-    assert successful_staging_commands == 1
+    assert successful_staging_commands == count
 
     keyfile_touches = count_shadow_log_lines(
         the_condor, "Elected producer touch"
     )
-    assert keyfile_touches == 1
+    assert keyfile_touches == count
 
 
 class TestCIF:
 
     def test_one_cif_job(self, the_lock_dir, the_condor, completed_cif_job):
         output_is_as_expected(completed_cif_job)
-        shadow_log_is_as_expected(the_condor)
+        shadow_log_is_as_expected(the_condor, 1)
         lock_dir_is_clean(the_lock_dir)
+
+
+    def test_many_cif_jobs(self, the_big_lock_dir, the_big_condor, completed_cif_jobs):
+        output_is_as_expected(completed_cif_jobs)
+        shadow_log_is_as_expected(the_big_condor, 2)
+        lock_dir_is_clean(the_big_lock_dir)
