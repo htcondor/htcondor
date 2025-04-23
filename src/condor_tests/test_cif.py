84bc21312b0b7e0ec2fbbef1cba851652eee6a0c
Todd L Miller <tlmiller@cs.wisc.edu>
2025-04-23 13:04:09 -0500

    (HTCONDOR-2887)  Remove a bunch of race conditions from this test.

--- src/condor_tests/test_cif.py
+++ src/condor_tests/test_cif.py
@@ -9,10 +9,14 @@ from ornithology import (
     Condor,
     action,
     ClusterState,
+    format_script,
+    write_file,
 )
 
 import os
 from pathlib import Path
+
+import htcondor2
 
 
 @action
@@ -94,15 +98,31 @@ def the_big_condor(test_dir, the_big_lock_dir):
         yield the_condor
 
 
-def lock_dir_is_clean(the_lock_dir):
-    files = list(the_lock_dir.iterdir())
-    for file in files:
-        assert file.name == "shared_port_ad" or file.name == "InstanceLock"
-    assert len(files) == 2
+@action
+def cif_jobs_script(test_dir):
+    script_path = test_dir / "big_job_script"
 
+    script_text = format_script(
+    """
+        #!/bin/bash
+
+        cat big_input1.txt big_input2.txt big_input3.txt
+
+        while true; do
+            sleep 1
+            if [ -e $1 ]; then
+                exit 0
+            fi
+        done
+
+        exit 1
+    """
+    )
+    write_file(script_path, script_text)
+    return script_path
 
 @action
-def completed_cif_jobs(the_big_condor, path_to_sleep, test_dir):
+def completed_cif_jobs(the_big_condor, path_to_sleep, test_dir, cif_jobs_script):
     os.chdir(test_dir.as_posix())
     (test_dir / "big_input1.txt").write_text("input A\n")
     (test_dir / "big_input2.txt").write_text("II input\n");
@@ -111,8 +131,9 @@ def completed_cif_jobs(the_big_condor, path_to_sleep, test_dir):
     job_description = {
         "universe":                 "vanilla",
 
-        "shell":                    "cat big_input1.txt big_input2.txt big_input3.txt; sleep 20",
-        "transfer_executable":      False,
+        "executable":               cif_jobs_script.as_posix(),
+        "arguments":                (test_dir / "kill-$(ClusterID).$(ProcID)").as_posix(),
+        "transfer_executable":      True,
         "should_transfer_files":    True,
 
         "log":                      "cif_job.log",
@@ -126,15 +147,47 @@ def completed_cif_jobs(the_big_condor, path_to_sleep, test_dir):
         "transfer_input_files":     "big_input3.txt",
 
         "leave_in_queue":           True,
+        "hold":                     True,
     }
 
+    # Submit eight jobs on hold.
     job_handle = the_big_condor.submit(
         description=job_description,
         count=8,
     )
 
+    # Release four of the jobs.
+    jobIDs = [ f"{job_handle.clusterid}.{x}" for x in range(0,4) ]
+    the_big_condor.act( htcondor2.JobAction.Release, jobIDs )
+
+    # Wait for them to start.
     assert job_handle.wait(
-        timeout=180,
+        timeout=60,
+        condition=ClusterState.running_exactly(4),
+        fail_condition=ClusterState.any_terminal
+    )
+
+    # Release the other four jobs.
+    jobIDs = [ f"{job_handle.clusterid}.{x}" for x in range(4,8) ]
+    the_big_condor.act( htcondor2.JobAction.Release, jobIDs )
+
+    # Touch the kill files on the first four jobs, causing them to finish.
+    for i in range(0,4):
+        (test_dir / f"kill-{job_handle.clusterid}.{i}").touch(exist_ok=True)
+
+    # Wait for the last four jobs to start.
+    assert job_handle.wait(
+        timeout=60,
+        condition=ClusterState.none_idle,
+    )
+
+    # Touch the kill files on the last four jobs, causing them to finish.
+    for i in range(4,8):
+        (test_dir / f"kill-{job_handle.clusterid}.{i}").touch(exist_ok=True)
+
+    # Wait for the last four jobs to finish.
+    assert job_handle.wait(
+        timeout=60,
         condition=ClusterState.all_complete,
     )
 
@@ -177,6 +230,13 @@ def shadow_log_is_as_expected(the_condor, count):
         the_condor, "Elected producer touch"
     )
     assert keyfile_touches == count
+
+
+def lock_dir_is_clean(the_lock_dir):
+    files = list(the_lock_dir.iterdir())
+    for file in files:
+        assert file.name == "shared_port_ad" or file.name == "InstanceLock"
+    assert len(files) == 2
 
 
 class TestCIF:
