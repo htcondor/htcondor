htcondor/htcondor

commit 5fcb6dad2b5445ffd22c63ec3efacf89dbb10f8c

HTCONDOR-2844 Add knob to disable deleting of unclaimed broken dslots    A new knob CONTINUE_TO_ADVERTISE_BROKEN_DYNAMIC_SLOTS controls      whether broken dslots will be deleted or not when the become unclaimed    Adds a new broken_things data structure to the STARTD that holds information      about slots and other resources that are broken, this structure will      hold the job and claim information that were in effect at the time the      resource became broken so that the STARTD daemon ad can populate a      new BrokenContextAds attribute.  A new knob BROKEN_SLOT_CONTEXT_ATTRS      controls which attributes from the job and claim are published there.

src/condor_startd.V6/ResState.cpp
@@ -387,9 +387,13 @@ ResState::eval_policy( void )
 		}
 		break;	// case preempting_state:
 
-
 	case unclaimed_state:
 		if (rip->is_dynamic_slot() || rip->is_broken_slot()) {
+			if (rip->is_dynamic_slot() && rip->r_attr->is_broken()) {
+				// we don't want dynamic slots with broken resources
+				// to be deleted when the claim is released instead we just stick around.broken...
+				if (continue_to_advertise_broken_dslots) break;
+			}
 #if HAVE_JOB_HOOKS
 				// If we're currently fetching we can't delete
 				// ourselves. If we do when the hook returns we won't
@@ -455,6 +459,12 @@ ResState::eval_policy( void )
 
 
 	case owner_state:
+			// once a dynamic slot with broken resources transitions to owner state
+			// it should stay there if we want to advertise broken dslots
+		if (rip->r_attr && rip->r_attr->is_broken()) {
+			if (continue_to_advertise_broken_dslots) break;
+		}
+
 			// If the dynamic slot is allocated in the owner state
 			// (e.g. because of START expression contains attributes
 			// of job ClassAd), it may never go back to Unclaimed 
@@ -1043,7 +1053,7 @@ ResState::starterExited( void )
 	case matched_state:
 			// for all 3 of these, once the starter is gone, we can
 			// enter the destination directly.
-		dprintf( D_ALWAYS, "State change: starter exited\n" );
+		dprintf( D_ALWAYS, "State change: starter exited : %s(%d)\n", __FILE__, __LINE__ );
 		change( r_destination );
 		return TRUE; // XXX: change TRUE
 		break;
@@ -1053,7 +1063,7 @@ ResState::starterExited( void )
 			// request to claim, we can finally accept it.  if that
 			// pending request is gone for some reason, go back to
 			// the Owner state... 
-		dprintf( D_ALWAYS, "State change: starter exited\n" );
+		dprintf( D_ALWAYS, "State change: starter exited : %s(%d)\n", __FILE__, __LINE__ );
 		if (rip->acceptClaimRequest()) {
 				// Successfully accepted the claim and changed state.
 			return TRUE;

docs/version-history/v24-version.hist
@@ -5,6 +5,17 @@
   of a job which uses file transfer.
   :jira:`2837`
 
+- The *condor_startd* will now keep dynamic slots that have a :ad-attr:`SlotBrokenReason`
+  attribute in ``Unclaimed`` state rather than deleting them when they change state
+  to ``Unclaimed``.  A new configuration variable :macro:`CONTINUE_TO_ADVERTISE_BROKEN_DYNAMIC_SLOTS`
+  controls this behavior.  It defaults to ``true`` but can be set to ``false`` to preserve
+  the old behavior.  This change also adds a new attribute :ad-attr:`BrokenContextAds`
+  to the daemon ad of the *condor_startd*. This attribute has a classad for each broken resource
+  in the startd.  *condor_status* has been enhanced to use this new attribute to display
+  more information about the context of broken resources when both ``-startd`` and ``-broken``
+  arguments are used.
+  :jira:`2844`
+
 *** 24.5.0 bugs
 
 - Fixed a bug where the *condor_gridmanager* would write to log file

src/condor_startd.V6/ResAttributes.cpp
@@ -627,7 +627,7 @@ const char * MachAttributes::AllocateDevId(const std::string & tag, const char *
 	return NULL;
 }
 
-bool MachAttributes::ReleaseDynamicDevId(const std::string & tag, const char * id, int was_assigned_to, int was_assign_to_sub)
+bool MachAttributes::ReleaseDynamicDevId(const std::string & tag, const char * id, int was_assigned_to, int was_assign_to_sub, int new_sub)
 {
 	auto found(m_machres_nft_map.find(tag));
 	if (found != m_machres_nft_map.end()) {
@@ -637,11 +637,11 @@ bool MachAttributes::ReleaseDynamicDevId(const std::string & tag, const char * i
 			NonFungibleRes & nfr = nft.ids[ixid];
 			if (nfr.id == id) {
 				if (nfr.owner.id == was_assigned_to && nfr.owner.dyn_id == was_assign_to_sub) {
-					nfr.owner.dyn_id = 0;
+					nfr.owner.dyn_id = new_sub;
 					return true;
 				}
 				if (nfr.bkowner.id == was_assigned_to && nfr.bkowner.dyn_id == was_assign_to_sub) {
-					nfr.bkowner.dyn_id = 0;
+					nfr.bkowner.dyn_id = new_sub;
 					return true;
 				}
 			}
@@ -788,6 +788,27 @@ int MachAttributes::RefreshDevIds(
 	}
 	return num_res;
 }
+
+// return a list of devids that are assigned to the given broken id
+int MachAttributes::ReportBrokenDevIds(const std::string & tag, slotres_assigned_ids_t & devids, int broken_sub_id)
+{
+	devids.clear();
+
+	auto found = machres_devIds().find(tag);
+	if (found == machres_devIds().end()) {
+		return 0;
+	}
+
+	int num_res = 0;
+	for (const auto & nfr : found->second.ids) {
+		if (nfr.owner.dyn_id == broken_sub_id) {
+			devids.emplace_back(nfr.id);
+			num_res += 1;
+		}
+	}
+	return num_res;
+}
+
 
 // calculate an aggregate properties classad for the given resource tag from the given resource ids
 // this classad will be merged into the slot classad that has those resources assigned.
@@ -1940,7 +1961,7 @@ CpuAttributes::bind_DevIds(MachAttributes* map, int slot_id, int slot_sub_id, bo
 }
 
 void
-CpuAttributes::unbind_DevIds(MachAttributes* map, int slot_id, int slot_sub_id) // release non-fungable resource ids
+CpuAttributes::unbind_DevIds(MachAttributes* map, int slot_id, int slot_sub_id, int new_sub_id) // release non-fungable resource ids
 {
 	if ( ! map) return;
 
@@ -1956,7 +1977,7 @@ CpuAttributes::unbind_DevIds(MachAttributes* map, int slot_id, int slot_sub_id) 
 		if (k != c_slotres_ids_map.end()) {
 			slotres_assigned_ids_t & ids = c_slotres_ids_map[j.first];
 			while ( ! ids.empty()) {
-				bool released = map->ReleaseDynamicDevId(j.first, ids.back().c_str(), slot_id, slot_sub_id);
+				bool released = map->ReleaseDynamicDevId(j.first, ids.back().c_str(), slot_id, slot_sub_id, new_sub_id);
 				dprintf(released ? d_log_devids : D_ALWAYS, "ubind DevIds for slot%d.%d unbind %s %d %s\n",
 					slot_id, slot_sub_id, ids.back().c_str(), (int)ids.size(), released ? "OK" : "failed");
 				ids.pop_back();

src/condor_startd.V6/ResAttributes.h
@@ -271,11 +271,12 @@ public:
 	const slotres_nft_map_t& machres_devIds() const { return m_machres_nft_map; }
 	const ClassAd& machres_attrs() const { return m_machres_attr; }
 	const char * AllocateDevId(const std::string & tag, const char* request, int assign_to, int assign_to_sub, bool backfill);
-	bool         ReleaseDynamicDevId(const std::string & tag, const char * id, int was_assign_to, int was_assign_to_sub);
+	bool         ReleaseDynamicDevId(const std::string & tag, const char * id, int was_assign_to, int was_assign_to_sub, int new_sub=0);
 	bool         DevIdMatches(const NonFungibleType & nft, int ixid, ConstraintHolder & require);
 	const char * DumpDevIds(std::string & buf, const char * tag = NULL, const char * sep = "\n");
 	void         ReconfigOfflineDevIds();
 	int          RefreshDevIds(const std::string & tag, slotres_assigned_ids_t & slot_res_devids, int assign_to, int assign_to_sub);
+	int          ReportBrokenDevIds(const std::string & tag, slotres_assigned_ids_t & devids, int broken_sub_id);
 	bool         ComputeDevProps(ClassAd & ad, const std::string & tag, const slotres_assigned_ids_t & ids);
 	//bool ReAssignDevId(const std::string & tag, const char * id, void * was_assigned_to, void * assign_to);
 
@@ -436,7 +437,7 @@ public:
 
 	void attach( Resource* );	// Attach to the given Resource
 	bool bind_DevIds(MachAttributes* map, int slot_id, int slot_sub_id, bool backfill_slot, bool abort_on_fail);   // bind non-fungable resource ids to a slot
-	void unbind_DevIds(MachAttributes* map, int slot_id, int slot_sub_id); // release non-fungable resource ids
+	void unbind_DevIds(MachAttributes* map, int slot_id, int slot_sub_id, int new_sub_id=0); // release non-fungable resource ids
 	void reconfig_DevIds(MachAttributes* map, int slot_id, int slot_sub_id); // check for offline changes for non-fungible resource ids
 
 	void publish_static(ClassAd*, const ResBag * inuse) const;  // Publish desired info to given CA
@@ -474,7 +475,7 @@ public:
     const slotres_devIds_map_t & get_slotres_ids_map() { return c_slotres_ids_map; }
 
 	void set_broken(int code, std::string_view reason) { c_broken_code = code; c_broken_reason = reason; }
-	bool is_broken(std::string * reason=nullptr) const { if (reason) *reason = c_broken_reason; return c_broken_code; }
+	unsigned int is_broken(std::string * reason=nullptr) const { if (reason) *reason = c_broken_reason; return c_broken_code; }
 
 	void init_total_disk(const CpuAttributes* r_attr) {
 		if (r_attr && (r_attr->c_execute_partition_id == c_execute_partition_id)) {
@@ -550,6 +551,7 @@ public:
 	ResBag& operator+=(const CpuAttributes& rhs);
 	ResBag& operator-=(const CpuAttributes& rhs);
 
+	bool empty() {return (cpus<=0) && !disk && !mem && resmap.empty();}
 	void reset();
 	bool underrun(std::string * names);
 	bool excess(std::string * names);
@@ -557,12 +559,13 @@ public:
 	void clear_excess();   // reset only the excess values
 	const char * dump(std::string & buf) const;
 	void Publish(ClassAd& ad, const char * prefix) const;
+	const MachAttributes::slotres_map_t & nfrmap() { return resmap; }
 
 protected:
-	double     cpus = 0;
-	long long  disk = 0;
-	int        mem = 0;
-	int        slots = 0;
+	double     cpus{0};
+	long long  disk{0};
+	int        mem{0};
+	int        slots{0};
 	MachAttributes::slotres_map_t resmap;
 	friend class CpuAttributes;
 };

src/condor_startd.V6/ResMgr.cpp
@@ -218,7 +218,8 @@ ResMgr::~ResMgr()
 #endif /* HAVE_HIBERNATION */
 
 
-	for (auto rip : slots) { delete rip; }
+	// disconnect slots from parent (if any still exist) and then delete them
+	for (auto rip : slots) { rip->set_parent(nullptr); delete rip; }
 	slots.clear();
 	delete [] type_nums;
 	if( new_type_nums ) {
@@ -336,6 +337,75 @@ ResMgr::final_update_daemon_ad()
 	resmgr->send_update( INVALIDATE_STARTD_ADS, &invalidate_ad, NULL, false );
 }
 
+// build and return a BrokenContext ad, this ad will be inserted into the StartDaemon ad
+ClassAd * BrokenItem::new_context_ad()
+{
+	ClassAd * ad = new ClassAd();
+	ad->Assign("Id", b_tag);
+	if (b_code) ad->Assign("Code", b_code);
+	if (b_time) ad->Assign("Time", b_time);
+	if ( ! b_reason.empty()) ad->Assign("Reason", b_reason);
+	classad::References attrs;
+	param_and_insert_attrs("BROKEN_SLOT_CONTEXT_ATTRS", attrs);
+	if (b_client) {
+		if (attrs.count(ATTR_REMOTE_USER) && ! b_client->c_user.empty()) {
+			ad->Assign(ATTR_REMOTE_USER, b_client->c_user);
+			attrs.erase(ATTR_REMOTE_USER);
+		}
+		if (attrs.count(ATTR_REMOTE_SCHEDD_NAME) &&  ! b_client->c_scheddName.empty()) {
+			ad->Assign(ATTR_REMOTE_SCHEDD_NAME, b_client->c_scheddName);
+			attrs.erase(ATTR_REMOTE_SCHEDD_NAME);
+		}
+		if (attrs.count(ATTR_CLIENT_MACHINE) && ! b_client->c_host.empty() ) {
+			ad->Assign(ATTR_CLIENT_MACHINE, b_client->c_host);
+			attrs.erase(ATTR_CLIENT_MACHINE);
+		}
+	}
+	if (b_context) {
+		// special case for JobId attribute, which isn't in the client object, or as a single attribute in the job
+		if (attrs.count(ATTR_JOB_ID)) {
+			JOB_ID_KEY jid;
+			b_context->LookupInteger(ATTR_CLUSTER_ID, jid.cluster);
+			b_context->LookupInteger(ATTR_PROC_ID, jid.proc);
+			ad->Assign(ATTR_JOB_ID, std::string(jid));
+			attrs.erase(ATTR_JOB_ID);
+		}
+		for (auto const & attr : attrs) {
+			classad::Value val;
+			if (b_context->EvaluateAttr(attr, val, classad::Value::SCALAR_VALUES)) {
+				ad->InsertLiteral(attr, classad::Literal::MakeLiteral(val));
+			}
+		}
+	}
+
+	// publish resource quantities
+	if ( ! b_res.empty()) {
+		b_res.Publish(*ad, "");
+		int broken_sub_id = (1000*1000) + b_id;
+		for (const auto & [tag,quan] : b_res.nfrmap()) {
+			MachAttributes::slotres_assigned_ids_t devids;
+			if (resmgr->m_attr->ReportBrokenDevIds(tag, devids, broken_sub_id)) {
+				std::string idlist = join(devids, ",");
+				std::string attr = "Assigned" + tag;
+				ad->Assign(attr, idlist);
+			}
+		}
+	}
+	return ad;
+}
+
+// return a,b,c etc, or aa,bb,cc, etc depending on num
+static std::string tag_uniqifier(int num) {
+	std::string aa;
+	if (--num < 1) return aa;
+	while (num > 0) {
+		int digit = num % 26;
+		aa.push_back('a' + (digit-1));
+		num -= 26;
+	}
+	return aa;
+}
+
 void
 ResMgr::publish_daemon_ad(ClassAd & ad)
 {
@@ -358,37 +428,40 @@ ResMgr::publish_daemon_ad(ClassAd & ad)
 	excess_backfill_res.Publish(ad, "ExcessBackfill");
 
 	// publish broken slot list and broken slot reasons
-	std::string broken_slots;
-	std::set<unsigned int> reported_types;
-	for(Resource* rip : slots) {
-		if ( ! rip) continue;
-		std::string reason;
-		if (rip->r_attr->is_broken(&reason)) {
-			if ( ! broken_slots.empty()) broken_slots += ",";
-			broken_slots += rip->r_id_str;
-			broken_slots += "BrokenReason";
-			std::string attr(rip->r_id_str); attr += "BrokenReason";
-			ad.Assign(attr, reason);
-			reported_types.insert(rip->type_id());
+	// TODO: publish contextual information along with the list of broken resources
+	std::string broken_reasons;
+	std::string broken_contexts;
+	std::map<std::string, int> broken_tags;
+	for (auto & brit : broken_things) {
+		// b_tag values in the BrokenItems aren't necessarily unique
+		// but we need the attribute names to be unique, so we append a,b,c etc to the 2nd and subsequent
+		std::string tag = brit.b_tag;
+		if (++broken_tags[tag] > 1) tag += tag_uniqifier(broken_tags[tag]);
+		std::string attr = tag + "BrokenReason";
+		if ( ! broken_reasons.empty()) broken_reasons += ",";
+		broken_reasons += attr;
+		ad.Assign(attr, brit.b_reason);
+		if (brit.b_context || brit.b_client) {
+			attr = tag + "BrokenContext";
+			if ( ! broken_contexts.empty()) broken_contexts += ",";
+			ad.Insert(attr, brit.new_context_ad());
+			broken_contexts += attr;
 		}
 	}
 
-	// check for slot types so broken there are missing slots
-	for (int type_id = 0; type_id < max_types; ++type_id) {
-		if (bad_slot_types[type_id]) {
-			std::string attr, reason;
-			formatstr(attr, "slot_type%dBrokenReason", type_id);
-			if ( ! broken_slots.empty()) broken_slots += ",";
-			broken_slots += attr;
-			formatstr(reason, "Could not create required number of type%d slots", type_id);
-			ad.Assign(attr, reason);
-		}
+	if ( ! broken_reasons.empty()) {
+		broken_reasons.insert(0,"{");
+		broken_reasons.push_back('}');
+		ad.AssignExpr("BrokenReasons", broken_reasons.c_str());
+		// for backward compat, assign a BrokenSlots attribute that just refs the new BrokenReasons attribute
+		// TODO: add add for compat with 24.2,  remove someday
+		ad.AssignExpr("BrokenSlots", "BrokenReasons");
 	}
 
-	if ( ! broken_slots.empty()) {
-		broken_slots.insert(0,"{");
-		broken_slots.push_back('}');
-		ad.AssignExpr("BrokenSlots", broken_slots.c_str());
+	if ( ! broken_contexts.empty()) {
+		broken_contexts.insert(0,"{");
+		broken_contexts.push_back('}');
+		ad.AssignExpr("BrokenContextAds", broken_contexts.c_str());
 	}
 
 	// static information about custom resources
@@ -584,6 +657,7 @@ ResMgr::init_resources( void )
 {
 	int i=0, num_res;
 	CpuAttributes** new_cpu_attrs;
+	std::vector<bool> bad_slot_types;
 	bool *bkfill_bools = nullptr;
 
 	// See if the config file defines a valid set of CpuAttributes objects.
@@ -636,7 +710,27 @@ ResMgr::init_resources( void )
 		// populate it.
 		for( i=0; i<num_res; i++ ) {
 			CpuAttributes * cpu_attrs = new_cpu_attrs[i];
-			if (cpu_attrs) { addResource( new Resource(cpu_attrs, i+1)); }
+			if (cpu_attrs) {
+				Resource * rip = new Resource(cpu_attrs, i+1);
+				// create a broken_things record for each resource that is born broken
+				// TODO: maybe these things should not be added to the slots array?
+				if (cpu_attrs->is_broken()) {
+					auto & brit = broken_things.emplace_back();
+					brit.b_tag = rip->r_id_str;
+					brit.b_code = rip->r_attr->is_broken(&brit.b_reason);
+					brit.b_refptr = rip;
+				}
+				addResource(rip);
+			}
+		}
+	}
+
+	// create a broken_thing for each slot type that we were unable to create all of the slots for
+	for (int type_id = 0; type_id < max_types; ++type_id) {
+		if (bad_slot_types[type_id]) {
+			auto & brit = broken_things.emplace_back();
+			brit.b_tag = "slot_type" + std::to_string(type_id);
+			formatstr(brit.b_reason, "Could not create required number of type%d slots", type_id);
 		}
 	}
 
@@ -954,6 +1048,26 @@ ResMgr::get_by_slot_id( int id )
 	return NULL;
 }
 
+BrokenItem &
+ResMgr::get_broken_context(Resource * rip)
+{
+	// look for an existing broken record, and return it
+	for (BrokenItem & brit : broken_things) {
+		if (brit.b_refptr == (void*)rip) {
+			return brit;
+		}
+	}
+
+	// no broken record, make a new one and partially initialize it
+	BrokenItem & brit = broken_things.emplace_back(BrokenItem());
+	brit.b_id = (int)broken_things.size();
+	brit.b_time = time(nullptr);
+	brit.b_refptr = rip;
+	brit.b_tag = rip->r_id_str;
+	return brit;
+}
+
+
 State
 ResMgr::state( void )
 {
@@ -1121,7 +1235,7 @@ void ResMgr::send_updates_and_clear_dirty(int /*timerID = -1*/)
 				}
 			}
 
-			dprintf(D_ZKM, "ENABLE_STARTD_DAEMON_AD is AUTO, collector_list has %d modern and %d/%d old/unknown collectors\n",
+			dprintf(D_FULLDEBUG, "ENABLE_STARTD_DAEMON_AD is AUTO, collector_list has %d modern and %d/%d old/unknown collectors\n",
 				num_modern, num_old, num_unknown);
 
 			if (num_modern > 0 && num_old == 0 && num_unknown == 0) {
@@ -1139,7 +1253,7 @@ void ResMgr::send_updates_and_clear_dirty(int /*timerID = -1*/)
 				// (which opens a persistent connnection) has been sent.  Instead we just set the
 				// daemon ad dirty bit, which will queue a timer for another update of just the daemon ad
 				if (whyfor_mask != (1<<Resource::WhyFor::wf_daemonAd)) {
-					dprintf(D_ZKM, "Queueing STARTD daemon ad update after slot ad updates are started\n");
+					//dprintf(D_ZKM, "Queueing STARTD daemon ad update after slot ad updates are started\n");
 					rip_update_needed(1<<Resource::WhyFor::wf_daemonAd);
 					send_daemon_ad_first = false;
 				}
@@ -1156,7 +1270,7 @@ void ResMgr::send_updates_and_clear_dirty(int /*timerID = -1*/)
 			auto * collectorList = daemonCore->getCollectorList();
 			if (collectorList) { collectorList->checkVersionBeforeSendingUpdates(false); }
 		}
-		dprintf(D_ZKM, "Sending STARTD daemon ad update to collectors\n");
+		// dprintf(D_ZKM, "Sending STARTD daemon ad update to collectors\n");
 		publish_daemon_ad(public_ad);
 		send_update(UPDATE_STARTD_AD, &public_ad, nullptr, true);
 	}
@@ -1790,7 +1904,7 @@ ResMgr::got_cmd_xevent()
 	if (console_slots > 0 || keyboard_slots > 0) {
 		// machine_keyboard_idle() should still be the cached KeyboardIdle value at this point
 		if (m_attr->machine_keyboard_idle() > update_interval && poll_tid <= 0) {
-			dprintf(D_ZKM, "got_x_event during no-polling interval, refreshing slots connected to keyboard/console\n");
+			//dprintf(D_ZKM, "got_x_event during no-polling interval, refreshing slots connected to keyboard/console\n");
 			// keyboard has been idle for a while, and there are slots connected to the keyboard
 			// but we aren't currently running a policy evaluation poll timer so we won't
 			// be telling the collector about our potential change of availability for a while
@@ -1972,6 +2086,11 @@ void ResMgr::_remove_and_delete_slot_res(Resource * rip)
 	// remove the resource from our slot collection
 	auto last = std::remove(slots.begin(), slots.end(), rip);
 	if (last != slots.end()) { slots.erase(last, slots.end()); }
+
+	// for safety, clear a possible broken things reference (in case of pointer re-use)
+	for (auto & brit : broken_things) {
+		if (brit.b_refptr == (void*)rip) brit.b_refptr = nullptr;
+	}
 
 	// and now we can delete the object itself.
 	delete rip;

src/condor_startd.V6/ResMgr.h
@@ -150,6 +150,34 @@ public:
 	}
 };
 
+// holds a broken resource, slot of slot_type
+class BrokenItem {
+public:
+	//BrokenItem() = default;
+	//~BrokenItem() = default;
+	//BrokenItem(const BrokenItem &) = default;
+	//BrokenItem(BrokenItem &&) = default;
+	//BrokenItem & operator=(const BrokenItem &) = default;
+
+	unsigned int b_id{0};   // an id that we can bind GPUs to
+	unsigned int b_code{0}; // a reason code
+	time_t       b_time{0}; // the time we logged the brokenness
+	void *       b_refptr{nullptr}; // holds a pointer to the trigger object, (a Resource * for broken slots)
+	std::string  b_tag;     // item tag, same as r_slot_id for broken slots
+	std::string  b_reason;  // reason string
+
+	// ad holds arbitrary context information that correlates to the brokenness
+	// from job and Client* object of the claim for broken resources and slots
+	std::unique_ptr<ClassAd> b_context{nullptr};
+	std::unique_ptr<Client> b_client{nullptr};
+
+	// resources held by this item (used when not held by a slot)
+	ResBag b_res;
+
+	// create a new classad from this object for adding into the STARTD daemon ad
+	ClassAd * new_context_ad();
+};
+
 class ResMgr : public Service
 {
 public:
@@ -284,6 +312,7 @@ public:
 	Resource*	get_by_slot_id(int);	// Find rip by r_id
 	State		state( void );			// Return the machine state
 
+	BrokenItem & get_broken_context(Resource * rip); // find or allocate a broken context for this slot
 
 	void report_updates( void ) const;	// Log updates w/ dprintf()
 
@@ -454,6 +483,9 @@ private:
 	// but may not be for brief periods during slot creation
 	std::vector<Resource*> slots;
 
+	// the collection of broken things, could be slots, slot_types, or resources
+	std::vector<BrokenItem> broken_things;
+
 	// The resources-in-use collections. this is recalculated each time
 	// compute_resource_conflicts is called and is used by both the daemon-ad and by the
 	// backfill slot advertising code
@@ -474,9 +506,8 @@ private:
 	time_t	cur_time;		// current time
 	time_t	deathTime = 0;		// If non-zero, time we will SIGTERM
 
-	std::vector<std::string> type_strings;	// Array of strings that
-		// define the resource types specified in the config file.  
-	std::vector<bool> bad_slot_types; // Array of slot types which had init time slot creation failures
+	// Array of strings that define the resource types specified in the config file.
+	std::vector<std::string> type_strings;
 	int*		type_nums;		// Number of each type.
 	int*		new_type_nums;	// New numbers of each type.
 	int			max_types;		// Maximum # of types.

src/condor_startd.V6/Resource.cpp
@@ -394,8 +394,17 @@ Resource::clear_parent()
 
 	// If we have a parent, return our resources to it
 	if( m_parent && !m_currently_fetching ) {
-		r_attr->unbind_DevIds(resmgr->m_attr, r_id, r_sub_id);
-		*(m_parent->r_attr) += *(r_attr);
+		if (r_attr->is_broken()) {
+			// we don't give broken d-slot resources back to the parent
+			// we bind the GPUs to an invalid d-slot id and let the fungible resources be lost
+			// the broken_context knows the resource quantities already
+			auto & brit = resmgr->get_broken_context(this);
+			int broken_sub_id = (1000*1000) + brit.b_id;
+			r_attr->unbind_DevIds(resmgr->m_attr, r_id, r_sub_id, broken_sub_id);
+		} else {
+			r_attr->unbind_DevIds(resmgr->m_attr, r_id, r_sub_id);
+			*(m_parent->r_attr) += *(r_attr);
+		}
 		m_parent->m_id_dispenser->insert( r_sub_id );
 		resmgr->refresh_classad_resources(m_parent);
 		m_parent->update_needed(wf_dslotDelete);
@@ -865,6 +874,34 @@ Resource::hackLoadForCOD( void )
 	r_classad->Assign( ATTR_CPU_BUSY_TIME, 0 );
 }
 
+void
+Resource::set_broken_context(const Client* client, std::unique_ptr<ClassAd> & job)
+{
+	// we save only the *first* broken context we get for each slot
+	// so here we find or create a BrokenItem object and initialize it
+	// if it is not empty
+	auto & brit = resmgr->get_broken_context(this);
+
+	// if we don't have a broken reason yet, copy the one in the r_attr
+	if (brit.b_reason.empty() && r_attr) {
+		brit.b_code = r_attr->is_broken(&brit.b_reason);
+	}
+	// remember the size of the resource quantities
+	if (brit.b_res.empty() && r_attr) {
+		brit.b_res += *r_attr;
+	}
+
+	// we steal the job ad here, since the only time we are passed one is
+	// from a claim that is being deleted.
+	if (job.get() && ! brit.b_context.get()) {
+		brit.b_context = std::move(job);
+	}
+	// we can't steal the client object from the claim, so make a copy here instead
+	if (client && ! brit.b_client.get()) {
+		brit.b_client.reset(new Client(*client));
+	}
+}
+
 
 void
 Resource::starterExited( Claim* cur_claim )
@@ -900,7 +937,8 @@ Resource::starterExited( Claim* cur_claim )
 		// exiting, so let folks know that happened.  The logic in
 		// leave_preempting_state() is more complicated, and we'll
 		// describe why we make the change we do in there.
-	dprintf( D_ALWAYS, "State change: starter exited\n" );
+	dprintf( D_ALWAYS, "State change: starter exited : %s(%d) %s/%s\n",
+		__FILE__, __LINE__, state_to_string(state()), activity_to_string(activity()) );
 
 	State s = state();
 	Activity a = activity();
@@ -1037,7 +1075,7 @@ Resource::leave_preempting_state( void )
 {
 	bool tmp;
 
-	if (r_cur) { r_cur->vacate(); } // Send a vacate to the client of the claim
+	if (r_cur) { r_cur->vacate(); } // Send a vacate to the client of the claim (schedd or hook)
 	delete r_cur;
 	r_cur = NULL;
 

src/condor_startd.V6/Resource.h
@@ -272,6 +272,9 @@ public:
 		// Called when the starter of one of our claims exits
 	void	starterExited( Claim* cur_claim );
 
+		// save context for broken slots, this will take ownership of the job classad
+	void	set_broken_context(const Client* client, std::unique_ptr<ClassAd> & job);
+
 		// Since the preempting state is so weird, and when we want to
 		// leave it, we need to decide where we want to go, and we
 		// have to do lots of funky twiddling with our claim objects,

commit 455cccc5b395fbb5195b0a148876941bc88e20e7

Fix claim re-use by checking IS_OWNER. HTCONDOR-3045
Fix claim re-use by having the startd check IS_OWNER
instead of checking for START=?=False to determine if
a claim on a slot should be relinquished when a job completes.
Claim re-use broke with commit 5fcb6dad2b.

src/condor_startd.V6/ResState.cpp
@@ -330,14 +330,14 @@ ResState::eval_policy( void )
 			change( preempting_state );
 			return TRUE; // XXX: change TRUE
 		}
-		if( (r_act == idle_act) && (rip->eval_start() == 0) ) {
-				// START evaluates to False, so return to the owner
-				// state.  In this case, we don't need to worry about
-				// START locally evaluating to FALSE due to undefined
-				// job attributes and well-placed meta-operators, b/c
-				// we're in the claimed state, so we'll have a job ad
-				// to evaluate against.
-			dprintf( D_ALWAYS, "State change: START is false\n" );
+		if( (r_act == idle_act) && (rip->eval_is_owner()) ) {
+				// Be warned: we may not have a job ad here, so we don't
+				// want to just look at the START expression, as it could
+				// eval to False even if it could match a job (due to use
+				// of meta-operators or defaults for job ad values).
+				// So we explicitly wanna look at IS_OWNER here instead
+				// of START.
+			dprintf( D_ALWAYS, "State change: IS_OWNER is true\n" );
 			// TLM: STATE TRANSITION #10
 			change( preempting_state );
 			return TRUE; // XXX: change TRUE