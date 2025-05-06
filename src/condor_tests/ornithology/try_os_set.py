4acf8c75b01d9325236cd021464693e87b7ca01c
Todd L Miller <tlmiller@cs.wisc.edu>
2025-05-06 14:03:02 -0500

    (HTCONDOR-2887)  Windows test fix.

--- src/condor_tests/ornithology/try_os_set.py
+++ src/condor_tests/ornithology/try_os_set.py
@@ -1,8 +1,5 @@
 import os
 
-from pwd import getpwnam
-from grp import getgrnam
-
 
 def try_os_setegid(number=None, name=None):
     # This can happen if we're calling this function with is previous result.
@@ -10,11 +7,15 @@ def try_os_setegid(number=None, name=None):
         return None
 
     try:
+        from grp import getgrnam
+
         egid = os.getegid()
         if number is None:
             number = getgrnam(name).gr_gid
         os.setegid(number)
         return egid
+    except ModuleNotFoundError:
+        return None
     except PermissionError:
         return None
     except KeyError:
@@ -26,12 +27,17 @@ def try_os_seteuid(number=None, name=None):
     if number is None and name is None:
         return None
 
+
     try:
+        from pwd import getpwnam
+
         euid=os.geteuid()
         if number is None:
             number = getpwnam(name).pw_uid
         os.seteuid(number)
         return euid
+    except ModuleNotFoundError:
+        return None
     except PermissionError:
         return None
     except KeyError:
