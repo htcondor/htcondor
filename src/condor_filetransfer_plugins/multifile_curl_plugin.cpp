b7c71d87a5130ff1a889b1fa11ec106e83a40a1c
Todd L Miller <tlmiller@cs.wisc.edu>
2025-06-12 16:28:29 -0500

    (HTCONDOR-3121)  Add user-agent header for weird-URL compability.

--- src/condor_filetransfer_plugins/multifile_curl_plugin.cpp
+++ src/condor_filetransfer_plugins/multifile_curl_plugin.cpp
@@ -259,6 +259,14 @@ MultiFileCurlPlugin::InitializeCurlHandle(const std::string &url, const std::str
 			fprintf(stderr, "Can't setopt HEADERFUNCTION\n");
 		}
 
+        // We MUST successfully set the USERAGENT, because some URLs
+        // will 403 without one. (!!)  [https://repo.anaconda.com]
+        // ... blowing as ASSERT() here is probably not OK, though.
+        r = curl_easy_setopt( _handle, CURLOPT_USERAGENT, "multifile_curl_plugin/" "PLUGIN_VERSION" );
+        if (r != CURL_OK) {
+            fprintf(stderr, "Can't setopt USERAGENT\n");
+        }
+
         GetToken(cred, token);
     }
     // Libcurl options for FTP
@@ -929,7 +937,7 @@ MultiFileCurlPlugin::InitializeStats( std::string request_url ) {
 
 size_t
 MultiFileCurlPlugin::HeaderCallback( char* buffer, size_t size, size_t nitems, void *userdata ) {
-    fprintf(stderr, "[MultiFileCurlPlugin::HeaderCallback] called\n");
+    // fprintf(stderr, "[MultiFileCurlPlugin::HeaderCallback] called\n");
     auto ft_stats = static_cast<FileTransferStats*>(userdata);
 
     // Work around a bug in libcurl; see HTCONDOR-1426.
