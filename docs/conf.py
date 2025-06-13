2a4c9541bacb353c74054b4f97c93f6a92efade8
Tim Theisen <tim@cs.wisc.edu>
2025-06-13 11:11:33 -0500

    HTCONDOR-3058: Bump version number to 24.9.1 for code change

--- docs/conf.py
+++ docs/conf.py
@@ -74,7 +74,7 @@ Licensed under the Apache License, Version 2.0.'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '24.9.0'
+release = '24.9.1'
 
 rst_epilog = """
 .. |release_date| replace:: June 24, 2025
