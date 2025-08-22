f334a605fe561cb26586ded55c55e6c07ff3d070
Jaime Frey <jfrey@cs.wisc.edu>
2025-08-22 12:00:35 -0500

    HTCONDOR-3201 Fix python doc strings

--- bindings/python/htcondor2/_security_context.py
+++ bindings/python/htcondor2/_security_context.py
@@ -1,6 +1,10 @@
 
 
 class SecurityContext():
+    """
+    Alterations to the default configuration used when performing
+    authentication between with an HTCondor daemon over the network.
+    """
 
     def __init__(self, token: str = None):
         self._preferred_token = token
