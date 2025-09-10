89420b3d62a72901e4781aa65898af21ac307fee
Greg Thain <gthain@cs.wisc.edu>
2025-09-10 13:12:13 -0500

    HTCONDOR-3243 add test for singularity_target_dir and file xfer

--- src/condor_tests/test_singularity_sif.py
+++ src/condor_tests/test_singularity_sif.py
@@ -70,14 +70,15 @@ def condor(test_dir):
     with Condor(test_dir / "condor", config={"SINGULARITY_BIND_EXPR": "\"/bin:/bin /usr:/usr /lib:/lib /lib64:/lib64\""}) as condor:
         yield condor
 
+# Setup a personal condor with SINGULARITY_TARGET_DIR set
 @standup
 def condor_with_td(test_dir):
-    # Bind all the hosts's bin and lib dirs into the container, so can run /bin/cat and similar commands
-    # without building a large container with lots of shared libraries
-    with Condor(test_dir / "condor_td", config={"SINGULARITY_BIND_EXPR": "\"/bin:/bin /usr:/usr /lib:/lib /lib64:/lib64\"",
-                                             "SINGULARITY_TARGET_DIR": "/td"}) as condor_with_td:
+    with Condor(test_dir / "condor_td", config={
+        "SINGULARITY_BIND_EXPR": "\"/bin:/bin /usr:/usr /lib:/lib /lib64:/lib64\"",
+        "SINGULARITY_TARGET_DIR": "/td"}) as condor_with_td:
         yield condor_with_td
 
+# The actual job will cat a file
 # The actual job will cat a file
 @action
 def test_job_hash():
@@ -94,21 +95,6 @@ def test_job_hash():
             "leave_in_queue": "True"
             }
 
-# The actual job will cat a file
-@action
-def test_job_hash_with_td():
-    return {
-            "universe": "container",
-            "container_image": "empty.sif",
-            "executable": "/bin/cat",
-            "arguments": "/singularity",
-            "should_transfer_files": "yes",
-            "when_to_transfer_output": "on_exit",
-            "output": "output",
-            "error": "error",
-            "log": "log_td",
-            "leave_in_queue": "True"
-            }
 @action
 def completed_test_job(condor, test_job_hash):
 
@@ -125,12 +111,31 @@ def completed_test_job(condor, test_job_hash):
 
     return ctj.query()[0]
 
-
 @action
-def completed_test_job_with_td(condor_with_td, test_job_hash_with_td):
+def test_job_hash_with_xfer():
+    return {
+            "universe": "container",
+            "container_image": "empty.sif",
+            "executable": "/bin/cp",
+            "arguments": "an_input_file an_output_file",
+            "transfer_input_files": "an_input_file",
+            "should_transfer_files": "yes",
+            "when_to_transfer_output": "on_exit",
+            "transfer_executable": "False",
+            "output": "output",
+            "error": "error",
+            "log": "log_td",
+            "leave_in_queue": "True"
+            }
+# Test that file xfer works with SINGULARITY_TARGET_DIR
+@action
+def completed_test_job_with_xfer(condor_with_td, test_job_hash_with_xfer):
+
+    with open("an_input_file", "w") as f:
+        f.write("This is the input file\n")
 
     ctj_td = condor_with_td.submit(
-        {**test_job_hash_with_td}, count=1
+        {**test_job_hash_with_xfer}, count=1
     )
 
     assert ctj_td.wait(
@@ -182,14 +187,11 @@ def UserNamespacesFunctional():
         print("unshare command failed, test cannot work, skipping test\n")
         return False
 
-@pytest.mark.skipif(not SingularityIsWorthy(), reason="No worthy Singularity/Apptainer found")
-@pytest.mark.skipif(not UserNamespacesFunctional(), reason="User namespaces not working -- some limit hit?")
-@pytest.mark.skipif(not SingularityIsWorking(), reason="Singularity doesn't seem to be working")
 class TestContainerUni:
+    @pytest.mark.skipif(not SingularityIsWorthy(), reason="No worthy Singularity/Apptainer found")
+    @pytest.mark.skipif(not UserNamespacesFunctional(), reason="User namespaces not working -- some limit hit?")
+    @pytest.mark.skipif(not SingularityIsWorking(), reason="Singularity doesn't seem to be working")
     def test_container_uni(self, sif_file_fixture, completed_test_job):
             assert completed_test_job['ExitCode'] == 0
-
-    def test_container_uni_with_td(self, sif_file_fixture, completed_test_job_with_td):
-            assert completed_test_job_with_td['ExitCode'] == 0
-
-
+    def test_container_uni_with_xfer(self, completed_test_job_with_xfer):
+            assert completed_test_job_with_xfer['ExitCode'] == 0
