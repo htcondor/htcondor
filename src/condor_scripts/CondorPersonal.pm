28fcd96f70e7c8abc0343ffd954004ea628deda5
Greg Thain <gthain@cs.wisc.edu>
2025-09-29 14:10:32 -0500

    HTCONDOR-3303 Robustify regression tests by not testing singularity start in tests that don't need it or look at the results

--- src/condor_scripts/CondorPersonal.pm
+++ src/condor_scripts/CondorPersonal.pm
@@ -1215,6 +1215,7 @@ debug( "HMMMMMMMMMMM personal local is $personal_local , mytoppath is $mytoppath
 				print NEW "JOB_START_DELAY = 0\n";
 				print NEW "DAGMAN_USER_LOG_SCAN_INTERVAL = 1\n";
 				print NEW "SINGULARITY_TEST_SANDBOX_TIMEOUT = 8\n";
+				print NEW "SINGULARITY = /usr/bin/false\n";
 				print NEW "LOCK = \$(LOG)\n";
 				if($iswindows == 1) {
 				#print NEW "PROCD_LOG = \$(LOG)/ProcLog\n";
