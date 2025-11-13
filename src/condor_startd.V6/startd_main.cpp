e427ac64f2f799a4d8ebc21b3f3c7ab2e542b629
Todd L Miller <tlmiller@cs.wisc.edu>
2025-11-13 12:54:02 -0600

    (HTCONDOR-3222) Typo in previous commit, s/D/R/g.

--- src/condor_startd.V6/startd_main.cpp
+++ src/condor_startd.V6/startd_main.cpp
@@ -338,8 +338,8 @@ main_init( int, char* argv[] )
 								  "DEACTIVATE_CLAIM_JOB_DONE",
 								  command_handler,
 								  "command_handler", DAEMON );
-	daemonCore->Register_Command( DEACTIVATE_CLAIM_CHECK,
-								  "DEACTIVATE_CLAIM_CHECK",
+	daemonCore->Register_Command( REACTIVATE_CLAIM_CHECK,
+								  "REACTIVATE_CLAIM_CHECK",
 								  command_handler,
 								  "command_handler", DAEMON );
 
