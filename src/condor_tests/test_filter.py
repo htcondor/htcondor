ea8282e7f87996e9de812c70fa982ca19c20c190
Greg Thain <gthain@cs.wisc.edu>
2025-12-15 10:52:46 -0600

    HTCONDOR-3247 Also bump timeout on test_filter while we are here

--- src/condor_tests/test_filter.py
+++ src/condor_tests/test_filter.py
@@ -16,5 +16,5 @@ def test_filter_exe_results():
     rv = subprocess.run(["test_filter.exe"],
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
-        timeout=2)
+        timeout=20)
     assert rv.returncode == 0
