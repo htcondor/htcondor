247fdce13355edcc2bceb112284f5dea066aa98c
Tim Theisen <tim@cs.wisc.edu>
2026-03-30 21:36:10 -0500

    HTCONDOR-3579: Bump version to 25.8.1 in V25_8_0-branch

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,7 @@ Licensed under the Apache License, Version 2.0.'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '25.8.0'
+release = '25.8.1'
 
 rst_epilog = """
 .. |release_date| replace:: April 16, 2026
