e2ade69f67301a7d9a7cf6af5e75221a1211360e
Greg Thain <gthain@cs.wisc.edu>
2026-04-01 10:18:49 -0500

    HTCONDOR-3638 Documentation

--- src/condor_starter.V6.1/singularity.cpp
+++ src/condor_starter.V6.1/singularity.cpp
@@ -443,7 +443,7 @@ Singularity::setup(ClassAd &machineAd,
 	}
 
 	if (job_iwd != execute_dir) {
-		// File xfer off, if image was a relative path, prepend iwd
+		// File xfer off, if image was a relative path, prepend iwd, unless an URL
 		if (image[0] != '/' && image.find("://") == std::string::npos) {
 			image = job_iwd + '/' + image;
 		}
