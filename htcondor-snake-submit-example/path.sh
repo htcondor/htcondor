86b1a503b6f449d08fe2279d9bab18c230402355
houth <houth@ap2001.chtc.wisc.edu>
2026-06-23 16:33:25 -0500

    added readme instruction for the command and an example folder

---
+++ htcondor-snake-submit-example/path.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# look at this path for executables
+echo 'export PYTHONPATH="/home/houth/snakemake-executor-dev/htcondor-snake-testing/htcondor/src/condor_tools:$PYTHONPATH"' >> ~/.bashrc
+source ~/.bashrc
+
