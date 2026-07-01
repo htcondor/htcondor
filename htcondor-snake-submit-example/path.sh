3a922f443d61f3eaa748585209dbb0cb23941434
ghouth <houth@wisc.edu>
2026-07-01 17:03:26 +0000

    removed accidental additions of htcondor-snake-submit guides

--- htcondor-snake-submit-example/path.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# look at this path for executables
-echo 'export PYTHONPATH="/home/houth/snakemake-executor-dev/htcondor-snake-testing/htcondor/src/condor_tools:$PYTHONPATH"' >> ~/.bashrc
-source ~/.bashrc
-
