86bfb6bf5e5db481cb863ae8edb4b67396536217
Tim Theisen <tim@cs.wisc.edu>
2026-07-07 07:23:51 -0500

    HTCONDOR-3747: Bump version to 25.12.2

--- docs/conf.py
+++ docs/conf.py
@@ -76,7 +76,7 @@ Licensed under the Apache License, Version 2.0.'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '25.12.1'
+release = '25.12.2'
 
 rst_prolog = """
 .. role:: config(code)
