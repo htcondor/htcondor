8525e4cad8ec05cc1bab52a93272b532fa71ee75
Todd L Miller <tlmiller@cs.wisc.edu>
2026-08-10 13:57:54 -0500

    (HTCONDOR-3859)  This error should never happen, but if it does, handle it sanely.

--- src/condor_utils/submit_utils.cpp
+++ src/condor_utils/submit_utils.cpp
@@ -7027,7 +7027,8 @@ int SubmitHash::process_container_input_files(std::vector<std::string> & input_f
 			if(! AWSv4Impl::doSha256( hash_key, messageDigest, & mdLength )) {
 				// There's doesn't seem to be a failure path out of this
 				// function, so for now, just fail catastrophically.
-				EXCEPT( "Failed to container image ('%s', hashed as '%s'), aborting.\n", container_image.ptr(), hash_key.c_str() );
+				push_error( stderr, "Failed to hash container image ('%s', hashed as '%s'), aborting.\n", container_image.ptr(), hash_key.c_str() );
+				ABORT_AND_RETURN(2);
 			}
 
 			std::string catalogName;
