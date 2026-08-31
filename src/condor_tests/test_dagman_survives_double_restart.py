3b7c855362fd912cd552371654d757419bd0ab41
Greg Thain <gthain@cs.wisc.edu>
2026-08-31 10:44:36 -0500

    HTCONDOR-3665 More dagman test robustification

--- src/condor_tests/test_dagman_survives_double_restart.py
+++ src/condor_tests/test_dagman_survives_double_restart.py
@@ -89,6 +89,7 @@ def condor(test_dir):
         local_dir=test_dir / "condor",
         config={
             "SCHEDULER_UNIVERSE_COOL_DOWN_DURATION": 3,
+            "SCHEDD_INTERVAL": 5,
             "SCHEDD_INTERVAL_TIMESLICE": 1.0,
             "SCHEDD_MIN_INTERVAL": 2,
             # See test_dagman_survives_ap_restart.py: speed up DAGMan's own
